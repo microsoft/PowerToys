3dbef1400c4de97fcf8ebb4b3952b4eff4a19b35
qianlifeng <qianlf2008@163.com>
2014-03-23 16:20:00 +0800

    Refactor setting storage related

---
+++ Wox.Infrastructure/Storage/UserSettings/PluginHotkey.cs
@@ -0,0 +1,8 @@
+﻿namespace Wox.Infrastructure.Storage.UserSettings
+{
+    public class CustomPluginHotkey
+    {
+        public string Hotkey { get; set; }
+        public string ActionKeyword { get; set; }
+    }
+}
