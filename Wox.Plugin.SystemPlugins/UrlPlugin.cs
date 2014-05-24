be25226172757cc4c35f67e20d938cb0b95eee9d
Yaroslav Zyubanov <antigravity56@yandex.ru>
2014-05-24 14:29:56 +0600

    URL plugin

---
+++ Wox.Plugin.SystemPlugins/UrlPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Wox.Plugin.SystemPlugins
+{
+    public class UrlPlugin : BaseSystemPlugin
+    {
+        protected override List<Result> QueryInternal(Query query)
+        {
+            var raw = query.RawQuery;
+            Uri uri;
+            if (Uri.TryCreate(raw, UriKind.Absolute, out uri))
+            {
+                return new List<Result>
+                {
+                    new Result
+                    {
+                        Title = raw,
+                        SubTitle = "Open the typed URL...",
+                        IcoPath = "Images/url1.png",
+                        Score = 8,
+                        Action = _ =>
+                        {
+                            Process.Start(raw);
+                            return true;
+                        }
+                    }
+                };
+            }
+            return new List<Result>(0);
+        }
+
+        public override string Name { get { return "URL handler"; } }
+        public override string Description { get { return "Open the typed URL..."; } }
+        public override string IcoPath { get { return "Images/url2.png"; } }
+
+        protected override void InitInternal(PluginInitContext context)
+        {
+        }
+    }
+}
