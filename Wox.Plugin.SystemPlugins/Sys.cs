3f1064b22d4ebb2223a9e82cd0a59c0191e6dc13
zlsun <zlsun1995@gmail.com>
2014-06-16 14:08:48 +0800

    Use sleep instead of ping.

--- Wox.Plugin.SystemPlugins/Sys.cs
+++ Wox.Plugin.SystemPlugins/Sys.cs
@@ -94,7 +94,7 @@ namespace Wox.Plugin.SystemPlugins
                 Action = (c) =>
                 {
                     ProcessStartInfo Info = new ProcessStartInfo();
-                    Info.Arguments = "/C ping 127.0.0.1 -n 1 && \"" + Application.ExecutablePath + "\"";
+                    Info.Arguments = "/C sleep 1 && \"" + Application.ExecutablePath + "\"";
                     Info.WindowStyle = ProcessWindowStyle.Hidden;
                     Info.CreateNoWindow = true;
                     Info.FileName = "cmd.exe";
