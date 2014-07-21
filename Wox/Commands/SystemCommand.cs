0a45a6fea9efc9b48010097403240ef93b897e0d
qianlifeng <qianlf2008@163.com>
2014-07-21 23:18:39 +0800

    #129 exclude system plugins results when searching websearch.

--- Wox/Commands/SystemCommand.cs
+++ Wox/Commands/SystemCommand.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using Wox.Infrastructure.Storage.UserSettings;
 using Wox.Plugin;
+using Wox.Plugin.SystemPlugins;
 using Wox.PluginLoader;
 
 namespace Wox.Commands
@@ -13,16 +14,25 @@ namespace Wox.Commands
     {
         public override void Dispatch(Query query)
         {
-            foreach (PluginPair pair in Plugins.AllPlugins.Where(o => o.Metadata.PluginType == PluginType.System))
+            var allSytemPlugins = Plugins.AllPlugins.Where(o => o.Metadata.PluginType == PluginType.System);
+            if (UserSettingStorage.Instance.WebSearches.Exists(o => o.ActionWord == query.ActionName && o.Enabled))
+            {
+                //websearch mode
+                allSytemPlugins = new List<PluginPair>()
+                {
+                    allSytemPlugins.First(o => ((ISystemPlugin)o.Plugin).ID == "565B73353DBF4806919830B9202EE3BF")
+                };
+            }
+
+            foreach (PluginPair pair in allSytemPlugins)
             {
                 PluginPair pair1 = pair;
-
                 ThreadPool.QueueUserWorkItem(state =>
                 {
                     List<Result> results = pair1.Plugin.Query(query);
-                    results.ForEach(o=> { o.AutoAjustScore = true; });
+                    results.ForEach(o => { o.AutoAjustScore = true; });
 
-                    App.Window.PushResults(query,pair1.Metadata,results);
+                    App.Window.PushResults(query, pair1.Metadata, results);
                 });
             }
         }
