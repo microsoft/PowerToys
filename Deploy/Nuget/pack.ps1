0fefead7d5576a6a2e15791d07e824ed0f7e8321
qianlifeng <qianlf2008@163.com>
2014-12-12 18:32:21 +0800

    Update auto build script

--- Deploy/Nuget/pack.ps1
+++ Deploy/Nuget/pack.ps1
@@ -9,4 +9,4 @@ $content = $content -replace '\$version\$',$versionStr
 
 $content | Out-File $root\wox.plugin.nuspec
 
-& $root\deploy\NuGet\NuGet.exe pack $root\deploy\nuget\wox.plugin.nuspec
+& $root\.nuget\NuGet.exe pack $root\deploy\nuget\wox.plugin.nuspec
