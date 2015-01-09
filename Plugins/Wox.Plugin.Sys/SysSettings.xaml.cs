56788c8b8fb17870647035e535963e6a4f4a0e42
qianlifeng <qianlf2008@163.com>
2015-01-09 09:44:03 +0800

    Project structure change

--- Plugins/Wox.Plugin.Sys/SysSettings.xaml.cs
+++ Plugins/Wox.Plugin.Sys/SysSettings.xaml.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Controls;
 
-namespace Wox.Plugin.Sys {
-	/// <summary>
-	/// Interaction logic for SysSettings.xaml
-	/// </summary>
-	public partial class SysSettings : UserControl {
-		public SysSettings(List<Result> Results) {
-			InitializeComponent();
+namespace Wox.Plugin.Sys
+{
+    /// <summary>
+    /// Interaction logic for SysSettings.xaml
+    /// </summary>
+    public partial class SysSettings : UserControl
+    {
+        public SysSettings(List<Result> Results)
+        {
+            InitializeComponent();
 
-			foreach (var Result in Results) {
-				this.lbxCommands.Items.Add(Result);
-			}
-		}
-	}
+            foreach (var Result in Results)
+            {
+                this.lbxCommands.Items.Add(Result);
+            }
+        }
+    }
 }
