8d746cd2064038a997e08df599e3e0512b49b5ae
qianlifeng <qianlf2008@163.com>
2015-01-15 20:47:48 +0800

    Upgrade to .net framework 4.5

--- Plugins/Wox.Plugin.Caculator/Calculator.cs
+++ Plugins/Wox.Plugin.Caculator/Calculator.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Windows;
 using YAMP;
 
-namespace Wox.Plugin.SystemPlugins
+namespace Wox.Plugin.Caculator
 {
     public class Calculator : IPlugin
     {
--- Wox/ImageLoader/ImageLoader.cs
+++ Wox/ImageLoader/ImageLoader.cs
@@ -37,16 +37,17 @@ namespace Wox.ImageLoader
 
         private static ImageSource GetIcon(string fileName)
         {
-            if (System.IO.File.Exists(fileName) == false) return null;
-
-            Icon icon = GetFileIcon(fileName);
-            if (icon == null) icon = Icon.ExtractAssociatedIcon(fileName);
-
-            if (icon != null)
+            try
             {
-                return System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(icon.Handle,
-                    new Int32Rect(0, 0, icon.Width, icon.Height), BitmapSizeOptions.FromEmptyOptions());
+                if (File.Exists(fileName) == false) return null;
+                Icon icon = GetFileIcon(fileName) ?? Icon.ExtractAssociatedIcon(fileName);
+                if (icon != null)
+                {
+                    return System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(icon.Handle,
+                        new Int32Rect(0, 0, icon.Width, icon.Height), BitmapSizeOptions.FromEmptyOptions());
+                }
             }
+            catch{}
 
             return null;
         }
