Wox-launcher/Wox

commit a5abe9234faafcd170f9468a70f42bf60eda0f0d

Refactoring DelayInvoke
In MainWindow.xaml.cs, the reference of originQuery, lastQuery and
tbQuery.Text are same, so remove originQuery is fine.

Wox/MainWindow.xaml.cs
@@ -457,27 +457,27 @@ namespace Wox
             int searchDelay = GetSearchDelay(lastQuery);
 
             Dispatcher.DelayInvoke("UpdateSearch",
-                o =>
+                () =>
                 {
-                    Dispatcher.DelayInvoke("ClearResults", i =>
+                    Dispatcher.DelayInvoke("ClearResults", () =>
                     {
                         // first try to use clear method inside pnlResult, which is more closer to the add new results
                         // and this will not bring splash issues.After waiting 100ms, if there still no results added, we
                         // must clear the result. otherwise, it will be confused why the query changed, but the results
                         // didn't.
                         if (pnlResult.Dirty) pnlResult.Clear();
-                    }, TimeSpan.FromMilliseconds(100), null);
+                    }, TimeSpan.FromMilliseconds(100));
                     queryHasReturn = false;
                     Query query = new Query(lastQuery);
                     query.IsIntantQuery = searchDelay == 0;
                     Query(query);
-                    Dispatcher.DelayInvoke("ShowProgressbar", originQuery =>
+                    Dispatcher.DelayInvoke("ShowProgressbar", () =>
                     {
-                        if (!queryHasReturn && originQuery == tbQuery.Text && !string.IsNullOrEmpty(lastQuery))
+                        if (!queryHasReturn && !string.IsNullOrEmpty(lastQuery))
                         {
                             StartProgress();
                         }
-                    }, TimeSpan.FromMilliseconds(150), tbQuery.Text);
+                    }, TimeSpan.FromMilliseconds(150));
                     //reset query history index after user start new query
                     ResetQueryHistoryIndex();
                 }, TimeSpan.FromMilliseconds(searchDelay));

Wox/Helper/DispatcherExtensions.cs
@@ -12,20 +12,13 @@ namespace Wox
             new Dictionary<string, DispatcherTimer>();
         private static readonly object syncRoot = new object();
 
-        public static string DelayInvoke(this Dispatcher dispatcher, string namedInvocation,
-            Action<string> action, TimeSpan delay,
-            DispatcherPriority priority = DispatcherPriority.Normal)
-        {
-            return DelayInvoke(dispatcher, namedInvocation, action, delay, string.Empty, priority);
-        }
-
-        public static string DelayInvoke(this Dispatcher dispatcher, string namedInvocation,
-            Action<string> action, TimeSpan delay, string arg,
+        public static void DelayInvoke(this Dispatcher dispatcher, string namedInvocation,
+            Action action, TimeSpan delay,
             DispatcherPriority priority = DispatcherPriority.Normal)
         {
             lock (syncRoot)
             {
-                if (String.IsNullOrEmpty(namedInvocation))
+                if (string.IsNullOrEmpty(namedInvocation))
                 {
                     namedInvocation = Guid.NewGuid().ToString();
                 }
@@ -36,11 +29,10 @@ namespace Wox
                 var timer = new DispatcherTimer(delay, priority, (s, e) =>
                 {
                     RemoveTimer(namedInvocation);
-                    action(arg);
+                    action();
                 }, dispatcher);
                 timer.Start();
                 timers.Add(namedInvocation, timer);
-                return namedInvocation;
             }
         }
 

Wox/HotkeyControl.xaml.cs
@@ -53,7 +53,7 @@ namespace Wox
             }
 
             Dispatcher.DelayInvoke("HotkeyAvailabilityTest", 
-                o =>
+                () =>
                 {
                     SetHotkey(hotkeyModel);
                 },

Wox/Msg.xaml.cs
@@ -71,11 +71,11 @@ namespace Wox {
 			Show();
 
 			Dispatcher.DelayInvoke("ShowMsg",
-								   o => {
-									   if (!closing) {
-										   closing = true;
-										   Dispatcher.Invoke(new Action(fadeOutStoryboard.Begin));
-									   }
+								   () => {
+								            if (!closing) {
+								                closing = true;
+								                Dispatcher.Invoke(new Action(fadeOutStoryboard.Begin));
+								            }
 								   }, TimeSpan.FromSeconds(3));
 		}
 	}

Wox/ResultPanel.xaml.cs
@@ -225,7 +225,7 @@ namespace Wox
             if (e.AddedItems.Count > 0 && e.AddedItems[0] != null)
             {
                 lbResults.ScrollIntoView(e.AddedItems[0]);
-                Dispatcher.DelayInvoke("UpdateItemNumber", o =>
+                Dispatcher.DelayInvoke("UpdateItemNumber", () =>
                 {
                     UpdateItemNumber();
                 }, TimeSpan.FromMilliseconds(3));

Wox/SettingWindow.xaml.cs
@@ -464,7 +464,7 @@ namespace Wox
 
         private void DelayChangeTheme()
         {
-            Dispatcher.DelayInvoke("delayChangeTheme", o =>
+            Dispatcher.DelayInvoke("delayChangeTheme", () =>
             {
                 ThemeManager.Theme.ChangeTheme(UserSettingStorage.Instance.Theme);
             }, TimeSpan.FromMilliseconds(100));

microsoft/PowerToys

commit 3085799c3c3650494be4dad278f5969b2f495d7f

Fix #357
Fix bug introduced since a5abe9234faafcd170f9468a70f42bf60eda0f0d

Wox/MainWindow.xaml.cs
@@ -464,7 +464,7 @@ namespace Wox
             Query(tbQuery.Text);
             Dispatcher.DelayInvoke("ShowProgressbar", () =>
             {
-                if (!queryHasReturn && !string.IsNullOrEmpty(lastQuery))
+                if (!queryHasReturn && !string.IsNullOrEmpty(tbQuery.Text) && tbQuery.Text != lastQuery)
                 {
                     StartProgress();
                 }