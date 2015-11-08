Wox-launcher/Wox

commit 9962ddf2ed80a00036ebdb2965429675c4355472

Fix #357
Fix bug introduced since a5abe9234faafcd170f9468a70f42bf60eda0f0d

Wox/MainWindow.xaml.cs
@@ -464,7 +464,7 @@ namespace Wox
             Query(tbQuery.Text);
             Dispatcher.DelayInvoke("ShowProgressbar", () =>
             {
-                if (!queryHasReturn && !string.IsNullOrEmpty(lastQuery))
+                if (!queryHasReturn && !string.IsNullOrEmpty(tbQuery.Text) && tbQuery.Text != lastQuery)
                 {
                     StartProgress();
                 }

microsoft/PowerToys

commit 543cd5af802c17e9728f3123d72eb13ce797df86

Fix progress bar
Bug introduced in 9962ddf2ed80a00036ebdb2965429675c4355472

Wox/MainWindow.xaml.cs
@@ -443,25 +443,19 @@ namespace Wox
 
         private void TbQuery_OnTextChanged(object sender, TextChangedEventArgs e)
         {
-
             if (_ignoreTextChange) { _ignoreTextChange = false; return; }
-            string query = tbQuery.Text.Trim();
+
             toolTip.IsOpen = false;
             if (IsInContextMenuMode)
             {
                 QueryContextMenu();
                 return;
             }
+
+            string query = tbQuery.Text.Trim();
             if (!string.IsNullOrEmpty(query))
             {
                 Query(query);
-                Dispatcher.DelayInvoke("ShowProgressbar", () =>
-                {
-                    if (!string.IsNullOrEmpty(query) && query != _lastQuery.RawQuery && !_queryHasReturn)
-                    {
-                        StartProgress();
-                    }
-                }, TimeSpan.FromMilliseconds(150));
                 //reset query history index after user start new query
                 ResetQueryHistoryIndex();
             }
@@ -478,6 +472,7 @@ namespace Wox
         }
         private void Query(string text)
         {
+            _queryHasReturn = false;
             var query = PluginManager.QueryInit(text);
             if (query != null)
             {
@@ -503,6 +498,13 @@ namespace Wox
                     }
                 }
                 _lastQuery = query;
+                Dispatcher.DelayInvoke("ShowProgressbar", () =>
+                {
+                    if (!string.IsNullOrEmpty(query.RawQuery) && query.RawQuery == _lastQuery.RawQuery && !_queryHasReturn)
+                    {
+                        StartProgress();
+                    }
+                }, TimeSpan.FromMilliseconds(150));
                 PluginManager.QueryForAllPlugins(query);
             }
             StopProgress();