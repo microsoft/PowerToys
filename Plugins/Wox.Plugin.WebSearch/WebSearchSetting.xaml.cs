Wox-launcher/Wox

commit 38cc115c1f9e1efbb2256f6e88168493797adb5c

Fix icon path for Web Search plugin

Plugins/Wox.Plugin.WebSearch/WebSearchSetting.xaml.cs
@@ -50,13 +50,7 @@ namespace Wox.Plugin.WebSearch
 
         private void ShowIcon(string path)
         {
-            try
-            {
-                imgIcon.Source = new BitmapImage(new Uri(path, UriKind.Relative));
-            }
-            catch (Exception)
-            {
-            }
+            imgIcon.Source = new BitmapImage(new Uri($"{_defaultWebSearchImageDirectory}\\{path}", UriKind.Absolute));
         }
 
         private void BtnCancel_OnClick(object sender, RoutedEventArgs e)
@@ -86,13 +80,6 @@ namespace Wox.Plugin.WebSearch
             }
 
             string newActionKeyword = tbActionword.Text.Trim();
-            if (string.IsNullOrEmpty(newActionKeyword))
-            {
-                string warning = _context.API.GetTranslation("wox_plugin_websearch_input_action_keyword");
-                MessageBox.Show(warning);
-                return;
-            }
-
             if (_isUpdate)
             {
                 try
@@ -155,8 +142,8 @@ namespace Wox.Plugin.WebSearch
             if (result == true)
             {
                 string filename = dlg.FileName;
-                tbIconPath.Text = filename;
-                ShowIcon(filename);
+                tbIconPath.Text = Path.GetFileName(filename);
+                ShowIcon(tbIconPath.Text);
             }
         }
     }

Plugins/Wox.Plugin.WebSearch/WebSearchStorage.cs
@@ -41,7 +41,7 @@ namespace Wox.Plugin.WebSearch
             {
                 Title = "Google",
                 ActionKeyword = "g",
-                IconPath = @"Images\websearch\google.png",
+                IconPath = @"google.png",
                 Url = "https://www.google.com/search?q={q}",
                 Enabled = true
             };
@@ -52,7 +52,7 @@ namespace Wox.Plugin.WebSearch
             {
                 Title = "Wikipedia",
                 ActionKeyword = "wiki",
-                IconPath = @"Images\websearch\wiki.png",
+                IconPath = @"wiki.png",
                 Url = "http://en.wikipedia.org/wiki/{q}",
                 Enabled = true
             };
@@ -62,7 +62,7 @@ namespace Wox.Plugin.WebSearch
             {
                 Title = "FindIcon",
                 ActionKeyword = "findicon",
-                IconPath = @"Images\websearch\pictures.png",
+                IconPath = @"pictures.png",
                 Url = "http://findicons.com/search/{q}",
                 Enabled = true
             };

microsoft/PowerToys

commit 8b97914438192516c49181672fa6b0aa5abe61d2

Fix web search icon
part of #384
bug introduced in 38cc115c1f9e1efbb2256f6e88168493797adb5c

Plugins/Wox.Plugin.WebSearch/WebSearchSetting.xaml.cs
@@ -11,7 +11,8 @@ namespace Wox.Plugin.WebSearch
 {
     public partial class WebSearchSetting : Window
     {
-        private string _defaultWebSearchImageDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Images\\websearch");
+        private const string _imageDirectoryName = "Images";
+        private string _pluginDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         private readonly WebSearchesSetting _settingWindow;
         private bool _isUpdate;
         private WebSearch _updateWebSearch;
@@ -50,7 +51,7 @@ namespace Wox.Plugin.WebSearch
 
         private void ShowIcon(string path)
         {
-            imgIcon.Source = new BitmapImage(new Uri($"{_defaultWebSearchImageDirectory}\\{path}", UriKind.Absolute));
+            imgIcon.Source = new BitmapImage(new Uri(Path.Combine(_pluginDirectory, path), UriKind.Absolute));
         }
 
         private void BtnCancel_OnClick(object sender, RoutedEventArgs e)
@@ -126,15 +127,15 @@ namespace Wox.Plugin.WebSearch
 
         private void BtnSelectIcon_OnClick(object sender, RoutedEventArgs e)
         {
-            if (!Directory.Exists(_defaultWebSearchImageDirectory))
+            if (!Directory.Exists(_pluginDirectory))
             {
-                _defaultWebSearchImageDirectory =
+                _pluginDirectory =
                     Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             }
 
             var dlg = new OpenFileDialog
             {
-                InitialDirectory = _defaultWebSearchImageDirectory,
+                InitialDirectory = Path.Combine(_pluginDirectory, _imageDirectoryName),
                 Filter = "Image files (*.jpg, *.jpeg, *.gif, *.png, *.bmp) |*.jpg; *.jpeg; *.gif; *.png; *.bmp"
             };
 
@@ -142,8 +143,11 @@ namespace Wox.Plugin.WebSearch
             if (result == true)
             {
                 string filename = dlg.FileName;
-                tbIconPath.Text = Path.GetFileName(filename);
-                ShowIcon(tbIconPath.Text);
+                if (filename != null)
+                {
+                    tbIconPath.Text = Path.Combine(_imageDirectoryName, Path.GetFileName(filename));
+                    ShowIcon(tbIconPath.Text);
+                }
             }
         }
     }

Plugins/Wox.Plugin.WebSearch/WebSearchStorage.cs
@@ -41,7 +41,7 @@ namespace Wox.Plugin.WebSearch
             {
                 Title = "Google",
                 ActionKeyword = "g",
-                IconPath = @"google.png",
+                IconPath = @"Images\google.png",
                 Url = "https://www.google.com/search?q={q}",
                 Enabled = true
             };
@@ -52,7 +52,7 @@ namespace Wox.Plugin.WebSearch
             {
                 Title = "Wikipedia",
                 ActionKeyword = "wiki",
-                IconPath = @"wiki.png",
+                IconPath = @"Images\wiki.png",
                 Url = "http://en.wikipedia.org/wiki/{q}",
                 Enabled = true
             };
@@ -62,7 +62,7 @@ namespace Wox.Plugin.WebSearch
             {
                 Title = "FindIcon",
                 ActionKeyword = "findicon",
-                IconPath = @"pictures.png",
+                IconPath = @"Images\pictures.png",
                 Url = "http://findicons.com/search/{q}",
                 Enabled = true
             };

Plugins/Wox.Plugin.WebSearch/Wox.Plugin.WebSearch.csproj
@@ -66,7 +66,13 @@
     <Compile Include="WebSearchStorage.cs" />
   </ItemGroup>
   <ItemGroup>
-    <None Include="Images\websearch\pictures.png">
+    <None Include="Images\google.png">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
+    <None Include="Images\pictures.png">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
+    <None Include="Images\wiki.png">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
     <Content Include="Languages\en.xaml">
@@ -111,16 +117,6 @@
   </ItemGroup>
   <ItemGroup>
     <None Include="Images\web_search.png">
-      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
-    </None>
-  </ItemGroup>
-  <ItemGroup>
-    <None Include="Images\websearch\google.png">
-      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
-    </None>
-  </ItemGroup>
-  <ItemGroup>
-    <None Include="Images\websearch\wiki.png">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
   </ItemGroup>