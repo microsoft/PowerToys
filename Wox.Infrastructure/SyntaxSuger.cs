6c705e1e25e1c8f64cf74d644a0abad1bac68de8
bao-qian <bao.github@outlook.com>
2016-04-22 23:29:38 +0100

    Add RequireNonNull
    http://www.yinwang.org/blog-cn/2015/11/21/programming-philosophy

---
+++ Wox.Infrastructure/SyntaxSuger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Wox.Infrastructure
+{
+    static class SyntaxSuger<T>
+    {
+        public static T RequireNonNull(T obj)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException();
+            }
+            else
+            {
+                return obj;
+            }
+        }
+    }
+}
