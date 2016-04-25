Wox-launcher/Wox

commit 152a7d719b15b3fb4559bea09b378b963555fe45

Fix #565
1. Fix resultlistbox visibility
2. Fix #565
3. Remove unused convertor

Wox/ViewModel/MainViewModel.cs
@@ -389,6 +389,7 @@ namespace Wox.ViewModel
                 else
                 {
                     Results.Clear();
+                    ResultListBoxVisibility = Visibility.Collapsed;
                 }
             }
         }
@@ -565,6 +566,11 @@ namespace Wox.ViewModel
             {
                 ResultListBoxVisibility = Visibility.Visible;
             }
+            else
+            {
+                ResultListBoxVisibility = Visibility.Collapsed;
+            }
+
         }
 
         #endregion

Wox/Converters/OpacityModeConverter.cs
@@ -1,40 +1,0 @@
-﻿using System;
-using System.Globalization;
-using Wox.Core.UserSettings;
-
-namespace Wox.Converters
-{
-    public class OpacityModeConverter : ConvertorBase<OpacityModeConverter>
-    {
-        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (!(value is OpacityMode)) return value.ToString();
-            var mode = (OpacityMode) value;
-            switch (mode)
-            {
-                case OpacityMode.Normal:
-                    return "Normal Window";
-                case OpacityMode.LayeredWindow:
-                {
-                    if (Environment.OSVersion.Version.Major < 5)
-                        return "Layered Window (not supported by your Windows)";
-                    if (Environment.OSVersion.Version.Major == 5)
-                        return "Layered Window (not recommended on your Windows)";
-                    return "Layered Window";
-                }
-                case OpacityMode.DWM:
-                {
-                    if (Environment.OSVersion.Version.Major < 6)
-                        return "DWM-Enabled Window (not supported by your Windows)";
-                    return "DWM-Enabled Window";
-                }
-            }
-            return value.ToString();
-        }
-
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            return this;
-        }
-    }
-}

Wox/Converters/StringEmptyConverter.cs
@@ -1,18 +1,0 @@
-﻿using System;
-using System.Globalization;
-
-namespace Wox.Converters
-{
-    public class StringEmptyConverter : ConvertorBase<StringEmptyConverter>
-    {
-        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return string.IsNullOrEmpty((string)value) ? parameter : value;
-        }
-
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            return this;
-        }
-    }
-}

Wox/Converters/StringNullOrEmptyToVisibilityConverter.cs
@@ -1,45 +1,0 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Windows;
-using Wox.Plugin;
-
-namespace Wox.Converters
-{
-    public class StringNullOrEmptyToVisibilityConverter : ConvertorBase<StringNullOrEmptyToVisibilityConverter>
-    {
-        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return string.IsNullOrEmpty(value as string) ? Visibility.Collapsed : Visibility.Visible;
-        }
-
-        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return null;
-        }
-
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            return this;
-        }
-    }
-
-    public class ContextMenuEmptyToWidthConverter : ConvertorBase<ContextMenuEmptyToWidthConverter>
-    {
-        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            List<Result> results = value as List<Result>;
-            return results == null || results.Count == 0 ? 0 : 17;
-        }
-
-        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return null;
-        }
-
-        public override object ProvideValue(IServiceProvider serviceProvider)
-        {
-            return this;
-        }
-    }
-}

Wox/ResultListBox.xaml
@@ -45,7 +45,6 @@
                                        VerticalAlignment="Center" ToolTip="{Binding Title}" x:Name="tbTitle"
                                        Text="{Binding Title}" />
                             <TextBlock Style="{DynamicResource ItemSubTitleStyle}" ToolTip="{Binding SubTitle}"
-                                       Visibility="{Binding SubTitle, Converter={converters:StringNullOrEmptyToVisibilityConverter}}"
                                        Grid.Row="1" x:Name="tbSubTitle" Text="{Binding SubTitle}" />
                         </Grid>
                     </Grid>

Wox/SettingWindow.xaml
@@ -79,7 +79,6 @@
                                         <TextBlock VerticalAlignment="Center" ToolTip="{Binding Metadata.Name}"
                                                    x:Name="tbTitle" Text="{Binding Metadata.Name}" />
                                         <TextBlock ToolTip="{Binding Metadata.Description}"
-                                                   Visibility="{Binding Metadata.Description, Converter={converters:StringNullOrEmptyToVisibilityConverter}}"
                                                    Grid.Row="1" x:Name="tbSubTitle"
                                                    Text="{Binding Metadata.Description}" Opacity="0.5" />
                                     </Grid>
@@ -115,8 +114,7 @@
                                                x:Name="pluginAuthor" Text="{DynamicResource author}" />
                                 </DockPanel>
                                 <TextBlock Grid.Row="1" x:Name="pluginSubTitle" Opacity="0.5"
-                                           ToolTip="{Binding Source=pluginSubTitle, Path=Text}"
-                                           Visibility="{Binding Source=pluginSubTitle, Path=Text, Converter={converters:StringNullOrEmptyToVisibilityConverter}}" />
+                                           ToolTip="{Binding Source=pluginSubTitle, Path=Text}"/>
                                 <DockPanel Grid.Row="2" Margin="0 10 0 8">
                                     <CheckBox x:Name="cbDisablePlugin" Click="CbDisablePlugin_OnClick">
                                         <TextBlock Text="{DynamicResource disable}" />

Wox/Wox.csproj
@@ -124,9 +124,6 @@
     <Compile Include="CommandArgs\ToggleCommandArg.cs" />
     <Compile Include="Converters\ConvertorBase.cs" />
     <Compile Include="Converters\ImagePathConverter.cs" />
-    <Compile Include="Converters\OpacityModeConverter.cs" />
-    <Compile Include="Converters\StringEmptyConverter.cs" />
-    <Compile Include="Converters\StringNullOrEmptyToVisibilityConverter.cs" />
     <Compile Include="Helper\VisibilityExtensions.cs" />
     <Compile Include="Helper\SingletonWindowOpener.cs" />
     <Compile Include="ImageLoader\ImageCache.cs" />

microsoft/PowerToys

commit 4a7e9c16d0f3f92c5e17df9c562b52bb71a70a10

Fix flashing bug introduced in 152a7d719b15b3fb4559bea09b378b963555fe45
part of #565

Wox/ViewModel/MainViewModel.cs
@@ -453,6 +453,7 @@ namespace Wox.ViewModel
                     }
                 };
                 action.Invoke();
+
                 var plugins = PluginManager.ValidPluginsForQuery(query);
                 foreach (var plugin in plugins)
                 {
@@ -466,9 +467,9 @@ namespace Wox.ViewModel
                         });
                     }
                 }
-            }
 
-            IsProgressBarTooltipVisible = false;
+             
+            }
         }
 
         private void ResetQueryHistoryIndex()
@@ -479,8 +480,7 @@ namespace Wox.ViewModel
 
         private void UpdateResultViewInternal(List<Result> list, PluginMetadata metadata)
         {
-            Stopwatch.Normal($"UI update cost for {metadata.Name}",
-                    () => { Results.AddResults(list, metadata.ID); });
+            Results.AddResults(list, metadata.ID);
         }
 
         private void DisplayQueryHistory(HistoryItem history)
@@ -562,13 +562,9 @@ namespace Wox.ViewModel
                 });
             }
 
-            if (list.Count > 0)
+            if (list.Count > 0 && ! ResultListBoxVisibility.IsVisible())
             {
                 ResultListBoxVisibility = Visibility.Visible;
-            }
-            else
-            {
-                ResultListBoxVisibility = Visibility.Collapsed;
             }
 
         }