784137fdaca63978055a6f12f732fabec97b3b04
bao-qian <bao.github@outlook.com>
2016-05-03 00:04:04 +0100

    Misc

--- Wox.Plugin/Feature.cs
+++ Wox.Plugin/Feature.cs
@@ -21,9 +21,9 @@ namespace Wox.Plugin
     /// Represent plugin query will be executed in UI thread directly. Don't do long-running operation in Query method if you implement this interface
     /// <remarks>This will improve the performance of instant search like websearch or cmd plugin</remarks>
     /// </summary>
+    [Obsolete("Wox is fast enough now, executed on ui thread is no longer needed")]
     public interface IInstantQuery : IFeatures
     {
-        [Obsolete("Empty interface is enough. it will be removed in v1.3.0 and possibly replaced by attribute")]
         bool IsInstantQuery(string query);
     }
 
