4645dd00119eff09ab5ad056dd8b13ba0ee4fdc9
Zaycn <dinecobra@hotmail.com>
2016-05-23 21:48:20 +0200

    Add dutch translation (#683)

--- Wox.Core/Resource/AvailableLanguages.cs
+++ Wox.Core/Resource/AvailableLanguages.cs
@@ -10,6 +10,7 @@ namespace Wox.Core.Resource
         public static Language Russian = new Language("ru", "Русский");
         public static Language French = new Language("fr", "Français");
         public static Language Japanese = new Language("ja", "日本語");
+        public static Language Dutch = new Language("nl", "Dutch");
 
         public static List<Language> GetAvailableLanguages()
         {
@@ -20,7 +21,9 @@ namespace Wox.Core.Resource
                 Chinese_TW,
                 Russian,
                 French,
-                Japanese
+                Japanese,
+                Dutch
+
             };
             return languages;
         }
