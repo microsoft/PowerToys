bec81ddbfe46a083b05792974166f0030df1f98a
bao-qian <bao.github@outlook.com>
2016-05-23 01:37:41 +0100

    misc

--- Wox/ViewModel/PluginViewModel.cs
+++ Wox/ViewModel/PluginViewModel.cs
@@ -18,15 +18,6 @@ namespace Wox.ViewModel
         public Visibility ActionKeywordsVisibility => Metadata.ActionKeywords.Count > 1 ? Visibility.Collapsed : Visibility.Visible;
         public string InitilizaTime => string.Format(_translator.GetTranslation("plugin_init_time"), Metadata.InitTime);
         public string QueryTime => string.Format(_translator.GetTranslation("plugin_query_time"), Metadata.AvgQueryTime);
-        public string ActionKeywordsText
-        {
-            get
-            {
-                var text = string.Join(Query.ActionKeywordSeperater, Metadata.ActionKeywords);
-                return text;
-            }
-        }
-
-       
+        public string ActionKeywordsText => string.Join(Query.ActionKeywordSeperater, Metadata.ActionKeywords);
     }
 }
