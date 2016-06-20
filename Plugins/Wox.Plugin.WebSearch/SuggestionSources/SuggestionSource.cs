91e9d7431a110fd01f2486ecc8064d4c8587e413
bao-qian <bao.github@outlook.com>
2016-06-21 00:17:55 +0100

    MVVM refactoring for web search plugin, part 2
    1. #486
    2. rename

--- Plugins/Wox.Plugin.WebSearch/SuggestionSources/SuggestionSource.cs
+++ Plugins/Wox.Plugin.WebSearch/SuggestionSources/SuggestionSource.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 
 namespace Wox.Plugin.WebSearch.SuggestionSources
 {
-    //todo rename file
     public abstract class SuggestionSource
     {
         public abstract Task<List<string>> Suggestions(string query);
