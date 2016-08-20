34a8d0b99b01b7b6a5c38c702ac147e6ba34e1ff
bao-qian <bao.github@outlook.com>
2016-08-20 18:19:13 +0100

    fixup

--- Plugins/Wox.Plugin.Program/Programs/Win32.cs
+++ Plugins/Wox.Plugin.Program/Programs/Win32.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Windows;
-using IWshRuntimeLibrary;
 using Microsoft.Win32;
 using Shell;
 using Wox.Infrastructure.Logger;
