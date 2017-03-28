d31347c0e0b9ffbaf0d49993f86b5bcc2f220d22
bao-qian <bao.github@outlook.com>
2017-03-28 22:30:55 +0100

    Revert "Remove first chance exception"
    This reverts commit 4aa4d98c08f3bcc176bd3ac1b9b718ab98298ca4.
    The squirrel crash is not caused by this #1320

--- Wox/App.xaml.cs
+++ Wox/App.xaml.cs
@@ -141,6 +141,8 @@ namespace Wox
         private static void RegisterAppDomainExceptions()
         {
             AppDomain.CurrentDomain.UnhandledException += ErrorReporting.UnhandledExceptionHandle;
+            AppDomain.CurrentDomain.FirstChanceException +=
+                (s, e) => { Log.Exception("|App.RegisterAppDomainExceptions|First Chance Exception:", e.Exception); };
         }
 
         public void Dispose()
