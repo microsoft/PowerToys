f4d0cc4dd1c04962f8bbd957a6f3b51352b5f465
bao-qian <bao.github@outlook.com>
2017-04-01 18:50:22 +0100

    Misc

--- Wox/App.xaml.cs
+++ Wox/App.xaml.cs
@@ -141,8 +141,10 @@ namespace Wox
         private static void RegisterAppDomainExceptions()
         {
             AppDomain.CurrentDomain.UnhandledException += ErrorReporting.UnhandledExceptionHandle;
-            AppDomain.CurrentDomain.FirstChanceException +=
-                (s, e) => { Log.Exception("|App.RegisterAppDomainExceptions|First Chance Exception:", e.Exception); };
+            AppDomain.CurrentDomain.FirstChanceException += (_, e) =>
+            {
+                Log.Exception("|App.RegisterAppDomainExceptions|First Chance Exception:", e.Exception);
+            };
         }
 
         public void Dispose()
