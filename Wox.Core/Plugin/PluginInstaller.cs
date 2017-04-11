fd4fec534030142180d5effad8859994a5fcdc57
Hüseyin Tüfekçilerli <huseyint@gmail.com>
2017-04-11 17:43:10 +0300

    Removed extra leading space char (#1368)

--- Wox.Core/Plugin/PluginInstaller.cs
+++ Wox.Core/Plugin/PluginInstaller.cs
@@ -83,7 +83,7 @@ namespace Wox.Core.Plugin
                     //    Plugins.Initialize();
                     //}
                     if (MessageBox.Show($"You have installed plugin {plugin.Name} successfully.{Environment.NewLine}" +
-                                        " Restart Wox to take effect?",
+                                        "Restart Wox to take effect?",
                                         "Install plugin", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
                     {
                         PluginManager.API.RestarApp();
