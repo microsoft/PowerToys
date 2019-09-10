1ca93891109d0eb95532295c78e260545ff6565b
Jeremy Wu <jeremy24wu@gmail.com>
2019-09-11 07:16:38 +1000

    Update class to inherite from Settings.ProgramSource

--- Plugins/Wox.Plugin.Program/Views/Models/ProgramSource.cs
+++ Plugins/Wox.Plugin.Program/Views/Models/ProgramSource.cs
@@ -1,19 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Wox.Plugin.Program.Views.Models
 {
-    public class ProgramSource
-    {
-        private string name;
-
-        public string Location { get; set; }
-        public string Name { get => name ?? new DirectoryInfo(Location).Name; set => name = value; }
-        public string UniqueIdentifier { get; set; }
-        public bool Enabled { get; set; } = true;
-    }
+    public class ProgramSource : Settings.ProgramSource { }
 }
