774ae1c8da6ebc0af469470794b78b29253229ec
Sasha Pierson <sashapie@microsoft.com>
2019-09-18 11:05:43 -0700

    change step names, verify build output

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,3 +1,9 @@
 cd /D "%~dp0"
 set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
-call %MSBUILD_EXE% ../PowerToys.sln || exit /b 1
+call %MSBUILD_EXE% ../PowerToys.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
+
+dir
+cd ..
+dir
+cd x64
+dir
