c836b22e485725ffa38b1a0c591e8c7f5613f055
Sasha Pierson <sashapie@microsoft.com>
2019-09-22 21:07:14 -0700

    set vs dev environment

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,3 +1,4 @@
 cd /D "%~dp0"
-set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
-call %MSBUILD_EXE% ../PowerToys.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
+REM set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0"
+call msbuild ../PowerToys.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
