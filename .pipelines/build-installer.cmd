5e72e51cddb13efcd1963b71fa8599d630a6dd2e
Sasha Pierson <sashapie@microsoft.com>
2019-09-22 17:32:57 -0700

    new container, try installer

--- .pipelines/build-installer.cmd
+++ .pipelines/build-installer.cmd
@@ -1,4 +1,8 @@
 cd /D "%~dp0"
 set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
 REM commented out until new container is finished
-REM call %MSBUILD_EXE% ../installer/PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
+call %MSBUILD_EXE% ../installer/PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
+
+dir
+cd installer\PowerToysSetup\x64\Release
+dir
