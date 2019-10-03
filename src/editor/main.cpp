3d7d9254bad1143f3d01c1ea31a6f6c0fa521d24
Enrico Giordani <enrico.giordani@gmail.com>
2019-10-03 08:44:44 +0200

    remove unused libs

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -10,11 +10,6 @@
 #pragma comment(lib, "shlwapi.lib")
 #pragma comment(lib, "shcore.lib")
 #pragma comment(lib, "windowsapp")
-#pragma comment(lib, "dxgi")
-#pragma comment(lib, "d3d11")
-#pragma comment(lib, "d2d1")
-#pragma comment(lib, "dcomp")
-#pragma comment(lib, "dwmapi")
 
 #ifdef _DEBUG
 #define _DEBUG_WITH_LOCALHOST 0
