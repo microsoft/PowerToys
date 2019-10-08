a4ba07a6751f7467f51d0f3934e7c3ed3062edb6
Sasha Pierson <sashapie@microsoft.com>
2019-10-08 16:08:41 -0700

    check if file is present

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,4 +1,10 @@
 cd /D "%~dp0"
 
+dir "C:\Program Files (x86)"
+dir "C:\Program Files (x86)\Microsoft Visual Studio"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0"
 call msbuild ../PowerToys.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
