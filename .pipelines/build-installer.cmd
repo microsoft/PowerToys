31b95194867bd1371882a2c51f90740f114b58f3
Sasha Pierson <sashapie@microsoft.com>
2019-10-08 14:23:19 -0700

    install and build scripts and new container

--- .pipelines/build-installer.cmd
+++ .pipelines/build-installer.cmd
@@ -1,8 +1,4 @@
 cd /D "%~dp0"
-REM set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0"
 call msbuild ../installer/PowerToysSetup.sln /p:Configuration=Release /p:Platform=x64 || exit /b 1
-
-dir
-cd installer\PowerToysSetup\x64\Release
-dir
