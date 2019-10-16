c059bdeeffab06736dd92aa92a75617ac2b22608
Enrico Giordani <enrico.giordani@gmail.com>
2019-10-16 14:20:08 +0200

    Trace the 'hide' event only if the guide was visible 'on_hide()' is invoked when the module is turned off, in that case don't trace the hide event.

--- src/modules/shortcut_guide/overlay_window.cpp
+++ src/modules/shortcut_guide/overlay_window.cpp
@@ -350,7 +350,11 @@ void D2DOverlayWindow::on_hide() {
     DwmUnregisterThumbnail(thumbnail);
   }
   std::chrono::steady_clock::time_point shown_end_time = std::chrono::steady_clock::now();
-  Trace::EventHide(std::chrono::duration_cast<std::chrono::milliseconds>(shown_end_time - shown_start_time).count(), key_pressed);
+  // Trace the event only if the overaly window was visible.
+  if (shown_start_time.time_since_epoch().count() > 0) {
+    Trace::EventHide(std::chrono::duration_cast<std::chrono::milliseconds>(shown_end_time - shown_start_time).count(), key_pressed);
+    shown_start_time = {};
+  }
   key_pressed.clear();
 }
 
