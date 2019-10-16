ac79803f1bf3a9bf6b0a3094fb6b3875e9d862c0
Jeremy Wu <jeremy24wu@gmail.com>
2019-10-16 22:38:44 +1100

    Update setting description summary

--- Plugins/Wox.Plugin.Program/Settings.cs
+++ Plugins/Wox.Plugin.Program/Settings.cs
@@ -20,8 +20,9 @@ namespace Wox.Plugin.Program
         /// Contains user added folder location contents as well as all user disabled applications
         /// </summary>
         /// <remarks>
-        /// <para>Win32 class applications sets UniqueIdentifier using their full file path</para>
-        /// <para>UWP class applications sets UniqueIdentifier using their Application User Model ID</para>
+        /// <para>Win32 class applications set UniqueIdentifier using their full file path</para>
+        /// <para>UWP class applications set UniqueIdentifier using their Application User Model ID</para>
+        /// <para>Custom user added program sources set UniqueIdentifier using their location</para>
         /// </remarks>
         public class ProgramSource
         {
