e219bec2d877362129bc54a115fad47f24715a60
Enrico Giordani <enrico.giordani@gmail.com>
2019-10-21 08:22:39 +0200

    Add PowerRename icon remove unused icons.

--- src/settings-web/src/setup_icons.tsx
+++ src/settings-web/src/setup_icons.tsx
@@ -3,24 +3,18 @@ import {registerIcons} from 'office-ui-fabric-react';
 import {initializeIcons} from './icons/src';
 
 // Import SVG files for the icons here.
-import AnimatedGifRecorderSVG from './svg/animated_gif_recorder.svg';
-import BatchRenamerSVG from './svg/batch_renamer.svg';
 import FancyZonesSVG from './svg/fancy_zones.svg';
+import PowerRenameSVG from './svg/power_rename.svg';
 import ShortcutGuideSVG from './svg/shortcut_guide.svg';
-import TerminateToolSVG from './svg/terminate_tool.svg';
-import MaximizeNewDesktopSVG from './svg/maximize_new_desktop.svg';
 
 export function setup_powertoys_icons(): void {
   initializeIcons('icons/fonts/');
 
   registerIcons({
     icons: {
-      'pt-animated-gif-recorder': ( <AnimatedGifRecorderSVG /> ),
-      'pt-batch-renamer': ( <BatchRenamerSVG /> ),
       'pt-fancy-zones': ( <FancyZonesSVG /> ),
+      'pt-power-rename': ( <PowerRenameSVG /> ),
       'pt-shortcut-guide': ( <ShortcutGuideSVG /> ),
-      'pt-terminate-tool': ( <TerminateToolSVG /> ),
-      'pt-maximize-new-desktop': ( <MaximizeNewDesktopSVG /> )
     }
   });
 }
