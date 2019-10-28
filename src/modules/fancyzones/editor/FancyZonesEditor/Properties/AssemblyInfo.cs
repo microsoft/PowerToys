843b8d3a77351d68bbfe4684dfcf20561e7395e1
Enrico Giordani <enrico.giordani@gmail.com>
2019-10-28 22:09:52 +0100

    version 0.12.0

--- src/modules/fancyzones/editor/FancyZonesEditor/Properties/AssemblyInfo.cs
+++ src/modules/fancyzones/editor/FancyZonesEditor/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.11.0.0")]
-[assembly: AssemblyFileVersion("0.11.0.0")]
+[assembly: AssemblyVersion("0.12.0.0")]
+[assembly: AssemblyFileVersion("0.12.0.0")]
