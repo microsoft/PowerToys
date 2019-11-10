dcbd4f335662f33f178b88e200daeaf254f4bb36
Jeremy Wu <jeremy24wu@gmail.com>
2019-11-11 06:04:16 +1100

    add option to Url plugin

--- Plugins/Wox.Plugin.Url/Settings.cs
+++ Plugins/Wox.Plugin.Url/Settings.cs
@@ -9,5 +9,7 @@ namespace Wox.Plugin.Url
     public class Settings
     {
         public string BrowserPath { get; set; }
+
+        public bool OpenInNewBrowserWindow { get; set; } = true;
     }
 }
