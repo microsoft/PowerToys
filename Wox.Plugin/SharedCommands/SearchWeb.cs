2debabe66426417494ba1a364082326fea8dc5bd
Jeremy Wu <jeremy24wu@gmail.com>
2019-11-10 19:12:43 +1100

    Add option to open in tab for WebSearch plugin

--- Wox.Plugin/SharedCommands/SearchWeb.cs
+++ Wox.Plugin/SharedCommands/SearchWeb.cs
@@ -7,7 +7,8 @@ namespace Wox.Plugin.SharedCommands
 {
     public static class SearchWeb
     {
-        /// <summary> Opens search in a new browser. If no browser path is passed in then Chrome is used. 
+        /// <summary> 
+        /// Opens search in a new browser. If no browser path is passed in then Chrome is used. 
         /// Leave browser path blank to use Chrome.
         /// </summary>
         public static void NewBrowserWindow(this string url, string browserPath)
@@ -19,11 +20,30 @@ namespace Wox.Plugin.SharedCommands
             var browser = string.IsNullOrEmpty(browserExecutableName) ? "chrome" : browserPath;
 
             // Internet Explorer will open url in new browser window, and does not take the --new-window parameter
-            var browserArguements = browserExecutableName == "iexplore.exe" ? url : "--new-window " + url;
+            var browserArguements = browserExecutableName == "iexplore.exe" ? "" : "--new-window ";
 
+            OpenWebSearch(browser, browserArguements, url);
+        }
+
+        /// <summary> 
+        /// Opens search as a tab in the default browser chosen in Windows settings.
+        /// </summary>
+        public static void NewTabInBrowser(this string url, string browserPath)
+        {
+            var browserExecutableName = browserPath?
+                                        .Split(new[] { Path.DirectorySeparatorChar }, StringSplitOptions.None)
+                                        .Last();
+
+            var browser = string.IsNullOrEmpty(browserExecutableName) ? "" : browserPath;
+
+            OpenWebSearch(browser, "", url);
+        }
+
+        private static void OpenWebSearch(string chosenBrowser, string browserArguements, string url)
+        {
             try
             {
-                Process.Start(browser, browserArguements);
+                Process.Start(chosenBrowser, browserArguements + url);
             }
             catch (System.ComponentModel.Win32Exception)
             {
