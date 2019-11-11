d7e6f97d9c788621accaef52d51e379a8321e8e6
Jeremy Wu <jeremy24wu@gmail.com>
2019-11-11 21:17:37 +1100

    Add browser path selection for browserbookmark plugin

--- Plugins/Wox.Plugin.BrowserBookmark/Views/SettingsControl.xaml.cs
+++ Plugins/Wox.Plugin.BrowserBookmark/Views/SettingsControl.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Win32;
 using System.Windows;
 using System.Windows.Controls;
 using Wox.Plugin.BrowserBookmark.Models;
@@ -27,6 +28,23 @@ namespace Wox.Plugin.BrowserBookmark.Views
         private void OnNewTabClick(object sender, RoutedEventArgs e)
         {
             _settings.OpenInNewBrowserWindow = false;
+        }
+
+        private void OnApplyBTClick(object sender, RoutedEventArgs e)
+        {
+            _settings.BrowserPath = browserPathBox.Text;
+        }
+
+        private void OnChooseClick(object sender, RoutedEventArgs e)
+        {
+            var fileBrowserDialog = new OpenFileDialog();
+            fileBrowserDialog.Filter = "Application(*.exe)|*.exe|All files|*.*";
+            fileBrowserDialog.CheckFileExists = true;
+            fileBrowserDialog.CheckPathExists = true;
+            if (fileBrowserDialog.ShowDialog() == true)
+            {
+                browserPathBox.Text = fileBrowserDialog.FileName;
+            }
         }
     }
 }
