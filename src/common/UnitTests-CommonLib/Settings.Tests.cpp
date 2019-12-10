e1d8aae40a168325dda83dd93e06b398983e09e0
Serafima Zykova <zykovas91@gmail.com>
2019-12-10 18:29:43 +0300

    unit-tests: PowerToyValues creation

--- src/common/UnitTests-CommonLib/Settings.Tests.cpp
+++ src/common/UnitTests-CommonLib/Settings.Tests.cpp
@@ -11,6 +11,22 @@ namespace UnitTestsCommonLib
     private:
         const std::wstring m_json = L"{\"name\":\"Module Name\",\"properties\" : {\"bool_toggle_true\":{\"value\":true},\"bool_toggle_false\":{\"value\":false},\"color_picker\" : {\"value\":\"#ff8d12\"},\"int_spinner\" : {\"value\":10},\"string_text\" : {\"value\":\"a quick fox\"}},\"version\" : \"1.0\" }";
 
+        void compareJsons(const json::JsonObject& expected, const json::JsonObject& actual)
+        {
+            auto iter = expected.First();
+            while (iter.HasCurrent())
+            {
+                const auto key = iter.Current().Key();
+                Assert::IsTrue(actual.HasKey(key));
+
+                const std::wstring expectedStringified = iter.Current().Value().Stringify().c_str();
+                const std::wstring actualStringified = actual.GetNamedValue(key).Stringify().c_str();
+
+                Assert::AreEqual(expectedStringified, actualStringified);
+                iter.MoveNext();
+            }
+        }
+
     public:
         TEST_METHOD(LoadFromJsonBoolTrue)
         {
@@ -54,6 +70,70 @@ namespace UnitTestsCommonLib
             Assert::IsTrue(value.has_value());
             std::wstring expected = L"#ff8d12";
             Assert::AreEqual(expected, *value);
+        }
+
+        TEST_METHOD(LoadFromEmptyString)
+        {
+            auto func = [] { PowerToyValues values = PowerToyValues::from_json_string(L""); };
+            Assert::ExpectException<winrt::hresult_error>(func);
+        }
+
+        TEST_METHOD(LoadFromInvalidString_NameMissed)
+        {
+            auto func = [] { PowerToyValues values = PowerToyValues::from_json_string(L"{\"properties\" : {\"bool_toggle_true\":{\"value\":true},\"bool_toggle_false\":{\"value\":false},\"color_picker\" : {\"value\":\"#ff8d12\"},\"int_spinner\" : {\"value\":10},\"string_text\" : {\"value\":\"a quick fox\"}},\"version\" : \"1.0\" }"); };
+            Assert::ExpectException<winrt::hresult_error>(func);
+        }
+
+        TEST_METHOD(LoadFromInvalidString_VersionMissed)
+        {
+            PowerToyValues values = PowerToyValues::from_json_string(L"{\"name\":\"Module Name\",\"properties\" : {}}");
+            const std::wstring expectedStr = L"{\"name\" : \"Module Name\", \"properties\" : {},\"version\" : \"1.0\"}";
+            const auto expected = json::JsonObject::Parse(expectedStr);
+            const auto actual = json::JsonObject::Parse(values.serialize());
+
+            compareJsons(expected, actual);
+        }
+
+        TEST_METHOD(LoadFromInvalidString_PropertiesMissed)
+        {
+            PowerToyValues values = PowerToyValues::from_json_string(L"{\"name\":\"Module Name\",\"version\" : \"1.0\" }");
+            const std::wstring expectedStr = L"{\"name\":\"Module Name\",\"properties\" : {},\"version\" : \"1.0\" }";
+            const auto expected = json::JsonObject::Parse(expectedStr);
+            const auto actual = json::JsonObject::Parse(values.serialize());
+
+            compareJsons(expected, actual);
+        }
+
+        TEST_METHOD(LoadFromValidString_EmptyProperties)
+        {
+            PowerToyValues values = PowerToyValues::from_json_string(L"{\"name\":\"Module Name\",\"properties\" : {}, \"version\" : \"1.0\" }");
+            const std::wstring expectedStr = L"{\"name\":\"Module Name\",\"properties\" : {},\"version\" : \"1.0\" }";
+            const auto expected = json::JsonObject::Parse(expectedStr);
+            const auto actual = json::JsonObject::Parse(values.serialize());
+
+            compareJsons(expected, actual);
+        }
+
+        TEST_METHOD(LoadFromValidString_ChangedVersion)
+        {
+            PowerToyValues values = PowerToyValues::from_json_string(L"{\"name\":\"Module Name\",\"properties\" : {},\"version\" : \"2.0\"}");
+            const std::wstring expectedStr = L"{\"name\" : \"Module Name\", \"properties\" : {},\"version\" : \"2.0\"}";
+
+            const auto expected = json::JsonObject::Parse(expectedStr);
+            const auto actual = json::JsonObject::Parse(values.serialize());
+
+            compareJsons(expected, actual);
+        }
+
+        TEST_METHOD(CreateWithName)
+        {
+            PowerToyValues values(L"Module Name");
+            const std::wstring expectedStr = L"{\"name\":\"Module Name\",\"properties\" : {},\"version\" : \"1.0\" }";
+
+            const auto expected = json::JsonObject::Parse(expectedStr);
+            const auto actual = json::JsonObject::Parse(values.serialize());
+
+            compareJsons(expected, actual);
         }
     };
 }
