cfd28ea2607a3e479d57fc410a503a97e6d91872
Divyansh <somm14divi@gmail.com>
2020-01-08 11:15:16 -0800

    Changed namespace to Common

--- src/modules/previewpane/common/cominterop/IInitializeWithStream.cs
+++ src/modules/previewpane/common/cominterop/IInitializeWithStream.cs
@@ -6,7 +6,7 @@ using System;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
 
-namespace PreviewHandleManage.ComInterlop
+namespace Common.ComInterlop
 {
     [ComImport]
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
