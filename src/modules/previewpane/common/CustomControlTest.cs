dd1f0b7bcdcd740bf83472c1fd649b009e4efcb3
Udit Singh <udsing@microsoft.com>
2020-01-08 22:58:33 -0800

    Added test control and handler

---
+++ src/modules/previewpane/common/CustomControlTest.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO;
+using System.Windows.Forms;
+
+namespace Common
+{
+    /// <summary>
+    /// Todl.
+    /// </summary>
+    public class CustomControlTest : FormHandlerControl
+    {
+        /// <summary>
+        /// Todo.
+        /// </summary>
+        /// <param name="filePath">Path to the file.</param>
+        public void DoPreview(string filePath)
+        {
+            this.InvokeOnControlThread(() =>
+            {
+                var text = File.ReadAllText(filePath);
+                WebBrowser browser = new WebBrowser();
+                browser.DocumentText = text;
+                browser.Dock = DockStyle.Fill;
+                browser.IsWebBrowserContextMenuEnabled = false;
+                browser.AllowWebBrowserDrop = false;
+                browser.Navigating += new WebBrowserNavigatingEventHandler((sender, e) => { e.Cancel = true; });
+                browser.Invalidate();
+                this.Controls.Add(browser);
+            });
+        }
+    }
+}
---
+++ src/modules/previewpane/common/cominterop/IInitializeWithFile.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Common.Cominterop
+{
+    /// <summary>
+    /// Todod.
+    /// </summary>
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("b7d14566-0509-4cce-a71f-0a554233bd9b")]
+    public interface IInitializeWithFile
+    {
+        /// <summary>
+        /// Todo.
+        /// </summary>
+        /// <param name="pszFilePath">Path.</param>
+        /// <param name="grfMode">Mode.</param>
+        void Initialize([MarshalAs(UnmanagedType.LPWStr)] string pszFilePath, uint grfMode);
+    }
+}
