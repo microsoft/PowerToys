28f617358e238e872203d465d0479a3ecb91dfe1
Alekhya Kommuru <alkommur@microsoft.com>
2020-01-14 15:17:08 -0800

    localized dllmain powerrename

--- src/modules/powerrename/dll/resource.h
+++ src/modules/powerrename/dll/resource.h
@@ -1,5 +1,18 @@
-#define IDS_POWERRENAME                 801
-#define IDI_RENAME                      132
+#define IDS_POWERRENAME                  99
+#define IDI_RENAME                      100
+#define IDS_SETTINGS_DESCRIPTION        101
+#define IDS_SETTINGS_ICON               102
+#define IDS_OVERVIEW_LINK               103
+#define IDS_BOOL_PERSIST                104
+#define IDS_RESTORE_SEARCH              105
+#define IDS_MRU_ENABLED                 106
+#define IDS_ENABLE_AUTO                 107
+#define IDS_MAX_MRU_SIZE                108   
+#define IDS_MAX_ITEMS                   109
+#define IDS_SHOW_ICON                   110
+#define IDS_ICON_CONTEXT_MENU           111
+#define IDS_EXTENDED_MENU               112
+#define IDS_EXTENDED_MENU_INFO          113
 
 // Next default values for new objects
 // 
