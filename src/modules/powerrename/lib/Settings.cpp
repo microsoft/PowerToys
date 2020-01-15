d06b648970f0de9b4c1acfd5ca5ce91355c80045
Alekhya Kommuru <alkommur@microsoft.com>
2020-01-14 16:39:03 -0800

    localized the settings file

--- src/modules/powerrename/lib/Settings.cpp
+++ src/modules/powerrename/lib/Settings.cpp
@@ -3,20 +3,22 @@
 #include <commctrl.h>
 #include "Settings.h"
 #include "PowerRenameInterfaces.h"
+#include "resource.h"
+#include <common.h>
 
-const wchar_t c_rootRegPath[] = L"Software\\Microsoft\\PowerRename";
-const wchar_t c_mruSearchRegPath[] = L"SearchMRU";
-const wchar_t c_mruReplaceRegPath[] = L"ReplaceMRU";
+wchar_t* c_rootRegPath = GET_RES_STRING_WCHAR(IDS_ROOT_PATH);
+wchar_t* c_mruSearchRegPath = GET_RES_STRING_WCHAR(IDS_SEARCH_PATH);
+wchar_t* c_mruReplaceRegPath = GET_RES_STRING_WCHAR(IDS_REPLACE_PATH);
 
-const wchar_t c_enabled[] = L"Enabled";
-const wchar_t c_showIconOnMenu[] = L"ShowIcon";
-const wchar_t c_extendedContextMenuOnly[] = L"ExtendedContextMenuOnly";
-const wchar_t c_persistState[] = L"PersistState";
-const wchar_t c_maxMRUSize[] = L"MaxMRUSize";
-const wchar_t c_flags[] = L"Flags";
-const wchar_t c_searchText[] = L"SearchText";
-const wchar_t c_replaceText[] = L"ReplaceText";
-const wchar_t c_mruEnabled[] = L"MRUEnabled";
+wchar_t* c_enabled = GET_RES_STRING_WCHAR(IDS_ENABLED);
+wchar_t* c_showIconOnMenu = GET_RES_STRING_WCHAR(IDS_SHOW_ICON);
+wchar_t* c_extendedContextMenuOnly = GET_RES_STRING_WCHAR(IDS_CONTEXT_MENU);
+wchar_t* c_persistState = GET_RES_STRING_WCHAR(IDS_PERSIST_STATE);
+wchar_t* c_maxMRUSize = GET_RES_STRING_WCHAR(IDS_MAX_MRU_SIZE);
+wchar_t* c_flags = GET_RES_STRING_WCHAR(IDS_FLAGS);
+wchar_t* c_searchText = GET_RES_STRING_WCHAR(IDS_SEARCH_TEXT);
+wchar_t* c_replaceText = GET_RES_STRING_WCHAR(IDS_REPLACE_TEXT);
+wchar_t* c_mruEnabled = GET_RES_STRING_WCHAR(IDS_MRU_ENABLED);
 
 const bool c_enabledDefault = true;
 const bool c_showIconOnMenuDefault = true;
---
+++ src/modules/powerrename/lib/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PowerRenameLib.rc
+
+#define IDS_ROOT_PATH                   100
+#define IDS_SEARCH_PATH                 101
+#define IDS_REPLACE_PATH                102
+#define IDS_ENABLED                     103
+#define IDS_SHOW_ICON                   104
+#define IDS_CONTEXT_MENU                105
+#define IDS_PERSIST_STATE               106
+#define IDS_MAX_MRU_SIZE                107
+#define IDS_FLAGS                       108
+#define IDS_SEARCH_TEXT                 109
+#define IDS_REPLACE_TEXT                110
+#define IDS_MRU_ENABLED                 111
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
