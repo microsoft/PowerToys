4787e8a82c54080570ae5d64fea7d2463d9f7f65
Udit Singh <udsing@microsoft.com>
2020-01-16 11:26:08 -0800

    Added registration and unregistration logic for the handler

--- src/modules/previewpane/common/TestCustomHandler.cs
+++ src/modules/previewpane/common/TestCustomHandler.cs
@@ -2,11 +2,18 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Runtime.InteropServices;
+
 namespace Common
 {
     /// <summary>
     /// This is a test custom handler to check the implemnatation will be removed in future PR.
     /// </summary>
+    [PreviewHandler("SvgPreviewHandler", ".svg", "{88235ab2-bfce-4be8-9ed0-0408cd8da792}")]
+    [ProgId("SvgPreviewHandler")]
+    [Guid("22a1a8e8-e929-4732-90ce-91eaff38b614")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComVisible(true)]
     public class TestCustomHandler : FileBasedPreviewHandler
     {
         private CustomControlTest previewHandlerControl;
