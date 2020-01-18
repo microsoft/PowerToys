043cdc474a91e69286c85fefc13bc3f28cc0cea9
Udit Singh <udsing@microsoft.com>
2020-01-17 18:10:32 -0800

    Added a new project for Svg preview handler

---
+++ src/modules/previewpane/SvgPreviewHandler/SvgPreviewHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgPreviewHandler
+{
+    public class SvgPreviewHandler
+    {
+
+    }
+}
