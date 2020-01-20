506be2a3d3b6e6a5a7704e567bf4e04a742b2268
Alekhya Kommuru <alkommur@microsoft.com>
2020-01-20 11:52:46 -0800

    Removed some IDs from resource file. Changed SHIFT to Shift

--- src/modules/powerrename/dll/resource.h
+++ src/modules/powerrename/dll/resource.h
@@ -1,18 +1,12 @@
 #define IDS_POWERRENAME                 801
 #define IDI_RENAME                      132
 #define IDS_SETTINGS_DESCRIPTION        2101
-#define IDS_SETTINGS_ICON               2102
-#define IDS_OVERVIEW_LINK               2103
-#define IDS_BOOL_PERSIST                2104
-#define IDS_RESTORE_SEARCH              2105
-#define IDS_MRU_ENABLED                 2106
-#define IDS_ENABLE_AUTO                 2107
-#define IDS_MAX_MRU_SIZE                2108   
-#define IDS_MAX_ITEMS                   2109
-#define IDS_SHOW_ICON                   2110
-#define IDS_ICON_CONTEXT_MENU           2111
-#define IDS_EXTENDED_MENU               2112
-#define IDS_EXTENDED_MENU_INFO          2113
+#define IDS_OVERVIEW_LINK               2102
+#define IDS_RESTORE_SEARCH              2103
+#define IDS_ENABLE_AUTO                 2104
+#define IDS_MAX_ITEMS                   2105
+#define IDS_ICON_CONTEXT_MENU           2106
+#define IDS_EXTENDED_MENU_INFO          2107
 
 // Next default values for new objects
 
