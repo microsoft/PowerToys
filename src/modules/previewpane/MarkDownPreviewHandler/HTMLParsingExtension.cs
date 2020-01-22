fe1f62aefa7612d5cc20438587e78bca02aa6a61
Divyansh Srivastava <somm14divi@gmail.com>
2020-01-21 17:18:14 -0800

    HTML Parsing Extension for preview pane markdown renderer (#1108)
    * Added Extension for html post processing
    
    * Added unit test poroject for preview pane
    
    * Added pipline test and base test function
    
    * Added Tests for extension
    
    * Added tests for url slashes
    
    * Added tests for url and figure caption

---
+++ src/modules/previewpane/MarkDownPreviewHandler/HTMLParsingExtension.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using Markdig;
+using Markdig.Extensions.Figures;
+using Markdig.Extensions.Tables;
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+
+namespace MarkDownPreviewHandler
+{
+    /// <summary>
+    /// Markdig Extension to process html nodes in markdown AST.
+    /// </summary>
+    internal class HTMLParsingExtension : IMarkdownExtension
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HTMLParsingExtension"/> class.
+        /// </summary>
+        public HTMLParsingExtension(string baseUrl = "")
+        {
+            this.BaseUrl = baseUrl;
+        }
+
+        /// <summary>
+        /// Gets or sets path to directory containing markdown file.
+        /// </summary>
+        public string BaseUrl { get; set; }
+
+        /// <inheritdoc/>
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            // Make sure we don't have a delegate twice
+            pipeline.DocumentProcessed -= this.PipelineOnDocumentProcessed;
+            pipeline.DocumentProcessed += this.PipelineOnDocumentProcessed;
+        }
+
+        /// <inheritdoc/>
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+        }
+
+        /// <summary>
+        /// Process nodes in markdown AST.
+        /// </summary>
+        /// <param name="document">Markdown Document.</param>
+        public void PipelineOnDocumentProcessed(MarkdownDocument document)
+        {
+            foreach (var node in document.Descendants())
+            {
+                if (node is Block)
+                {
+                    if (node is Table)
+                    {
+                        node.GetAttributes().AddClass("table table-striped table-bordered");
+                    }
+                    else if (node is QuoteBlock)
+                    {
+                        node.GetAttributes().AddClass("blockquote");
+                    }
+                    else if (node is Figure)
+                    {
+                        node.GetAttributes().AddClass("figure");
+                    }
+                    else if (node is FigureCaption)
+                    {
+                        node.GetAttributes().AddClass("figure-caption");
+                    }
+                }
+                else if (node is Inline)
+                {
+                    if (node is LinkInline link && link.IsImage)
+                    {
+                        if (!Uri.TryCreate(link.Url, UriKind.Absolute, out Uri uriLink))
+                        {
+                            link.Url = link.Url.TrimStart('/', '\\');
+                            this.BaseUrl = this.BaseUrl.TrimEnd('/', '\\');
+                            uriLink = new Uri(Path.Combine(this.BaseUrl, link.Url));
+                            link.Url = uriLink.ToString();
+                        }
+
+                        link.GetAttributes().AddClass("img-fluid");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/modules/previewpane/PreviewPaneUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PreviewPaneUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PreviewPaneUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a2b51b8b-8f90-424e-bc97-f9ab7d76ca1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
