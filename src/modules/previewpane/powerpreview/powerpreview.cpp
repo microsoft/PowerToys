944936584cf4947937145dadac902470411e5fc8
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-01-22 15:26:46 -0800

    moved PowerPreviewModule implemnetations to .cpp file

--- src/modules/previewpane/powerpreview/powerpreview.cpp
+++ src/modules/previewpane/powerpreview/powerpreview.cpp
@@ -1,5 +1,118 @@
 #include "pch.h"
+#include <interface/lowlevel_keyboard_event_data.h>
+#include <interface/win_hook_event_data.h>
+#include <settings_objects.h>
+#include <common.h>
 #include "powerpreview.h"
+#include "trace.h"
+#include "settings.h"
+#include "resource.h"
+
+// Destroy the powertoy and free memory
+void PowerPreviewModule::destroy()
+{
+    delete this;
+    Trace::Destroyed();
+}
+
+// Return the display name of the powertoy, this will be cached
+const wchar_t* PowerPreviewModule::get_name()
+{
+    return moduleName.c_str();
+}
+
+const wchar_t** PowerPreviewModule::get_events()
+{
+    return nullptr;
+}
+
+// Return JSON with the configuration options.
+bool PowerPreviewModule::get_config(_Out_ wchar_t* buffer, _Out_ int* buffer_size)
+{
+    HINSTANCE hinstance = reinterpret_cast<HINSTANCE>(&__ImageBase);
+
+    // Create a Settings object.
+    PowerToysSettings::Settings settings(hinstance, get_name());
+
+    // General Settings.
+    settings.set_description(GET_RESOURCE_STRING(IDS_GENERAL_DESCRIPTION));
+    settings.set_icon_key(GET_RESOURCE_STRING(IDS_ICON_KEY_NAME));
+
+    // Explorer: Settings Group Header.
+    settings.add_header_szLarge(
+        GET_RESOURCE_STRING(IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_HEADER_ID),
+        GET_RESOURCE_STRING(IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_DESC),
+        GET_RESOURCE_STRING(IDS_PRVPANE_FILE_PREV_STTNGS_GROUP_TEXT));
+
+    // Explorer: SVG Icon Settings.
+    settings.add_bool_toogle(
+        explrSVGSettings.GetName(),
+        explrSVGSettings.GetDescription(),
+        explrSVGSettings.GetState());
+
+    // Preview Pane: Settings Group Header.
+    settings.add_header_szLarge(
+        GET_RESOURCE_STRING(IDS_PRVPANE_FILE_PREV_STTNGS_GROUP_HEADER_ID),
+        GET_RESOURCE_STRING(IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_DESC),
+        GET_RESOURCE_STRING(IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_TEXT));
+
+    // Preview Pane: SVG Settings.
+    settings.add_bool_toogle(
+        prevPaneSVGSettings.GetName(),
+        prevPaneSVGSettings.GetDescription(),
+        prevPaneSVGSettings.GetState());
+
+    // Preview Pane: Mark Down Settings.
+    settings.add_bool_toogle(
+        prevPaneMDSettings.GetName(),
+        prevPaneMDSettings.GetDescription(),
+        prevPaneMDSettings.GetState());
+
+    return settings.serialize_to_buffer(buffer, buffer_size);
+}
+
+// Called by the runner to pass the updated settings values as a serialized JSON.
+void PowerPreviewModule::set_config(const wchar_t* config)
+{
+    try
+    {
+        PowerToysSettings::PowerToyValues values = PowerToysSettings::PowerToyValues::from_json_string(config);
+        explrSVGSettings.UpdateState(values);
+        prevPaneSVGSettings.UpdateState(values);
+        prevPaneMDSettings.UpdateState(values);
+        values.save_to_settings_file();
+    }
+    catch (std::exception const& e)
+    {
+        Trace::SetConfigInvalidJSON(e.what());
+    }
+}
+
+// Enable the powertoy
+void PowerPreviewModule::enable()
+{
+    this->m_enabled = true;
+    Trace::FilePreviewerIsEnabled();
+}
+
+// Disable the powertoy
+void PowerPreviewModule::disable()
+{
+    this->m_enabled = false;
+    Trace::FilePreviewerIsDisabled();
+}
+
+// Returns if the powertoys is enabled
+bool PowerPreviewModule::is_enabled()
+{
+    return this->m_enabled;
+}
+
+// Handle incoming event, data is event-specific
+intptr_t PowerPreviewModule::signal_event(const wchar_t* name, intptr_t data)
+{
+    return 0;
+}
 
 // Load the settings file.
 void PowerPreviewModule::init_settings()
