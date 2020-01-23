782c97ade53b663a5b2b46aaa4d0cc381e51c457
udit3333 <udit3333@gmail.com>
2020-01-23 09:27:12 -0800

    Add Svg preview handler (#1129)
    * Added a new project for Svg preview handler
    
    * Added initial implementation of Svg Preview Handler
    
    * Fixed output path
    
    * Added Unit Test Project
    
    * Added StreamWrapper and Update Svg Control
    
    * Updated Svg Handler Guid
    
    * Removed migration backup folder
    
    * Removed Fluent Assertions NuGet
    
    * Added Comments for StreamWrapper
    
    * Removed the manual GC collect
    
    * Added unit tests for Svg preview Handler
    
    * Updated the xml doc for stream wrapper

---
+++ src/modules/previewpane/SvgPreviewHandler/SvgPreviewControl.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Forms;
+using Common;
+using Common.Utilities;
+
+namespace SvgPreviewHandler
+{
+    /// <summary>
+    /// Implementation of Control for Svg Preview Handler.
+    /// </summary>
+    public class SvgPreviewControl : FormHandlerControl
+    {
+        private Stream dataSourceStream;
+
+        /// <summary>
+        /// Start the preview on the Control.
+        /// </summary>
+        /// <param name="dataSource">Stream reference to access source file.</param>
+        public override void DoPreview<T>(T dataSource)
+        {
+            this.InvokeOnControlThread(() =>
+            {
+                WebBrowser browser = new WebBrowser();
+                this.dataSourceStream = new StreamWrapper(dataSource as IStream);
+
+                browser.DocumentStream = this.dataSourceStream;
+                browser.Dock = DockStyle.Fill;
+                browser.IsWebBrowserContextMenuEnabled = false;
+                browser.ScriptErrorsSuppressed = true;
+                browser.ScrollBarsEnabled = true;
+                this.Controls.Add(browser);
+                base.DoPreview(dataSource);
+            });
+        }
+
+        /// <summary>
+        /// Free resources on the unload of Preview.
+        /// </summary>
+        public override void Unload()
+        {
+            base.Unload();
+            if (this.dataSourceStream != null)
+            {
+                this.dataSourceStream.Dispose();
+                this.dataSourceStream = null;
+            }
+        }
+    }
+}
---
+++ src/modules/previewpane/SvgPreviewHandler/SvgPreviewHandler.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+using Common;
+
+namespace SvgPreviewHandler
+{
+    /// <summary>
+    /// Extends <see cref="StreamBasedPreviewHandler"/> for Svg Preview Handler.
+    /// </summary>
+    [PreviewHandler("SvgPreviewHandler", ".svg", "{88235ab2-bfce-4be8-9ed0-0408cd8da792}")]
+    [ProgId("SvgPreviewHandler")]
+    [Guid("ddee2b8a-6807-48a6-bb20-2338174ff779")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComVisible(true)]
+    public class SvgPreviewHandler : StreamBasedPreviewHandler
+    {
+        private SvgPreviewControl svgPreviewControl;
+
+        /// <inheritdoc/>
+        public override void DoPreview()
+        {
+            this.svgPreviewControl.DoPreview(this.Stream);
+        }
+
+        /// <inheritdoc/>
+        protected override IPreviewHandlerControl CreatePreviewHandlerControl()
+        {
+            this.svgPreviewControl = new SvgPreviewControl();
+            return this.svgPreviewControl;
+        }
+    }
+}
