54e317ab5f455efd482430cd00ea1e9bb517b0df
Udit Singh <udsing@microsoft.com>
2020-01-23 19:37:48 -0800

    Added tests for file and stream based handler

---
+++ src/modules/previewpane/UnitTests-PreviewHandlerCommon/FileBasedPreviewHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests_PreviewHandlerCommon
+{
+    [TestClass]
+    public class FileBasedPreviewHandlerTests
+    {
+        public class TestFileBasedPreviewHandler : FileBasedPreviewHandler
+        {
+            public override void DoPreview()
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override IPreviewHandlerControl CreatePreviewHandlerControl()
+            {
+                return new Mock<IPreviewHandlerControl>().Object;
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow((uint)0)]
+        [DataRow((uint)1)]
+        public void FileBasedPreviewHandler_ShouldSetFilePath_WhenInitializeCalled(uint grfMode)
+        {
+            // Arrange
+            var fileBasedPreviewHandler = new TestFileBasedPreviewHandler();
+            var filePath = "C:\\valid-path";
+
+            // Act
+            fileBasedPreviewHandler.Initialize(filePath, grfMode);
+
+            // Assert
+            Assert.AreEqual(fileBasedPreviewHandler.FilePath, filePath);
+        }
+    }
+}
---
+++ src/modules/previewpane/UnitTests-PreviewHandlerCommon/StreamBasedPreviewHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests_PreviewHandlerCommon
+{
+    [TestClass]
+    public class StreamBasedPreviewHandlerTests
+    {
+        public class TestStreamBasedPreviewHandler : StreamBasedPreviewHandler
+        {
+            public override void DoPreview()
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override IPreviewHandlerControl CreatePreviewHandlerControl()
+            {
+                return new Mock<IPreviewHandlerControl>().Object;
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow((uint)0)]
+        [DataRow((uint)1)]
+        public void FileBasedPreviewHandler_ShouldSetFilePath_WhenInitializeCalled(uint grfMode)
+        {
+            // Arrange
+            var streamBasedPreviewHandler = new TestStreamBasedPreviewHandler();
+            var stream = new Mock<IStream>().Object;
+
+            // Act
+            streamBasedPreviewHandler.Initialize(stream, grfMode);
+
+            // Assert
+            Assert.AreEqual(streamBasedPreviewHandler.Stream, stream);
+        }
+    }
+}
