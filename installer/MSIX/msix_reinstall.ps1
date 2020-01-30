53af5ab73f75bf918221dedd62880cc9b7fce0ae
yuyoyuppe <a.yuyoyuppe@gmail.com>
2020-01-30 16:16:47 +0300

    MSIX: fix uninstallation from msix_reinstall

--- installer/MSIX/msix_reinstall.ps1
+++ installer/MSIX/msix_reinstall.ps1
@@ -1,6 +1,6 @@
 taskkill /f /im explorer.exe
 
-Get-AppxPackage -Name 'PowerToys' | select -ExpandProperty "PackageFullName" | Remove-AppxPackage
+Get-AppxPackage -Name '*PowerToys' | select -ExpandProperty "PackageFullName" | Remove-AppxPackage
 
 .\build_msix.ps1
 signtool sign /debug /a /fd SHA256 /f PowerToys_TemporaryKey.pfx /p 12345 bin\PowerToys-x64.msix
