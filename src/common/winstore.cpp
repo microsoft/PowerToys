8b8f053c626de1a2238a5b7d9b2f9aacc7921ef5
yuyoyuppe <yuyoyuppe@users.noreply.github.com>
2020-01-31 20:35:21 +0300

    Runner: fix startup task state setting for MSIX (#1181)

--- src/common/winstore.cpp
+++ src/common/winstore.cpp
@@ -3,9 +3,70 @@
 
 #include <appmodel.h>
 
-bool running_as_packaged()
+#include <winrt/Windows.ApplicationModel.h>
+
+using winrt::Windows::ApplicationModel::StartupTask;
+
+namespace
 {
-    UINT32 length = 0;
-    const auto rc = GetPackageFamilyName(GetCurrentProcess(), &length, nullptr);
-    return rc != APPMODEL_ERROR_NO_PACKAGE;
+    const wchar_t* STARTUP_TASKID = L"PowerToysStartupTaskID";
+}
+
+namespace winstore
+{
+    bool running_as_packaged()
+    {
+        UINT32 length = 0;
+        const auto rc = GetPackageFamilyName(GetCurrentProcess(), &length, nullptr);
+        return rc != APPMODEL_ERROR_NO_PACKAGE;
+    }
+
+    std::future<StartupTaskState> get_startup_task_status_async()
+    {
+        const auto startupTask = co_await StartupTask::GetAsync(STARTUP_TASKID);
+        co_return startupTask.State();
+    }
+
+    std::future<void> switch_startup_task_state_async(const bool enabled)
+    {
+        const auto startupTask = co_await StartupTask::GetAsync(STARTUP_TASKID);
+        enum class action
+        {
+            none,
+            enable,
+            disable,
+        } action_to_try = action::none;
+        switch (startupTask.State())
+        {
+        case StartupTaskState::Disabled:
+            if (enabled)
+            {
+                action_to_try = action::enable;
+            }
+            break;
+        case StartupTaskState::Enabled:
+            if (!enabled)
+            {
+                action_to_try = action::disable;
+            }
+            break;
+        }
+        try
+        {
+            switch (action_to_try)
+            {
+            case action::enable:
+                co_await startupTask.RequestEnableAsync();
+                break;
+            case action::disable:
+                startupTask.Disable();
+                break;
+            }
+        }
+        catch (...)
+        {
+            // We can't handle the error, in case we don't have a permission to change startup task state
+        }
+    }
+
 }
