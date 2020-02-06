274d42cc41c5ecbded1d765b7ba35b6b534f4402
Udit Singh <udsing@microsoft.com>
2020-02-06 09:00:06 -0800

    Added implementation to remove script and image tag

--- src/modules/previewpane/SvgPreviewHandler/SvgPreviewControl.cs
+++ src/modules/previewpane/SvgPreviewHandler/SvgPreviewControl.cs
@@ -3,9 +3,13 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 using Common;
 using Common.Utilities;
 
@@ -18,6 +22,10 @@ namespace SvgPreviewHandler
     {
         private Stream dataSourceStream;
 
+        private RichTextBox textBox;
+
+        private bool foundFilteredElements = false;
+
         /// <summary>
         /// Start the preview on the Control.
         /// </summary>
@@ -26,15 +34,40 @@ namespace SvgPreviewHandler
         {
             this.InvokeOnControlThread(() =>
             {
+                this.foundFilteredElements = false;
                 WebBrowser browser = new WebBrowser();
-                this.dataSourceStream = new StreamWrapper(dataSource as IStream);
+                string svgData = null;
+                using (var stream = new StreamWrapper(dataSource as IStream))
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        svgData = reader.ReadToEnd();
+                    }
+                }
 
-                browser.DocumentStream = this.dataSourceStream;
+                browser.DocumentText = this.RemoveFilteredElements(svgData);
                 browser.Dock = DockStyle.Fill;
                 browser.IsWebBrowserContextMenuEnabled = false;
                 browser.ScriptErrorsSuppressed = true;
                 browser.ScrollBarsEnabled = true;
+
+                if (this.foundFilteredElements)
+                {
+                    this.textBox = new RichTextBox();
+                    this.textBox.Text = "Blocked scripts and image to load external resources components Blocked scripts and image to load external resources components Blocked scripts and image to load external resources components";
+                    this.textBox.BackColor = Color.LightYellow;
+                    this.textBox.Multiline = true;
+                    this.textBox.Dock = DockStyle.Top;
+                    this.textBox.ReadOnly = true;
+                    this.textBox.ContentsResized += this.RTBContentsResized;
+                    this.textBox.ScrollBars = RichTextBoxScrollBars.None;
+                    this.textBox.BorderStyle = BorderStyle.None;
+                    this.Controls.Add(this.textBox);
+                }
+
                 this.Controls.Add(browser);
+
+                this.Resize += this.FormResized;
                 base.DoPreview(dataSource);
             });
         }
@@ -50,6 +83,36 @@ namespace SvgPreviewHandler
                 this.dataSourceStream.Dispose();
                 this.dataSourceStream = null;
             }
+        }
+
+        private void RTBContentsResized(object sender, ContentsResizedEventArgs e)
+        {
+            RichTextBox richTextBox = (RichTextBox)sender;
+            richTextBox.Height = e.NewRectangle.Height + 5;
+        }
+
+        private void FormResized(object sender, EventArgs e)
+        {
+            if (this.foundFilteredElements)
+            {
+                this.textBox.Width = this.Width;
+            }
+        }
+
+        private string RemoveFilteredElements(string svgData)
+        {
+            var doc = XDocument.Parse(svgData);
+            var elements = doc.Descendants().ToList();
+            foreach (XElement node in elements)
+            {
+                if (string.Equals(node.Name.LocalName, "script", StringComparison.OrdinalIgnoreCase) || string.Equals(node.Name.LocalName, "image", StringComparison.OrdinalIgnoreCase))
+                {
+                    node.RemoveAll();
+                    this.foundFilteredElements = true;
+                }
+            }
+
+            return doc.ToString();
         }
     }
 }
