e0ce471cf70104a265672f70c449999b59f74eb5
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-02-06 13:04:10 -0800

    formatted .rc file.

--- src/modules/previewpane/powerpreview/resource.h
+++ src/modules/previewpane/powerpreview/resource.h
@@ -2,23 +2,23 @@
 // Microsoft Visual C++ generated include file.
 // Used by powerpreview.rc
 
-#define	IDS_GENERAL_DESCRIPTION                               200
-#define	IDS_MODULE_NAME                                       201
-#define	IDS_ICON_KEY_NAME                                     202
-#define	IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_HEADER_ID           203
-#define	IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_DESC                204
-#define	IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_TEXT                205
-#define	IDS_PRVPANE_FILE_PREV_STTNGS_GROUP_HEADER_ID          206
-#define	IDS_PRVPANE_FILE_PREV_STTNGS_GROUP_DESC               207
-#define	IDS_PRVPANE_FILE_PREV_STTNGS_GROUP_TEXT               208
-#define	IDS_PREVPANE_MD_BOOL_TOGGLE_CONTROLL                  209
-#define	IDS_PREVPANE_MD_SETTINGS_DESCRIPTION                  210
-#define	IDS_PREVPANE_SVG_BOOL_TOGGLE_CONTROLL                 211
-#define	IDS_PREVPANE_SVG_SETTINGS_DESCRIPTION                 212
-#define	IDS_EXPLR_SVG_BOOL_TOGGLE_CONTROLL                    213
-#define	IDS_EXPLR_SVG_SETTINGS_DESCRIPTION                    214
-#define	IDS_PREVPANE_SVG_SETTINGS_DISPLAYNAME				          215
-#define	IDS_PREVPANE_MD_SETTINGS_DISPLAYNAME				          216
+#define	IDS_GENERAL_DESCRIPTION                               	200
+#define	IDS_MODULE_NAME                                       	201
+#define	IDS_ICON_KEY_NAME                                     	202
+#define	IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_HEADER_ID           	203
+#define	IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_DESC                	204
+#define	IDS_EXPLR_ICONS_PREV_STTNGS_GROUP_TEXT                	205
+#define	IDS_PRVPANE_FILE_PREV_STTNGS_GROUP_HEADER_ID          	206
+#define	IDS_PRVPANE_FILE_PREV_STTNGS_GROUP_DESC               	207
+#define	IDS_PRVPANE_FILE_PREV_STTNGS_GROUP_TEXT               	208
+#define	IDS_PREVPANE_MD_BOOL_TOGGLE_CONTROLL                  	209
+#define	IDS_PREVPANE_MD_SETTINGS_DESCRIPTION                  	210
+#define	IDS_PREVPANE_SVG_BOOL_TOGGLE_CONTROLL                 	211
+#define	IDS_PREVPANE_SVG_SETTINGS_DESCRIPTION                 	212
+#define	IDS_EXPLR_SVG_BOOL_TOGGLE_CONTROLL                    	213
+#define	IDS_EXPLR_SVG_SETTINGS_DESCRIPTION                    	214
+#define	IDS_PREVPANE_SVG_SETTINGS_DISPLAYNAME                   215
+#define	IDS_PREVPANE_MD_SETTINGS_DISPLAYNAME                    216
 
 // Next default values for new objects
 // 
