a18967054cccf6c8e23815fa87fc1c0434ae8993
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-02-07 15:38:33 -0800

    added registry methods and enable/disable preview handlers (#1230)
    * added registry methods and enable/disable preview handlers
    
    * formatted .rc file.
    
    * formatted resource file
    
    * formatted .rc file.
    
    * formatted settings.cpp
    
    * formatted settings.h
    
    * formatted SVGPreviewSettingsClassTest.cpp
    
    * Formatted MarkDownPreviewSettingsClassTest.cpp
    
    * using wide strings
    
    * formatted settings.h
    
    * FileExplorerPreviewSettingsTest.cpp formatting
    
    * fixed typo and formatting
    
    * closing Registry and fixing typos
    
    * formmarted code using ctrl+k+d
    
    * fixed naming
    
    * fixed typo
    
    * changed if/else reverse order

--- src/modules/previewpane/powerpreview/settings.h
+++ src/modules/previewpane/powerpreview/settings.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <pch.h>
 #include <string>
 #include "resource.h"
 #include <settings_objects.h>
@@ -10,50 +11,52 @@ namespace PowerPreviewSettings
 	{
 	private:
 		bool m_isPreviewEnabled;
-        std::wstring m_name;
-        std::wstring m_description;
+		std::wstring m_name;
+		std::wstring m_description;
+		std::wstring m_displayName;
+
+		LPCWSTR m_clsid;
+		LPCWSTR m_subKey = L"Software\\Microsoft\\Windows\\CurrentVersion\\PreviewHandlers";
 
 	public:
-        FileExplorerPreviewSettings(bool state, const std::wstring name, const std::wstring description);
+		FileExplorerPreviewSettings(bool state, const std::wstring name, const std::wstring description, LPCWSTR clsid, const std::wstring displayname);
 		FileExplorerPreviewSettings();
 
 		virtual bool GetState() const;
 		virtual void SetState(bool state);
-        virtual void LoadState(PowerToysSettings::PowerToyValues &settings);
-		virtual void UpdateState(PowerToysSettings::PowerToyValues &values);
-        virtual std::wstring GetName() const;
-		virtual void SetName(const std::wstring name);
-        virtual std::wstring GetDescription() const;
-        virtual void SetDescription(const std::wstring description);
+		virtual void LoadState(PowerToysSettings::PowerToyValues& settings);
+		virtual void UpdateState(PowerToysSettings::PowerToyValues& values);
+		virtual std::wstring GetName() const;
+		virtual void SetName(const std::wstring& name);
+		virtual std::wstring GetDescription() const;
+		virtual void SetDescription(const std::wstring& description);
+		virtual void SetDisplayName(const std::wstring& displayName);
+		virtual LONG SetRegistryValue() const;
+		virtual LONG DeleteRegistryValue() const;
+		virtual bool GetRegistryValue() const;
+		virtual std::wstring GetDisplayName() const;
+		virtual LPCWSTR GetCLSID() const;
+		virtual LPCWSTR GetSubKey() const;
 		virtual void EnablePreview() = 0;
-		virtual void DisabledPreview() = 0;
+		virtual void DisablePreview() = 0;
 	};
 
-	class ExplrSVGSttngs: public FileExplorerPreviewSettings
-	{
-	public:
-		ExplrSVGSttngs();
-
-		virtual void EnablePreview();
-		virtual void DisabledPreview();
-	};
-
-	class PrevPaneSVGRendrSettings: public FileExplorerPreviewSettings
+	class PrevPaneSVGRendrSettings : public FileExplorerPreviewSettings
 	{
 	public:
 		PrevPaneSVGRendrSettings();
 
 		virtual void EnablePreview();
-		virtual void DisabledPreview();
+		virtual void DisablePreview();
 	};
 
-	class PrevPaneMDRendrSettings: public FileExplorerPreviewSettings
+	class PrevPaneMDRendrSettings : public FileExplorerPreviewSettings
 	{
 	public:
 		PrevPaneMDRendrSettings();
 
 		virtual void EnablePreview();
-		virtual void DisabledPreview();
+		virtual void DisablePreview();
 	};
 
 }
