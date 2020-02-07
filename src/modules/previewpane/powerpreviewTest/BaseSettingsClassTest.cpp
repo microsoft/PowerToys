6b0e6a42e613c515263175d1382f9c0db14b4143
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-02-07 15:38:33 -0800

    added registry methods and enable/disable preview handlers (#1230)
    * added registry methods and enable/disable preview handlers
    
    * formatted .rc file.
    
    * formatted resource file
    
    * formatted .rc file.
    
    * formatted settings.cpp
    
    * formatted settings.h
    
    * formatted SVGPreviewSettingsClassTest.cpp
    
    * Formatted MarkDownPreviewSettingsClassTest.cpp
    
    * using wide strings
    
    * formatted settings.h
    
    * FileExplorerPreviewSettingsTest.cpp formatting
    
    * fixed typo and formatting
    
    * closing Registry and fixing typos
    
    * formmarted code using ctrl+k+d
    
    * fixed naming
    
    * fixed typo
    
    * changed if/else reverse order

--- src/modules/previewpane/powerpreviewTest/BaseSettingsClassTest.cpp
+++ src/modules/previewpane/powerpreviewTest/BaseSettingsClassTest.cpp
@@ -6,11 +6,13 @@
 extern "C" IMAGE_DOS_HEADER __ImageBase;
 
 BaseSettingsClassTest::BaseSettingsClassTest() :
-    FileExplorerPreviewSettings(
-        false,
-        GET_RESOURCE_STRING(IDS_PREVPANE_MD_BOOL_TOGGLE_CONTROLL),
-        GET_RESOURCE_STRING(IDS_PREVPANE_MD_SETTINGS_DESCRIPTION)){}
+	FileExplorerPreviewSettings(
+		false,
+		GET_RESOURCE_STRING(IDS_PREVPANE_MD_BOOL_TOGGLE_CONTROLL),
+		GET_RESOURCE_STRING(IDS_PREVPANE_MD_SETTINGS_DESCRIPTION),
+		L"{test-guid}",
+		TEXT("Test Handler\0")) {}
 
 void BaseSettingsClassTest::EnablePreview() {}
 
-void BaseSettingsClassTest::DisabledPreview() {}
+void BaseSettingsClassTest::DisablePreview() {}
