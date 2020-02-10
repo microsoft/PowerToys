9cfadd29e96cc9f25c27ee6cd4b9eec2903533d1
Arjun Balgovind <arjunbalgovind@gmail.com>
2020-02-10 10:25:48 -0800

    Fixed copyright header

--- src/modules/imageresizer/tests/Properties/AppFixture.cs
+++ src/modules/imageresizer/tests/Properties/AppFixture.cs
@@ -1,6 +1,6 @@
-﻿// Copyright (c) Brice Lambson
-// The Brice Lambson licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.  Code forked from Brice Lambson's https://github.com/bricelam/ImageResizer/
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 using System;
 
