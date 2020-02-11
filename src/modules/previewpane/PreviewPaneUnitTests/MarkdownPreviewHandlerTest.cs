7666912ec1b9f289d126f709a66774fbb7ad6634
Divyansh Srivastava <somm14divi@gmail.com>
2020-02-11 11:59:42 -0800

    Markdown preview pane  (#1220)
    * Added rich text bar for information display
    
    * Added infobar
    
    * Added tests for extension and markdown control
    
    * Added xml docstring for markdown preview handler control
    
    * Updated assembly file for markdown preview pane
    
    * Updated removal of script tag without modifying CSS
    
    * Added info bar text string to resource file.
    
    * Removed error with infobar display on first rendering
    
    * Updated assembly version

--- src/modules/previewpane/PreviewPaneUnitTests/MarkdownPreviewHandlerTest.cs
+++ src/modules/previewpane/PreviewPaneUnitTests/MarkdownPreviewHandlerTest.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using Markdig;
 using MarkdownPreviewHandler;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -16,7 +20,35 @@ namespace PreviewPaneUnitTests
             MarkdownPreviewHandlerControl markdownPreviewHandlerControl = new MarkdownPreviewHandlerControl();
 
             // Act
-            markdownPreviewHandlerControl.DoPreview<string>("HelperFiles/SampleMarkDown.txt");
+            markdownPreviewHandlerControl.DoPreview<string>("HelperFiles/MarkdownWithExternalImage.txt");
+
+            // Assert
+            Assert.AreEqual(markdownPreviewHandlerControl.Controls.Count, 2);
+            Assert.IsInstanceOfType(markdownPreviewHandlerControl.Controls[0], typeof(WebBrowser));
+        }
+
+        [TestMethod]
+        public void MarkdownPreviewHandlerControl__AddsInfoBarToFormIfExternalImageLinkPresent_WhenDoPreviewIsCalled()
+        {
+            // Arrange 
+            MarkdownPreviewHandlerControl markdownPreviewHandlerControl = new MarkdownPreviewHandlerControl();
+
+            // Act
+            markdownPreviewHandlerControl.DoPreview<string>("HelperFiles/MarkdownWithExternalImage.txt");
+
+            // Assert
+            Assert.AreEqual(markdownPreviewHandlerControl.Controls.Count, 2);
+            Assert.IsInstanceOfType(markdownPreviewHandlerControl.Controls[1], typeof(RichTextBox));
+        }
+
+        [TestMethod]
+        public void MarkdownPreviewHandlerControl__DoesNotAddInfoBarToFormIfExternalImageLinkNotPresent_WhenDoPreviewIsCalled()
+        {
+            // Arrange 
+            MarkdownPreviewHandlerControl markdownPreviewHandlerControl = new MarkdownPreviewHandlerControl();
+
+            // Act
+            markdownPreviewHandlerControl.DoPreview<string>("HelperFiles/MarkdownWithScript.txt");
 
             // Assert
             Assert.AreEqual(markdownPreviewHandlerControl.Controls.Count, 1);
@@ -30,7 +62,7 @@ namespace PreviewPaneUnitTests
             MarkdownPreviewHandlerControl markdownPreviewHandlerControl = new MarkdownPreviewHandlerControl();
 
             // Act
-            markdownPreviewHandlerControl.DoPreview<string>("HelperFiles/SampleMarkDown.txt");
+            markdownPreviewHandlerControl.DoPreview<string>("HelperFiles/MarkdownWithExternalImage.txt");
 
             // Assert
             Assert.IsInstanceOfType(markdownPreviewHandlerControl.Controls[0], typeof(WebBrowser));
@@ -39,6 +71,38 @@ namespace PreviewPaneUnitTests
             Assert.AreEqual(((WebBrowser)markdownPreviewHandlerControl.Controls[0]).IsWebBrowserContextMenuEnabled, false);
             Assert.AreEqual(((WebBrowser)markdownPreviewHandlerControl.Controls[0]).ScriptErrorsSuppressed, true);
             Assert.AreEqual(((WebBrowser)markdownPreviewHandlerControl.Controls[0]).ScrollBarsEnabled, true);
+        }
+
+        [TestMethod]
+        public void MarkdownPreviewHandlerControl__UpdateInfobarSettings_WhenDoPreviewIsCalled()
+        {
+            // Arrange 
+            MarkdownPreviewHandlerControl markdownPreviewHandlerControl = new MarkdownPreviewHandlerControl();
+
+            // Act
+            markdownPreviewHandlerControl.DoPreview<string>("HelperFiles/MarkdownWithExternalImage.txt");
+
+            // Assert
+            Assert.IsInstanceOfType(markdownPreviewHandlerControl.Controls[1], typeof(RichTextBox));
+            Assert.IsNotNull(((RichTextBox)markdownPreviewHandlerControl.Controls[1]).Text);
+            Assert.AreEqual(((RichTextBox)markdownPreviewHandlerControl.Controls[1]).Dock, DockStyle.Top);
+            Assert.AreEqual(((RichTextBox)markdownPreviewHandlerControl.Controls[1]).BorderStyle, BorderStyle.None);
+            Assert.AreEqual(((RichTextBox)markdownPreviewHandlerControl.Controls[1]).BackColor, Color.LightYellow);
+            Assert.AreEqual(((RichTextBox)markdownPreviewHandlerControl.Controls[1]).Multiline, true);
+        }
+
+        [TestMethod]
+        public void MarkdownPreviewHandlerControl_RemovesScriptTags_RemoveScriptFromHTMLIsCalled()
+        {
+            // Arrange
+            MarkdownPreviewHandlerControl markdownPreviewHandlerControl = new MarkdownPreviewHandlerControl();
+            string html = "<html><style></style><script>alert(\"hello\");</script><script></script></html>";
+
+            // Act
+            string parsedHTML = markdownPreviewHandlerControl.RemoveScriptFromHTML(html);
+
+            // Assert
+            Assert.AreEqual(parsedHTML, "<html>\r\n  <style></style>\r\n</html>");
         }
     }
 }
