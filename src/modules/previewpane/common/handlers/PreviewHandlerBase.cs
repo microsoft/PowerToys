a3d90ff912cfcf72cf183e85efce6c18f64b974f
udit3333 <udit3333@gmail.com>
2020-02-11 18:30:36 -0800

    Remove script and image element from Svg (#1231)
    * Added implementation to remove script and image tag
    
    * Added Unit tests for SvgPreviewHandlerHelper
    
    * Updated Unit tests for SvgPreviewControl
    
    * Moved the hardcoded string to resource file
    
    * Changed the LocalMachine to CurrentUser for preview handler registration
    
    * Added unit tests for multiple blocked elements tags

--- src/modules/previewpane/common/handlers/PreviewHandlerBase.cs
+++ src/modules/previewpane/common/handlers/PreviewHandlerBase.cs
@@ -203,7 +203,7 @@ namespace Common
             }
 
             // Add preview handler to preview handler list
-            using (RegistryKey handlersKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PreviewHandlers", true))
+            using (RegistryKey handlersKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PreviewHandlers", true))
             {
                 handlersKey.SetValue(previewerGuid, name, RegistryValueKind.String);
             }
@@ -244,7 +244,7 @@ namespace Common
                 appIdsKey.DeleteSubKey(appId);
             }
 
-            using (RegistryKey classesKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PreviewHandlers", true))
+            using (RegistryKey classesKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PreviewHandlers", true))
             {
                 classesKey.DeleteValue(previewerGuid);
             }
