25f85ddf9a6d94073cb809d8daaab4e7084266af
yuyoyuppe <a.yuyoyuppe@gmail.com>
2020-02-13 13:40:31 +0300

    fixup: launch MSIX version of PT after uninstallation

--- src/action_runner/action_runner.cpp
+++ src/action_runner/action_runner.cpp
@@ -6,6 +6,9 @@
 
 #include <common/msi_to_msix_upgrade_lib/msi_to_msix_upgrade.h>
 
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.Storage.h>
+
 void uninstall_msi_action()
 {
     const auto package_path = get_msi_package_path();
@@ -14,6 +17,15 @@ void uninstall_msi_action()
         return;
     }
     uninstall_msi_version(package_path);
+
+    // Launch PowerToys again, since it's been terminated by the MSI uninstaller
+    std::wstring runner_path{winrt::Windows::ApplicationModel::Package::Current().InstalledLocation().Path()};
+    runner_path += L"\\PowerToys.exe";
+    SHELLEXECUTEINFOW sei{sizeof(sei)};
+    sei.fMask = {SEE_MASK_FLAG_NO_UI | SEE_MASK_NOASYNC};
+    sei.lpFile = runner_path.c_str();
+    sei.nShow = SW_SHOWNORMAL;
+    ShellExecuteExW(&sei);
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
