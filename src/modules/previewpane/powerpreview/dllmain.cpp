69ae2344dd8f7adda380d593ccc1ece9c8e97254
udit3333 <udit3333@gmail.com>
2020-02-26 12:10:13 -0800

    (0.16) - Install preview handler with msix (#1339)
    * Removed the registration logic from preview handlers
    
    * Updated the output path
    
    * Added logic to shim the activation of .net assembly
    
    * Updated manifest file
    
    * Fix the allowedsilenttakeover filed in manifest
    
    * Updated the appxmanifest file
    
    * Added AllowSilentDefaultTakeOver in manifest
    
    * Fix returned error code by DllGetClassObject
    
    * Moved the CLSID to a common header file
    
    * Added info about where CLSID needs to be updated
    
    * Added a .reg file for the Keys added in registry.dat
    
    * Added comments for DllGetClassObject

--- src/modules/previewpane/powerpreview/dllmain.cpp
+++ src/modules/previewpane/powerpreview/dllmain.cpp
@@ -2,7 +2,34 @@
 #include <interface/powertoy_module_interface.h>
 #include "trace.h"
 #include "powerpreview.h"
+#include "CLSID.h"
 
+// Logic to shim the Activation of .Net Assembly by calling CoGetClassObject. CLSID's of Preview Handlers should be present in the registry.dat under /Classes/CLSID/{guid}.
+// See the existing Preview Handlers registry entry in registry.reg file.
+// This is required since MSIX currently not support .Net Assembly for Com Activation for Preview Handlers.
+HRESULT CALLBACK DllGetClassObject(REFCLSID clsid, REFIID riid, void** ppv)
+{
+    *ppv = NULL;
+    HRESULT hr = CLASS_E_CLASSNOTAVAILABLE;
+
+    for (auto handler : NativeToManagedClsid)
+    {
+        if (handler.first == clsid)
+        {
+            hr = CoGetClassObject(handler.second, CLSCTX_INPROC_SERVER, NULL, riid, ppv);
+            break;
+        }
+    }
+
+    // In case of failed error code return by CoGetClassObject return CLASS_E_CLASSNOTAVAILABLE to the caller.
+    if (FAILED(hr))
+    {
+        hr = CLASS_E_CLASSNOTAVAILABLE;
+        *ppv = NULL;
+    }
+
+    return hr;
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
