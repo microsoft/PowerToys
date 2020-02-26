c5710d0035b1ea13000dd9724fe674f48ac95344
Enrico Giordani <enrico.giordani@gmail.com>
2020-02-26 18:50:37 +0100

    [CDPx] remove nuget.config and update restore script

--- .pipelines/restore-telemetry.cmd
+++ .pipelines/restore-telemetry.cmd
@@ -1,5 +1,5 @@
 cd /D "%~dp0"
 
-call nuget.exe restore -ConfigFile nuget.config packages.config || exit /b 1
+call nuget.exe restore -PackagesDirectory . packages.config || exit /b 1
 
 move /Y "Microsoft.PowerToys.Telemetry.1.0.0\build\include\TraceLoggingDefines.h" "..\src\common\Telemetry\TraceLoggingDefines.h" || exit /b 1
