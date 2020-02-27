f9a835c58f6859f8ab6877c6593c8c98695b31a3
Serafima Zykova <zykovas91@gmail.com>
2020-02-27 20:27:10 +0300

    launch power toys for tests automatically

--- src/tests/win-app-driver/PowerToysSession.cs
+++ src/tests/win-app-driver/PowerToysSession.cs
@@ -2,7 +2,6 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
-using OpenQA.Selenium.Remote;
 using OpenQA.Selenium;
 
 namespace PowerToysTests
@@ -11,10 +10,11 @@ namespace PowerToysTests
     {
         protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
         protected static WindowsDriver<WindowsElement> session;
+        protected static bool isPowerToysLaunched = false;
+        protected static WindowsElement trayButton;
 
         public static void Setup(TestContext context)
         {
-            // TODO: Launch PowerToys application if it is not yet launched
             if (session == null)
             {
                 // Create a new Desktop session to use PowerToys.
@@ -23,6 +23,14 @@ namespace PowerToysTests
                 appiumOptions.AddAdditionalCapability("app", "Root");
                 session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appiumOptions);
                 Assert.IsNotNull(session);
+
+                trayButton = session.FindElementByAccessibilityId("1502");
+
+                isPowerToysLaunched = CheckPowerToysLaunched();
+                if (!isPowerToysLaunched)
+                {
+                    LaunchPowerToys();
+                }
             }
 
         }
@@ -36,5 +44,41 @@ namespace PowerToysTests
             }
         }
 
+        private static bool CheckPowerToysLaunched()        
+        {
+            trayButton.Click();
+            bool isLaunched = false;
+
+            try
+            {
+                WindowsElement pt = session.FindElementByName("PowerToys");
+                isLaunched = (pt != null);
+            }
+            catch(OpenQA.Selenium.WebDriverException)
+            {
+                //PowerToys not found
+            }
+
+            trayButton.Click(); //close
+            return isLaunched;
+        }
+
+        public static void LaunchPowerToys()
+        {
+            try
+            {
+                AppiumOptions opts = new AppiumOptions();
+                opts.PlatformName = "Windows";
+                opts.AddAdditionalCapability("app", "Microsoft.PowerToys_8wekyb3d8bbwe!PowerToys");
+                
+                WindowsDriver<WindowsElement> driver = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), opts);
+                Assert.IsNotNull(driver);
+                driver.LaunchApp();
+            }
+            catch (OpenQA.Selenium.WebDriverException)
+            {
+                //exception could be thrown even if app launched successfully
+            }
+        }
     }
 }
