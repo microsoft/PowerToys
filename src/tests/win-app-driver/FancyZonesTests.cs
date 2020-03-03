5ea0fb5cca0eb850ecda199931e228c326ba3272
Serafima Zykova <zykovas91@gmail.com>
2020-03-03 15:08:07 +0300

    toggles click tests

--- src/tests/win-app-driver/FancyZonesTests.cs
+++ src/tests/win-app-driver/FancyZonesTests.cs
@@ -49,7 +49,51 @@ namespace PowerToysTests
             Assert.IsNotNull(editorWindow);
         }
 
+        /*
+         * click each toggle,
+         * save changes,
+         * check if settings are changed after clicking save button
+         */
         [TestMethod]
+        public void TogglesSingleClickSaveButtonTest()
+        {
+            OpenFancyZonesSettings();
+
+            WindowsElement saveButton = session.FindElementByName("Save");
+            Assert.IsNotNull(saveButton);
+            string IsEnabled = saveButton.GetAttribute("IsEnabled");
+            Assert.AreEqual("False", IsEnabled);
+                        
+            for (int i = 37; i < 45; i++)
+            {
+                string toggleId = "Toggle" + i.ToString();
+                WindowsElement toggle = session.FindElementByAccessibilityId(toggleId);
+                Assert.IsNotNull(toggle);
+                toggle.Click();
+
+                IsEnabled = saveButton.GetAttribute("IsEnabled");
+                Assert.AreEqual("True", IsEnabled);
+
+                saveButton.Click();
+                IsEnabled = saveButton.GetAttribute("IsEnabled");
+                Assert.AreEqual("False", IsEnabled);
+            }
+            
+            //check saved settings
+            JObject savedSettings = JObject.Parse(File.ReadAllText(_settingsPath));
+            JObject savedProps = savedSettings["properties"].ToObject<JObject>();
+            JObject initialProps = _initialSettingsJson["properties"].ToObject<JObject>();
+
+            Assert.AreNotEqual(initialProps["fancyzones_shiftDrag"].ToObject<JObject>()["value"].Value<bool>(), savedProps["fancyzones_shiftDrag"].ToObject<JObject>()["value"].Value<bool>());
+            Assert.AreNotEqual(initialProps["fancyzones_overrideSnapHotkeys"].ToObject<JObject>()["value"].Value<bool>(), savedProps["fancyzones_overrideSnapHotkeys"].ToObject<JObject>()["value"].Value<bool>());
+            Assert.AreNotEqual(initialProps["fancyzones_zoneSetChange_flashZones"].ToObject<JObject>()["value"].Value<bool>(), savedProps["fancyzones_zoneSetChange_flashZones"].ToObject<JObject>()["value"].Value<bool>());
+            Assert.AreNotEqual(initialProps["fancyzones_displayChange_moveWindows"].ToObject<JObject>()["value"].Value<bool>(), savedProps["fancyzones_displayChange_moveWindows"].ToObject<JObject>()["value"].Value<bool>());
+            Assert.AreNotEqual(initialProps["fancyzones_zoneSetChange_moveWindows"].ToObject<JObject>()["value"].Value<bool>(), savedProps["fancyzones_zoneSetChange_moveWindows"].ToObject<JObject>()["value"].Value<bool>());
+            Assert.AreNotEqual(initialProps["fancyzones_virtualDesktopChange_moveWindows"].ToObject<JObject>()["value"].Value<bool>(), savedProps["fancyzones_virtualDesktopChange_moveWindows"].ToObject<JObject>()["value"].Value<bool>());
+            Assert.AreNotEqual(initialProps["fancyzones_appLastZone_moveWindows"].ToObject<JObject>()["value"].Value<bool>(), savedProps["fancyzones_appLastZone_moveWindows"].ToObject<JObject>()["value"].Value<bool>());
+            Assert.AreNotEqual(initialProps["use_cursorpos_editor_startupscreen"].ToObject<JObject>()["value"].Value<bool>(), savedProps["use_cursorpos_editor_startupscreen"].ToObject<JObject>()["value"].Value<bool>());
+        }
+
         /*
          * click each toggle twice,
          * save changes,
