c416967ba80b420ccb5867521f5f6c9ab5dec493
Serafima Zykova <zykovas91@gmail.com>
2020-03-03 18:23:41 +0300

    opacity highlight input tests

--- src/tests/win-app-driver/FancyZonesTests.cs
+++ src/tests/win-app-driver/FancyZonesTests.cs
@@ -1,9 +1,10 @@
 using System;
 using System.IO;
+using System.Windows.Forms;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Appium.Windows;
 using Newtonsoft.Json.Linq;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
 
 namespace PowerToysTests
 {
@@ -22,6 +23,27 @@ namespace PowerToysTests
             fzMenuButton.Click();
 
             ShortWait();
+        }
+        
+        private void SaveAndCheckOpacitySettings(WindowsElement saveButton, WindowsElement editor, int expected)
+        {
+            Assert.AreEqual(expected.ToString() + "\r\n", editor.Text);
+
+            saveButton.Click();
+            ShortWait();
+
+            JObject savedSettings = JObject.Parse(File.ReadAllText(_settingsPath));
+            JObject savedProps = savedSettings["properties"].ToObject<JObject>();
+            int value = savedProps["fancyzones_highlight_opacity"].ToObject<JObject>()["value"].Value<int>();
+            Assert.AreEqual(expected, value);
+        }
+
+        private void SetOpacity(WindowsElement editor, string key)
+        {
+            editor.Click(); //activate
+            editor.SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Backspace); //clear previous value
+            editor.SendKeys(key);
+            editor.SendKeys(OpenQA.Selenium.Keys.Enter); //confirm changes
         }
 
         [TestMethod]
@@ -128,6 +150,95 @@ namespace PowerToysTests
             string savedSettings = File.ReadAllText(_settingsPath);
             Assert.AreEqual(_initialSettings, savedSettings);
         }
+
+        [TestMethod]
+        public void HighlightOpacitySetValue()
+        {
+            OpenFancyZonesSettings();
+            WindowsElement saveButton = session.FindElementByName("Save");
+            Assert.IsNotNull(saveButton);
+
+            WindowsElement editor = session.FindElementByName("Zone Highlight Opacity (%)");
+            Assert.IsNotNull(editor);
+
+            SetOpacity(editor, "50");
+            SaveAndCheckOpacitySettings(saveButton, editor, 50);
+
+            SetOpacity(editor, "-50");
+            SaveAndCheckOpacitySettings(saveButton, editor, 0);
+
+            SetOpacity(editor, "200");
+            SaveAndCheckOpacitySettings(saveButton, editor, 100);
+
+            //for invalid input values previously saved value expected
+            SetOpacity(editor, "asdf"); 
+            SaveAndCheckOpacitySettings(saveButton, editor, 100); 
+            
+            SetOpacity(editor, "*");
+            SaveAndCheckOpacitySettings(saveButton, editor, 100); 
+            
+            SetOpacity(editor, OpenQA.Selenium.Keys.Return);
+            SaveAndCheckOpacitySettings(saveButton, editor, 100);
+
+            Clipboard.SetText("Hello, clipboard");
+            SetOpacity(editor, OpenQA.Selenium.Keys.Control + "v");
+            SaveAndCheckOpacitySettings(saveButton, editor, 100);
+        }
+
+        [TestMethod]
+        public void HighlightOpacityIncreaseValue()
+        {
+            OpenFancyZonesSettings();
+            WindowsElement saveButton = session.FindElementByName("Save");
+            Assert.IsNotNull(saveButton);
+
+            WindowsElement editor = session.FindElementByName("Zone Highlight Opacity (%)");
+            Assert.IsNotNull(editor);
+
+            SetOpacity(editor, "99");
+            SaveAndCheckOpacitySettings(saveButton, editor, 99);
+
+            System.Drawing.Rectangle editorRect = editor.Rect;
+            
+            Actions action = new Actions(session);
+            action.MoveToElement(editor).MoveByOffset(editorRect.Width / 2 + 10, -editorRect.Height / 4).Perform();
+            ShortWait();
+
+            action.Click().Perform();
+            Assert.AreEqual("100\r\n", editor.Text);
+            SaveAndCheckOpacitySettings(saveButton, editor, 100);
+
+            action.Click().Perform();
+            Assert.AreEqual("100\r\n", editor.Text);
+            SaveAndCheckOpacitySettings(saveButton, editor, 100);
+        }
+
+        [TestMethod]
+        public void HighlightOpacityDecreaseValue()
+        {
+            OpenFancyZonesSettings();
+            WindowsElement saveButton = session.FindElementByName("Save");
+            Assert.IsNotNull(saveButton);
+
+            WindowsElement editor = session.FindElementByName("Zone Highlight Opacity (%)");
+            Assert.IsNotNull(editor);
+
+            SetOpacity(editor, "1");
+            SaveAndCheckOpacitySettings(saveButton, editor, 1);
+
+            System.Drawing.Rectangle editorRect = editor.Rect;
+
+            Actions action = new Actions(session);
+            action.MoveToElement(editor).MoveByOffset(editorRect.Width / 2 + 10, editorRect.Height / 4).Perform();
+            ShortWait();
+
+            action.Click().Perform();
+            Assert.AreEqual("0\r\n", editor.Text);
+            SaveAndCheckOpacitySettings(saveButton, editor, 0);
+
+            action.Click().Perform();
+            Assert.AreEqual("0\r\n", editor.Text);
+            SaveAndCheckOpacitySettings(saveButton, editor, 0);
         }
 
         [ClassInitialize]
@@ -145,8 +256,16 @@ namespace PowerToysTests
         [TestInitialize]
         public void TestInitialize()
         {
-            _initialSettings = File.ReadAllText(_settingsPath);
-            _initialSettingsJson = JObject.Parse(_initialSettings);
+            try
+            {
+                _initialSettings = File.ReadAllText(_settingsPath);
+                _initialSettingsJson = JObject.Parse(_initialSettings);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                _initialSettings = "";
+            }
+            
 
             OpenSettings();
             ShortWait();
@@ -160,7 +279,7 @@ namespace PowerToysTests
                 WindowsElement editorWindow = session.FindElementByName("FancyZones Editor");
                 if (editorWindow != null)
                 {
-                    editorWindow.SendKeys(Keys.Alt + Keys.F4);
+                    editorWindow.SendKeys(OpenQA.Selenium.Keys.Alt + OpenQA.Selenium.Keys.F4);
                 }
             } 
             catch (OpenQA.Selenium.WebDriverException)
@@ -169,7 +288,10 @@ namespace PowerToysTests
             }
 
             CloseSettings();
-            File.WriteAllText(_settingsPath, _initialSettings);
+            if (_initialSettings.Length > 0)
+            {
+                File.WriteAllText(_settingsPath, _initialSettings);
+            }            
         }
     }
 }
