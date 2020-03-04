6f0daab8d2032db2585fa59010361aa97f18b447
yuyoyuppe <a.yuyoyuppe@gmail.com>
2020-03-04 20:06:27 +0300

    MSIX: inject correct version to appxmanifest

--- installer/MSIX/build_msix_cdpx.cmd
+++ installer/MSIX/build_msix_cdpx.cmd
@@ -1,6 +1,9 @@
 cd /D "%~dp0"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 -winsdk=10.0.18362.0
+
+powershell -file update_appxmanifest_version.ps1 || exit /b 1
+
 call makeappx build /v /overwrite /f PackagingLayout.xml /id "PowerToys-x64" /op bin\ || exit /b 1
 
 setlocal EnableDelayedExpansion
