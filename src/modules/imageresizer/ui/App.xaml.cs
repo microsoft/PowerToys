5f4d345c7e34141ad51ae6aead6912c379568d49
Arjun Balgovind <32061677+arjunbalgovind@users.noreply.github.com>
2020-03-05 09:19:52 -0800

    Implemented fix for foreground issue for ImageResizer (#1434)

--- src/modules/imageresizer/ui/App.xaml.cs
+++ src/modules/imageresizer/ui/App.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using GalaSoft.MvvmLight.Threading;
 using ImageResizer.Models;
 using ImageResizer.Properties;
+using ImageResizer.Utilities;
 using ImageResizer.ViewModels;
 using ImageResizer.Views;
 
@@ -26,7 +27,19 @@ namespace ImageResizer
             var batch = ResizeBatch.FromCommandLine(Console.In, e.Args);
 
             // TODO: Add command-line parameters that can be used in lieu of the input page (issue #14)
-            new MainWindow(new MainViewModel(batch, Settings.Default)).Show();
+            var mainWindow = new MainWindow(new MainViewModel(batch, Settings.Default));
+            mainWindow.Show();
+
+            // Temporary workaround for issue #1273
+            BecomeForegroundWindow(new System.Windows.Interop.WindowInteropHelper(mainWindow).Handle);
+        }
+
+        private void BecomeForegroundWindow(IntPtr hWnd)
+        {
+            Win32Helpers.INPUT input = new Win32Helpers.INPUT { type = Win32Helpers.INPUTTYPE.INPUT_MOUSE, data = { } };
+            Win32Helpers.INPUT[] inputs = new Win32Helpers.INPUT[] { input };
+            Win32Helpers.SendInput(1, inputs, Win32Helpers.INPUT.Size);
+            Win32Helpers.SetForegroundWindow(hWnd);
         }
     }
 }
