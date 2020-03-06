b5a14aaebc79f79ee6c0830a1758385618b50441
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-03-06 13:01:44 -0800

    Added tests

---
+++ src/settingsUintTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace settingsUintTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
