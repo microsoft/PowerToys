2e0fce329cdf209e8c03d6261a47b2cebd5afd41
PrzemyslawTusinski <61138537+PrzemyslawTusinski@users.noreply.github.com>
2020-03-06 12:31:39 +0100

    Typo fix, rename show_fancy_zones_on_all_monitors - >showZonesOnAllMonitors

--- src/modules/fancyzones/lib/Settings.cpp
+++ src/modules/fancyzones/lib/Settings.cpp
@@ -45,7 +45,7 @@ private:
         { L"fancyzones_virtualDesktopChange_moveWindows", &m_settings.virtualDesktopChange_moveWindows, IDS_SETTING_DESCRIPTION_VIRTUALDESKTOPCHANGE_MOVEWINDOWS },
         { L"fancyzones_appLastZone_moveWindows", &m_settings.appLastZone_moveWindows, IDS_SETTING_DESCRIPTION_APPLASTZONE_MOVEWINDOWS },
         { L"use_cursorpos_editor_startupscreen", &m_settings.use_cursorpos_editor_startupscreen, IDS_SETTING_DESCRIPTION_USE_CURSORPOS_EDITOR_STARTUPSCREEN },
-        { L"fancyzones_show_on_all_monitors", &m_settings.show_fancy_zones_on_all_monitors, IDS_SETTING_DESCRIPTION_SHOW_FANCY_ZONES_ON_ALL_MONITORS},
+        { L"fancyzones_show_on_all_monitors", &m_settings.showZonesOnAllMonitors, IDS_SETTING_DESCRIPTION_SHOW_FANCY_ZONES_ON_ALL_MONITORS},
     };
 
     const std::wstring m_zoneHiglightName = L"fancyzones_zoneHighlightColor";
