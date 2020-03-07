ed069c4280332d66627ea4896a2c3c0264355955
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-03-06 17:46:51 -0800

    Lavius / Base Project for New Settings Project (#1481)
    * base WPF Project
    
    * Added tests
    
    * Update build configuration .sln file
    
    * removed unnessesary asset files

---
+++ src/settings_v2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace settings_v2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ src/settings_v2/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+
+namespace settings_v2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
