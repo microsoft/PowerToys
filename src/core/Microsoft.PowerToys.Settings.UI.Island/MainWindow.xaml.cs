5ade906a7b0285dd67b753dd4f4a316a1eff1f9f
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-03-09 23:55:08 -0700

    archive

--- src/core/Microsoft.PowerToys.Settings.UI.Island/MainWindow.xaml.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Island/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Windows;
-using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+﻿using System;
+using System.Windows;
+using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using Microsoft.PowerToys.Settings.UI.Controls;
 
-namespace SettingsMain
+namespace SettingsRunner
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -11,6 +13,26 @@ namespace SettingsMain
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void WindowsXamlHost_ChildChanged(object sender, EventArgs e)
+        {
+            // Hook up x:Bind source.
+            WindowsXamlHost windowsXamlHost = sender as WindowsXamlHost;
+            DummyUserControl userControl = windowsXamlHost.GetUwpInternalObject() as DummyUserControl;
+
+            if (userControl != null)
+            {
+                userControl.XamlIslandMessage = this.WPFMessage;
+            }
+        }
+
+        public string WPFMessage
+        {
+            get
+            {
+                return "Binding from WPF to UWP XAML";
+            }
         }
     }
 }
