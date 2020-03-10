bb7801436f8c2dc7ef533bd3b244d61edb2e2a99
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-03-09 23:55:08 -0700

    archive

--- src/core/Microsoft.PowerToys.Settings.UI.Island/App.xaml.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Island/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SettingsMain
+namespace SettingsRunner
 {
     /// <summary>
     /// Interaction logic for App.xaml
