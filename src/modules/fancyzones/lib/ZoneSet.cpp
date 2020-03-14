3a1e52c6884a597516ccb26ae82ed27412d335eb
ivan100sic <ivan100sic@gmail.com>
2020-03-14 12:20:42 +0100

    Fixed some compiler warnings

--- src/modules/fancyzones/lib/ZoneSet.cpp
+++ src/modules/fancyzones/lib/ZoneSet.cpp
@@ -560,7 +560,7 @@ bool ZoneSet::CalculateGridZones(Rect workArea, JSONHelpers::GridLayoutInfo grid
 
     // Note: The expressions below are carefully written to 
     // make the sum of all zones' sizes exactly total{Width|Height}
-    long long totalPercents = 0;
+    long totalPercents = 0;
     for (int row = 0; row < gridLayoutInfo.rows(); row++)
     {
         rowInfo[row].Start = totalPercents * totalHeight / C_MULTIPLIER + (row + 1) * spacing;
