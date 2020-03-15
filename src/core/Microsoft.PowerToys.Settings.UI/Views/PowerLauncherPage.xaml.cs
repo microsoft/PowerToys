4578c4b9b5fe6f8cc3ef1dc1c5f440dbdadfb21c
Niels Laute <niels.laute@live.nl>
2020-03-15 14:38:23 +0100

    Minor comments for shortcut textboxes

---
+++ src/core/Microsoft.PowerToys.Settings.UI/Views/PowerLauncherPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.PowerToys.Settings.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Microsoft.PowerToys.Settings.UI.Views
+{
+    public sealed partial class PowerLauncherPage : Page
+    {
+        public PowerLauncherViewModel ViewModel { get; } = new PowerLauncherViewModel();
+
+        public PowerLauncherPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
