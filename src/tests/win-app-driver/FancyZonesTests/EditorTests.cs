af3d82d99dad34c4d6ecc619f915f074d0ef5ffd
Serafima Zykova <zykovas91@gmail.com>
2020-03-16 16:34:13 +0300

    added editor tests file

---
+++ src/tests/win-app-driver/FancyZonesTests/EditorTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace PowerToysTests
+{
+    [TestClass]
+    public class FancyZonesEditorTests : PowerToysSession
+    {
+        [TestMethod]
+        public void OpenEditorBySettingsButton()
+        {
+            OpenFancyZonesSettings();
+
+            WindowsElement editorButton = session.FindElementByXPath("//Button[@Name=\"Edit zones\"]");
+            Assert.IsNotNull(editorButton);
+
+            editorButton.Click();
+            ShortWait();
+
+            WindowsElement editorWindow = session.FindElementByXPath("//Window[@Name=\"FancyZones Editor\"]");
+            Assert.IsNotNull(editorWindow);
+
+            //Close editor
+            session.FindElementByAccessibilityId("PART_Close").Click();
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+            OpenSettings();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            CloseSettings();
+            TearDown();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            
+        }
+    }
+}
