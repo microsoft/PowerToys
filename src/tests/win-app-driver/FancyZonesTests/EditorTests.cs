9f472c9db785bc4855fad5b20752bda40d0fadd5
Serafima Zykova <zykovas91@gmail.com>
2020-03-17 19:43:36 +0300

    open editor using button with different settings

--- src/tests/win-app-driver/FancyZonesTests/EditorTests.cs
+++ src/tests/win-app-driver/FancyZonesTests/EditorTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Appium.Windows;
 
 namespace PowerToysTests
@@ -6,9 +8,50 @@ namespace PowerToysTests
     [TestClass]
     public class FancyZonesEditorTests : PowerToysSession
     {
-        [TestMethod]
-        public void OpenEditorBySettingsButton()
+        WindowsElement editorWindow;
+
+        void RemoveSettingsFile()
         {
+            if (Directory.Exists(_zoneSettingsPath))
+            {
+                Directory.Delete(_zoneSettingsPath);
+            }
+        }
+
+        void CreateEmptySettingsFile()
+        {
+            string zoneSettings = "";
+            File.WriteAllText(_zoneSettingsPath, zoneSettings);
+        }
+
+        void CreateDefaultSettingsFile()
+        {
+            string zoneSettings = "{\"app-zone-history\":[],\"devices\":[],\"custom-zone-sets\":[]}";
+            File.WriteAllText(_zoneSettingsPath, zoneSettings);
+        }
+
+        void CreateValidSettingsFile()
+        {
+            string zoneSettings = "{\"app-zone-history\":[{\"app-path\":\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\Common7\\IDE\\Extensions\\TestPlatform\\testhost.exe\",\"zone-index\":3,\"device-id\":\"DELA026#5&10a58c63&0&UID16777488_1920_1200_{39B25DD2-130D-4B5D-8851-4791D66B1539}\",\"zoneset-uuid\":\"{D13ABB6D-7721-4176-9647-C8C0836D99CC}\"}],\"devices\":[{\"device-id\":\"DELA026#5&10a58c63&0&UID16777488_1920_1200_{39B25DD2-130D-4B5D-8851-4791D66B1539}\",\"active-zoneset\":{\"uuid\":\"{D13ABB6D-7721-4176-9647-C8C0836D99CC}\",\"type\":\"columns\"},\"editor-show-spacing\":true,\"editor-spacing\":16,\"editor-zone-count\":3}],\"custom-zone-sets\":[]}";
+            File.WriteAllText(_zoneSettingsPath, zoneSettings);
+        }
+
+        void CreateValidSettingsFileWithUtf8()
+        {
+            string zoneSettings = "{\"app-zone-history\":[{\"app-path\":\"C:\\Program Files (x86)\\йцукен\\testhost.exe\",\"zone-index\":3,\"device-id\":\"DELA026#5&10a58c63&0&UID16777488_1920_1200_{39B25DD2-130D-4B5D-8851-4791D66B1539}\",\"zoneset-uuid\":\"{D13ABB6D-7721-4176-9647-C8C0836D99CC}\"}],\"devices\":[{\"device-id\":\"DELA026#5&10a58c63&0&UID16777488_1920_1200_{39B25DD2-130D-4B5D-8851-4791D66B1539}\",\"active-zoneset\":{\"uuid\":\"{D13ABB6D-7721-4176-9647-C8C0836D99CC}\",\"type\":\"columns\"},\"editor-show-spacing\":true,\"editor-spacing\":16,\"editor-zone-count\":3}],\"custom-zone-sets\":[]}";
+            File.WriteAllText(_zoneSettingsPath, zoneSettings);
+        }
+
+        void CreateInvalidSettingsFile()
+        {
+            string zoneSettings = "{\"app-zone-history\":[{\"app-path\":\"C:\\Program Files (x86)\\Microsoft Visual Studio\\testhost.exe\",\"zone-index\":3,\"device-id\":\"wrong-device-id\",\"zoneset-uuid\":\"{D13ABB6D-invalid-uuid-C8C0836D99CC}\"}],\"devices\":[{\"device-id\":\"DELA026#5&10a58c63&0&UID16777488_1920_1200_{39B25DD2-130D-4B5D-8851-4791D66B1539}\",\"active-zoneset\":{\"uuid\":\"{D13ABB6D-7721-4176-9647-C8C0836D99CC}\",\"type\":\"columns\"},\"editor-show-spacing\":true,\"editor-spacing\":16,\"editor-zone-count\":3}],\"custom-zone-sets\":[]}";
+            File.WriteAllText(_zoneSettingsPath, zoneSettings);
+        }
+
+        [TestMethod]
+        public void OpenEditorBySettingsButtonNoSettings()
+        {
+            RemoveSettingsFile();
             OpenFancyZonesSettings();
 
             WindowsElement editorButton = session.FindElementByXPath("//Button[@Name=\"Edit zones\"]");
@@ -17,11 +60,88 @@ namespace PowerToysTests
             editorButton.Click();
             ShortWait();
 
-            WindowsElement editorWindow = session.FindElementByXPath("//Window[@Name=\"FancyZones Editor\"]");
+            editorWindow = session.FindElementByXPath("//Window[@Name=\"FancyZones Editor\"]");
             Assert.IsNotNull(editorWindow);
+        }
 
-            //Close editor
-            session.FindElementByAccessibilityId("PART_Close").Click();
+        [TestMethod]
+        public void OpenEditorBySettingsButtonEmptySettings()
+        {
+            CreateEmptySettingsFile();
+            OpenFancyZonesSettings();
+
+            WindowsElement editorButton = session.FindElementByXPath("//Button[@Name=\"Edit zones\"]");
+            Assert.IsNotNull(editorButton);
+
+            editorButton.Click();
+            ShortWait();
+
+            editorWindow = session.FindElementByXPath("//Window[@Name=\"FancyZones Editor\"]");
+            Assert.IsNotNull(editorWindow);
+        }
+
+        [TestMethod]
+        public void OpenEditorBySettingsButtonDefaultSettings()
+        {
+            CreateDefaultSettingsFile();
+            OpenFancyZonesSettings();
+
+            WindowsElement editorButton = session.FindElementByXPath("//Button[@Name=\"Edit zones\"]");
+            Assert.IsNotNull(editorButton);
+
+            editorButton.Click();
+            ShortWait();
+
+            editorWindow = session.FindElementByXPath("//Window[@Name=\"FancyZones Editor\"]");
+            Assert.IsNotNull(editorWindow);
+        }
+
+        [TestMethod]
+        public void OpenEditorBySettingsButtonValidSettings()
+        {
+            CreateValidSettingsFile();
+            OpenFancyZonesSettings();
+
+            WindowsElement editorButton = session.FindElementByXPath("//Button[@Name=\"Edit zones\"]");
+            Assert.IsNotNull(editorButton);
+
+            editorButton.Click();
+            ShortWait();
+
+            editorWindow = session.FindElementByXPath("//Window[@Name=\"FancyZones Editor\"]");
+            Assert.IsNotNull(editorWindow);
+        }
+
+        [TestMethod]
+        public void OpenEditorBySettingsButtonValidUtf8Settings()
+        {
+            CreateValidSettingsFileWithUtf8();
+            OpenFancyZonesSettings();
+
+            WindowsElement editorButton = session.FindElementByXPath("//Button[@Name=\"Edit zones\"]");
+            Assert.IsNotNull(editorButton);
+
+            editorButton.Click();
+            ShortWait();
+
+            editorWindow = session.FindElementByXPath("//Window[@Name=\"FancyZones Editor\"]");
+            Assert.IsNotNull(editorWindow);
+        }
+
+        [TestMethod]
+        public void OpenEditorBySettingsButtonInvalidSettings()
+        {
+            CreateInvalidSettingsFile();
+            OpenFancyZonesSettings();
+
+            WindowsElement editorButton = session.FindElementByXPath("//Button[@Name=\"Edit zones\"]");
+            Assert.IsNotNull(editorButton);
+
+            editorButton.Click();
+            ShortWait();
+
+            editorWindow = session.FindElementByXPath("//Window[@Name=\"FancyZones Editor\"]");
+            Assert.IsNotNull(editorWindow);
         }
 
         [ClassInitialize]
@@ -47,7 +167,11 @@ namespace PowerToysTests
         [TestCleanup]
         public void TestCleanup()
         {
-            
+            //Close editor
+            if (editorWindow != null)
+            {
+                editorWindow.SendKeys(OpenQA.Selenium.Keys.Alt + OpenQA.Selenium.Keys.F4);
+            }
         }
     }
 }
