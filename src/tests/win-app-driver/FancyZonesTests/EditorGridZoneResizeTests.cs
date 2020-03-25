3f213dca73ce768137ee944de72570bf8014f7f1
Serafima <zykovas91@gmail.com>
2020-03-25 18:34:00 +0300

    added grid-zone-resize test class

---
+++ src/tests/win-app-driver/FancyZonesTests/EditorGridZoneResizeTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+using System.Windows.Forms;
+
+namespace PowerToysTests
+{
+    [TestClass]
+    public class FancyZonesEditorGridZoneResizeTests : FancyZonesEditor
+    {
+        [TestMethod]
+        public void MoveVerticalDelimiter()
+        {
+            OpenCreatorWindow("Columns", "Custom layout creator", "EditTemplateButton");
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context, false);
+            ResetSettings();
+
+            if (!isPowerToysLaunched)
+            {
+                LaunchPowerToys();
+            }
+            OpenEditor();
+            OpenTemplates();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            CloseEditor();
+            TearDown();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            new Actions(session).MoveToElement(session.FindElementByXPath("//Button[@Name=\"Cancel\"]")).Click().Perform();
+        }
+    }
+}
