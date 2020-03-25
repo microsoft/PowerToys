d6fce7c0993936e1de2ca74bf8f119ce631ab8bf
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-03-25 16:34:56 -0700

    removing locks from the ResultListBox code behind file.  All callbacks are accessed from the Main/UI thread.

--- src/modules/launcher/Wox/ResultListBox.xaml.cs
+++ src/modules/launcher/Wox/ResultListBox.xaml.cs
@@ -6,7 +6,6 @@ namespace Wox
 {
     public partial class ResultListBox
     {
-        protected object _lock = new object();
         private Point _lastpos;
         private ListBoxItem curItem = null;
         public ResultListBox()
@@ -24,34 +23,25 @@ namespace Wox
 
         private void OnMouseEnter(object sender, MouseEventArgs e)
         {
-            lock(_lock)
-            {
-                curItem = (ListBoxItem)sender;
-                var p = e.GetPosition((IInputElement)sender);
-                _lastpos = p;
-            }
+            curItem = (ListBoxItem)sender;
+            var p = e.GetPosition((IInputElement)sender);
+            _lastpos = p;
         }
 
         private void OnMouseMove(object sender, MouseEventArgs e)
         {
-            lock(_lock)
+            var p = e.GetPosition((IInputElement)sender);
+            if (_lastpos != p)
             {
-                var p = e.GetPosition((IInputElement)sender);
-                if (_lastpos != p)
-                {
-                    ((ListBoxItem)sender).IsSelected = true;
-                }
+                ((ListBoxItem)sender).IsSelected = true;
             }
         }
 
         private void ListBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            lock(_lock)
+            if (curItem != null)
             {
-                if (curItem != null)
-                {
-                    curItem.IsSelected = true;
-                }
+                curItem.IsSelected = true;
             }
         }
     }
