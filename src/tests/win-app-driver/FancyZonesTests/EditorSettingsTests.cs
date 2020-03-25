9cd1e33be34b4b80091014b91f25ae839b56ed20
Yevhenii Holovachov <yevhenii.holovachov@gmail.com>
2020-03-25 11:06:40 +0200

    Added tests for padding checkbox and padding values

--- src/tests/win-app-driver/FancyZonesTests/EditorSettingsTests.cs
+++ src/tests/win-app-driver/FancyZonesTests/EditorSettingsTests.cs
@@ -61,11 +61,123 @@ namespace PowerToysTests
             Assert.AreEqual(zoneCountQty, getSavedZoneCount());
         }
 
+        [TestMethod]
+        public void SpacingTestsValid()
+        {
+            ShortWait();
+            OpenFancyZonesSettings();
+
+            WindowsElement editorButton = WaitElementByXPath("//Button[@Name=\"Edit zones\"]");
+            editorButton.Click();
+
+            WindowsElement spaceAroundSetting = WaitElementByAccessibilityId("spaceAroundSetting");
+            bool spaceAroundSettingValue = spaceAroundSetting.Selected;
+
+            string[] validValues = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+            foreach (string value in validValues)
+            {
+                WindowsElement paddingValue = WaitElementByAccessibilityId("paddingValue");
+                clearText(paddingValue);
+                paddingValue.SendKeys(value);
+                WindowsElement applyButton = WaitElementByAccessibilityId("ApplyTemplateButton");
+                applyButton.Click();
+                ShortWait();
+                Assert.AreEqual(spaceAroundSettingValue, getSavedSpaceAroundSetting());
+                Assert.AreEqual(value, getPaddingValue());
+                editorButton.Click();
+            }
+
+            spaceAroundSetting = WaitElementByAccessibilityId("spaceAroundSetting");
+            spaceAroundSetting.Click();
+            spaceAroundSettingValue = spaceAroundSetting.Selected;
+
+            foreach (string value in validValues)
+            {
+                WindowsElement paddingValue = WaitElementByAccessibilityId("paddingValue");
+                clearText(paddingValue);
+                paddingValue.SendKeys(value);
+                WindowsElement applyButton = WaitElementByAccessibilityId("ApplyTemplateButton");
+                applyButton.Click();
+                ShortWait();
+                Assert.AreEqual(spaceAroundSettingValue, getSavedSpaceAroundSetting());
+                Assert.AreEqual(value, getPaddingValue());
+                editorButton.Click();
+            }
+        }
+
+        [TestMethod]
+        public void SpacingTestsInvalid()
+        {
+            ShortWait();
+            OpenFancyZonesSettings();
+
+            WindowsElement editorButton = WaitElementByXPath("//Button[@Name=\"Edit zones\"]");
+            editorButton.Click();
+
+            WindowsElement spaceAroundSetting = WaitElementByAccessibilityId("spaceAroundSetting");
+            bool spaceAroundSettingValue = spaceAroundSetting.Selected;
+
+            string[] invalidValues = { "!", "/", "<", "?", "D", "Z", "]", "m", "}" };
+
+            string savedPadding = getPaddingValue();
+
+            foreach (string value in invalidValues)
+            {
+                WindowsElement paddingValue = WaitElementByAccessibilityId("paddingValue");
+                clearText(paddingValue);
+                paddingValue.SendKeys(value);
+                WindowsElement applyButton = WaitElementByAccessibilityId("ApplyTemplateButton");
+                applyButton.Click();
+                ShortWait();
+                Assert.AreEqual(spaceAroundSettingValue, getSavedSpaceAroundSetting());
+                Assert.AreEqual(savedPadding, getPaddingValue());
+                editorButton.Click();
+            }
+
+            spaceAroundSetting = WaitElementByAccessibilityId("spaceAroundSetting");
+            spaceAroundSetting.Click();
+            spaceAroundSettingValue = spaceAroundSetting.Selected;
+
+            foreach (string value in invalidValues)
+            {
+                WindowsElement paddingValue = WaitElementByAccessibilityId("paddingValue");
+                clearText(paddingValue);
+                paddingValue.SendKeys(value);
+                WindowsElement applyButton = WaitElementByAccessibilityId("ApplyTemplateButton");
+                applyButton.Click();
+                ShortWait();
+                Assert.AreEqual(spaceAroundSettingValue, getSavedSpaceAroundSetting());
+                Assert.AreEqual(savedPadding, getPaddingValue());
+                editorButton.Click();
+            }
+        }
+
         private int getSavedZoneCount()
         {
             JObject zoneSettings = JObject.Parse(File.ReadAllText(_zoneSettingsPath));
             int editorZoneCount = (int)zoneSettings["devices"][0]["editor-zone-count"];
             return editorZoneCount;
+        }
+
+        private bool getSavedSpaceAroundSetting()
+        {
+            JObject zoneSettings = JObject.Parse(File.ReadAllText(_zoneSettingsPath));
+            bool editorSpaceAroundSetting = (bool)zoneSettings["devices"][0]["editor-show-spacing"];
+            return editorSpaceAroundSetting;
+        }
+
+        private string getPaddingValue()
+        {
+            JObject zoneSettings = JObject.Parse(File.ReadAllText(_zoneSettingsPath));
+            string editorSpaceAroundSetting = (string)zoneSettings["devices"][0]["editor-spacing"];
+            return editorSpaceAroundSetting;
+        }
+
+        private void clearText(WindowsElement windowsElement)
+        {
+            windowsElement.SendKeys(Keys.Home);
+            windowsElement.SendKeys(Keys.Control + Keys.Delete);
         }
 
         [ClassInitialize]
