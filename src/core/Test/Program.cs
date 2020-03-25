f7adf53ba8853234aca4fdfc3f23186d1fd04c42
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-03-24 19:55:02 -0700

    cherry-pick : Added functionality for General Settings Page

---
+++ src/core/Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.PowerToys.Settings.UI.Lib;
+using Microsoft.PowerToys.Settings.UI.ViewModels;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneralSettings settings = SettingsUtils.GetSettings<GeneralSettings>(string.Empty);
+            OutGoingGeneralSettings outSettings = new OutGoingGeneralSettings(settings);
+            Console.WriteLine(outSettings.ToString());
+        }
+    }
+}
