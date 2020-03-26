15cefc664a215b697d4864d3dd3e216fae16af75
udit3333 <udit3333@gmail.com>
2020-03-26 13:58:52 -0700

    Svg Icon for Power preview module (#1718)
    * Added svg icon for power preview
    
    * Updated bundle.js

--- src/settings-web/src/setup_icons.tsx
+++ src/settings-web/src/setup_icons.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {registerIcons} from 'office-ui-fabric-react';
-import {Icon} from 'office-ui-fabric-react/lib/Icon';
 import {initializeIcons} from './icons/src';
 
 // Import SVG files for the icons here.
@@ -8,6 +7,7 @@ import FancyZonesSVG from './svg/fancy_zones.svg';
 import PowerRenameSVG from './svg/power_rename.svg';
 import ShortcutGuideSVG from './svg/shortcut_guide.svg';
 import ImageResizerSVG from './svg/image_resizer.svg';
+import PowerPreviewSVG from './svg/power_preview.svg';
 
 export function setup_powertoys_icons(): void {
   initializeIcons('icons/fonts/');
@@ -17,7 +17,7 @@ export function setup_powertoys_icons(): void {
       'pt-fancy-zones': ( <FancyZonesSVG /> ),
       'pt-power-rename': ( <PowerRenameSVG /> ),
       'pt-shortcut-guide': ( <ShortcutGuideSVG /> ),
-      'pt-power-preview': ( <Icon iconName="FabricReportLibrary" /> ),
+      'pt-power-preview': (<PowerPreviewSVG /> ),
       'pt-image-resizer': ( <ImageResizerSVG />) ,
     }
   });
