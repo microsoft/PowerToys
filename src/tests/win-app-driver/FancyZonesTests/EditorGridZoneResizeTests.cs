e8b6f0b8c98e581e07bf66f8d9a58a74e2d6d4f5
Serafima <zykovas91@gmail.com>
2020-03-26 16:11:36 +0300

    updated move tests

--- src/tests/win-app-driver/FancyZonesTests/EditorGridZoneResizeTests.cs
+++ src/tests/win-app-driver/FancyZonesTests/EditorGridZoneResizeTests.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium.Interactions;
+using System;
 using System.Collections.ObjectModel;
 using System.Windows.Forms;
 
@@ -10,24 +11,35 @@ namespace PowerToysTests
     [TestClass]
     public class FancyZonesEditorGridZoneResizeTests : FancyZonesEditor
     {
-        private const int defaultZoneSpace = 16;
+        private const int moveStep = 5;
 
-        private void MoveLeft(AppiumWebElement thumb, int leftBorder)
+        private void Move(AppiumWebElement thumb, int border, bool moveAscending, bool moveHorizontally)
         {
             Actions action = new Actions(session);
             action.ClickAndHold(thumb);
 
-            const int step = 5;
-            int moves = (thumb.Rect.X - leftBorder) / step;
-            for (int j = 0; j < moves; j++)
+            int thumbCenter = 0;
+            if (moveHorizontally)
             {
-                action.MoveByOffset(-step, 0);
+                thumbCenter = thumb.Rect.X + thumb.Rect.Width / 2;
+            }
+            else
+            {
+                thumbCenter = thumb.Rect.Y + thumb.Rect.Height / 2;
             }
 
-            //additional steps to move closer to border
-            for (int j = 0; j < step; j++)
+            int moves = Math.Abs(thumbCenter - border) / moveStep;
+            for (int j = 0; j < moves; j++)
             {
-                action.MoveByOffset(-1, 0);
+                int step = moveAscending ? moveStep : -moveStep;
+                if (moveHorizontally)
+                {
+                    action.MoveByOffset(step, 0);
+                }
+                else
+                {
+                    action.MoveByOffset(0, step);
+                }
             }
 
             action.Release().Perform();
@@ -44,14 +56,104 @@ namespace PowerToysTests
             ReadOnlyCollection<AppiumWebElement> thumbs = gridEditor.FindElementsByClassName("Thumb");
             Assert.AreEqual(2, thumbs.Count);
 
+            //move left
             for (int i = 0; i < thumbs.Count; i++)
             {
                 AppiumWebElement thumb = thumbs[i];
-                int border = i == 0 ? 0 : thumbs[i - 1].Rect.X;
-                MoveLeft(thumb, border);
-                Assert.IsTrue(thumb.Rect.Left <= border);
-                Assert.IsTrue(thumb.Rect.X >= border);
+                int border = i == 0 ? 0 : thumbs[i - 1].Rect.Right;
+                Move(thumb, border, false, true);
+
+                Assert.IsTrue(thumb.Rect.Left - border <= moveStep);
                 Assert.IsTrue(thumb.Rect.Right > border);
+            }
+
+            //move right
+            for (int i = thumbs.Count - 1; i >= 0; i--)
+            {
+                AppiumWebElement thumb = thumbs[i];
+                int border = i == thumbs.Count - 1 ? Screen.PrimaryScreen.WorkingArea.Right : thumbs[i + 1].Rect.Left;
+                Move(thumb, border, true, true);
+
+                Assert.IsTrue(border - thumb.Rect.Right <= moveStep);
+                Assert.IsTrue(thumb.Rect.Left < border);
+            }
+
+            //move up
+            foreach (AppiumWebElement thumb in thumbs)
+            {
+                int expected = thumb.Rect.X;
+                
+                Move(thumb, 0, false, false);
+                int actual = thumb.Rect.X;
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            //move down
+            foreach (AppiumWebElement thumb in thumbs)
+            {
+                int expected = thumb.Rect.X;
+
+                Move(thumb, Screen.PrimaryScreen.WorkingArea.Right, false, false);
+                int actual = thumb.Rect.X;
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod]
+        public void MoveHorizontalDelimiter()
+        {
+            OpenCreatorWindow("Rows", "Custom table layout creator", "EditTemplateButton");
+            WindowsElement gridEditor = session.FindElementByClassName("GridEditor");
+            Assert.IsNotNull(gridEditor);
+
+            Assert.AreEqual(3, session.FindElementsByClassName("GridZone").Count);
+            ReadOnlyCollection<AppiumWebElement> thumbs = gridEditor.FindElementsByClassName("Thumb");
+            Assert.AreEqual(2, thumbs.Count);
+
+            //move up
+            for (int i = 0; i < thumbs.Count; i++)
+            {
+                AppiumWebElement thumb = thumbs[i];
+                int border = i == 0 ? 0 : thumbs[i - 1].Rect.Bottom;
+                Move(thumb, border, false, false);
+
+                Assert.IsTrue(thumb.Rect.Top - border <= moveStep);
+                Assert.IsTrue(thumb.Rect.Right > border);
+            }
+
+            //move down
+            for (int i = thumbs.Count - 1; i >= 0; i--)
+            {
+                AppiumWebElement thumb = thumbs[i];
+                int border = i == thumbs.Count - 1 ? Screen.PrimaryScreen.WorkingArea.Bottom : thumbs[i + 1].Rect.Top;
+                Move(thumb, border, true, false);
+
+                Assert.IsTrue(border - thumb.Rect.Bottom <= moveStep);
+                Assert.IsTrue(thumb.Rect.Top < border);
+            }
+
+            //move left
+            foreach (AppiumWebElement thumb in thumbs)
+            {
+                int expected = thumb.Rect.Y;
+
+                Move(thumb, 0, false, true);
+                int actual = thumb.Rect.Y;
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            //move right
+            foreach (AppiumWebElement thumb in thumbs)
+            {
+                int expected = thumb.Rect.Y;
+
+                Move(thumb, Screen.PrimaryScreen.WorkingArea.Right, true, true);
+                int actual = thumb.Rect.Y;
+
+                Assert.AreEqual(expected, actual);
             }
         }
 
