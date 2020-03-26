0c0dddab1259e8b78b233d0788db39ceed3f2e0b
Andrey Nekrasov <yuyoyuppe@users.noreply.github.com>
2020-03-26 16:31:59 +0300

    chore: fix WinRT-related warnings (#1710)

--- src/common/msi_to_msix_upgrade_lib/pch.h
+++ src/common/msi_to_msix_upgrade_lib/pch.h
@@ -3,7 +3,9 @@
 #ifndef PCH_H
 #define PCH_H
 
+#pragma warning (disable: 5205)
 #include <winrt/base.h>
+#pragma warning (default: 5205)
 #include <Windows.h>
 #include <MsiQuery.h>
 #include <Shlwapi.h>
