8b4d8c0de1cce502b0d6b21303ddc05e67f4fce5
Niels Laute <niels.laute@live.nl>
2020-03-27 16:58:53 +0100

    New settings pages (XAML) and improvements to ShellPage. Added more styles for typical margins and textblocks (#1735)

---
+++ src/core/Microsoft.PowerToys.Settings.UI/Views/ShortcutGuidePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.PowerToys.Settings.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Microsoft.PowerToys.Settings.UI.Views
+{
+    public sealed partial class ShortcutGuidePage : Page
+    {
+        public ShortcutGuideViewModel ViewModel { get; } = new ShortcutGuideViewModel();
+
+        public ShortcutGuidePage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
