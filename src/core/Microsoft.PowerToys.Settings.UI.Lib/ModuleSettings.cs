75995ac71cc83c8fb6591600567a164ce4069898
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-03-27 00:55:41 -0700

    Update src/core/Microsoft.PowerToys.Settings.UI.Lib/ModuleSettings.cs
    Co-Authored-By: Andrey Nekrasov <yuyoyuppe@users.noreply.github.com>

--- src/core/Microsoft.PowerToys.Settings.UI.Lib/ModuleSettings.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Lib/ModuleSettings.cs
@@ -7,7 +7,7 @@ namespace Microsoft.PowerToys.Settings.UI.Lib
 {
     /// <summary>
     /// PowerToys runner expects a json text that contains one of the following attributes: refresh, general and powertoys.
-    /// The one for general settings is placed in the General settings model. This class reprentes the json text that starts with the "powertoys" attribute.
+    /// The one for general settings is placed in the General settings model. This class represents the json text that starts with the "powertoys" attribute.
     /// this will tell the runner that we are sending settings for a powertoy module and not for general settings.
     /// </summary>
     /// <typeparam name="M">M stands for the Model of PT Module Settings to be sent.</typeparam>
