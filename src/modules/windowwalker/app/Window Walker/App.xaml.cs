9ede5bc1e2575c119c9964c1aa15e2d335c0c999
ivan100sic <ivan100sic@gmail.com>
2020-03-27 13:13:33 +0100

    Prevent Window Walker from having multiple running instances

--- src/modules/windowwalker/app/Window Walker/App.xaml.cs
+++ src/modules/windowwalker/app/Window Walker/App.xaml.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.  Code forked from Betsegaw Tadele's https://github.com/betsegaw/windowwalker/
 
+using System.Threading;
 using System.Windows;
 
 namespace WindowWalker
@@ -11,5 +12,18 @@ namespace WindowWalker
     /// </summary>
     public partial class App : Application
     {
+        private Mutex _appMutex;
+
+        public App()
+        {
+            // Check if there is already a running instance
+            _appMutex = new Mutex(false, "Global\\" + Guid);
+            if (!_appMutex.WaitOne(0, false))
+            {
+                Shutdown();
+            }
+        }
+
+        private static readonly string Guid = "5dedb2a2-690f-45db-9ef7-07605223a70a";
     }
 }
