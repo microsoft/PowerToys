5a13850043bb3f4647e731b492dc46421c1bf1fa
Divyansh <somm14divi@gmail.com>
2020-04-02 10:10:36 -0700

    Fixed issue with wpf marshalling events to a non UI thread

--- src/modules/launcher/PowerLauncher.UI/LauncherControl.xaml.cs
+++ src/modules/launcher/PowerLauncher.UI/LauncherControl.xaml.cs
@@ -13,25 +13,14 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
-
 namespace PowerLauncher.UI
 {
     public sealed partial class LauncherControl : UserControl
     {
-        List<SearchApp> SearchResults;
         public LauncherControl()
         {
             this.InitializeComponent();
             ShellBarShadow.Receivers.Add(ShadowReceiverGrid);
         }
-    }
-
-
-    public class SearchApp
-    {
-        public string Title { get; set; }
-        public string Icon { get; set; }
-        public string Path { get; set; }
     }
 }
