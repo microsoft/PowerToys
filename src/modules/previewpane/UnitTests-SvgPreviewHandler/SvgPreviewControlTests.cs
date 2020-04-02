945ec5daf2e19b0f60159942eb15f568e0d3fbd7
Udit Singh <udsing@microsoft.com>
2020-04-01 17:58:36 -0700

    Restore SVg preview control

--- src/modules/previewpane/UnitTests-SvgPreviewHandler/SvgPreviewControlTests.cs
+++ src/modules/previewpane/UnitTests-SvgPreviewHandler/SvgPreviewControlTests.cs
@@ -117,79 +117,81 @@ namespace UnitTests_SvgPreviewHandler
                 Assert.AreEqual(((WebBrowser)svgPreviewControl.Controls[0]).AllowNavigation, false);
             }
         }
-/*
-        [STAThread]
-        [TestMethod]
+
+        /*[TestMethod]
         public void SvgPreviewControl_ShouldAddValidInfoBar_IfSvgPreviewThrows()
         {
             // Arrange
-            var svgPreviewControl = new SvgPreviewControl();
-            var mockStream = new Mock<IStream>();
-            mockStream
-                .Setup(x => x.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<IntPtr>()))
-                .Throws(new Exception());
+            using (var svgPreviewControl = new SvgPreviewControl()) 
+            {
+                var mockStream = new Mock<IStream>();
+                mockStream
+                    .Setup(x => x.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<IntPtr>()))
+                    .Throws(new Exception());
 
-            // Act
-            svgPreviewControl.DoPreview(mockStream.Object);
-            var textBox = svgPreviewControl.Controls[0] as RichTextBox;
+                // Act
+                svgPreviewControl.DoPreview(mockStream.Object);
+                var textBox = svgPreviewControl.Controls[0] as RichTextBox;
 
-            // Assert
-            Assert.IsFalse(string.IsNullOrWhiteSpace(textBox.Text));
-            Assert.AreEqual(svgPreviewControl.Controls.Count, 1);
-            Assert.AreEqual(textBox.Dock, DockStyle.Top);
-            Assert.AreEqual(textBox.BackColor, Color.LightYellow);
-            Assert.IsTrue(textBox.Multiline);
-            Assert.IsTrue(textBox.ReadOnly);
-            Assert.AreEqual(textBox.ScrollBars, RichTextBoxScrollBars.None);
-            Assert.AreEqual(textBox.BorderStyle, BorderStyle.None);
-        }
+                // Assert
+                Assert.IsFalse(string.IsNullOrWhiteSpace(textBox.Text));
+                Assert.AreEqual(svgPreviewControl.Controls.Count, 1);
+                Assert.AreEqual(textBox.Dock, DockStyle.Top);
+                Assert.AreEqual(textBox.BackColor, Color.LightYellow);
+                Assert.IsTrue(textBox.Multiline);
+                Assert.IsTrue(textBox.ReadOnly);
+                Assert.AreEqual(textBox.ScrollBars, RichTextBoxScrollBars.None);
+                Assert.AreEqual(textBox.BorderStyle, BorderStyle.None);
+            }
+        }*/
 
-        [STAThread]
-        [TestMethod]
+        /*[TestMethod]
         public void SvgPreviewControl_InfoBarWidthShouldAdjustWithParentControlWidthChanges_IfSvgPreviewThrows()
         {
             // Arrange
-            var svgPreviewControl = new SvgPreviewControl();
-            var mockStream = new Mock<IStream>();
-            mockStream
-                .Setup(x => x.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<IntPtr>()))
-                .Throws(new Exception());
-            svgPreviewControl.DoPreview(mockStream.Object);
-            var textBox = svgPreviewControl.Controls[0] as RichTextBox;
-            var incrementParentControlWidth = 5;
-            var intialParentWidth = svgPreviewControl.Width;
-            var intitialTextBoxWidth = textBox.Width;
-            var finalParentWidth = intialParentWidth + incrementParentControlWidth;
+            using (var svgPreviewControl = new SvgPreviewControl()) 
+            {
+                var mockStream = new Mock<IStream>();
+                mockStream
+                    .Setup(x => x.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<IntPtr>()))
+                    .Throws(new Exception());
+                svgPreviewControl.DoPreview(mockStream.Object);
+                var textBox = svgPreviewControl.Controls[0] as RichTextBox;
+                var incrementParentControlWidth = 5;
+                var intialParentWidth = svgPreviewControl.Width;
+                var intitialTextBoxWidth = textBox.Width;
+                var finalParentWidth = intialParentWidth + incrementParentControlWidth;
 
-            // Act
-            svgPreviewControl.Width += incrementParentControlWidth;
+                // Act
+                svgPreviewControl.Width += incrementParentControlWidth;
 
-            // Assert
-            Assert.AreEqual(intialParentWidth, intitialTextBoxWidth);
-            Assert.AreEqual(finalParentWidth, textBox.Width);
+                // Assert
+                Assert.AreEqual(intialParentWidth, intitialTextBoxWidth);
+                Assert.AreEqual(finalParentWidth, textBox.Width);
+            }
         }*/
 
-        /*[STAThread]
         [TestMethod]
         public void SvgPreviewControl_ShouldAddTextBox_IfBlockedElementsArePresent()
         {
             // Arrange
-            var svgPreviewControl = new SvgPreviewControl();
-            var svgBuilder = new StringBuilder();
-            svgBuilder.AppendLine("<svg width =\"200\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
-            svgBuilder.AppendLine("\t<script>alert(\"hello\")</script>");
-            svgBuilder.AppendLine("</svg>");
+            using (var svgPreviewControl = new SvgPreviewControl()) 
+            {
+                var svgBuilder = new StringBuilder();
+                svgBuilder.AppendLine("<svg width =\"200\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
+                svgBuilder.AppendLine("\t<script>alert(\"hello\")</script>");
+                svgBuilder.AppendLine("</svg>");
 
-            // Act
-            svgPreviewControl.DoPreview(GetMockStream(svgBuilder.ToString()));
+                // Act
+                svgPreviewControl.DoPreview(GetMockStream(svgBuilder.ToString()));
 
-            // Assert
-            Assert.IsInstanceOfType(svgPreviewControl.Controls[0], typeof(RichTextBox));
-            Assert.IsInstanceOfType(svgPreviewControl.Controls[1], typeof(WebBrowserExt));
-            Assert.AreEqual(svgPreviewControl.Controls.Count, 2);
-        }*/
+                // Assert
+                Assert.IsInstanceOfType(svgPreviewControl.Controls[0], typeof(RichTextBox));
+                Assert.IsInstanceOfType(svgPreviewControl.Controls[1], typeof(WebBrowserExt));
+                Assert.AreEqual(svgPreviewControl.Controls.Count, 2);
+            }
+        }
 
-        [STAThread]
         [TestMethod]
         public void SvgPreviewControl_ShouldNotAddTextBox_IfNoBlockedElementsArePresent()
         {
@@ -211,30 +213,31 @@ namespace UnitTests_SvgPreviewHandler
             }
         }
 
-        /*[STAThread]
         [TestMethod]
         public void SvgPreviewControl_InfoBarWidthShouldAdjustWithParentControlWidthChanges_IfBlockedElementsArePresent()
         {
             // Arrange
-            var svgPreviewControl = new SvgPreviewControl();
-            var svgBuilder = new StringBuilder();
-            svgBuilder.AppendLine("<svg width =\"200\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
-            svgBuilder.AppendLine("\t<script>alert(\"hello\")</script>");
-            svgBuilder.AppendLine("</svg>");
-            svgPreviewControl.DoPreview(GetMockStream(svgBuilder.ToString()));
-            var textBox = svgPreviewControl.Controls[0] as RichTextBox;
-            var incrementParentControlWidth = 5;
-            var intialParentWidth = svgPreviewControl.Width;
-            var intitialTextBoxWidth = textBox.Width;
-            var finalParentWidth = intialParentWidth + incrementParentControlWidth;
+            using (var svgPreviewControl = new SvgPreviewControl())
+            {
+                var svgBuilder = new StringBuilder();
+                svgBuilder.AppendLine("<svg width =\"200\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
+                svgBuilder.AppendLine("\t<script>alert(\"hello\")</script>");
+                svgBuilder.AppendLine("</svg>");
+                svgPreviewControl.DoPreview(GetMockStream(svgBuilder.ToString()));
+                var textBox = svgPreviewControl.Controls[0] as RichTextBox;
+                var incrementParentControlWidth = 5;
+                var intialParentWidth = svgPreviewControl.Width;
+                var intitialTextBoxWidth = textBox.Width;
+                var finalParentWidth = intialParentWidth + incrementParentControlWidth;
 
-            // Act
-            svgPreviewControl.Width += incrementParentControlWidth;
+                // Act
+                svgPreviewControl.Width += incrementParentControlWidth;
 
-            // Assert
-            Assert.AreEqual(intialParentWidth, intitialTextBoxWidth);
-            Assert.AreEqual(finalParentWidth, textBox.Width);
-        }*/
+                // Assert
+                Assert.AreEqual(intialParentWidth, intitialTextBoxWidth);
+                Assert.AreEqual(finalParentWidth, textBox.Width);
+            }
+        }
 
         private IStream GetMockStream(string streamData)
         {
