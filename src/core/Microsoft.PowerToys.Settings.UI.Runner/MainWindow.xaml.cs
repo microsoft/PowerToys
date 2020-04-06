30c790ff61820986e7e15cddbd2058e735191e21
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-04-06 16:26:57 -0700

    added two way IPC

---
+++ src/core/Microsoft.PowerToys.Settings.UI.Runner/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Windows;
+using Microsoft.PowerToys.Settings.UI.Views;
+using Microsoft.Toolkit.Wpf.UI.XamlHost;
+
+namespace Microsoft.PowerToys.Settings.UI.Runner
+{
+    // Interaction logic for MainWindow.xaml.
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void WindowsXamlHost_ChildChanged(object sender, EventArgs e)
+        {
+            // Hook up x:Bind source.
+            WindowsXamlHost windowsXamlHost = sender as WindowsXamlHost;
+            ShellPage shellPage = windowsXamlHost.GetUwpInternalObject() as ShellPage;
+
+            if (shellPage != null)
+            {
+                // send IPC Message
+                shellPage.SetDefaultSndMessageCallback(msg =>
+                {
+                    Program.GetTwoWayIPCManager().SendMessage(msg);
+                });
+            }
+        }
+    }
+}
---
+++ src/core/Microsoft.PowerToys.Settings.UI.Runner/Program.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Windows;
+using TwoWayIPCLibLib;
+
+namespace Microsoft.PowerToys.Settings.UI.Runner
+{
+    public class Program
+    {
+        // Create an instance of the  IPC wrapper.
+        private static ITwoWayIPCManager ipcmanager = new TwoWayIPCManager();
+
+        [STAThreadAttribute()]
+        public static void Main(string[] args)
+        {
+            using (new UI.App())
+            {
+                App app = new App();
+                app.InitializeComponent();
+
+                if (args.Length > 1)
+                {
+                    ipcmanager.Initialize(args[1], args[0]);
+                    app.Run();
+                }
+                else
+                {
+                    MessageBox.Show(
+                        "The application cannot be run as a standalone process. Please start the application through the runner.",
+                        "Forbidden",
+                        MessageBoxButton.OK);
+                    app.Shutdown();
+                }
+            }
+        }
+
+        public static ITwoWayIPCManager GetTwoWayIPCManager()
+        {
+            return ipcmanager;
+        }
+    }
+}
