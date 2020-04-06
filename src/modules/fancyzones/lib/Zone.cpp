545a227de2f85ee34ffa3124c24cb99c6f023b52
Serafima <zykovas91@gmail.com>
2020-04-06 13:07:40 +0300

    wait loop fix

--- src/modules/fancyzones/lib/Zone.cpp
+++ src/modules/fancyzones/lib/Zone.cpp
@@ -106,16 +106,18 @@ void Zone::SizeWindowToZone(HWND window, HWND zoneWindow) noexcept
     }
 
     WINDOWPLACEMENT placement{};
-    
+    ::GetWindowPlacement(window, &placement);
+
+    //wait if SW_SHOWMINIMIZED would be removed from window (Issue #1685)    
+    for (int i = 0; i < 5 && (placement.showCmd & SW_SHOWMINIMIZED) != 0; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        ::GetWindowPlacement(window, &placement);
+    }
+
     // Do not restore minimized windows. We change their placement though so they restore to the correct zone.
     if ((placement.showCmd & SW_SHOWMINIMIZED) == 0)
     {
-        //wait if SW_SHOWMINIMIZED would be removed from window (Issue #1685)
-        for (int i = 0; i < 5; i++)
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            ::GetWindowPlacement(window, &placement);
-        }
         placement.showCmd = SW_RESTORE | SW_SHOWNA;
     }
 
