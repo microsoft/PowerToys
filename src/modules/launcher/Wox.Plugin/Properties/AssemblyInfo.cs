e004f96b655af1f38e363b053db9afc7a87dd001
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-04-07 14:41:33 -0700

    Making Query internals visible to wox.

--- src/modules/launcher/Wox.Plugin/Properties/AssemblyInfo.cs
+++ src/modules/launcher/Wox.Plugin/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("PowerLauncher")]
+[assembly: InternalsVisibleTo("Wox")]
 [assembly: InternalsVisibleTo("Wox.Core")]
 [assembly: InternalsVisibleTo("Wox.Test")]
