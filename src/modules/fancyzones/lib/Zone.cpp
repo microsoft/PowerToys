69e58de7e0b59aa6f5fd19dbe879a1f518ae7fbd
PrzemyslawTusinski <61138537+PrzemyslawTusinski@users.noreply.github.com>
2020-04-08 18:12:18 +0200

    minor merge fix

--- src/modules/fancyzones/lib/Zone.cpp
+++ src/modules/fancyzones/lib/Zone.cpp
@@ -121,13 +121,14 @@ void Zone::SizeWindowToZone(HWND window, HWND zoneWindow) noexcept
         ::GetWindowPlacement(window, &placement);
     }
 
-    placement.rcNormalPosition = newWindowRect;
-    placement.flags |= WPF_ASYNCWINDOWPLACEMENT;
     // Do not restore minimized windows. We change their placement though so they restore to the correct zone.
     if ((placement.showCmd & SW_SHOWMINIMIZED) == 0)
     {
         placement.showCmd = SW_RESTORE | SW_SHOWNA;
     }
+
+    placement.rcNormalPosition = newWindowRect;
+    placement.flags |= WPF_ASYNCWINDOWPLACEMENT;
 
     ::SetWindowPlacement(window, &placement);
     // Do it again, allowing Windows to resize the window and set correct scaling
