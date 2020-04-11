e64d295215ac415d9540eec0b43fd9f333e8b9ce
Betsegaw Tadele <betsegaw.ta@gmail.com>
2020-04-11 12:46:38 -0700

    Added the actual code which apparently doesn't work

--- src/modules/launcher/Plugins/Wox.Plugin.WindowWalker/Main.cs
+++ src/modules/launcher/Plugins/Wox.Plugin.WindowWalker/Main.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Wox.Plugin.WindowWalker.Components;
 
 namespace Wox.Plugin.WindowWalker
 {
     public class Main : IPlugin, IPluginI18n
     {
+        private static List<SearchResult> _results = new List<SearchResult>();
+        private static string IcoPath = "Images/windowwalker.png";
         private PluginInitContext Context { get; set; }
 
         static Main()
         {
-            
+            SearchController.Instance.OnSearchResultUpdate += SearchResultUpdated;
+            OpenWindows.Instance.UpdateOpenWindowsList();
         }
 
         public List<Result> Query(Query query)
         {
-            return new List<Result>{ new Result()
+            return _results.Select(x => new Result()
             {
-                Title = "Yo!",
-                IcoPath = "Images/windowwalker.png",
-            } };
+                Title = x.Result.Title,
+                IcoPath = IcoPath,
+                SubTitle = x.Result.ProcessName
+            }
+            ).ToList();
         }
 
         public void Init(PluginInitContext context)
@@ -33,6 +40,11 @@ namespace Wox.Plugin.WindowWalker
         public string GetTranslatedPluginDescription()
         {
             return Context.API.GetTranslation("wox_plugin_caculator_plugin_description");
+        }
+
+        private static void SearchResultUpdated(object sender, SearchController.SearchResultUpdateEventArgs e)
+        {
+            _results = SearchController.Instance.SearchMatches;
         }
     }
 }
