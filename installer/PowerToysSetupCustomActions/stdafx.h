910cc6b31ad677aef1fd998df13bda9c64ed8e88
yuyoyuppe <a.yuyoyuppe@gmail.com>
2020-04-13 18:06:57 +0300

    msi: detect and set previous installation path via a custom action

--- installer/PowerToysSetupCustomActions/stdafx.h
+++ installer/PowerToysSetupCustomActions/stdafx.h
@@ -1,13 +1,23 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
 #include <strsafe.h>
 #include <msiquery.h>
+#include <Msi.h>
 
 // WiX Header Files:
 #include <wcautil.h>
 
+#define SECURITY_WIN32
+#include <Security.h>
+#include <Lmcons.h>
 
-// TODO: reference additional headers your program requires here
+#include <comdef.h>
+#include <taskschd.h>
+#include <iostream>
+#include <strutil.h>
+#include <string>
+#include <optional>
+#include <pathcch.h>
