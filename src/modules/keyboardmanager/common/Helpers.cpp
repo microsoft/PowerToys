13a8ac3e50737b5f779bac6251fe76c16370801d
Arjun Balgovind <32061677+arjunbalgovind@users.noreply.github.com>
2020-04-16 15:17:57 -0700

    Handle RAlt, RCtrl and other extended keys correctly (dev/build-features) (#2145)
    * Fixed SendInput for RAlt and RCtrl
    
    * Fixed shortcuts containing Del, Arrow keys, etc

--- src/modules/keyboardmanager/common/Helpers.cpp
+++ src/modules/keyboardmanager/common/Helpers.cpp
@@ -26,3 +26,19 @@ IInspectable getSiblingElement(IInspectable const& element)
     parentElement.Children().IndexOf(frameworkElement, index);
     return parentElement.Children().GetAt(index + 1);
 }
+
+// Function to return if the key is an extended key which requires the use of the extended key flag
+bool isExtendedKey(DWORD key)
+{
+    switch (key)
+    {
+    case VK_RCONTROL:
+    case VK_RMENU:
+    case VK_NUMLOCK:
+    case VK_SNAPSHOT:
+    case VK_CANCEL:
+        return true;
+    default:
+        return false;
+    }
+}
