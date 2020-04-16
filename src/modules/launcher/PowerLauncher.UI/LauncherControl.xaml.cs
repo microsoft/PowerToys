d64e14047091a762f0716b44187ad3c351697404
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-04-15 21:36:31 -0700

    Hooking up execution on the selected index, removing two way binding on selection, and experimenting with popup that doesn't work

--- src/modules/launcher/PowerLauncher.UI/LauncherControl.xaml.cs
+++ src/modules/launcher/PowerLauncher.UI/LauncherControl.xaml.cs
@@ -4,15 +4,19 @@ namespace PowerLauncher.UI
 {
     public sealed partial class LauncherControl : UserControl
     {
-        //private Point _lastpos;
-        private ListBoxItem curItem = null;
+        public ListView ResultsList => SuggestionsList;
 
         public LauncherControl()
         {
             InitializeComponent();
         }
 
-        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void TextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+           // SuggestionsPopup.IsOpen = true;
+        }
+
+        private void SuggestionsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (e.AddedItems.Count > 0 && e.AddedItems[0] != null)
             {
