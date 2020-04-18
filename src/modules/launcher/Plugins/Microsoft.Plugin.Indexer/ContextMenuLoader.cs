84175d4a2b2ec27daadd26392e81443fd0687854
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-04-18 14:48:15 -0700

    Adding context buttons to Indexer plugin.  This allows for the following: 1. [Files] Open Containing folder 2. [Folders/Files] Copy Path

---
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Indexer/ContextMenuLoader.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using Wox.Infrastructure.Logger;
+using Wox.Plugin;
+using Microsoft.Plugin.Indexer.SearchHelper;
+
+namespace Microsoft.Plugin.Indexer
+{
+    internal class ContextMenuLoader : IContextMenu
+    {
+        private readonly PluginInitContext _context;
+
+        public enum ResultType
+        {
+            Folder,
+            File
+        }
+
+        public ContextMenuLoader(PluginInitContext context)
+        {
+            _context = context;
+        }
+
+        public List<Result> LoadContextMenus(Result selectedResult)
+        {
+            var contextMenus = new List<Result>();
+            if (selectedResult.ContextData is SearchResult record)
+            {
+                ResultType type = Path.HasExtension(record.Path) ? ResultType.File : ResultType.Folder;
+
+                if (type == ResultType.File)
+                {
+                    contextMenus.Add(CreateOpenContainingFolderResult(record));
+                }
+
+                var fileOrFolder = (type == ResultType.File) ? "file" : "folder";
+                contextMenus.Add(new Result
+                {
+                    Title = "Copy path",
+                    Glyph = "\xE8C8",
+                    FontFamily = "Segoe MDL2 Assets",
+                    SubTitle = $"Copy the current {fileOrFolder} path to clipboard",
+                    Action = (context) =>
+                    {
+                        try
+                        {
+                            Clipboard.SetText(record.Path);
+                            return true;
+                        }
+                        catch (Exception e)
+                        {
+                            var message = "Fail to set text in clipboard";
+                            LogException(message, e);
+                            _context.API.ShowMsg(message);
+                            return false;
+                        }
+                    }
+                });
+            }
+
+            return contextMenus;
+        }
+
+        private Result CreateOpenContainingFolderResult(SearchResult record)
+        {
+            return new Result
+            {
+                Title = "Open containing folder",
+                Glyph = "\xE838",
+                FontFamily = "Segoe MDL2 Assets",
+                Action = _ =>
+                {
+                    try
+                    {
+                        Process.Start("explorer.exe", $" /select,\"{record.Path}\"");
+                    }
+                    catch(Exception e)
+                    {
+                        var message = $"Fail to open file at {record.Path}";
+                        LogException(message, e);
+                        _context.API.ShowMsg(message);
+                        return false;
+                    }
+
+                    return true;
+                },
+            };
+        }
+
+        public void LogException(string message, Exception e)
+        {
+            Log.Exception($"|Wox.Plugin.Folder.ContextMenu|{message}", e);
+        }
+    }
+
+}
