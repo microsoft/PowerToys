92db5902895ca0e87896dde5b6e805d8ab69f776
Tomas Raies <tomas.raies@gmail.com>
2020-04-20 13:19:55 -0700

    Fix HotkeyControl virtual key display
    * A new interop project was setup to provide wrappers for C# projects
      that want to access functionality in the common project.

---
+++ src/common/interop/AssemblyInfo.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"interop")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"")];
+[assembly:AssemblyProductAttribute(L"interop")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2020")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute("1.0.*")];
+
+[assembly:ComVisible(false)];
+
+[assembly:CLSCompliantAttribute(true)];
