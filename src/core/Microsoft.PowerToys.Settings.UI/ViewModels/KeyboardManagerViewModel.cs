6a460371b1ed474bc2f8592ad1499a18ccd104f1
Udit Singh <udsing@microsoft.com>
2020-04-21 08:53:28 -0700

    Move changes

--- src/core/Microsoft.PowerToys.Settings.UI/ViewModels/KeyboardManagerViewModel.cs
+++ src/core/Microsoft.PowerToys.Settings.UI/ViewModels/KeyboardManagerViewModel.cs
@@ -3,7 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -11,6 +13,9 @@ using Microsoft.PowerToys.Settings.UI.Helpers;
 using Microsoft.PowerToys.Settings.UI.Lib;
 using Microsoft.PowerToys.Settings.UI.Lib.Utilities;
 using Microsoft.PowerToys.Settings.UI.Views;
+using Microsoft.Toolkit.Uwp.Helpers;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
 
 namespace Microsoft.PowerToys.Settings.UI.ViewModels
 {
@@ -25,21 +30,25 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
         private const string ConfigFileMutexName = "PowerToys.KeyboardManager.ConfigMutex";
         private const int ConfigFileMutexWaitTimeoutMiliSeconds = 1000;
 
+        private readonly CoreDispatcher dispatcher;
+
         private ICommand remapKeyboardCommand;
         private ICommand editShortcutCommand;
         private FileSystemWatcher watcher;
         private KeyboardManagerSettings settings;
-
-        public ICommand RemapKeyboardCommand => remapKeyboardCommand ?? (remapKeyboardCommand = new RelayCommand(OnRemapKeyboard));
-
-        public ICommand EditShortcutCommand => editShortcutCommand ?? (editShortcutCommand = new RelayCommand(OnEditShortcut));
+        private KeyboadManagerConfigModel config;
+        private List<KeysUiDataModels> remapKeysList = new List<KeysUiDataModels>();
 
         public KeyboardManagerViewModel()
         {
+            dispatcher = Window.Current.Dispatcher;
             if (SettingsUtils.SettingsExists(PowerToyName))
             {
                 // Todo: Be more resillent while reading and saving settings.
                 settings = SettingsUtils.GetSettings<KeyboardManagerSettings>(PowerToyName);
+
+                // Load previous configuration.
+                LoadConfiguration();
             }
             else
             {
@@ -49,6 +58,45 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
 
             watcher = Helper.GetFileWatcher(PowerToyName, settings.Properties.ActiveConfiguration.Value + JsonFileType, OnConfigFileUpdate);
         }
+
+        // store remappings
+        public List<KeysUiDataModels> RemapKeysList
+        {
+            get
+            {
+                return remapKeysList;
+            }
+
+            set
+            {
+                if (remapKeysList != value)
+                {
+                    remapKeysList = value;
+                    OnPropertyChanged(nameof(RemapKeysList));
+                }
+            }
+        }
+
+        public List<KeysUiDataModels> RemapShortcutsList
+        {
+            get
+            {
+                return RemapShortcutsList;
+            }
+
+            set
+            {
+                if (RemapShortcutsList != value)
+                {
+                    RemapShortcutsList = value;
+                    OnPropertyChanged(nameof(RemapShortcutsList));
+                }
+            }
+        }
+
+        public ICommand RemapKeyboardCommand => remapKeyboardCommand ?? (remapKeyboardCommand = new RelayCommand(OnRemapKeyboard));
+
+        public ICommand EditShortcutCommand => editShortcutCommand ?? (editShortcutCommand = new RelayCommand(OnEditShortcut));
 
         private async void OnRemapKeyboard()
         {
@@ -78,11 +126,28 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
         {
             // Note: FileSystemWatcher raise notification mutiple times for single update operation.
             // Todo: Handle duplicate events either by somehow supress them or re-read the configuration everytime since we will be updating the UI only if something is changed.
-            GetKeyboardManagerConfigFile();
+            LoadConfiguration();
         }
 
-        private void GetKeyboardManagerConfigFile()
+        private void LoadConfiguration()
         {
+            if (TryGetKeyboardManagerConfigFile())
+            {
+                var data = config?.RemapKeys?.InProcessRemapKeys
+                    ?.Select(keysDataModel => new KeysUiDataModels
+                    {
+                        RemapFromKeys = new List<string> { keysDataModel.OriginalKeys },
+                        RemapToKeys = new List<string> { keysDataModel.NewRemapKeys },
+                    }).ToList() ?? new List<KeysUiDataModels>();
+
+                var notUsed = dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => { RemapKeysList = data; });
+            }
+        }
+
+        private bool TryGetKeyboardManagerConfigFile()
+        {
+            var sucess = true;
+
             try
             {
                 using (var configFileMutex = Mutex.OpenExisting(ConfigFileMutexName))
@@ -92,7 +157,7 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                         // update the UI element here.
                         try
                         {
-                            var config = SettingsUtils.GetSettings<KeyboadManagerConfigModel>(PowerToyName, settings.Properties.ActiveConfiguration.Value + JsonFileType);
+                            config = SettingsUtils.GetSettings<KeyboadManagerConfigModel>(PowerToyName, settings.Properties.ActiveConfiguration.Value + JsonFileType);
                         }
                         finally
                         {
@@ -100,12 +165,19 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                             configFileMutex.ReleaseMutex();
                         }
                     }
+                    else
+                    {
+                        sucess = false;
+                    }
                 }
             }
             catch (Exception)
             {
                 // Failed to load the configuration.
+                sucess = false;
             }
+
+            return sucess;
         }
     }
 }
---
+++ src/core/Microsoft.PowerToys.Settings.UI/ViewModels/KeysUiDataModels.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.PowerToys.Settings.UI.ViewModels
+{
+    public class KeysUiDataModels
+    {
+        public List<string> RemapFromKeys { get; set; }
+
+        public List<string> RemapToKeys { get; set; }
+    }
+}
