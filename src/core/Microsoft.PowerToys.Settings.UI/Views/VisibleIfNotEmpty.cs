d941b31c455d9ba2c61b8c8d7e76a90a69f6ce92
Tomas Agustin Raies <tomas.raies@gmail.com>
2020-04-22 14:55:45 -0700

    Display Key Mappings in Settings (#2314)
    * Move changes
    
    * Display Key Mapping in Settings
    
    * Fix display of empty keys list
    
    Co-authored-by: Udit Singh <udsing@microsoft.com>

---
+++ src/core/Microsoft.PowerToys.Settings.UI/Views/VisibleIfNotEmpty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Microsoft.PowerToys.Settings.UI.Views
+{
+    public class VisibleIfNotEmpty : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value as IList).Count == 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
