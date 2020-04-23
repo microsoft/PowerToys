3653545b43f5475c96dee4560ef9ae2a01a33d17
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-04-23 12:46:08 -0700

    Revert "Removed folder plugin"
    This reverts commit 064d638588cfb2d2d69cd1f88b92127c200eac2d.
    
    We will use the folder plugin to better mimic the way the start menu and run prompt deal with directories

---
+++ src/modules/launcher/Plugins/Wox.Plugin.Folder/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Wox.Infrastructure.Storage;
+
+namespace Wox.Plugin.Folder
+{
+    public class Settings
+    {
+        [JsonProperty]
+        public List<FolderLink> FolderLinks { get; set; } = new List<FolderLink>();
+    }
+}
