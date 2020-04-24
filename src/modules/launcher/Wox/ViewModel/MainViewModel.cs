a6e8cbc50d51d45b31fd0934e8c6c560b8915176
Divyansh Srivastava <somm14divi@gmail.com>
2020-04-24 13:31:36 -0700

    Updated program execution to call action on background thread. (#2370)

--- src/modules/launcher/Wox/ViewModel/MainViewModel.cs
+++ src/modules/launcher/Wox/ViewModel/MainViewModel.cs
@@ -160,17 +160,17 @@ namespace Wox.ViewModel
                     if (!didExecuteContextButton)
                     {
                         var result = results.SelectedItem.Result;
-                        if (result != null) // SelectedItem returns null if selection is empty.
+                        if (result != null && result.Action != null) // SelectedItem returns null if selection is empty.
                         {
-                            bool hideWindow = result.Action != null && result.Action(new ActionContext
-                            {
-                                SpecialKeyState = GlobalHotkey.Instance.CheckModifiers()
-                            });
+                            MainWindowVisibility = Visibility.Collapsed;
 
-                            if (hideWindow)
+                            Task.Run(() =>
                             {
-                                MainWindowVisibility = Visibility.Collapsed;
-                            }
+                                result.Action(new ActionContext
+                                {
+                                    SpecialKeyState = GlobalHotkey.Instance.CheckModifiers()
+                                });
+                            });
 
                             if (SelectedIsFromQueryResults())
                             {
