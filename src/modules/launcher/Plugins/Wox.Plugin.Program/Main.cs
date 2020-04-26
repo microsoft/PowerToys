6062c632c9b077ef650ee538d55b55fb44fe400a
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-04-26 07:41:15 -0700

    Removing unused namespace.

--- src/modules/launcher/Plugins/Wox.Plugin.Program/Main.cs
+++ src/modules/launcher/Plugins/Wox.Plugin.Program/Main.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Security.Permissions;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows.Controls;
