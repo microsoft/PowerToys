f8af479eb3c0a5608632d3544ae10e45b352fc85
Ebenezer Ewumi <ebenezer.ewumi@wsu.edu>
2020-04-30 10:08:05 -0700

    Update PowerRenameItem.h

--- src/modules/powerrename/lib/PowerRenameItem.h
+++ src/modules/powerrename/lib/PowerRenameItem.h
@@ -3,41 +3,39 @@
 #include "PowerRenameInterfaces.h"
 #include "srwlock.h"
 
-class CPowerRenameItem :
-    public IPowerRenameItem,
-    public IPowerRenameItemFactory
+class CPowerRenameItem : public IPowerRenameItem, public IPowerRenameItemFactory
 {
 public:
     // IUnknown
-    IFACEMETHODIMP  QueryInterface(_In_ REFIID iid, _Outptr_ void** resultInterface);
-    IFACEMETHODIMP_(ULONG) AddRef();
-    IFACEMETHODIMP_(ULONG) Release();
+    IFACEMETHODIMP  QueryInterface(_In_ REFIID iid, _Outptr_ void** resultInterface) override;
+    IFACEMETHODIMP_(ULONG) AddRef() override;
+    IFACEMETHODIMP_(ULONG) Release() override;
 
     // IPowerRenameItem
-    IFACEMETHODIMP get_path(_Outptr_ PWSTR* path);
-    IFACEMETHODIMP get_shellItem(_Outptr_ IShellItem** ppsi);
-    IFACEMETHODIMP get_originalName(_Outptr_ PWSTR* originalName);
-    IFACEMETHODIMP put_newName(_In_opt_ PCWSTR newName);
-    IFACEMETHODIMP get_newName(_Outptr_ PWSTR* newName);
-    IFACEMETHODIMP get_isFolder(_Out_ bool* isFolder);
-    IFACEMETHODIMP get_isSubFolderContent(_Out_ bool* isSubFolderContent);
-    IFACEMETHODIMP get_selected(_Out_ bool* selected);
-    IFACEMETHODIMP put_selected(_In_ bool selected);
-    IFACEMETHODIMP get_id(_Out_ int* id);
-    IFACEMETHODIMP get_iconIndex(_Out_ int* iconIndex);
-    IFACEMETHODIMP get_depth(_Out_ UINT* depth);
-    IFACEMETHODIMP put_depth(_In_ int depth);
-    IFACEMETHODIMP Reset();
-    IFACEMETHODIMP ShouldRenameItem(_In_ DWORD flags, _Out_ bool* shouldRename);
+    IFACEMETHODIMP get_path(_Outptr_ PWSTR* path) override;
+    IFACEMETHODIMP get_shellItem(_Outptr_ IShellItem** ppsi) override;
+    IFACEMETHODIMP get_originalName(_Outptr_ PWSTR* originalName) override;
+    IFACEMETHODIMP put_newName(_In_opt_ PCWSTR newName) override;
+    IFACEMETHODIMP get_newName(_Outptr_ PWSTR* newName) override;
+    IFACEMETHODIMP get_isFolder(_Out_ bool* isFolder) override;
+    IFACEMETHODIMP get_isSubFolderContent(_Out_ bool* isSubFolderContent) override;
+    IFACEMETHODIMP get_selected(_Out_ bool* selected) override;
+    IFACEMETHODIMP put_selected(_In_ bool selected) override;
+    IFACEMETHODIMP get_id(_Out_ int* id) override;
+    IFACEMETHODIMP get_iconIndex(_Out_ int* iconIndex) override;
+    IFACEMETHODIMP get_depth(_Out_ UINT* depth) override;
+    IFACEMETHODIMP put_depth(_In_ int depth) override;
+    IFACEMETHODIMP Reset() override;
+    IFACEMETHODIMP ShouldRenameItem(_In_ DWORD flags, _Out_ bool* shouldRename) override;
 
     // IPowerRenameItemFactory
-    IFACEMETHODIMP Create(_In_ IShellItem* psi, _Outptr_ IPowerRenameItem** ppItem)
+    IFACEMETHODIMP Create(_In_ IShellItem* psi, _Outptr_ IPowerRenameItem** ppItem) override
     {
         return CPowerRenameItem::s_CreateInstance(psi, IID_PPV_ARGS(ppItem));
     }
 
-public:
-    static HRESULT s_CreateInstance(_In_opt_ IShellItem* psi, _In_ REFIID iid, _Outptr_ void** resultInterface);
+
+  static HRESULT s_CreateInstance(_In_opt_ IShellItem* psi, _In_ REFIID iid, _Outptr_ void** resultInterface);
 
 protected:
     static int s_id;
