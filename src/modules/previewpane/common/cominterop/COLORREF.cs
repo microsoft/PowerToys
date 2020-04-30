2be078c26aa8e1e4d4dd11e7acd801d31173504d
Ebenezer Ewumi <ebenezer.ewumi@wsu.edu>
2020-04-30 10:07:25 -0700

    Update COLORREF.cs

--- src/modules/previewpane/common/cominterop/COLORREF.cs
+++ src/modules/previewpane/common/cominterop/COLORREF.cs
@@ -21,15 +21,10 @@ namespace Common.ComInterlop
         /// <summary>
         /// Gets RGB value stored in <see cref="Dword"/> in <see cref="Color"/> structure.
         /// </summary>
-        public Color Color
-        {
-            get
-            {
-                return Color.FromArgb(
-                    (int)(0x000000FFU & this.Dword),
-                    (int)(0x0000FF00U & this.Dword) >> 8,
-                    (int)(0x00FF0000U & this.Dword) >> 16);
-            }
-        }
+        public Color Color =>
+            Color.FromArgb(
+                (int)(0x000000FFU & this.Dword),
+                (int)(0x0000FF00U & this.Dword) >> 8,
+                (int)(0x00FF0000U & this.Dword) >> 16);
     }
 }
