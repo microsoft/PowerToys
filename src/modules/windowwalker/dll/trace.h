bd703bb8c81ec299802108f903d5ad04b2c12d89
Divyansh Srivastava <somm14divi@gmail.com>
2020-05-01 10:25:00 -0700

    Removed WW module from sln file

--- src/modules/windowwalker/dll/trace.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Trace
-{
-public:
-    static void RegisterProvider();
-    static void UnregisterProvider();
-    static void EnableWindowWalker(const bool enabled) noexcept;
-};
