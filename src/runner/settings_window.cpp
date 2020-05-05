98feafed845e6f6e6ade2d1c9b5ecd1797ee3c28
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-05-05 13:32:16 -0700

    removed unused string

--- src/runner/settings_window.cpp
+++ src/runner/settings_window.cpp
@@ -338,14 +338,15 @@ void bring_settings_to_front()
         DWORD processId;
         if (GetWindowThreadProcessId(hwnd, &processId) && processId == g_settings_process_id)
         {
-            WINDOWPLACEMENT wndPlacement;
+            std::wstring windowTitle = L"PowerToys Settings";
+
             WCHAR title[MAX_TITLE_LENGTH];
             int len = GetWindowTextW(hwnd, title, MAX_TITLE_LENGTH);
             if (len <= 0)
             {
                 return TRUE;
             }
-            if (wcsncmp(title, L"PowerToys Settings", len) == 0)
+            if (wcsncmp(title, windowTitle.c_str(), len) == 0)
             {
                 ShowWindow(hwnd, SW_RESTORE);
                 SetForegroundWindow(hwnd);
