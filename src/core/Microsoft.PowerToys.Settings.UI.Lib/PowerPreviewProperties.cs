0461e7c8abd763475047ef94547b6b224d2d7389
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-05-05 19:45:43 -0700

    Raising Telemetry events when svg and markdown preview pane is turned off

--- src/core/Microsoft.PowerToys.Settings.UI.Lib/PowerPreviewProperties.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Lib/PowerPreviewProperties.cs
@@ -2,6 +2,9 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.PowerToys.Settings.Telemetry;
+using Microsoft.PowerToys.Telemetry;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -9,21 +12,58 @@ namespace Microsoft.PowerToys.Settings.UI.Lib
 {
     public class PowerPreviewProperties
     {
+
         [JsonPropertyName("svg-previewer-toggle-setting")]
-        public BoolProperty EnableSvg { get; set; }
+        private BoolProperty EnableSvgInner { get; set; }
+
+        public bool EnableSvg
+        {
+            get => this.EnableSvgInner.Value;
+            set
+            {
+                if (value != this.EnableSvgInner.Value)
+                {
+                    LogTelemetryEvent(value);
+                    this.EnableSvgInner.Value = value;
+                }
+            }
+        }
 
         [JsonPropertyName("md-previewer-toggle-setting")]
-        public BoolProperty EnableMd { get; set; }
+        private BoolProperty EnableMdInner { get; set; }
+
+        public bool EnableMd
+        {
+            get => this.EnableMdInner.Value;
+            set
+            {
+                if (value != this.EnableMdInner.Value)
+                {
+                    LogTelemetryEvent(value);
+                    this.EnableMdInner.Value = value;
+                }
+            }
+        }
 
         public PowerPreviewProperties()
         {
-            EnableSvg = new BoolProperty();
-            EnableMd = new BoolProperty();
+            EnableSvgInner = new BoolProperty(true);
+            EnableMdInner = new BoolProperty(true);
         }
 
         public override string ToString()
         {
             return JsonSerializer.Serialize(this);
+        }
+
+        private void LogTelemetryEvent(bool value, [CallerMemberName] string propertyName = null)
+        {
+            var dataEvent = new SettingsEnabledEvent()
+            {
+                Value = value,
+                Name = propertyName,
+            };
+            PowerToysTelemetry.Log.WriteEvent(dataEvent);
         }
     }
 }
