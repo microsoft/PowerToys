e310c210c32128e1bc9273e3edadbed113db2bd3
Jessica Lim <t-jesl@microsoft.com>
2020-05-11 17:44:21 -0400

    Move notifications as a service seperate from Activation Services

--- POC-OOBE/OOBE-Sandbox/PowerToys Settings Sandbox/Services/ActivationService.cs
+++ POC-OOBE/OOBE-Sandbox/PowerToys Settings Sandbox/Services/ActivationService.cs
@@ -25,88 +25,6 @@ namespace PowerToys_Settings_Sandbox.Services
 
         private object _lastActivationArgs;
 
-        private void newNotification()
-        {
-            
-            // In a real app, these would be initialized with actual data
-            string title = "Power Toys";
-            string content = "Power Toys is installed";
-            string image = "../Assets/Logo.png";
-            string logo = "../Assets/MiniLogo.png";
-
-            // Construct the visuals of the toast
-            ToastVisual visual = new ToastVisual()
-            {
-                BindingGeneric = new ToastBindingGeneric()
-                {
-                    Children =
-                    {
-                        new AdaptiveText() {
-                            Text = title
-                        },
-
-                        new AdaptiveText()
-                        {
-                            Text = content
-                        },
-
-                        new AdaptiveImage()
-                        {
-                            Source = image
-                        }
-                    },
-
-                    AppLogoOverride = new ToastGenericAppLogo()
-                    {
-                        Source = logo,
-                        HintCrop = ToastGenericAppLogoCrop.Circle
-                    }
-                }
-            };
-
-            // In a real app, these would be initialized with actual data
-            int conversationId = 384928;
-
-            // Construct the actions for the toast (inputs and buttons)
-            ToastActionsCustom actions = new ToastActionsCustom()
-            {
-                Inputs = { },
-                Buttons =
-                {
-                    new ToastButton("Open PowerToys", new QueryString()
-                    {
-                        // Insert query here
-                    }.ToString())
-                    {
-                        // Insert activation type here
-                    },
-                }
-            };
-
-            // Now we can construct the final toast content
-            ToastContent toastContent = new ToastContent()
-            {
-                Visual = visual,
-                Actions = actions,
-
-                // Arguments when the user taps body of toast
-                Launch = new QueryString()
-                {
-                    // Insert query here
-                }.ToString()
-             };
-
-            // And create the toast notification
-            var toast = new ToastNotification(toastContent.GetXml());
-
-            toast.ExpirationTime = DateTime.Now.AddDays(2);
-
-            toast.Tag = "18365";
-            toast.Group = "wallPosts";
-
-            ToastNotificationManager.CreateToastNotifier().Show(toast);
-        }
-
         public ActivationService(App app, Type defaultNavItem, Lazy<UIElement> shell = null)
         {
             _app = app;
@@ -145,7 +63,7 @@ namespace PowerToys_Settings_Sandbox.Services
                 await StartupAsync();
             }
 
-            newNotification();
+            Notifications.Toast();
 
         }
 
---
+++ POC-OOBE/OOBE-Sandbox/PowerToys Settings Sandbox/Services/Notifications.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications; // Notifications library
+using Microsoft.QueryStringDotNET; // QueryString.NET
+
+namespace PowerToys_Settings_Sandbox.Services
+{
+    internal class Notifications {
+        public static void Toast() {
+
+            // In a real app, these would be initialized with actual data
+            string title = "Power Toys";
+            string content = "Power Toys is installed";
+            string image = "../Assets/Logo.png";
+            string logo = "../Assets/MiniLogo.png";
+
+            // Construct the visuals of the toast
+            ToastVisual visual = new ToastVisual()
+            {
+                BindingGeneric = new ToastBindingGeneric()
+                {
+                    Children =
+                    {
+                        new AdaptiveText() {
+                            Text = title
+                        },
+
+                        new AdaptiveText()
+                        {
+                            Text = content
+                        },
+
+                        new AdaptiveImage()
+                        {
+                            Source = image
+                        }
+                    },
+
+                    AppLogoOverride = new ToastGenericAppLogo()
+                    {
+                        Source = logo,
+                        HintCrop = ToastGenericAppLogoCrop.Circle
+                    }
+                }
+            };
+
+            // Construct the actions for the toast (inputs and buttons)
+            ToastActionsCustom actions = new ToastActionsCustom()
+            {
+                Inputs = { },
+                Buttons =
+                {
+                    new ToastButton("Open PowerToys", new QueryString()
+                    {
+                        // Insert query here
+                    }.ToString())
+                    {
+                        // Insert activation type here
+                    },
+                }
+            };
+
+            // Now we can construct the final toast content
+            ToastContent toastContent = new ToastContent()
+            {
+                Visual = visual,
+                Actions = actions,
+
+                // Arguments when the user taps body of toast
+                Launch = new QueryString()
+                {
+                    // Insert query here
+                }.ToString()
+            };
+
+            // And create the toast notification
+            var toast = new ToastNotification(toastContent.GetXml());
+
+            toast.ExpirationTime = DateTime.Now.AddDays(2); // May need to add Tag and group
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+    }
+
+}
