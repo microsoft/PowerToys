101b0c8e580436d84f2f8d0664252c19011fdc3d
Samantha Difeo <t-sadife@microsoft.com>
2020-05-11 17:50:49 -0400

    Implement get pixel within wpf window

---
+++ proofOfConcept/ColorPickerAlpha/ColorPickerAlpha/ColorPicker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace ColorPickerAlpha
+{
+    public class ColorPicker
+    {
+        //Interops with C++
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        static extern int ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+        [DllImport("user32.dll")]
+        internal static extern bool GetPhysicalCursorPos(ref CursorPoint lpPoint);
+        [DllImport("user32.dll")]
+        internal static extern bool SetProcessDPIAware(); //TODO: set correctly
+
+        public struct CursorPoint
+        {
+            public int X;
+            public int Y;
+        }
+
+        static public Color GetPixelColor(int x, int y)
+        {
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            uint pixel = GetPixel(hdc, x, y);
+            ReleaseDC(IntPtr.Zero, hdc);
+            Color color = Color.FromArgb(
+                (int)(pixel & 0x000000FF),
+                (int)(pixel & 0x0000FF00) >> 8,
+                (int)(pixel & 0x00FF0000) >> 16);
+            return color;
+        }
+
+        static public (int x, int y) GetPhysicalCursorCoords()
+        {
+            CursorPoint cursorPos = new CursorPoint();
+            GetPhysicalCursorPos(ref cursorPos);
+
+            return (cursorPos.X, cursorPos.Y);
+        }
+    }
+}
--- proofOfConcept/ColorPickerAlpha/ColorPickerAlpha/MainWindow.xaml.cs
+++ proofOfConcept/ColorPickerAlpha/ColorPickerAlpha/MainWindow.xaml.cs
@@ -1,28 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ColorPickerAlpha
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point pnt = e.GetPosition(null);
+            (int x, int y) = ColorPicker.GetPhysicalCursorCoords();
+            System.Drawing.Color newCol = ColorPicker.GetPixelColor(x, y);
+
+            txt2.Text = "Mouse Click: " + pnt.ToString();
+            txt3.Text = "RGB: " + newCol.R + "," + newCol.B + "," + newCol.G;
+
+            //Test with rectangle windows of different colors
+            if (e.Source is Rectangle source)
+            {
+                source.Fill = source.Name.Equals("mrRec") ? Brushes.Aqua : Brushes.Red;
+            }
         }
     }
 }
