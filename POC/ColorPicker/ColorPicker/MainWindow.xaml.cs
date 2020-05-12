5747ddb740fa92c721215780007bf37dc495430b
Misha Krieger-Raynauld <t-mikrie@microsoft.com>
2020-05-12 15:46:38 -0400

    Create initial front-end with updating fields POC

--- POC/ColorPicker/ColorPicker/MainWindow.xaml.cs
+++ POC/ColorPicker/ColorPicker/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,29 @@ namespace ColorPicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random _random = new Random(); // For testing only
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+        
+        // This function is only for testing and this event handling will be removed
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.H)
+            {
+                Color randomColor = Color.FromRgb((byte)_random.Next(256), (byte)_random.Next(256), (byte)_random.Next(256));
+                SetColor(randomColor);
+            }
+        }
+
+        private void SetColor(Color color)
+        {
+            ColorPreviewRectangle.Fill = new SolidColorBrush(color);
+            RgbTextBox.Text = $"{color.R}, {color.G}, {color.B}";
+            HexTextBox.Text = "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
         }
     }
 }
