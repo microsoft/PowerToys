6d97108707b6681229d268e8e79d9c943bf20bb8
Samantha Difeo <t-sadife@microsoft.com>
2020-05-12 16:32:49 -0400

    Remove DPI awareness settings

--- proofOfConcept/ColorPickerAlpha/ColorPickerAlpha/ColorPicker.cs
+++ proofOfConcept/ColorPickerAlpha/ColorPickerAlpha/ColorPicker.cs
@@ -18,37 +18,19 @@ namespace ColorPickerAlpha
 
         [DllImport("user32.dll")]
         internal static extern bool GetPhysicalCursorPos(ref Point lpPoint);
-        [DllImport("user32.dll")]
-        internal static extern bool SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT value); 
-
-        public enum DPI_AWARENESS_CONTEXT
-        {
-            DPI_AWARENESS_CONTEXT_UNAWARE,             
-            DPI_AWARENESS_CONTEXT_SYSTEM_AWARE,         
-            DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE,    
-            DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2, 
-            DPI_AWARENESS_CONTEXT_UNAWARE_GDISCALED   
-        }
 
         static public Color GetPixelColor(int x, int y)
         {
-            DPI_AWARENESS_CONTEXT dpitype = DPI_AWARENESS_CONTEXT.DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE;
-            if (SetProcessDpiAwarenessContext(dpitype))
-            {
-                IntPtr hdc = GetDC(IntPtr.Zero);
-                uint pixel = GetPixel(hdc, x, y);
-                ReleaseDC(IntPtr.Zero, hdc);
-                Color color = Color.FromArgb(
-                    (int)(pixel & 0x000000FF),
-                    (int)(pixel & 0x0000FF00) >> 8,
-                    (int)(pixel & 0x00FF0000) >> 16);
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            uint pixel = GetPixel(hdc, x, y);
+            ReleaseDC(IntPtr.Zero, hdc);
 
-                return color;
-            }
-            else
-            {
-                throw new System.InvalidOperationException("Set process dpi aware failed");
-            }
+            Color color = Color.FromArgb(
+                (int)(pixel & 0x000000FF),
+                (int)(pixel & 0x0000FF00) >> 8,
+                (int)(pixel & 0x00FF0000) >> 16);
+
+            return color;
         }
 
         static public (int x, int y) GetPhysicalCursorCoords()
