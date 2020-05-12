ab0ee84a46ed9eb5fa14e8fd3c24ec1d8cb76151
Alexander Vorobev <t-alvoro@microsoft.com>
2020-05-12 14:04:44 -0400

    front-end part merge

---
+++ src/modules/fancyzones/editor/FancyZonesEditor/EventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FancyZonesEditor
+{
+    public class EventArgs<T> : EventArgs
+    {
+        public EventArgs(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; private set; }
+    }
+}
