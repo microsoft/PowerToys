58b9f78f0c932dd85d28ed283813fab4b8a6cf54
Jessica Lim <t-jesl@microsoft.com>
2020-05-13 14:38:43 -0400

    add activation

--- POC-OOBE/OOBE-Sandbox/PowerToys Settings Sandbox/App.xaml.cs
+++ POC-OOBE/OOBE-Sandbox/PowerToys Settings Sandbox/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Microsoft.QueryStringDotNET;
 using PowerToys_Settings_Sandbox.Services;
-using PowerToys_Settings_Sandbox.Views;
 
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
@@ -31,51 +30,36 @@ namespace PowerToys_Settings_Sandbox
             if (!args.PrelaunchActivated)
             {
                 await ActivationService.ActivateAsync(args);
+                NotificationService.AppInstalledToast();
+                NotificationService.AppUpdatedToast();
             }
         }
 
         protected override async void OnActivated(IActivatedEventArgs e)
         {
-            //await ActivationService.ActivateAsync(args);
-            Frame rootFrame = Window.Current.Content as Frame;
+            Frame rootFrame = Window.Current.Content as Frame;           
 
-            // TODO: Initialize root frame just like in OnLaunched
-
-            // Handle toast activation
             if (e is ToastNotificationActivatedEventArgs)
             {
-                var toastActivationArgs = e as ToastNotificationActivatedEventArgs;
+                ToastNotificationActivatedEventArgs toastActivationArgs = e as ToastNotificationActivatedEventArgs;
 
-                // Parse the query string (using QueryString.NET)
                 QueryString args = QueryString.Parse(toastActivationArgs.Argument);
-                var x = args["action"];
-                // See what action is being requested 
-                switch (x)
+
+                switch (args["action"])
                 {
                     case "openApp":
-
                         if (rootFrame == null)
                         {
-                            // Create a Frame to act as the navigation context and navigate to the first page
                             rootFrame = new Frame();
-                            // Place the frame in the current Window
                             Window.Current.Content = rootFrame;
                         }
 
-                        rootFrame.Navigate(typeof(FancyZonesPage));
-                        break;
+                        rootFrame.Navigate(typeof(Views.ShellPage));
+                        NavigationService.Navigate(typeof(Views.MainPage));
+                        break;                        
                 }
-
-                // If we're loading the app for the first time, place the main page on
-                // the back stack so that user can go back after they've been
-                // navigated to the specific page
-                //if (rootFrame.BackStack.Count == 0)
-                //    rootFrame.BackStack.Add(new PageStackEntry(typeof(MainPage), null, null));
             }
 
-            // TODO: Handle other types of activation
-
-            // Ensure the current window is active
             Window.Current.Activate();
         }
 
