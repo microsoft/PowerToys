e64962d174f253e2c8ba27f1919b3d15d8d692da
Tyrone Wong <t-tywong@microsoft.com>
2020-05-13 11:37:40 -0400

    Backend connection complete and clipboard added

--- proofOfConcept/ColorPickerAlpha/ColorPickerAlpha/MainWindow.xaml.cs
+++ proofOfConcept/ColorPickerAlpha/ColorPickerAlpha/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,15 +24,19 @@ namespace ColorPickerAlpha
                 while (true)
                 {
                     (int x, int y) = ColorPicker.GetPhysicalCursorCoords();
-                    Debug.WriteLine(x);
+
 
                     Dispatcher.BeginInvoke(new Action(() =>
                     {
                         System.Drawing.Color color = ColorPicker.GetPixelColor(x, y);
-                        Debug.WriteLine(color);
                         
                         curColor = System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
-                        //mrRec.Fill = new SolidColorBrush(curColor);
+                        Color_Box.Fill = new SolidColorBrush(curColor);
+
+                        R_val.Text = curColor.R.ToString();
+                        G_val.Text = curColor.G.ToString();
+                        B_val.Text = curColor.B.ToString();
+
                     }));
 
                     Thread.Sleep(100);
@@ -43,6 +48,24 @@ namespace ColorPickerAlpha
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Copy_Clip(object sender, RoutedEventArgs e)
+        {
+            StringBuilder rgb_hex = new StringBuilder();
+            string rgba_hex = curColor.ToString();
+            for (int index = 0; index < rgba_hex.Length; index++)
+            {
+                // Remove the initial 2 alpha digits in the #ARGB
+                if (index == 1 || index == 2)
+                {
+                    continue;
+                }
+
+                rgb_hex.Append(rgba_hex[index]);
+            }
+
+            Clipboard.SetText(rgb_hex.ToString());
         }
     }
 }
