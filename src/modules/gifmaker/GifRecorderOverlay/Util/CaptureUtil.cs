a4172e5260be8818dd273798cc680a0b3a42abc5
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-13 13:56:42 -0400

    Defined CaptureUtil file

---
+++ src/modules/gifmaker/GifRecorderOverlay/Util/CaptureUtil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GifRecorderOverlay.Util
+{
+    class CaptureUtil
+    {
+        CaptureUtil(
+            uint x,
+            uint y,
+            uint width,
+            uint height,
+            double framerate)
+        {
+
+        }
+
+        void Capture()
+        {
+
+        }
+
+        void Pause()
+        {
+
+        }
+
+        void Stop()
+        {
+
+        }
+
+        void Save(string filepath)
+        {
+
+        }
+
+    }
+}
