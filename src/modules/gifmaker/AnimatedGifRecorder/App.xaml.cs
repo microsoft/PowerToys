bf168870812ae3a5e0395503f184a5488bcba275
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-13 19:11:02 -0400

    Added MainViewModel

--- src/modules/gifmaker/AnimatedGifRecorder/App.xaml.cs
+++ src/modules/gifmaker/AnimatedGifRecorder/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using AnimatedGifRecorder.ViewModels;
 
 namespace AnimatedGifRecorder
 {
@@ -13,5 +14,24 @@ namespace AnimatedGifRecorder
     /// </summary>
     public partial class App : Application
     {
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var _mainWindow = new MainWindow();
+            var _mainViewModel= new MainViewModel();
+            _mainWindow.DataContext = _mainViewModel;  
+        }
+
+        /// <summary>
+        /// Application wide exception handler.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
---
+++ src/modules/gifmaker/AnimatedGifRecorder/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnimatedGifRecorder.Helpers;
+
+namespace AnimatedGifRecorder.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+
+
+    }
+}
