d89c6b68aea7dcdd5b8c13a53427cb4e8eba9d23
Misha Krieger-Raynauld <t-mikrie@microsoft.com>
2020-05-13 14:41:40 -0400

    Rework PixelColorFinder to find cursor position and integrate with front-end

--- POC/ColorPicker/ColorPicker/MainWindow.xaml.cs
+++ POC/ColorPicker/ColorPicker/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Threading;
 using ColorPicker.ColorPickingFunctionality;
 using ColorPicker.ColorPickingFunctionality.SystemEvents;
 
@@ -24,11 +24,32 @@ namespace ColorPicker
     /// </summary>
     public partial class MainWindow : Window
     {
-        private RegisteredMouseEventHook mouseEvent;
+        private bool _isColorSelectionEnabled = true;
+        private DispatcherTimer _updateTimer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
-            mouseEvent = new RegisteredMouseEventHook(PixelColorFinder.HandleMouseClick);
+            _updateTimer.Tick += UpdateCurrentColor;
+            _updateTimer.Interval = new TimeSpan(1000);
+            _updateTimer.Start();
+        }
+
+        private void UpdateCurrentColor(object sender, EventArgs e)
+        {
+            if (_isColorSelectionEnabled)
+            {
+                SetColor(PixelColorFinder.GetColorUnderCursor());
+            }
+        }
+
+        // TODO: Replace this with mouse down (needs transparent overlay window)
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                ToggleColorSelectionMode();
+            }
         }
 
         private void SetColor(Color color)
@@ -36,6 +57,19 @@ namespace ColorPicker
             ColorPreviewRectangle.Fill = new SolidColorBrush(color);
             RgbTextBox.Text = $"{color.R}, {color.G}, {color.B}";
             HexTextBox.Text = "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+        }
+
+        private void ToggleColorSelectionMode()
+        {
+            _isColorSelectionEnabled = !_isColorSelectionEnabled;
+            if (_isColorSelectionEnabled)
+            {
+                _updateTimer.Start();
+            }
+            else
+            {
+                _updateTimer.Stop();
+            }
         }
     }
 }
