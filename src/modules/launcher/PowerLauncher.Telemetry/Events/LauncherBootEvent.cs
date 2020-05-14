3bb8ba0b8fa2f9399b37dffeae6376204fa1d20f
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-05-13 22:51:02 -0700

    Adding hardcoded version number to boot events.

--- src/modules/launcher/PowerLauncher.Telemetry/Events/LauncherBootEvent.cs
+++ src/modules/launcher/PowerLauncher.Telemetry/Events/LauncherBootEvent.cs
@@ -7,6 +7,11 @@ namespace Microsoft.PowerLauncher.Telemetry
     [EventData]
     public class LauncherBootEvent : EventBase, IEvent
     {
+        /// <summary>
+        /// TODO: This should be replaced by a P/Invoke call to get_product_version
+        /// </summary>
+        public string Version => "v0.18.0";
+
         public double BootTimeMs { get; set; }
 
         public PartA_PrivTags PartA_PrivTags => PartA_PrivTags.ProductAndServicePerformance;
