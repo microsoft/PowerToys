679d8fabd6a865927850201e56ef9621c8ebeba6
Sanid <t-sansin@microsoft.com>
2020-05-14 09:47:05 -0700

    Cleaning up code/fixing PR comments

--- src/modules/Cursor/Cursor.cpp
+++ src/modules/Cursor/Cursor.cpp
@@ -7,16 +7,12 @@
 #include <cstdio>
 #include <stdio.h>
 #include <csignal>
-#include<math.h>
 #include <ShellScalingApi.h>
 #include <winuser.h>
 
-
-
-
 using namespace std;
 
-
+#define RECT_LENGTH 1000;
 
 
 
@@ -29,71 +25,6 @@ void cursorposition(int &x_coord, int &y_coord)
         y_coord = cursor.y;
     } 
 }
-/*
-void scalefac(int x, int y, float &scale)
-{
-    POINT pt;
-    pt.x = x;
-    pt.y = y;
-    HMONITOR monitor = MonitorFromPoint( pt, MONITOR_DEFAULTTONEAREST);
-    DEVICE_SCALE_FACTOR pScale;
-    GetScaleFactorForMonitor( monitor, &pScale);
-    
-    switch(pScale) 
-        {
-    SCALE_100_PERCENT:
-        scale = 1.0;
-        break;
-    SCALE_120_PERCENT:
-        scale = 1.20;
-        break;
-    SCALE_125_PERCENT:
-        scale = 1.25;
-        break;
-    SCALE_140_PERCENT:
-        scale = 1.40;
-        break;
-    SCALE_150_PERCENT:
-        scale = 1.50;
-        break;
-    SCALE_160_PERCENT:
-        scale = 1.60;
-        break;
-    SCALE_175_PERCENT:
-        scale = 1.75;
-        break;
-    SCALE_180_PERCENT:
-        scale = 1.80;
-        break;
-    SCALE_200_PERCENT:
-        scale= 2.0;
-        break;
-    SCALE_225_PERCENT:
-        scale = 2.25;
-        break;
-    SCALE_250_PERCENT:
-        scale = 2.50;
-        break;
-    SCALE_300_PERCENT:
-        scale = 3.0;
-        break;
-    SCALE_350_PERCENT:
-        scale = 3.50;
-        break;
-    SCALE_400_PERCENT:
-        scale = 4.0;
-        break;
-    SCALE_450_PERCENT:
-        scale = 4.50;
-        break;
-    SCALE_500_PERCENT:
-        scale = 5.0;
-        break;
-    }
-
-   
-}
-*/
 
 
 void getcolour(int x_coord, int y_coord, int &r, int&g, int&b)
@@ -194,10 +125,10 @@ void erase(int x_coord, int y_coord, int r, int g, int b)
 
     ReleaseDC(GetDesktopWindow(), hDC);
     RECT rect;
-    rect.left = x_coord - 1000;
-    rect.right = x_coord + 1000;
-    rect.top = y_coord - 1000;
-    rect.bottom = y_coord + 1000;
+    rect.left = x_coord - RECT_LENGTH;
+    rect.right = x_coord + RECT_LENGTH;
+    rect.top = y_coord - RECT_LENGTH;
+    rect.bottom = y_coord + RECT_LENGTH;
     HWND screen = GetForegroundWindow();
     HWND desktopscreen = GetDesktopWindow();
 
@@ -235,10 +166,10 @@ void locate(int x_coord, int y_coord, int r, int g, int b)
 
     ReleaseDC(GetDesktopWindow(), hDC);
     RECT rect;
-    rect.left = x_coord - 1000;
-    rect.right = x_coord + 1000;
-    rect.top = y_coord - 1000;
-    rect.bottom = y_coord + 1000;
+    rect.left = x_coord - RECT_LENGTH;
+    rect.right = x_coord + RECT_LENGTH;
+    rect.top = y_coord - RECT_LENGTH;
+    rect.bottom = y_coord + RECT_LENGTH;
     HWND screen = GetForegroundWindow();
     HWND desktopscreen = GetDesktopWindow();
     InvalidateRect(screen, NULL, true);
@@ -258,13 +189,10 @@ int main()
     int horizontalscreen, verticalscreen;
     float scale;
     SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+  
+    while(true) {
 
-
-        
-
-        while(true) {
-
-         if ((GetKeyState(VK_CONTROL) &(GetKeyState(VK_LWIN)&0x8000)))
+        if ((GetKeyState(VK_CONTROL) &(GetKeyState(VK_LWIN)&0x8000)))
         {
              GetDesktopResolution(horizontalscreen, verticalscreen);
             SetCursorPos(horizontalscreen / 2, verticalscreen / 2);
@@ -274,14 +202,14 @@ int main()
             {
             
             cursorposition(x_coord, y_coord);
-         // scalefac(x_coord, y_coord, scale);
-         // x_coord = x_coord * scale;
-         // y_coord = y_coord * scale;
+      
 
             getcolour(x_coord, y_coord, r, g, b);
             locate(x_coord, y_coord, r, g, b);
             //erase 
-      erase(x_coord, y_coord, 255-r, 255-g, 255-b);
+            erase(x_coord, y_coord, 255-r, 255-g, 255-b);
+
+            //Print out coordinates/RGB of cursor position
             
             cout << "x:" << x_coord << " y:" << y_coord << "\n";
             cout << "R:" << r << " G:" << g << " B:" << b << "\n \n";
