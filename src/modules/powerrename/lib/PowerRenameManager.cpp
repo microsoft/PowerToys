62725b5946807ad3b4ed72f8edcb2de402ac0ff9
muratakburak <muratakburakma@gmail.com>
2020-05-14 23:27:30 +0300

    Avoid wcslen() in for statement

--- src/modules/powerrename/lib/PowerRenameManager.cpp
+++ src/modules/powerrename/lib/PowerRenameManager.cpp
@@ -843,6 +843,7 @@ DWORD WINAPI CPowerRenameManager::s_regexWorkerThread(_In_ void* pv)
                                 }
 
 
+
                                 spItem->put_newName(newNameToUse);
 
                                 // Was there a change?
