8e180b14835df16f5fccb4e676fa0969b7d09812
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-15 10:47:04 -0400

    Defined RecorderConfiguration

---
+++ src/modules/gifmaker/AnimatedGifRecorder/RecorderConf.cs
@@ -0,0 +1,33 @@
+﻿namespace AnimatedGifRecorder
+{
+    /// <summary>
+    /// Summarizes capture session configurations in a single object
+    /// </summary>
+    public class RecorderConf
+    {
+        /// <summary>
+        /// Width of recording
+        /// </summary>
+        public int Width;
+
+        /// <summary>
+        /// Height of recording
+        /// </summary>
+        public int Height;
+
+        /// <summary>
+        /// Left most pixel of recording region
+        /// </summary>
+        public int X;
+
+        /// <summary>
+        /// Top most pixel of recording region
+        /// </summary>
+        public int Y;
+
+        /// <summary>
+        /// Determines the time interval between frames (1000ms / framerate)
+        /// </summary>
+        public double FrameRate;
+    }
+}
