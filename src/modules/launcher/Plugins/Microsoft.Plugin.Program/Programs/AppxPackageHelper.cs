d4b56f99ff235e0b5445c9f8064f85d313e7434d
Alekhya <reddykalekhya@gmail.com>
2020-05-15 08:49:44 -0700

    [launcher] Remove Program Plugin dll (AppxPackagingTlb.dll) (#3037)
    * Removed AppxPackaging dll
    
    * Added helper class and fixed some AppxPackage errors
    
    * Modified it to use the COM interface
    
    * Got it to build without errors but UWP apps don't show up
    
    * Added app to the array
    
    * Deleted the AppxPackaging.dll
    
    * Added github reference
    
    * Removed unnecessary uwp app argument
    
    * Removed dll from installer wxs file

---
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Program/Programs/AppxPackageHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using Windows.Storage;
+
+namespace Microsoft.Plugin.Program.Programs
+{
+    public class AppxPackageHelper
+    {
+        // This function returns a list of attributes of applications
+        public List<IAppxManifestApplication> getAppsFromManifest(IStream stream)
+        {
+            List<IAppxManifestApplication> apps = new List<IAppxManifestApplication>();
+            var appxFactory = new AppxFactory();
+            var reader = ((IAppxFactory)appxFactory).CreateManifestReader(stream);
+            var manifestApps = reader.GetApplications();
+            while (manifestApps.GetHasCurrent())
+            {
+                string appListEntry;
+                var manifestApp = manifestApps.GetCurrent();
+                manifestApp.GetStringValue("AppListEntry", out appListEntry);
+                if (appListEntry != "none")
+                {
+                    apps.Add(manifestApp);
+                }
+                manifestApps.MoveNext();
+            }
+            return apps;
+        }
+
+        // Reference : https://stackoverflow.com/questions/32122679/getting-icon-of-modern-windows-app-from-a-desktop-application
+        [Guid("5842a140-ff9f-4166-8f5c-62f5b7b0c781"), ComImport]
+        public class AppxFactory
+        {
+        }
+
+        [Guid("BEB94909-E451-438B-B5A7-D79E767B75D8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IAppxFactory
+        {
+            void _VtblGap0_2(); // skip 2 methods
+            IAppxManifestReader CreateManifestReader(IStream inputStream);
+        }
+
+        [Guid("4E1BD148-55A0-4480-A3D1-15544710637C"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IAppxManifestReader
+        {
+            void _VtblGap0_1(); // skip 1 method
+            IAppxManifestProperties GetProperties();
+            void _VtblGap1_5(); // skip 5 methods
+            IAppxManifestApplicationsEnumerator GetApplications();
+        }
+
+        [Guid("9EB8A55A-F04B-4D0D-808D-686185D4847A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IAppxManifestApplicationsEnumerator
+        {
+            IAppxManifestApplication GetCurrent();
+            bool GetHasCurrent();
+            bool MoveNext();
+        }
+
+        [Guid("5DA89BF4-3773-46BE-B650-7E744863B7E8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IAppxManifestApplication
+        {
+            [PreserveSig]
+            int GetStringValue([MarshalAs(UnmanagedType.LPWStr)] string name, [MarshalAs(UnmanagedType.LPWStr)] out string value);
+
+            [PreserveSig]
+            int GetAppUserModelId([MarshalAs(UnmanagedType.LPWStr)] out string value);
+        }
+
+        [Guid("03FAF64D-F26F-4B2C-AAF7-8FE7789B8BCA"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IAppxManifestProperties
+        {
+            [PreserveSig]
+            int GetBoolValue([MarshalAs(UnmanagedType.LPWStr)]string name, out bool value);
+            [PreserveSig]
+            int GetStringValue([MarshalAs(UnmanagedType.LPWStr)] string name, [MarshalAs(UnmanagedType.LPWStr)] out string value);
+        }
+    }
+}
--- src/modules/launcher/Plugins/Microsoft.Plugin.Program/Programs/UWP.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Program/Programs/UWP.cs
@@ -10,10 +10,8 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using Windows.ApplicationModel;
 using Windows.Management.Deployment;
-using AppxPackaing;
 using Wox.Infrastructure;
 using Microsoft.Plugin.Program.Logger;
-using IStream = AppxPackaing.IStream;
 using Rect = System.Windows.Rect;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Media;
@@ -21,6 +19,7 @@ using System.Windows.Controls;
 using Wox.Plugin;
 using System.Reflection;
 using Wox.Plugin.SharedCommands;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Microsoft.Plugin.Program.Programs
 {
@@ -54,12 +53,12 @@ namespace Microsoft.Plugin.Program.Programs
 
         private void InitializeAppInfo()
         {
+            AppxPackageHelper _helper = new AppxPackageHelper();
             var path = Path.Combine(Location, "AppxManifest.xml");
 
             var namespaces = XmlNamespaces(path);
             InitPackageVersion(namespaces);
 
-            var appxFactory = new AppxFactory();
             IStream stream;
             const uint noAttribute = 0x80;
             const Stgm exclusiveRead = Stgm.Read | Stgm.ShareExclusive;
@@ -67,20 +66,15 @@ namespace Microsoft.Plugin.Program.Programs
 
             if (hResult == Hresult.Ok)
             {
-                var reader = appxFactory.CreateManifestReader(stream);
-                var manifestApps = reader.GetApplications();
                 var apps = new List<Application>();
-                while (manifestApps.GetHasCurrent() != 0)
+             
+                List<AppxPackageHelper.IAppxManifestApplication> _apps = _helper.getAppsFromManifest(stream);
+                foreach(var _app in _apps)
                 {
-                    var manifestApp = manifestApps.GetCurrent();
-                    var appListEntry = manifestApp.GetStringValue("AppListEntry");
-                    if (appListEntry != "none")
-                    {
-                        var app = new Application(manifestApp, this);
-                        apps.Add(app);
-                    }
-                    manifestApps.MoveNext();
+                    var app = new Application(_app, this);
+                    apps.Add(app);
                 }
+                
                 Apps = apps.Where(a => a.AppListEntry != "none").ToArray();
             }
             else
@@ -382,16 +376,32 @@ namespace Microsoft.Plugin.Program.Programs
                 });
             }
 
-            public Application(IAppxManifestApplication manifestApp, UWP package)
+            public Application(AppxPackageHelper.IAppxManifestApplication manifestApp, UWP package)
             {
-                UserModelId = manifestApp.GetAppUserModelId();
-                UniqueIdentifier = manifestApp.GetAppUserModelId();
-                DisplayName = manifestApp.GetStringValue("DisplayName");
-                Description = manifestApp.GetStringValue("Description");
-                BackgroundColor = manifestApp.GetStringValue("BackgroundColor");
+                // This is done because we cannot use the keyword 'out' along with a property
+                string tmpUserModelId;
+                string tmpUniqueIdentifier;
+                string tmpDisplayName;
+                string tmpDescription;
+                string tmpBackgroundColor;
+                string tmpEntryPoint;
+
+                manifestApp.GetAppUserModelId(out tmpUserModelId);
+                manifestApp.GetAppUserModelId(out tmpUniqueIdentifier);
+                manifestApp.GetStringValue("DisplayName", out tmpDisplayName);
+                manifestApp.GetStringValue("Description", out tmpDescription);
+                manifestApp.GetStringValue("BackgroundColor", out tmpBackgroundColor);
+                manifestApp.GetStringValue("EntryPoint", out tmpEntryPoint);
+
+                UserModelId = tmpUserModelId;
+                UniqueIdentifier = tmpUniqueIdentifier;
+                DisplayName = tmpDisplayName;
+                Description = tmpDescription;
+                BackgroundColor = tmpBackgroundColor;
+                EntryPoint = tmpEntryPoint;
+
                 Package = package;
-                EntryPoint = manifestApp.GetStringValue("EntryPoint");
-                               
+
                 DisplayName = ResourceFromPri(package.FullName, DisplayName);
                 Description = ResourceFromPri(package.FullName, Description);
                 LogoUri = LogoUriFromManifest(manifestApp);
@@ -482,7 +492,7 @@ namespace Microsoft.Plugin.Program.Programs
             }
 
 
-            internal string LogoUriFromManifest(IAppxManifestApplication app)
+            internal string LogoUriFromManifest(AppxPackageHelper.IAppxManifestApplication app)
             {
                 var logoKeyFromVersion = new Dictionary<PackageVersion, string>
                 {
@@ -492,8 +502,9 @@ namespace Microsoft.Plugin.Program.Programs
                 };
                 if (logoKeyFromVersion.ContainsKey(Package.Version))
                 {
+                    string logoUri;
                     var key = logoKeyFromVersion[Package.Version];
-                    var logoUri = app.GetStringValue(key);
+                    app.GetStringValue(key, out logoUri);
                     return logoUri;
                 }
                 else
