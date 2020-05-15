a2e49da46cf38e4a64f0b6a20c30ec281a9a0c4d
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-15 10:44:57 -0400

    Defined frame model

---
+++ src/modules/gifmaker/AnimatedGifRecorder/FrameInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace AnimatedGifRecorder
+{
+    /// <summary>
+    /// Used to store individual frame metadata
+    /// </summary>
+    public class FrameInfo
+    {
+        /// <summary>
+        /// Frame index
+        /// </summary>
+        public int Index;
+
+        /// <summary>
+        /// Delay to next frame
+        /// </summary>
+        public int Delay;
+
+        /// <summary>
+        /// Path to bitmap
+        /// </summary>
+        public string Path;
+
+        /// <summary>
+        /// Bitmap image
+        /// </summary>
+        public Image Image;
+    }
+}
