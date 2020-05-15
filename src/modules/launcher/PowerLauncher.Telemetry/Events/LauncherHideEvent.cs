dfde90d503b7e15e122b856e9ac5817e57d29dee
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-05-14 17:47:59 -0700

    removing unused explicit interface exception

--- src/modules/launcher/PowerLauncher.Telemetry/Events/LauncherHideEvent.cs
+++ src/modules/launcher/PowerLauncher.Telemetry/Events/LauncherHideEvent.cs
@@ -8,7 +8,5 @@ namespace Microsoft.PowerLauncher.Telemetry
     public class LauncherHideEvent : EventBase, IEvent
     {
         public PartA_PrivTags PartA_PrivTags => PartA_PrivTags.ProductAndServiceUsage;
-
-        PartA_PrivTags IEvent.PartA_PrivTags => throw new System.NotImplementedException();
     }
 }
