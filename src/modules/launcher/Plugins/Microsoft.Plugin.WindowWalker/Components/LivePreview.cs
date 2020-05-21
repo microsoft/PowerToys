0a42106e732dbdf6f515ae07fbbaf685f236d4c8
Josh Soref <jsoref@users.noreply.github.com>
2020-05-20 21:08:15 -0400

    spelling: exclusion

--- src/modules/launcher/Plugins/Microsoft.Plugin.WindowWalker/Components/LivePreview.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.WindowWalker/Components/LivePreview.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Plugin.WindowWalker.Components
         /// Makes sure that a window is excluded from the live preview
         /// </summary>
         /// <param name="hwnd">handle to the window to exclude</param>
-        public static void SetWindowExlusionFromLivePreview(IntPtr hwnd)
+        public static void SetWindowExclusionFromLivePreview(IntPtr hwnd)
         {
             int renderPolicy = (int)InteropAndHelpers.DwmNCRenderingPolicy.Enabled;
 
