3d3a087fa9a93fff68da463d710b61c9bb454627
Josh Soref <jsoref@users.noreply.github.com>
2020-05-20 21:14:41 -0400

    spelling: successful

--- src/core/Microsoft.PowerToys.Settings.UnitTest/ViewModelTests/ImageResizer.cs
+++ src/core/Microsoft.PowerToys.Settings.UnitTest/ViewModelTests/ImageResizer.cs
@@ -68,7 +68,7 @@ namespace ViewModelTests
         }
 
         [TestMethod]
-        public void JPEGQualityLevel_ShouldSetValueToTen_WhenSuccefull()
+        public void JPEGQualityLevel_ShouldSetValueToTen_WhenSuccessful()
         {
             // arrange
             ImageResizerViewModel viewModel = new ImageResizerViewModel();
@@ -82,7 +82,7 @@ namespace ViewModelTests
         }
 
         [TestMethod]
-        public void PngInterlaceOption_ShouldSetValueToTen_WhenSuccefull()
+        public void PngInterlaceOption_ShouldSetValueToTen_WhenSuccessful()
         {
             // arrange
             ImageResizerViewModel viewModel = new ImageResizerViewModel();
@@ -96,7 +96,7 @@ namespace ViewModelTests
         }
 
         [TestMethod]
-        public void TiffCompressOption_ShouldSetValueToTen_WhenSuccefull()
+        public void TiffCompressOption_ShouldSetValueToTen_WhenSuccessful()
         {
             // arrange
             ImageResizerViewModel viewModel = new ImageResizerViewModel();
@@ -110,7 +110,7 @@ namespace ViewModelTests
         }
 
         [TestMethod]
-        public void FileName_ShouldUpdateValue_WhenSuccefull()
+        public void FileName_ShouldUpdateValue_WhenSuccessful()
         {
             // arrange
             ImageResizerViewModel viewModel = new ImageResizerViewModel();
@@ -157,7 +157,7 @@ namespace ViewModelTests
         }
 
         [TestMethod]
-        public void KeepDateModified_ShouldUpdateValue_WhenSuccefull()
+        public void KeepDateModified_ShouldUpdateValue_WhenSuccessful()
         {
             // arrange
             ImageResizerViewModel viewModel = new ImageResizerViewModel();
@@ -172,7 +172,7 @@ namespace ViewModelTests
 
 
         [TestMethod]
-        public void Encoder_ShouldUpdateValue_WhenSuccefull()
+        public void Encoder_ShouldUpdateValue_WhenSuccessful()
         {
             // arrange
             ImageResizerViewModel viewModel = new ImageResizerViewModel();
@@ -187,7 +187,7 @@ namespace ViewModelTests
         }
 
         [TestMethod]
-        public void AddRow_ShouldAddEmptyImageSize_WhenSuccefull()
+        public void AddRow_ShouldAddEmptyImageSize_WhenSuccessful()
         {
             // arrange
             ImageResizerViewModel viewModel = new ImageResizerViewModel();
@@ -201,7 +201,7 @@ namespace ViewModelTests
         }
 
         [TestMethod]
-        public void DeleteImageSize_ShouldDeleteImageSize_WhenSuccefull()
+        public void DeleteImageSize_ShouldDeleteImageSize_WhenSuccessful()
         {
             // arrange
             ImageResizerViewModel viewModel = new ImageResizerViewModel();
