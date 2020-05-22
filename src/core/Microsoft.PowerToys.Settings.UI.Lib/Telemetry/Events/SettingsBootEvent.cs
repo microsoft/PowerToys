41ca982833d2607b516610a4f9fc345ae0dd113f
Enrico Giordani <enricogior@users.noreply.github.com>
2020-05-22 20:28:49 +0200

    Now working on 0.18.2 (#3542)

--- src/core/Microsoft.PowerToys.Settings.UI.Lib/Telemetry/Events/SettingsBootEvent.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Lib/Telemetry/Events/SettingsBootEvent.cs
@@ -14,7 +14,7 @@ namespace Microsoft.PowerLauncher.Telemetry
         /// <summary>
         /// Gets The version string. TODO: This should be replaced by a P/Invoke call to get_product_version
         /// </summary>
-        public string Version => "v0.18.1";
+        public string Version => "v0.18.2";
 
         public double BootTimeMs { get; set; }
 
