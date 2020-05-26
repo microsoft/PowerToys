18c0c3bdb1ff68c375b7a7272ca946230f7b18e6
yuyoyuppe <a.yuyoyuppe@gmail.com>
2020-05-26 13:05:21 +0300

    sln: specify WinSDK version for cpp projects

--- src/modules/keyboardmanager/common/pch.h
+++ src/modules/keyboardmanager/common/pch.h
@@ -1,13 +1,15 @@
 #pragma once
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <winrt/base.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Graphics.h>
 #include <winrt/Windows.system.h>
 #include <winrt/windows.ui.xaml.hosting.h>
 #include <windows.ui.xaml.hosting.desktopwindowxamlsource.h>
 #include <winrt/windows.ui.xaml.controls.h>
 #include <winrt/Windows.ui.xaml.media.h>
 #include <winrt/Windows.Foundation.Collections.h>
-#include "winrt/Windows.Foundation.h"
 #include "winrt/Windows.Foundation.Numerics.h"
 #include "winrt/Windows.UI.Xaml.Controls.Primitives.h"
 #include "winrt/Windows.UI.Text.h"
