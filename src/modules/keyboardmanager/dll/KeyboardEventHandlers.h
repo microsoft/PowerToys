8e44b67def8af90b2a536857eb615cba7a002909
Arjun <arjunbalgovind@gmail.com>
2020-06-01 16:03:55 -0700

    fixed formatting

--- src/modules/keyboardmanager/dll/KeyboardEventHandlers.h
+++ src/modules/keyboardmanager/dll/KeyboardEventHandlers.h
@@ -1,4 +1,4 @@
- #pragma once
+#pragma once
 #include <keyboardmanager/common/KeyboardManagerState.h>
 #include <keyboardmanager/common/KeyboardManagerConstants.h>
 #include "InputInterface.h"
@@ -6,17 +6,17 @@
 namespace KeyboardEventHandlers
 {
     // Function to a handle a single key remap
-    intptr_t HandleSingleKeyRemapEvent(InputInterface &ii, LowlevelKeyboardEvent* data, KeyboardManagerState& keyboardManagerState) noexcept;
+    intptr_t HandleSingleKeyRemapEvent(InputInterface& ii, LowlevelKeyboardEvent* data, KeyboardManagerState& keyboardManagerState) noexcept;
 
     // Function to a change a key's behavior from toggle to modifier
-    intptr_t HandleSingleKeyToggleToModEvent(InputInterface &ii, LowlevelKeyboardEvent* data, KeyboardManagerState& keyboardManagerState) noexcept;
+    intptr_t HandleSingleKeyToggleToModEvent(InputInterface& ii, LowlevelKeyboardEvent* data, KeyboardManagerState& keyboardManagerState) noexcept;
 
     // Function to a handle a shortcut remap
-    intptr_t HandleShortcutRemapEvent(InputInterface &ii, LowlevelKeyboardEvent* data, std::map<Shortcut, RemapShortcut>& reMap, std::mutex& map_mutex) noexcept;
+    intptr_t HandleShortcutRemapEvent(InputInterface& ii, LowlevelKeyboardEvent* data, std::map<Shortcut, RemapShortcut>& reMap, std::mutex& map_mutex) noexcept;
 
     // Function to a handle an os-level shortcut remap
-    intptr_t HandleOSLevelShortcutRemapEvent(InputInterface &ii, LowlevelKeyboardEvent* data, KeyboardManagerState& keyboardManagerState) noexcept;
+    intptr_t HandleOSLevelShortcutRemapEvent(InputInterface& ii, LowlevelKeyboardEvent* data, KeyboardManagerState& keyboardManagerState) noexcept;
 
     // Function to a handle an app-specific shortcut remap
-    intptr_t HandleAppSpecificShortcutRemapEvent(InputInterface &ii, LowlevelKeyboardEvent* data, KeyboardManagerState& keyboardManagerState) noexcept;
+    intptr_t HandleAppSpecificShortcutRemapEvent(InputInterface& ii, LowlevelKeyboardEvent* data, KeyboardManagerState& keyboardManagerState) noexcept;
 };
