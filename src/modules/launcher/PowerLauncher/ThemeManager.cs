10eafc8583a1251168347c084d0851f4317d1666
AnuthaDev <anuthadev@gmail.com>
2020-06-06 12:06:38 +0530

    Do stuff

--- src/modules/launcher/PowerLauncher/ThemeManager.cs
+++ src/modules/launcher/PowerLauncher/ThemeManager.cs
@@ -1,57 +1,34 @@
-﻿using ControlzEx.Theming;
-using MahApps.Metro.Theming;
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
 using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Windows.UI.ViewManagement;
 
 namespace Wox.Core.Resource
 {
     public class ThemeManager
     {
-        private Theme currentTheme;
+        private string currentTheme
+        {
+            get => WindowsThemeHelper.getAppTheme();
+        }
         private readonly Application App;
-        private readonly string LightTheme = "Light.Accent1";
-        private readonly string DarkTheme = "Dark.Accent1";
-        private readonly string HighContrastOneTheme = "HighContrast.Accent2";
-        private readonly string HighContrastTwoTheme = "HighContrast.Accent3";
-        private readonly string HighContrastBlackTheme = "HighContrast.Accent4";
-        private readonly string HighContrastWhiteTheme = "HighContrast.Accent5";
-
+        
         public ThemeManager(Application app)
         {
             this.App = app;
-
-            Uri HighContrastOneThemeUri = new Uri("pack://application:,,,/Themes/HighContrast1.xaml");
-            Uri HighContrastTwoThemeUri = new Uri("pack://application:,,,/Themes/HighContrast2.xaml");
-            Uri HighContrastBlackThemeUri = new Uri("pack://application:,,,/Themes/HighContrastWhite.xaml");
-            Uri HighContrastWhiteThemeUri = new Uri("pack://application:,,,/Themes/HighContrastBlack.xaml");
-            Uri LightThemeUri = new Uri("pack://application:,,,/Themes/Light.xaml");
-            Uri DarkThemeUri = new Uri("pack://application:,,,/Themes/Dark.xaml");
-
-            ControlzEx.Theming.ThemeManager.Current.AddLibraryTheme(
-                new LibraryTheme(HighContrastOneThemeUri,
-                MahAppsLibraryThemeProvider.DefaultInstance));
-            ControlzEx.Theming.ThemeManager.Current.AddLibraryTheme(
-                new LibraryTheme(HighContrastTwoThemeUri,
-                MahAppsLibraryThemeProvider.DefaultInstance));
-            ControlzEx.Theming.ThemeManager.Current.AddLibraryTheme(
-                new LibraryTheme(HighContrastBlackThemeUri,
-                MahAppsLibraryThemeProvider.DefaultInstance));
-            ControlzEx.Theming.ThemeManager.Current.AddLibraryTheme(
-                new LibraryTheme(HighContrastWhiteThemeUri,
-                MahAppsLibraryThemeProvider.DefaultInstance));
-            ControlzEx.Theming.ThemeManager.Current.AddLibraryTheme(
-                new LibraryTheme(LightThemeUri,
-                MahAppsLibraryThemeProvider.DefaultInstance));
-            ControlzEx.Theming.ThemeManager.Current.AddLibraryTheme(
-                new LibraryTheme(DarkThemeUri,
-                MahAppsLibraryThemeProvider.DefaultInstance));
-
             ResetTheme();
-            ControlzEx.Theming.ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
-            ControlzEx.Theming.ThemeManager.Current.ThemeChanged += Current_ThemeChanged;
+
+            SystemEvents.UserPreferenceChanged += (sender, args) =>
+            {
+                if (args.Category == UserPreferenceCategory.General)
+                {
+                    ResetTheme();
+                }
+            };
             SystemParameters.StaticPropertyChanged += (sender, args) =>
             {
                 if (args.PropertyName == nameof(SystemParameters.HighContrast))
@@ -62,93 +39,16 @@ namespace Wox.Core.Resource
         }
         
 
-        public Theme GetHighContrastBaseType()
-        {
-            string RegistryKey = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes";
-            string theme = (string) Registry.GetValue(RegistryKey, "CurrentTheme", string.Empty);
-            theme = theme.Split('\\').Last().Split('.').First().ToString();
-
-            if (theme == "hc1")
-                return Theme.HighContrastOne;
-            else if (theme == "hc2")
-                return Theme.HighContrastTwo;
-            else if (theme == "hcwhite")
-                return Theme.HighContrastWhite;
-            else if (theme == "hcblack")
-                return Theme.HighContrastBlack;
-            else
-                return Theme.None;
-        }
-
         public void ResetTheme()
         {
-            if (SystemParameters.HighContrast)
-            {
-                Theme highContrastBaseType = GetHighContrastBaseType();
-                ChangeTheme(highContrastBaseType);
-            }
-            else
-            {
-                string baseColor = WindowsThemeHelper.GetWindowsBaseColor();
-                ChangeTheme((Theme)Enum.Parse(typeof(Theme), baseColor));
-            }
+            App.Resources.MergedDictionaries[0].Source = new Uri($"/Themes/{currentTheme}.xaml", UriKind.Relative);
         }
 
-        private void ChangeTheme(Theme theme)
+        private Color ConvertColor(global::Windows.UI.Color color)
         {
-            if (theme == currentTheme)
-                return;
-            if (theme == Theme.HighContrastOne)
-            {
-                ControlzEx.Theming.ThemeManager.Current.ChangeTheme(this.App, this.HighContrastOneTheme);
-                currentTheme = Theme.HighContrastOne;
-            }
-            else if (theme == Theme.HighContrastTwo)
-            {
-                ControlzEx.Theming.ThemeManager.Current.ChangeTheme(this.App, this.HighContrastTwoTheme);
-                currentTheme = Theme.HighContrastTwo;
-            }
-            else if (theme == Theme.HighContrastWhite)
-            {
-                ControlzEx.Theming.ThemeManager.Current.ChangeTheme(this.App, this.HighContrastWhiteTheme);
-                currentTheme = Theme.HighContrastWhite;
-            }
-            else if (theme == Theme.HighContrastBlack)
-            {
-                ControlzEx.Theming.ThemeManager.Current.ChangeTheme(this.App, this.HighContrastBlackTheme);
-                currentTheme = Theme.HighContrastBlack;
-            }
-            else if (theme == Theme.Light)
-            {
-                ControlzEx.Theming.ThemeManager.Current.ChangeTheme(this.App, this.LightTheme);
-                currentTheme = Theme.Light;
-            }
-            else if (theme == Theme.Dark)
-            {
-                ControlzEx.Theming.ThemeManager.Current.ChangeTheme(this.App, this.DarkTheme);
-                currentTheme = Theme.Dark;
-            }
-            else
-            {
-                currentTheme = Theme.None;
-            }
-            Debug.WriteLine("Theme Changed to :" + currentTheme);
-        }
-
-        private void Current_ThemeChanged(object sender, ThemeChangedEventArgs e)
-        {
-            ResetTheme();
+            //Convert the specified UWP color to a WPF color
+            return Color.FromArgb(color.A, color.R, color.G, color.B);
         }
     }
 
-    public enum Theme
-    {
-        None,
-        Light, 
-        Dark, 
-        HighContrastOne,
-        HighContrastTwo,
-        HighContrastBlack,
-        HighContrastWhite,
-    }
 }
---
+++ src/modules/launcher/PowerLauncher/WindowsThemeHelper.cs
@@ -0,0 +1,110 @@
+﻿namespace Wox.Core.Resource
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+    using System.Windows.Media;
+    using JetBrains.Annotations;
+    using Microsoft.Win32;
+    using Windows.UI.ViewManagement;
+
+    public static class WindowsThemeHelper
+    {
+        public static bool IsHighContrastEnabled()
+        {
+            return SystemParameters.HighContrast;
+        }
+
+        [MustUseReturnValue]
+        public static bool AppsUseLightTheme()
+        {
+            Color? themeColor = null;
+            try
+            {
+                var uiSettings = new global::Windows.UI.ViewManagement.UISettings();
+                themeColor = ConvertColor(uiSettings.GetColorValue(UIColorType.Background));
+                if (themeColor.Equals(Colors.White))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception exception)
+            {
+                Trace.TraceError(exception.ToString());
+            }
+
+            return true;
+        }
+
+        [MustUseReturnValue]
+        public static string getAppTheme()
+        {
+            if (IsHighContrastEnabled())
+            {
+                return GetHighContrastBaseType();
+            }
+            else
+            {
+                if (AppsUseLightTheme())
+                {
+                    return "Light";
+                }
+                else
+                {
+                    return "Dark";
+                }
+            }
+        }
+
+        public static string GetHighContrastBaseType()
+        {
+            string RegistryKey = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes";
+            string theme = (string)Registry.GetValue(RegistryKey, "CurrentTheme", string.Empty);
+            theme = theme.Split('\\').Last().Split('.').First().ToString();
+
+            if (theme == "hc1")
+                return "HighContrast1";
+            else if (theme == "hc2")
+                return "HighContrast2";
+            else if (theme == "hcwhite")
+                return "HighContrastWhite";
+            else if (theme == "hcblack")
+                return "HighContrastBlack";
+            else
+                return "";
+        }
+
+        [CanBeNull]
+        [MustUseReturnValue]
+        public static Color? GetWindowsAccentColor()
+        {
+            Color? accentColor = null;
+
+            try
+            {
+                var uiSettings = new global::Windows.UI.ViewManagement.UISettings();
+                accentColor = ConvertColor(uiSettings.GetColorValue(UIColorType.Accent));
+                return accentColor;
+            }
+            catch (Exception exception)
+            {
+                Trace.TraceError(exception.ToString());
+            }
+
+            return accentColor;
+        }
+
+
+        private static Color ConvertColor(global::Windows.UI.Color color)
+        {
+            //Convert the specified UWP color to a WPF color
+            return Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+    }
+}
