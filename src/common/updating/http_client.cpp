334551d11080d79dabc45015a50a3e36101f5b60
Serafima <zykovas91@gmail.com>
2020-06-19 17:13:49 +0300

    address PR comment: embedded method, removed class member

--- src/common/updating/http_client.cpp
+++ src/common/updating/http_client.cpp
@@ -35,23 +35,14 @@ namespace http
         msi_installer_file_stream.Close();
     }
 
-    std::future<void> HttpClient::download(const winrt::Windows::Foundation::Uri& url, const std::wstring& dstFilePath, const std::function<void(float)>& progressUpdateHandle)
+    std::future<void> HttpClient::download(const winrt::Windows::Foundation::Uri& url, const std::wstring& dstFilePath, const std::function<void(float)>& progressUpdateCallback)
     {
-        m_progressChangeHandle = progressUpdateHandle;
-
         auto response = co_await m_client.GetAsync(url, HttpCompletionOption::ResponseHeadersRead);
         response.EnsureSuccessStatusCode();
 
         uint64_t totalBytes = response.Content().Headers().ContentLength().GetUInt64();
         auto contentStream = co_await response.Content().ReadAsInputStreamAsync();
-        co_await downloadFile(totalBytes, contentStream, dstFilePath);
 
-        contentStream.Close();
-        m_progressChangeHandle = nullptr;
-    }
-
-    std::future<void> HttpClient::downloadFile(uint64_t totalDownloadSize, const storage::Streams::IInputStream& contentStream, const std::wstring& dstFilePath)
-    {
         uint64_t totalBytesRead = 0;
         storage::Streams::Buffer buffer(8192);
         auto fileStream = co_await storage::Streams::FileRandomAccessStream::OpenAsync(dstFilePath.c_str(), storage::FileAccessMode::ReadWrite, storage::StorageOpenOptions::AllowReadersAndWriters, storage::Streams::FileOpenDisposition::CreateAlways);
@@ -61,19 +52,21 @@ namespace http
         {
             co_await fileStream.WriteAsync(buffer);
             totalBytesRead += buffer.Length();
-            if (m_progressChangeHandle)
+            if (progressUpdateCallback)
             {
-                float percentage = (float)totalBytesRead / totalDownloadSize;
-                m_progressChangeHandle(percentage);
+                float percentage = (float)totalBytesRead / totalBytes;
+                progressUpdateCallback(percentage);
             }
 
             co_await contentStream.ReadAsync(buffer, buffer.Capacity(), storage::Streams::InputStreamOptions::None);
         }
 
-        if (m_progressChangeHandle)
+        if (progressUpdateCallback)
         {
-            m_progressChangeHandle(1);
+            progressUpdateCallback(1);
         }
+
         fileStream.Close();
+        contentStream.Close();
     }
 }
--- src/common/updating/http_client.h
+++ src/common/updating/http_client.h
@@ -11,13 +11,9 @@ namespace http
         HttpClient();
         std::future<std::wstring> request(const winrt::Windows::Foundation::Uri& url);
         std::future<void> download(const winrt::Windows::Foundation::Uri& url, const std::wstring& dstFle);
-        std::future<void> download(const winrt::Windows::Foundation::Uri& url, const std::wstring& dstFle, const std::function<void(float)>& progressUpdateHandle);
-
-    private:
-        std::future<void> downloadFile(uint64_t totalDownloadSize, const winrt::Windows::Storage::Streams::IInputStream& contentStream, const std::wstring& dstFilePath);
+        std::future<void> download(const winrt::Windows::Foundation::Uri& url, const std::wstring& dstFle, const std::function<void(float)>& progressUpdateCallback);
 
     private:
         winrt::Windows::Web::Http::HttpClient m_client;
-        std::function<void(float)> m_progressChangeHandle;
     };
 }
