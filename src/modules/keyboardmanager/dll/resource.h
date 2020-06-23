8c355917a080f88811b9baffa7049200f1a80210
Yevhenii Holovachov <yevhenii.holovachov@gmail.com>
2020-06-23 17:18:18 +0300

    Added info to FancyZones, KeyboardManager and PowerLauncher projects

--- src/modules/keyboardmanager/dll/resource.h
+++ src/modules/keyboardmanager/dll/resource.h
@@ -1,3 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by KeyboardManager.rc
+
+//////////////////////////////
+// Non-localizable
+
+#define FILE_DESCRIPTION "PowerToys KeyboardManager"
+#define INTERNAL_NAME "KeyboardManager"
+#define ORIGINAL_FILENAME "KeyboardManager.dll"
+
+// Non-localizable
+//////////////////////////////
+
 #define IDS_SETTINGS_DESCRIPTION 101
-#define IDS_KEYBOARDMANAGER 102
-#define IDS_KEYBOARDMANAGER_ICON    103
+#define IDS_KEYBOARDMANAGER      102
+#define IDS_KEYBOARDMANAGER_ICON 103
