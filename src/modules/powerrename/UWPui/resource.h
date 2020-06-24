500150e60ea2ca0b68f7c37b953bd0916e21e0aa
Yevhenii Holovachov <yevhenii.holovachov@gmail.com>
2020-06-24 14:30:57 +0300

    Fixed info for powerrename, previewpane and shortcutguide

---
+++ src/modules/powerrename/UWPui/resource.h
@@ -0,0 +1,13 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PowerRenameUWPUI.rc
+
+//////////////////////////////
+// Non-localizable
+
+#define FILE_DESCRIPTION "PowerToys PowerRenameUWPUI"
+#define INTERNAL_NAME "PowerRenameUWPUI"
+#define ORIGINAL_FILENAME "PowerRenameUWPUI.exe"
+
+// Non-localizable
+//////////////////////////////
