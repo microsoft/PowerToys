023d1484abc46f5a3f715a9a1da2e77cdcc281d5
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-06-26 11:44:51 -0700

    Changing test structure.  Need to add unit tests.

--- src/modules/launcher/Plugins/Microsoft.Plugin.Program.UnitTests/Storage/PackageRepositoryTest.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Program.UnitTests/Storage/PackageRepositoryTest.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Microsoft.Plugin.Program.UnitTests.Programs
+namespace Microsoft.Plugin.Program.UnitTests.Storage
 {
-    class PackageCatalogWrapperTests
+    class PackageRepositoryTest
     {
     }
 }
