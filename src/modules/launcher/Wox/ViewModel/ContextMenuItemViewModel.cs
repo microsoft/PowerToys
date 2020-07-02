18f4e9db31cf051572ac04a7761169aa4cc94f00
Alekhya <reddykalekhya@gmail.com>
2020-07-02 13:48:41 -0700

    Fix for Memory issue with context menu items (#4597)
    * Added the inotifyPropertyChanged to all the properties and that stops the memory for shooting up
    
    * some more inotify properties added
    
    (cherry picked from commit 26fa05d9b661dadc5ab0257d540ab838a07c43a6)
    
    * Revert "some more inotify properties added"
    
    This reverts commit 845a94c9b22f8fcaaa73cca6a9e8f511169fdaa0.
    
    * Removed unnecessary inotifypropertychanged interfaces and cleaned up the code
    
    * removed the ctrl+c from folder plugin
    
    * removed unnecessary init
    
    * Added unit test to check if PropertyChanged is called
    
    * renamed var
    
    * refactored the tests
    
    * formatting and adding comments
    
    * changed access modifier in test
    
    * Used observable collection instead of a list
    
    * clearing the observable collection instead of setting it to a new one

--- src/modules/launcher/Wox/ViewModel/ContextMenuItemViewModel.cs
+++ src/modules/launcher/Wox/ViewModel/ContextMenuItemViewModel.cs
@@ -9,11 +9,29 @@ namespace Wox.ViewModel
 {
     public class ContextMenuItemViewModel : BaseModel
     {
+        private ICommand _command;
+
         public string PluginName { get; set; }
         public string Title { get; set; }
         public string Glyph { get; set; }
         public string FontFamily { get; set; }
-        public ICommand Command { get; set; }
+        public ICommand Command {
+            get
+            {
+                return this._command;
+            }
+
+            set
+            {
+                // ICommand does not implement the INotifyPropertyChanged interface and must call OnPropertyChanged() to prevent memory leaks
+                if (value != this._command)
+                {
+                    this._command = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         public Key AcceleratorKey { get; set; }
         public ModifierKeys AcceleratorModifiers { get; set; }
         public bool IsAcceleratorKeyEnabled { get; set; }
