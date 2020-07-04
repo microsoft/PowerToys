aa2747251342e292ff80056668725295f60082ba
Alekhya Reddy <reddykalekhya@gmail.com>
2020-07-03 18:37:08 -0700

    Added run as admin context menu item to apps returned by indexer plugin

--- src/modules/launcher/Plugins/Microsoft.Plugin.Indexer/ContextMenuLoader.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Indexer/ContextMenuLoader.cs
@@ -8,12 +8,14 @@ using Wox.Plugin;
 using Microsoft.Plugin.Indexer.SearchHelper;
 using System.Windows.Input;
 using System.Reflection;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace Microsoft.Plugin.Indexer
 {
     internal class ContextMenuLoader : IContextMenu
     {
-        private readonly PluginInitContext _context;
+        private readonly IPublicAPI _api;
 
         public enum ResultType
         {
@@ -21,9 +23,12 @@ namespace Microsoft.Plugin.Indexer
             File
         }
 
-        public ContextMenuLoader(PluginInitContext context)
+        // Extensions for applications
+        private readonly string[]  appExtensions = { ".exe", ".bat", ".appref-ms", ".lnk" };
+
+        public ContextMenuLoader(IPublicAPI api)
         {
-            _context = context;
+            _api = api;
         }
 
         public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
@@ -38,11 +43,17 @@ namespace Microsoft.Plugin.Indexer
                     contextMenus.Add(CreateOpenContainingFolderResult(record));
                 }
 
+                // Test to check if File can be Run as admin, if yes, we add a 'run as admin' context menu item
+                if(CanFileBeRunAsAdmin(record.Path))
+                {
+                    contextMenus.Add(CreateRunAsAdminResult(record));
+                }
+
                 var fileOrFolder = (type == ResultType.File) ? "file" : "folder";
                 contextMenus.Add(new ContextMenuResult
                 {
                     PluginName = Assembly.GetExecutingAssembly().GetName().Name,
-                    Title = _context.API.GetTranslation("Microsoft_plugin_indexer_copy_path"),
+                    Title = _api.GetTranslation("Microsoft_plugin_indexer_copy_path"),
                     Glyph = "\xE8C8",
                     FontFamily = "Segoe MDL2 Assets",
                     SubTitle = $"Copy the current {fileOrFolder} path to clipboard",
@@ -60,7 +71,7 @@ namespace Microsoft.Plugin.Indexer
                         {
                             var message = "Fail to set text in clipboard";
                             LogException(message, e);
-                            _context.API.ShowMsg(message);
+                            _api.ShowMsg(message);
                             return false;
                         }
                     }
@@ -70,12 +81,70 @@ namespace Microsoft.Plugin.Indexer
             return contextMenus;
         }
 
+        // Function to add the context menu item to run as admin
+        private ContextMenuResult CreateRunAsAdminResult(SearchResult record)
+        {
+            return new ContextMenuResult
+            {
+                PluginName = Assembly.GetExecutingAssembly().GetName().Name,
+                Title = _api.GetTranslation("Run as admin"),
+                Glyph = "\xE7EF",
+                FontFamily = "Segoe MDL2 Assets",
+                AcceleratorKey = Key.Enter,
+                AcceleratorModifiers = (ModifierKeys.Control | ModifierKeys.Shift),
+                Action = _ =>
+                {
+                    var info = new ProcessStartInfo
+                    {
+                        FileName = record.Path,
+                        WorkingDirectory = Path.GetDirectoryName(record.Path),
+                        Verb = "runas",
+                        UseShellExecute = true
+                    };
+
+                    Task.Run(() => StartProcess(Process.Start, info));
+
+                    return true;
+                }
+            };
+        }
+
+        // Function to start and process and log errors if the process doesn't start as expected
+        private void StartProcess(Func<ProcessStartInfo, Process> runProcess, ProcessStartInfo info)
+        {
+            try
+            {
+                runProcess(info);
+            }
+            catch (Exception)
+            {
+                var name = "Plugin: Indexer";
+                var message = $"Unable to start: {info.FileName}";
+                _api.ShowMsg(name, message, string.Empty);
+            }
+        }
+
+        // Function to test if the file can be run as admin
+        private bool CanFileBeRunAsAdmin(string path)
+        {
+            string fileExtension = Path.GetExtension(path);
+            foreach(string extension in appExtensions)
+            {
+                if(extension.Equals(fileExtension, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
         private ContextMenuResult CreateOpenContainingFolderResult(SearchResult record)
         {
             return new ContextMenuResult
             {
                 PluginName = Assembly.GetExecutingAssembly().GetName().Name,
-                Title = _context.API.GetTranslation("Microsoft_plugin_indexer_open_containing_folder"),
+                Title = _api.GetTranslation("Microsoft_plugin_indexer_open_containing_folder"),
                 Glyph = "\xE838",
                 FontFamily = "Segoe MDL2 Assets",
                 AcceleratorKey = Key.E,
@@ -90,7 +159,7 @@ namespace Microsoft.Plugin.Indexer
                     {
                         var message = $"Fail to open file at {record.Path}";
                         LogException(message, e);
-                        _context.API.ShowMsg(message);
+                        _api.ShowMsg(message);
                         return false;
                     }
 
