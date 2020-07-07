8d72bc0ea4273a1714f438cd000cad2b04bf14ce
Divyansh Srivastava <somm14divi@gmail.com>
2020-07-07 13:02:39 -0700

    Tooltip for indexer and program plugin (#4589)
    * Fix multiline title issue
    
    * Added code to display tooltip for program and indexer plugin
    
    * Added tests for Result class
    
    * Theme based color for tooltip
    
    * Added colors for tooltip
    
    * Added string tags to tooltip
    
    * Add initial show delay
    
    * Seperated textbox for title and path

---
+++ src/modules/launcher/Wox.Plugin/ToolTipData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Wox.Plugin
+{
+    public class ToolTipData
+    {
+        public string Title { get; private set; }
+
+        public string Text { get; private set; }
+
+        public ToolTipData(string title, string text)
+        {          
+            if(string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("title cannot be null or empty", "title");
+            }
+            Title = title;
+            Text = text;
+        }
+    }
+}
