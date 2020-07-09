6d8fe419961ee8a0055e91c82816177d1830f38e
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-07-09 12:03:51 -0700

    Revert "Changing test structure.  Need to add unit tests."
    This reverts commit 1c267a55d5eab0262167c251c5a2d766993eeae6.

--- src/modules/launcher/Plugins/Microsoft.Plugin.Program.UnitTests/Programs/PackageCatalogWrapperTests.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Program.UnitTests/Programs/PackageCatalogWrapperTests.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Microsoft.Plugin.Program.UnitTests.Storage
+namespace Microsoft.Plugin.Program.UnitTests.Programs
 {
-    class PackageRepositoryTest
+    class PackageCatalogWrapperTests
     {
     }
 }
