ea79a4c3338f00e8c7635850fc1cd181ed6cf59c
Alekhya Reddy <reddykalekhya@gmail.com>
2020-07-12 11:46:21 -0700

    Added fix for shortcut applications to work as expected while renaming and deleting them

--- src/modules/launcher/Plugins/Microsoft.Plugin.Program/Storage/Win32ProgramRepository.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Program/Storage/Win32ProgramRepository.cs
@@ -16,6 +16,7 @@ namespace Microsoft.Plugin.Program.Storage
         private string[] _pathsToWatch;
         private int _numberOfPathsToWatch;
         private Collection<string> extensionsToWatch = new Collection<string>{ "*.exe", "*.lnk", "*.appref-ms", "*.url" };
+        private readonly string lnkExtension = ".lnk";
 
         public Win32ProgramRepository(List<IFileSystemWatcherWrapper> fileSystemWatcherHelpers, IStorage<IList<Programs.Win32>> storage, Settings settings, string[] pathsToWatch)
         {
@@ -55,28 +56,77 @@ namespace Microsoft.Plugin.Program.Storage
             string oldPath = e.OldFullPath;
             string newPath = e.FullPath;
 
-            Programs.Win32 oldApp = Programs.Win32.GetAppFromPath(oldPath);
+            string extension = Path.GetExtension(newPath);
             Programs.Win32 newApp = Programs.Win32.GetAppFromPath(newPath);
+            Programs.Win32 oldApp;
 
-            if(oldApp != null)
+            // Once the shortcut application is renamed, the old app does not exist and therefore when we try to get the FullPath we get the lnk path instead of the exe path
+            // This changes the hashCode() of the old application.
+            // Therefore, instead of retrieving the old app using the GetAppFromPath(), we construct the application ourself
+            // This situation is not encountered for other application types because the fullPath is the path itself, instead of being computed by using the path to the app.
+            if (extension.Equals(lnkExtension, StringComparison.OrdinalIgnoreCase))
+            {
+                oldApp = new Programs.Win32() { Name = GetNameOfLnkApp(e.OldName), ExecutableName = newApp.ExecutableName, FullPath = newApp.FullPath };
+            }
+            else
+            {
+                oldApp = Programs.Win32.GetAppFromPath(oldPath);
+            }
+
+            // To remove the old app which has been renamed and to add the new application.
+            if (oldApp != null)
             {
                 Remove(oldApp);
             }
 
-            if(newApp != null)
+            if (newApp != null)
             {
                 Add(newApp);
             }
         }
 
+        // Remove Extension to return the Name of the app
+        // Eg: Notepad.lnk must return NotePad.
+        private string GetNameOfLnkApp(string oldName)
+        {
+            int length = oldName.Length;
+            return oldName.Substring(0, oldName.Length - lnkExtension.Length);
+        }
+
         private void OnAppDeleted(object sender, FileSystemEventArgs e)
         {
             string path = e.FullPath;
-            Programs.Win32 app = Programs.Win32.GetAppFromPath(path);
-            if(app != null)
+            string extension = Path.GetExtension(path);
+            Programs.Win32 app;
+
+            // To mitigate the issue of not having a FullPath for a shortcut app, we iterate through the items and find the app with the same hashcode.
+            if (extension.Equals(lnkExtension, StringComparison.OrdinalIgnoreCase))
+            {
+                app = GetAppWithSameLnkResolvedPath(path);
+            }
+            else
+            {
+                app = Programs.Win32.GetAppFromPath(path);
+            }
+
+            if (app != null)
             {
                 Remove(app);
             }
+        }
+
+        // To mitigate the issue faced (as stated above) when a shortcut application is renamed, the Exe FullPath and executable name must be obtained.
+        // Unlike the rename event args, since we do not have a newPath, we iterate through all the programs and find the one with the same LnkResolved path.
+        private Programs.Win32 GetAppWithSameLnkResolvedPath(string lnkResolvedPath)
+        {
+            foreach(Programs.Win32 app in Items)
+            {
+                if (lnkResolvedPath.ToLower().Equals(app.LnkResolvedPath))
+                {
+                    return app;
+                }
+            }
+            return null;
         }
 
         private void OnAppCreated(object sender, FileSystemEventArgs e)
