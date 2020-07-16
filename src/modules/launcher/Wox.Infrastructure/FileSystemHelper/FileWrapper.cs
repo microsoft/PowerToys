92e42f6ecac0a30825c92be73d0a507c28d0b23c
Alekhya Reddy <reddykalekhya@gmail.com>
2020-07-16 13:37:58 -0700

    Added a retry block for ReadAllLines

--- src/modules/launcher/Wox.Infrastructure/FileSystemHelper/FileWrapper.cs
+++ src/modules/launcher/Wox.Infrastructure/FileSystemHelper/FileWrapper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using Wox.Infrastructure.Logger;
+using System.Threading;
 
 namespace Wox.Infrastructure.FileSystemHelper
 {
@@ -12,15 +13,28 @@ namespace Wox.Infrastructure.FileSystemHelper
 
         public string[] ReadAllLines(string path)
         {
-            try
+            int attempt = 0;
+            int maxRetries = 5;
+
+            // Sometimes when files are being installed, url applications are written to line by line.
+            // During this process their contents cannot be read as they are being accessed by an other process.
+            // This ensures that if we do face this scenario, we retry after some time.
+            while(attempt < maxRetries)
             {
-                return File.ReadAllLines(path);
+                try
+                {
+                    return File.ReadAllLines(path);
+                }
+                catch (IOException ex)
+                {
+                    attempt++;
+                    Thread.Sleep(500);
+                    Log.Info($"File {path} is being accessed by another process| {ex.Message}");
+                    
+                }
             }
-            catch(IOException ex)
-            {
-                Log.Info($"File {path} is being accessed by another process| {ex.Message}");
-                return new string[] { String.Empty };
-            }
+
+            return new string[] { String.Empty };
         }
 
     }
