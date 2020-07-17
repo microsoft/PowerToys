e8de6f0acec31cf86364ae4b459c2254385222b6
Nkateko <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-07-17 11:01:46 -0700

    [Settings]Change text color for disabled controls (#4837)
    * change text color for disabled controls
    
    * added custom textblock controls and updated FZ Xaml
    
    * updated Image Resizer Page
    
    * updated Shotcut Guid
    
    * updated KBM Settings
    
    * reverted unrealted changes
    
    * reverted unrealted changes
    
    * reverted unrealted changes
    
    * reverted unrealted changes
    
    * updated power launcher
    
    * reverted unrealted changes
    
    * Image Resizer: renoved spaces in file format hint text
    
    * Image Resizer: restored file format header
    
    Co-authored-by: Lavius Motileng <laviusntk>

--- src/core/Microsoft.PowerToys.Settings.UI/ViewModels/PowerLauncherViewModel.cs
+++ src/core/Microsoft.PowerToys.Settings.UI/ViewModels/PowerLauncherViewModel.cs
@@ -187,6 +187,172 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
 
         public HotkeySettings OpenConsole
         {
+            callback = (PowerLauncherSettings settings) =>
+            {
+                // Propagate changes to Power Launcher through IPC
+                ShellPage.DefaultSndMSGCallback(
+                    string.Format("{{ \"powertoys\": {{ \"{0}\": {1} }} }}", PowerLauncherSettings.POWERTOYNAME, JsonSerializer.Serialize(settings)));
+            };
+            if (SettingsUtils.SettingsExists(PowerLauncherSettings.POWERTOYNAME))
+            {
+                settings = SettingsUtils.GetSettings<PowerLauncherSettings>(PowerLauncherSettings.POWERTOYNAME);
+            }
+            else
+            {
+                settings = new PowerLauncherSettings();
+                settings.properties.open_powerlauncher.Alt = true;
+                settings.properties.open_powerlauncher.Code = (int)Windows.System.VirtualKey.Space;
+                settings.properties.maximum_number_of_results = 4;
+                callback(settings);
+            }
+
+            if (SettingsUtils.SettingsExists())
+            {
+                generalSettings = SettingsUtils.GetSettings<GeneralSettings>();
+            }
+            else
+            {
+                generalSettings = new GeneralSettings();
+            }
+        }
+
+        public PowerLauncherViewModel(PowerLauncherSettings settings, SendCallback callback)
+        {
+            this.settings = settings;
+            this.callback = callback;
+        }
+
+        private void UpdateSettings([CallerMemberName] string propertyName = null)
+        {
+            // Notify UI of property change
+            OnPropertyChanged(propertyName);
+
+            callback(settings);
+        }
+
+        public bool EnablePowerLauncher
+        {
+            get
+            {
+                return generalSettings.Enabled.PowerLauncher;
+            }
+
+            set
+            {
+                if (generalSettings.Enabled.PowerLauncher != value)
+                {
+                    generalSettings.Enabled.PowerLauncher = value;
+                    OnPropertyChanged(nameof(EnablePowerLauncher));
+                    OutGoingGeneralSettings outgoing = new OutGoingGeneralSettings(generalSettings);
+                    ShellPage.DefaultSndMSGCallback(outgoing.ToString());
+                }
+            }
+        }
+
+        public string SearchResultPreference
+        {
+            get
+            {
+                return settings.properties.search_result_preference;
+            }
+
+            set
+            {
+                if (settings.properties.search_result_preference != value)
+                {
+                    settings.properties.search_result_preference = value;
+                    UpdateSettings();
+                }
+            }
+        }
+
+        public string SearchTypePreference
+        {
+            get
+            {
+                return settings.properties.search_type_preference;
+            }
+
+            set
+            {
+                if (settings.properties.search_type_preference != value)
+                {
+                    settings.properties.search_type_preference = value;
+                    UpdateSettings();
+                }
+            }
+        }
+
+        public int MaximumNumberOfResults
+        {
+            get
+            {
+                return settings.properties.maximum_number_of_results;
+            }
+
+            set
+            {
+                if (settings.properties.maximum_number_of_results != value)
+                {
+                    settings.properties.maximum_number_of_results = value;
+                    UpdateSettings();
+                }
+            }
+        }
+
+        public HotkeySettings OpenPowerLauncher
+        {
+            get
+            {
+                return settings.properties.open_powerlauncher;
+            }
+
+            set
+            {
+                if (settings.properties.open_powerlauncher != value)
+                {
+                    settings.properties.open_powerlauncher = value;
+                    UpdateSettings();
+                }
+            }
+        }
+
+        public HotkeySettings OpenFileLocation
+        {
+            get
+            {
+                return settings.properties.open_file_location;
+            }
+
+            set
+            {
+                if (settings.properties.open_file_location != value)
+                {
+                    settings.properties.open_file_location = value;
+                    UpdateSettings();
+                }
+            }
+        }
+
+        public HotkeySettings CopyPathLocation
+        {
+            get
+            {
+                return settings.properties.copy_path_location;
+            }
+
+            set
+            {
+                if (settings.properties.copy_path_location != value)
+                {
+                    settings.properties.copy_path_location = value;
+                    UpdateSettings();
+                }
+            }
+        }
+
+        public HotkeySettings OpenConsole
+        {
             get
             {
                 return settings.properties.open_console;
