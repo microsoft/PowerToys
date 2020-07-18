d09253e532b8a85412f7c55657b534733f915d67
Alekhya <reddykalekhya@gmail.com>
2020-07-17 22:32:21 -0700

    Functionality to detect Win32 apps which are installed, deleted or renamed while PowerToys is running (#4960)
    * Added file system wrapper and interface
    
    * added win32program repository which would load store app and also handle new apps being added/deleted
    
    * Added event handlers to win32 program repo
    
    * added paths to monitor and setting FSWs for each location
    
    * Events firing as expected
    
    * filter extensions added, events fire as expected
    
    * override gethashcode so that duplicates don't show up. OnCreated and OnDeleted events trigger as expected
    
    * implemented setter for filters in FileSystemWatcher
    
    * Rename adds item but does not seem to delete the previous app
    
    * catching an exception when a duplicate item is inserted
    
    * Removed notify filter for directory because we only need to monitor files
    
    * Added exe programs to be indexed in the desktop and startmenu
    
    * created a new class to init FileSystemHelpers instead of main
    
    * Added fix for shortcut applications to work as expected while renaming and deleting them
    
    * Added wrappers for file system operations
    
    * Added some tests
    
    * Added all tests for appref-ms and added a condition to search in sub directories
    
    * Added tests for Exe applications
    
    * Added lnk app tests
    
    * Added tests for shortcut applications
    
    * removed unnecessary wrappers
    
    * override Equals for win32
    
    * removed debug statements
    
    * Fixed exe issue
    
    * Fixed internet shortcut exception
    
    * fixed renaming shortcut apps
    
    * Added a retry block for ReadAllLines
    
    * capitalized method name - RetrieveTargetPath
    
    * made naming consistent, helper variables start with underscore
    
    * Added the exception condition back
    
    * renamed Win32ProgramRepositoryHelper to Win32ProgramFileSystemWatchers
    
    * made win32Program repository variable static
    
    * changed list to ilist
    
    * disposed file system watchers
    
    * make retrieveTargetPath upper case in tests

---
+++ src/modules/launcher/Wox.Infrastructure/Storage/FileSystemWatcherWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+
+namespace Wox.Infrastructure.Storage
+{
+    // File System Watcher Wrapper class which implements the IFileSystemWatcherWrapper interface
+    public class FileSystemWatcherWrapper : FileSystemWatcher, IFileSystemWatcherWrapper
+    {
+        public FileSystemWatcherWrapper() { }
+
+        Collection<string> IFileSystemWatcherWrapper.Filters 
+        { 
+            get => this.Filters;
+            set 
+            { 
+                if(value != null)
+                {
+                    foreach(string filter in value)
+                    {
+                        this.Filters.Add(filter);
+                    }
+                }
+            }
+        }
+    }
+}
