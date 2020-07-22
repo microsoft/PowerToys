e2b3604b8044e4afb343092bff4dd75fe6e0f768
PrzemyslawTusinski <61138537+PrzemyslawTusinski@users.noreply.github.com>
2020-07-22 13:19:53 +0200

    Added loggin to imageloader

--- src/modules/videoconference/VideoConferenceCustomMediaSource/ImageLoader.cpp
+++ src/modules/videoconference/VideoConferenceCustomMediaSource/ImageLoader.cpp
@@ -16,6 +16,13 @@
 
 #include "ImageLoader.h"
 
+#define RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(val) \
+                                                   \
+    if (FAILED(val))                               \
+    {                                              \
+        return nullptr;                            \
+    }
+
 IWICImagingFactory* _GetWIC()
 {
     static IWICImagingFactory* s_Factory = nullptr;
@@ -39,15 +46,6 @@ IWICImagingFactory* _GetWIC()
     return s_Factory;
 }
 
-#ifdef RETURN_IF_FAILED
-#undef RETURN_IF_FAILED
-#endif
-#define RETURN_IF_FAILED(val) \
-    if (FAILED(val))          \
-    {                         \
-        return nullptr;       \
-    }
-
 using Microsoft::WRL::ComPtr;
 
 ComPtr<IMFSample> LoadImageAsSample(ComPtr<IStream> imageStream, IMFMediaType* sampleMediaType)
@@ -55,7 +53,7 @@ ComPtr<IMFSample> LoadImageAsSample(ComPtr<IStream> imageStream, IMFMediaType* s
     // Get target sample frame dimensions
     UINT targetWidth = 0;
     UINT targetHeight = 0;
-    RETURN_IF_FAILED(MFGetAttributeSize(sampleMediaType, MF_MT_FRAME_SIZE, &targetWidth, &targetHeight));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(MFGetAttributeSize(sampleMediaType, MF_MT_FRAME_SIZE, &targetWidth, &targetHeight));
 
     IWICImagingFactory* pWIC = _GetWIC();
     if (!pWIC)
@@ -65,21 +63,21 @@ ComPtr<IMFSample> LoadImageAsSample(ComPtr<IStream> imageStream, IMFMediaType* s
 
     // Initialize image bitmap decoder from filename and get the image frame
     ComPtr<IWICBitmapDecoder> bitmapDecoder;
-    RETURN_IF_FAILED(pWIC->CreateDecoderFromStream(imageStream.Get(), nullptr, WICDecodeMetadataCacheOnLoad, &bitmapDecoder));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(pWIC->CreateDecoderFromStream(imageStream.Get(), nullptr, WICDecodeMetadataCacheOnLoad, &bitmapDecoder));
 
     ComPtr<IWICBitmapFrameDecode> decodedFrame;
-    RETURN_IF_FAILED(bitmapDecoder->GetFrame(0, &decodedFrame));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(bitmapDecoder->GetFrame(0, &decodedFrame));
 
     UINT imageWidth = 0, imageHeight = 0;
-    RETURN_IF_FAILED(decodedFrame->GetSize(&imageWidth, &imageHeight));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(decodedFrame->GetSize(&imageWidth, &imageHeight));
 
     // Scale the image if required
     ComPtr<IWICBitmapSource> sourceImageFrame;
     if (targetWidth != imageWidth || targetHeight != imageHeight)
     {
         ComPtr<IWICBitmapScaler> scaler;
-        RETURN_IF_FAILED(pWIC->CreateBitmapScaler(&scaler));
-        RETURN_IF_FAILED(scaler->Initialize(decodedFrame.Get(), targetWidth, targetHeight, WICBitmapInterpolationModeHighQualityCubic));
+        RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(pWIC->CreateBitmapScaler(&scaler));
+        RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(scaler->Initialize(decodedFrame.Get(), targetWidth, targetHeight, WICBitmapInterpolationModeHighQualityCubic));
         sourceImageFrame.Attach(scaler.Detach());
     }
     else
@@ -90,58 +88,58 @@ ComPtr<IMFSample> LoadImageAsSample(ComPtr<IStream> imageStream, IMFMediaType* s
     // We need to encode the image as jpg, since it's always supported by MT video decoders and could be converted to
     // other formats
     ComPtr<IWICBitmapEncoder> jpgEncoder;
-    RETURN_IF_FAILED(pWIC->CreateEncoder(GUID_ContainerFormatJpeg, nullptr, &jpgEncoder));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(pWIC->CreateEncoder(GUID_ContainerFormatJpeg, nullptr, &jpgEncoder));
 
     // Prepare the encoder output memory stream and encoding params
     ComPtr<IStream> jpgStream;
-    RETURN_IF_FAILED(CreateStreamOnHGlobal(nullptr, true, &jpgStream));
-    RETURN_IF_FAILED(jpgEncoder->Initialize(jpgStream.Get(), WICBitmapEncoderNoCache));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(CreateStreamOnHGlobal(nullptr, true, &jpgStream));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgEncoder->Initialize(jpgStream.Get(), WICBitmapEncoderNoCache));
     ComPtr<IWICBitmapFrameEncode> jpgFrame;
-    RETURN_IF_FAILED(jpgEncoder->CreateNewFrame(&jpgFrame, nullptr));
-    RETURN_IF_FAILED(jpgFrame->Initialize(nullptr));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgEncoder->CreateNewFrame(&jpgFrame, nullptr));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgFrame->Initialize(nullptr));
 
     WICPixelFormatGUID jpgFormat = GUID_WICPixelFormat24bppBGR;
-    RETURN_IF_FAILED(jpgFrame->SetPixelFormat(&jpgFormat));
-    RETURN_IF_FAILED(jpgFrame->SetSize(targetWidth, targetHeight));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgFrame->SetPixelFormat(&jpgFormat));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgFrame->SetSize(targetWidth, targetHeight));
 
     // Commit the image encoding
-    RETURN_IF_FAILED(jpgFrame->WriteSource(sourceImageFrame.Get(), nullptr));
-    RETURN_IF_FAILED(jpgFrame->Commit());
-    RETURN_IF_FAILED(jpgEncoder->Commit());
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgFrame->WriteSource(sourceImageFrame.Get(), nullptr));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgFrame->Commit());
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgEncoder->Commit());
 
     // Obtain stream size and lock its memory pointer
     STATSTG jpgStreamStat{};
-    RETURN_IF_FAILED(jpgStream->Stat(&jpgStreamStat, STATFLAG_NONAME));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgStream->Stat(&jpgStreamStat, STATFLAG_NONAME));
     const size_t jpgStreamSize = jpgStreamStat.cbSize.QuadPart;
     HGLOBAL streamMemoryHandle{};
-    RETURN_IF_FAILED(GetHGlobalFromStream(jpgStream.Get(), &streamMemoryHandle));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(GetHGlobalFromStream(jpgStream.Get(), &streamMemoryHandle));
 
     auto jpgStreamMemory = static_cast<uint8_t*>(GlobalLock(streamMemoryHandle));
     auto unlockJpgStreamMemory = wil::scope_exit([jpgStreamMemory] { GlobalUnlock(jpgStreamMemory); });
 
     // Create a sample from the input image buffer
     ComPtr<IMFSample> inputSample;
-    RETURN_IF_FAILED(MFCreateSample(&inputSample));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(MFCreateSample(&inputSample));
 
     IMFMediaBuffer* inputMediaBuffer = nullptr;
-    RETURN_IF_FAILED(MFCreateAlignedMemoryBuffer(static_cast<DWORD>(jpgStreamSize), MF_64_BYTE_ALIGNMENT, &inputMediaBuffer));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(MFCreateAlignedMemoryBuffer(static_cast<DWORD>(jpgStreamSize), MF_64_BYTE_ALIGNMENT, &inputMediaBuffer));
     BYTE* inputBuf = nullptr;
     DWORD max_length = 0, current_length = 0;
-    RETURN_IF_FAILED(inputMediaBuffer->Lock(&inputBuf, &max_length, &current_length));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(inputMediaBuffer->Lock(&inputBuf, &max_length, &current_length));
     if (max_length < jpgStreamSize)
     {
         return nullptr;
     }
     std::copy(jpgStreamMemory, jpgStreamMemory + jpgStreamSize, inputBuf);
     unlockJpgStreamMemory.reset();
-    RETURN_IF_FAILED(inputMediaBuffer->Unlock());
-    RETURN_IF_FAILED(inputMediaBuffer->SetCurrentLength(static_cast<DWORD>(jpgStreamSize)));
-    RETURN_IF_FAILED(inputSample->AddBuffer(inputMediaBuffer));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(inputMediaBuffer->Unlock());
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(inputMediaBuffer->SetCurrentLength(static_cast<DWORD>(jpgStreamSize)));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(inputSample->AddBuffer(inputMediaBuffer));
 
     // Now we are ready to convert it to the requested media type, so we need to find a suitable jpg encoder
     MFT_REGISTER_TYPE_INFO inputFilter = { MFMediaType_Video, MFVideoFormat_MJPG };
     MFT_REGISTER_TYPE_INFO outputFilter = { MFMediaType_Video, {} };
-    RETURN_IF_FAILED(sampleMediaType->GetGUID(MF_MT_SUBTYPE, &outputFilter.guidSubtype));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(sampleMediaType->GetGUID(MF_MT_SUBTYPE, &outputFilter.guidSubtype));
 
     // But if no conversion is needed, just return the input sample
     if (!memcmp(&inputFilter.guidSubtype, &outputFilter.guidSubtype, sizeof(GUID)))
@@ -152,7 +150,7 @@ ComPtr<IMFSample> LoadImageAsSample(ComPtr<IStream> imageStream, IMFMediaType* s
     IMFActivate** ppVDActivate = nullptr;
     UINT32 count = 0;
 
-    RETURN_IF_FAILED(MFTEnumEx(MFT_CATEGORY_VIDEO_DECODER, MFT_ENUM_FLAG_SYNCMFT, &inputFilter, &outputFilter, &ppVDActivate, &count));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(MFTEnumEx(MFT_CATEGORY_VIDEO_DECODER, MFT_ENUM_FLAG_SYNCMFT, &inputFilter, &outputFilter, &ppVDActivate, &count));
     ComPtr<IMFTransform> videoDecoder;
 
     bool videoDecoderActivated = false;
@@ -175,22 +173,22 @@ ComPtr<IMFSample> LoadImageAsSample(ComPtr<IStream> imageStream, IMFMediaType* s
     auto shutdownVideoDecoder = wil::scope_exit([&videoDecoder] { MFShutdownObject(videoDecoder.Get()); });
     // Set input/output types for the decoder
     ComPtr<IMFMediaType> jpgFrameMediaType;
-    RETURN_IF_FAILED(MFCreateMediaType(&jpgFrameMediaType));
-    RETURN_IF_FAILED(jpgFrameMediaType->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video));
-    RETURN_IF_FAILED(jpgFrameMediaType->SetGUID(MF_MT_SUBTYPE, MFVideoFormat_MJPG));
-    RETURN_IF_FAILED(jpgFrameMediaType->SetUINT32(MF_MT_INTERLACE_MODE, MFVideoInterlace_Progressive));
-    RETURN_IF_FAILED(jpgFrameMediaType->SetUINT32(MF_MT_ALL_SAMPLES_INDEPENDENT, TRUE));
-    RETURN_IF_FAILED(MFSetAttributeSize(jpgFrameMediaType.Get(), MF_MT_FRAME_SIZE, targetWidth, targetHeight));
-    RETURN_IF_FAILED(MFSetAttributeRatio(jpgFrameMediaType.Get(), MF_MT_PIXEL_ASPECT_RATIO, 1, 1));
-    RETURN_IF_FAILED(videoDecoder->SetInputType(0, jpgFrameMediaType.Get(), 0));
-    RETURN_IF_FAILED(videoDecoder->SetOutputType(0, sampleMediaType, 0));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(MFCreateMediaType(&jpgFrameMediaType));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgFrameMediaType->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgFrameMediaType->SetGUID(MF_MT_SUBTYPE, MFVideoFormat_MJPG));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgFrameMediaType->SetUINT32(MF_MT_INTERLACE_MODE, MFVideoInterlace_Progressive));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(jpgFrameMediaType->SetUINT32(MF_MT_ALL_SAMPLES_INDEPENDENT, TRUE));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(MFSetAttributeSize(jpgFrameMediaType.Get(), MF_MT_FRAME_SIZE, targetWidth, targetHeight));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(MFSetAttributeRatio(jpgFrameMediaType.Get(), MF_MT_PIXEL_ASPECT_RATIO, 1, 1));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(videoDecoder->SetInputType(0, jpgFrameMediaType.Get(), 0));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(videoDecoder->SetOutputType(0, sampleMediaType, 0));
 
     // Process the input sample
-    RETURN_IF_FAILED(videoDecoder->ProcessInput(0, inputSample.Get(), 0));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(videoDecoder->ProcessInput(0, inputSample.Get(), 0));
 
     // Check whether we need to allocate output sample and buffer ourselves
     MFT_OUTPUT_STREAM_INFO outputStreamInfo{};
-    RETURN_IF_FAILED(videoDecoder->GetOutputStreamInfo(0, &outputStreamInfo));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(videoDecoder->GetOutputStreamInfo(0, &outputStreamInfo));
     const bool onlyProvidesSamples = outputStreamInfo.dwFlags & MFT_OUTPUT_STREAM_PROVIDES_SAMPLES;
     const bool canProvideSamples = outputStreamInfo.dwFlags & MFT_OUTPUT_STREAM_CAN_PROVIDE_SAMPLES;
     const bool mustAllocateSample = (!onlyProvidesSamples && !canProvideSamples) || (!onlyProvidesSamples && (outputStreamInfo.dwFlags & MFT_PROCESS_OUTPUT_DISCARD_WHEN_NO_BUFFER));
@@ -201,18 +199,18 @@ ComPtr<IMFSample> LoadImageAsSample(ComPtr<IStream> imageStream, IMFMediaType* s
     // If so, do the allocation
     if (mustAllocateSample)
     {
-        RETURN_IF_FAILED(MFCreateSample(&outputSample));
-        RETURN_IF_FAILED(outputSample->SetSampleDuration(333333));
+        RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(MFCreateSample(&outputSample));
+        RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(outputSample->SetSampleDuration(333333));
         IMFMediaBuffer* outputMediaBuffer = nullptr;
-        RETURN_IF_FAILED(MFCreateAlignedMemoryBuffer(outputStreamInfo.cbSize, outputStreamInfo.cbAlignment - 1, &outputMediaBuffer));
-        RETURN_IF_FAILED(outputMediaBuffer->SetCurrentLength(outputStreamInfo.cbSize));
-        RETURN_IF_FAILED(outputSample->AddBuffer(outputMediaBuffer));
+        RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(MFCreateAlignedMemoryBuffer(outputStreamInfo.cbSize, outputStreamInfo.cbAlignment - 1, &outputMediaBuffer));
+        RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(outputMediaBuffer->SetCurrentLength(outputStreamInfo.cbSize));
+        RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(outputSample->AddBuffer(outputMediaBuffer));
         outputSamples.pSample = outputSample;
     }
 
     // Finally, produce the output sample
     DWORD processStatus = 0;
-    RETURN_IF_FAILED(videoDecoder->ProcessOutput(0, 1, &outputSamples, &processStatus));
+    RETURN_NULLPTR_IF_FAILED_WITH_LOGGING(videoDecoder->ProcessOutput(0, 1, &outputSamples, &processStatus));
     if (outputSamples.pEvents)
     {
         outputSamples.pEvents->Release();
