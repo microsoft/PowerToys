b596cbc3d2c8b2d62e752d885ab5c0eb80a3e454
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-07-29 09:57:17 -0700

    Removing dependency on powertoysInterop for getting version string.

--- src/common/ManagedTelemetry/Telemetry/Events/EventBase.cs
+++ src/common/ManagedTelemetry/Telemetry/Events/EventBase.cs
@@ -1,7 +1,7 @@
-﻿using interop;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
+using System.Reflection;
 using System.Text;
 
 namespace Microsoft.PowerToys.Telemetry.Events
@@ -14,12 +14,25 @@ namespace Microsoft.PowerToys.Telemetry.Events
     {
         public bool UTCReplace_AppSessionGuid => true;
 
+        private string _version;
         public string Version
         {
             get
             {
-                return CommonManaged.GetProductVersion();
+                if (string.IsNullOrEmpty(_version))
+                {
+                    _version = GetVersionFromAssembly();
+                }
+                return _version;
             }
+        }
+
+        private string GetVersionFromAssembly()
+        {
+            // For consistency this should be formatted the same way as 
+            // https://github.com/microsoft/PowerToys/blob/710f92d99965109fd788d85ebf8b6b9e0ba1524a/src/common/common.cpp#L635
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            return $"v{version.Major}.{version.Minor}.{version.Build}";
         }
     }
 }
