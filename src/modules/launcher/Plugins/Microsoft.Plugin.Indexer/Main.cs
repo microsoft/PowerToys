90aa57a0366604804c29975bcaddde6fccb934ad
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-07-30 11:14:50 -0700

    Setting translated text to use current culture as per PR feedback.

--- src/modules/launcher/Plugins/Microsoft.Plugin.Indexer/Main.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Indexer/Main.cs
@@ -97,8 +97,8 @@ namespace Microsoft.Plugin.Indexer
                         foreach (var searchResult in searchResultsList)
                         {
                             var path = searchResult.Path;
-                            var toolTipTitle = string.Format(CultureInfo.InvariantCulture, "{0} : {1}", _context.API.GetTranslation("Microsoft_plugin_indexer_name"), searchResult.Title);
-                            var toolTipText = string.Format(CultureInfo.InvariantCulture, "{0} : {1}", _context.API.GetTranslation("Microsoft_plugin_indexer_path"), path);
+                            var toolTipTitle = string.Format(CultureInfo.CurrentCulture, "{0} : {1}", _context.API.GetTranslation("Microsoft_plugin_indexer_name"), searchResult.Title);
+                            var toolTipText = string.Format(CultureInfo.CurrentCulture, "{0} : {1}", _context.API.GetTranslation("Microsoft_plugin_indexer_path"), path);
                             string workingDir = null;
                             if (_settings.UseLocationAsWorkingDir)
                                 workingDir = Path.GetDirectoryName(path);
