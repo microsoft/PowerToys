d032956124902cf29389eb93c7bbf58d60d71403
Clint Rutkas <clint@rutkas.com>
2020-07-30 12:17:08 -0700

    adding in stylecop goodness (#5301)
    Co-authored-by: Clint Rutkas <crutkas@microsoft.com>

--- src/common/ManagedTelemetry/Telemetry/Events/DebugEvent.cs
+++ src/common/ManagedTelemetry/Telemetry/Events/DebugEvent.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Diagnostics.Tracing;
-using System.Text;
 
 namespace Microsoft.PowerToys.Telemetry.Events
 {
@@ -9,6 +10,7 @@ namespace Microsoft.PowerToys.Telemetry.Events
     public class DebugEvent : EventBase, IEvent
     {
         public string Message { get; set; }
+
         public PartA_PrivTags PartA_PrivTags => PartA_PrivTags.ProductAndServicePerformance;
     }
 }
