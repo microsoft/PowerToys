abe50a2e1b908d1b76c01e950a96b22839787546
yuyoyuppe <a.yuyoyuppe@gmail.com>
2020-08-05 16:41:37 +0300

    fixup! formatting

--- src/action_runner/action_runner.cpp
+++ src/action_runner/action_runner.cpp
@@ -112,18 +112,18 @@ bool install_new_version_stage_2(std::wstring installer_path, std::wstring_view 
     {
         // If it's not .msi, then it's our .exe installer
         SHELLEXECUTEINFOW sei{ sizeof(sei) };
-        sei.fMask = { SEE_MASK_FLAG_NO_UI | SEE_MASK_NOASYNC | SEE_MASK_NOCLOSEPROCESS | SEE_MASK_NO_CONSOLE};
+        sei.fMask = { SEE_MASK_FLAG_NO_UI | SEE_MASK_NOASYNC | SEE_MASK_NOCLOSEPROCESS | SEE_MASK_NO_CONSOLE };
         sei.lpFile = installer_path.c_str();
         sei.nShow = SW_SHOWNORMAL;
         std::wstring parameters = L"--no_full_ui";
-        if(launch_powertoys)
+        if (launch_powertoys)
         {
-          // .exe installer launches the main app by default
-          launch_powertoys = false;
+            // .exe installer launches the main app by default
+            launch_powertoys = false;
         }
         else
         {
-          parameters += L"--no_start_pt";
+            parameters += L"--no_start_pt";
         }
         sei.lpParameters = parameters.c_str();
 
