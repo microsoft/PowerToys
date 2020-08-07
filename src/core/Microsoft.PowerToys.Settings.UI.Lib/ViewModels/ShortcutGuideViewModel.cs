680039e045aa586a32bc7480df5b8e584dd34a96
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-08-07 12:00:32 -0700

    migrated shortcut guide tests

--- src/core/Microsoft.PowerToys.Settings.UI.Lib/ViewModels/ShortcutGuideViewModel.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Lib/ViewModels/ShortcutGuideViewModel.cs
@@ -2,12 +2,11 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Runtime.CompilerServices;
-using Microsoft.PowerToys.Settings.UI.Helpers;
-using Microsoft.PowerToys.Settings.UI.Lib;
-using Microsoft.PowerToys.Settings.UI.Views;
+using Microsoft.PowerToys.Settings.UI.Lib.Helpers;
 
-namespace Microsoft.PowerToys.Settings.UI.ViewModels
+namespace Microsoft.PowerToys.Settings.UI.Lib.ViewModels
 {
     public class ShortcutGuideViewModel : Observable
     {
@@ -15,7 +14,9 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
 
         private const string ModuleName = "Shortcut Guide";
 
-        public ShortcutGuideViewModel()
+        private Func<string, int> SendConfigMSG { get; }
+
+        public ShortcutGuideViewModel(Func<string, int> ipcMSGCallBackFunc)
         {
             try
             {
@@ -39,9 +40,12 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 SettingsUtils.SaveSettings(generalSettings.ToJsonString(), string.Empty);
             }
 
-            _isEnabled = generalSettings.Enabled.ShortcutGuide;
-            _pressTime = Settings.Properties.PressTime.Value;
-            _opacity = Settings.Properties.OverlayOpacity.Value;
+            // set the callback functions value to hangle outgoing IPC message.
+            SendConfigMSG = ipcMSGCallBackFunc;
+
+            this._isEnabled = generalSettings.Enabled.ShortcutGuide;
+            this._pressTime = Settings.Properties.PressTime.Value;
+            this._opacity = Settings.Properties.OverlayOpacity.Value;
 
             string theme = Settings.Properties.Theme.Value;
 
@@ -81,7 +85,7 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                     GeneralSettings generalSettings = SettingsUtils.GetSettings<GeneralSettings>(string.Empty);
                     generalSettings.Enabled.ShortcutGuide = value;
                     OutGoingGeneralSettings snd = new OutGoingGeneralSettings(generalSettings);
-                    ShellPage.DefaultSndMSGCallback(snd.ToString());
+                    SendConfigMSG(snd.ToString());
                     OnPropertyChanged("IsEnabled");
                 }
             }
@@ -166,7 +170,7 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
             OnPropertyChanged(propertyName);
             SndShortcutGuideSettings outsettings = new SndShortcutGuideSettings(Settings);
             SndModuleSettings<SndShortcutGuideSettings> ipcMessage = new SndModuleSettings<SndShortcutGuideSettings>(outsettings);
-            ShellPage.DefaultSndMSGCallback(ipcMessage.ToJsonString());
+            SendConfigMSG(ipcMessage.ToJsonString());
         }
     }
 }
---
+++ src/core/Microsoft.PowerToys.Settings.UI.UnitTests/ViewModelTests/ShortcutGuide.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.Text.Json;
+using Microsoft.PowerToys.Settings.UI.Lib;
+using Microsoft.PowerToys.Settings.UI.Lib.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ViewModelTests
+{
+    [TestClass]
+    public class ShortcutGuide
+    {
+        private const string ModuleName = "Shortcut Guide";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // initialize creation of test settings file.
+            // Test base path:
+            // C:\Users\<user name>\AppData\Local\Packages\08e1807b-8b6d-4bfa-adc4-79c64aae8e78_9abkseg265h2m\LocalState\Microsoft\PowerToys\
+            GeneralSettings generalSettings = new GeneralSettings();
+            ShortcutGuideSettings shortcutGuide = new ShortcutGuideSettings();
+
+            SettingsUtils.SaveSettings(generalSettings.ToJsonString());
+            SettingsUtils.SaveSettings(shortcutGuide.ToJsonString(), shortcutGuide.Name);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            // delete folder created.
+            string generalSettings_file_name = string.Empty;
+            if (SettingsUtils.SettingsFolderExists(generalSettings_file_name))
+            {
+                DeleteFolder(generalSettings_file_name);
+            }
+
+            if (SettingsUtils.SettingsFolderExists(ModuleName))
+            {
+                DeleteFolder(ModuleName);
+            }
+        }
+
+        public void DeleteFolder(string powertoy)
+        {
+            Directory.Delete(Path.Combine(SettingsUtils.LocalApplicationDataFolder(), $"Microsoft\\PowerToys\\{powertoy}"), true);
+        }
+
+        [TestMethod]
+        public void IsEnabled_ShouldEnableModule_WhenSuccessful()
+        {
+            // Assert
+            // Initialize mock function of sending IPC message.
+            Func<string, int> SendMockIPCConfigMSG = msg =>
+            {
+                OutGoingGeneralSettings snd = JsonSerializer.Deserialize<OutGoingGeneralSettings>(msg);
+                Assert.IsTrue(snd.GeneralSettings.Enabled.ShortcutGuide);
+                return 0;
+            };
+
+            // Arrange
+            ShortcutGuideViewModel viewModel = new ShortcutGuideViewModel(SendMockIPCConfigMSG);
+
+            // Act
+            viewModel.IsEnabled = true;
+        }
+
+        [TestMethod]
+        public void ThemeIndex_ShouldSetThemeToDark_WhenSuccessful()
+        {
+            // Assert
+            // Initialize mock function of sending IPC message.
+            Func<string, int> SendMockIPCConfigMSG = msg =>
+            {
+                ShortcutGuideSettingsIPCMessage snd = JsonSerializer.Deserialize<ShortcutGuideSettingsIPCMessage>(msg);
+                Assert.AreEqual("dark", snd.Powertoys.ShortcutGuide.Properties.Theme.Value);
+                return 0;
+            };
+
+            // Arrange
+            ShortcutGuideViewModel viewModel = new ShortcutGuideViewModel(SendMockIPCConfigMSG);
+            Assert.AreEqual(1, viewModel.ThemeIndex);
+
+            // Act
+            viewModel.ThemeIndex = 0;
+        }
+
+        [TestMethod]
+        public void PressTime_ShouldSetPressTimeToOneHundred_WhenSuccessful()
+        {
+            // Assert
+            // Initialize mock function of sending IPC message.
+            Func<string, int> SendMockIPCConfigMSG = msg =>
+            {
+                ShortcutGuideSettingsIPCMessage snd = JsonSerializer.Deserialize<ShortcutGuideSettingsIPCMessage>(msg);
+                Assert.AreEqual(100, snd.Powertoys.ShortcutGuide.Properties.PressTime.Value);
+                return 0;
+            };
+
+            // Arrange
+            ShortcutGuideViewModel viewModel = new ShortcutGuideViewModel(SendMockIPCConfigMSG);
+            Assert.AreEqual(900, viewModel.PressTime);
+
+            // Act
+            viewModel.PressTime = 100;
+        }
+
+        [TestMethod]
+        public void OverlayOpacity_ShouldSeOverlayOpacityToOneHundred_WhenSuccessful()
+        {
+            // Assert
+            // Initialize mock function of sending IPC message.
+            Func<string, int> SendMockIPCConfigMSG = msg =>
+            {
+                ShortcutGuideSettingsIPCMessage snd = JsonSerializer.Deserialize<ShortcutGuideSettingsIPCMessage>(msg);
+
+                // Serialisation not working as expected in the test project:
+                Assert.AreEqual(100, snd.Powertoys.ShortcutGuide.Properties.OverlayOpacity.Value);
+                return 0;
+            };
+
+            // Arrange
+            ShortcutGuideViewModel viewModel = new ShortcutGuideViewModel(SendMockIPCConfigMSG);
+            Assert.AreEqual(90, viewModel.OverlayOpacity);
+
+            // Act
+            viewModel.OverlayOpacity = 100;
+        }
+    }
+}
