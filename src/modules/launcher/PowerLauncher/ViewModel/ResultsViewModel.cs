2a79b68ce9698620b4418cc297dfb9e57a825a28
Divyansh Srivastava <somm14divi@gmail.com>
2020-08-10 15:01:33 -0700

    Stable sorting of ResultCollection

--- src/modules/launcher/PowerLauncher/ViewModel/ResultsViewModel.cs
+++ src/modules/launcher/PowerLauncher/ViewModel/ResultsViewModel.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,12 +20,12 @@ namespace PowerLauncher.ViewModel
     {
         #region Private Fields
 
-        public ResultCollection Results { get; }
-
         private readonly object _collectionLock = new object();
+
         private readonly Settings _settings;
 
-        // private int MaxResults => _settings?.MaxResultsToShow ?? 6;
+        #endregion
+
         public ResultsViewModel()
         {
             Results = new ResultCollection();
@@ -45,8 +46,6 @@ namespace PowerLauncher.ViewModel
                 }
             };
         }
-
-        #endregion
 
         #region Properties
 
@@ -89,6 +88,8 @@ namespace PowerLauncher.ViewModel
         public Thickness Margin { get; set; }
 
         public Visibility Visibility { get; set; } = Visibility.Hidden;
+
+        public ResultCollection Results { get; }
 
         #endregion
 
@@ -238,6 +239,14 @@ namespace PowerLauncher.ViewModel
 
             Results.AddRange(newResults);
         }
+
+        public void Sort()
+        {
+            var sorted = Results.OrderByDescending(x => x.Result.Score).ToList();
+            Results.Clear();
+            Results.AddRange(sorted);
+        }
+
         #endregion
 
         #region FormattedText Dependency Property
