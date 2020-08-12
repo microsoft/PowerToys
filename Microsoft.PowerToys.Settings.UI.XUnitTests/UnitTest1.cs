c3cc38abf69a96e0b2b8be9a754fe6bfdda5c76f
Lavius Motileng <58791731+laviusmotileng-ms@users.noreply.github.com>
2020-08-11 23:57:25 -0700

    added xunit test project

---
+++ Microsoft.PowerToys.Settings.UI.XUnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Microsoft.PowerToys.Settings.UI.XUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Sample_Xunit_Test()
+        {
+
+        }
+    }
+}
