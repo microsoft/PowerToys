ac0ba258df629bf56825cbc18c907bad69534539
Yevhenii Holovachov <yevhenii.holovachov@gmail.com>
2020-08-18 13:25:13 +0300

    Added PowerShell script to clean up PowerToys

---
+++ tools/CleanUp_tool_powershell_script/CleanUp_tool.ps1
@@ -0,0 +1,31 @@
+﻿$SettingsPath = $Env:LOCALAPPDATA + '\Microsoft\PowerToys'
+
+if (Test-Path -Path $SettingsPath -PathType Any)
+{
+    Remove-Item –Path $SettingsPath –Recurse
+}
+
+$SuperFancyZones = "HKCU:\Software\SuperFancyZones"
+$PowerRename = "HKCU:\Software\Microsoft\PowerRename"
+$ImageResizer = "HKCU:\Software\Microsoft\ImageResizer"
+$DontShowThisDialogAgain = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\DontShowMeThisDialogAgain"
+
+if (Test-Path -Path $SuperFancyZones -PathType Any)
+{
+    Remove-Item –Path $SuperFancyZones –Recurse
+}
+
+if (Test-Path -Path $PowerRename -PathType Any)
+{
+    Remove-Item –Path $PowerRename –Recurse
+}
+
+if (Test-Path -Path $ImageResizer -PathType Any)
+{
+    Remove-Item –Path $ImageResizer –Recurse
+}
+
+if (Test-Path -Path $DontShowThisDialogAgain -PathType Any)
+{
+    Remove-Item –Path $DontShowThisDialogAgain –Recurse
+}
