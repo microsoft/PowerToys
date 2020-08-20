fdd38b11167e01369a52f04212f13e6019cc7975
Yevhenii Holovachov <yevhenii.holovachov@gmail.com>
2020-08-20 16:00:39 +0300

    Added about and comments to script, fixed deleting of DontShowThisAgain directory inside registry

--- tools/CleanUp_tool_powershell_script/CleanUp_tool.ps1
+++ tools/CleanUp_tool_powershell_script/CleanUp_tool.ps1
@@ -1,4 +1,10 @@
-﻿$SettingsPath = $Env:LOCALAPPDATA + '\Microsoft\PowerToys'
+﻿#CleanUp tool 1.0
+#Copyright (C) 2020 Microsoft Corporation
+#Tool to clean PowerToys settings inside AppData folder and registry
+
+$SettingsPath = $Env:LOCALAPPDATA + '\Microsoft\PowerToys'
+
+#Deleting json settings files in %AppData%/Local/Microsoft/PowerToys.
 
 if (Test-Path -Path $SettingsPath -PathType Any)
 {
@@ -8,24 +14,32 @@ if (Test-Path -Path $SettingsPath -PathType Any)
 $SuperFancyZones = "HKCU:\Software\SuperFancyZones"
 $PowerRename = "HKCU:\Software\Microsoft\PowerRename"
 $ImageResizer = "HKCU:\Software\Microsoft\ImageResizer"
-$DontShowThisDialogAgain = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\DontShowMeThisDialogAgain"
+$DontShowThisDialogAgain = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\DontShowMeThisDialogAgain\{e16ea82f-6d94-4f30-bb02-d6d911588afd}"
+
+#Deleting SuperFancyZones registry key
 
 if (Test-Path -Path $SuperFancyZones -PathType Any)
 {
     Remove-Item –Path $SuperFancyZones –Recurse
 }
 
+#Deleting PowerRename registry key
+
 if (Test-Path -Path $PowerRename -PathType Any)
 {
     Remove-Item –Path $PowerRename –Recurse
 }
+
+#Deleting ImageResizer registry key
 
 if (Test-Path -Path $ImageResizer -PathType Any)
 {
     Remove-Item –Path $ImageResizer –Recurse
 }
 
+#Deleting DontShowThisDialogAgain registry key
+
 if (Test-Path -Path $DontShowThisDialogAgain -PathType Any)
 {
-    Remove-Item –Path $DontShowThisDialogAgain –Recurse
+    Remove-Item –Path $DontShowThisDialogAgain
 }
