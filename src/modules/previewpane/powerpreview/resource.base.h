fa2f33cb1c40e839e7e3f1654e7ffe7b27b10259
Arjun <arjunbalgovind@gmail.com>
2020-08-20 18:30:18 -0700

    Migrated resources for power preview project

---
+++ src/modules/previewpane/powerpreview/resource.base.h
@@ -0,0 +1,13 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by powerpreview.rc
+
+//////////////////////////////
+// Non-localizable
+
+#define FILE_DESCRIPTION "PowerToys PowerPreview"
+#define INTERNAL_NAME "powerpreview"
+#define ORIGINAL_FILENAME "powerpreview.dll"
+
+// Non-localizable
+//////////////////////////////
