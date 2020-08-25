2738c334aa1d59aea137eb4765ee3393bbcb16f8
Clint Rutkas <clint@rutkas.com>
2020-08-25 16:06:15 -0700

    trying to adjust stuff slightly

--- .pipelines/restore.cmd
+++ .pipelines/restore.cmd
@@ -1,5 +1,7 @@
 cd /D "%~dp0"
 
+nuget restore ../PowerToys.sln || exit /b 1
+
 powershell.exe -Command "Invoke-WebRequest -OutFile %tmp%\wdksetup.exe https://go.microsoft.com/fwlink/p/?linkid=2085767"
 %tmp%\wdksetup.exe /q
 
@@ -9,8 +11,3 @@ powershell Expand-Archive %tmp%\wdkvsix.zip -DestinationPath %tmp%\wdkvsix
 robocopy /e %tmp%\wdkvsix\$MSBuild\Microsoft\VC\v160  "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Microsoft\VC\v160"  || IF %ERRORLEVEL% LEQ 7  EXIT 0
 robocopy /e %tmp%\wdkvsix\$VCTargets "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\VC\VCTargets" || IF %ERRORLEVEL% LEQ 7  EXIT 0
 robocopy /e %tmp%\wdkvsix  "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\Extensions\lw4pmzma.r5u" || IF %ERRORLEVEL% LEQ 7  EXIT 0
-
-nuget restore ../PowerToys.sln || exit /b 1
-
-
-
