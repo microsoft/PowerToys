3bba5aa8c33113bb8498dd210be36afb454c5844
Enrico Giordani <enrico.giordani@gmail.com>
2020-08-26 21:30:37 +0200

    Update restore.cmd

--- .pipelines/restore.cmd
+++ .pipelines/restore.cmd
@@ -8,5 +8,4 @@ powershell.exe -Command "Invoke-WebRequest -OutFile %tmp%\wdksetup.exe https://g
 copy "C:\Program Files (x86)\Windows Kits\10\Vsix\VS2019\WDK.vsix" %tmp%\wdkvsix.zip
 powershell Expand-Archive %tmp%\wdkvsix.zip -DestinationPath %tmp%\wdkvsix -Force
 
-robocopy /e %tmp%\wdkvsix\$MSBuild\Microsoft\VC\v160  "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Microsoft\VC\v160"  || IF %ERRORLEVEL% LEQ 7  EXIT 0
-robocopy /e %tmp%\wdkvsix\$VCTargets "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\VC\VCTargets" || IF %ERRORLEVEL% LEQ 7  EXIT 0
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\VSIXInstaller.exe" /quiet "C:\Program Files (x86)\Windows Kits\10\Vsix\VS2019\WDK.vsix"
