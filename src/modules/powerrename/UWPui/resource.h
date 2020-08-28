d51640c2f0a2b63da39f2646766baac99315b37d
Enrico Giordani <enricogior@users.noreply.github.com>
2020-08-28 14:45:24 +0200

    [PowerRename] fix build warning (#6210)
    PowerRenameUWPUI uses the resources from powerrename\dll\Resources.resx

--- src/modules/powerrename/UWPui/resource.h
+++ src/modules/powerrename/UWPui/resource.h
@@ -2,12 +2,3 @@
 // Microsoft Visual C++ generated include file.
 // Used by PowerRenameUWPUI.rc
 
-//////////////////////////////
-// Non-localizable
-
-#define FILE_DESCRIPTION "PowerToys PowerRenameUWPUI"
-#define INTERNAL_NAME "PowerRenameUWPUI"
-#define ORIGINAL_FILENAME "PowerRenameUWPUI.exe"
-
-// Non-localizable
-//////////////////////////////
