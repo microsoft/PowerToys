7b518cf7f259cf036038b132de31b2552c83550c
Arjun <arjunbalgovind@gmail.com>
2020-09-03 11:10:54 -0700

    Tweaked fix to resolve empty ImageResizer box fix

--- src/core/Microsoft.PowerToys.Settings.UI.Lib/ImageSize.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Lib/ImageSize.cs
@@ -142,14 +142,17 @@ namespace Microsoft.PowerToys.Settings.UI.Lib
             set
             {
                 double newWidth = -1;
-                double.TryParse(value + string.Empty, out newWidth);
 
-                if (newWidth < 0)
+                if (value < 0 || double.IsNaN(value))
                 {
                     newWidth = 0;
                 }
+                else
+                {
+                    newWidth = value;
+                }
 
-                if (_width != value)
+                if (_width != newWidth)
                 {
                     _width = newWidth;
                     OnPropertyChanged();
@@ -168,14 +171,17 @@ namespace Microsoft.PowerToys.Settings.UI.Lib
             set
             {
                 double newHeight = -1;
-                double.TryParse(value + string.Empty, out newHeight);
 
-                if (newHeight < 0)
+                if (value < 0 || double.IsNaN(value))
                 {
                     newHeight = 0;
                 }
+                else
+                {
+                    newHeight = value;
+                }
 
-                if (_height != value)
+                if (_height != newHeight)
                 {
                     _height = newHeight;
                     OnPropertyChanged();
