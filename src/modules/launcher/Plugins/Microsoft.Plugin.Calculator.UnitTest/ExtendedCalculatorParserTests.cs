d0dc215bf937c4b1eda981ab48a9a2a3aa0a2bb6
p-storm <paul.de.man@gmail.com>
2020-09-04 10:16:14 +0200

    Refactored logic and made it unit testable

---
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Calculator.UnitTest/ExtendedCalculatorParserTests.cs
@@ -0,0 +1,106 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using Microsoft.Plugin.Calculator;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+using Wox.Plugin;
+
+namespace Microsoft.Plugin.Calculator.UnitTests
+{
+    [TestFixture]
+    public class ExtendedCalculatorParserTests
+    {
+        [TestCase("42")]
+        [TestCase("test")]
+        public void SingleNumberNoResult(string input)
+        {
+            // Arrange
+            var engine = new CalculateEngine();
+
+            // Act
+            var result = engine.Interpret(input);
+
+            // Assert
+            Assert.AreEqual(default(CalculateResult), result);
+        }
+
+        [TestCase("2 * 2", 4D)]
+        [TestCase("-2 ^ 2", 4D)]
+        [TestCase("-(2 ^ 2)", -4D)]
+        [TestCase("2 * pi", 6.28318530717959D)]
+        [TestCase("round(2 * pi)", 6D)]
+        [TestCase("1 == 2", default(double))]
+        [TestCase("pi * ( sin ( cos ( 2)))", -1.26995475603563D)]
+        [TestCase("5.6/2",  2.8D)]
+        [TestCase("123 * 4.56", 560.88D)]
+        [TestCase("1 - 9.0 / 10", 0.1D)]
+        [TestCase("0.5 * ((2*-395.2)+198.2)", -296.1D)]
+        [TestCase("2+2.11", 4.11D)]
+        public void ParseValues(string input, decimal expectedResult)
+        {
+            // Arrange
+            var engine = new CalculateEngine();
+
+            // Act
+            var result = engine.Interpret(input, CultureInfo.InvariantCulture);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedResult, result.Result);
+        }
+
+        [TestCase("0.100000000000000000000", 0.00776627963145224D)] // BUG: Because data structure
+        [TestCase("0.200000000000000000000000", 0.000000400752841041379D)] // BUG: Because data structure
+        [TestCase("123 456", 56088D)] // BUG: Framework accepts ' ' as multiplication
+        public void ParseValuesKnownIssues(string input, decimal expectedResult)
+        {
+            // Arrange
+            var engine = new CalculateEngine();
+
+            // Act
+            var result = engine.Interpret(input, CultureInfo.InvariantCulture);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedResult, result.Result);
+        }
+
+        [TestCase("4.5/3", 1.5D, "nl-NL")]
+        [TestCase("4.5/3", 1.5D, "en-EN")]
+        [TestCase("4.5/3", 1.5D, "de-DE")]
+        public void ParseValuesWithCulture(string input, decimal expectedResult, string cultureName)
+        {
+            // Arrange
+            var cultureInfo = CultureInfo.GetCultureInfo(cultureName);
+            var engine = new CalculateEngine();
+
+            // Act
+            var result = engine.Interpret(input, cultureInfo);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedResult, result.Result);
+        }
+
+        [TestCase("ceil(2 * (pi ^ 2))", true)]
+        [TestCase("((1 * 2)", false)]
+        [TestCase("(1 * 2)))", false)]
+        [TestCase("abcde", false)]
+        [TestCase("plot( 2 * 3)", true)]
+        public void InputValid(string input, bool valid)
+        {
+            // Arrange
+
+            // Act
+            var result = CalculateHelper.InputValid(input);
+
+            // Assert
+            Assert.AreEqual(valid, result);
+        }
+    }
+}
---
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Calculator/CalculateHelper.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Plugin.Calculator
+{
+    public static class CalculateHelper
+    {
+        private static readonly Regex RegBrackets = new Regex(@"[\(\)\[\]]", RegexOptions.Compiled);
+        private static readonly Regex RegValidExpressChar = new Regex(
+            @"^(" +
+            @"ceil|floor|exp|pi|e|max|min|det|abs|log|ln|sqrt|" +
+            @"sin|cos|tan|arcsin|arccos|arctan|" +
+            @"eigval|eigvec|eig|sum|polar|plot|round|sort|real|zeta|" +
+            @"bin2dec|hex2dec|oct2dec|" +
+            @"==|~=|&&|\|\||" +
+            @"[ei]|[0-9]|[\+\-\*\/\^\., ""]|[\(\)\|\!\[\]]" +
+            @")+$", RegexOptions.Compiled);
+
+        public static bool InputValid(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(input));
+            }
+
+            if (input.Length <= 2)
+            {
+                return false;
+            }
+
+            if (!RegValidExpressChar.IsMatch(input))
+            {
+                return false;
+            }
+
+            if (!IsBracketComplete(input))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsBracketComplete(string query)
+        {
+            var matchs = RegBrackets.Matches(query);
+            var leftBracketCount = 0;
+            foreach (Match match in matchs)
+            {
+                if (match.Value == "(" || match.Value == "[")
+                {
+                    leftBracketCount++;
+                }
+                else
+                {
+                    leftBracketCount--;
+                }
+            }
+
+            return leftBracketCount == 0;
+        }
+    }
+}
