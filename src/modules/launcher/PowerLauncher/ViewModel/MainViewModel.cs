e12a038f1f01f3f1720d90b88102a29811966323
Arjun <arjunbalgovind@gmail.com>
2020-09-11 15:38:42 -0700

    Fix for issue 3886

--- src/modules/launcher/PowerLauncher/ViewModel/MainViewModel.cs
+++ src/modules/launcher/PowerLauncher/ViewModel/MainViewModel.cs
@@ -118,6 +118,60 @@ namespace PowerLauncher.ViewModel
             }
         }
 
+        private void OpenResultsEvent(object index, bool isMouseClick)
+        {
+            var results = SelectedResults;
+
+            if (index != null)
+            {
+                results.SelectedIndex = int.Parse(index.ToString(), CultureInfo.InvariantCulture);
+            }
+
+            if (results.SelectedItem != null)
+            {
+                bool executeResultRequired = false;
+
+                if (isMouseClick)
+                {
+                    executeResultRequired = true;
+                }
+                else
+                {
+                    // If there is a context button selected fire the action for that button instead, and the main command will not be executed
+                    executeResultRequired = !results.SelectedItem.ExecuteSelectedContextButton();
+                }
+
+                if (executeResultRequired)
+                {
+                    var result = results.SelectedItem.Result;
+
+                    // SelectedItem returns null if selection is empty.
+                    if (result != null && result.Action != null)
+                    {
+                        MainWindowVisibility = Visibility.Collapsed;
+
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            result.Action(new ActionContext
+                            {
+                                SpecialKeyState = KeyboardHelper.CheckModifiers(),
+                            });
+                        });
+
+                        if (SelectedIsFromQueryResults())
+                        {
+                            _userSelectedRecord.Add(result);
+                            _history.Add(result.OriginQuery.RawQuery);
+                        }
+                        else
+                        {
+                            SelectedResults = Results;
+                        }
+                    }
+                }
+            }
+        }
+
         private void InitializeKeyCommands()
         {
             IgnoreCommand = new RelayCommand(_ => { });
@@ -181,49 +235,14 @@ namespace PowerLauncher.ViewModel
                 Process.Start("https://aka.ms/PowerToys/");
             });
 
-            OpenResultCommand = new RelayCommand(index =>
+            OpenResultWithKeyboardCommand = new RelayCommand(index =>
             {
-                var results = SelectedResults;
+                OpenResultsEvent(index, false);
+            });
 
-                if (index != null)
-                {
-                    results.SelectedIndex = int.Parse(index.ToString(), CultureInfo.InvariantCulture);
-                }
-
-                if (results.SelectedItem != null)
-                {
-                    // If there is a context button selected fire the action for that button before the main command.
-                    bool didExecuteContextButton = results.SelectedItem.ExecuteSelectedContextButton();
-
-                    if (!didExecuteContextButton)
-                    {
-                        var result = results.SelectedItem.Result;
-
-                        // SelectedItem returns null if selection is empty.
-                        if (result != null && result.Action != null)
-                        {
-                            MainWindowVisibility = Visibility.Collapsed;
-
-                            Application.Current.Dispatcher.Invoke(() =>
-                            {
-                                result.Action(new ActionContext
-                                {
-                                    SpecialKeyState = KeyboardHelper.CheckModifiers(),
-                                });
-                            });
-
-                            if (SelectedIsFromQueryResults())
-                            {
-                                _userSelectedRecord.Add(result);
-                                _history.Add(result.OriginQuery.RawQuery);
-                            }
-                            else
-                            {
-                                SelectedResults = Results;
-                            }
-                        }
-                    }
-                }
+            OpenResultWithMouseCommand = new RelayCommand(index =>
+            {
+                OpenResultsEvent(index, true);
             });
 
             LoadContextMenuCommand = new RelayCommand(_ =>
@@ -391,7 +410,9 @@ namespace PowerLauncher.ViewModel
 
         public ICommand LoadHistoryCommand { get; set; }
 
-        public ICommand OpenResultCommand { get; set; }
+        public ICommand OpenResultWithKeyboardCommand { get; set; }
+
+        public ICommand OpenResultWithMouseCommand { get; set; }
 
         public ICommand ClearQueryCommand { get; set; }
 
