b0f09405341f8315ee22ec1a5d1bea0a1f581b9c
Clint Rutkas <clint@rutkas.com>
2020-09-16 12:23:50 -0700

    marking all warnings (#6642)

--- src/common/ManagedTelemetry/Telemetry/Events/EventBase.cs
+++ src/common/ManagedTelemetry/Telemetry/Events/EventBase.cs
@@ -14,6 +14,8 @@ namespace Microsoft.PowerToys.Telemetry.Events
     [EventData]
     public class EventBase
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Part of telem, can't adjust")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Part of telem, can't adjust")]
         public bool UTCReplace_AppSessionGuid => true;
 
         private string _version;
@@ -31,11 +33,12 @@ namespace Microsoft.PowerToys.Telemetry.Events
             }
         }
 
-        private string GetVersionFromAssembly()
+        private static string GetVersionFromAssembly()
         {
             // For consistency this should be formatted the same way as
             // https://github.com/microsoft/PowerToys/blob/710f92d99965109fd788d85ebf8b6b9e0ba1524a/src/common/common.cpp#L635
             var version = Assembly.GetExecutingAssembly()?.GetName()?.Version ?? new Version();
+
             return $"v{version.Major}.{version.Minor}.{version.Build}";
         }
     }
