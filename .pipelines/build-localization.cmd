042b1c704c8b1bbd7ba2ec0d843591aab7e0c11e
Arjun <arjunbalgovind@gmail.com>
2020-09-17 11:04:55 -0700

    Changed to repo root

--- .pipelines/build-localization.cmd
+++ .pipelines/build-localization.cmd
@@ -18,7 +18,7 @@ set LocalizationXLocPkgVer=2.0.0
 echo Running localization build...
 
 set XLocPath=%NUGET_PACKAGES%\Localization.XLoc.%LocalizationXLocPkgVer%
-set LocProjectDirectory=%RepoRoot%src
+set LocProjectDirectory=%RepoRoot%
 
 rem Run the localization tool on all LocProject.json files in the src directory and it's subdirectories
 dotnet "%XLocPath%\tools\netcore\Microsoft.Localization.XLoc.dll" /f "%LocProjectDirectory%"
