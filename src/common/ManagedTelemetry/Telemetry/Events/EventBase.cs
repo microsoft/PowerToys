cadc0b3d4ce19ca7b3e7d06d9fc347c137fad63d
Arjun Balgovind <32061677+arjunbalgovind@users.noreply.github.com>
2020-09-17 12:48:01 -0700

    Revert "marking all warnings (#6642)"
    This reverts commit b0f09405341f8315ee22ec1a5d1bea0a1f581b9c.

--- src/common/ManagedTelemetry/Telemetry/Events/EventBase.cs
+++ src/common/ManagedTelemetry/Telemetry/Events/EventBase.cs
@@ -14,8 +14,6 @@ namespace Microsoft.PowerToys.Telemetry.Events
     [EventData]
     public class EventBase
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Part of telem, can't adjust")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Part of telem, can't adjust")]
         public bool UTCReplace_AppSessionGuid => true;
 
         private string _version;
@@ -33,12 +31,11 @@ namespace Microsoft.PowerToys.Telemetry.Events
             }
         }
 
-        private static string GetVersionFromAssembly()
+        private string GetVersionFromAssembly()
         {
             // For consistency this should be formatted the same way as
             // https://github.com/microsoft/PowerToys/blob/710f92d99965109fd788d85ebf8b6b9e0ba1524a/src/common/common.cpp#L635
             var version = Assembly.GetExecutingAssembly()?.GetName()?.Version ?? new Version();
-
             return $"v{version.Major}.{version.Minor}.{version.Build}";
         }
     }
