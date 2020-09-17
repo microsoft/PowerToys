2d65cf539a4462f558abc8cea799b8141eeca177
ivan100sic <ivan100sic@gmail.com>
2020-09-17 12:43:59 +0200

    Added a claryfing comment

--- src/runner/centralized_kb_hook.cpp
+++ src/runner/centralized_kb_hook.cpp
@@ -61,6 +61,7 @@ namespace CentralizedKeyboardHook
         {
             if (action())
             {
+                // Swallow the key press
                 return 1;
             }
         }
