34abad520d0e7649b0e8077df2598352bf7383db
Alekhya Reddy <reddykalekhya@gmail.com>
2020-09-18 13:10:37 -0700

    added settings repository tests

--- src/core/Microsoft.PowerToys.Settings.UI.UnitTests/ModelsTests/SettingsRepositoryTest.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.UnitTests/ModelsTests/SettingsRepositoryTest.cs
@@ -2,6 +2,10 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.PowerToys.Settings.UI.Lib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,6 +14,15 @@ namespace CommonLibTest
     [TestClass]
     public class SettingsRepositoryTest
     {
+        private Task<SettingsRepository<GeneralSettings>> GetSettingsRepository()
+        {
+            return Task.Run(() =>
+            {
+                return SettingsRepository<GeneralSettings>.Instance;
+            });
+        }
+
+
         [TestMethod]
         public void SettingsRepositoryInstanceWhenCalledMustReturnSameObject()
         {
@@ -21,6 +34,31 @@ namespace CommonLibTest
 
             // Assert
             Assert.IsTrue(object.ReferenceEquals(firstInstance, secondInstance));
+        }
+
+        [TestMethod]
+        public void SettingsRepositoryInstanceMustBeTheSameAcrossThreads()
+        {
+            // Multiple tasks try to access and initialize the settings repository class, however they must all access the same settings Repository object.
+
+            // Arrange
+            List<Task<SettingsRepository<GeneralSettings>>> settingsRepoTasks = new List<Task<SettingsRepository<GeneralSettings>>>();
+            int numberOfTasks = 100;
+
+            for(int i = 0; i < numberOfTasks; i++)
+            {
+                settingsRepoTasks.Add(GetSettingsRepository());
+            }
+
+            // Act
+            Task.WaitAll(settingsRepoTasks.ToArray());
+
+            // Assert
+            for(int i=0; i< numberOfTasks-1; i++)
+            {
+                Assert.IsTrue(object.ReferenceEquals(settingsRepoTasks[i].Result, settingsRepoTasks[i + 1].Result));
+            }
+
         }
     }
 }
