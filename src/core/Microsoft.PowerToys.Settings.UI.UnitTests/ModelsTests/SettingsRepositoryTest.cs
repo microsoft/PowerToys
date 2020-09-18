f29a6645273761e56b45b13ba7fbab48c7be9908
Alekhya Reddy <reddykalekhya@gmail.com>
2020-09-18 12:00:49 -0700

    Added test for settingsRepository that a single instance is created

---
+++ src/core/Microsoft.PowerToys.Settings.UI.UnitTests/ModelsTests/SettingsRepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.PowerToys.Settings.UI.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonLibTest
+{
+    [TestClass]
+    public class SettingsRepositoryTest
+    {
+        [TestMethod]
+        public void SettingsRepositoryInstanceWhenCalledMustReturnSameObject()
+        {
+            // The singleton class Settings Repository must always have a single instance
+
+            // Arrange and Act
+            SettingsRepository<GeneralSettings> firstInstance = SettingsRepository<GeneralSettings>.Instance;
+            SettingsRepository<GeneralSettings> secondInstance = SettingsRepository<GeneralSettings>.Instance;
+
+            // Assert
+            Assert.IsTrue(object.ReferenceEquals(firstInstance, secondInstance));
+        }
+    }
+}
