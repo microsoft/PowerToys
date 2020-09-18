830434110f9e98a1ea3744dff710a663a277c3e0
ryanbodrug-microsoft <56318517+ryanbodrug-microsoft@users.noreply.github.com>
2020-09-18 09:19:12 -0700

    Removing redifined mock

--- src/core/Microsoft.PowerToys.Settings.UI.UnitTests/ViewModelTests/ImageResizer.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.UnitTests/ViewModelTests/ImageResizer.cs
@@ -212,31 +212,5 @@ namespace ViewModelTests
             Assert.AreEqual(50, imageSize.Width);
             Assert.AreEqual(50, imageSize.Height);
         }
-
-        /// <summary>
-        /// This method mocks an IO provider to validate tests wich required saving to a file, and then reading the contents of that file, or verifying it exists
-        /// </summary>
-        /// <returns></returns>
-        Mock<IIOProvider> GetMockIOProviderForSaveLoadExists()
-        {
-            string savePath = string.Empty;
-            string saveContent = string.Empty;
-            var mockIOProvider = new Mock<IIOProvider>();
-            mockIOProvider.Setup(x => x.WriteAllText(It.IsAny<string>(), It.IsAny<string>()))
-                          .Callback<string, string>((path, content) =>
-                          {
-                              savePath = path;
-                              saveContent = content;
-                          });
-            mockIOProvider.Setup(x => x.ReadAllText(It.Is<string>(x => x.Equals(savePath, StringComparison.Ordinal))))
-                          .Returns(() => saveContent);
-
-            mockIOProvider.Setup(x => x.FileExists(It.Is<string>(x => x.Equals(savePath, StringComparison.Ordinal))))
-                          .Returns(true);
-            mockIOProvider.Setup(x => x.FileExists(It.Is<string>(x => !x.Equals(savePath, StringComparison.Ordinal))))
-                          .Returns(false);
-
-            return mockIOProvider;
-        }
     }
 }
