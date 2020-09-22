c72bfac963c64149837429f977147b0f4734f3e1
Clint Rutkas <clint@rutkas.com>
2020-09-22 16:13:46 -0700

    Getting Props in a ready state for DevEnvironment and resetting props to 0.0.1

---
+++ src/common/ManagedCommon/ApplicationTitle.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ManagedCommon
+{
+    public static class ApplicationTitle
+    {
+        public static string DevEnvironment
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(ManagedCommon.DevEnvironment.Environment))
+                {
+                    return string.Format(CultureInfo.CurrentCulture, "[{0}]", ManagedCommon.DevEnvironment.Environment);
+                }
+
+                return string.Empty;
+            }
+        }
+    }
+}
