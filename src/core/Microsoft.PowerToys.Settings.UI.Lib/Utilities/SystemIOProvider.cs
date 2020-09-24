4d898d3f54962bdf61096c2b0038132c4b0a8e84
Arjun <arjunbalgovind@gmail.com>
2020-09-24 10:40:15 -0700

    Changed to named parameter

--- src/core/Microsoft.PowerToys.Settings.UI.Lib/Utilities/SystemIOProvider.cs
+++ src/core/Microsoft.PowerToys.Settings.UI.Lib/Utilities/SystemIOProvider.cs
@@ -16,7 +16,7 @@ namespace Microsoft.PowerToys.Settings.UI.Lib.Utilities
 
         public void DeleteDirectory(string path)
         {
-            Directory.Delete(path, true);
+            Directory.Delete(path, recursive: true);
         }
 
         public bool DirectoryExists(string path)
