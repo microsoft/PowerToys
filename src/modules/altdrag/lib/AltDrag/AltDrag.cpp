d9c89b0066452172b380ee7033d64d615adffe0c
AnuthaDev <anuthadev@gmail.com>
2020-10-02 22:37:57 +0530

    Add Library Project

---
+++ src/modules/altdrag/lib/AltDrag/AltDrag.cpp
@@ -0,0 +1,10 @@
+// AltDrag.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnAltDrag()
+{
+}
