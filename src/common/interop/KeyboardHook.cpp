b8758f4eabca1995115cd9a44f736ef0ebfbff40
Alekhya Reddy <reddykalekhya@gmail.com>
2020-10-06 13:41:13 -0700

    remove new line before initializing dwextraInfo

--- src/common/interop/KeyboardHook.cpp
+++ src/common/interop/KeyboardHook.cpp
@@ -61,7 +61,6 @@ LRESULT __clrcall KeyboardHook::HookProc(int nCode, WPARAM wParam, LPARAM lParam
         KeyboardEvent ^ ev = gcnew KeyboardEvent();
         ev->message = wParam;
         ev->key = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam)->vkCode;
-
         ev->dwExtraInfo = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam)->dwExtraInfo;
 
         // Ignore the keyboard hook if either the ignore flag is set or if the FilterkeyboardEvent returns false.
