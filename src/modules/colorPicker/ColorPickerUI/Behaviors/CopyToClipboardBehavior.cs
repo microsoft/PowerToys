2a13f38c4825389392d09b45a37fa1b27515b99a
Martin Chrzan <martinchrzan@gmail.com>
2020-10-19 14:48:51 +0200

    Added animation for "color copied" message

--- src/modules/colorPicker/ColorPickerUI/Behaviors/CopyToClipboardBehavior.cs
+++ src/modules/colorPicker/ColorPickerUI/Behaviors/CopyToClipboardBehavior.cs
@@ -2,9 +2,11 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interactivity;
+using System.Windows.Media.Animation;
 using ColorPicker.Helpers;
 
 namespace ColorPicker.Behaviors
@@ -12,6 +14,9 @@ namespace ColorPicker.Behaviors
     public class CopyToClipboardBehavior : Behavior<TextBlock>
     {
         public static readonly DependencyProperty CopiedIndicationBorderProperty = DependencyProperty.Register("CopiedIndicationBorder", typeof(Border), typeof(CopyToClipboardBehavior));
+        private const int CopyIndicatorStayTimeInMs = 3000;
+        private IThrottledActionInvoker _actionInvoker;
+        private bool _copyIndicatorVisible;
 
         public Border CopiedIndicationBorder
         {
@@ -23,6 +28,7 @@ namespace ColorPicker.Behaviors
         {
             base.OnAttached();
             AssociatedObject.Loaded += AssociatedObject_Loaded;
+            _actionInvoker = Bootstrapper.Container.GetExportedValue<IThrottledActionInvoker>();
         }
 
         private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
@@ -33,9 +39,36 @@ namespace ColorPicker.Behaviors
         private void AssociatedObject_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             ClipboardHelper.CopyToClipboard(AssociatedObject.Text);
+            if (!_copyIndicatorVisible)
+            {
+                AppearCopiedIndicator();
+            }
 
-            // TODO start appear animation and hide after some time
-            CopiedIndicationBorder.Visibility = Visibility.Visible;
+            _actionInvoker.ScheduleAction(() => HideCopiedIndicator(), CopyIndicatorStayTimeInMs);
+        }
+
+        private void AppearCopiedIndicator()
+        {
+            _copyIndicatorVisible = true;
+            var opacityAppear = new DoubleAnimation(1.0, new Duration(TimeSpan.FromMilliseconds(300)));
+            opacityAppear.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseInOut };
+            var resize = new DoubleAnimation(56, new Duration(TimeSpan.FromMilliseconds(300)));
+
+            resize.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseInOut };
+            CopiedIndicationBorder.BeginAnimation(Border.OpacityProperty, opacityAppear);
+            CopiedIndicationBorder.BeginAnimation(Border.HeightProperty, resize);
+        }
+
+        private void HideCopiedIndicator()
+        {
+            _copyIndicatorVisible = false;
+            var opacityDisappear = new DoubleAnimation(0, new Duration(TimeSpan.FromMilliseconds(300)));
+            opacityDisappear.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseInOut };
+            var resize = new DoubleAnimation(0, new Duration(TimeSpan.FromMilliseconds(300)));
+
+            resize.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseInOut };
+            CopiedIndicationBorder.BeginAnimation(Border.OpacityProperty, opacityDisappear);
+            CopiedIndicationBorder.BeginAnimation(Border.HeightProperty, resize);
         }
     }
 }
