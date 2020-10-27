7398d87262d00524d77c92bb9397c8e01d08c9c0
Alekhya Reddy <reddykalekhya@gmail.com>
2020-10-27 11:44:29 -0700

    removed interface from the namespace due to stylecop

--- src/modules/launcher/Plugins/Microsoft.Plugin.Indexer/DriveDetection/DriveInfoWrapper.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Indexer/DriveDetection/DriveInfoWrapper.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.IO;
-using Microsoft.Plugin.Indexer.Interface;
 
 namespace Microsoft.Plugin.Indexer.DriveDetection
 {
--- src/modules/launcher/Plugins/Microsoft.Plugin.Indexer/Interface/IDriveInfoWrapper.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Indexer/Interface/IDriveInfoWrapper.cs
@@ -2,7 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace Microsoft.Plugin.Indexer.Interface
+namespace Microsoft.Plugin.Indexer
 {
     public interface IDriveInfoWrapper
     {
