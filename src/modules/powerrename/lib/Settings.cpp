481c6fb69cfda73bd2faced41b6d18b0e3739284
Raphael Horber <raphael.horber@gmail.com>
2020-10-27 22:50:58 +0100

    Settings: Read useBoostLib from JSON file

--- src/modules/powerrename/lib/Settings.cpp
+++ src/modules/powerrename/lib/Settings.cpp
@@ -31,6 +31,7 @@ namespace
     const wchar_t c_mruEnabled[] = L"MRUEnabled";
     const wchar_t c_mruList[] = L"MRUList";
     const wchar_t c_insertionIdx[] = L"InsertionIdx";
+    const wchar_t c_useBoostLib[] = L"UseBoostLib";
 
     unsigned int GetRegNumber(const std::wstring& valueName, unsigned int defaultValue)
     {
@@ -414,6 +415,7 @@ void CSettings::Save()
     jsonData.SetNamedValue(c_maxMRUSize,              json::value(settings.maxMRUSize));
     jsonData.SetNamedValue(c_searchText,              json::value(settings.searchText));
     jsonData.SetNamedValue(c_replaceText,             json::value(settings.replaceText));
+    jsonData.SetNamedValue(c_useBoostLib,             json::value(settings.useBoostLib));
 
     json::to_file(jsonFilePath, jsonData);
     GetSystemTimeAsFileTime(&lastLoadedTime);
@@ -457,6 +459,7 @@ void CSettings::MigrateFromRegistry()
     settings.flags                   = GetRegNumber(c_flags, 0);
     settings.searchText              = GetRegString(c_searchText, L"");
     settings.replaceText             = GetRegString(c_replaceText, L"");
+    settings.useBoostLib             = false; // Never existed in registry, disabled by default.
 }
 
 void CSettings::ParseJson()
@@ -498,6 +501,10 @@ void CSettings::ParseJson()
             if (json::has(jsonSettings, c_replaceText, json::JsonValueType::String))
             {
                 settings.replaceText = jsonSettings.GetNamedString(c_replaceText);
+            }
+            if (json::has(jsonSettings, c_useBoostLib, json::JsonValueType::Boolean))
+            {
+                settings.useBoostLib = jsonSettings.GetNamedBoolean(c_useBoostLib);
             }
         }
         catch (const winrt::hresult_error&) { }
