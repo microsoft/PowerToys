13f3421d3fd9993607eec144bf38fa9b39d71d80
Martin Chrzan <martinchrzan@gmail.com>
2020-10-27 16:08:30 +0100

    Added missed file

---
+++ src/modules/colorPicker/ColorPickerUI/Models/ColorFormatModel.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Windows.Media;
+
+namespace ColorPicker.Models
+{
+    public class ColorFormatModel
+    {
+        public string FormatName { get; set; }
+
+        public Func<Color, string> Convert { get; set; }
+    }
+}
