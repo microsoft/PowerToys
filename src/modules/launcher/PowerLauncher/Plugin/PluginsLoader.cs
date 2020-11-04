21c16656784d6086b24fef4ad90522a9de622d34
Davide <davide.giacometti@outlook.it>
2020-11-04 19:57:55 +0100

    fix release build

--- src/modules/launcher/PowerLauncher/Plugin/PluginsLoader.cs
+++ src/modules/launcher/PowerLauncher/Plugin/PluginsLoader.cs
@@ -5,9 +5,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Loader;
 using Wox.Infrastructure;
 using Wox.Plugin;
+using Wox.Plugin.Logger;
 
 namespace PowerLauncher.Plugin
 {
