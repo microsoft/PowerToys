9aafaa020fd9a02ed00622dbc46e3d8be0c5ae77
vldmr11080 z <vldmr11080@gmail.com>
2020-11-06 14:36:49 +0100

    Add name property for delete buton

--- src/modules/fancyzones/editor/FancyZonesEditor/Properties/Resources.Designer.cs
+++ src/modules/fancyzones/editor/FancyZonesEditor/Properties/Resources.Designer.cs
@@ -142,6 +142,15 @@ namespace FancyZonesEditor.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Delete custom layout.
+        /// </summary>
+        public static string Custom_Layout_Delete_Button {
+            get {
+                return ResourceManager.GetString("Custom_Layout_Delete_Button", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Custom table layout creator.
         /// </summary>
         public static string Custom_Table_Layout {
