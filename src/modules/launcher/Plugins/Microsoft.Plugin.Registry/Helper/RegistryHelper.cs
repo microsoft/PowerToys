ea55841edfa16ab6babaf5cda8da5501c85fca4b
Sekan, Tobias <tobias.sekan@startmail.com>
2020-11-09 15:22:45 +0100

    Add registry plugin (not finished)

---
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Registry/Helper/RegistryHelper.cs
@@ -0,0 +1,175 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Win32;
+
+namespace Microsoft.Plugin.Registry.Helper
+{
+    #pragma warning disable CA1031 // Do not catch general exception types
+
+    internal static class RegistryHelper
+    {
+        private static readonly IReadOnlyDictionary<string, RegistryKey?> _mainKeys = new Dictionary<string, RegistryKey?>(13)
+        {
+            { "HKEY_", null },
+            { "HKCR", Win32.Registry.ClassesRoot },
+            { Win32.Registry.ClassesRoot.Name, Win32.Registry.ClassesRoot },
+            { "HKCC", Win32.Registry.CurrentConfig },
+            { Win32.Registry.CurrentConfig.Name, Win32.Registry.CurrentConfig },
+            { "HKCU", Win32.Registry.CurrentUser },
+            { Win32.Registry.CurrentUser.Name, Win32.Registry.CurrentUser },
+            { "HKLM", Win32.Registry.LocalMachine },
+            { Win32.Registry.LocalMachine.Name, Win32.Registry.LocalMachine },
+            { "HKPD", Win32.Registry.PerformanceData },
+            { Win32.Registry.PerformanceData.Name, Win32.Registry.PerformanceData },
+            { "HKU", Win32.Registry.Users },
+            { Win32.Registry.Users.Name, Win32.Registry.Users },
+        };
+
+        /// <summary>
+        /// Try to find a registery key based on the given serach query
+        /// </summary>
+        /// <param name="query">The query to search</param>
+        /// <returns>A combination of the main <see cref="RegistryKey"/> and the sub keys</returns>
+        internal static (RegistryKey? mainKey, string subKey) GetRegisteryKey(in string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return (null, "Search is empty");
+            }
+
+            var mainKey = query.Split('\\').FirstOrDefault();
+            var subKey = query.TrimStart('\\').Replace(mainKey, string.Empty, StringComparison.InvariantCultureIgnoreCase);
+            var mainKeyResult = _mainKeys.FirstOrDefault(found => found.Key.StartsWith(mainKey, StringComparison.InvariantCultureIgnoreCase));
+
+            return (mainKeyResult.Value, subKey);
+        }
+
+        /// <summary>
+        /// Return a list of all registery main key
+        /// </summary>
+        /// <returns>A list with a key-value-pair of all registery main key and possible exceptions</returns>
+        internal static ICollection<(string, RegistryKey?, Exception?)> GetAllMainKeys()
+            => new Collection<(string, RegistryKey?, Exception?)>
+            {
+                (Win32.Registry.ClassesRoot.Name, null, null),
+                (Win32.Registry.CurrentConfig.Name, null, null),
+                (Win32.Registry.CurrentUser.Name, null, null),
+                (Win32.Registry.LocalMachine.Name, null, null),
+                (Win32.Registry.PerformanceData.Name, null, null),
+                (Win32.Registry.Users.Name, null, null),
+            };
+
+        internal static ICollection<(string, RegistryKey?, Exception?)> SerachForKey(in RegistryKey mainKey, in string subKeyPath)
+        {
+            Debug.WriteLine($"Serarch for {mainKey.Name}\\{subKeyPath}\n");
+
+            var subKeysNames = subKeyPath.Split('\\');
+            var index = 0;
+            var subKey = mainKey;
+
+            ICollection<(string, RegistryKey?, Exception?)> result;
+
+            do
+            {
+                var subKeyName = subKeysNames.ElementAtOrDefault(index);
+                result = FindKey(subKey, subKeyName);
+
+                if (result.Count == 0)
+                {
+                    return GetAllSubKeys(mainKey);
+                }
+
+                if (result.Count == 1 && index < subKeysNames.Length)
+                {
+                    subKey = result.First().Item2;
+                }
+
+                if (subKey == null)
+                {
+                    break;
+                }
+
+                index++;
+            }
+            while (index < subKeysNames.Length);
+
+            return result;
+        }
+
+        internal static string GetKeyResult(in RegistryKey subKey)
+            => $"Sub-keys: {subKey.SubKeyCount} - Values: {subKey.ValueCount}";
+
+        internal static void OpenRegisteryKey(string fullKey)
+        {
+            // it's impossible to direct open a key via command-line option, so we must override the last remembert key
+            Win32.Registry.SetValue(@"HKEY_Current_User\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit", "LastKey", fullKey);
+
+            // -m => allow multi-instance (hidden start option)
+            Process.Start("regedit.exe", "-m");
+        }
+
+        private static ICollection<(string, RegistryKey?, Exception?)> FindKey(RegistryKey parentkey, string searchSubKey)
+        {
+            var list = new Collection<(string, RegistryKey?, Exception?)>();
+
+            Debug.WriteLine($"Search for {searchSubKey} in {parentkey.Name}");
+
+            try
+            {
+                foreach (var subKey in parentkey.GetSubKeyNames())
+                {
+                    if (subKey.StartsWith(searchSubKey, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        try
+                        {
+                            list.Add(($"{parentkey.Name}\\{subKey}", parentkey.OpenSubKey(subKey), null));
+                        }
+                        catch (Exception exception)
+                        {
+                            list.Add(($"{parentkey.Name}\\{subKey}", null, exception));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                list.Add((parentkey.Name, null, ex));
+            }
+
+            return list;
+        }
+
+        private static ICollection<(string, RegistryKey?, Exception?)> GetAllSubKeys(in RegistryKey key, in int maxCount = 50)
+        {
+            var list = new Collection<(string, RegistryKey?, Exception?)>();
+
+            try
+            {
+                foreach (var subKey in key.GetSubKeyNames())
+                {
+                    list.Add(($"{key.Name}\\{subKey}", key, null));
+
+                    if (list.Count > maxCount)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                list.Add(($"{key.Name}", null, exception));
+            }
+
+            return list;
+        }
+    }
+
+    #pragma warning restore CA1031 // Do not catch general exception types
+}
---
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Registry/Main.cs
@@ -0,0 +1,127 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using Microsoft.Plugin.Registry.Helper;
+using Microsoft.Win32;
+using Wox.Plugin;
+
+namespace Microsoft.Plugin.Registry
+{
+    // TEST:
+    // Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\Shell\Bags\31\Shell\{5C4F28B5-F869-4E84-8E60-F11DB97C5CC7}
+    // hklm/so/mi/wi/sh/ba/31/sh/{5c
+
+    // Finished:
+    // - shortcuts for main keys (e.g. HKLM)
+    // - support search for full main keys not only shortcuts
+    // - show all root keys when a user type only a part of "HKEY"
+    // - match keys they start with shortcut + search
+    // - list of found keys
+    // - result of found keys
+    // - auto replace "/" with "\"
+    // - always case-intensitive
+    // - make as PT plugin
+
+    // TODO:
+    // - reduce used of strings, use RegisterKey instead
+    // - avoid use of tuples use key value instead
+    // - simple key-walker with full keys
+    // - extended key-walker with only parts of the keys
+    // - command: open direct in regedit.exe
+    // - command: copy to clipboard
+    // - multi-language
+    // - cache results ?
+    // - benchmark
+    // - unittests
+    public class Main : IPlugin
+    {
+        public void Init(PluginInitContext context)
+        {
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var search = query?.Search.Replace('/', '\\') ?? string.Empty;
+
+            ICollection<(string, RegistryKey?, Exception?)> list = new Collection<(string, RegistryKey?, Exception?)>();
+
+            var (mainKey, path) = RegistryHelper.GetRegisteryKey(search);
+
+            if (mainKey is null && search.StartsWith("HKEY", StringComparison.InvariantCultureIgnoreCase))
+            {
+                list = RegistryHelper.GetAllMainKeys();
+            }
+            else if (!(mainKey is null))
+            {
+                list = RegistryHelper.SerachForKey(mainKey, path);
+            }
+
+            return list.Count == 0
+                ? new List<Result> { new Result { Title = $"{query?.Search} Key not found" } }
+                : PrepareResults(list);
+        }
+
+        private static List<Result> PrepareResults(ICollection<(string, RegistryKey?, Exception?)> list)
+        {
+            var resultList = new List<Result>();
+
+            foreach (var item in list)
+            {
+                var result = new Result();
+
+                if (item.Item3 is null && !(item.Item2 is null))
+                {
+                    // when key contains keys or fields
+                    result.Title = item.Item2.Name;
+                    result.SubTitle = RegistryHelper.GetKeyResult(item.Item2);
+                }
+                else if (item.Item2 is null && !(item.Item3 is null))
+                {
+                    // on error (e.g access denied)
+                    result.Title = item.Item1;
+                    result.SubTitle = item.Item3.Message;
+                }
+                else
+                {
+                    result.Title = item.Item1;
+                }
+
+                result.Action = action =>
+                {
+                    try
+                    {
+                        RegistryHelper.OpenRegisteryKey(result.Title);
+                        return true;
+                    }
+                    catch (System.ComponentModel.Win32Exception)
+                    {
+                        MessageBox.Show(
+                            "You have not enought rights to open the Registry Editor",
+                            "Error on open Registry Editor",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+                    }
+#pragma warning disable CA1031 // Do not catch general exception types
+                    catch (Exception exception)
+                    {
+                        MessageBox.Show(exception.ToString(), "Error on open Registry Editor", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+#pragma warning restore CA1031 // Do not catch general exception types
+
+                    return false;
+                };
+
+                resultList.Add(result);
+            }
+
+            return resultList;
+        }
+    }
+}
