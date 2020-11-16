d67e787020dff9e9ea9eb05cc015e737e9e777cc
Benjamin Hooper <me@benhooper.org>
2020-11-16 00:09:56 -0600

    Fix Styling for Microsoft.Plugin.Sys.UnitTests

--- src/modules/launcher/Plugins/Microsoft.Plugin.Sys.UnitTests/QueryTests.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Sys.UnitTests/QueryTests.cs
@@ -1,7 +1,11 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Linq;
-using NUnit.Framework;
 using Mono.Collections.Generic;
 using Moq;
+using NUnit.Framework;
 using Wox.Infrastructure;
 using Wox.Plugin;
 
@@ -12,7 +16,7 @@ namespace Microsoft.Plugin.Sys.UnitTests
         [SetUp]
         public void Setup()
         {
-	        StringMatcher.Instance = new StringMatcher();
+            StringMatcher.Instance = new StringMatcher();
         }
 
         [TestCase("shutdown", "Shutdown Computer")]
@@ -21,15 +25,15 @@ namespace Microsoft.Plugin.Sys.UnitTests
         [TestCase("lock", "Lock this Computer")]
         [TestCase("sleep", "Put computer to sleep")]
         [TestCase("hibernate", "Hibernate computer")]
-        [TestCase("empty recycle", "Empty recycle bin")] 
+        [TestCase("empty recycle", "Empty recycle bin")]
         public void QueryResults(string typedString, string expectedResult)
         {
             // Setup
             Mock<Main> main = new Mock<Main>();
             string[] terms = { typedString };
             Query expectedQuery = new Query(typedString, typedString, new ReadOnlyCollection<string>(terms), string.Empty);
-            // Act
 
+            // Act
             var result = main.Object.Query(expectedQuery).FirstOrDefault().SubTitle;
 
             // Assert
