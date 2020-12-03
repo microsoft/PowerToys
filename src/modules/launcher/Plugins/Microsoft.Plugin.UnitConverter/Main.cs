58126e37c35827fb245f77b22ace77104277f922
ThiefZero <ss_jimmy@hotmail.com>
2020-12-03 18:55:55 +0100

    Update csproj, add UnitConversion package, WIP

--- src/modules/launcher/Plugins/Microsoft.Plugin.UnitConverter/Main.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.UnitConverter/Main.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows;
+using UnitConversion;
 using Wox.Infrastructure.Logger;
 using Wox.Plugin;
 
@@ -13,8 +14,9 @@ namespace Microsoft.Plugin.UnitConverter
     public class Main : IPlugin, /*IPluginI18n,*/ IDisposable
     {
         private PluginInitContext _context;
-        private static string _warningIconPath;
+        private static string _icon_path;
         private bool _disposed;
+        private DistanceConverter _ft_to_m = new DistanceConverter("ft", "m");
 
         public void Init(PluginInitContext context) {
             if (context == null) {
@@ -22,7 +24,6 @@ namespace Microsoft.Plugin.UnitConverter
             }
 
             _context = context;
-
             _context.API.ThemeChanged += OnThemeChanged;
             UpdateIconPath(_context.API.GetCurrentTheme());
         }
@@ -32,9 +33,35 @@ namespace Microsoft.Plugin.UnitConverter
                 throw new ArgumentNullException(paramName: nameof(query));
             }
 
-            System.Diagnostics.Debug.WriteLine(string.Format("works {0}", query));
+            string[] split = query.Search.Split(' ');
+            if (split.Length < 4) {
+                return new List<Result>();
+            }
 
-            return new List<Result>();
+            double ans = _ft_to_m.LeftToRight(double.Parse(split[0]));
+
+            return new List<Result>        {                new Result {
+                Title = string.Format("{0} {1}", ans.ToString(), split[3]),
+                IcoPath = _icon_path,
+                Score = 300,
+                SubTitle = "Copy to clipboard", //Context.API.GetTranslation("wox_plugin_calculator_copy_number_to_clipboard"),
+                Action = c => {
+                    var ret = false;
+                    var thread = new Thread(() => {
+                        try {
+                            Clipboard.SetText(ans.ToString());
+                            ret = true;
+                        }
+                        catch (ExternalException) {
+                            MessageBox.Show("Copy failed, please try later");
+                        }
+                    });
+                    thread.SetApartmentState(ApartmentState.STA);
+                    thread.Start();
+                    thread.Join();
+                    return ret;
+                },
+            },};
         }
 
         private void OnThemeChanged(Theme _, Theme newTheme) {
@@ -43,10 +70,10 @@ namespace Microsoft.Plugin.UnitConverter
 
         private static void UpdateIconPath(Theme theme) {
             if (theme == Theme.Light || theme == Theme.HighContrastWhite) {
-                _warningIconPath = "Images/Warning.light.png";
+                _icon_path = "Images/Warning.light.png";
             }
             else {
-                _warningIconPath = "Images/Warning.dark.png";
+                _icon_path = "Images/Warning.dark.png";
             }
         }
 
