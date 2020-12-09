26f8d49d7813ec3361502ca6b56be433b77eeb29
Clint Rutkas <clint@rutkas.com>
2020-12-08 16:43:45 -0800

    running VS script

---
+++ .pipelines/restore-dependencies.cmd
@@ -0,0 +1,19 @@
+cd "%ProgramFiles(x86)%\Microsoft Visual Studio\2019"
+SET targetFolder="\"
+IF EXIST Preview\NUL (SET targetFolder=Preview)
+IF EXIST Enterprise\NUL (SET targetFolder=Enterprise)
+IF EXIST Professional\NUL (SET targetFolder=Professional)
+IF EXIST Community\NUL (SET targetFolder=Community)
+
+ECHO %targetFolder%
+
+"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe" ^
+modify --installpath "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\%targetFolder%" ^
+--add Microsoft.VisualStudio.Workload.NativeDesktop ^
+--add Microsoft.VisualStudio.Workload.ManagedDesktop ^
+--add Microsoft.VisualStudio.Workload.Universal ^
+--add Microsoft.VisualStudio.Component.Windows10SDK.17134 ^
+--add Microsoft.VisualStudio.ComponentGroup.UWP.VC ^
+--add Microsoft.VisualStudio.Component.VC.Runtimes.x86.x64.Spectre ^
+--add Microsoft.VisualStudio.Component.VC.ATL.Spectre ^
+--quiet
