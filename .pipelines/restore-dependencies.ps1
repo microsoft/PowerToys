15b1d19c6b4f71b457f424be1b7ad411329137a7
Clint Rutkas <clint@rutkas.com>
2020-12-09 12:12:40 -0800

    trying to break up

--- .pipelines/restore-dependencies.ps1
+++ .pipelines/restore-dependencies.ps1
@@ -16,7 +16,7 @@ try
     # https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2019
     # https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples?view=vs-2019#using---wait
 
-    $process = Start-Process -FilePath $vsInstallerPath -ArgumentList "modify", "--installPath `"${vsInstallPath}`"", "--add Microsoft.VisualStudio.Workload.NativeDesktop", "--add  Microsoft.VisualStudio.Workload.ManagedDesktop", "--add Microsoft.VisualStudio.Workload.Universal", "--add Microsoft.VisualStudio.Component.Windows10SDK.17134", "--add Microsoft.VisualStudio.ComponentGroup.UWP.VC", "--add Microsoft.VisualStudio.Component.VC.Runtimes.x86.x64.Spectre", "--add Microsoft.VisualStudio.Component.VC.ATL.Spectre", "--quiet" -Wait -PassThru
+    $process = Start-Process -FilePath $vsInstallerPath -ArgumentList "modify", "--installPath `"${vsInstallPath}`"", "--add Microsoft.VisualStudio.Workload.NativeDesktop", "--add  Microsoft.VisualStudio.Workload.ManagedDesktop", "--add Microsoft.VisualStudio.Workload.Universal", "--add Microsoft.VisualStudio.ComponentGroup.UWP.VC", "--add Microsoft.VisualStudio.Component.VC.Runtimes.x86.x64.Spectre", "--add Microsoft.VisualStudio.Component.VC.ATL.Spectre", "--quiet" -Wait -PassThru
     Write-Host $process.ExitCode 
 }
 finally
