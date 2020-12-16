53aea7c706bc0c1c48c2e79292a812b879c00d46
ThiefZero <ss_jimmy@hotmail.com>
2020-12-16 21:45:27 +0100

    Unitconversion supports double unabbreviated unitnames

--- src/modules/launcher/Plugins/Microsoft.Plugin.UnitConverter/Main.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.UnitConverter/Main.cs
@@ -1,15 +1,14 @@
-﻿using System;
+﻿using ManagedCommon;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows;
 using UnitsNet;
 using UnitsNet.Units;
-using Wox.Infrastructure.Logger;
 using Wox.Plugin;
-
+using System.Diagnostics;
 
 namespace Microsoft.Plugin.UnitConverter
 {
@@ -44,44 +43,75 @@ namespace Microsoft.Plugin.UnitConverter
                 return new List<Result>();
             }
 
+            string input_first_unit = split[1];//.ToLower();
+            string input_second_unit = split[3];//.ToLower();
+            double converted = -1;
+
             List<Result> final_list = new List<Result>();
 
             foreach (QuantityType quantity_type in _included) {
                 QuantityInfo unit_info = Quantity.GetInfo(quantity_type);
-
                 bool first_unit_recognized = UnitParser.Default.TryParse(split[1], unit_info.UnitType, out Enum first_unit);
                 bool second_unit_recognized = UnitParser.Default.TryParse(split[3], unit_info.UnitType, out Enum _);
 
-                if (first_unit_recognized && second_unit_recognized) {
-                    double converted = UnitsNet.UnitConverter.ConvertByAbbreviation(int.Parse(split[0]), unit_info.Name, split[1], split[3]);
+                // 3 types of matches:
+                // a) 10 ft in cm (double abbreviation)
+                // b) 10 feet in centimeter (double unabbreviated)
+                // c) 10 feet in cm (single abbreviation
 
-                    // answer found, add result to list
-                    final_list.Add(new Result {
-                        Title = string.Format("{0} {1}", converted, split[3]),
-                        IcoPath = _icon_path,
-                        Score = 300,
-                        SubTitle = "Copy to clipboard", //Context.API.GetTranslation("wox_plugin_calculator_copy_number_to_clipboard"),
-                        Action = c => {
-                            var ret = false;
-                            var thread = new Thread(() => {
-                                try {
-                                    Clipboard.SetText(converted.ToString());
-                                    ret = true;
-                                }
-                                catch (ExternalException) {
-                                    MessageBox.Show("Copy failed, please try later");
-                                }
-                            });
-                            thread.SetApartmentState(ApartmentState.STA);
-                            thread.Start();
-                            thread.Join();
-                            return ret;
-                        }
-                    });
+                if (first_unit_recognized && second_unit_recognized) {
+                    // a
+                    converted = UnitsNet.UnitConverter.ConvertByAbbreviation(int.Parse(split[0]), unit_info.Name, input_first_unit, input_second_unit);
+                    AddToResult(final_list, converted, split[3]);
+                }
+                else if ((first_unit_recognized && !second_unit_recognized) || (!first_unit_recognized && second_unit_recognized)) {
+                    // c
+                    if (first_unit_recognized) {
+                        // get first unabbreviated, convert first to second
+                    }
+                    else if (second_unit_recognized) {
+                        // get second unabbreviated, convert first to second
+                    }
+                }
+                else if ((!first_unit_recognized) && (!second_unit_recognized)) {
+                    // b
+                    bool first_unabbreviated = Array.Exists(unit_info.UnitInfos, unitName => unitName.Name.ToLower() == input_first_unit);
+                    bool second_unabbreviated = Array.Exists(unit_info.UnitInfos, unitName => unitName.Name.ToLower() == input_second_unit);
+
+                    if (first_unabbreviated && second_unabbreviated) {
+                        converted = UnitsNet.UnitConverter.ConvertByName(double.Parse(split[0]), unit_info.Name, input_first_unit, input_second_unit);
+                        AddToResult(final_list, converted, split[3]);
+                    }
                 }
             }
 
             return final_list;
+        }
+
+        private void AddToResult(List<Result> currentList, double converted_value, string unit_name) {
+            // answer found, add result to list
+            currentList.Add(new Result {
+                Title = string.Format("{0} {1}", converted_value, unit_name),
+                IcoPath = _icon_path,
+                Score = 300,
+                SubTitle = "Copy to clipboard", //Context.API.GetTranslation("wox_plugin_calculator_copy_number_to_clipboard"),
+                Action = c => {
+                    var ret = false;
+                    var thread = new Thread(() => {
+                        try {
+                            Clipboard.SetText(converted_value.ToString());
+                            ret = true;
+                        }
+                        catch (ExternalException) {
+                            MessageBox.Show("Copy failed, please try later");
+                        }
+                    });
+                    thread.SetApartmentState(ApartmentState.STA);
+                    thread.Start();
+                    thread.Join();
+                    return ret;
+                }
+            });
         }
 
         private void OnThemeChanged(Theme _, Theme newTheme) {
