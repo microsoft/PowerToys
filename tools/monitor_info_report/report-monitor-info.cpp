321a722b8e0204ee41b1c944ee040478e6898f2a
Mykhailo Pylyp <pylypsmol@gmail.com>
2020-12-22 12:27:28 +0200

    Bug report tool (#8633)
    * added diagnostic tool project
    
    * Add copy to a temp folder, escape private info and zip it
    
    * Added diagnostic tool to installer
    
    * zip folder path as cmd argument
    
    * renamed project to BugReportTool
    
    * do not use precompile headers for release
    
    * Added zip lib
    
    * Added license
    
    * Delete old zip source files
    
    * Use fork version while PR is not merged
    
    * fix spelling
    
    * exclude deps folder from spell checking
    
    * exclude only zip
    
    * removed redundant configuration from zip project
    
    * fix spelling
    
    * Add error handling to implementation
    
    * Added build of BugReportTool.sln to pipeline
    
    * Delete redundant info from BugReportTool.vcxproj
    
    * Deleted submodule
    
    * Added submodule
    
    * fix build
    
    * Restore nuget packages for BugReportTool.sln on CI
    
    * spelling fix
    
    * Use SettingsAPI
    
    * changed git submodule
    
    * added new sensitive info
    
    * Removed zip project
    
    * use json.h, add date to zipfolder, handle zip is not created
    
    * fix spelling
    
    * delete bad_alloc catch
    
    * add new sensative info
    
    * report monitor info
    
    * report windows version
    
    * fix spelling
    
    * delete platform specific configuration
    
    * fix output

---
+++ tools/monitor_info_report/report-monitor-info.cpp
@@ -0,0 +1,60 @@
+#pragma once
+#include "report-monitor-info.h"
+#include <Windows.h>
+#include "../../src/common/utils/winapi_error.h"
+
+int report(std::wostream& os)
+{
+    auto callback = [](HMONITOR monitor, HDC, RECT*, LPARAM prm) -> BOOL {
+        std::wostream& os = *(std::wostream*)prm;
+        MONITORINFOEX mi;
+        mi.cbSize = sizeof(mi);
+        if (GetMonitorInfo(monitor, &mi))
+        {
+            os << "GetMonitorInfo OK\n";
+            DISPLAY_DEVICE displayDevice = { sizeof(displayDevice) };
+
+            if (EnumDisplayDevices(mi.szDevice, 0, &displayDevice, 1))
+            {
+                if (displayDevice.StateFlags & DISPLAY_DEVICE_MIRRORING_DRIVER)
+                {
+                    os << "EnumDisplayDevices OK[MIRRORING_DRIVER]: \n"
+                        << "\tDeviceID = " << displayDevice.DeviceID << '\n'
+                        << "\tDeviceKey = " << displayDevice.DeviceKey << '\n'
+                        << "\tDeviceName = " << displayDevice.DeviceName << '\n'
+                        << "\tDeviceString = " << displayDevice.DeviceString << '\n';
+                }
+                else
+                {
+                    os << "EnumDisplayDevices OK:\n"
+                        << "\tDeviceID = " << displayDevice.DeviceID << '\n'
+                        << "\tDeviceKey = " << displayDevice.DeviceKey << '\n'
+                        << "\tDeviceName = " << displayDevice.DeviceName << '\n'
+                        << "\tDeviceString = " << displayDevice.DeviceString << '\n';
+                }
+            }
+            else
+            {
+                auto message = get_last_error_message(GetLastError());
+                os << "EnumDisplayDevices FAILED: " << (message.has_value() ? message.value() : L"") << '\n';
+            }
+        }
+        else
+        {
+            auto message = get_last_error_message(GetLastError());
+            os << "GetMonitorInfo FAILED: " << (message.has_value() ? message.value() : L"") << '\n';
+        }
+        return TRUE;
+    };
+
+    if (EnumDisplayMonitors(nullptr, nullptr, callback, (LPARAM)&os))
+    {
+        os << "EnumDisplayMonitors OK\n";
+    }
+    else
+    {
+        auto message = get_last_error_message(GetLastError());
+        os << "EnumDisplayMonitors FAILED: " << (message.has_value() ? message.value() : L"") << '\n';
+    }
+    return 0;
+}
---
+++ tools/monitor_info_report/report-monitor-info.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <fstream>
+
+int report(std::wostream& os);
