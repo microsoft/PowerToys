0258ad2a6cc42b570f2f3374a80bbf57b5f86d11
Robert de Veen <github@robertdeveen.com>
2021-01-13 20:57:03 +0100

    Rename folder for Markdown unit tests. Add unit test for pdf preview handler

---
+++ src/modules/previewpane/UnitTests-PdfPreviewHandler/PdfPreviewHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Windows.Forms;
+using Microsoft.PowerToys.PreviewHandler.Pdf;
+using Microsoft.PowerToys.STATestExtension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PdfPreviewHandlerUnitTests
+{
+    [STATestClass]
+    public class PdfPreviewHandlerTest
+    {
+        [TestMethod]
+        public void PdfPreviewHandlerControlAddsPdfViewerToFormWhenDoPreviewIsCalled()
+        {
+            // Arrange
+            using (var pdfPreviewHandlerControl = new PdfPreviewHandlerControl())
+            {
+                // Act
+                pdfPreviewHandlerControl.DoPreview<string>("HelperFiles/dummy.pdf");
+
+                // Assert
+                Assert.AreEqual(1, pdfPreviewHandlerControl.Controls.Count);
+                Assert.IsInstanceOfType(pdfPreviewHandlerControl.Controls[0], typeof(PdfiumViewer.PdfViewer));
+            }
+        }
+    }
+}
