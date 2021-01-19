ec68307abdd75570a8f6069de9fe097069f6b1c0
mykhailopylyp <17161067+mykhailopylyp@users.noreply.github.com>
2021-01-19 17:29:17 +0200

    delete QueryHistory.json from report

--- tools/BugReportTool/BugReportTool/Main.cpp
+++ tools/BugReportTool/BugReportTool/Main.cpp
@@ -19,15 +19,15 @@ using namespace winrt::Windows::Data::Json;
 
 map<wstring, vector<wstring>> escapeInfo = {
     { L"FancyZones\\app-zone-history.json", { L"app-zone-history/app-path" } },
-    { L"FancyZones\\settings.json", { L"properties/fancyzones_excluded_apps" } },
-    { L"PowerToys Run\\Settings\\QueryHistory.json", { L"Items/Query" }}
+    { L"FancyZones\\settings.json", { L"properties/fancyzones_excluded_apps" } }
 };
 
 vector<wstring> filesToDelete = {
     L"PowerToys Run\\Cache",
     L"PowerRename\\replace-mru.json",
     L"PowerRename\\search-mru.json",
-    L"PowerToys Run\\Settings\\UserSelectedRecord.json"
+    L"PowerToys Run\\Settings\\UserSelectedRecord.json",
+    L"PowerToys Run\\Settings\\QueryHistory.json"
 };
 
 vector<wstring> getXpathArray(wstring xpath)
