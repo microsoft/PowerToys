aadf19db8cb8d25fdfb935872ea935a83ef6e057
Mykhailo Pylyp <pylypsmol@gmail.com>
2021-01-22 16:59:37 +0200

    Update src/modules/launcher/Microsoft.Launcher/dllmain.cpp
    Co-authored-by: Enrico Giordani <enricogior@users.noreply.github.com>

--- src/modules/launcher/Microsoft.Launcher/dllmain.cpp
+++ src/modules/launcher/Microsoft.Launcher/dllmain.cpp
@@ -372,7 +372,7 @@ void Microsoft_Launcher::parse_hotkey(PowerToysSettings::PowerToyValues& setting
             m_hotkey.alt = jsonHotkeyObject.GetNamedBoolean(JSON_KEY_ALT);
             m_hotkey.shift = jsonHotkeyObject.GetNamedBoolean(JSON_KEY_SHIFT);
             m_hotkey.ctrl = jsonHotkeyObject.GetNamedBoolean(JSON_KEY_CTRL);
-            m_hotkey.key = static_cast<unsigned char>(jsonHotkeyObject.GetNamedNumber(JSON_KEY_CODE));        
+            m_hotkey.key = static_cast<unsigned char>(jsonHotkeyObject.GetNamedNumber(JSON_KEY_CODE));
         }
         catch(...)
         {
