77c8db8bc45972c01be38c8f53f2de92eddc22bf
Serafima <zykovas91@gmail.com>
2021-02-09 09:55:42 +0300

    added oobe window

---
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI.Runner/OobeWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Windows;
+
+namespace Microsoft.PowerToys.Settings.UI.Runner
+{
+    /// <summary>
+    /// Interaction logic for OobeWindow.xaml
+    /// </summary>
+    public partial class OobeWindow : Window
+    {
+        public OobeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/OobePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.PowerToys.Settings.UI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class OobePage : Page
+    {
+        public OobePage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
