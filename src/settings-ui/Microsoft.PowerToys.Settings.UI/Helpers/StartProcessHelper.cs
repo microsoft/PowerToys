19ee066087f176d451619d994e2fc4440d5061d4
Serafima <zykovas91@gmail.com>
2021-02-09 11:30:34 +0300

    added helper

---
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/Helpers/StartProcessHelper.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+
+namespace Microsoft.PowerToys.Settings.UI.Helpers
+{
+    public static class StartProcessHelper
+    {
+        public const string ColorsSettings = "ms-settings:colors";
+
+        public static void Start(string process)
+        {
+            Process.Start(new ProcessStartInfo(process) { UseShellExecute = true });
+        }
+    }
+}
