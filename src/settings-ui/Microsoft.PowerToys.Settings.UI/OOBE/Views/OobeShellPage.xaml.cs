dff10838c11fc9689589240377a5a2a6352c39f2
Serafima <zykovas91@gmail.com>
2021-02-12 10:01:30 +0300

    oobe shell page

---
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/OOBE/Views/OobeShellPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.PowerToys.Settings.UI.OOBE.Views
+{
+    public sealed partial class OobeShellPage : UserControl
+    {
+        public OobeShellPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
