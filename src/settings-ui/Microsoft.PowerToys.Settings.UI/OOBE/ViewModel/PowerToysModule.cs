b4f05a26506a11f78ed0960342324f48ec5714b7
Serafima <zykovas91@gmail.com>
2021-02-12 18:13:21 +0300

    pt module

---
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/OOBE/ViewModel/PowerToysModule.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.PowerToys.Settings.UI.OOBE.ViewModel
+{
+    public class PowerToysModule
+    {
+        public string Name { get; set; }
+
+        public string Tag { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public string Image { get; set; }
+
+        public int NavIndex { get; set; }
+
+        public string Icon { get; set; }
+
+        public string FluentIcon { get; set; }
+    }
+}
