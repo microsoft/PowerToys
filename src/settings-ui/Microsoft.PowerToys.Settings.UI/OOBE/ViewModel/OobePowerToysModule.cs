1b0b9f44eacb286330006dc41dc483265c77d6e9
Serafima <zykovas91@gmail.com>
2021-02-15 12:42:11 +0300

    added gif source

--- src/settings-ui/Microsoft.PowerToys.Settings.UI/OOBE/ViewModel/OobePowerToysModule.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/OOBE/ViewModel/OobePowerToysModule.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation
+// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -20,6 +20,8 @@ namespace Microsoft.PowerToys.Settings.UI.OOBE.ViewModel
 
         public string FluentIcon { get; set; }
 
+        public string GifSource { get; set; }
+
         public OobePowerToysModule()
         {
         }
@@ -38,6 +40,7 @@ namespace Microsoft.PowerToys.Settings.UI.OOBE.ViewModel
             NavIndex = other.NavIndex;
             Icon = other.Icon;
             FluentIcon = other.FluentIcon;
+            GifSource = other.GifSource;
         }
     }
 }
--- src/settings-ui/Microsoft.PowerToys.Settings.UI/OOBE/Views/OobeShellPage.xaml.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/OOBE/Views/OobeShellPage.xaml.cs
@@ -33,15 +33,95 @@ namespace Microsoft.PowerToys.Settings.UI.OOBE.Views
 
             Modules = new ObservableCollection<OobePowerToysModule>();
 
-            Modules.Insert((int)PowerToysModulesEnum.ColorPicker, new OobePowerToysModule() { ModuleName = "Color Picker", Tag = "ColorPicker", IsNew = false, NavIndex = 0, Icon = "\uEF3C", Image = "ms-appx:///Assets/Modules/ColorPicker.png", FluentIcon = "ms-appx:///Assets/FluentIcons/ColorPicker.png" });
-            Modules.Insert((int)PowerToysModulesEnum.FancyZones, new OobePowerToysModule() { ModuleName = "FancyZones", Tag = "FancyZones", IsNew = false, NavIndex = 1, Icon = "\uE737", Image = "ms-appx:///Assets/Modules/FancyZones.png", FluentIcon = "ms-appx:///Assets/FluentIcons/FancyZones.png" });
-            Modules.Insert((int)PowerToysModulesEnum.ImageResizer, new OobePowerToysModule() { ModuleName = "ImageResizer", Tag = "ImageResizer", IsNew = false, NavIndex = 2, Icon = "\uEB9F", Image = "ms-appx:///Assets/Modules/ImageResizer.png", FluentIcon = "ms-appx:///Assets/FluentIcons/ImageResizer.png" });
-            Modules.Insert((int)PowerToysModulesEnum.KBM, new OobePowerToysModule() { ModuleName = "KBM", Tag = "KBM", IsNew = false, NavIndex = 0, Icon = "\uE765", Image = "ms-appx:///Assets/Modules/KBM.png", FluentIcon = "ms-appx:///Assets/FluentIcons/KBM.png" });
-            Modules.Insert((int)PowerToysModulesEnum.Run, new OobePowerToysModule() { ModuleName = "PowerToys Run", Tag = "Run", IsNew = false, NavIndex = 2, Icon = "\uE773", Image = "ms-appx:///Assets/Modules/PowerLauncher.png", FluentIcon = "ms-appx:///Assets/FluentIcons/ColorPicker.png" });
-            Modules.Insert((int)PowerToysModulesEnum.FileExplorer, new OobePowerToysModule() { ModuleName = "File explorer add-ons", Tag = "FileExplorer", IsNew = false, NavIndex = 1, Icon = "\uEC50", Image = "ms-appx:///Assets/Modules/PowerPreview.png", FluentIcon = "ms-appx:///Assets/FluentIcons/FancyZones.png" });
-            Modules.Insert((int)PowerToysModulesEnum.PowerRename, new OobePowerToysModule() { ModuleName = "PowerRename", Tag = "PowerRename", IsNew = false, NavIndex = 2, Icon = "\uE8AC", Image = "ms-appx:///Assets/Modules/PowerRename.png", FluentIcon = "ms-appx:///Assets/FluentIcons/PowerRename.png" });
-            Modules.Insert((int)PowerToysModulesEnum.ShortcutGuide, new OobePowerToysModule() { ModuleName = "Shortcut Guide", Tag = "ShortcutGuide", IsNew = false, NavIndex = 0, Icon = "\uEDA7", Image = "ms-appx:///Assets/Modules/ShortcutGuide.png", FluentIcon = "ms-appx:///Assets/FluentIcons/ImageResizer.png" });
-            Modules.Insert((int)PowerToysModulesEnum.VideoConference, new OobePowerToysModule() { ModuleName = "Video Conference", Tag = "VideoConference", IsNew = true, NavIndex = 1, Icon = "\uEC50", Image = "ms-appx:///Assets/Modules/VideoConference.png", FluentIcon = "ms-appx:///Assets/FluentIcons/FancyZones.png" });
+            Modules.Insert((int)PowerToysModulesEnum.ColorPicker, new OobePowerToysModule()
+            {
+                ModuleName = "Color Picker",
+                Tag = "ColorPicker",
+                IsNew = false,
+                NavIndex = 0,
+                Icon = "\uEF3C",
+                Image = "ms-appx:///Assets/Modules/ColorPicker.png",
+                FluentIcon = "ms-appx:///Assets/FluentIcons/ColorPicker.png",
+                GifSource = "https://raw.githubusercontent.com/wiki/microsoft/PowerToys/images/colorpicker/ColorPicking.gif",
+            });
+            Modules.Insert((int)PowerToysModulesEnum.FancyZones, new OobePowerToysModule()
+            {
+                ModuleName = "FancyZones",
+                Tag = "FancyZones",
+                IsNew = false,
+                NavIndex = 1,
+                Icon = "\uE737",
+                Image = "ms-appx:///Assets/Modules/FancyZones.png",
+                FluentIcon = "ms-appx:///Assets/FluentIcons/FancyZones.png",
+                GifSource = "https://user-images.githubusercontent.com/9866362/101410242-5b90a280-38df-11eb-834a-8365453b8429.gif",
+            });
+            Modules.Insert((int)PowerToysModulesEnum.ImageResizer, new OobePowerToysModule()
+            {
+                ModuleName = "ImageResizer",
+                Tag = "ImageResizer",
+                IsNew = false,
+                NavIndex = 2,
+                Icon = "\uEB9F",
+                Image = "ms-appx:///Assets/Modules/ImageResizer.png",
+                FluentIcon = "ms-appx:///Assets/FluentIcons/ImageResizer.png",
+            });
+            Modules.Insert((int)PowerToysModulesEnum.KBM, new OobePowerToysModule()
+            {
+                ModuleName = "KBM",
+                Tag = "KBM",
+                IsNew = false,
+                NavIndex = 0,
+                Icon = "\uE765",
+                Image = "ms-appx:///Assets/Modules/KBM.png",
+                FluentIcon = "ms-appx:///Assets/FluentIcons/KBM.png",
+            });
+            Modules.Insert((int)PowerToysModulesEnum.Run, new OobePowerToysModule()
+            {
+                ModuleName = "PowerToys Run",
+                Tag = "Run",
+                IsNew = false,
+                NavIndex = 2,
+                Icon = "\uE773",
+                Image = "ms-appx:///Assets/Modules/PowerLauncher.png",
+                GifSource = "https://raw.githubusercontent.com/wiki/microsoft/PowerToys/images/Launcher/QuickStart.gif",
+            });
+            Modules.Insert((int)PowerToysModulesEnum.FileExplorer, new OobePowerToysModule()
+            {
+                ModuleName = "File explorer add-ons",
+                Tag = "FileExplorer",
+                IsNew = false,
+                NavIndex = 1,
+                Icon = "\uEC50",
+                Image = "ms-appx:///Assets/Modules/PowerPreview.png",
+            });
+            Modules.Insert((int)PowerToysModulesEnum.PowerRename, new OobePowerToysModule()
+            {
+                ModuleName = "PowerRename",
+                Tag = "PowerRename",
+                IsNew = false,
+                NavIndex = 2,
+                Icon = "\uE8AC",
+                Image = "ms-appx:///Assets/Modules/PowerRename.png",
+                FluentIcon = "ms-appx:///Assets/FluentIcons/PowerRename.png",
+            });
+            Modules.Insert((int)PowerToysModulesEnum.ShortcutGuide, new OobePowerToysModule()
+            {
+                ModuleName = "Shortcut Guide",
+                Tag = "ShortcutGuide",
+                IsNew = false,
+                NavIndex = 0,
+                Icon = "\uEDA7",
+                Image = "ms-appx:///Assets/Modules/ShortcutGuide.png",
+            });
+            Modules.Insert((int)PowerToysModulesEnum.VideoConference, new OobePowerToysModule()
+            {
+                ModuleName = "Video Conference",
+                Tag = "VideoConference",
+                IsNew = true,
+                NavIndex = 1,
+                Icon = "\uEC50",
+                Image = "ms-appx:///Assets/Modules/VideoConference.png",
+            });
         }
 
         private void UserControl_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
