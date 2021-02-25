aa2d49e107a964bbfe9b7b298f3c76d884acaac0
Mykhailo Pylyp <pylypsmol@gmail.com>
2021-02-25 12:01:46 +0200

    fix navigation from additional panel (#9878)

--- src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/ViewModels/PowerLauncherPluginViewModel.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/ViewModels/PowerLauncherPluginViewModel.cs
@@ -130,6 +130,21 @@ namespace Microsoft.PowerToys.Settings.UI.Library.ViewModels
 
         public string IconPath { get => isDark() ? settings.IconPathDark : settings.IconPathLight; }
 
+        private bool _showAdditionalInfoPanel;
+
+        public bool ShowAdditionalInfoPanel
+        {
+            get => _showAdditionalInfoPanel;
+            set
+            {
+                if (value != _showAdditionalInfoPanel)
+                {
+                    _showAdditionalInfoPanel = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
