3135ae56a59a4494d7afb50ee36cc28e0df03c11
Clare DuVal <ckduval@hotmail.com>
2021-03-01 21:44:25 -0500

    Add camel case testing

--- src/modules/powerrename/unittests/PowerRenameManagerTests.cpp
+++ src/modules/powerrename/unittests/PowerRenameManagerTests.cpp
@@ -270,6 +270,16 @@ namespace PowerRenameManagerTests
             RenameHelper(renamePairs, ARRAYSIZE(renamePairs), L"foo", L"bar", SYSTEMTIME{ 2020, 7, 3, 22, 15, 6, 42, 453 }, DEFAULT_FLAGS | Titlecase);
         }
 
+        TEST_METHOD (VerifyCamelcaseTransform)
+        {
+            rename_pairs renamePairs[] = {
+                { L"foo and the to", L"bar And The To", false, true, 0 },
+                { L"Test", L"test_norename", false, false, 0 }
+            };
+
+            RenameHelper(renamePairs, ARRAYSIZE(renamePairs), L"foo", L"bar", SYSTEMTIME{ 2020, 7, 3, 22, 15, 6, 42, 453 }, DEFAULT_FLAGS | Camelcase);
+        }
+
         TEST_METHOD (VerifyNameOnlyTransform)
         {
             rename_pairs renamePairs[] = {
