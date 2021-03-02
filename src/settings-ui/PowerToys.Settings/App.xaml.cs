42f9b9902f355ba8b4861b3334cc4aef20b7e90c
Enrico Giordani <enrico.giordani@gmail.com>
2021-03-02 13:03:23 +0100

    [oobe] init settings window when oobe starts

--- src/settings-ui/PowerToys.Settings/App.xaml.cs
+++ src/settings-ui/PowerToys.Settings/App.xaml.cs
@@ -29,6 +29,26 @@ namespace PowerToys.Settings
             settingsWindow.NavigateToSection(type);
         }
 
+        private void InitHiddenSettingsWindow()
+        {
+            settingsWindow = new MainWindow();
+
+            // To avoid visual flikering, show the window with a size of 0,0
+            // and don't show it in the taskbar
+            var originalHight = settingsWindow.Height;
+            var originalWidth = settingsWindow.Width;
+            settingsWindow.Height = 0;
+            settingsWindow.Width = 0;
+            settingsWindow.ShowInTaskbar = false;
+
+            settingsWindow.Show();
+            settingsWindow.Hide();
+
+            settingsWindow.Height = originalHight;
+            settingsWindow.Width = originalWidth;
+            settingsWindow.ShowInTaskbar = true;
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             if (!ShowOobe)
@@ -40,8 +60,13 @@ namespace PowerToys.Settings
             {
                 PowerToysTelemetry.Log.WriteEvent(new OobeStartedEvent());
 
-                OobeWindow otherWindow = new OobeWindow();
-                otherWindow.Show();
+                // Create the Settings window so that it's fully initialized and
+                // it will be ready to receive the notification if the user opens
+                // the Settings from the tray icon.
+                InitHiddenSettingsWindow();
+
+                OobeWindow oobeWindow = new OobeWindow();
+                oobeWindow.Show();
             }
         }
     }
