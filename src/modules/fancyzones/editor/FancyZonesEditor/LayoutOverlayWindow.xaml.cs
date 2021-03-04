f7e2fa52634907ffee64991a6e3d74ba7bae503f
Enrico Giordani <enricogior@users.noreply.github.com>
2021-03-04 13:49:29 +0100

    [FZ Editor] hide tool windows from alt-tab (#10011)

--- src/modules/fancyzones/editor/FancyZonesEditor/LayoutOverlayWindow.xaml.cs
+++ src/modules/fancyzones/editor/FancyZonesEditor/LayoutOverlayWindow.xaml.cs
@@ -2,9 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Windows;
-using System.Windows.Media;
 
 namespace FancyZonesEditor
 {
@@ -16,6 +14,11 @@ namespace FancyZonesEditor
         public LayoutOverlayWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Utils.NativeMethods.SetWindowStyleToolWindow(this);
         }
     }
 }
