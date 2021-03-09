f3ff3870bd8c93d60d11351593e9e6ad9d0a5a7b
Serafima <zykovas91@gmail.com>
2021-03-09 15:08:44 +0300

    add fast-access-layout-keys to the json schema

---
+++ src/modules/fancyzones/editor/FancyZonesEditor/Models/FastAccessKeysModel.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace FancyZonesEditor.Models
+{
+    public class FastAccessKeysModel
+    {
+        public static List<int> FreeKeys { get; } = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        public static SortedDictionary<int, string> SelectedKeys { get; } = new SortedDictionary<int, string>();
+
+        public FastAccessKeysModel()
+        {
+        }
+
+        public static void FreeKey(int key)
+        {
+            SelectedKeys.Remove(key);
+            FreeKeys.Add(key);
+        }
+
+        public static bool SelectKey(int key, string uuid)
+        {
+            if (SelectedKeys.ContainsKey(key))
+            {
+                return false;
+            }
+
+            FreeKeys.Remove(key);
+            SelectedKeys.Add(key, uuid);
+            return true;
+        }
+    }
+}
