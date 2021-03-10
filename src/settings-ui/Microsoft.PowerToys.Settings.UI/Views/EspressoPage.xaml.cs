1840cc9b62b402cab19ceda9be81ad6af494c62e
Den Delimarsky <1389609+dend@users.noreply.github.com>
2021-03-10 08:16:34 -0800

    Getting the settings UI to show Espresso.

---
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/Views/EspressoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO.Abstractions;
+using Microsoft.PowerToys.Settings.UI.Library;
+using Microsoft.PowerToys.Settings.UI.Library.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.PowerToys.Settings.UI.Views
+{
+    public sealed partial class EspressoPage : Page
+    {
+        public EspressoPage()
+        {
+            var settingsUtils = new SettingsUtils();
+            InitializeComponent();
+        }
+    }
+}
