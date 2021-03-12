ab16b02f87e59dc9067a0be0ebc9e4339babef12
Ivan Stošić <ivan100sic@gmail.com>
2021-03-12 13:28:49 +0100

    Add indentation

--- src/modules/fancyzones/lib/CallTracer.cpp
+++ src/modules/fancyzones/lib/CallTracer.cpp
@@ -1,20 +1,39 @@
 #include "pch.h"
 #include "CallTracer.h"
+#include <thread>
 
 namespace
 {
     // Non-localizable
     const std::string entering = " Enter";
     const std::string exiting = " Exit";
+
+    std::mutex indentLevelMutex;
+    std::map<std::thread::id, int> indentLevel;
+
+    std::string GetIndentation()
+    {
+        std::unique_lock lock(indentLevelMutex);
+        int level = indentLevel[std::this_thread::get_id()];
+
+        if (level == 0)
+        {
+            return {};
+        }
+        else
+        {
+            return std::string(2 * min(level, 64) - 1, ' ') + " - ";
+        }
+    }
 }
 
 CallTracer::CallTracer(const char* functionName) :
     functionName(functionName)
-{
-    Logger::trace((functionName + entering).c_str());
+{  
+    Logger::trace((GetIndentation() + functionName + entering).c_str());
 }
 
 CallTracer::~CallTracer()
 {
-    Logger::trace((functionName + exiting).c_str());
+    Logger::trace((GetIndentation() + functionName + exiting).c_str());
 }
