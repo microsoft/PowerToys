32984dc1265a9a4f78e9fa5492e9292d97559f2b
Den Delimarsky <1389609+dend@users.noreply.github.com>
2021-03-24 07:42:15 -0700

    Add property storage capabilities

---
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/EspressoProperties.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Microsoft.PowerToys.Settings.UI.Library
+{
+    public class EspressoProperties
+    {
+        public EspressoProperties()
+        {
+            IsEnabled = new BoolProperty();
+            KeepDisplayOn = new BoolProperty();
+            Mode = EspressoMode.INDEFINITE;
+            TimeAllocation = new IntProperty();
+        }
+
+        [JsonPropertyName("espresso_is_enabled")]
+        public BoolProperty IsEnabled { get; set; }
+
+        [JsonPropertyName("espresso_keep_display_on")]
+        public BoolProperty KeepDisplayOn { get; set; }
+
+        [JsonPropertyName("espresso_mode")]
+        public EspressoMode Mode { get; set; }
+
+        [JsonPropertyName("espresso_time_allocation")]
+        public IntProperty TimeAllocation { get; set; }
+    }
+
+    public enum EspressoMode
+    {
+        INDEFINITE = 0,
+        TIMED = 1,
+    }
+}
---
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/EspressoSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Microsoft.PowerToys.Settings.UI.Library.Interfaces;
+
+namespace Microsoft.PowerToys.Settings.UI.Library
+{
+    public class EspressoSettings : BasePTModuleSettings, ISettingsConfig
+    {
+        public const string ModuleName = "Espresso";
+        public const string ModuleVersion = "1.0.0";
+
+        public EspressoSettings()
+        {
+            Name = ModuleName;
+            Version = ModuleVersion;
+            Properties = new EspressoProperties();
+        }
+
+        [JsonPropertyName("properties")]
+        public EspressoProperties Properties { get; set; }
+
+        public string GetModuleName()
+        {
+            return Name;
+        }
+
+        public bool UpgradeSettingsConfiguration()
+        {
+            return false;
+        }
+    }
+}
