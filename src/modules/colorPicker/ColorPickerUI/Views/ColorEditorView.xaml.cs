84bd02531944ae5742a145269566b1124894d3a1
mykhailopylyp <17161067+mykhailopylyp@users.noreply.github.com>
2021-03-24 16:08:14 +0200

    Fix spelling

--- src/modules/colorPicker/ColorPickerUI/Views/ColorEditorView.xaml.cs
+++ src/modules/colorPicker/ColorPickerUI/Views/ColorEditorView.xaml.cs
@@ -18,7 +18,7 @@ namespace ColorPicker.Views
         private void HistoryColors_ItemClick(object sender, ModernWpf.Controls.ItemClickEventArgs e)
         {
             // Note: it does not handle clicking on the same color.
-            // More appropriate event would be SelectionChanged but we can not distinguish beetween user action and program action inside of it.
+            // More appropriate event would be SelectionChanged but we can not distinguish between user action and program action inside of it.
             SessionEventHelper.Event.EditorPickedColorFromHistory = true;
         }
     }
