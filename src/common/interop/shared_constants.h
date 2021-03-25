718289c115de77542ea54bfdaaa9deda503ac820
Mykhailo Pylyp <pylypsmol@gmail.com>
2021-03-25 16:04:56 +0200

    Settings telemetry for ColorPicker (#10417)

--- src/common/interop/shared_constants.h
+++ src/common/interop/shared_constants.h
@@ -17,6 +17,8 @@ namespace CommonSharedConstants
 
     const wchar_t RUN_SEND_SETTINGS_TELEMETRY_EVENT[] = L"Local\\PowerToysRunInvokeEvent-638ec522-0018-4b96-837d-6bd88e06f0d6";
 
+    const wchar_t COLOR_PICKER_SEND_SETTINGS_TELEMETRY_EVENT[] = L"Local\\ColorPickerSettingsTelemetryEvent-6c7071d8-4014-46ec-b687-913bd8a422f1";
+
     // Path to the event used to show Color Picker
     const wchar_t SHOW_COLOR_PICKER_SHARED_EVENT[] = L"Local\\ShowColorPickerEvent-8c46be2a-3e05-4186-b56b-4ae986ef2525";
 
