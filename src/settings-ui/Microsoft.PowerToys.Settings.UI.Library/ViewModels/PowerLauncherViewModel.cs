a91f74d6486e5c61107015c9ee195b8ff6df2f99
Mykhailo Pylyp <pylypsmol@gmail.com>
2021-03-31 21:54:10 +0300

    fix glitch (#10517)

--- src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/ViewModels/PowerLauncherViewModel.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/ViewModels/PowerLauncherViewModel.cs
@@ -262,11 +262,9 @@ namespace Microsoft.PowerToys.Settings.UI.Library.ViewModels
                 if (value == true)
                 {
                     settings.Properties.Position = StartupPosition.Cursor;
+                    _isCursorPositionRadioButtonChecked = value;
+                    UpdateSettings();
                 }
-
-                _isCursorPositionRadioButtonChecked = value;
-
-                UpdateSettings();
             }
         }
 
@@ -282,11 +280,9 @@ namespace Microsoft.PowerToys.Settings.UI.Library.ViewModels
                 if (value == true)
                 {
                     settings.Properties.Position = StartupPosition.PrimaryMonitor;
+                    _isPrimaryMonitorPositionRadioButtonChecked = value;
+                    UpdateSettings();
                 }
-
-                _isPrimaryMonitorPositionRadioButtonChecked = value;
-
-                UpdateSettings();
             }
         }
 
@@ -302,11 +298,9 @@ namespace Microsoft.PowerToys.Settings.UI.Library.ViewModels
                 if (value == true)
                 {
                     settings.Properties.Position = StartupPosition.Focus;
+                    _isFocusPositionRadioButtonChecked = value;
+                    UpdateSettings();
                 }
-
-                _isFocusPositionRadioButtonChecked = value;
-
-                UpdateSettings();
             }
         }
 
