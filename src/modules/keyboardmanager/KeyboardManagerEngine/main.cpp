7d2adbd2e1047cc04dd9b6643b90ac256aeab83d
mykhailopylyp <17161067+mykhailopylyp@users.noreply.github.com>
2021-04-02 17:12:05 +0300

    Copy engine logic into KeyboardManagerEngine project

---
+++ src/modules/keyboardmanager/KeyboardManagerEngine/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include <common/utils/window.h>
+#include "KeyboardManager.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    init_apartment();
+    auto kbm = KeyboardManager();
+    kbm.start_lowlevel_keyboard_hook();
+    run_message_loop();
+    kbm.stop_lowlevel_keyboard_hook();
+}
---
+++ src/modules/keyboardmanager/KeyboardManagerEngine/pch.cpp
@@ -0,0 +1,1 @@
+﻿#include "pch.h"
