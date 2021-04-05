f5adf7a96eb3089c677e687caaa4d564dbfb137d
Sima <zykovas91@gmail.com>
2021-04-05 20:04:06 +0300

    try-catch inside of the loop

--- src/common/utils/logger_helper.h
+++ src/common/utils/logger_helper.h
@@ -30,26 +30,26 @@ namespace LoggerHelpers
 
     inline bool delete_other_versions_log_folders(std::wstring_view appPath, const std::filesystem::path& currentVersionLogFolder)
     {
-        try
-        {
-            std::filesystem::path logFolderPath(appPath);
-            logFolderPath.append(LogSettings::logPath);
+        bool result = true;
+        std::filesystem::path logFolderPath(appPath);
+        logFolderPath.append(LogSettings::logPath);
 
-            for (const auto& dir : std::filesystem::directory_iterator(logFolderPath))
+        for (const auto& dir : std::filesystem::directory_iterator(logFolderPath))
+        {
+            if (dir != currentVersionLogFolder)
             {
-                if (dir != currentVersionLogFolder)
+                try
                 {
                     std::filesystem::remove_all(dir);
                 }
+                catch (std::filesystem::filesystem_error& e)
+                {
+                    Logger::error("Failed to delete previous version log folder: {}", e.what());
+                    result = false;
+                }                
             }
-
-            return true;
-        }
-        catch (std::filesystem::filesystem_error& e)
-        {
-            Logger::error("Failed to delete previous version log folder: {}", e.what());
         }
 
-        return false;
+        return result;
     }
 }
