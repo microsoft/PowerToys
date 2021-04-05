c093c08935385ecad3e2e1bd23d069044ffe8b7d
mykhailopylyp <17161067+mykhailopylyp@users.noreply.github.com>
2021-04-05 13:28:33 +0300

    Rename SettingsWatcher to SettingsReader

--- src/modules/launcher/PowerLauncher/App.xaml.cs
+++ src/modules/launcher/PowerLauncher/App.xaml.cs
@@ -37,7 +37,7 @@ namespace PowerLauncher
         private ThemeManager _themeManager;
         private SettingWindowViewModel _settingsVM;
         private StringMatcher _stringMatcher;
-        private SettingsWatcher _settingsWatcher;
+        private SettingsReader _settingsReader;
 
         [STAThread]
         public static void Main()
@@ -103,7 +103,8 @@ namespace PowerLauncher
                 _mainVM = new MainViewModel(_settings);
                 _mainWindow = new MainWindow(_settings, _mainVM);
                 API = new PublicAPIInstance(_settingsVM, _mainVM, _themeManager);
-                _settingsWatcher = new SettingsWatcher(_settings, _themeManager);
+                _settingsReader = new SettingsReader(_settings, _themeManager);
+                _settingsReader.ReadSettings();
 
                 PluginManager.InitializePlugins(API);
 
@@ -114,6 +115,8 @@ namespace PowerLauncher
                 HttpClient.Proxy = _settings.Proxy;
 
                 RegisterExitEvents();
+
+                _settingsReader.ReadSettingsOnChange();
 
                 _mainVM.MainWindowVisibility = Visibility.Visible;
                 _mainVM.ColdStartFix();
