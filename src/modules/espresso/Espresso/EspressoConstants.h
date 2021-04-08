afbddfc23bf3c3c891532f2970623c99bbfe6f44
Den Delimarsky <1389609+dend@users.noreply.github.com>
2021-04-08 10:47:28 -0700

    Updating proper references.

---
+++ src/modules/espresso/Espresso/EspressoConstants.h
@@ -0,0 +1,7 @@
+#include <string>
+
+namespace EspressoConstants
+{
+    // Name of the powertoy module.
+    inline const std::wstring ModuleKey = L"Espresso";
+}
--- src/modules/espresso/Espresso/dllmain.cpp
+++ src/modules/espresso/Espresso/dllmain.cpp
@@ -1,9 +1,22 @@
 #include "pch.h"
-#include <interface/powertoy_module_interface.h>
-#include <interface/lowlevel_keyboard_event_data.h>
-#include <interface/win_hook_event_data.h>
-#include <common/settings_objects.h>
+#include "resource.h"
 #include "trace.h"
+#include "EspressoConstants.h"
+
+#include <interface/powertoy_module_interface.h>
+
+#include <common/SettingsAPI/settings_objects.h>
+#include <common/interop/shared_constants.h>
+#include <common/logger/logger.h>
+#include <common/SettingsAPI/settings_helpers.h>
+#include <common/utils/elevation.h>
+#include <common/utils/process_path.h>
+#include <common/utils/resources.h>
+#include <common/utils/os-detect.h>
+#include <common/utils/winapi_error.h>
+
+#include <filesystem>
+
 
 extern "C" IMAGE_DOS_HEADER __ImageBase;
 
@@ -24,8 +37,10 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     return TRUE;
 }
 
+
+
 // The PowerToy name that will be shown in the settings.
-const static wchar_t* MODULE_NAME = L"$projectname$";
+const static wchar_t* MODULE_NAME = L"Espresso";
 // Add a description that will we shown in the module settings page.
 const static wchar_t* MODULE_DESC = L"<no description>";
 
@@ -46,8 +61,12 @@ struct ModuleSettings
 } g_settings;
 
 // Implement the PowerToy Module Interface and all the required methods.
-class $safeprojectname$ : public PowertoyModuleIface
+class Espresso : public PowertoyModuleIface
 {
+    std::wstring app_name;
+    //contains the non localized key of the powertoy
+    std::wstring app_key;
+
 private:
     // The PowerToy state.
     bool m_enabled = false;
@@ -57,8 +76,10 @@ private:
 
 public:
     // Constructor
-    $safeprojectname$()
+    Espresso()
     {
+        app_name = GET_RESOURCE_STRING(IDS_ESPRESSO_NAME);
+        app_key = EspressoConstants::ModuleKey;
         init_settings();
     };
 
@@ -74,22 +95,22 @@ public:
         return MODULE_NAME;
     }
 
-    // Return array of the names of all events that this powertoy listens for, with
-    // nullptr as the last element of the array. Nullptr can also be retured for empty
-    // list.
-    virtual const wchar_t** get_events() override
-    {
-        static const wchar_t* events[] = { nullptr };
-        // Available events:
-        // - ll_keyboard
-        // - win_hook_event
-        //
-        // static const wchar_t* events[] = { ll_keyboard,
-        //                                   win_hook_event,
-        //                                   nullptr };
+    //// Return array of the names of all events that this powertoy listens for, with
+    //// nullptr as the last element of the array. Nullptr can also be retured for empty
+    //// list.
+    //virtual const wchar_t** get_events() override
+    //{
+    //    static const wchar_t* events[] = { nullptr };
+    //    // Available events:
+    //    // - ll_keyboard
+    //    // - win_hook_event
+    //    //
+    //    // static const wchar_t* events[] = { ll_keyboard,
+    //    //                                   win_hook_event,
+    //    //                                   nullptr };
 
-        return events;
-    }
+    //    return events;
+    //}
 
     // Return JSON with the configuration options.
     virtual bool get_config(wchar_t* buffer, int* buffer_size) override
@@ -149,6 +170,12 @@ public:
         return settings.serialize_to_buffer(buffer, buffer_size);
     }
 
+    // Return the non localized key of the powertoy, this will be cached by the runner
+    virtual const wchar_t* get_key() override
+    {
+        return app_key.c_str();
+    }
+
     // Signal from the Settings editor to call a custom action.
     // This can be used to spawn more complex editors.
     virtual void call_custom_action(const wchar_t* action) override
@@ -177,7 +204,7 @@ public:
         {
             // Parse the input JSON string.
             PowerToysSettings::PowerToyValues values =
-                PowerToysSettings::PowerToyValues::from_json_string(config);
+                PowerToysSettings::PowerToyValues::from_json_string(config, get_key());
 
             // Update a bool property.
             //if (auto v = values.get_bool_value(L"bool_toggle_1")) {
@@ -229,43 +256,43 @@ public:
         return m_enabled;
     }
 
-    // Handle incoming event, data is event-specific
-    virtual intptr_t signal_event(const wchar_t* name, intptr_t data) override
-    {
-        if (wcscmp(name, ll_keyboard) == 0)
-        {
-            auto& event = *(reinterpret_cast<LowlevelKeyboardEvent*>(data));
-            // Return 1 if the keypress is to be suppressed (not forwarded to Windows),
-            // otherwise return 0.
-            return 0;
-        }
-        else if (wcscmp(name, win_hook_event) == 0)
-        {
-            auto& event = *(reinterpret_cast<WinHookEvent*>(data));
-            // Return value is ignored
-            return 0;
-        }
-        return 0;
-    }
+    //// Handle incoming event, data is event-specific
+    //virtual intptr_t signal_event(const wchar_t* name, intptr_t data) override
+    //{
+    //    if (wcscmp(name, ll_keyboard) == 0)
+    //    {
+    //        auto& event = *(reinterpret_cast<LowlevelKeyboardEvent*>(data));
+    //        // Return 1 if the keypress is to be suppressed (not forwarded to Windows),
+    //        // otherwise return 0.
+    //        return 0;
+    //    }
+    //    else if (wcscmp(name, win_hook_event) == 0)
+    //    {
+    //        auto& event = *(reinterpret_cast<WinHookEvent*>(data));
+    //        // Return value is ignored
+    //        return 0;
+    //    }
+    //    return 0;
+    //}
 
-    // This methods are part of an experimental features not fully supported yet
-    virtual void register_system_menu_helper(PowertoySystemMenuIface* helper) override
-    {
-    }
+    //// This methods are part of an experimental features not fully supported yet
+    //virtual void register_system_menu_helper(PowertoySystemMenuIface* helper) override
+    //{
+    //}
 
-    virtual void signal_system_menu_action(const wchar_t* name) override
-    {
-    }
+    //virtual void signal_system_menu_action(const wchar_t* name) override
+    //{
+    //}
 };
 
 // Load the settings file.
-void $safeprojectname$::init_settings()
+void Espresso::init_settings()
 {
     try
     {
         // Load and parse the settings file for this PowerToy.
         PowerToysSettings::PowerToyValues settings =
-            PowerToysSettings::PowerToyValues::load_from_settings_file($safeprojectname$::get_name());
+            PowerToysSettings::PowerToyValues::load_from_settings_file(Espresso::get_name());
 
         // Load a bool property.
         //if (auto v = settings.get_bool_value(L"bool_toggle_1")) {
@@ -334,5 +361,5 @@ void $safeprojectname$::init_settings()
 
 extern "C" __declspec(dllexport) PowertoyModuleIface* __cdecl powertoy_create()
 {
-    return new $safeprojectname$();
+    return new Espresso();
 }
