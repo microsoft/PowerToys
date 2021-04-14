a8a05bc439ca478a66e7bdaddda574e2a1435e3e
Sima <zykovas91@gmail.com>
2021-04-14 14:51:04 +0300

    resources

--- src/modules/keyboardmanager/KeyboardManagerEditor/resource.base.h
+++ src/modules/keyboardmanager/KeyboardManagerEditor/resource.base.h
@@ -1,13 +1,13 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by KeyboardManager.rc
+// Used by KeyboardManagerEditor.rc
 
 //////////////////////////////
 // Non-localizable
 
-#define FILE_DESCRIPTION "PowerToys KeyboardManagerEditor"
-#define INTERNAL_NAME "KeyboardManagerEditor"
-#define ORIGINAL_FILENAME "KeyboardManagerEditor.exe"
+//#define FILE_DESCRIPTION "PowerToys KeyboardManagerEditor"
+//#define INTERNAL_NAME "KeyboardManagerEditor"
+//#define ORIGINAL_FILENAME "KeyboardManagerEditor.exe"
 #define IDS_KEYBOARDMANAGER_ICON 1001
 
 // Non-localizable
