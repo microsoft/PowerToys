866a1d66c3a1d0c84a395f7d6702284bb137da4c
mykhailopylyp <17161067+mykhailopylyp@users.noreply.github.com>
2021-04-20 19:13:11 +0300

    Remove unused file

--- src/modules/keyboardmanager/KeyboardManagerEditorLibrary/Constants.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "pch.h"
-
-namespace KeyboardManagerConstants
-{
-    // String constant for the default app name in Remap shortcuts
-    inline const std::wstring DefaultAppName = GET_RESOURCE_STRING(IDS_EDITSHORTCUTS_ALLAPPS);
-}
