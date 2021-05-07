d0311a7aebda555b3eeb72ccdeedde66c8b9e78b
mykhailopylyp <17161067+mykhailopylyp@users.noreply.github.com>
2021-05-07 17:01:58 +0300

    Add ShortcutGuide project

---
+++ src/modules/ShortcutGuide/ShortcutGuide/main.cpp
@@ -0,0 +1,7 @@
+﻿#include "pch.h"
+#include <Windows.h>
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    winrt::init_apartment();
+}
