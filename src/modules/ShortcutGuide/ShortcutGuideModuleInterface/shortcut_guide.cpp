b01073228a5a07f216ba3f45f6b6f07813db4ac5
Mykhailo Pylyp <pylypsmol@gmail.com>
2021-05-10 17:47:58 +0300

    Start SG process from ModuleInterface, (#11194)

--- src/modules/ShortcutGuide/ShortcutGuideModuleInterface/shortcut_guide.cpp
+++ src/modules/ShortcutGuide/ShortcutGuideModuleInterface/shortcut_guide.cpp
@@ -2,6 +2,8 @@
 #include "shortcut_guide.h"
 
 #include <common/SettingsAPI/settings_helpers.h>
+#include <common/utils/winapi_error.h>
+#include <common/utils/logger_helper.h>
 
 #include "trace.h"
 
@@ -12,9 +14,12 @@ OverlayWindow::OverlayWindow()
 {
     app_name = GET_RESOURCE_STRING(IDS_SHORTCUT_GUIDE);
     app_key = L"Shortcut Guide";
-    std::filesystem::path logFilePath(PTSettingsHelper::get_module_save_folder_location(app_key));
-    logFilePath.append(LogSettings::shortcutGuideLogPath);
-    Logger::init(LogSettings::shortcutGuideLoggerName, logFilePath.wstring(), PTSettingsHelper::get_log_settings_file_location());
+    LoggerHelpers::init_logger(app_key, L"ModuleInterface", LogSettings::shortcutGuideLoggerName);
+    
+    std::filesystem::path oldLogPath(PTSettingsHelper::get_module_save_folder_location(app_key));
+    oldLogPath.append("ShortcutGuideLogs");
+    LoggerHelpers::delete_old_log_folder(oldLogPath);
+
     Logger::info("Overlay Window is creating");
 }
 
@@ -45,6 +50,54 @@ constexpr int alternative_switch_hotkey_id = 0x2;
 constexpr UINT alternative_switch_modifier_mask = MOD_WIN | MOD_SHIFT;
 constexpr UINT alternative_switch_vk_code = VK_OEM_2;
 
+bool OverlayWindow::StartProcess()
+{
+    unsigned long powertoys_pid = GetCurrentProcessId();
+    std::wstring executable_args = L"";
+    executable_args.append(std::to_wstring(powertoys_pid));
+
+    SHELLEXECUTEINFOW sei{ sizeof(sei) };
+    sei.fMask = { SEE_MASK_NOCLOSEPROCESS | SEE_MASK_FLAG_NO_UI };
+    sei.lpFile = L"modules\\ShortcutGuide\\ShortcutGuide\\PowerToys.ShortcutGuide.exe";
+    sei.nShow = SW_SHOWNORMAL;
+    sei.lpParameters = executable_args.data();
+    if (ShellExecuteExW(&sei) == false)
+    {
+        Logger::error(L"Failed to start SG process. {}", get_last_error_or_default(GetLastError()));
+        auto message = get_last_error_message(GetLastError());
+        if (message.has_value())
+        {
+            Logger::error(message.value());
+        }
+
+        return false;
+    }
+
+    Logger::trace(L"Started SG process with pid={}", GetProcessId(sei.hProcess));
+    m_hProcess = sei.hProcess;
+    return true;
+}
+
+void OverlayWindow::TerminateProcess()
+{
+    if (m_hProcess)
+    {
+        if (WaitForSingleObject(m_hProcess, 0) != WAIT_OBJECT_0)
+        {
+            ::TerminateProcess(m_hProcess, 0);
+            m_hProcess = nullptr;
+        }
+        else
+        {
+            Logger::warn("SG process was already terminated");
+        }
+    }
+    else
+    {
+        Logger::warn("Process handle is not initialized");
+    }
+}
+
 void OverlayWindow::enable()
 {
     Logger::info("Shortcut Guide is enabling");
@@ -52,16 +105,20 @@ void OverlayWindow::enable()
     if (!_enabled)
     {
         Trace::EnableShortcutGuide(true);
-
-        // todo: start Shortcut Guide process
-        _enabled = true;
+        if (StartProcess())
+        {
+            _enabled = true;
+        }
+    }
+    else
+    {
+        Logger::warn("Shortcut guide is already enabled");    
     }
 }
 
 void OverlayWindow::disable(bool trace_event)
 {
     Logger::info("Shortcut Guide is disabling");
-
     if (_enabled)
     {
         _enabled = false;
@@ -70,7 +127,11 @@ void OverlayWindow::disable(bool trace_event)
             Trace::EnableShortcutGuide(false);
         }
 
-        // todo: stop Shortcut guide process
+        TerminateProcess();
+    }
+    else
+    {
+        Logger::warn("Shortcut Guide is already disabled");
     }
 }
 
