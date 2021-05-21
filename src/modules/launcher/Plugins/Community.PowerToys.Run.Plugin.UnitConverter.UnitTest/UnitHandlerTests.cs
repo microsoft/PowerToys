cd092c0f98c6c7f6ee46522f2568656e48c1071a
Jimmeh <ThiefZero@users.noreply.github.com>
2021-05-21 16:58:38 +0200

    [PT Run][New Plugin] Unit Converter (#9800)
    * [wip] add unit converter plugin
    
    * Plugin loads and accepts queries
    
    * Update csproj, add UnitConversion package, WIP
    
    * Switch unit conversion packages, add first convert logic
    
    * Move unitconverter to correct solution
    
    * Unitconversion supports double unabbreviated unitnames
    
    * Units convert with 1 abbreviation and 1 unabbreviated
    
    * Feature: make shorthand feet/inch notation work
    
    * Update to new naming scheme
    
    * Rename to community
    
    * Shorthand ft/in notation supports decimals.
    
    * Update plugin.json
    
    * Fix decimal/cultureInfo issue
    
    * Update plugin.json
    
    * Add degree prefixer function
    
    * Add unitConverter to wxs installer
    
    * Add name/description for interface
    
    * Add english translation, separate input parsing functions
    
    * Add words to spell-check
    
    * Add result rounding
    
    * PR feedback (rename stuff)
    
    * Unit conversion takes culture into account
    
    * Add InputInterpreter tests, small cleanup
    
    * Split conversion logic, add integration tests
    
    * Fix improper resource property names
    
    * [UnitConverter] Add image from niels9001. Fix exception error. Add function summaries.
    
    * [UnitConverter] Update image in installer
    
    * [UnitConverter] Support inputs without space (eg. 1cm to mm)
    
    * [UnitConverter] Add doc. Add more units. Update test packages.
    
    * [UnitConverter] Fix bug, regular unit conversion AND kB kb capital differences are recognized and work
    
    * [UnitConverter] Add doc img, fix spelling
    
    * [UnitConverter] Add byte capital test, just in case
    
    * [UnitConverter] Add unit tests to pipeline test build step
    
    * [UnitConverter] Change test to x64 from Any CPU. Fix issue where conversion to feet did not use cultureInfo.
    
    * [UnitConverter] Fix build config
    
    * removing allcpu
    forcing this to be global
    correcting depencies on launcher
    
    * [UnitConverter] Fix bug in degree conversion. Add unit to display result. Fix rounding display/value mismatch.
    
    * [UnitConverter] Improve test
    
    * [UnitConverter] Add Stylecop package, fix formatting, add
    GlobalSuppressions.cs
    
    * [UnitConverter] Change actionkeyword, more formatting
    
    * [UnitConverter] Add context menu to conversion results
    
    * [UnitConverter] Disable global
    
    * [UnitConverter] Add UnitsNet.dll to installer, insert example in description
    
    Co-authored-by: Clint Rutkas <clint@rutkas.com>
    Co-authored-by: Enrico Giordani <enricogior@users.noreply.github.com>

---
+++ src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.UnitConverter.UnitTest/UnitHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+
+namespace Community.PowerToys.Run.Plugin.UnitConverter.UnitTest
+{
+    [TestFixture]
+    public class UnitHandlerTests
+    {
+        [TestCase(new string[] { "1", "meter", "in", "centimeter" }, UnitsNet.QuantityType.Length, UnitHandler.Abbreviated.Neither)]
+        [TestCase(new string[] { "1", "m", "in", "cm" }, UnitsNet.QuantityType.Length, UnitHandler.Abbreviated.Both)]
+        [TestCase(new string[] { "1", "m", "in", "centimeter" }, UnitsNet.QuantityType.Length, UnitHandler.Abbreviated.First)]
+        [TestCase(new string[] { "1", "meter", "in", "cm" }, UnitsNet.QuantityType.Length, UnitHandler.Abbreviated.Second)]
+        public void ParsesInputForAbbreviations(string[] input, UnitsNet.QuantityType qType, UnitHandler.Abbreviated expectedResult)
+        {
+            (UnitHandler.Abbreviated abbreviated, UnitsNet.QuantityInfo quantityInfo) result = UnitHandler.ParseInputForAbbreviation(input, qType);
+            Assert.AreEqual(expectedResult, result.abbreviated);
+        }
+
+        [TestCase(new string[] { "1", "meter", "in", "centimeter" }, UnitsNet.QuantityType.Length, 100d)]
+        [TestCase(new string[] { "1", "DegreeCelsius", "in", "DegreeFahrenheit" }, UnitsNet.QuantityType.Temperature, 33.79999999999999d)]
+        public void ConvertsInput(string[] input, UnitsNet.QuantityType unit, double expectedResult)
+        {
+            double result = UnitHandler.ConvertInput(input, unit, CultureInfo.InvariantCulture);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(new string[] { "1", "kB", "in", "kb" }, 8)]
+        public void HandlesByteCapitals(string[] input, double expectedResult)
+        {
+            double result = UnitHandler.ConvertInput(input, UnitsNet.QuantityType.Information, CultureInfo.InvariantCulture);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.UnitConverter/Main.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using ManagedCommon;
+using UnitsNet;
+using Wox.Plugin;
+
+namespace Community.PowerToys.Run.Plugin.UnitConverter
+{
+    public class Main : IPlugin, IPluginI18n, IContextMenu, IDisposable
+    {
+        public string Name => Properties.Resources.plugin_name;
+
+        public string Description => Properties.Resources.plugin_description;
+
+        private PluginInitContext _context;
+        private static string _icon_path;
+        private bool _disposed;
+        private static readonly QuantityType[] _included = new QuantityType[]
+        {
+            QuantityType.Acceleration,
+            QuantityType.Angle,
+            QuantityType.Area,
+            QuantityType.Duration,
+            QuantityType.Energy,
+            QuantityType.Information,
+            QuantityType.Length,
+            QuantityType.Mass,
+            QuantityType.Power,
+            QuantityType.Pressure,
+            QuantityType.Speed,
+            QuantityType.Temperature,
+            QuantityType.Volume,
+        };
+
+        private readonly CultureInfo _currentCulture = CultureInfo.CurrentCulture;
+        private readonly int _roundingFractionalDigits = 4;
+
+        public void Init(PluginInitContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(context));
+            }
+
+            _context = context;
+            _context.API.ThemeChanged += OnThemeChanged;
+            UpdateIconPath(_context.API.GetCurrentTheme());
+        }
+
+        public List<Result> Query(Query query)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(query));
+            }
+
+            string[] split = query.Search.Split(' ');
+
+            InputInterpreter.ShorthandFeetInchHandler(ref split, _currentCulture);
+            InputInterpreter.InputSpaceInserter(ref split);
+
+            if (split.Length < 4 || split.Length > 4)
+            {
+                // deny any other queries than:
+                // 10 ft in cm
+                // 10 ft to cm
+                return new List<Result>();
+            }
+
+            InputInterpreter.DegreePrefixer(ref split);
+
+            List<Result> final_list = new List<Result>();
+
+            foreach (QuantityType quantityType in _included)
+            {
+                double convertedValue = UnitHandler.ConvertInput(split, quantityType, _currentCulture);
+
+                if (!double.IsNaN(convertedValue))
+                {
+                    UnitConversionResult result = new UnitConversionResult(Math.Round(convertedValue, _roundingFractionalDigits), split[3], quantityType);
+                    AddToResult(final_list, result);
+                }
+            }
+
+            return final_list;
+        }
+
+        private void AddToResult(List<Result> currentList, UnitConversionResult result)
+        {
+            // answer found, add result to list
+            currentList.Add(new Result
+            {
+                ContextData = result,
+                Title = string.Format("{0} {1}", result.ConvertedValue, result.UnitName),
+                IcoPath = _icon_path,
+                Score = 300,
+                SubTitle = string.Format(Properties.Resources.copy_to_clipboard, result.QuantityType),
+                Action = c =>
+                {
+                    var ret = false;
+                    var thread = new Thread(() =>
+                    {
+                        try
+                        {
+                            Clipboard.SetText(result.ConvertedValue.ToString());
+                            ret = true;
+                        }
+                        catch (ExternalException)
+                        {
+                            MessageBox.Show(Properties.Resources.copy_failed);
+                        }
+                    });
+                    thread.SetApartmentState(ApartmentState.STA);
+                    thread.Start();
+                    thread.Join();
+                    return ret;
+                },
+            });
+        }
+
+        private ContextMenuResult CreateContextMenuEntry(UnitConversionResult result)
+        {
+            return new ContextMenuResult
+            {
+                PluginName = Name,
+                Title = Properties.Resources.context_menu_copy,
+                Glyph = "\xE8C8",
+                FontFamily = "Segoe MDL2 Assets",
+                AcceleratorKey = Key.Enter,
+                Action = _ =>
+                {
+                    bool ret = false;
+                    var thread = new Thread(() =>
+                    {
+                        try
+                        {
+                            Clipboard.SetText(result.ConvertedValue.ToString());
+                            ret = true;
+                        }
+                        catch (ExternalException)
+                        {
+                            MessageBox.Show(Properties.Resources.copy_failed);
+                        }
+                    });
+                    thread.SetApartmentState(ApartmentState.STA);
+                    thread.Start();
+                    thread.Join();
+                    return ret;
+                },
+            };
+        }
+
+        public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
+        {
+            if (!(selectedResult?.ContextData is UnitConversionResult))
+            {
+                return new List<ContextMenuResult>();
+            }
+
+            List<ContextMenuResult> contextResults = new List<ContextMenuResult>();
+            UnitConversionResult result = selectedResult.ContextData as UnitConversionResult;
+            contextResults.Add(CreateContextMenuEntry(result));
+
+            return contextResults;
+        }
+
+        public string GetTranslatedPluginTitle()
+        {
+            return Properties.Resources.plugin_name;
+        }
+
+        public string GetTranslatedPluginDescription()
+        {
+            return Properties.Resources.plugin_description;
+        }
+
+        private void OnThemeChanged(Theme _, Theme newTheme)
+        {
+            UpdateIconPath(newTheme);
+        }
+
+        private static void UpdateIconPath(Theme theme)
+        {
+            if (theme == Theme.Light || theme == Theme.HighContrastWhite)
+            {
+                _icon_path = "Images/unitconverter.light.png";
+            }
+            else
+            {
+                _icon_path = "Images/unitconverter.dark.png";
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.API.ThemeChanged -= OnThemeChanged;
+                    _disposed = true;
+                }
+            }
+        }
+    }
+}
