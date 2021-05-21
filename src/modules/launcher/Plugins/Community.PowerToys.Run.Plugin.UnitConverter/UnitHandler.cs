cc275fa04c9b7da2601b427c49972b0d983d3f97
Jimmeh <ThiefZero@users.noreply.github.com>
2021-05-21 16:58:38 +0200

    [PT Run][New Plugin] Unit Converter (#9800)
    * [wip] add unit converter plugin
    
    * Plugin loads and accepts queries
    
    * Update csproj, add UnitConversion package, WIP
    
    * Switch unit conversion packages, add first convert logic
    
    * Move unitconverter to correct solution
    
    * Unitconversion supports double unabbreviated unitnames
    
    * Units convert with 1 abbreviation and 1 unabbreviated
    
    * Feature: make shorthand feet/inch notation work
    
    * Update to new naming scheme
    
    * Rename to community
    
    * Shorthand ft/in notation supports decimals.
    
    * Update plugin.json
    
    * Fix decimal/cultureInfo issue
    
    * Update plugin.json
    
    * Add degree prefixer function
    
    * Add unitConverter to wxs installer
    
    * Add name/description for interface
    
    * Add english translation, separate input parsing functions
    
    * Add words to spell-check
    
    * Add result rounding
    
    * PR feedback (rename stuff)
    
    * Unit conversion takes culture into account
    
    * Add InputInterpreter tests, small cleanup
    
    * Split conversion logic, add integration tests
    
    * Fix improper resource property names
    
    * [UnitConverter] Add image from niels9001. Fix exception error. Add function summaries.
    
    * [UnitConverter] Update image in installer
    
    * [UnitConverter] Support inputs without space (eg. 1cm to mm)
    
    * [UnitConverter] Add doc. Add more units. Update test packages.
    
    * [UnitConverter] Fix bug, regular unit conversion AND kB kb capital differences are recognized and work
    
    * [UnitConverter] Add doc img, fix spelling
    
    * [UnitConverter] Add byte capital test, just in case
    
    * [UnitConverter] Add unit tests to pipeline test build step
    
    * [UnitConverter] Change test to x64 from Any CPU. Fix issue where conversion to feet did not use cultureInfo.
    
    * [UnitConverter] Fix build config
    
    * removing allcpu
    forcing this to be global
    correcting depencies on launcher
    
    * [UnitConverter] Fix bug in degree conversion. Add unit to display result. Fix rounding display/value mismatch.
    
    * [UnitConverter] Improve test
    
    * [UnitConverter] Add Stylecop package, fix formatting, add
    GlobalSuppressions.cs
    
    * [UnitConverter] Change actionkeyword, more formatting
    
    * [UnitConverter] Add context menu to conversion results
    
    * [UnitConverter] Disable global
    
    * [UnitConverter] Add UnitsNet.dll to installer, insert example in description
    
    Co-authored-by: Clint Rutkas <clint@rutkas.com>
    Co-authored-by: Enrico Giordani <enricogior@users.noreply.github.com>

---
+++ src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.UnitConverter/UnitHandler.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Globalization;
+using UnitsNet;
+
+namespace Community.PowerToys.Run.Plugin.UnitConverter
+{
+    public static class UnitHandler
+    {
+        public enum Abbreviated
+        {
+            First,
+            Second,
+            Both,
+            Neither,
+            NotFound,
+        }
+
+        /// <summary>
+        /// Given user unit input, converts it. (E.g "1 foot in cm").
+        /// </summary>
+        /// <returns>The converted value as a double.</returns>
+        public static double ConvertInput(string[] split, QuantityType quantityType, CultureInfo currentCulture)
+        {
+            string input_first_unit = split[1];
+            string input_second_unit = split[3];
+
+            (Abbreviated abbreviated, QuantityInfo unitInfo) = ParseInputForAbbreviation(split, quantityType);
+
+            switch (abbreviated)
+            {
+                case Abbreviated.Both:
+                    return UnitsNet.UnitConverter.ConvertByAbbreviation(double.Parse(split[0], currentCulture), unitInfo.Name, input_first_unit, input_second_unit);
+
+                case Abbreviated.Neither:
+                    return UnitsNet.UnitConverter.ConvertByName(double.Parse(split[0], currentCulture), unitInfo.Name, input_first_unit, input_second_unit);
+
+                case Abbreviated.Second:
+                    UnitInfo first = Array.Find(unitInfo.UnitInfos, info => info.Name.ToLower() == input_first_unit.ToLower());
+                    UnitParser.Default.TryParse(split[3], unitInfo.UnitType, out Enum second_unit);
+                    return UnitsNet.UnitConverter.Convert(double.Parse(split[0], currentCulture), first.Value, second_unit);
+
+                case Abbreviated.First:
+                    UnitParser.Default.TryParse(split[1], unitInfo.UnitType, out Enum first_unit);
+                    UnitInfo second = Array.Find(unitInfo.UnitInfos, info => info.Name.ToLower() == input_second_unit.ToLower());
+                    return UnitsNet.UnitConverter.Convert(double.Parse(split[0], currentCulture), first_unit, second.Value);
+
+                case Abbreviated.NotFound:
+                default:
+                    return double.NaN;
+            }
+        }
+
+        /// <summary>
+        /// Given a split array of user input, parses the input for abbreviations (e.g. "1 cm in meters").
+        /// </summary>
+        /// <returns>A tuple consisting of an Abbreviated enum and QuantityInfo.</returns>
+        public static (Abbreviated Abbreviated, QuantityInfo UnitInfo) ParseInputForAbbreviation(string[] split, QuantityType quantityType)
+        {
+            string input_first_unit = split[1];
+            string input_second_unit = split[3];
+
+            QuantityInfo unit_info = Quantity.GetInfo(quantityType);
+            bool first_unit_is_abbreviated = UnitParser.Default.TryParse(split[1], unit_info.UnitType, out Enum _);
+            bool second_unit_is_abbreviated = UnitParser.Default.TryParse(split[3], unit_info.UnitType, out Enum _);
+
+            // 3 types of matches:
+            // a) 10 ft in cm (double abbreviation)
+            // b) 10 feet in centimeter (double unabbreviated)
+            // c) 10 feet in cm (single abbreviation)
+
+            if (first_unit_is_abbreviated && second_unit_is_abbreviated)
+            {
+                // a
+                return (Abbreviated.Both, unit_info);
+            }
+            else if ((!first_unit_is_abbreviated) && (!second_unit_is_abbreviated))
+            {
+                // b
+                bool first_unabbreviated = Array.Exists(unit_info.UnitInfos, unitName => unitName.Name.ToLower() == input_first_unit.ToLower());
+                bool second_unabbreviated = Array.Exists(unit_info.UnitInfos, unitName => unitName.Name.ToLower() == input_second_unit.ToLower());
+
+                if (first_unabbreviated && second_unabbreviated)
+                {
+                    return (Abbreviated.Neither, unit_info);
+                }
+            }
+            else if ((first_unit_is_abbreviated && !second_unit_is_abbreviated) || (!first_unit_is_abbreviated && second_unit_is_abbreviated))
+            {
+                // c
+                if (first_unit_is_abbreviated)
+                {
+                    bool second_unabbreviated = Array.Exists(unit_info.UnitInfos, unitName => unitName.Name.ToLower() == input_second_unit.ToLower());
+
+                    if (second_unabbreviated)
+                    {
+                        return (Abbreviated.First, unit_info);
+                    }
+                }
+                else if (second_unit_is_abbreviated)
+                {
+                    bool first_unabbreviated = Array.Exists(unit_info.UnitInfos, unitName => unitName.Name.ToLower() == input_first_unit.ToLower());
+
+                    if (first_unabbreviated)
+                    {
+                        return (Abbreviated.Second, unit_info);
+                    }
+                }
+            }
+
+            return (Abbreviated.NotFound, null);
+        }
+    }
+}
