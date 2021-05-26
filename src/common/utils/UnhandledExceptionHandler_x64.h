856d9482525daa36f788a269909a2c1dfc31ce72
SeraphimaZ <zykovas91@gmail.com>
2021-05-26 11:37:49 +0300

    include "winapi_error.h" in the UnhandledExceptionHandler

--- src/common/utils/UnhandledExceptionHandler_x64.h
+++ src/common/utils/UnhandledExceptionHandler_x64.h
@@ -3,6 +3,8 @@
 #include <signal.h>
 #include <sstream>
 #include <stdio.h>
+
+#include "winapi_error.h"
 #include "../logger/logger.h"
 
 static IMAGEHLP_SYMBOL64* pSymbol = (IMAGEHLP_SYMBOL64*)malloc(sizeof(IMAGEHLP_SYMBOL64) + MAX_PATH * sizeof(TCHAR));
