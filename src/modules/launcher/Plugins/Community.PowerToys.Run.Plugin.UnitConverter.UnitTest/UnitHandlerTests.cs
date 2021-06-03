571f75ef194e50630c2505e901cec1dd3227abe9
mykhailopylyp <17161067+mykhailopylyp@users.noreply.github.com>
2021-06-03 15:21:03 +0300

    Divide logic into two parts: parse and convert

--- src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.UnitConverter.UnitTest/UnitHandlerTests.cs
+++ src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.UnitConverter.UnitTest/UnitHandlerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Community.PowerToys.Run.Plugin.UnitConverter.UnitTest
@@ -6,19 +7,36 @@ namespace Community.PowerToys.Run.Plugin.UnitConverter.UnitTest
     [TestFixture]
     public class UnitHandlerTests
     {
-        [TestCase(new string[] { "1", "meter", "in", "centimeter" }, UnitsNet.QuantityType.Length, 100d)]
-        [TestCase(new string[] { "1", "DegreeCelsius", "in", "DegreeFahrenheit" }, UnitsNet.QuantityType.Temperature, 33.79999999999999d)]
-        public void ConvertsInput(string[] input, UnitsNet.QuantityType unit, double expectedResult)
+        [Test]
+        public void HandleTempreture()
         {
-            double result = UnitHandler.ConvertInput(input, unit, CultureInfo.InvariantCulture);
-            Assert.AreEqual(expectedResult, result);
+            var convertModel = new ConvertModel(1, "DegreeCelsius", "DegreeFahrenheit");
+            double result = UnitHandler.ConvertInput(convertModel, UnitsNet.QuantityType.Temperature);
+            Assert.AreEqual(33.79999999999999d, result);
         }
 
-        [TestCase(new string[] { "1", "kB", "in", "kb" }, 8)]
-        public void HandlesByteCapitals(string[] input, double expectedResult)
+        [Test]
+        public void HandleLength()
         {
-            double result = UnitHandler.ConvertInput(input, UnitsNet.QuantityType.Information, CultureInfo.InvariantCulture);
-            Assert.AreEqual(expectedResult, result);
+            var convertModel = new ConvertModel(1, "meter", "centimeter");
+            double result = UnitHandler.ConvertInput(convertModel, UnitsNet.QuantityType.Length);
+            Assert.AreEqual(100, result);
+        }
+
+        [Test]
+        public void HandlesByteCapitals()
+        {
+            var convertModel = new ConvertModel(1, "kB", "kb");
+            double result = UnitHandler.ConvertInput(convertModel, UnitsNet.QuantityType.Information);
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void HandleInvalidModel()
+        {
+            var convertModel = new ConvertModel(1, "aa", "bb");
+            var results = UnitHandler.Convert(convertModel);
+            Assert.AreEqual(0, results.Count());
         }
     }
 }
--- src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.UnitConverter/UnitHandler.cs
+++ src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.UnitConverter/UnitHandler.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using UnitsNet;
 
 namespace Community.PowerToys.Run.Plugin.UnitConverter
 {
     public static class UnitHandler
     {
+        private static readonly int _roundingFractionalDigits = 4;
+
+        private static readonly QuantityType[] _included = new QuantityType[]
+        {
+            QuantityType.Acceleration,
+            QuantityType.Angle,
+            QuantityType.Area,
+            QuantityType.Duration,
+            QuantityType.Energy,
+            QuantityType.Information,
+            QuantityType.Length,
+            QuantityType.Mass,
+            QuantityType.Power,
+            QuantityType.Pressure,
+            QuantityType.Speed,
+            QuantityType.Temperature,
+            QuantityType.Volume,
+        };
+
         /// <summary>
         /// Given string representation of unit, converts it to the enum.
         /// </summary>
@@ -27,24 +48,43 @@ namespace Community.PowerToys.Run.Plugin.UnitConverter
         }
 
         /// <summary>
-        /// Given user unit input, converts it. (E.g "1 foot in cm").
+        /// Given parsed ConvertModel, computes result. (E.g "1 foot in cm").
         /// </summary>
         /// <returns>The converted value as a double.</returns>
-        public static double ConvertInput(string[] split, QuantityType quantityType, CultureInfo currentCulture)
+        public static double ConvertInput(ConvertModel convertModel, QuantityType quantityType)
         {
-            string firstUnitString = split[1];
-            string secondUnitString = split[3];
             QuantityInfo unitInfo = Quantity.GetInfo(quantityType);
 
-            var firstUnit = GetUnitEnum(firstUnitString, unitInfo);
-            var secondUnit = GetUnitEnum(secondUnitString, unitInfo);
+            var fromUnit = GetUnitEnum(convertModel.FromUnit, unitInfo);
+            var toUnit = GetUnitEnum(convertModel.ToUnit, unitInfo);
 
-            if (firstUnit != null && secondUnit != null)
+            if (fromUnit != null && toUnit != null)
             {
-                return UnitsNet.UnitConverter.Convert(double.Parse(split[0], currentCulture), firstUnit, secondUnit);
+                return UnitsNet.UnitConverter.Convert(convertModel.Value, fromUnit, toUnit);
             }
 
             return double.NaN;
+        }
+
+        /// <summary>
+        /// Given ConvertModel returns collection of possible results.
+        /// </summary>
+        /// <returns>The converted value as a double.</returns>
+        public static IEnumerable<UnitConversionResult> Convert(ConvertModel convertModel)
+        {
+            var results = new List<UnitConversionResult>();
+            foreach (QuantityType quantityType in _included)
+            {
+                double convertedValue = UnitHandler.ConvertInput(convertModel, quantityType);
+
+                if (!double.IsNaN(convertedValue))
+                {
+                    UnitConversionResult result = new UnitConversionResult(Math.Round(convertedValue, _roundingFractionalDigits), convertModel.ToUnit, quantityType);
+                    results.Add(result);
+                }
+            }
+
+            return results;
         }
     }
 }
