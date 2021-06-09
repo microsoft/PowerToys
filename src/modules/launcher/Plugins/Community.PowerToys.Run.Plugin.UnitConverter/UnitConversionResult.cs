4e438981fdb10c070b448dcf683f90a10ab3eec5
ThiefZero <ss_jimmy@hotmail.com>
2021-06-09 10:46:19 +0100

    [PT Run][New Plugin] Unit Converter (#9800) (#11406)

---
+++ src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.UnitConverter/UnitConversionResult.cs
@@ -0,0 +1,20 @@
+﻿using UnitsNet;
+
+namespace Community.PowerToys.Run.Plugin.UnitConverter
+{
+    public class UnitConversionResult
+    {
+        public double ConvertedValue { get; }
+
+        public string UnitName { get; }
+
+        public QuantityType QuantityType { get; }
+
+        public UnitConversionResult(double convertedValue, string unitName, QuantityType quantityType)
+        {
+            ConvertedValue = convertedValue;
+            UnitName = unitName;
+            QuantityType = quantityType;
+        }
+    }
+}
