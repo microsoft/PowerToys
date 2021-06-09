c9c54b7780dec2f55732fed053206ff7dadbcc0c
Mykhailo Pylyp <pylypsmol@gmail.com>
2021-06-09 18:04:03 +0300

    [Run-Plugin] Settings plugin (#11663)
    * Current settings plugin state on fresh master
    
    * typo fixes
    
    * Add to YML
    
    * Add to WXS
    
    * Address feedback - highlight the note in the tool-tip a little bit
    
    * Address feedback add extra note for "Manage known networks"
    
    * Address feedback - Show type of settings in sub-line and remove extra ControlPanel prefix in json
    
    * Add "WiFi" as alternative name for each Wi-Fi setting
    
    * Add a few more alternative names
    
    * Make RESX happy
    
    * exclude WindowsSettings.json from spell checker because all entries are placeholders  for the translation
    
    * Translate all alternative names
    
    * Translate all notes
    
    * fix for not find "wifi"
    
    * fix typo
    
    * typo fixes and remove debug
    
    * Address feedback - correct author
    
    * Address feedback - settings entries
    
    * Address feedback - code changes
    
    * Address feedback - RESX changes and tool-tip
    
    * fix typo
    
    * Address feedback - remove superfluous interface
    
    * Address feedback - Update RESX
    
    * Address feedback - simplification
    
    * Address feedback - remove enumeration
    
    * Address feedback - move big function in extra helper classes
    
    * Address feedback - move big function in extra helper class
    
    * Address feedback - correct namespace
    
    * Address feedback - move translation to translation helper and make translation more robust
    
    * fix typo
    
    * Update src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/Main.cs
    
    Co-authored-by: Heiko <61519853+htcfreek@users.noreply.github.com>
    
    * Update src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/Helper/ResultHelper.cs
    
    Co-authored-by: Heiko <61519853+htcfreek@users.noreply.github.com>
    
    * Update src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/Main.cs
    
    Co-authored-by: Heiko <61519853+htcfreek@users.noreply.github.com>
    
    * fix build
    
    * Address feedback
    
    * ups
    
    * Address feedback - Correct windows update settings name
    
    * Update src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/WindowsSettings.json
    
    Co-authored-by: Heiko <61519853+htcfreek@users.noreply.github.com>
    
    * adding in dependencies so when you build Launcher, all plugins are included
    
    * Address feedback - add optional updates
    
    * Address feedback - use build numebr instaed of Windows version
    
    * Address feedback - Log difference between registry values + fix wrong ValueType (ushort -> uint)
    
    * Address feebdback - improved warning message on different registry values
    
    * fix typo
    
    * removed not need using
    
    * Update src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/Helper/UnsupportedSettingsHelper.cs
    
    Co-authored-by: Heiko <61519853+htcfreek@users.noreply.github.com>
    
    * Update src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/Helper/UnsupportedSettingsHelper.cs
    
    Co-authored-by: Heiko <61519853+htcfreek@users.noreply.github.com>
    
    * Update src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/Helper/UnsupportedSettingsHelper.cs
    
    Co-authored-by: Heiko <61519853+htcfreek@users.noreply.github.com>
    
    * Addrress feedback, don't copy embed file
    
    * Address feedback - Remove duplicated or not available settings
    
    * Address feedback - Improve scoring
    
    * Address feedback - Add extra filter
    
    * Address feedback - replace the are filter sign with a better one
    
    * Address feedback - fix unwanted behavior
    
    * Address feedback - Change class name
    
    * Address feedback - Rename settings type
    
    * typo fix
    
    * Fix installer
    
    * Comment out localization support
    
    Co-authored-by: Sekan, Tobias <tobias.sekan@startmail.com>
    Co-authored-by: Heiko <61519853+htcfreek@users.noreply.github.com>
    Co-authored-by: crutkas <crutkas@microsoft.com>

---
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/Classes/WindowsSetting.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.PowerToys.Run.Plugin.WindowsSettings
+{
+    /// <summary>
+    /// A windows setting
+    /// </summary>
+    internal class WindowsSetting
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WindowsSetting"/> class.
+        /// </summary>
+        public WindowsSetting()
+        {
+            Name = string.Empty;
+            Area = string.Empty;
+            Command = string.Empty;
+            Type = string.Empty;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of this setting.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the area of this setting.
+        /// </summary>
+        public string Area { get; set; }
+
+        /// <summary>
+        /// Gets or sets the command of this setting.
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the windows setting.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the alternative names of this setting.
+        /// </summary>
+        public IEnumerable<string>? AltNames { get; set; }
+
+        /// <summary>
+        /// Gets or sets a additional note of this settings.
+        /// <para>(e.g. why is not supported on your system)</para>
+        /// </summary>
+        public string? Note { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum need Windows build for this setting.
+        /// </summary>
+        public uint? IntroducedInBuild { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Windows build since this settings is not longer present.
+        /// </summary>
+        public uint? DeprecatedInBuild { get; set; }
+    }
+}
