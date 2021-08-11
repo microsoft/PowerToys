da13f79d131afebe2bbf7c12a21e0f2acb7bea19
yuyoyuppe <a.yuyoyuppe@gmail.com>
2021-08-11 23:35:57 +0300

    [VCM] Activate the module

--- src/settings-ui/Microsoft.PowerToys.Settings.UI/OOBE/Views/OobeShellPage.xaml.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/OOBE/Views/OobeShellPage.xaml.cs
@@ -180,7 +180,8 @@ namespace Microsoft.PowerToys.Settings.UI.OOBE.Views
                 Link = "https://aka.ms/PowerToysOverview_ShortcutGuide",
             });
 
-            /* Modules.Insert((int)PowerToysModulesEnum.VideoConference, new OobePowerToysModule()
+            // TODO(yuyoyuppe): uncomment when VCM should be enabled
+            Modules.Insert((int)PowerToysModulesEnum.VideoConference, new OobePowerToysModule()
             {
                 ModuleName = loader.GetString("Oobe_VideoConference"),
                 Tag = "VideoConference",
@@ -191,7 +192,7 @@ namespace Microsoft.PowerToys.Settings.UI.OOBE.Views
                 Description = loader.GetString("Oobe_VideoConference_Description"),
                 PreviewImageSource = "ms-appx:///Assets/Modules/OOBE/VideoConferenceMute.png",
                 Link = "https://aka.ms/PowerToysOverview_VideoConference",
-            }); */
+            });
         }
 
         public void OnClosing()
