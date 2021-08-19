babd5a123bd28db4325f2b13aac59e5bb9536b18
Niels Laute <niels9001@hotmail.com>
2021-08-19 16:45:13 +0200

    Updated converter

--- src/settings-ui/Microsoft.PowerToys.Settings.UI/Converters/UpdateStateToBoolConverter.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/Converters/UpdateStateToBoolConverter.cs
@@ -3,17 +3,34 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.PowerToys.Settings.UI.Library;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
 namespace Microsoft.PowerToys.Settings.UI.Converters
 {
-    public sealed class UpdatingStateCannotDownloadToVisibilityConverter : IValueConverter
+    public sealed class UpdateStateToBoolConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return (UpdatingSettings.UpdatingState)value == UpdatingSettings.UpdatingState.ErrorDownloading ? true : false;
+            if (value == null || parameter == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (value.ToString() == (string)parameter)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
