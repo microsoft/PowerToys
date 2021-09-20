9899afe2aba93b4549f057738928ae24975cb43d
Niels Laute <niels9001@hotmail.com>
2021-09-20 16:46:12 +0200

    Updated UI

---
+++ src/modules/imageresizer/ui/Views/SizeTypeToVisibilityConverter.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using ImageResizer.Models;
+
+namespace ImageResizer.Views
+{
+    [ValueConversion(typeof(Visibility), typeof(ResizeSize))]
+    internal class SizeTypeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if (value.GetType() == typeof(CustomSize))
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+             return (Visibility)value == Visibility.Visible;
+        }
+    }
+
+    [ValueConversion(typeof(bool), typeof(ResizeSize))]
+#pragma warning disable SA1402 // File may only contain a single type
+    internal class SizeTypeToBoolConverter : IValueConverter
+#pragma warning restore SA1402 // File may only contain a single type
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if (value.GetType() == typeof(CustomSize))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
