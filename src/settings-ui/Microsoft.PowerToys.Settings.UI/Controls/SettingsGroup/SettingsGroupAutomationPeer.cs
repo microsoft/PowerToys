a8730637d6ce900bf5a40258d60c011ffa5975ec
Laute <Niels.Laute@philips.com>
2021-09-30 14:10:02 +0200

    Added name to info button

--- src/settings-ui/Microsoft.PowerToys.Settings.UI/Controls/SettingsGroup/SettingsGroupAutomationPeer.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/Controls/SettingsGroup/SettingsGroupAutomationPeer.cs
@@ -2,14 +2,9 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml.Automation.Peers;
 
-namespace Microsoft.PowerToys.Settings.UI.Controls.SettingsGroup
+namespace Microsoft.PowerToys.Settings.UI.Controls
 {
     public class SettingsGroupAutomationPeer : FrameworkElementAutomationPeer
     {
