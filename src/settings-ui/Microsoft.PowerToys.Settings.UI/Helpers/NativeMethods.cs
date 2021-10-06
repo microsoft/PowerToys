805d8d81c592cd2a7875908fe0251fef335ed017
Davide Giacometti <davide.giacometti@outlook.it>
2021-10-06 15:37:33 +0200

    Fix ColorPicker editor and OOBE FancyZones snap (#13598)

--- src/settings-ui/Microsoft.PowerToys.Settings.UI/Helpers/NativeMethods.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/Helpers/NativeMethods.cs
@@ -9,11 +9,20 @@ namespace Microsoft.PowerToys.Settings.UI.Helpers
 {
     public static class NativeMethods
     {
+        private const int GWL_STYLE = -16;
+        private const int WS_POPUP = 1 << 31; // 0x80000000
+
         [DllImport("user32.dll")]
         internal static extern uint SendInput(uint nInputs, NativeKeyboardHelper.INPUT[] pInputs, int cbSize);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
         internal static extern short GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
 #pragma warning disable CA1401 // P/Invokes should not be visible
         [DllImport("user32.dll")]
@@ -28,5 +37,10 @@ namespace Microsoft.PowerToys.Settings.UI.Helpers
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
         public static extern bool FreeLibrary(IntPtr hModule);
 #pragma warning restore CA1401 // P/Invokes should not be visible
+
+        public static void SetPopupStyle(IntPtr hwnd)
+        {
+            _ = SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) | WS_POPUP);
+        }
     }
 }
