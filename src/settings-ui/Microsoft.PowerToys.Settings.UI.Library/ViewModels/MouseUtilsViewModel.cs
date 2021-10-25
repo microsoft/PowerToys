55c5dcf223cdc4c435c89cfc7fb00465e30ff6f8
Jaime Bernardo <jaime@janeasystems.com>
2021-10-25 12:38:09 +0100

    Add settings scaffolding

--- src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/ViewModels/MouseUtilsViewModel.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI.Library/ViewModels/MouseUtilsViewModel.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Runtime.CompilerServices;
 using Microsoft.PowerToys.Settings.UI.Library.Helpers;
 using Microsoft.PowerToys.Settings.UI.Library.Interfaces;
 
@@ -10,10 +11,16 @@ namespace Microsoft.PowerToys.Settings.UI.Library.ViewModels
 {
     public class MouseUtilsViewModel : Observable
     {
+        private ISettingsUtils SettingsUtils { get; set; }
+
         private GeneralSettings GeneralSettingsConfig { get; set; }
 
-        public MouseUtilsViewModel(ISettingsRepository<GeneralSettings> settingsRepository, Func<string, int> ipcMSGCallBackFunc)
+        private FindMyMouseSettings FindMyMouseSettingsConfig { get; set; }
+
+        public MouseUtilsViewModel(ISettingsUtils settingsUtils, ISettingsRepository<GeneralSettings> settingsRepository, ISettingsRepository<FindMyMouseSettings> findMyMouseSettingsRepository, Func<string, int> ipcMSGCallBackFunc)
         {
+            SettingsUtils = settingsUtils;
+
             // To obtain the general settings configurations of PowerToys Settings.
             if (settingsRepository == null)
             {
@@ -24,7 +31,17 @@ namespace Microsoft.PowerToys.Settings.UI.Library.ViewModels
 
             _isFindMyMouseEnabled = GeneralSettingsConfig.Enabled.FindMyMouse;
 
-            // set the callback functions value to hangle outgoing IPC message.
+            // To obtain the find my mouse settings, if the file exists.
+            // If not, to create a file with the default settings and to return the default configurations.
+            if (findMyMouseSettingsRepository == null)
+            {
+                throw new ArgumentNullException(nameof(findMyMouseSettingsRepository));
+            }
+
+            FindMyMouseSettingsConfig = findMyMouseSettingsRepository.SettingsConfig;
+            _findMyMouseDoNotActivateOnGameMode = FindMyMouseSettingsConfig.Properties.DoNotActivateOnGameMode.Value;
+
+            // set the callback functions value to handle outgoing IPC message.
             SendConfigMSG = ipcMSGCallBackFunc;
         }
 
@@ -43,14 +60,42 @@ namespace Microsoft.PowerToys.Settings.UI.Library.ViewModels
                     OutGoingGeneralSettings outgoing = new OutGoingGeneralSettings(GeneralSettingsConfig);
                     SendConfigMSG(outgoing.ToString());
 
-                    // TODO: Implement when this module has properties.
-                    // NotifyPropertyChanged();
+                    NotifyFindMyMousePropertyChanged();
                 }
             }
+        }
+
+        public bool FindMyMouseDoNotActivateOnGameMode
+        {
+            get
+            {
+                return _findMyMouseDoNotActivateOnGameMode;
+            }
+
+            set
+            {
+                if (_findMyMouseDoNotActivateOnGameMode != value)
+                {
+                    _findMyMouseDoNotActivateOnGameMode = value;
+                    FindMyMouseSettingsConfig.Properties.DoNotActivateOnGameMode.Value = value;
+                    NotifyFindMyMousePropertyChanged();
+                }
+            }
+        }
+
+        public void NotifyFindMyMousePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            OnPropertyChanged(propertyName);
+
+            SndFindMyMouseSettings outsettings = new SndFindMyMouseSettings(FindMyMouseSettingsConfig);
+            SndModuleSettings<SndFindMyMouseSettings> ipcMessage = new SndModuleSettings<SndFindMyMouseSettings>(outsettings);
+            SendConfigMSG(ipcMessage.ToJsonString());
+            SettingsUtils.SaveSettings(FindMyMouseSettingsConfig.ToJsonString(), FindMyMouseSettings.ModuleName);
         }
 
         private Func<string, int> SendConfigMSG { get; }
 
         private bool _isFindMyMouseEnabled;
+        private bool _findMyMouseDoNotActivateOnGameMode;
     }
 }
