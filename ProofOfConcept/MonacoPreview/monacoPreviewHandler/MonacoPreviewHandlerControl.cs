4d37cca44156725946d12d52edd18c1ad0310e60
Aaron-Junker <aaron.junker@edu.sh.ch>
2021-11-04 16:11:38 +0100

    push

---
+++ ProofOfConcept/MonacoPreview/monacoPreviewHandler/MonacoPreviewHandlerControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Common;
+using PreviewHandlerCommon;
+
+namespace MonacoPreviewHandler
+{
+    public class MonacoPreviewHandler : FormHandlerControl
+    {
+        public void Test()
+        {
+            System.Console.WriteLine("Hello");
+        }
+    }
+}
