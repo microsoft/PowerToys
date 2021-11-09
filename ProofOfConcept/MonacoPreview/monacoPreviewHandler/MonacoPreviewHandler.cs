b9c439da48198e60dcf604daf656c8c26180df98
Aaron-Junker <aaron.junker@edu.sh.ch>
2021-11-09 17:28:26 +0100

    push

---
+++ ProofOfConcept/MonacoPreview/monacoPreviewHandler/MonacoPreviewHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Common;
+using PreviewHandlerCommon;
+
+
+namespace MonacoPreviewHandler
+{
+    [Guid("afbd5a44-2520-4ae0-9224-6cfce8fe4400")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComVisible(true)]
+    public class MonacoPreviewHandler : FileBasedPreviewHandler, IDisposable
+    {
+        
+        private MonacoPreviewHandlerControl _monacoPreviewHandlerControl;
+        private bool disposedValue;
+
+        public MonacoPreviewHandler()
+        {
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\aaron\\Desktop\\testfolder\\111111.txt"))  
+            {  
+                writer.WriteLine("MonacoPreviewHandlerStart");
+            } 
+            Initialize();
+        }
+        
+        public override void DoPreview()
+        {
+            try
+            {
+                _monacoPreviewHandlerControl.DoPreview(FilePath);
+            }
+            catch (Exception e)
+            {
+                using (StreamWriter writer = new StreamWriter("C:\\Users\\aaron\\Desktop\\testfolder\\111111.txt"))  
+                {  
+                    writer.WriteLine(e.Message);
+                } 
+            }
+        }
+        
+        protected override IPreviewHandlerControl CreatePreviewHandlerControl()
+        {
+            _monacoPreviewHandlerControl = new MonacoPreviewHandlerControl();
+
+            return _monacoPreviewHandlerControl;
+        }
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _monacoPreviewHandlerControl.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+        
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- ProofOfConcept/MonacoPreview/monacoPreviewHandler/MonacoPreviewHandlerControl.cs
+++ ProofOfConcept/MonacoPreview/monacoPreviewHandler/MonacoPreviewHandlerControl.cs
@@ -1,14 +1,51 @@
 ﻿using System;
+using System.Drawing;
+using System.IO.Abstractions;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 using Common;
 using PreviewHandlerCommon;
 
 namespace MonacoPreviewHandler
 {
-    public class MonacoPreviewHandler : FormHandlerControl
+    public class MonacoPreviewHandlerControl : FormHandlerControl
     {
-        public void Test()
+        
+        private static readonly IFileSystem FileSystem = new FileSystem();
+        private static readonly IPath Path = FileSystem.Path;
+        private static readonly IFile File = FileSystem.File;
+        
+        private WebBrowserExt _browser;
+        
+        public MonacoPreviewHandlerControl()
         {
-            System.Console.WriteLine("Hello");
+
         }
+
+        public override void DoPreview<T>(T dataSource)
+        {
+            if (!(dataSource is string filePath))
+            {
+                throw new ArgumentException($"{nameof(dataSource)} for {nameof(MonacoPreviewHandler)} must be a string but was a '{typeof(T)}'");
+            }
+            
+            string fileText = File.ReadAllText(filePath);
+            
+            
+            
+            _browser = new WebBrowserExt
+            {
+                DocumentText = fileText,
+                Dock = DockStyle.Fill,
+                IsWebBrowserContextMenuEnabled = false,
+                ScriptErrorsSuppressed = true,
+                ScrollBarsEnabled = true,
+                AllowNavigation = false,
+            };
+            
+            Controls.Add(_browser);
+
+            base.DoPreview(dataSource);
+            }
     }
 }
