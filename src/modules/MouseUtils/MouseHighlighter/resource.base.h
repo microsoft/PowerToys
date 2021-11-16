c93e0729629fa4b9f19f1f1a28cf8f5c41c55d69
Jaime Bernardo <jaime@janeasystems.com>
2021-11-16 21:56:32 +0000

    Proper resource file generation

---
+++ src/modules/MouseUtils/MouseHighlighter/resource.base.h
@@ -0,0 +1,14 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MouseHighlighter.rc
+
+//////////////////////////////
+// Non-localizable
+
+#define FILE_DESCRIPTION "PowerToys MouseHighlighter"
+#define INTERNAL_NAME "MouseHighlighter"
+#define ORIGINAL_FILENAME "MouseHighlighter.dll"
+#define IDS_KEYBOARDMANAGER_ICON 1001
+
+// Non-localizable
+//////////////////////////////
