1cd00cd49566243958da0ffcecb71cd45466248a
Jaime Bernardo <jaime@janeasystems.com>
2021-11-16 18:05:44 +0000

    New PowerToys template

---
+++ src/modules/MouseUtils/MouseHighlighter/pch.h
@@ -0,0 +1,6 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <ProjectTelemetry.h>
