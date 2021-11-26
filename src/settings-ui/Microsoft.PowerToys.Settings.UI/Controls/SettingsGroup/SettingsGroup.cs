cb16089d0918eb0d473b8e92f23d9e03f91fa18d
niels9001 <niels.laute@live.nl>
2021-11-26 15:01:46 +0100

    Adding ItemsPanel description

--- src/settings-ui/Microsoft.PowerToys.Settings.UI/Controls/SettingsGroup/SettingsGroup.cs
+++ src/settings-ui/Microsoft.PowerToys.Settings.UI/Controls/SettingsGroup/SettingsGroup.cs
@@ -19,11 +19,23 @@ namespace Microsoft.PowerToys.Settings.UI.Controls
     /// </summary>
     [TemplateVisualState(Name = "Normal", GroupName = "CommonStates")]
     [TemplateVisualState(Name = "Disabled", GroupName = "CommonStates")]
+    [TemplatePart(Name = PartDescriptionPresenter, Type = typeof(ContentPresenter))]
     public partial class SettingsGroup : ItemsControl
     {
+        private const string PartDescriptionPresenter = "DescriptionPresenter";
+        private ContentPresenter _descriptionPresenter;
+        private SettingsGroup _settingsGroup;
+
         public SettingsGroup()
         {
             DefaultStyleKey = typeof(SettingsGroup);
+        }
+
+        [Localizable(true)]
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
         }
 
         public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
@@ -33,18 +45,31 @@ namespace Microsoft.PowerToys.Settings.UI.Controls
             new PropertyMetadata(default(string)));
 
         [Localizable(true)]
-        public string Header
+        public object Description
         {
-            get => (string)GetValue(HeaderProperty);
-            set => SetValue(HeaderProperty, value);
+            get => (object)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
         }
+
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+            "Description",
+            typeof(object),
+            typeof(SettingsGroup),
+            new PropertyMetadata(null, OnDescriptionChanged));
 
         protected override void OnApplyTemplate()
         {
             IsEnabledChanged -= SettingsGroup_IsEnabledChanged;
+            _settingsGroup = (SettingsGroup)this;
+            _descriptionPresenter = (ContentPresenter)_settingsGroup.GetTemplateChild(PartDescriptionPresenter);
             SetEnabledState();
             IsEnabledChanged += SettingsGroup_IsEnabledChanged;
             base.OnApplyTemplate();
+        }
+
+        private static void OnDescriptionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((SettingsGroup)d).Update();
         }
 
         private void SettingsGroup_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
@@ -55,6 +80,23 @@ namespace Microsoft.PowerToys.Settings.UI.Controls
         private void SetEnabledState()
         {
             VisualStateManager.GoToState(this, IsEnabled ? "Normal" : "Disabled", true);
+        }
+
+        private void Update()
+        {
+            if (_settingsGroup == null)
+            {
+                return;
+            }
+
+            if (_settingsGroup.Description == null)
+            {
+                _settingsGroup._descriptionPresenter.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                _settingsGroup._descriptionPresenter.Visibility = Visibility.Visible;
+            }
         }
 
         protected override AutomationPeer OnCreateAutomationPeer()
