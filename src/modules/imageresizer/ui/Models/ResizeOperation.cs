9152ea8f1c2f9fcf0ca596d00deac6a3ad50c6b8
CleanCodeDeveloper <16760760+CleanCodeDeveloper@users.noreply.github.com>
2021-12-10 14:54:05 +0100

    [Image Resizer]Try to recover metadata even when the metadata data structure is invalid (#14914)
    * metadata.Clone() fails also for situations where we still can recover metadata
    
    metadata.Clone() is also an expensive operation (deep copy) and it is not necessary anymore as we build up the metadata object from scratch anyway
    
    * If an exception is throw here something is seriously wrong with the metadata structure
    
    We take all metadata we have read so far an write it to the resized image
    
    * add log statement
    
    * Adjust test written for #2447 as we are able to copy the metadata now
    
    * Improve documentation

--- src/modules/imageresizer/ui/Models/ResizeOperation.cs
+++ src/modules/imageresizer/ui/Models/ResizeOperation.cs
@@ -83,17 +83,14 @@ namespace ImageResizer.Models
                     {
                         try
                         {
-                            // Detect whether metadata can copied successfully
-                            var modifiableMetadata = metadata.Clone();
-
 #if DEBUG
                             Debug.WriteLine($"### Processing metadata of file {_file}");
-                            modifiableMetadata.PrintsAllMetadataToDebugOutput();
+                            metadata.PrintsAllMetadataToDebugOutput();
 #endif
 
                             // read all metadata and build up metadata object from the scratch. Discard invalid (unreadable/unwritable) metadata.
                             var newMetadata = new BitmapMetadata(metadata.Format);
-                            var listOfMetadata = modifiableMetadata.GetListOfMetadata();
+                            var listOfMetadata = metadata.GetListOfMetadata();
                             foreach (var (metadataPath, value) in listOfMetadata)
                             {
                                 if (value is BitmapMetadata bitmapMetadata)
