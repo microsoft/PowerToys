8e8c68aa359b62777665b61f5a7517bbc9b423fe
Dustin L. Howett <duhowett@microsoft.com>
2021-12-16 14:40:15 -0600

    [PR INTO CRUTKAS BR] Fix the localization pipeline (#15026)
    * Fix localization
    
    This commit makes a few changes:
    1. It introduces a couple scripts for moving localization files
       around based on Touchdown's output shape. They are well-documented.
    2. It rewrites portions of the RESX->RC converter to avoid resgen and
       handle the new touchdown language types.
    
    * I forgot the cardinal rule of using YAML: Don't
    
    * Fix duplicate keys in Keyboard Editor!
    
    * Up the timeout
    
    * Update the language list in the wxs
    
    * Use IsPipeline
    
    * Special case SvgPreviewHandler >:{
    
    * Push this down to Clint's branch: fix the publish profile
    
    * Reinstate the call to publish.cmd
    
    * Document it
    
    * build the publishing rules

---
+++ tools/build/move-and-rename-resx.ps1
@@ -0,0 +1,34 @@
+[CmdletBinding()]
+
+# This script finds all C#/.NET resx files and renames them from
+# Folder/Language/x.resw to Folder/x.Language.resw, with language
+# mapping. This is required because Touchdown localization uses a
+# different directory structure.
+$Items = Get-ChildItem . -Recurse -Filter *.resx
+
+# Each of the projects we care about stores its resources
+# in a Properties directory. We **DO NOT** want to move
+# resource files from other projects (since we use resx files
+# in standard Win32 projects as well.)
+#
+# **SPECIAL CASE**
+# The SvgPreviewHandler project did not follow this convention.
+# Renaming it now might cause a re-localization event.
+$Items = $Items | Where-Object {
+	$_.Directory.Parent.Name -Eq "Properties" -Or
+		$_.Directory.Parent.Name -Eq "SvgPreviewHandler"
+}
+
+If ($Items.Count -Le 0) {
+	# Nothing to do.
+	Write-Verbose "Nothing to do."
+	Exit 0
+}
+
+ForEach($Item in $Items) {
+	$PropertiesRoot = $Item.Directory.Parent
+	$Language = $Item.Directory.Name
+	$Destination = Join-Path $PropertiesRoot.FullName ("{0}.{1}{2}" -F ($Item.BaseName, $Language, $Item.Extension))
+	Write-Verbose "Renaming $($Item.FullName) to $Destination"
+	Move-Item $Item.FullName $Destination
+}
