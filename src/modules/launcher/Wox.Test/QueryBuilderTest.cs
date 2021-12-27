979d3c6011511c69cc95cba593c4708f64ebdbbc
Heiko <61519853+htcfreek@users.noreply.github.com>
2021-12-27 17:00:07 +0100

    [PT Run] Results only for plugin with longest matching activation phrase  (#15132)
    * first try to fix with new idea
    
    * small changes
    
    * [PT Run] Updated QuryBuilder unit tests
    
    * [PT Run] Updated QueryBuilder test
    
    * [PT Run] Updated QueryBuilder test name
    
    * Improvements
    
    * Improve readability of test
    
    * Improve Query-Tests for spellcheck
    
    * clean expect.txt
    
    Co-authored-by: cyberrex5 <ghassanjad2004@gmail.com>

--- src/modules/launcher/Wox.Test/QueryBuilderTest.cs
+++ src/modules/launcher/Wox.Test/QueryBuilderTest.cs
@@ -16,6 +16,11 @@ namespace Wox.Test
     {
         private static bool AreEqual(Query firstQuery, Query secondQuery)
         {
+            if (firstQuery is null && secondQuery is null)
+            {
+                return true;
+            }
+
             // Using Ordinal since this is used internally
             return firstQuery.ActionKeyword.Equals(secondQuery.ActionKeyword, StringComparison.Ordinal)
                 && firstQuery.Search.Equals(secondQuery.Search, StringComparison.Ordinal)
@@ -69,8 +74,8 @@ namespace Wox.Test
                 plugin,
             });
 
-            var firstQueryText = "asearch";
-            var secondQueryText = "a search";
+            var firstQueryText = "aSearch";
+            var secondQueryText = "a Search";
 
             // Act
             var firstPluginQueryPair = QueryBuilder.Build(firstQueryText);
@@ -86,12 +91,13 @@ namespace Wox.Test
         }
 
         [TestMethod]
-        public void QueryBuildShouldGenerateCorrectQueryForPluginsWhoseActionKeywordsHaveSamePrefix()
+        public void QueryBuildShouldGenerateQueriesOnlyForPluginsWhoseActionKeywordsAreLongestIfMultipleMatch()
         {
             // Arrange
-            string searchQuery = "abcdefgh";
-            var firstPlugin = new PluginPair(new PluginMetadata { ActionKeyword = "ab", ID = "plugin1" });
-            var secondPlugin = new PluginPair(new PluginMetadata { ActionKeyword = "abcd", ID = "plugin2" });
+            string firstSearchQuery = "MyKeyword";
+            string secondSearchQuery = "My Keyword";
+            var firstPlugin = new PluginPair(new PluginMetadata { ActionKeyword = "My", ID = "plugin1" });
+            var secondPlugin = new PluginPair(new PluginMetadata { ActionKeyword = "MyKey", ID = "plugin2" });
             PluginManager.SetAllPlugins(new List<PluginPair>()
             {
                 firstPlugin,
@@ -99,46 +105,50 @@ namespace Wox.Test
             });
 
             // Act
-            var pluginQueryPairs = QueryBuilder.Build(searchQuery);
+            var firstSearchQueryPluginPair = QueryBuilder.Build(firstSearchQuery);
+            var firstSearchQueryFirstPlugin = firstSearchQueryPluginPair.GetValueOrDefault(firstPlugin);
+            var firstSearchQuerySecondPlugin = firstSearchQueryPluginPair.GetValueOrDefault(secondPlugin);
 
-            var firstQuery = pluginQueryPairs.GetValueOrDefault(firstPlugin);
-            var secondQuery = pluginQueryPairs.GetValueOrDefault(secondPlugin);
+            var secondSearchQueryPluginPair = QueryBuilder.Build(secondSearchQuery);
+            var secondSearchQueryFirstPlugin = secondSearchQueryPluginPair.GetValueOrDefault(firstPlugin);
+            var secondSearchQuerySecondPlugin = secondSearchQueryPluginPair.GetValueOrDefault(secondPlugin);
 
             // Assert
-            Assert.IsTrue(AreEqual(firstQuery, new Query(searchQuery, firstPlugin.Metadata.ActionKeyword)));
-            Assert.IsTrue(AreEqual(secondQuery, new Query(searchQuery, secondPlugin.Metadata.ActionKeyword)));
+            Assert.IsTrue(AreEqual(firstSearchQueryFirstPlugin, null));
+            Assert.IsTrue(AreEqual(firstSearchQuerySecondPlugin, new Query(firstSearchQuery, secondPlugin.Metadata.ActionKeyword)));
+            Assert.IsTrue(AreEqual(secondSearchQueryFirstPlugin, new Query(secondSearchQuery, firstPlugin.Metadata.ActionKeyword)));
+            Assert.IsTrue(AreEqual(secondSearchQuerySecondPlugin, null));
         }
 
         [TestMethod]
         public void QueryBuilderShouldSetTermsCorrectlyWhenCalled()
         {
             // Arrange
-            string searchQuery = "abcd efgh";
-            var firstPlugin = new PluginPair(new PluginMetadata { ActionKeyword = "ab", ID = "plugin1" });
-            var secondPlugin = new PluginPair(new PluginMetadata { ActionKeyword = "abcd", ID = "plugin2" });
+            string searchQuery = "MyTest search term";
+            var plugin = new PluginPair(new PluginMetadata { ActionKeyword = "My", ID = "plugin1" });
             PluginManager.SetAllPlugins(new List<PluginPair>()
             {
-                firstPlugin,
-                secondPlugin,
+                plugin,
             });
 
             // Act
             var pluginQueryPairs = QueryBuilder.Build(searchQuery);
 
-            var firstQuery = pluginQueryPairs.GetValueOrDefault(firstPlugin);
-            var secondQuery = pluginQueryPairs.GetValueOrDefault(secondPlugin);
+            var builtQuery = pluginQueryPairs.GetValueOrDefault(plugin);
 
             // Assert
             // Using Ordinal since this is used internally
-            Assert.IsTrue(firstQuery.Terms[0].Equals("cd", StringComparison.Ordinal) && firstQuery.Terms[1].Equals("efgh", StringComparison.Ordinal) && firstQuery.Terms.Count == 2);
-            Assert.IsTrue(secondQuery.Terms[0].Equals("efgh", StringComparison.Ordinal) && secondQuery.Terms.Count == 1);
+            Assert.IsTrue(builtQuery.Terms.Count == 3
+                && builtQuery.Terms[0].Equals("Test", StringComparison.Ordinal)
+                && builtQuery.Terms[1].Equals("search", StringComparison.Ordinal)
+                && builtQuery.Terms[2].Equals("term", StringComparison.Ordinal));
         }
 
         [TestMethod]
         public void QueryBuilderShouldReturnAllPluginsWithTheActionWord()
         {
             // Arrange
-            string searchQuery = "!efgh";
+            string searchQuery = "!Query";
             var firstPlugin = new PluginPair(new PluginMetadata { ActionKeyword = "!", ID = "plugin1" });
             var secondPlugin = new PluginPair(new PluginMetadata { ActionKeyword = "!", ID = "plugin2" });
             PluginManager.SetAllPlugins(new List<PluginPair>()
