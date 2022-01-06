f657825b49900af96c67cbba330835ddfd950a0f
Clint Rutkas <clint@rutkas.com>
2022-01-06 13:42:15 -0800

    ps script for validation

---
+++ .pipelines/versionAndSignCheck.ps1
@@ -0,0 +1,36 @@
+[CmdletBinding()]
+# todo: send in arch / conf
+Param()
+
+$DirPath = $PSScriptRoot + '/../x64/release';
+Write-Host $DirPath;
+
+$totalFailure = 0;
+
+Get-ChildItem -Path $DirPath -File -Recurse -Force -ErrorAction SilentlyContinue | 
+ForEach-Object { 
+    if($_.VersionInfo.FileVersion -eq "0.0.1.0" -or $_.VersionInfo.FileVersion -eq "1.0.0.0" )
+	{
+		if($_.Name -ne "Microsoft.Search.Interop.dll")
+		{
+			Write-Host "Not Versioned: " + $_.FullName
+			$totalFailure++;
+		}
+	}
+}
+
+Get-ChildItem -Path $DirPath -File -Recurse -Force -ErrorAction SilentlyContinue | 
+ForEach-Object { 
+    if($_.VersionInfo.FileVersion -eq "0.0.1.0" -or $_.VersionInfo.FileVersion -eq "1.0.0.0" )
+	{
+		Write-Host "Not Versioned: " + $_.FullName
+		$totalFailure++;
+	}
+}
+
+if($totalFailure -gt 0)
+{
+	exit 1
+}
+
+exit 0
