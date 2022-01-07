8cd17845dec36d2dd7f463807c677433357858d5
Aaron-Junker <aaron.junker@outlook.com>
2022-01-07 20:14:12 +0100

    Deleted everything from the monaco source code expect the minified version

--- ProofOfConcept/MonacoPreview/monacoPreviewHandler/monacoSRC/esm/vs/platform/clipboard/common/clipboardService.js
+++
@@ -1,6 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-import { createDecorator } from '../../instantiation/common/instantiation.js';
-export const IClipboardService = createDecorator('clipboardService');
--- ProofOfConcept/MonacoPreview/monacoPreviewHandler/monacoSRC/esm/vs/platform/files/common/files.js
+++
@@ -1,6 +0,0 @@
-export var FileKind;
-(function (FileKind) {
-    FileKind[FileKind["FILE"] = 0] = "FILE";
-    FileKind[FileKind["FOLDER"] = 1] = "FOLDER";
-    FileKind[FileKind["ROOT_FOLDER"] = 2] = "ROOT_FOLDER";
-})(FileKind || (FileKind = {}));
