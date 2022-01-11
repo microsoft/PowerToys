fd2a719adeb1ee926bdbe46ec773232440fa20b3
htcfreek <61519853+htcfreek@users.noreply.github.com>
2022-01-11 14:27:02 +0100

    update debug tool tip

--- src/modules/launcher/Plugins/Microsoft.Plugin.WindowWalker/Main.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.WindowWalker/Main.cs
@@ -49,7 +49,7 @@ namespace Microsoft.Plugin.WindowWalker
                     x.Result.SwitchToWindow();
                     return true;
                 },
-                ToolTipData = new ToolTipData(x.Result.Title, $"hWnd: {x.Result.Hwnd}\nProcess ID: {x.Result.ProcessInfo.ProcessID}\nThread ID: {x.Result.ProcessInfo.ThreadID}\nProcess: {x.Result.ProcessInfo.Name}\nIs running elevated: {x.Result.ProcessInfo.IsRunningElevated}\nIs uwp app: {x.Result.ProcessInfo.IsUwpApp}\nWindow cloak state: {x.Result.GetWindowCloakState()}"),
+                ToolTipData = new ToolTipData(x.Result.Title, $"hWnd: {x.Result.Hwnd}\nProcess ID: {x.Result.ProcessInfo.ProcessID}\nThread ID: {x.Result.ProcessInfo.ThreadID}\nProcess: {x.Result.ProcessInfo.Name}\nIs running elevated: {x.Result.ProcessInfo.IsRunningElevated}\nIs uwp app: {x.Result.ProcessInfo.IsUwpApp}\nIs window cloaked: {x.Result.IsCloaked}\nWindow cloak state: {x.Result.GetWindowCloakState()}"),
             }).ToList();
         }
 
