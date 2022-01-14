52c161df951c720f48c52d5607589affeb6698e6
Jaime Bernardo <jaime@janeasystems.com>
2022-01-14 13:28:18 +0000

    Proper resource file generation

---
+++ src/modules/MouseUtils/InclusiveMouse/rsource.base.h
@@ -0,0 +1,14 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by InclusiveMouse.rc
+
+//////////////////////////////
+// Non-localizable
+
+#define FILE_DESCRIPTION "PowerToys InclusiveMouse"
+#define INTERNAL_NAME "InclusiveMouse"
+#define ORIGINAL_FILENAME "InclusiveMouse.dll"
+#define IDS_KEYBOARDMANAGER_ICON 1001
+
+// Non-localizable
+//////////////////////////////
