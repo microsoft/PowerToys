9bb4ab18c4cca29d386346282f17ee0408484952
Clint Rutkas <clint@rutkas.com>
2022-01-23 13:40:08 -0800

    tweaking script to ignore 1.0 check on theme dll

--- .pipelines/versionAndSignCheck.ps1
+++ .pipelines/versionAndSignCheck.ps1
@@ -28,7 +28,11 @@ if($items.Count -eq 0)
 $items | ForEach-Object { 
     if($_.VersionInfo.FileVersion -eq "1.0.0.0" )
 	{
-		if(-not $_.Name.EndsWith("Microsoft.Search.Interop.dll"))
+		if(-not (
+			$_.Name.EndsWith("Microsoft.Search.Interop.dll") -or
+			$_.Name.EndsWith("System.Personalization.dll")
+			)
+		)
 		{
 			Write-Host "Version not set: " + $_.FullName
 			$totalFailure++;
