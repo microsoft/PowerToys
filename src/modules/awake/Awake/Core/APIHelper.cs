c351439f16367184a243060ef47d7b8853a545c0
Christopher Greaves <ChristopherG@thomas.co>
2022-02-24 19:10:22 +0000

    Update sound file location logic to support both SettingsUI and CLI

--- src/modules/awake/Awake/Core/APIHelper.cs
+++ src/modules/awake/Awake/Core/APIHelper.cs
@@ -40,7 +40,16 @@ namespace Awake.Core
             _timedLoopTimer = new System.Timers.Timer();
             _log = LogManager.GetCurrentClassLogger();
             _tokenSource = new CancellationTokenSource();
-            _soundPlayer = new SoundPlayer("modules/Awake/Sounds/anti-sleep.wav");
+
+            if (File.Exists("modules/Awake/Sounds/anti-sleep.wav"))
+            {
+                _soundPlayer = new SoundPlayer("modules/Awake/Sounds/anti-sleep.wav");
+            }
+            else
+            {
+                _soundPlayer = new SoundPlayer("Sounds/anti-sleep.wav");
+            }
+
             _soundPlayer.Load();
         }
 
