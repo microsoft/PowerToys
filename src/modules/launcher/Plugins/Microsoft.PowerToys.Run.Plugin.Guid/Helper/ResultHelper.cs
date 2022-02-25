765108b8c76f36d3b3ce7b92d47bf468d528737c
Sekan, Tobias <tobias.sekan@startmail.com>
2022-02-25 08:13:51 +0100

    Guid plugin initial commit

---
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.Guid/Helper/ResultHelper.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Wox.Plugin;
+
+namespace Microsoft.PowerToys.Run.Plugin.Guid.Helper
+{
+    /// <summary>
+    /// Helper class to easier work with results
+    /// </summary>
+    internal static class ResultHelper
+    {
+        /// <summary>
+        /// Return a list with <see cref="Result"/>s, based on the given list.
+        /// </summary>
+        /// <param name="iconPath">The path to the icon of each entry.</param>
+        /// <returns>A list with <see cref="Result"/>.</returns>
+        internal static IEnumerable<Result> GetResults(Query query, in string iconPath)
+        {
+            var guid = System.Guid.NewGuid();
+
+            var reverseSearch = string.IsNullOrEmpty(query.Search)
+                ? string.Empty
+                : string.Join(string.Empty, query.Search.Reverse());
+
+            var left = query.Search
+                .Replace('}', '{')
+                .Replace(')', '(')
+                .Replace(']', '[');
+
+            var right = reverseSearch
+                .Replace('{', '}')
+                .Replace('(', ')')
+                .Replace('[', ']');
+
+            var resultList = new List<Result>
+            {
+                new Result
+                {
+                    IcoPath = iconPath,
+                    Title = $"{left}{guid.ToString("N").ToUpper()}{right}",
+                    SubTitle = $"{left}{guid.ToString("N").ToLower()}{right}",
+                    ContextData = $"{left}{guid.ToString("N").ToUpper()}{right}",
+                },
+                new Result
+                {
+                    IcoPath = iconPath,
+                    Title = $"{left}{guid.ToString("D").ToUpper()}{right}",
+                    SubTitle = $"{left}{guid.ToString("D").ToLower()}{right}",
+                    ContextData = $"{left}{guid.ToString("D").ToUpper()}{right}",
+                },
+
+                new Result
+                {
+                    IcoPath = iconPath,
+                    Title = $"{left}urn:uuid:{guid.ToString("D").ToUpper()}{right}",
+                    SubTitle = $"Uniform Resource Name (URN)",
+                    ContextData = $"{left}urn:uuid:{guid.ToString("D").ToUpper()}{right}",
+                },
+            };
+
+            return resultList;
+        }
+    }
+}
---
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.Guid/Main.cs
@@ -0,0 +1,168 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ManagedCommon;
+using Microsoft.PowerToys.Run.Plugin.Guid.Helper;
+using Microsoft.PowerToys.Run.Plugin.Guid.Properties;
+using Wox.Plugin;
+
+namespace Microsoft.PowerToys.Run.Plugin.Guid
+{
+    /// <summary>
+    /// A power launcher plugin to search across time zones.
+    /// </summary>
+    public class Main : IPlugin, IContextMenu, IPluginI18n, IDisposable
+    {
+        /// <summary>
+        /// The name of this assembly
+        /// </summary>
+        private readonly string _assemblyName;
+
+        /// <summary>
+        /// The initial context for this plugin (contains API and meta-data)
+        /// </summary>
+        private PluginInitContext? _context;
+
+        /// <summary>
+        /// The path to the icon for each result
+        /// </summary>
+        private string _defaultIconPath;
+
+        /// <summary>
+        /// Indicate that the plugin is disposed
+        /// </summary>
+        private bool _disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Main"/> class.
+        /// </summary>
+        public Main()
+        {
+            _assemblyName = Assembly.GetExecutingAssembly().GetName().Name ?? GetTranslatedPluginTitle();
+            _defaultIconPath = "Images/timeZone.light.png";
+        }
+
+        /// <summary>
+        /// Gets the localized name.
+        /// </summary>
+        public string Name
+        {
+            get { return Resources.PluginTitle; }
+        }
+
+        /// <summary>
+        /// Gets the localized description.
+        /// </summary>
+        public string Description
+        {
+            get { return Resources.PluginDescription; }
+        }
+
+        /// <summary>
+        /// Initialize the plugin with the given <see cref="PluginInitContext"/>
+        /// </summary>
+        /// <param name="context">The <see cref="PluginInitContext"/> for this plugin</param>
+        public void Init(PluginInitContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _context.API.ThemeChanged += OnThemeChanged;
+            UpdateIconPath(_context.API.GetCurrentTheme());
+        }
+
+        /// <summary>
+        /// Return a filtered list, based on the given query
+        /// </summary>
+        /// <param name="query">The query to filter the list</param>
+        /// <returns>A filtered list, can be empty when nothing was found</returns>
+        public List<Result> Query(Query query)
+         {
+            if (query is null)
+            {
+                return new List<Result>(0);
+            }
+
+            var results = ResultHelper.GetResults(query, _defaultIconPath);
+            return results.ToList();
+        }
+
+        /// <summary>
+        /// Return a list context menu entries for a given <see cref="Result"/> (shown at the right side of the result)
+        /// </summary>
+        /// <param name="selectedResult">The <see cref="Result"/> for the list with context menu entries</param>
+        /// <returns>A list context menu entries</returns>
+        public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
+        {
+            return ContextMenuHelper.GetContextMenu(selectedResult, _assemblyName);
+        }
+
+        /// <summary>
+        /// Change all theme-based elements (typical called when the plugin theme has changed)
+        /// </summary>
+        /// <param name="oldtheme">The old <see cref="Theme"/></param>
+        /// <param name="newTheme">The new <see cref="Theme"/></param>
+        private void OnThemeChanged(Theme oldtheme, Theme newTheme)
+        {
+            UpdateIconPath(newTheme);
+        }
+
+        /// <summary>
+        /// Update all icons (typical called when the plugin theme has changed)
+        /// </summary>
+        /// <param name="theme">The new <see cref="Theme"/> for the icons</param>
+        private void UpdateIconPath(Theme theme)
+        {
+            _defaultIconPath = theme is Theme.Light or Theme.HighContrastWhite
+                ? "Images/timeZone.light.png"
+                : "Images/timeZone.dark.png";
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Wrapper method for <see cref="Dispose"/> that dispose additional objects and events form the plugin itself
+        /// </summary>
+        /// <param name="disposing">Indicate that the plugin is disposed</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed || !disposing)
+            {
+                return;
+            }
+
+            if (_context is not null)
+            {
+                _context.API.ThemeChanged -= OnThemeChanged;
+            }
+
+            _disposed = true;
+        }
+
+        /// <summary>
+        /// Return the translated plugin title.
+        /// </summary>
+        /// <returns>A translated plugin title.</returns>
+        public string GetTranslatedPluginTitle()
+        {
+            return Resources.PluginTitle;
+        }
+
+        /// <summary>
+        /// Return the translated plugin description.
+        /// </summary>
+        /// <returns>A translated plugin description.</returns>
+        public string GetTranslatedPluginDescription()
+        {
+            return Resources.PluginDescription;
+        }
+    }
+}
