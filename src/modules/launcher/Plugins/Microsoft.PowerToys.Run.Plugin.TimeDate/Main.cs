30ee838a1bb91dcec48e414da1659c5e16e0732a
htcfreek <61519853+htcfreek@users.noreply.github.com>
2022-03-01 12:37:50 +0100

    Update plugin code

--- src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.TimeDate/Main.cs
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.TimeDate/Main.cs
@@ -4,73 +4,43 @@
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Interop;
 using ManagedCommon;
+using Microsoft.PowerToys.Run.Plugin.TimeDate.Classes;
 using Microsoft.PowerToys.Run.Plugin.TimeDate.Properties;
 using Microsoft.PowerToys.Settings.UI.Library;
 using Wox.Infrastructure;
 using Wox.Plugin;
-using Wox.Plugin.Common.Win32;
+using Wox.Plugin.Logger;
 
 namespace Microsoft.PowerToys.Run.Plugin.TimeDate
 {
     public class Main : IPlugin, IPluginI18n, ISettingProvider
     {
         private PluginInitContext _context;
-        private const string ConfirmSystemCommands = nameof(ConfirmSystemCommands);
-        private const string LocalizeSystemCommands = nameof(LocalizeSystemCommands);
-
-        internal const int EWXLOGOFF = 0x00000000;
-        internal const int EWXSHUTDOWN = 0x00000001;
-        internal const int EWXREBOOT = 0x00000002;
-        internal const int EWXFORCE = 0x00000004;
-        internal const int EWXPOWEROFF = 0x00000008;
-        internal const int EWXFORCEIFHUNG = 0x00000010;
 
         public string IconTheme { get; set; }
 
         public bool IsBootedInUefiMode { get; set; }
 
-        public string Name => Resources.Microsoft_plugin_sys_plugin_name;
+        public string Name => Resources.Microsoft_plugin_timedate_plugin_name;
 
-        public string Description => Resources.Microsoft_plugin_sys_plugin_description;
+        public string Description => Resources.Microsoft_plugin_timedate_plugin_description;
 
-        private bool _confirmSystemCommands;
-        private bool _localizeSystemCommands;
-
-        public IEnumerable<PluginAdditionalOption> AdditionalOptions => new List<PluginAdditionalOption>()
+        public IEnumerable<PluginAdditionalOption> AdditionalOptions
         {
-            new PluginAdditionalOption()
+            get
             {
-                Key = ConfirmSystemCommands,
-                DisplayLabel = Resources.confirm_system_commands,
-                Value = false,
-            },
-            new PluginAdditionalOption()
-            {
-                Key = LocalizeSystemCommands,
-                DisplayLabel = Resources.Use_localized_system_commands,
-                Value = true,
-            },
-        };
+                return TimeDateSettings.GetAdditionalOptions();
+            }
+        }
 
         public void Init(PluginInitContext context)
         {
             _context = context;
             _context.API.ThemeChanged += OnThemeChanged;
             UpdateIconTheme(_context.API.GetCurrentTheme());
-            IsBootedInUefiMode = Win32Helpers.GetSystemFirmwareType() == FirmwareType.Uefi;
-
-            // Log info if the system hasn't boot in uefi mode.
-            // (Because this is only going into the log we can ignore the fact that normally UEFI and BIOS are written upper case. No need to convert the enumeration value to upper case.)
-            if (!IsBootedInUefiMode)
-            {
-                Wox.Plugin.Logger.Log.Info($"The UEFI command will not show to the user. The system has not booted in UEFI mode or the system does not have an UEFI firmware! (Detected type: {Win32Helpers.GetSystemFirmwareType()})", typeof(Main));
-            }
         }
 
         public List<Result> Query(Query query)
@@ -85,11 +55,11 @@ namespace Microsoft.PowerToys.Run.Plugin.TimeDate
 
             foreach (var c in commands)
             {
-                var titleMatch = StringMatcher.FuzzySearch(query.Search, c.Title);
-                if (titleMatch.Score > 0)
+                var resultMatch = StringMatcher.FuzzySearch(query.Search, c.ContextData.ToString());
+                if (resultMatch.Score > 0)
                 {
-                    c.Score = titleMatch.Score;
-                    c.TitleHighlightData = titleMatch.MatchData;
+                    c.Score = resultMatch.Score;
+                    c.SubTitleHighlightData = resultMatch.MatchData;
                     results.Add(c);
                 }
             }
@@ -99,114 +69,18 @@ namespace Microsoft.PowerToys.Run.Plugin.TimeDate
 
         private List<Result> Commands()
         {
-            CultureInfo culture = CultureInfo.CurrentUICulture;
-
-            if (!_localizeSystemCommands)
-            {
-                culture = new CultureInfo("en-US");
-            }
-
             var results = new List<Result>();
             results.AddRange(new[]
             {
-                new Result
+                new Result()
                 {
-                    Title = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_shutdown_computer), culture),
-                    SubTitle = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_shutdown_computer_description), culture),
-                    IcoPath = $"Images\\shutdown.{IconTheme}.png",
-                    Action = c =>
-                    {
-                        return ExecuteCommand(Resources.Microsoft_plugin_sys_shutdown_computer_confirmation, () => Helper.OpenInShell("shutdown", "/s /hybrid /t 0"));
-                    },
-                },
-                new Result
-                {
-                    Title = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_restart_computer), culture),
-                    SubTitle = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_restart_computer_description), culture),
-                    IcoPath = $"Images\\restart.{IconTheme}.png",
-                    Action = c =>
-                    {
-                        return ExecuteCommand(Resources.Microsoft_plugin_sys_restart_computer_confirmation, () => Helper.OpenInShell("shutdown", "/r /t 0"));
-                    },
-                },
-                new Result
-                {
-                    Title = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_sign_out), culture),
-                    SubTitle = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_sign_out_description), culture),
-                    IcoPath = $"Images\\logoff.{IconTheme}.png",
-                    Action = c =>
-                    {
-                        return ExecuteCommand(Resources.Microsoft_plugin_sys_sign_out_confirmation, () => NativeMethods.ExitWindowsEx(EWXLOGOFF, 0));
-                    },
-                },
-                new Result
-                {
-                    Title = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_lock), culture),
-                    SubTitle = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_lock_description), culture),
-                    IcoPath = $"Images\\lock.{IconTheme}.png",
-                    Action = c =>
-                    {
-                        return ExecuteCommand(Resources.Microsoft_plugin_sys_lock_confirmation, () => NativeMethods.LockWorkStation());
-                    },
-                },
-                new Result
-                {
-                    Title = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_sleep), culture),
-                    SubTitle = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_sleep_description), culture),
-                    IcoPath = $"Images\\sleep.{IconTheme}.png",
-                    Action = c =>
-                    {
-                        return ExecuteCommand(Resources.Microsoft_plugin_sys_sleep_confirmation, () => NativeMethods.SetSuspendState(false, true, true));
-                    },
-                },
-                new Result
-                {
-                    Title = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_hibernate), culture),
-                    SubTitle = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_hibernate_description), culture),
-                    IcoPath = $"Images\\sleep.{IconTheme}.png", // Icon change needed
-                    Action = c =>
-                    {
-                        return ExecuteCommand(Resources.Microsoft_plugin_sys_hibernate_confirmation, () => NativeMethods.SetSuspendState(true, true, true));
-                    },
-                },
-                new Result
-                {
-                    Title = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_emptyrecyclebin), culture),
-                    SubTitle = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_emptyrecyclebin_description), culture),
-                    IcoPath = $"Images\\recyclebin.{IconTheme}.png",
-                    Action = c =>
-                    {
-                        // http://www.pinvoke.net/default.aspx/shell32/SHEmptyRecycleBin.html
-                        // FYI, couldn't find documentation for this but if the recycle bin is already empty, it will return -2147418113 (0x8000FFFF (E_UNEXPECTED))
-                        // 0 for nothing
-                        var result = NativeMethods.SHEmptyRecycleBin(new WindowInteropHelper(Application.Current.MainWindow).Handle, 0);
-                        if (result != (uint)HRESULT.S_OK && result != 0x8000FFFF)
-                        {
-                            var name = "Plugin: " + Resources.Microsoft_plugin_sys_plugin_name;
-                            var message = $"Error emptying recycle bin, error code: {result}\n" +
-                                          "please refer to https://msdn.microsoft.com/en-us/library/windows/desktop/aa378137";
-                            _context.API.ShowMsg(name, message);
-                        }
-
-                        return true;
-                    },
+                    Title = "Time",
+                    SubTitle = "Test result",
+                    IcoPath = $"Images\\datetime.{IconTheme}.png",
+                    Action = _ => TryToCopyToClipBoard("text dummy"),
+                    ContextData = "SearchTerm",
                 },
             });
-
-            // UEFI command/result. It is only available on systems booted in UEFI mode.
-            if (IsBootedInUefiMode)
-            {
-                results.Add(new Result
-                {
-                    Title = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_uefi), culture),
-                    SubTitle = Resources.ResourceManager.GetString(nameof(Resources.Microsoft_plugin_sys_uefi_description), culture),
-                    IcoPath = $"Images\\firmwareSettings.{IconTheme}.png",
-                    Action = c =>
-                    {
-                        return ExecuteCommand(Resources.Microsoft_plugin_sys_uefi_confirmation, () => Helper.OpenInShell("shutdown", "/r /fw /t 0", null, true));
-                    },
-                });
-            }
 
             return results;
         }
@@ -230,32 +104,33 @@ namespace Microsoft.PowerToys.Run.Plugin.TimeDate
 
         public string GetTranslatedPluginDescription()
         {
-            return Resources.Microsoft_plugin_sys_plugin_description;
+            return Resources.Microsoft_plugin_timedate_plugin_description;
         }
 
         public string GetTranslatedPluginTitle()
         {
-            return Resources.Microsoft_plugin_sys_plugin_name;
+            return Resources.Microsoft_plugin_timedate_plugin_name;
         }
 
-        private bool ExecuteCommand(string confirmationMessage, Action command)
+        /// <summary>
+        /// Copy the given text to the clipboard
+        /// </summary>
+        /// <param name="text">The text to copy to the clipboard</param>
+        /// <returns><see langword="true"/>The text successful copy to the clipboard, otherwise <see langword="false"/></returns>
+        /// <remarks>Code copied from TimeZone plugin</remarks>
+        private static bool TryToCopyToClipBoard(in string text)
         {
-            if (_confirmSystemCommands)
+            try
             {
-                MessageBoxResult messageBoxResult = MessageBox.Show(
-                    confirmationMessage,
-                    Resources.Microsoft_plugin_sys_confirmation,
-                    MessageBoxButton.YesNo,
-                    MessageBoxImage.Warning);
-
-                if (messageBoxResult == MessageBoxResult.No)
-                {
-                    return false;
-                }
+                Clipboard.Clear();
+                Clipboard.SetText(text);
+                return true;
             }
-
-            command();
-            return true;
+            catch (Exception exception)
+            {
+                Log.Exception("Can't copy to clipboard", exception, typeof(Main));
+                return false;
+            }
         }
 
         public Control CreateSettingPanel()
@@ -265,20 +140,7 @@ namespace Microsoft.PowerToys.Run.Plugin.TimeDate
 
         public void UpdateSettings(PowerLauncherPluginSettings settings)
         {
-            var confirmSystemCommands = false;
-            var localizeSystemCommands = true;
-
-            if (settings != null && settings.AdditionalOptions != null)
-            {
-                var optionConfirm = settings.AdditionalOptions.FirstOrDefault(x => x.Key == ConfirmSystemCommands);
-                confirmSystemCommands = optionConfirm?.Value ?? false;
-
-                var optionLocalize = settings.AdditionalOptions.FirstOrDefault(x => x.Key == LocalizeSystemCommands);
-                localizeSystemCommands = optionLocalize?.Value ?? true;
-            }
-
-            _confirmSystemCommands = confirmSystemCommands;
-            _localizeSystemCommands = localizeSystemCommands;
+            TimeDateSettings.Instance.UpdateSettings(settings);
         }
     }
 }
--- src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.TimeDate/Properties/Resources.Designer.cs
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.TimeDate/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.PowerToys.Run.Plugin.TimeDate.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -39,7 +39,7 @@ namespace Microsoft.PowerToys.Run.Plugin.TimeDate.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.PowerToys.Run.Plugin.System.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.PowerToys.Run.Plugin.TimeDate.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -61,254 +61,38 @@ namespace Microsoft.PowerToys.Run.Plugin.TimeDate.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Show a dialog to confirm system commands.
+        ///   Looks up a localized string similar to Provide actual time and date informations.
         /// </summary>
-        internal static string confirm_system_commands {
+        internal static string Microsoft_plugin_timedate_plugin_description {
             get {
-                return ResourceManager.GetString("confirm_system_commands", resourceCulture);
+                return ResourceManager.GetString("Microsoft_plugin_timedate_plugin_description", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Please confirm..
+        ///   Looks up a localized string similar to Time and Date.
         /// </summary>
-        internal static string Microsoft_plugin_sys_confirmation {
+        internal static string Microsoft_plugin_timedate_plugin_name {
             get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_confirmation", resourceCulture);
+                return ResourceManager.GetString("Microsoft_plugin_timedate_plugin_name", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Empty Recycle Bin.
+        ///   Looks up a localized string similar to Show date with weekday.
         /// </summary>
-        internal static string Microsoft_plugin_sys_emptyrecyclebin {
+        internal static string Microsoft_plugin_timedate_plugin_settingDateWithWekday {
             get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_emptyrecyclebin", resourceCulture);
+                return ResourceManager.GetString("Microsoft_plugin_timedate_plugin_settingDateWithWekday", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Empty Recycle Bin.
+        ///   Looks up a localized string similar to Show time with seconds.
         /// </summary>
-        internal static string Microsoft_plugin_sys_emptyrecyclebin_description {
+        internal static string Microsoft_plugin_timedate_plugin_settingTimeWithSeconds {
             get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_emptyrecyclebin_description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Hibernate.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_hibernate {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_hibernate", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to You are about to put this computer into hibernation, are you sure?.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_hibernate_confirmation {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_hibernate_confirmation", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Hibernate computer.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_hibernate_description {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_hibernate_description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Lock.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_lock {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_lock", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to You are about to lock this computer, are you sure?.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_lock_confirmation {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_lock_confirmation", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Lock computer.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_lock_description {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_lock_description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Executes system commands (e.g. &apos;shutdown&apos;, &apos;lock&apos;, &apos;sleep&apos;)..
-        /// </summary>
-        internal static string Microsoft_plugin_sys_plugin_description {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_plugin_description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Windows System Commands.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_plugin_name {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_plugin_name", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Restart.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_restart_computer {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_restart_computer", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to You are about to restart this computer, are you sure?.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_restart_computer_confirmation {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_restart_computer_confirmation", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Restart computer.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_restart_computer_description {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_restart_computer_description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Shutdown.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_shutdown_computer {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_shutdown_computer", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to You are about to shut down this computer, are you sure?.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_shutdown_computer_confirmation {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_shutdown_computer_confirmation", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Shutdown computer.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_shutdown_computer_description {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_shutdown_computer_description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Sign out.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_sign_out {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_sign_out", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to You are about to sign out of this computer, are you sure?.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_sign_out_confirmation {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_sign_out_confirmation", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Sign out of computer.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_sign_out_description {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_sign_out_description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Sleep.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_sleep {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_sleep", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to You are about to put this computer to sleep, are you sure?.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_sleep_confirmation {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_sleep_confirmation", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Put computer to sleep.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_sleep_description {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_sleep_description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to UEFI Firmware Settings.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_uefi {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_uefi", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to You are about to reboot this computer into UEFI Firmware Settings menu, are you sure?.
-        /// </summary>
-        internal static string Microsoft_plugin_sys_uefi_confirmation {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_uefi_confirmation", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Reboot computer into UEFI Firmware Settings (Requires administrative permissions.).
-        /// </summary>
-        internal static string Microsoft_plugin_sys_uefi_description {
-            get {
-                return ResourceManager.GetString("Microsoft_plugin_sys_uefi_description", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Use localized system commands instead of English ones.
-        /// </summary>
-        internal static string Use_localized_system_commands {
-            get {
-                return ResourceManager.GetString("Use_localized_system_commands", resourceCulture);
+                return ResourceManager.GetString("Microsoft_plugin_timedate_plugin_settingTimeWithSeconds", resourceCulture);
             }
         }
     }
