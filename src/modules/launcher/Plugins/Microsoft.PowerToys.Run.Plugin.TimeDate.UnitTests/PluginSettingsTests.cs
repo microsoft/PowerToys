46fb582defec6da67a1c7ac4a1a52cfae92b7c1a
htcfreek <61519853+htcfreek@users.noreply.github.com>
2022-03-04 18:43:56 +0100

    add settings tests

---
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.TimeDate.UnitTests/PluginSettingsTests.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Reflection;
+using Microsoft.PowerToys.Run.Plugin.TimeDate.Components;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.PowerToys.Run.Plugin.TimeDate.UnitTests
+{
+    [TestClass]
+    public class PluginSettingsTests
+    {
+        [TestMethod]
+        public void SettingsCount()
+        {
+            // Setup
+            PropertyInfo[] settings = TimeDateSettings.Instance?.GetType()?.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            // Act
+            var result = settings?.Length;
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("OnlyDateTimeNowGlobal")]
+        [DataRow("TimeWithSeconds")]
+        [DataRow("DateWithWeekday")]
+        [DataRow("HideNumberMessageOnGlobalQuery")]
+        public void DoesSettingExist(string name)
+        {
+            // Setup
+            Type settings = TimeDateSettings.Instance?.GetType();
+
+            // Act
+            var result = settings?.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [DataTestMethod]
+        [DataRow("OnlyDateTimeNowGlobal", true)]
+        [DataRow("TimeWithSeconds", false)]
+        [DataRow("DateWithWeekday", false)]
+        [DataRow("HideNumberMessageOnGlobalQuery", false)]
+        public void DefaultValues(string name, bool valueExpected)
+        {
+            // Setup
+            TimeDateSettings setting = TimeDateSettings.Instance;
+
+            // Act
+            PropertyInfo propertyInfo = setting?.GetType()?.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Instance);
+            var result = propertyInfo?.GetValue(setting);
+
+            // Assert
+            Assert.AreEqual(valueExpected, result);
+        }
+    }
+}
