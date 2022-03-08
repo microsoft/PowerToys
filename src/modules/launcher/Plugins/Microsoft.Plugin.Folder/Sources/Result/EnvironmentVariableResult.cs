7a709ac1fa6b0fc346a5d029153ee3c28c4e3c30
htcfreek <61519853+htcfreek@users.noreply.github.com>
2022-03-08 11:57:40 +0100

    Improvements

--- src/modules/launcher/Plugins/Microsoft.Plugin.Folder/Sources/Result/EnvironmentVariableResult.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Folder/Sources/Result/EnvironmentVariableResult.cs
@@ -36,6 +36,7 @@ namespace Microsoft.Plugin.Folder.Sources.Result
 
                 // Using CurrentCulture since this is user facing
                 SubTitle = string.Format(CultureInfo.CurrentCulture, Properties.Resources.wox_plugin_folder_select_folder_result_subtitle, Path),
+                ToolTipData = new ToolTipData(Title, string.Format(CultureInfo.CurrentCulture, Properties.Resources.wox_plugin_folder_select_folder_result_subtitle, Path)),
                 QueryTextDisplay = Path,
                 ContextData = new SearchResult { Type = ResultType.Folder, FullPath = Path },
                 Action = c => _shellAction.Execute(Path, contextApi),
--- src/modules/launcher/Plugins/Microsoft.Plugin.Folder/Sources/Result/TruncatedItemResult.cs
+++ src/modules/launcher/Plugins/Microsoft.Plugin.Folder/Sources/Result/TruncatedItemResult.cs
@@ -26,6 +26,7 @@ namespace Microsoft.Plugin.Folder.Sources.Result
 
                 // Using CurrentCulture since this is user facing
                 SubTitle = string.Format(CultureInfo.CurrentCulture, Properties.Resources.Microsoft_plugin_folder_truncation_warning_subtitle, PostTruncationCount, PreTruncationCount),
+                ToolTipData = new ToolTipData(Properties.Resources.Microsoft_plugin_folder_truncation_warning_title, string.Format(CultureInfo.CurrentCulture, Properties.Resources.Microsoft_plugin_folder_truncation_warning_subtitle, PostTruncationCount, PreTruncationCount)),
                 IcoPath = WarningIconPath,
             };
         }
