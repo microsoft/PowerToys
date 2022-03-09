a77151d6f9ae4aa4a5bd93b96724e35b9abbd66a
Den Delimarsky <1389609+dend@users.noreply.github.com>
2022-03-09 11:14:18 -0800

    Making sure we have constants separately, and a filter

---
+++ src/modules/awake/Awake/Core/NativeConstants.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#pragma warning disable SA1310 // Field names should not contain underscore
+
+namespace Awake.Core
+{
+    internal class NativeConstants
+    {
+        internal const int WM_COMMAND = 0x111;
+        internal const int WM_USER = 0x400;
+        internal const int MF_BYPOSITION = 1024;
+        internal const int MF_STRING = 0;
+        internal const uint WM_GETTEXT = 0x000D;
+    }
+}
---
+++ src/modules/awake/Awake/Core/TrayMessageFilter.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Windows.Forms;
+
+namespace Awake.Core
+{
+    public class TrayMessageFilter : IMessageFilter
+    {
+        public bool PreFilterMessage(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case NativeConstants.WM_COMMAND:
+                    switch (m.WParam.ToInt64() & 0xFFFF)
+                    {
+                        case NativeConstants.WM_USER + 1:
+                            // TODO
+                            break;
+                        case NativeConstants.WM_USER + 2:
+                            // TODO
+                            break;
+                    }
+
+                    break;
+            }
+
+            return false;
+        }
+    }
+}
