34e4e7e5bd977149f0e324caa6a59e84c02b2fe3
Heiko <61519853+htcfreek@users.noreply.github.com>
2022-03-17 20:33:05 +0100

    [PT Run] TimeDate plugin (#16662)
    * create plugin
    
    * Update plugin code
    
    * fix deps
    
    * last changes
    
    * unix
    
    * new results and small changes
    
    * Update settings name
    
    * make spellcheck happy
    
    * new time/date formats
    
    * add comment
    
    * code cleanup, installer, signing pipeline
    
    * fix unix result
    
    * UnitTests
    
    * spell fix
    
    * Update tests, Timestamp query feature
    
    * new formats
    
    * last changes
    
    * last changes
    
    * unit tests and fixes
    
    * cjhanges and fixes
    
    * fix installer
    
    * fix settings class init
    
    * context menu
    
    * fix tests
    
    * add settings tests
    
    * update/fix DateTimeResult tests
    
    * small improvements
    
    * update pipeline
    
    * enable analyzer
    
    * fixes and improvements
    
    * spell fix
    
    * dev docs
    
    * doc fixes
    
    * spell fix
    
    * last changes
    
    * changes and fixes
    
    * fixes and test updates
    
    * improvements
    
    * last changes
    
    * try to fix tests
    
    * remove obsolete code
    
    * add info to test log
    
    * fix search
    
    * tag fix
    
    * tests
    
    * change tests
    
    * update dev docs
    
    * fix spelling
    
    * fix culture for ui strings
    
    * improvements based on feedback
    
    * improve global search
    
    * improve text
    
    * docs improvement
    
    * add settings note
    
    * fix and update tests
    
    * fix spelling

---
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.TimeDate/Components/ResultHelper.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using Microsoft.PowerToys.Run.Plugin.TimeDate.Properties;
+using Wox.Plugin;
+using Wox.Plugin.Logger;
+
+[assembly: InternalsVisibleTo("Microsoft.PowerToys.Run.Plugin.TimeDate.UnitTests")]
+
+namespace Microsoft.PowerToys.Run.Plugin.TimeDate.Components
+{
+    internal static class ResultHelper
+    {
+        /// <summary>
+        /// Get the string based on the requested type
+        /// </summary>
+        /// <param name="isSystemTimeDate">Does the user search for system date/time?</param>
+        /// <param name="stringId">Id of the string. (Example: `MyString` for `MyString` and `MyStringNow`)</param>
+        /// <param name="stringIdNow">Optional string id for now case</param>
+        /// <returns>The string from the resource file, or <see cref="string.Empty"/> otherwise.</returns>
+        internal static string SelectStringFromResources(bool isSystemTimeDate, string stringId, string stringIdNow = default)
+        {
+            if (!isSystemTimeDate)
+            {
+                return Resources.ResourceManager.GetString(stringId, CultureInfo.CurrentUICulture) ?? string.Empty;
+            }
+            else if (!string.IsNullOrEmpty(stringIdNow))
+            {
+                return Resources.ResourceManager.GetString(stringIdNow, CultureInfo.CurrentUICulture) ?? string.Empty;
+            }
+            else
+            {
+                return Resources.ResourceManager.GetString(stringId + "Now", CultureInfo.CurrentUICulture) ?? string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Copy the given text to the clipboard
+        /// </summary>
+        /// <param name="text">The text to copy to the clipboard</param>
+        /// <returns><see langword="true"/>The text successful copy to the clipboard, otherwise <see langword="false"/></returns>
+        /// <remarks>Code copied from TimeZone plugin</remarks>
+        internal static bool CopyToClipBoard(in string text)
+        {
+            try
+            {
+                Clipboard.Clear();
+                Clipboard.SetText(text);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Log.Exception("Can't copy to clipboard", exception, typeof(ResultHelper));
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Create a tool tip for the alternative search tags
+        /// </summary>
+        /// <param name="result">The <see cref="AvailableResult"/>.</param>
+        /// <returns>New <see cref="ToolTipData"/> object or null if <see cref="AvailableResult.AlternativeSearchTag"/> is empty.</returns>
+        internal static ToolTipData GetSearchTagToolTip(AvailableResult result, out Visibility visibility)
+        {
+            switch (string.IsNullOrEmpty(result.AlternativeSearchTag))
+            {
+                case true:
+                    visibility = Visibility.Hidden;
+                    return null;
+                default:
+                    visibility = Visibility.Visible;
+                    return new ToolTipData(Resources.Microsoft_plugin_timedate_ToolTipAlternativeSearchTag, result.AlternativeSearchTag);
+            }
+        }
+
+        /// <summary>
+        /// Gets a result with an error message that only numbers can't be parsed
+        /// </summary>
+        /// <returns>Element of type <see cref="Result"/>.</returns>
+        internal static Result CreateNumberErrorResult(string theme) => new Result()
+        {
+            Title = Resources.Microsoft_plugin_timedate_ErrorResultTitle,
+            SubTitle = Resources.Microsoft_plugin_timedate_ErrorResultSubTitle,
+            IcoPath = $"Images\\Warning.{theme}.png",
+        };
+    }
+}
