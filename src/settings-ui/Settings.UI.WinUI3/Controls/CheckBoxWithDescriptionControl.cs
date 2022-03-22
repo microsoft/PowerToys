310f72f2629b76406feddde5725682c9e80c743d
Stefan Markovic <stefan@janeasystems.com>
2022-03-22 11:17:29 +0100

    Controls

---
+++ src/settings-ui/Settings.UI.WinUI3/Controls/CheckBoxWithDescriptionControl.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Automation;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.PowerToys.Settings.UI.WinUI3.Controls
+{
+    public class CheckBoxWithDescriptionControl : CheckBox
+    {
+        private CheckBoxWithDescriptionControl _checkBoxSubTextControl;
+
+        public CheckBoxWithDescriptionControl()
+        {
+            _checkBoxSubTextControl = (CheckBoxWithDescriptionControl)this;
+            this.Loaded += CheckBoxSubTextControl_Loaded;
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            Update();
+            base.OnApplyTemplate();
+        }
+
+        private void Update()
+        {
+            if (!string.IsNullOrEmpty(Header))
+            {
+                AutomationProperties.SetName(this, Header);
+            }
+        }
+
+        private void CheckBoxSubTextControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            StackPanel panel = new StackPanel() { Orientation = Orientation.Vertical };
+
+            // Add text box only if the description is not empty. Required for additional plugin options.
+            if (!string.IsNullOrWhiteSpace(Description))
+            {
+                panel.Children.Add(new TextBlock() { Margin = new Thickness(0, 10, 0, 0), Text = Header });
+                panel.Children.Add(new IsEnabledTextBlock() { Style = (Style)App.Current.Resources["SecondaryIsEnabledTextBlockStyle"], Text = Description });
+            }
+            else
+            {
+                panel.Children.Add(new TextBlock() { Margin = new Thickness(0, 0, 0, 0), Text = Header });
+            }
+
+            _checkBoxSubTextControl.Content = panel;
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+            "Header",
+            typeof(string),
+            typeof(CheckBoxWithDescriptionControl),
+            new PropertyMetadata(default(string)));
+
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+            "Description",
+            typeof(object),
+            typeof(CheckBoxWithDescriptionControl),
+            new PropertyMetadata(default(string)));
+
+        [Localizable(true)]
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        [Localizable(true)]
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+    }
+}
---
+++ src/settings-ui/Settings.UI.WinUI3/Controls/SettingsPageControl/SettingsPageControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.ObjectModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.PowerToys.Settings.UI.WinUI3.Controls
+{
+    public sealed partial class SettingsPageControl : UserControl
+    {
+        public SettingsPageControl()
+        {
+            this.InitializeComponent();
+            PrimaryLinks = new ObservableCollection<PageLink>();
+            SecondaryLinks = new ObservableCollection<PageLink>();
+        }
+
+        public string ModuleTitle
+        {
+            get { return (string)GetValue(ModuleTitleProperty); }
+            set { SetValue(ModuleTitleProperty, value); }
+        }
+
+        public string ModuleDescription
+        {
+            get => (string)GetValue(ModuleDescriptionProperty);
+            set => SetValue(ModuleDescriptionProperty, value);
+        }
+
+        public string ModuleImageSource
+        {
+            get => (string)GetValue(ModuleImageSourceProperty);
+            set => SetValue(ModuleImageSourceProperty, value);
+        }
+
+#pragma warning disable CA2227 // Collection properties should be read only
+        public ObservableCollection<PageLink> PrimaryLinks
+#pragma warning restore CA2227 // Collection properties should be read only
+        {
+            get => (ObservableCollection<PageLink>)GetValue(PrimaryLinksProperty);
+            set => SetValue(PrimaryLinksProperty, value);
+        }
+
+        public string SecondaryLinksHeader
+        {
+            get { return (string)GetValue(SecondaryLinksHeaderProperty); }
+            set { SetValue(SecondaryLinksHeaderProperty, value); }
+        }
+
+#pragma warning disable CA2227 // Collection properties should be read only
+        public ObservableCollection<PageLink> SecondaryLinks
+#pragma warning restore CA2227 // Collection properties should be read only
+        {
+            get => (ObservableCollection<PageLink>)GetValue(SecondaryLinksProperty);
+            set => SetValue(SecondaryLinksProperty, value);
+        }
+
+        public object ModuleContent
+        {
+            get { return (object)GetValue(ModuleContentProperty); }
+            set { SetValue(ModuleContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ModuleTitleProperty = DependencyProperty.Register("ModuleTitle", typeof(string), typeof(SettingsPageControl), new PropertyMetadata(default(string)));
+        public static readonly DependencyProperty ModuleDescriptionProperty = DependencyProperty.Register("ModuleDescription", typeof(string), typeof(SettingsPageControl), new PropertyMetadata(default(string)));
+        public static readonly DependencyProperty ModuleImageSourceProperty = DependencyProperty.Register("ModuleImageSource", typeof(string), typeof(SettingsPageControl), new PropertyMetadata(default(string)));
+        public static readonly DependencyProperty PrimaryLinksProperty = DependencyProperty.Register("PrimaryLinks", typeof(ObservableCollection<PageLink>), typeof(SettingsPageControl), new PropertyMetadata(new ObservableCollection<PageLink>()));
+        public static readonly DependencyProperty SecondaryLinksHeaderProperty = DependencyProperty.Register("SecondaryLinksHeader", typeof(string), typeof(SettingsPageControl), new PropertyMetadata(default(string)));
+        public static readonly DependencyProperty SecondaryLinksProperty = DependencyProperty.Register("SecondaryLinks", typeof(ObservableCollection<PageLink>), typeof(SettingsPageControl), new PropertyMetadata(new ObservableCollection<PageLink>()));
+        public static readonly DependencyProperty ModuleContentProperty = DependencyProperty.Register("ModuleContent", typeof(object), typeof(SettingsPageControl), new PropertyMetadata(new Grid()));
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            PrimaryLinksControl.Focus(FocusState.Programmatic);
+        }
+    }
+}
