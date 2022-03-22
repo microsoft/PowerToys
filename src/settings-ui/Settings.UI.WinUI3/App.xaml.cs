43c1919da472df5eac8e86620af8491fc1173473
Stefan Markovic <stefan@janeasystems.com>
2022-03-22 09:38:05 +0100

    New namespace

--- src/settings-ui/Settings.UI.WinUI3/App.xaml.cs
+++ src/settings-ui/Settings.UI.WinUI3/App.xaml.cs
@@ -19,7 +19,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Settings.UI.WinUI3
+namespace Microsoft.PowerToys.Settings.UI.WinUI3
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
