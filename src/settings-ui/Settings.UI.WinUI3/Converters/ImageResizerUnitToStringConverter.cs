68a712cecd329ce1553184c248f19dfc63d20d40
Stefan Markovic <stefan@janeasystems.com>
2022-03-22 11:17:22 +0100

    Converters and Helpers

---
+++ src/settings-ui/Settings.UI.WinUI3/Converters/ImageResizerUnitToStringConverter.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Globalization;
+using Windows.ApplicationModel.Resources;
+using Microsoft.UI.Xaml.Data;
+
+namespace Microsoft.PowerToys.Settings.UI.WinUI3.Converters
+{
+    public sealed class ImageResizerUnitToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var toLower = false;
+            if ((string)parameter == "ToLower")
+            {
+                toLower = true;
+            }
+
+            string targetValue = string.Empty;
+            switch (value)
+            {
+                case 0: targetValue = ResourceLoader.GetForCurrentView().GetString("ImageResizer_Unit_Centimeter"); break;
+                case 1: targetValue = ResourceLoader.GetForCurrentView().GetString("ImageResizer_Unit_Inch"); break;
+                case 2: targetValue = ResourceLoader.GetForCurrentView().GetString("ImageResizer_Unit_Percent"); break;
+                case 3: targetValue = ResourceLoader.GetForCurrentView().GetString("ImageResizer_Unit_Pixel"); break;
+            }
+
+            if (toLower)
+            {
+                targetValue = targetValue.ToLower(CultureInfo.CurrentCulture);
+            }
+
+            return targetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
---
+++ src/settings-ui/Settings.UI.WinUI3/Helpers/ResourceExtensions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Windows.ApplicationModel.Resources;
+
+namespace Microsoft.PowerToys.Settings.UI.WinUI3.Helpers
+{
+    internal static class ResourceExtensions
+    {
+        private static readonly ResourceLoader ResLoader = new ResourceLoader();
+
+        public static string GetLocalized(this string resourceKey)
+        {
+            return ResLoader.GetString(resourceKey);
+        }
+    }
+}
