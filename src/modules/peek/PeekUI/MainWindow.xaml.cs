04e7cbf8df974bf6d2520f4b0b8110d45a1b1eac
Michael Salmon 🐟 <michaelpsalmon@outlook.com>
2022-03-23 11:02:33 -0700

    add wpf app with toggle visibility on hotkey

---
+++ src/modules/peek/PeekUI/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using interop;
+using System.Windows;
+
+namespace PeekUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NativeEventWaiter.WaitForEventLoop(Constants.ShowPeekEvent(), TogglePeek);
+        }
+
+        private void TogglePeek()
+        {
+            // put window in focus and in foreground
+            Visibility = Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
