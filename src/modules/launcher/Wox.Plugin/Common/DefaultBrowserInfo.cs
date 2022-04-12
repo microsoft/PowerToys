86783e98154c6158d3ac44a2412fcb2028e6e42e
lncubus <randirbox@gmail.com>
2022-04-12 17:10:05 +0200

    [PT Run] Web search should use user's default browser (#17304)
    * Web search does not use my default browser #16549
    updated DefaultBrowserInfo logic to get program location from shell open command not icon location
    
    * Refactored DefaultBrowserInfo to start default browser with arguments specified in shell/open/command value for the browser.
    Added fallback to Microsoft Edge if no browser information available.
    #16549 Web search does not use my default browser
    
    * fixed comment
    #16549 Web search does not use my default browser

--- src/modules/launcher/Wox.Plugin/Common/DefaultBrowserInfo.cs
+++ src/modules/launcher/Wox.Plugin/Common/DefaultBrowserInfo.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.IO.Abstractions;
 using System.Text;
 using Wox.Plugin.Common.Win32;
 using Wox.Plugin.Logger;
@@ -18,40 +17,40 @@ namespace Wox.Plugin.Common
         private static readonly object _updateLock = new object();
         private static int _lastUpdateTickCount = -1;
 
-        public static readonly string MSEdgePath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + @"\Microsoft\Edge\Application\msedge.exe";
-        public const string MSEdgeName = "Microsoft Edge";
+        /// <summary>Gets the path to the MS Edge browser executable.</summary>
+        public static string MSEdgePath =>
+            Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) +
+            @"\Microsoft\Edge\Application\msedge.exe";
 
-        private static readonly IPath FilePath = new FileSystem().Path;
+        /// <summary>Gets the command line pattern of the MS Edge.</summary>
+        public static string MSEdgeArgumentsPattern => "--single-argument %1";
+
+        public const string MSEdgeName = "Microsoft Edge";
 
         /// <summary>Gets the path to default browser's executable.</summary>
         public static string Path { get; private set; }
 
-        /// <summary>Gets <see cref="Path" /> since the icon is embedded in the executable.</summary>
-        public static string IconPath { get => Path; }
+        /// <summary>Gets <see cref="Path"/> since the icon is embedded in the executable.</summary>
+        public static string IconPath => Path;
 
+        /// <summary>Gets the user-friendly name of the default browser.</summary>
         public static string Name { get; private set; }
 
-        /// <summary>Gets a value indicating whether the browser supports querying a web search via command line argument (`? <term>`).</summary>
-        public static bool SupportsWebSearchByCmdLineArgument { get => SearchEngineUrl is null; }
+        /// <summary>Gets the command line pattern of the default browser.</summary>
+        public static string ArgumentsPattern { get; private set; }
 
         public static bool IsDefaultBrowserSet { get => !string.IsNullOrEmpty(Path); }
 
-        /// <summary>
-        /// Gets the browser's default search engine's url ready to be formatted with a single value (like `https://www.bing.com/search?q={0}`)
-        /// </summary>
-#pragma warning disable CA1056 // URI-like properties should not be strings
-        public static string SearchEngineUrl { get; private set; }
-#pragma warning restore CA1056 // URI-like properties should not be strings
+        public const int UpdateTimeout = 300;
 
         /// <summary>
         /// Updates only if at least more than 300ms has passed since the last update, to avoid multiple calls to <see cref="Update"/>.
         /// (because of multiple plugins calling update at the same time.)
         /// </summary>
-        /// <param name="defaultToEdgeOnFail">If true, If <see cref="Update"/> fails, for any reason, the browser will be set to Microsoft Edge.</param>
         public static void UpdateIfTimePassed()
         {
             int curTickCount = Environment.TickCount;
-            if (curTickCount - _lastUpdateTickCount > 300)
+            if (curTickCount - _lastUpdateTickCount > UpdateTimeout)
             {
                 _lastUpdateTickCount = curTickCount;
                 Update();
@@ -72,131 +71,74 @@ namespace Wox.Plugin.Common
                     string progId = GetRegistryValue(
                         @"HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice",
                         "ProgId");
+                    string appName = GetRegistryValue($@"HKEY_CLASSES_ROOT\{progId}\Application", "ApplicationName")
+                        ?? GetRegistryValue($@"HKEY_CLASSES_ROOT\{progId}", "FriendlyTypeName");
 
-                    // The `?` argument doesn't work on opera, so we get the user's default search engine:
-                    if (progId.StartsWith("Opera", StringComparison.OrdinalIgnoreCase))
+                    if (appName != null)
                     {
-                        // Opera user preferences file:
-                        string prefFile;
-
-                        if (progId.Contains("GX", StringComparison.OrdinalIgnoreCase))
+                        // Handle indirect strings:
+                        if (appName.StartsWith("@", StringComparison.Ordinal))
                         {
-                            Name = "Opera GX";
-                            prefFile = System.IO.File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\Opera Software\\Opera GX Stable\\Preferences");
-                        }
-                        else
-                        {
-                            Name = "Opera";
-                            prefFile = System.IO.File.ReadAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\Opera Software\\Opera Stable\\Preferences");
+                            appName = GetIndirectString(appName);
                         }
 
-                        // "default_search_provider_data" doesn't exist if the user hasn't searched for the first time,
-                        // therefore we set `url` to opera's default search engine:
-                        string url = "https://www.google.com/search?client=opera&q={0}&sourceid=opera";
+                        appName = appName
+                            .Replace("URL", null, StringComparison.OrdinalIgnoreCase)
+                            .Replace("HTML", null, StringComparison.OrdinalIgnoreCase)
+                            .Replace("Document", null, StringComparison.OrdinalIgnoreCase)
+                            .Replace("Web", null, StringComparison.OrdinalIgnoreCase)
+                            .TrimEnd();
+                    }
 
-                        using (System.Text.Json.JsonDocument doc = System.Text.Json.JsonDocument.Parse(prefFile))
+                    Name = appName;
+
+                    string commandPattern = GetRegistryValue($@"HKEY_CLASSES_ROOT\{progId}\shell\open\command", null);
+
+                    if (string.IsNullOrEmpty(commandPattern))
+                    {
+                        throw new ArgumentOutOfRangeException(
+                            nameof(commandPattern),
+                            "Default browser program command is not specified.");
+                    }
+
+                    if (commandPattern.StartsWith('@'))
+                    {
+                        commandPattern = GetIndirectString(commandPattern);
+                    }
+
+                    if (commandPattern.StartsWith('\"'))
+                    {
+                        var endQuoteIndex = commandPattern.IndexOf('\"', 1);
+                        if (endQuoteIndex != -1)
                         {
-                            if (doc.RootElement.TryGetProperty("default_search_provider_data", out var element))
-                            {
-                                if (element.TryGetProperty("template_url_data", out element))
-                                {
-                                    if (element.TryGetProperty("url", out element))
-                                    {
-                                        url = element.GetString();
-                                    }
-                                }
-                            }
+                            Path = commandPattern.Substring(1, endQuoteIndex - 1);
+                            ArgumentsPattern = commandPattern.Substring(endQuoteIndex + 1).Trim();
                         }
-
-                        url = url
-                            .Replace("{searchTerms}", "{0}", StringComparison.Ordinal)
-                            .Replace("{inputEncoding}", "UTF-8", StringComparison.Ordinal)
-                            .Replace("{outputEncoding}", "UTF-8", StringComparison.Ordinal);
-
-                        int startIndex = url.IndexOf('}', StringComparison.Ordinal) + 1;
-
-                        // In case there are other url parameters (e.g. `&foo={bar}`), remove them:
-                        for (int i = url.IndexOf("}", startIndex, StringComparison.Ordinal);
-                                i != -1;
-                                i = url.IndexOf("}", startIndex, StringComparison.Ordinal))
-                        {
-                            for (int j = i - 1; j > 0; --j)
-                            {
-                                if (url[j] == '&')
-                                {
-                                    url = url.Remove(j, i - j + 1);
-                                    break;
-                                }
-                            }
-                        }
-
-                        SearchEngineUrl = url;
                     }
                     else
                     {
-                        string appName = GetRegistryValue($@"HKEY_CLASSES_ROOT\{progId}\Application", "ApplicationName")
-                            ?? GetRegistryValue($@"HKEY_CLASSES_ROOT\{progId}", "FriendlyTypeName");
-
-                        if (appName != null)
+                        var spaceIndex = commandPattern.IndexOf(' ');
+                        if (spaceIndex != -1)
                         {
-                            // Handle indirect strings:
-                            if (appName.StartsWith("@", StringComparison.Ordinal))
-                            {
-                                appName = GetIndirectString(appName);
-                            }
-
-                            appName = appName
-                                .Replace("URL", null, StringComparison.OrdinalIgnoreCase)
-                                .Replace("HTML", null, StringComparison.OrdinalIgnoreCase)
-                                .Replace("Document", null, StringComparison.OrdinalIgnoreCase)
-                                .TrimEnd();
+                            Path = commandPattern.Substring(0, spaceIndex);
+                            ArgumentsPattern = commandPattern.Substring(spaceIndex + 1).Trim();
                         }
-
-                        Name = appName;
-
-                        SearchEngineUrl = null;
-                    }
-
-                    string programLocation =
-
-                        // Resolve App Icon (UWP)
-                        GetRegistryValue(
-                            $@"HKEY_CLASSES_ROOT\{progId}\Application",
-                            "ApplicationIcon")
-
-                        // Resolves default  file association icon (UWP + Normal)
-                        ?? GetRegistryValue($@"HKEY_CLASSES_ROOT\{progId}\DefaultIcon", null);
-
-                    // "Handles 'Indirect Strings' (UWP programs)"
-                    // Using Ordinal since this is internal and used with a symbol
-                    if (programLocation.StartsWith("@", StringComparison.Ordinal))
-                    {
-                        string directProgramLocation = GetIndirectString(programLocation);
-                        Path = string.Equals(FilePath.GetExtension(directProgramLocation), ".exe", StringComparison.Ordinal)
-                            ? directProgramLocation
-                            : null;
-                    }
-                    else
-                    {
-                        // Using Ordinal since this is internal and used with a symbol
-                        var indexOfComma = programLocation.IndexOf(',', StringComparison.Ordinal);
-                        Path = indexOfComma > 0
-                            ? programLocation.Substring(0, indexOfComma)
-                            : programLocation;
                     }
 
                     if (string.IsNullOrEmpty(Path))
                     {
-                        throw new ArgumentOutOfRangeException(nameof(Path), "Browser path is null or empty.");
+                        throw new ArgumentOutOfRangeException(
+                            nameof(Path),
+                            "Default browser program path could not be determined.");
                     }
                 }
                 catch (Exception e)
                 {
-                    Path = null;
-                    Name = null;
-                    SearchEngineUrl = null;
-
-                    Log.Exception("Exception when retrieving browser path/name. Path and Name are null.", e, typeof(DefaultBrowserInfo));
+                    // fallback to MS Edge
+                    Path = MSEdgePath;
+                    Name = MSEdgeName;
+                    ArgumentsPattern = MSEdgeArgumentsPattern;
+                    Log.Exception("Exception when retrieving browser path/name. Path and Name are set to use Microsoft Edge.", e, typeof(DefaultBrowserInfo));
                 }
 
                 string GetRegistryValue(string registryLocation, string valueName)
@@ -217,7 +159,7 @@ namespace Wox.Plugin.Common
                         return stringBuilder.ToString();
                     }
 
-                    throw new ArgumentNullException(nameof(Path), "Could not load indirect string.");
+                    throw new ArgumentNullException(nameof(str), "Could not load indirect string.");
                 }
             }
         }
