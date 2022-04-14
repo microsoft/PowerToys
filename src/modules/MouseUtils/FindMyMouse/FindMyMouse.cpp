1727f2b81302faf4ab30d2e4492bed8d562771d4
Jaime Bernardo <jaime@janeasystems.com>
2022-04-14 13:21:02 +0100

    [MouseUtils]Fix for breaking Windows shortcuts (#17681)

--- src/modules/MouseUtils/FindMyMouse/FindMyMouse.cpp
+++ src/modules/MouseUtils/FindMyMouse/FindMyMouse.cpp
@@ -498,7 +498,7 @@ void SuperSonar<D>::StartSonar()
     Trace::MousePointerFocused();
     // Cover the entire virtual screen.
     // HACK: Draw with 1 pixel off. Otherwise Windows glitches the task bar transparency when a transparent window fill the whole screen.
-    SetWindowPos(m_hwnd, HWND_TOPMOST, GetSystemMetrics(SM_XVIRTUALSCREEN), GetSystemMetrics(SM_YVIRTUALSCREEN), GetSystemMetrics(SM_CXVIRTUALSCREEN), GetSystemMetrics(SM_CYVIRTUALSCREEN)-1, 0);
+    SetWindowPos(m_hwnd, HWND_TOPMOST, GetSystemMetrics(SM_XVIRTUALSCREEN) + 1, GetSystemMetrics(SM_YVIRTUALSCREEN) + 1, GetSystemMetrics(SM_CXVIRTUALSCREEN) - 2, GetSystemMetrics(SM_CYVIRTUALSCREEN) - 2, 0);
     m_sonarPos = ptNowhere;
     OnMouseTimer();
     UpdateMouseSnooping();
