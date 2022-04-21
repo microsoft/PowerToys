b2ec3451e5cef7a78993d3c482f8f7fd861213b5
yuyoyuppe <a.yuyoyuppe@gmail.com>
2022-04-21 14:05:12 +0300

    [ARM64] Add support for .msi assembly
    # Conflicts:
    #	installer/PowerToysSetupCustomActions/PowerToysSetupCustomActions.vcxproj

--- installer/PowerToysSetup/publish.cmd
+++ installer/PowerToysSetup/publish.cmd
@@ -2,8 +2,11 @@ setlocal enableDelayedExpansion
 
 IF NOT DEFINED PTRoot (SET PTRoot=..\..)
 
-rem In case of Release we should not use Debug CRT in VCRT forwarders
-msbuild !PTRoot!\src\settings-ui\Settings.UI\PowerToys.Settings.csproj -t:Publish -p:Configuration="Release" -p:Platform="x64" -p:PowerToysRoot=!PTRoot! -p:AppxBundle=Never -p:VCRTForwarders-IncludeDebugCRT=false -p:PublishProfile=InstallationPublishProfile.pubxml
+SET PlatformArg=%1
+IF NOT DEFINED PlatformArg (SET PlatformArg=x64)
 
 rem In case of Release we should not use Debug CRT in VCRT forwarders
-msbuild !PTRoot!\src\modules\launcher\PowerLauncher\PowerLauncher.csproj -t:Publish -p:Configuration="Release" -p:Platform="x64" -p:AppxBundle=Never -p:PowerToysRoot=!PTRoot! -p:VCRTForwarders-IncludeDebugCRT=false -p:PublishProfile=InstallationPublishProfile.pubxml
+msbuild !PTRoot!\src\settings-ui\Settings.UI\PowerToys.Settings.csproj -t:Publish -p:Configuration="Release" -p:Platform="!PlatformArg!" -p:PowerToysRoot=!PTRoot! -p:AppxBundle=Never -p:VCRTForwarders-IncludeDebugCRT=false -p:PublishProfile=InstallationPublishProfile.pubxml
+
+rem In case of Release we should not use Debug CRT in VCRT forwarders
+msbuild !PTRoot!\src\modules\launcher\PowerLauncher\PowerLauncher.csproj -t:Publish -p:Configuration="Release" -p:Platform="!PlatformArg!" -p:AppxBundle=Never -p:PowerToysRoot=!PTRoot! -p:VCRTForwarders-IncludeDebugCRT=false -p:PublishProfile=InstallationPublishProfile.pubxml
