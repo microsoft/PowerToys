10e12140a010ef689c6d95ec9b9c5df0cdc38c8f
seraphima <zykovas91@gmail.com>
2022-04-26 18:51:19 +0200

    app name

--- tools/WindowStylesReportTool/WindowStylesReportTool.cpp
+++ tools/WindowStylesReportTool/WindowStylesReportTool.cpp
@@ -39,6 +39,68 @@ std::filesystem::path get_desktop_path()
     return result;
 }
 
+// Get the executable path or module name for modern apps
+inline std::wstring get_process_path(DWORD pid) noexcept
+{
+    auto process = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, TRUE, pid);
+    std::wstring name;
+    if (process != INVALID_HANDLE_VALUE)
+    {
+        name.resize(MAX_PATH);
+        DWORD name_length = static_cast<DWORD>(name.length());
+        if (QueryFullProcessImageNameW(process, 0, (LPWSTR)name.data(), &name_length) == 0)
+        {
+            name_length = 0;
+        }
+        name.resize(name_length);
+        CloseHandle(process);
+    }
+    return name;
+}
+
+// Get the executable path or module name for modern apps
+inline std::wstring get_process_path(HWND window) noexcept
+{
+    const static std::wstring app_frame_host = L"ApplicationFrameHost.exe";
+
+    DWORD pid{};
+    GetWindowThreadProcessId(window, &pid);
+    auto name = get_process_path(pid);
+
+    if (name.length() >= app_frame_host.length() &&
+        name.compare(name.length() - app_frame_host.length(), app_frame_host.length(), app_frame_host) == 0)
+    {
+        // It is a UWP app. We will enumerate the windows and look for one created
+        // by something with a different PID
+        DWORD new_pid = pid;
+
+        EnumChildWindows(
+            window, [](HWND hwnd, LPARAM param) -> BOOL {
+                auto new_pid_ptr = reinterpret_cast<DWORD*>(param);
+                DWORD pid;
+                GetWindowThreadProcessId(hwnd, &pid);
+                if (pid != *new_pid_ptr)
+                {
+                    *new_pid_ptr = pid;
+                    return FALSE;
+                }
+                else
+                {
+                    return TRUE;
+                }
+            },
+            reinterpret_cast<LPARAM>(&new_pid));
+
+        // If we have a new pid, get the new name.
+        if (new_pid != pid)
+        {
+            return get_process_path(new_pid);
+        }
+    }
+
+    return name;
+}
+
 class Logger
 {
 private:
@@ -262,6 +324,16 @@ void LogVirtualDesktopInfo(HWND window)
 
 void LogInfo(HWND window)
 {
+    auto processPath = get_process_path(window);
+    auto app = processPath;
+    auto pos = processPath.find_last_of('\\');
+    if (pos != std::string::npos && pos + 1 < processPath.length())
+    {
+        app = processPath.substr(pos + 1);
+    }
+
+    Logger::log(L"Window: {}", app);
+    Logger::log(L"");
 
     LogStyles(window);
     LogExStyles(window);
