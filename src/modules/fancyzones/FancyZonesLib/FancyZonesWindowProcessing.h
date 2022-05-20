251ea6ded9c494193e9b8b984def347990c138b7
Seraphima Zykova <zykovas91@gmail.com>
2022-05-20 10:51:15 +0200

    [FancyZones] Popup behavior fix (#18270)
    * virtual desktop check
    
    * refactoring
    
    * unified check

---
+++ src/modules/fancyzones/FancyZonesLib/FancyZonesWindowProcessing.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <FancyZonesLib/VirtualDesktop.h>
+#include <FancyZonesLib/WindowUtils.h>
+
+namespace FancyZonesWindowProcessing
+{
+    inline bool IsProcessable(HWND window) noexcept
+    {
+        const bool isSplashScreen = FancyZonesWindowUtils::IsSplashScreen(window);
+        if (isSplashScreen)
+        {
+            return false;
+        }
+
+        const bool windowMinimized = IsIconic(window);
+        if (windowMinimized)
+        {
+            return false;
+        }
+
+        // Switch between virtual desktops results with posting same windows messages that also indicate
+        // creation of new window. We need to check if window being processed is on currently active desktop.
+        // For windows that FancyZones shouldn't process (start menu, tray, popup menus) 
+        // VirtualDesktopManager is unable to retrieve virtual desktop id and returns an error.
+        auto desktopId = VirtualDesktop::instance().GetDesktopId(window);
+        if (!desktopId.has_value() || (desktopId.has_value() && *desktopId != VirtualDesktop::instance().GetCurrentVirtualDesktopId()))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
