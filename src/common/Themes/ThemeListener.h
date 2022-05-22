6d36e8979ad8627576beb8a9f7666b08419f13d6
William Bradley <williamabradley@outlook.com>
2022-05-22 15:39:13 +1200

    Proper formatting

--- src/common/Themes/ThemeListener.h
+++ src/common/Themes/ThemeListener.h
@@ -10,25 +10,25 @@ DWORD WINAPI _checkTheme(LPVOID lpParam);
 #pragma once
 class ThemeListener
 {
-	public:
-        ThemeListener()
-        {
-          AppTheme = ThemeHelpers::GetAppTheme();
-          dwThreadHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)_checkTheme, this, 0, &dwThreadId);
-        }
-        ~ThemeListener() 
-        {
-            CloseHandle(dwThreadHandle);
-            dwThreadId = 0;
-        }
+public:
+    ThemeListener()
+    {
+        AppTheme = ThemeHelpers::GetAppTheme();
+        dwThreadHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)_checkTheme, this, 0, &dwThreadId);
+    }
+    ~ThemeListener()
+    {
+        CloseHandle(dwThreadHandle);
+        dwThreadId = 0;
+    }
 
-		AppTheme AppTheme;
-        void ThemeListener::AddChangedHandler(THEME_HANDLE handle);
-        void ThemeListener::DelChangedHandler(THEME_HANDLE handle);
-        void CheckTheme();
+    AppTheme AppTheme;
+    void ThemeListener::AddChangedHandler(THEME_HANDLE handle);
+    void ThemeListener::DelChangedHandler(THEME_HANDLE handle);
+    void CheckTheme();
 
-	private:
-        HANDLE dwThreadHandle;
-		DWORD dwThreadId;
-        std::vector<THEME_HANDLE> handles;
+private:
+    HANDLE dwThreadHandle;
+    DWORD dwThreadId;
+    std::vector<THEME_HANDLE> handles;
 };
