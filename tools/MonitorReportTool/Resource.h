b5531a1f6b6ee3beb448aa4305d806edcfbc1213
Seraphima Zykova <zykovas91@gmail.com>
2022-06-13 16:37:52 +0200

    [FancyZones][Tool]Monitor id tool (#18589)
    * tool
    
    * log ccd and wmi/cimv2
    
    * rename
    
    * spell
    
    * clean up
    
    * spellcheck

---
+++ tools/MonitorReportTool/Resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MonitorReportTool.rc
+
+#define IDR_MAINFRAME			128
+#define IDD_ABOUTBOX			103
+#define IDI_WINDOWSTYLESICON			107
+#define IDI_SMALLICON				108
+#define IDC_WINDOWSTYLES			109
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
