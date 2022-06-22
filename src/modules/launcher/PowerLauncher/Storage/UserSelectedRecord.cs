481104d0ed838477d27680e39447aecc42fdf825
Jeff Lord <jeff@jefflord.com>
2022-06-21 22:59:16 -0400

    Change to allow new settings for results tuning.

--- src/modules/launcher/PowerLauncher/Storage/UserSelectedRecord.cs
+++ src/modules/launcher/PowerLauncher/Storage/UserSelectedRecord.cs
@@ -11,8 +11,15 @@ namespace PowerLauncher.Storage
 {
     public class UserSelectedRecord
     {
+        public class UserSelectedRecordItem
+        {
+            public int SelectedCount { get; set; }
+
+            public DateTime LastSelected { get; set; }
+        }
+
         [JsonInclude]
-        public Dictionary<string, int> Records { get; private set; } = new Dictionary<string, int>();
+        public Dictionary<string, UserSelectedRecordItem> Records { get; private set; } = new Dictionary<string, UserSelectedRecordItem>();
 
         public void Add(Result result)
         {
@@ -22,29 +29,30 @@ namespace PowerLauncher.Storage
             }
 
             var key = result.ToString();
-            if (Records.TryGetValue(key, out int value))
+            if (Records.TryGetValue(key, out var value))
             {
-                Records[key] = value + 1;
+                Records[key].SelectedCount = Records[key].SelectedCount + 1;
+                Records[key].LastSelected = DateTime.UtcNow;
             }
             else
             {
-                Records.Add(key, 1);
+                Records.Add(key, new UserSelectedRecordItem { SelectedCount = 1, LastSelected = DateTime.UtcNow });
             }
         }
 
-        public int GetSelectedCount(Result result)
+        public UserSelectedRecordItem GetSelectedData(Result result)
         {
             if (result == null)
             {
                 throw new ArgumentNullException(nameof(result));
             }
 
-            if (result != null && Records.TryGetValue(result.ToString(), out int value))
+            if (result != null && Records.TryGetValue(result.ToString(), out var value))
             {
                 return value;
             }
 
-            return 0;
+            return new UserSelectedRecordItem { SelectedCount = 0, LastSelected = DateTime.MinValue };
         }
     }
 }
