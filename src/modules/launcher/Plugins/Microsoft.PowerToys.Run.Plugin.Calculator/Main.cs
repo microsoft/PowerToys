7a96de88c9f243673c7ee2a89e9f3c6eab45dbe4
htcfreek <61519853+htcfreek@users.noreply.github.com>
2022-06-26 13:46:54 +0200

    number examples calc plugin (#18934)

--- src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.Calculator/Main.cs
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.Calculator/Main.cs
@@ -38,14 +38,14 @@ namespace Microsoft.PowerToys.Run.Plugin.Calculator
             {
                 Key = "InputUseEnglishFormat",
                 DisplayLabel = Resources.wox_plugin_calculator_in_en_format,
-                DisplayDescription = Resources.wox_plugin_calculator_in_en_format_description,
+                DisplayDescription = string.Format(CultureInfo.InvariantCulture, Resources.wox_plugin_calculator_in_en_format_description, 1000.55.ToString("N", new CultureInfo("en-us"))),
                 Value = false,
             },
             new PluginAdditionalOption()
             {
                 Key = "OutputUseEnglishFormat",
                 DisplayLabel = Resources.wox_plugin_calculator_out_en_format,
-                DisplayDescription = Resources.wox_plugin_calculator_out_en_format_description,
+                DisplayDescription = string.Format(CultureInfo.InvariantCulture, Resources.wox_plugin_calculator_out_en_format_description, 1000.55.ToString("G", new CultureInfo("en-us"))),
                 Value = false,
             },
         };
