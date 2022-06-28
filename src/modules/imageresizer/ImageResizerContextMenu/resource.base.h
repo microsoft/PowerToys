f5eecaa13ad85eed2b429ac410797ff19ef5242a
Stefan Markovic <stefan@janeasystems.com>
2022-06-28 10:34:18 +0200

    Remove unneeded file & try fix resource build error

--- src/modules/imageresizer/ImageResizerContextMenu/resource.base.h
+++ src/modules/imageresizer/ImageResizerContextMenu/resource.base.h
@@ -1,4 +1,13 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by ImageResizerExt.rc
-//
+// Used by ImageResizerContextMenu.rc
+
+//////////////////////////////
+// Non-localizable
+
+#define FILE_DESCRIPTION "PowerToys Image Resizer Context Menu"
+#define INTERNAL_NAME "PowerToys.ImageResizerContextMenu.dll"
+#define ORIGINAL_FILENAME "PowerToys.ImageResizerContextMenu.dll"
+
+// Non-localizable
+//////////////////////////////
