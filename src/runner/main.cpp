0096230016f4afd014989e263488ae345daf6e2b
Joseph Finney <josephfinney@LIVE.COM>
2022-07-09 22:42:55 -0500

    Add PowerOCR to Runner Main.cpp

--- src/runner/main.cpp
+++ src/runner/main.cpp
@@ -152,6 +152,7 @@ int runner(bool isProcessElevated, bool openSettings, std::string settingsWindow
             L"modules/MouseUtils/PowerToys.MouseHighlighter.dll",
             L"modules/AlwaysOnTop/PowerToys.AlwaysOnTopModuleInterface.dll",
             L"modules/MouseUtils/PowerToys.MousePointerCrosshairs.dll",
+            L"modules/PowerOCR/PowerToys.PowerOCR.dll",
         };
         const auto VCM_PATH = L"modules/VideoConference/PowerToys.VideoConferenceModule.dll";
         if (const auto mf = LoadLibraryA("mf.dll"))
