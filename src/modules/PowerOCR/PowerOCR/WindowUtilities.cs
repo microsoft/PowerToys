a6144915349ffc90252d12776c3b43da74c28f74
Joseph Finney <josephfinney@LIVE.COM>
2022-07-09 19:44:47 -0500

    Add WindowUtilities and WindowForms

---
+++ src/modules/PowerOCR/PowerOCR/WindowUtilities.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace PowerOCR.Utilities;
+
+public static class WindowUtilities
+{
+    public static void LaunchOCROverlayOnEveryScreen()
+    {
+        Screen[] allScreens = Screen.AllScreens;
+        WindowCollection allWindows = System.Windows.Application.Current.Windows;
+
+        List<OCROverlay> allFullscreenGrab = new();
+
+        foreach (Screen screen in allScreens)
+        {
+            bool screenHasWindow = true;
+
+            foreach (Window window in allWindows)
+            {
+                System.Drawing.Point windowCenter =
+                    new System.Drawing.Point(
+                        (int)(window.Left + (window.Width / 2)),
+                        (int)(window.Top + (window.Height / 2)));
+                screenHasWindow = screen.Bounds.Contains(windowCenter);
+
+                // if (window is EditTextWindow)
+                //     isEditWindowOpen = true;
+            }
+
+            if (allWindows.Count < 1)
+                screenHasWindow = false;
+
+            OCROverlay fullscreenGrab = new()
+            {
+                WindowStartupLocation = WindowStartupLocation.Manual,
+                Width = 200,
+                Height = 200,
+                WindowState = WindowState.Normal
+            };
+
+            if (screen.WorkingArea.Left >= 0)
+                fullscreenGrab.Left = screen.WorkingArea.Left;
+            else
+                fullscreenGrab.Left = screen.WorkingArea.Left + (screen.WorkingArea.Width / 2);
+
+            if (screen.WorkingArea.Top >= 0)
+                fullscreenGrab.Top = screen.WorkingArea.Top;
+            else
+                fullscreenGrab.Top = screen.WorkingArea.Top + (screen.WorkingArea.Height / 2);
+
+            fullscreenGrab.Show();
+            fullscreenGrab.Activate();
+            allFullscreenGrab.Add(fullscreenGrab);
+        }
+    }
+
+    internal static void CloseAllOCROverlays()
+    {
+        WindowCollection allWindows = System.Windows.Application.Current.Windows;
+
+        foreach (Window window in allWindows)
+            if (window is OCROverlay fsg)
+                fsg.Close();
+
+        System.Windows.Application.Current.Shutdown();
+    }
+}
