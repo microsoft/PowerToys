45efdea1356c59912431f9847f519a51e36c74f9
Joseph Finney <josephfinney@LIVE.COM>
2022-07-10 10:01:44 -0500

    Add PowerOcrViewModel

---
+++ src/settings-ui/Settings.UI.Library/ViewModels/PowerOcrViewModel.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text.Json;
+using System.Timers;
+using Microsoft.PowerToys.Settings.UI.Library.Enumerations;
+using Microsoft.PowerToys.Settings.UI.Library.Helpers;
+using Microsoft.PowerToys.Settings.UI.Library.Interfaces;
+
+namespace Microsoft.PowerToys.Settings.UI.Library.ViewModels
+{
+    internal class PowerOcrViewModel : Observable, IDisposable
+    {
+        private bool disposedValue;
+
+        // Delay saving of settings in order to avoid calling save multiple times and hitting file in use exception. If there is no other request to save settings in given interval, we proceed to save it, otherwise we schedule saving it after this interval
+        private const int SaveSettingsDelayInMs = 500;
+
+        private GeneralSettings GeneralSettingsConfig { get; set; }
+
+        private readonly ISettingsUtils _settingsUtils;
+        private readonly object _delayedActionLock = new object();
+
+        private readonly PowerOcrSettings _powerOcrSettings;
+        private Timer _delayedTimer;
+
+        private bool _isEnabled;
+
+        private Func<string, int> SendConfigMSG { get; }
+
+        public PowerOcrViewModel(ISettingsUtils settingsUtils, ISettingsRepository<GeneralSettings> settingsRepository, Func<string, int> ipcMSGCallBackFunc)
+        {
+            GeneralSettingsConfig = settingsRepository.SettingsConfig;
+
+            _settingsUtils = settingsUtils ?? throw new ArgumentNullException(nameof(settingsUtils));
+            if (_settingsUtils.SettingsExists(PowerOcrSettings.ModuleName))
+            {
+                _powerOcrSettings = _settingsUtils.GetSettingsOrDefault<PowerOcrSettings>(PowerOcrSettings.ModuleName);
+            }
+            else
+            {
+                _powerOcrSettings = new PowerOcrSettings();
+            }
+
+            _isEnabled = GeneralSettingsConfig.Enabled.ColorPicker;
+
+            // set the callback functions value to hangle outgoing IPC message.
+            SendConfigMSG = ipcMSGCallBackFunc;
+
+            _delayedTimer = new Timer();
+            _delayedTimer.Interval = SaveSettingsDelayInMs;
+            _delayedTimer.Elapsed += DelayedTimer_Tick;
+            _delayedTimer.AutoReset = false;
+        }
+
+        private void ScheduleSavingOfSettings()
+        {
+            lock (_delayedActionLock)
+            {
+                if (_delayedTimer.Enabled)
+                {
+                    _delayedTimer.Stop();
+                }
+
+                _delayedTimer.Start();
+            }
+        }
+
+        private void DelayedTimer_Tick(object sender, EventArgs e)
+        {
+            lock (_delayedActionLock)
+            {
+                _delayedTimer.Stop();
+                NotifySettingsChanged();
+            }
+        }
+
+        private void NotifySettingsChanged()
+        {
+            // Using InvariantCulture as this is an IPC message
+            SendConfigMSG(
+                   string.Format(
+                       CultureInfo.InvariantCulture,
+                       "{{ \"powertoys\": {{ \"{0}\": {1} }} }}",
+                       PowerOcrSettings.ModuleName,
+                       JsonSerializer.Serialize(_powerOcrSettings)));
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _delayedTimer.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
