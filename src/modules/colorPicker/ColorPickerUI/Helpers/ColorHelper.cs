a07e19c8b9a4ff4f6e80e01191a03a3fa593e3ba
Frederico Moron <fredmoron@gmail.com>
2022-08-01 14:56:08 -0300

    Change HSB color convert and add unit test

--- src/modules/colorPicker/ColorPickerUI/Helpers/ColorHelper.cs
+++ src/modules/colorPicker/ColorPickerUI/Helpers/ColorHelper.cs
@@ -58,7 +58,12 @@ namespace ColorPicker.Helpers
         /// <param name="color">The <see cref="Color"/> to convert</param>
         /// <returns>The hue [0°..360°], saturation [0..1] and brightness [0..1] of the converted color</returns>
         internal static (double hue, double saturation, double brightness) ConvertToHSBColor(Color color)
-            => (color.GetHue(), color.GetSaturation(), color.GetBrightness());
+        {
+            var min = Math.Min(Math.Min(color.R, color.G), color.B) / 255d;
+            var max = Math.Max(Math.Max(color.R, color.G), color.B) / 255d;
+
+            return (color.GetHue(), max == 0d ? 0d : (max - min) / max, max);
+        }
 
         /// <summary>
         /// Convert a given <see cref="Color"/> to a HSI color (hue, saturation, intensity)
