aa3a065979dffcf347c725b770f9c71573bb11ca
Joseph Finney <josephfinney@LIVE.COM>
2022-08-09 21:47:43 -0500

    use mutex on PowerOCR app to keep to single instance

--- src/modules/PowerOCR/PowerOCR/App.xaml.cs
+++ src/modules/PowerOCR/PowerOCR/App.xaml.cs
@@ -3,8 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Threading;
 using System.Windows;
-
+using ManagedCommon;
 using PowerOCR.Keyboard;
 using PowerOCR.Settings;
 
@@ -16,6 +17,9 @@ namespace PowerOCR;
 public partial class App : Application, IDisposable
 {
     private KeyboardMonitor? keyboardMonitor;
+    private Mutex? _instanceMutex;
+    private static string[] _args = Array.Empty<string>();
+    private int _powerToysRunnerPid;
 
     public void Dispose()
     {
@@ -25,9 +29,42 @@ public partial class App : Application, IDisposable
 
     private void Application_Startup(object sender, StartupEventArgs e)
     {
+        // allow only one instance of PowerOCR
+        _instanceMutex = new Mutex(true, @"Local\PowerToys_PowerOCR_InstanceMutex", out bool createdNew);
+        if (!createdNew)
+        {
+            _instanceMutex = null;
+            Environment.Exit(0);
+            return;
+        }
+
+        if (e.Args?.Length > 0)
+        {
+            _ = int.TryParse(_args[0], out _powerToysRunnerPid);
+
+            RunnerHelper.WaitForPowerToysRunner(_powerToysRunnerPid, () =>
+            {
+                Environment.Exit(0);
+            });
+        }
+        else
+        {
+            _powerToysRunnerPid = -1;
+        }
+
         var userSettings = new UserSettings();
         keyboardMonitor = new KeyboardMonitor(userSettings);
         keyboardMonitor?.Start();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (_instanceMutex != null)
+        {
+            _instanceMutex.ReleaseMutex();
+        }
+
+        base.OnExit(e);
     }
 
     private void Application_Exit(object sender, ExitEventArgs e)
