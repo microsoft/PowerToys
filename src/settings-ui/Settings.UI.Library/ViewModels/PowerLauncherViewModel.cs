c85305695e7fd358ddd624434ebb33d47ba4f9bf
Heiko <61519853+htcfreek@users.noreply.github.com>
2022-08-23 22:28:41 +0200

    [Settings][Plugin manager] Activation commands: Fix conflict with calc plugin and add warning (#19593)
    * update activation commands
    
    * add warning and badges
    
    * fix spelling
    
    * fix badge style
    
    * fixes
    
    * small fixes missed
    
    * spell fix
    
    * revert changes for Program plugin
    
    * small tweaks
    
    * Add Grid control
    
    * merge warnings
    
    * fix spelling
    
    * fix resource
    
    * updating conflicitng keywords
    
    * remove keywordWarning and warningBagde
    
    * fix broken reference
    
    * add new information bar

--- src/settings-ui/Settings.UI.Library/ViewModels/PowerLauncherViewModel.cs
+++ src/settings-ui/Settings.UI.Library/ViewModels/PowerLauncherViewModel.cs
@@ -101,7 +101,7 @@ namespace Microsoft.PowerToys.Settings.UI.Library.ViewModels
         {
             if (
                 e.PropertyName == nameof(PowerLauncherPluginViewModel.ShowNotAccessibleWarning)
-                || e.PropertyName == nameof(PowerLauncherPluginViewModel.ShowNotAllowedKeywordWarning)
+                || e.PropertyName == nameof(PowerLauncherPluginViewModel.ShowBadgeOnPluginSettingError)
                 )
             {
                 // Don't trigger a settings update if the changed property is for visual notification.
