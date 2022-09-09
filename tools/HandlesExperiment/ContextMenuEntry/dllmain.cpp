fbd8e2bf425ac122be8e0ea2f6f3570ca7a0764d
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-09-09 11:23:38 +0200

    Added new project to implement the dll

---
+++ tools/HandlesExperiment/ContextMenuEntry/dllmain.cpp
@@ -0,0 +1,25 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+// Things to implement:
+// 1. A class which implements IExplorerCommand
+// 2. A class which implements IClassFactory
+// 3. DLL register/unregister functions which will create registry entries
+// 4. Other DLL exported functions
+
---
+++ tools/HandlesExperiment/ContextMenuEntry/framework.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
