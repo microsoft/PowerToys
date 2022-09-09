f65958b212594544798c4ead6e0617ccc74803e8
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-09-09 11:56:30 +0200

    Implemented IExplorerCommand methods

---
+++ tools/HandlesExperiment/ContextMenuEntry/Constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "pch.h"
+
+// Localizable Constants, these should be converted to resources
+namespace constants::localizable
+{
+	// Text shown in the context menu
+	constexpr WCHAR CommandTitle[] = L"What's using this file?";
+}
