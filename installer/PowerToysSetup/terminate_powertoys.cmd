0c701941187538bf9b3443d793365d5bbcd84750
yuyoyuppe <a.yuyoyuppe@gmail.com>
2022-09-09 13:35:10 +0200

    redirect 1 & 2

--- installer/PowerToysSetup/terminate_powertoys.cmd
+++ installer/PowerToysSetup/terminate_powertoys.cmd
@@ -5,7 +5,7 @@ setlocal ENABLEDELAYEDEXPANSION
 @REM doesn't give application an opportunity to cleanup. Thus we send WM_CLOSE which is being caught
 @REM by multiple windows running a msg loop in PowerToys.exe process, which we close one by one.
 for /l %%x in (1, 1, 100) do (
-    taskkill /IM PowerToys.exe 1>&2> nul
+    taskkill /IM PowerToys.exe 1>NUL 2>NUL
     if !ERRORLEVEL! NEQ 0 goto quit
 )
 
