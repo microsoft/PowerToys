479c768c755a0e57ce9f0832db90470b141c608b
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-09-12 12:41:59 +0200

    Implemented DLL register/unregister

--- tools/HandlesExperiment/ContextMenuEntry/Constants.h
+++ tools/HandlesExperiment/ContextMenuEntry/Constants.h
@@ -2,9 +2,24 @@
 
 #include "pch.h"
 
-// Localizable Constants, these should be converted to resources
+// Localizable constants, these should be converted to resources
 namespace constants::localizable
 {
 	// Text shown in the context menu
 	constexpr WCHAR CommandTitle[] = L"What's using this file?";
 }
+
+// Non-localizable constants
+namespace constants::nonlocalizable
+{
+	// Description of the registry key
+	constexpr WCHAR RegistryKeyDescription[] = L"File Locksmith Shell Extension";
+
+	// File name of the DLL
+	constexpr WCHAR FileNameDLL[] = L"ContextMenuEntry.dll";
+}
+
+// Macros, non-localizable
+ 
+// Description of the registry key
+#define REGISTRY_CONTEXT_MENU_KEY  L"FileLocksmithExt"
--- tools/HandlesExperiment/ContextMenuEntry/dllmain.cpp
+++ tools/HandlesExperiment/ContextMenuEntry/dllmain.cpp
@@ -4,6 +4,10 @@
 // Additional libraries to link
 #pragma comment(lib, "shlwapi")
 
+#include "Registry.h"
+
+HMODULE dll_instance;
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -12,6 +16,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        dll_instance = hModule;
+        break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
@@ -20,8 +26,26 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+
+STDAPI DllRegisterServer()
+{
+    if (!add_registry_keys())
+    {
+        // Best effort here
+        delete_registry_keys();
+        return E_FAIL;
+    }
+
+    return S_OK;
+}
+
+STDAPI DllUnregisterServer()
+{
+    return delete_registry_keys() ? S_OK : E_FAIL;
+}
+
 // Things to implement:
 // 1. (DONE) A class which implements IExplorerCommand
 // 2. (DONE) A class which implements IClassFactory
-// 3. DLL register/unregister functions which will create registry entries
+// 3. (DONE) DLL register/unregister functions which will create registry entries
 // 4. Other DLL exported functions
