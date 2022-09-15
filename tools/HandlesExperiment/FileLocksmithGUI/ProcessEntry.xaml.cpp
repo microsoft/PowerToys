7ac603fecfd366d45baad249993f20b3ebb3779b
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-09-15 14:21:09 +0200

    Added a control for displaying results

---
+++ tools/HandlesExperiment/FileLocksmithGUI/ProcessEntry.xaml.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "ProcessEntry.xaml.h"
+#if __has_include("ProcessEntry.g.cpp")
+#include "ProcessEntry.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::FileLocksmithGUI::implementation
+{
+    ProcessEntry::ProcessEntry(const winrt::hstring& process, int pid)
+    {
+        InitializeComponent();
+        processName().Text(process);
+        processPid().Text(winrt::to_hstring(pid));
+    }
+}
