8ebd02135e97729d843a84a13501e10fafd8c7ac
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-09-16 16:15:56 +0200

    Remove useless code

--- tools/HandlesExperiment/FileLocksmithGUI/MainWindow.xaml.h
+++ tools/HandlesExperiment/FileLocksmithGUI/MainWindow.xaml.h
@@ -9,11 +9,6 @@ namespace winrt::FileLocksmithGUI::implementation
     {
         MainWindow();
 
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
-
-        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
-
     private:
         std::vector<ProcessResult> m_process_info;
         void find_processes();
