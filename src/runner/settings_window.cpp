c2cde10b513e5531fdb4f2e7c653e999af958e98
Jeff Lord <jeff@jefflord.com>
2022-09-17 09:12:59 -0400

    cleanup

--- src/runner/settings_window.cpp
+++ src/runner/settings_window.cpp
@@ -85,6 +85,9 @@ std::optional<std::wstring> dispatch_json_action_to_module(const json::JsonObjec
                 }
                 else if (action == L"restart_maintain_elevation")
                 {
+                    // this was added to restart and maintain elevation, which is needed after settings are change from outside the normal process.
+                    // since a normal PostQuitMessage(0) would usually cause this procsess to save it's in memory settings to disk, we need to 
+                    // send a PostQuitMessage(1) and chaeck for that on exit, and skip the settings-flush.
                     auto loaded = PTSettingsHelper::load_general_settings();
 
                     if (is_process_elevated())
