eac98b4d865e2e6ee006cacb2dbb248a140bc5c6
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-09-19 10:41:19 +0200

    Add Ext and GUI projects to solution

---
+++ src/modules/FileLocksmith/FileLocksmithGUI/App.xaml.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::FileLocksmithGUI::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+
+    private:
+        winrt::Microsoft::UI::Xaml::Window window{ nullptr };
+    };
+}
