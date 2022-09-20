ff568ed010f4c3eccd9ea39bbc8c37b993d5cb74
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-09-20 15:27:52 +0200

    Attempt at adding resources

---
+++ src/modules/FileLocksmith/FileLocksmithLib/resource.base.h
@@ -0,0 +1,9 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FileLocksmithLib.rc
+
+//////////////////////////////
+// Non-localizable
+
+// Non-localizable
+//////////////////////////////
