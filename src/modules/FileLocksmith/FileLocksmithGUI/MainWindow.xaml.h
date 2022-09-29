c8be16802c9985fd0b884924b011e317622382cb
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-09-29 10:31:07 +0200

    Dark title bar on dark themes

--- src/modules/FileLocksmith/FileLocksmithGUI/MainWindow.xaml.h
+++ src/modules/FileLocksmith/FileLocksmithGUI/MainWindow.xaml.h
@@ -11,9 +11,12 @@ namespace winrt::FileLocksmithGUI::implementation
         void DisplayNoResultsInfo();
     private:
         void find_processes();
+        void find_native_handle();
         void place_and_resize();
         void display_text_info(std::wstring text);
         void watch_process(DWORD pid);
+
+        HWND m_native_handle = NULL;
     };
 }
 
