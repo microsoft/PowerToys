1161f3fc9d31aae3d6bddc30ec1811ad26ef5729
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-09-29 11:24:04 +0200

    Show "No results" after killing all processes

--- src/modules/FileLocksmith/FileLocksmithGUI/MainWindow.xaml.h
+++ src/modules/FileLocksmith/FileLocksmithGUI/MainWindow.xaml.h
@@ -8,12 +8,12 @@ namespace winrt::FileLocksmithGUI::implementation
     struct MainWindow : MainWindowT<MainWindow>
     {
         MainWindow();
-        void DisplayNoResultsInfo();
     private:
         void find_processes();
         void find_native_handle();
         void place_and_resize();
         void display_text_info(std::wstring text);
+        void display_no_results_if_empty();
         void watch_process(DWORD pid);
 
         HWND m_native_handle = NULL;
