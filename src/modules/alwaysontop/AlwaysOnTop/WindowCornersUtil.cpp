1ecaf039ca8a86d4e7e503e6c81157b96e821b54
Seraphima Zykova <zykovas91@gmail.com>
2022-09-29 15:11:01 +0200

    [AlwaysOnTop] Corners scaling (#20352)

--- src/modules/alwaysontop/AlwaysOnTop/WindowCornersUtil.cpp
+++ src/modules/alwaysontop/AlwaysOnTop/WindowCornersUtil.cpp
@@ -20,9 +20,9 @@ enum DWM_WINDOW_CORNER_PREFERENCE
     DWMWCP_ROUNDSMALL = 3
 };
 
-bool WindowBordersUtils::AreCornersRounded(HWND window) noexcept
+int WindowCornerUtils::CornerPreference(HWND window) noexcept
 {
-    int cornerPreference = DWMWCP_DEFAULT;
+    int cornerPreference = -1;
     auto res = DwmGetWindowAttribute(window, DWMWA_WINDOW_CORNER_PREFERENCE, &cornerPreference, sizeof(cornerPreference));
     if (res != S_OK)
     {
@@ -31,9 +31,24 @@ bool WindowBordersUtils::AreCornersRounded(HWND window) noexcept
         {
             Logger::error(L"Get corner preference error: {}", get_last_error_or_default(res)); 
         }
-
-        return false;
     }
 
-    return cornerPreference != DWM_WINDOW_CORNER_PREFERENCE::DWMWCP_DONOTROUND;
+    return cornerPreference;
+}
+
+int WindowCornerUtils::CornersRadius(HWND window) noexcept
+{
+    int cornerPreference = CornerPreference(window);
+
+    switch (cornerPreference)
+    {
+    case DWM_WINDOW_CORNER_PREFERENCE::DWMWCP_ROUND:
+        return 8;
+    case DWM_WINDOW_CORNER_PREFERENCE::DWMWCP_ROUNDSMALL:
+        return 4;
+    case DWM_WINDOW_CORNER_PREFERENCE::DWMWCP_DEFAULT:
+        return 8;
+    default:
+        return 0;
+    }
 }
