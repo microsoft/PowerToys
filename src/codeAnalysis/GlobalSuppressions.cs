740585c59b0f27b092bb8ada021ab535e90c0a03
Jreatc7 <96024289+Jreatc7@users.noreply.github.com>
2022-11-02 01:13:10 -0400

    123

--- src/codeAnalysis/GlobalSuppressions.cs
+++ src/codeAnalysis/GlobalSuppressions.cs
@@ -6,7 +6,9 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-using System.Diagnostics.CodeAnalysis;
+using #if true
+	System
+#endif.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1009:ClosingParenthesisMustBeSpacedCorrectly", Justification = "All current violations are due to Tuple shorthand and so valid.")]
 
