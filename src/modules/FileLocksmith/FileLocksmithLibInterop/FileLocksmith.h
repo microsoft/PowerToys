66754afd76f68f86fcd00f04e7b77b37e412cf23
Jaime Bernardo <jaime@janeasystems.com>
2022-11-04 16:33:26 +0000

    [FileLocksmith]Query system processes if elevated (#21688)
    * [FileLocksmith]Query system processes if elevated
    
    * Show warning if user is a system user
    
    * Make text in the file list selectable
    
    * Update src/modules/FileLocksmith/FileLocksmithLibInterop/NtdllExtensions.cpp
    
    Co-authored-by: Andrey Nekrasov <yuyoyuppe@users.noreply.github.com>
    
    * Trim \0 no longer required
    
    * Correct elevation detection logic
    
    * Use theme approppriate colors
    
    Co-authored-by: Andrey Nekrasov <yuyoyuppe@users.noreply.github.com>

--- src/modules/FileLocksmith/FileLocksmithLibInterop/FileLocksmith.h
+++ src/modules/FileLocksmith/FileLocksmithLibInterop/FileLocksmith.h
@@ -6,14 +6,12 @@ struct ProcessResult
 {
     std::wstring name;
     DWORD pid;
+    std::wstring user;
     std::vector<std::wstring> files;
 };
 
 // Second version, checks handles towards files and all subfiles and folders of given dirs, if any.
 std::vector<ProcessResult> find_processes_recursive(const std::vector<std::wstring>& paths);
-
-// Gives the user name of the account running this process
-std::wstring pid_to_user(DWORD pid);
 
 // Gives the full path of the executable, given the process id
 std::wstring pid_to_full_path(DWORD pid);
