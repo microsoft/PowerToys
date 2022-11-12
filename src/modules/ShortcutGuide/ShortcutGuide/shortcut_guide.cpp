e22c51d9781b2a706fa2ae38a8c4dc339409fef1
Okami Wong <wtfimba@live.com>
2022-11-12 21:00:48 +0800

    Handle the case when the shortcut guide is not activated by win key long press

--- src/modules/ShortcutGuide/ShortcutGuide/shortcut_guide.cpp
+++ src/modules/ShortcutGuide/ShortcutGuide/shortcut_guide.cpp
@@ -196,10 +196,20 @@ OverlayWindow::OverlayWindow(HWND activeWindow)
 void OverlayWindow::ShowWindow()
 {
     winkey_popup = std::make_unique<D2DOverlayWindow>();
-    winkey_popup->apply_press_time_for_global_windows_shortcuts(windowsKeyPressTimeForGlobalWindowsShortcuts.value);
-    winkey_popup->apply_press_time_for_taskbar_icon_shortcuts(windowsKeyPressTimeForTaskbarIconShortcuts.value);
     winkey_popup->apply_overlay_opacity(((float)overlayOpacity.value) / 100.0f);
     winkey_popup->set_theme(theme.value);
+
+    // The press time only takes effect when the shortcut guide is activated by pressing the win key.
+    if (shouldReactToPressedWinKey.value)
+    {
+        winkey_popup->apply_press_time_for_global_windows_shortcuts(windowsKeyPressTimeForGlobalWindowsShortcuts.value);
+        winkey_popup->apply_press_time_for_taskbar_icon_shortcuts(windowsKeyPressTimeForTaskbarIconShortcuts.value);
+    }
+    else
+    {
+        winkey_popup->apply_press_time_for_global_windows_shortcuts(0);
+        winkey_popup->apply_press_time_for_taskbar_icon_shortcuts(0);
+    }
 
     target_state = std::make_unique<TargetState>();
     try
