7a8a36689751e2694082b98554dcc42b5e4d0bfa
Sophia Chen <sophia.six.chen@gmail.com>
2022-11-21 12:46:47 -0800

    reverting unintended change

--- src/settings-ui/Settings.UI/OobeWindow.xaml.cs
+++ src/settings-ui/Settings.UI/OobeWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
+
 using System;
 using interop;
 using ManagedCommon;
