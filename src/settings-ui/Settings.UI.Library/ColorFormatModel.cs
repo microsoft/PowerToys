5e121b57d8c3d0564823f9330a43a3eb5af7f893
Laszlo Nemeth <laszlo.nemeth.hu@gmail.com>
2022-11-21 12:09:21 +0100

    Removing unnecessary using directive

--- src/settings-ui/Settings.UI.Library/ColorFormatModel.cs
+++ src/settings-ui/Settings.UI.Library/ColorFormatModel.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.ComponentModel;
-using System.Drawing;
 using System.Runtime.CompilerServices;
 
 namespace Microsoft.PowerToys.Settings.UI.Library
