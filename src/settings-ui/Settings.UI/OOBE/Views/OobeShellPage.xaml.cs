bdf237baa88b1b4ca69159ba7a7691f6bddf46e5
Sophia Chen <sophia.six.chen@gmail.com>
2022-11-22 16:25:50 -0800

    remove unused packages

--- src/settings-ui/Settings.UI/OOBE/Views/OobeShellPage.xaml.cs
+++ src/settings-ui/Settings.UI/OOBE/Views/OobeShellPage.xaml.cs
@@ -198,7 +198,7 @@ namespace Microsoft.PowerToys.Settings.UI.OOBE.Views
                         }
                         else
                         {
-                            NavigationFrame.Navigate(typeof(OobeOverview);
+                            NavigationFrame.Navigate(typeof(OobeOverview));
                         }
 
                         break;
