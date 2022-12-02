e731cc04c1edd7772a11dd7b380ee08e7bf696dd
Aaron Junker <aaron.junker@outlook.com>
2022-12-02 15:45:49 +0100

    [QuickAccent] Add description of current selected letter (#20587)
    * Adding unicode names and adjusting UI (WIP)
    
    * Added changing letter names
    
    * Add optioins to hide description (WIP)
    
    * WIP
    
    * Change to binding property
    
    * Adress PR comments
    
    * Set TextBlock in border
    
    * * Added to settings
    * Fixed string showing/not showing one time after switching character
    * Removed unneccessairy command in SettingsService.cs
    * Moved showdescription enum to settings.ui
    
    * Adding Fluent design :)
    
    * Fix merge errors
    
    * Center list
    
    * Fixed code not working. Accepted some code style changes.
    
    * Merge main in branch #2
    
    * [Quick Accent] support unicode description for UTF-16 surrogate pairs
    
    * [Quick Accent] fix check-spelling-bot errors
    
    * [check-spelling] accept LANGID as correct word
    
    * [Quick Accent] fix delay when calling ShowToolbar for the first time
    
    * [Quick Accent] use toggle switch to turn off/on Unicode description
    
    * [Quick Accent] fix after merge
    
    * [Quick Accent] add UnicodeInformation.dll to installer
    
    Co-authored-by: Niels Laute <niels.laute@live.nl>

--- src/modules/poweraccent/PowerAccent.UI/Selector.xaml.cs
+++ src/modules/poweraccent/PowerAccent.UI/Selector.xaml.cs
@@ -3,15 +3,39 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Unicode;
 using System.Windows;
+using PowerToys.PowerAccentKeyboardService;
 using Point = PowerAccent.Core.Point;
 using Size = PowerAccent.Core.Size;
 
 namespace PowerAccent.UI;
 
-public partial class Selector : Window, IDisposable
+public partial class Selector : Window, IDisposable, INotifyPropertyChanged
 {
-    private Core.PowerAccent _powerAccent = new Core.PowerAccent();
+    private readonly Core.PowerAccent _powerAccent = new ();
+
+    private Visibility _characterNameVisibility = Visibility.Visible;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public Visibility CharacterNameVisibility
+    {
+        get
+        {
+            return _characterNameVisibility;
+        }
+
+        set
+        {
+            _characterNameVisibility = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CharacterNameVisibility)));
+        }
+    }
 
     public Selector()
     {
@@ -31,11 +55,16 @@ public partial class Selector : Window, IDisposable
     private void PowerAccent_OnSelectionCharacter(int index, string character)
     {
         characters.SelectedIndex = index;
+
+        characterName.Text = _powerAccent.CharacterDescriptions[index];
     }
 
     private void PowerAccent_OnChangeDisplay(bool isActive, string[] chars)
     {
+        CharacterNameVisibility = _powerAccent.ShowUnicodeDescription ? Visibility.Visible : Visibility.Collapsed;
+
         this.Visibility = isActive ? Visibility.Visible : Visibility.Collapsed;
+
         if (isActive)
         {
             characters.ItemsSource = chars;
@@ -52,7 +81,7 @@ public partial class Selector : Window, IDisposable
     private void CenterWindow()
     {
         UpdateLayout();
-        Size window = new Size(((System.Windows.Controls.Panel)Application.Current.MainWindow.Content).ActualWidth, ((System.Windows.Controls.Panel)Application.Current.MainWindow.Content).ActualHeight);
+        Size window = new (((System.Windows.Controls.Panel)Application.Current.MainWindow.Content).ActualWidth, ((System.Windows.Controls.Panel)Application.Current.MainWindow.Content).ActualHeight);
         Point position = _powerAccent.GetDisplayCoordinates(window);
         this.Left = position.X;
         this.Top = position.Y;
