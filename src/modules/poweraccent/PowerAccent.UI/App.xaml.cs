810ab485e66d547a0d4ebf9e9766f7b6b6386a98
Niels Laute <niels.laute@live.nl>
2022-12-03 15:08:57 +0100

    Adding theme awereness

--- src/modules/poweraccent/PowerAccent.UI/App.xaml.cs
+++ src/modules/poweraccent/PowerAccent.UI/App.xaml.cs
@@ -2,17 +2,21 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Threading;
 using System.Windows;
+using Common.UI;
 
 namespace PowerAccent.UI
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, IDisposable
     {
         private static Mutex _mutex;
+        private bool disposedValue;
+        private ThemeManager _themeManager;
 
         protected override void OnStartup(StartupEventArgs e)
         {
@@ -26,7 +30,27 @@ namespace PowerAccent.UI
                 Application.Current.Shutdown();
             }
 
+            _themeManager = new ThemeManager(this);
             base.OnStartup(e);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                _themeManager?.Dispose();
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
