3950bba9cfc4381a5285a0b88c193f647a1fa95f
Julia Taylor-Hell <juliata@microsoft.com>
2022-12-06 12:58:58 -0800

    Using the same list of file extensions as Lightbox's AppxManifest, and ensuring we convert file extension to lowercase

--- src/modules/peek/Peek.FilePreviewer/Previewers/PreviewerFactory.cs
+++ src/modules/peek/Peek.FilePreviewer/Previewers/PreviewerFactory.cs
@@ -11,8 +11,9 @@ namespace Peek.FilePreviewer.Previewers
         public IPreviewer? Create(File file)
         {
             // TODO: investigate performance of reflection to resolve previewer type
-            switch (file.Extension)
+            switch (file.Extension.ToLower())
             {
+                // Image types
                 case ".bmp":
                 case ".gif":
                 case ".jpg":
@@ -24,6 +25,53 @@ namespace Peek.FilePreviewer.Previewers
                 case ".png":
                 case ".tif":
                 case ".tiff":
+                case ".dib":
+                case ".heic":
+                case ".heif":
+                case ".hif":
+                case ".avif":
+                case ".jxr":
+                case ".wdp":
+                case ".ico":
+                case ".thumb":
+
+                // Raw types
+                case ".arw":
+                case ".cr2":
+                case ".crw":
+                case ".erf":
+                case ".kdc":
+                case ".mrw":
+                case ".nef":
+                case ".nrw":
+                case ".orf":
+                case ".pef":
+                case ".raf":
+                case ".raw":
+                case ".rw2":
+                case ".rwl":
+                case ".sr2":
+                case ".srw":
+                case ".srf":
+                case ".dcs":
+                case ".dcr":
+                case ".drf":
+                case ".k25":
+                case ".3fr":
+                case ".ari":
+                case ".bay":
+                case ".cap":
+                case ".iiq":
+                case ".eip":
+                case ".fff":
+                case ".mef":
+                case ".mdc":
+                case ".mos":
+                case ".R3D":
+                case ".rwz":
+                case ".x3f":
+                case ".ori":
+                case ".cr3":
                     return new ImagePreviewer(file);
                 default:
                     return null;
