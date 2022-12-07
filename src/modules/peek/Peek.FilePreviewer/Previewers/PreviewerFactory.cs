6295ed86caad5ac0cf6c2b7c1535ddaa84f5a605
jth-ms <73617023+jth-ms@users.noreply.github.com>
2022-12-06 17:18:20 -0800

    Juliata/filetypes (#22538)
    * Using the same list of file extensions as Lightbox's AppxManifest, and ensuring we convert file extension to lowercase
    
    * Add IsFileTypeSupported to IPreviewer
    
    * respond to PR comments

--- src/modules/peek/Peek.FilePreviewer/Previewers/PreviewerFactory.cs
+++ src/modules/peek/Peek.FilePreviewer/Previewers/PreviewerFactory.cs
@@ -10,24 +10,13 @@ namespace Peek.FilePreviewer.Previewers
     {
         public IPreviewer? Create(File file)
         {
-            // TODO: investigate performance of reflection to resolve previewer type
-            switch (file.Extension)
+            if (ImagePreviewer.IsFileTypeSupported(file.Extension))
             {
-                case ".bmp":
-                case ".gif":
-                case ".jpg":
-                case ".jfif":
-                case ".jfi":
-                case ".jif":
-                case ".jpeg":
-                case ".jpe":
-                case ".png":
-                case ".tif":
-                case ".tiff":
-                    return new ImagePreviewer(file);
-                default:
-                    return null;
+                return new ImagePreviewer(file);
             }
+
+            // Other previewer types check their supported file types here
+            return null;
         }
     }
 }
