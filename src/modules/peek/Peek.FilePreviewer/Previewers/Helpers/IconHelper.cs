0327d4d55516bfb7fcb224537413ef1b86a54603
Jessie Su <Jessie.Su@microsoft.com>
2022-12-07 16:02:33 -0800

    Moving to string resource usage

--- src/modules/peek/Peek.FilePreviewer/Previewers/Helpers/IconHelper.cs
+++ src/modules/peek/Peek.FilePreviewer/Previewers/Helpers/IconHelper.cs
@@ -2,7 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace Peek.FilePreviewer.Previewers
+namespace Peek.FilePreviewer.Previewers.Helpers
 {
     using System;
     using System.Drawing;
