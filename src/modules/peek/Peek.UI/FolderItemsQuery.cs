cf9a02f382ead823e5fc410df24a18abd39f4e3b
Samuel Chapleau <sachaple@microsoft.com>
2022-12-08 18:41:03 -0800

    Undo last

--- src/modules/peek/Peek.UI/FolderItemsQuery.cs
+++ src/modules/peek/Peek.UI/FolderItemsQuery.cs
@@ -126,18 +126,6 @@ namespace Peek.UI
             }
         }
 
-        public static File? GetFileExplorerSelectedFile()
-        {
-            var shellItems = FileExplorerHelper.GetSelectedItems();
-            var firstSelectedItem = shellItems?.Item(0);
-            if (shellItems == null || firstSelectedItem == null)
-            {
-                return null;
-            }
-
-            return new File(firstSelectedItem.Path);
-        }
-
         // Finds index of firstSelectedItem either amongst folder items, initializing our internal File list
         //  since storing Shell32.FolderItems as a field isn't reliable.
         // Can take a few seconds for folders with 1000s of items; ensure it runs on a background thread.
