d737527f49bace357584b4057ccf842a40273101
אביתר אסתרסון <79063413+EvyatarEsterson@users.noreply.github.com>
2022-12-11 04:30:43 +0200

    add GetDefaultLetterKeyHE
    VK_MINUS:    { "־", "–" }
    
    modified:   src/modules/poweraccent/PowerAccent.Core/Languages.cs
    
    modified:   src/modules/poweraccent/PowerAccent.Core/Languages.cs

--- src/modules/poweraccent/PowerAccent.Core/Languages.cs
+++ src/modules/poweraccent/PowerAccent.Core/Languages.cs
@@ -20,6 +20,7 @@ namespace PowerAccent.Core
         DE,
         FR,
         HR,
+        HE,
         HU,
         IS,
         IT,
@@ -53,6 +54,7 @@ namespace PowerAccent.Core
                 Language.DE => GetDefaultLetterKeyDE(letter), // German
                 Language.FR => GetDefaultLetterKeyFR(letter), // French
                 Language.HR => GetDefaultLetterKeyHR(letter), // Croatian
+                Language.HE => GetDefaultLetterKeyHE(letter), // Hebrew
                 Language.HU => GetDefaultLetterKeyHU(letter), // Hungarian
                 Language.IS => GetDefaultLetterKeyIS(letter), // Iceland
                 Language.IT => GetDefaultLetterKeyIT(letter), // Italian
@@ -421,6 +423,16 @@ namespace PowerAccent.Core
                 LetterKey.VK_O => new string[] { "ö" },
                 LetterKey.VK_S => new string[] { "ß" },
                 LetterKey.VK_U => new string[] { "ü" },
+                _ => Array.Empty<string>(),
+            };
+        }
+
+        // Hebrew
+        private static string[] GetDefaultLetterKeyHE(LetterKey letter)
+        {
+            return letter switch
+            {
+                LetterKey.VK_MINUS => new string[] { "־", "–" },
                 _ => Array.Empty<string>(),
             };
         }
