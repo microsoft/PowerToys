5b77387270131cd19cd453dc664539359a85a15c
Jaime Bernardo <jaime@janeasystems.com>
2023-01-04 14:37:13 +0000

    [TextExtractor]Properly initialize PreferredLanguage

--- src/settings-ui/Settings.UI.Library/PowerOcrProperties.cs
+++ src/settings-ui/Settings.UI.Library/PowerOcrProperties.cs
@@ -11,6 +11,7 @@ namespace Microsoft.PowerToys.Settings.UI.Library
         public PowerOcrProperties()
         {
             ActivationShortcut = new HotkeySettings(true, false, false, true, 0x54); // Win+Shift+T
+            PreferredLanguage = string.Empty;
         }
 
         public HotkeySettings ActivationShortcut { get; set; }
