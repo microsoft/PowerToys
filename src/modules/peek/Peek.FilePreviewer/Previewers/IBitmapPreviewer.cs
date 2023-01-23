0b29d6ecf02e3d3777d9f5de135a8726a6384184
seraphima <zykovas91@gmail.com>
2023-01-23 18:05:12 +0100

    clean up

--- src/modules/peek/Peek.FilePreviewer/Previewers/IBitmapPreviewer.cs
+++ src/modules/peek/Peek.FilePreviewer/Previewers/IBitmapPreviewer.cs
@@ -2,7 +2,6 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 
 namespace Peek.FilePreviewer.Previewers
