ec0a838646bda5c0fa85cf25779051caada04279
Jeremy Sinclair <4016293+snickler@users.noreply.github.com>
2023-01-26 14:54:02 -0500

    Update parseRuntimes to use separate JSON for .NET Runtime / WPF

--- installer/PowerToysSetupCustomActions/parseRuntimes.ps1
+++ installer/PowerToysSetupCustomActions/parseRuntimes.ps1
@@ -1,13 +1,33 @@
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory = $True, Position = 1)]
-    [string]$depsjsonpath,
+    [string]$runtimedepsjsonpath,
     [Parameter(Mandatory = $True, Position = 2)]
-    [string]$depsfileslistspath,
+    [string]$wpfdepsjsonpath,
     [Parameter(Mandatory = $True, Position = 3)]
+    [string]$depsfileslistspath,
+    [Parameter(Mandatory = $True, Position = 4)]
     [string]$productwxspath
 )
 
+function Get-RuntimePack ($depsJsonFile, $runtimeName) {
+    Write-Host "Parsing $runtimeName Runtime"
+    $runtimePackList = ([array]$depsJsonFile.targets.PSObject.Properties)[-1].Value.PSObject.Properties | Where-Object { $_.Name -match "runtimepack.$runtimeName" };
+    
+    if ($runtimePackList.Length -eq 0) {
+        Write-Host -ForegroundColor Red "$runtimeName has not been found"
+        exit 1
+    }
+
+    # Enumerate through array of custom objects and parse the names of the property values into a HashTable
+    $runtimePackList | ForEach-Object { 
+        $runtimes += @{"$($_.Name -replace "runtimepack\.(\S+)\.\S+/\S+",'$1')" = $_.Value.PSObject.Properties.Value | ForEach-Object {
+                $_.PSObject.Properties.Name 
+            }
+        } 
+    }
+    $runtimes;
+}
 function Update-RuntimeFileList($runtimeToken, $runtimeKey) {
     $depsFilesLists -replace "($runtimeToken = )(.*);", "`$1 {`r`n$(($runtimes[$runtimeKey] | ForEach-Object {'    L"'+$_+'"'} | Sort-Object) -join ",`r`n") };"        
 }
@@ -16,8 +36,7 @@ function Update-ProductWxsRuntimeFileList($runtimeToken, $runtimeKey) {
     $productWxs -replace "(define $runtimeToken=)(.*)?>", "`$1$($runtimes[$runtimeKey] -join ';')?>"
 }
 
-function Update-DotnetFilesComponentGuid()
-{
+function Update-DotnetFilesComponentGuid() {
     $productWxs -replace "Dlls_DotnetFiles_Component"" Guid=""([{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?)""", "Dlls_DotnetFiles_Component"" Guid=""$((New-Guid).ToString().ToUpper())"""
 }
 
@@ -28,26 +47,13 @@ $depsFilesLists = Get-Content $depsfileslistspath;
 $productWxs = Get-Content $productwxspath;
 
 # Read the deps.json file and convert it to a JSON object
-$runtimeFile = Get-Content $depsjsonpath | ConvertFrom-Json;
+$runtimeFile = Get-Content $runtimedepsjsonpath | ConvertFrom-Json;
+$wpfRuntimeFile = Get-Content $wpfdepsjsonpath | ConvertFrom-Json;
 
 $runtimes = @{}
 
-Write-Host "Parsing .NET Runtimes from $depsjsonpath `r`n"
-
-$runtimeList = ([array]$runtimeFile.targets.PSObject.Properties)[-1].Value.PSObject.Properties | Where-Object { $_.Name -match "runtimepack" };
-
-if ($runtimeList.Length -eq 0) {
-    Write-Host -ForegroundColor Red "No runtimes have been detected"
-    exit 1
-}
-
-# Enumerate through array of custom objects and parse the names of the property values into a HashTable
-$runtimeList | ForEach-Object { 
-    $runtimes += @{"$($_.Name -replace "runtimepack\.(\S+)\.\S+/\S+",'$1')" = $_.Value.PSObject.Properties.Value | ForEach-Object {
-            $_.PSObject.Properties.Name 
-        }
-    } 
-} 
+$runtimes = Get-RuntimePack $runtimeFile "Microsoft.NETCore.App.Runtime"
+$runtimes = Get-RuntimePack $wpfRuntimeFile "Microsoft.WindowsDesktop.App.Runtime"
 
 Write-Host "Writing Microsoft.NETCore.App.Runtime files"
 $depsFilesLists = Update-RuntimeFileList "dotnetRuntimeFiles" "Microsoft.NETCore.App.Runtime"
