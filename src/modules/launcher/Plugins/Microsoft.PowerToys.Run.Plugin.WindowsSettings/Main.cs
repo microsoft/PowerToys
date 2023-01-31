a0094608eb6262853d0204b4d46b3f1e925fafbd
Heiko <61519853+htcfreek@users.noreply.github.com>
2023-01-31 20:57:52 +0100

    Update Main.cs

--- src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/Main.cs
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.WindowsSettings/Main.cs
@@ -107,7 +107,6 @@ namespace Microsoft.PowerToys.Run.Plugin.WindowsSettings
                 .OrderBy(found => found.Name);
 
             var newList = ResultHelper.GetResultList(filteredList, query.Search, _defaultIconPath);
-
             return newList;
 
             bool Predicate(WindowsSetting found)
