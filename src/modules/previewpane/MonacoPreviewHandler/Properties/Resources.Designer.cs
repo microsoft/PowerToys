77b0586677192d1d9843b892fc89d52e126d4478
Aaron Junker <aaron.junker@outlook.com>
2023-02-01 08:48:44 +0100

    Merge "main" into branch

--- src/modules/previewpane/MonacoPreviewHandler/Properties/Resources.Designer.cs
+++ src/modules/previewpane/MonacoPreviewHandler/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Microsoft.PowerToys.PreviewHandler.Monaco.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.PowerToys.PreviewHandler.Monaco.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
