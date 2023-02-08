fe658053b2936b5261c938930b772496fee58976
Jaime Bernardo <jaime@janeasystems.com>
2023-02-08 11:44:49 +0000

    Missing parenthesis after merge

--- src/modules/previewpane/SvgThumbnailProviderCpp/SvgThumbnailProvider.cpp
+++ src/modules/previewpane/SvgThumbnailProviderCpp/SvgThumbnailProvider.cpp
@@ -161,7 +161,7 @@ IFACEMETHODIMP SvgThumbnailProvider::GetThumbnail(UINT cx, HBITMAP* phbmp, WTS_A
 
                 if (std::filesystem::exists(fileNameBmp))
                 {
-                    *phbmp = static_cast<HBITMAP>LoadImage(NULL, fileNameBmp.c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+                    *phbmp = static_cast<HBITMAP>(LoadImage(NULL, fileNameBmp.c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE));
                     *pdwAlpha = WTS_ALPHATYPE::WTSAT_ARGB;
                     std::filesystem::remove(fileNameBmp);
                 }
