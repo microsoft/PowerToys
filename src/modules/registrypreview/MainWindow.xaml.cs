969640107d3231d04ddd4a2e5855a5de2cb21cd3
Randy <randy@randyrants.com>
2023-02-08 18:49:33 -0800

    Added comments for spell-checker
    Unclear if this side step should be done or not, but the kxz comes from a GUID and the other three names are constants.

--- src/modules/registrypreview/MainWindow.xaml.cs
+++ src/modules/registrypreview/MainWindow.xaml.cs
@@ -15,6 +15,10 @@ using Windows.ApplicationModel.Resources;
 using Windows.Management.Core;
 using Windows.Storage;
 
+// spell-check-ignore: REGISTRYHEADER4
+// spell-check-ignore: REGISTRYHEADER5
+// spell-check-ignore: APPNAME
+// spell-check-ignore: kxz
 namespace RegistryPreview
 {
     public sealed partial class MainWindow : Window
