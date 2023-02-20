a2d940cb12defc552c8cf7a50f296c4fb7ea1eb7
htcfreek <61519853+htcfreek@users.noreply.github.com>
2023-02-20 15:40:24 +0100

    fix and inactivate PeriodicUpdateCheck gpo

--- src/runner/UpdateUtils.cpp
+++ src/runner/UpdateUtils.cpp
@@ -191,11 +191,11 @@ void PeriodicUpdateWorker()
 {
     // Check if periodic update check is disabled by GPO.
     // This policy code is implemented but not active. It is for later usage in PT version after 1.0 release.
-    if (powertoys_gpo::getDisablePeriodicUpdateCheckValue() == powertoys_gpo::gpo_rule_configured_disabled)
-    {
-        Logger::info(L"Initialization of periodic update checks stopped. Periodic update checks are disabled by GPO.");
-        return;
-    }
+    //if (powertoys_gpo::getDisablePeriodicUpdateCheckValue() == powertoys_gpo::gpo_rule_configured_enabled)
+    //{
+    //    Logger::info(L"Initialization of periodic update checks stopped. Periodic update checks are disabled by GPO.");
+    //    return;
+    //}
 
     for (;;)
     {
