79ec79fd582957b01a7f3fbdb4216a7252bc5884
ChaseKnowlden <haroldknowlden@gmail.com>
2023-02-23 16:36:04 -0500

    Conflict resolving Part 1

--- src/settings-ui/Settings.UI.Library/PowerLauncherProperties.cs
+++ src/settings-ui/Settings.UI.Library/PowerLauncherProperties.cs
@@ -72,6 +72,9 @@ namespace Microsoft.PowerToys.Settings.UI.Library
         [JsonPropertyName("search_wait_for_slow_results")]
         public bool SearchWaitForSlowResults { get; set; }
 
+        [JsonPropertyName("should_use_pinyin")]
+        public bool ShouldUsePinyin { get; set; }
+
         [JsonPropertyName("generate_thumbnails_from_files")]
         public bool GenerateThumbnailsFromFiles { get; set; }
 
@@ -103,6 +106,7 @@ namespace Microsoft.PowerToys.Settings.UI.Library
             SearchQueryTuningEnabled = false;
             SearchWaitForSlowResults = false;
             GenerateThumbnailsFromFiles = true;
+            ShouldUsePinyin = false;
         }
     }
 }
