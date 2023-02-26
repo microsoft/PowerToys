25ccc3213eb8e023158e116eb187198aaf0bca96
Aaron Junker <aaron.junker@outlook.com>
2023-02-26 15:30:44 +0100

    Remove unneccesairy code

--- src/common/interop/GetLocalisation.h
+++ src/common/interop/GetLocalisation.h
@@ -1,10 +1,6 @@
 #pragma once
 #include "Generated Files/resource.h"
-#ifndef GET_RESOURCE_STRING
 #include <../utils/resources.h>
-#endif
-
-
 
 std::wstring GetLocalisation(int key) {
     return GET_RESOURCE_STRING(key);
