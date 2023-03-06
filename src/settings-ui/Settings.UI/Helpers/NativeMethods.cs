68428122b5de9510e3ba6fabb2be42a3ea257aa3
sosssego <fredmoron@gmail.com>
2023-03-06 17:26:54 -0300

    making Dll Import methods internal

--- src/settings-ui/Settings.UI/Helpers/NativeMethods.cs
+++ src/settings-ui/Settings.UI/Helpers/NativeMethods.cs
@@ -38,6 +38,9 @@ namespace Microsoft.PowerToys.Settings.UI.Helpers
         [DllImport("user32.dll")]
         internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
+        [DllImport("Comdlg32.dll", CharSet = CharSet.Unicode)]
+        internal static extern bool GetOpenFileName([In, Out] OpenFileName openFileName);
+
 #pragma warning disable CA1401 // P/Invokes should not be visible
         [DllImport("user32.dll")]
         public static extern bool ShowWindow(System.IntPtr hWnd, int nCmdShow);
@@ -60,8 +63,6 @@ namespace Microsoft.PowerToys.Settings.UI.Helpers
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
         public static extern bool FreeLibrary(IntPtr hModule);
 
-        [DllImport("Comdlg32.dll", CharSet = CharSet.Unicode)]
-        public static extern bool GetOpenFileName([In, Out] OpenFileName openFileName);
 #pragma warning restore CA1401 // P/Invokes should not be visible
 
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
