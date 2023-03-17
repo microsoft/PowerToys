e6767c8e8bb87e508646a2ac8a80c34f051b3900
Michael Clayton <mike@kingslandconsulting.co.uk>
2023-03-17 18:12:27 +0000

    [Mouse Jump] - screenshot performance (#24607) (#24630)
    * [Mouse Jump] - screenshot performance (#24607)
    
    * [Mouse Jump] - add words to spellchecker (#24607)
    
    * [Mouse Jump] - progressive activation (#24607)
    
    * [Mouse Jump] - fixing build (#24607)
    
    * [Mouse Jump] - fixing jump location, add unit tests, refactor (#24607)
    
    * [Mouse Jump] - removed testing code (#24607)
    
    * [Mouse Jump] added failing tests for
    
    * [Mouse Jump] - fix problem with some monitor layouts (#24607)
    
    * [Mouse Jump] - cleaning up some comments and namepsaces
    
    * [Mouse Jump] - added another screen layout test (#24607)

---
+++ src/modules/MouseUtils/MouseJumpUI/NativeWrappers/Gdi32/Gdi32.StretchBlt.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using MouseJumpUI.NativeMethods.Core;
+
+namespace MouseJumpUI.NativeWrappers;
+
+internal static partial class Gdi32
+{
+    public static BOOL StretchBlt(
+        HDC hdcDest,
+        int xDest,
+        int yDest,
+        int wDest,
+        int hDest,
+        HDC hdcSrc,
+        int xSrc,
+        int ySrc,
+        int wSrc,
+        int hSrc,
+        NativeMethods.Gdi32.ROP_CODE rop)
+    {
+        var result = NativeMethods.Gdi32.StretchBlt(
+            hdcDest,
+            xDest,
+            yDest,
+            wDest,
+            hDest,
+            hdcSrc,
+            xSrc,
+            ySrc,
+            wSrc,
+            hSrc,
+            rop);
+
+        return result
+            ? result
+            : throw new InvalidOperationException(
+                $"{nameof(Gdi32.StretchBlt)} returned {result.Value}");
+    }
+}
---
+++ src/modules/MouseUtils/MouseJumpUI/NativeWrappers/User32/User32.GetWindowDC.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using MouseJumpUI.NativeMethods.Core;
+
+namespace MouseJumpUI.NativeWrappers;
+
+internal static partial class User32
+{
+    public static HDC GetWindowDC(HWND hWnd)
+    {
+        var hdc = NativeMethods.User32.GetWindowDC(hWnd);
+
+        return hdc.IsNull
+            ? throw new InvalidOperationException(
+                $"{nameof(User32.GetWindowDC)} returned null")
+            : hdc;
+    }
+}
