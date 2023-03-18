8e4495e60784ee2f67fd5d2780058f206d399e75
Aaron Junker <aaron.junker@outlook.com>
2023-03-18 11:41:57 +0100

    Add Settings

--- src/settings-ui/Settings.UI/Views/AwakePage.xaml.cs
+++ src/settings-ui/Settings.UI/Views/AwakePage.xaml.cs
@@ -5,10 +5,10 @@
 using System;
 using System.IO;
 using System.IO.Abstractions;
+using ManagedCommon;
 using Microsoft.PowerToys.Settings.UI.Helpers;
 using Microsoft.PowerToys.Settings.UI.Library;
 using Microsoft.PowerToys.Settings.UI.Library.Interfaces;
-using Microsoft.PowerToys.Settings.UI.Library.Utilities;
 using Microsoft.PowerToys.Settings.UI.ViewModels;
 using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml.Controls;
