7d6a7744a886662b0a9e18216d53dadfb837b8db
Aaron Junker <aaron.junker@outlook.com>
2023-03-20 19:18:20 +0100

    Fix FancyZones tools build (#24901)
    * Fix FancyZones Hit test build
    
    * Fix DrawLayoutTest
    
    * Fix Zonable tester
    
    * Adress PR comments

--- tools/FancyZone_HitTest/FancyZone_HitTest/Utilities.cs
+++ tools/FancyZone_HitTest/FancyZone_HitTest/Utilities.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Windows;
 using System.Windows.Media;
 
