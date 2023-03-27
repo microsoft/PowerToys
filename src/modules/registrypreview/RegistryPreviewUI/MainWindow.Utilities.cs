a38f2c12836c1c7b6f452338218e2ad7c3a59a66
Randy <randy@randyrants.com>
2023-03-27 11:00:22 -0700

    Updating resizing logic
    Changed to the non-async version of reading a file.

--- src/modules/registrypreview/RegistryPreviewUI/MainWindow.Utilities.cs
+++ src/modules/registrypreview/RegistryPreviewUI/MainWindow.Utilities.cs
@@ -9,9 +9,12 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.Data.Json;
 using Windows.Foundation.Metadata;
 using Windows.Storage;
 
@@ -733,40 +736,23 @@ namespace RegistryPreview
             ChangeCursor(gridPreview, false);
         }
 
-        private async void OpenSettingsFile(string path, string filename)
+        private void OpenSettingsFile(string path, string filename)
         {
-            StorageFolder storageFolder = null;
-            StorageFile storageFile = null;
             string fileContents = string.Empty;
-
-            try
+            string storageFile = Path.Combine(path, filename);
+            if (File.Exists(storageFile))
             {
-                storageFolder = await StorageFolder.GetFolderFromPathAsync(path);
-            }
-            catch
-            {
-            }
-
-            try
-            {
-                if (storageFolder != null)
+                try
                 {
-                    storageFile = await storageFolder.GetFileAsync(filename);
+                    TextReader reader = new StreamReader(storageFile);
+                    fileContents = reader.ReadToEnd();
+                    reader.Close();
                 }
-            }
-            catch
-            {
-            }
-
-            try
-            {
-                if (storageFile != null)
+                catch
                 {
-                    fileContents = await Windows.Storage.FileIO.ReadTextAsync(storageFile);
+                    // set up default JSON blob
+                    fileContents = "{ }";
                 }
-            }
-            catch
-            {
             }
 
             try
