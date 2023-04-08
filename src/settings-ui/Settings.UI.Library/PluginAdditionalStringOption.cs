1494189260f538fd1e8bd6e82f8c881e2d8914bc
Simone Franco <ratchet97@live.it>
2023-04-08 14:10:15 +0200

    Adds a PluginAdditionalStringOption Class for String settings management in PT Run Plugins - First commit to open a Draft PR

---
+++ src/settings-ui/Settings.UI.Library/PluginAdditionalStringOption.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.PowerToys.Settings.UI.Library;
+
+namespace Settings.UI.Library
+{
+    public class PluginAdditionalStringOption : PluginAdditionalOption
+    {
+        public new string Value { get; set; }
+    }
+}
