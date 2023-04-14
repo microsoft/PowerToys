931108eca4b3d479268bf4b7e5e65f8fe67fc0af
Sebastian Zanoni <zanonisebastian@gmail.com>
2023-04-14 10:19:15 +0200

    SvgPreviewControl: Take user configuration into consideration when generating svg preview

--- src/modules/previewpane/SvgPreviewHandler/SvgHTMLPreviewGenerator.cs
+++ src/modules/previewpane/SvgPreviewHandler/SvgHTMLPreviewGenerator.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Drawing;
 using System.Globalization;
 
 namespace SvgPreviewHandler
@@ -12,7 +13,15 @@ namespace SvgPreviewHandler
             url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAABhGlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw0AcxV/TiqItDnYQcchQxcGCqIijVLEIFkpboVUHk0u/oElDkuLiKLgWHPxYrDq4OOvq4CoIgh8gri5Oii5S4v+SQosYD4778e7e4+4dIDQqTDUDE4CqWUYqHhOzuVWx+xUB9CEEYExipp5IL2bgOb7u4ePrXZRneZ/7c4SUvMkAn0g8x3TDIt4gntm0dM77xGFWkhTic+Jxgy5I/Mh12eU3zkWHBZ4ZNjKpeeIwsVjsYLmDWclQiaeJI4qqUb6QdVnhvMVZrdRY6578hcG8tpLmOs1hxLGEBJIQIaOGMiqwEKVVI8VEivZjHv4hx58kl0yuMhg5FlCFCsnxg//B727NwtSkmxSMAV0vtv0xAnTvAs26bX8f23bzBPA/A1da219tALOfpNfbWuQI6N8GLq7bmrwHXO4Ag0+6ZEiO5KcpFArA+xl9Uw4YuAV619zeWvs4fQAy1NXyDXBwCIwWKXvd4909nb39e6bV3w87j3KR+nFUEgAAAAlwSFlzAAAuIwAALiMBeKU/dgAAAAd0SU1FB+cECw0KNtiZThsAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAAMElEQVQoz2N0dXVlgIFdu3bB2W5ubljFmRhIBLTXwPj//3+C7kYWH4x+GI2HQeEHAKsiGbWMbaqGAAAAAElFTkSuQmCC');
             """;
 
-        private const string HtmlTemplate = """
+        private const string HtmlTemplateSolidColor = """
+            <html>
+                <body style="background-color: {0}">
+                    {1}
+                </body>
+            </html>
+            """;
+
+        private const string HtmlTemplateCheckered = """
             <html>
                 <body style="background-image: {0}">
                     {1}
@@ -20,9 +29,17 @@ namespace SvgPreviewHandler
             </html>
             """;
 
+        private Settings settings = new Settings();
+
         public string GeneratePreview(string svgData)
         {
-            return string.Format(CultureInfo.InvariantCulture, HtmlTemplate, CheckeredBackgroundShade1, svgData);
+            var colorMode = settings.ColorMode;
+            return colorMode switch
+            {
+                1 => string.Format(CultureInfo.InvariantCulture, HtmlTemplateSolidColor, ColorTranslator.ToHtml(settings.SolidColor), svgData),
+                2 => string.Format(CultureInfo.InvariantCulture, HtmlTemplateCheckered, CheckeredBackgroundShade1, svgData),
+                _ => string.Format(CultureInfo.InvariantCulture, HtmlTemplateSolidColor, ColorTranslator.ToHtml(settings.ThemeColor), svgData),
+            };
         }
     }
 }
