7d153240f705a1c6c76ca7d77c894c67ded3c20f
Aaron Junker <aaron.junker@outlook.com>
2023-05-25 22:45:36 +0200

    Update definition

--- src/common/FilePreviewCommon/customLanguages/gitignore.js
+++ src/common/FilePreviewCommon/customLanguages/gitignore.js
@@ -5,7 +5,7 @@
         tokenizer: {
             root: [
                 [/^#.*$/, 'comment'],
-                [/^\s*!/, 'negation'],
+                [/^\s*!.*/, 'invalid'],
                 [/^\s*[^#]+/, "tag"]
             ]
         }
