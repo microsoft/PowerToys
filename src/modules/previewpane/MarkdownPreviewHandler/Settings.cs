ab1a39381174823088c0f833e1215a66022affa0
Jaime Bernardo <jaime@janeasystems.com>
2023-06-14 11:08:12 +0100

    Update src/modules/previewpane/MarkdownPreviewHandler/Settings.cs

--- src/modules/previewpane/MarkdownPreviewHandler/Settings.cs
+++ src/modules/previewpane/MarkdownPreviewHandler/Settings.cs
@@ -14,6 +14,8 @@ namespace Microsoft.PowerToys.PreviewHandler.Markdown
     {
         /// <summary>
         /// Gets the color of the window background.
+        /// Even though this is not a setting yet, it's retrieved from a "Settings" class to be aligned with other preview handlers that contain this setting.
+        /// It's possible it can be converted into a setting in the future.
         /// </summary>
         public static Color BackgroundColor
         {
