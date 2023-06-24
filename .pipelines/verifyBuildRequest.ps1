7ea4a65ebf93f88d4998eb32525d2f46f7621d24
Jeremy Sinclair <4016293+snickler@users.noreply.github.com>
2023-06-24 12:50:44 -0400

    Fixed GitHub API url for commits

--- .pipelines/verifyBuildRequest.ps1
+++ .pipelines/verifyBuildRequest.ps1
@@ -4,7 +4,8 @@ Param(
     [string]$commit
 )
 
-$gitHubCommit = Invoke-RestMethod -Method Get "https://api.github.com/microsoft/PowerToys/$commit"
+$gitHubCommit = Invoke-RestMethod -Method Get "https://api.github.com/repos/microsoft/PowerToys/commits/$commit"
+# If there are no files updated in the commit that are .md, set skipBuild variable
 if(($githubCommit.files.filename -notmatch ".md").Length -eq 0)
 {
     Write-Host '##vso[task.setvariable variable=skipBuild;isOutput=true]Yes'
