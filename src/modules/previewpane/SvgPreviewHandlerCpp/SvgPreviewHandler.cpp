be6e2c89032d89d66dedb2ef0ed29fffec909ba3
Stefan Markovic <stefan@janeasystems.com>
2023-06-28 20:28:54 +0200

    Revert "[File Explorer Add-ons] Fix file preview pane flickering on file selection and resizing (#26660)"
    This reverts commit 9581cd7a273688bca0f42a9d11ecbc8fd079fd32.

--- src/modules/previewpane/SvgPreviewHandlerCpp/SvgPreviewHandler.cpp
+++ src/modules/previewpane/SvgPreviewHandlerCpp/SvgPreviewHandler.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
 #include "SvgPreviewHandler.h"
-#include "../powerpreview/powerpreviewConstants.h"
 
 #include <shellapi.h>
 #include <Shlwapi.h>
@@ -10,7 +9,6 @@
 #include <common/logger/logger.h>
 #include <common/SettingsAPI/settings_helpers.h>
 #include <common/utils/process_path.h>
-#include <common/Themes/windows_colors.h>
 
 extern HINSTANCE g_hInst;
 extern long g_cDllRef;
@@ -200,8 +198,6 @@ IFACEMETHODIMP SvgPreviewHandler::Unload()
 
 IFACEMETHODIMP SvgPreviewHandler::SetBackgroundColor(COLORREF color)
 {
-    HBRUSH brush = CreateSolidBrush(WindowsColors::is_dark_mode() ? powerpreviewConstants::DARK_THEME_COLOR : powerpreviewConstants::LIGHT_THEME_COLOR);
-    SetClassLongPtr(m_hwndParent, GCLP_HBRBACKGROUND, reinterpret_cast<LONG_PTR>(brush));
     return S_OK;
 }
 
