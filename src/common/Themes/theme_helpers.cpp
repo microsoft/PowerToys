60dd243238291896ec18be75296cf708a021af70
Stefan Markovic <stefan@janeasystems.com>
2023-06-29 14:11:17 +0200

    Revert "Revert "[File Explorer Add-ons] Fix file preview pane flickering on f… (#27093)"
    This reverts commit 217f3f9ff3f8587fdf4d9fb209710d25ff4a6d72.

--- src/common/Themes/theme_helpers.cpp
+++ src/common/Themes/theme_helpers.cpp
@@ -3,6 +3,7 @@
 #include "dwmapi.h"
 #include <windows.h>
 #include <vector>
+#pragma comment (lib,"Dwmapi.lib")
 
 #define DWMWA_USE_IMMERSIVE_DARK_MODE 20
 #define HKEY_WINDOWS_THEME L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize"
