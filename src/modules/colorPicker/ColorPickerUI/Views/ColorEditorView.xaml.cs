1443e27f2b41b59a6ea12f4cfe865ec8bc38de58
Niels Laute <niels.laute@live.nl>
2023-07-13 09:26:45 +0200

    Update ColorEditorView.xaml.cs

--- src/modules/colorPicker/ColorPickerUI/Views/ColorEditorView.xaml.cs
+++ src/modules/colorPicker/ColorPickerUI/Views/ColorEditorView.xaml.cs
@@ -15,14 +15,11 @@ namespace ColorPicker.Views
         public ColorEditorView() =>
             InitializeComponent();
 
-       /*
         private void HistoryColors_ItemClick(object sender, ItemClickEventArgs e)
         {
             // Note: it does not handle clicking on the same color.
             // More appropriate event would be SelectionChanged but we can not distinguish between user action and program action inside of it.
             SessionEventHelper.Event.EditorHistoryColorPicked = true;
         }
-
-        */
     }
 }
