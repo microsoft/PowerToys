3604fd6a0806f939f506b724c97c11b37b0394e8
Michael Clayton <mike@kingslandconsulting.co.uk>
2023-07-25 20:51:28 +0100

    [MouseJump] - fixing spell-checker (#26703)

--- src/modules/MouseUtils/MouseJumpUI/Hotkeys/HotKeyManager.cs
+++ src/modules/MouseUtils/MouseJumpUI/Hotkeys/HotKeyManager.cs
@@ -13,7 +13,6 @@ namespace MouseJumpUI.HotKeys;
 /// <remarks>
 /// See https://stackoverflow.com/a/3654821/3156906
 ///     https://learn.microsoft.com/en-us/archive/msdn-magazine/2007/june/net-matters-handling-messages-in-console-apps
-///     https://www.codeproject.com/Articles/5274425/Understanding-Windows-Message-Queues-for-the-Cshar
 /// </remarks>
 public sealed class HotKeyManager
 {
