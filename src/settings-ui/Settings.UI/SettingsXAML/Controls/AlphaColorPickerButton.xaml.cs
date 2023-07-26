a71411d9319f00b7fc2ee71b3f8e12aaec16f339
hayatogh <28914976+hayatogh@users.noreply.github.com>
2023-07-26 23:48:00 +0900

    [MouseHighlighter]Add always-on pointer (#27186)
    * [MouseHighlighter]Add always pointer
    
    Make color pick UI ARGB
    Delete Opacity setting
    
    * Fix opacity and color
    
    Fix "always color" incorrectly initialized
    Revert default opacity to 65%, which was unintentionally lowered to 25% when
    changing to percent
    
    * Fix crash when opening MouseUtils settings page
    
    Migration code was bugged, made malformed json and led to settings page crash.
    
    * Implement migration in module side

---
+++ src/settings-ui/Settings.UI/SettingsXAML/Controls/AlphaColorPickerButton.xaml.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.UI;
+
+namespace Microsoft.PowerToys.Settings.UI.Controls
+{
+    public sealed partial class AlphaColorPickerButton : UserControl
+    {
+        private Color _selectedColor;
+
+        public Color SelectedColor
+        {
+            get
+            {
+                return _selectedColor;
+            }
+
+            set
+            {
+                if (_selectedColor != value)
+                {
+                    _selectedColor = value;
+                    SetValue(SelectedColorProperty, value);
+                }
+            }
+        }
+
+        public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register("SelectedColor", typeof(Color), typeof(AlphaColorPickerButton), new PropertyMetadata(null));
+
+        public AlphaColorPickerButton()
+        {
+            this.InitializeComponent();
+            IsEnabledChanged -= AlphaColorPickerButton_IsEnabledChanged;
+            SetEnabledState();
+            IsEnabledChanged += AlphaColorPickerButton_IsEnabledChanged;
+        }
+
+        private void AlphaColorPickerButton_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            SetEnabledState();
+        }
+
+        private void SetEnabledState()
+        {
+            if (this.IsEnabled)
+            {
+                ColorPreviewBorder.Opacity = 1;
+            }
+            else
+            {
+                ColorPreviewBorder.Opacity = 0.2;
+            }
+        }
+    }
+}
