e3db6b66afb15e5d5945f84e4f324886dc1616a1
Jaime Bernardo <jaime@janeasystems.com>
2023-08-04 22:30:08 +0100

    Try to fix pre-compiled header CI errors

--- src/modules/CropAndLock/CropAndLockModuleInterface/pch.h
+++ src/modules/CropAndLock/CropAndLockModuleInterface/pch.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#ifndef PCH_H
-#define PCH_H
-
 #define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
 #include <Unknwn.h>
@@ -13,5 +10,3 @@
 #include <TraceLoggingActivity.h>
 #include <wil\common.h>
 #include <wil\result.h>
-
-#endif //PCH_H
