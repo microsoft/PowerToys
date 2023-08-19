8b87319196992412c35fbd9b29e0ccbf45b048ac
Deepak Sangle <sangleds20@iitk.ac.in>
2023-08-19 20:08:02 +0530

    [Peek] displaying file size in correct grammatical format

--- src/modules/peek/Peek.Common/Helpers/ReadableStringHelper.cs
+++ src/modules/peek/Peek.Common/Helpers/ReadableStringHelper.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace Peek.Common.Helpers
 {
@@ -17,7 +18,9 @@ namespace Peek.Common.Helpers
             var resourceLoader = ResourceLoaderInstance.ResourceLoader;
             List<string> format = new List<string>
             {
-                resourceLoader.GetString("ReadableString_ByteAbbreviationFormat"),     // "B"
+                (bytes == 1) ?
+                    resourceLoader.GetString("ReadableString_ByteAbbreviationFormat") : // "byte"
+                    resourceLoader.GetString("ReadableString_BytesAbbreviationFormat"), // "bytes"
                 resourceLoader.GetString("ReadableString_KiloByteAbbreviationFormat"), // "KB"
                 resourceLoader.GetString("ReadableString_MegaByteAbbreviationFormat"), // "MB"
                 resourceLoader.GetString("ReadableString_GigaByteAbbreviationFormat"), // "GB"
