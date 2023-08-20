ddff25dfc832dfc9d7912a0a08d462f5834f1c25
htcfreek <61519853+htcfreek@users.noreply.github.com>
2023-08-20 11:14:07 +0200

    fix spell check

--- src/modules/launcher/Wox.Plugin/IPlugin.cs
+++ src/modules/launcher/Wox.Plugin/IPlugin.cs
@@ -19,7 +19,7 @@ namespace Wox.Plugin
         // Localized description
         string Description { get; }
 
-        /* The two property lines are commented because the breake the unit testst. (The moq package used in the unit tests doesn't support 'static abstract' properties (a .Net 7 feature) yet.) - https://github.com/moq/moq/issues/1398
+        /* The two property lines are commented because they brake the unit tests. (The Moq package used in the unit tests doesn't support the .Net 7 feature 'static abstract' properties yet.) - https://github.com/Moq/Moq/issues/1398
         *
         * // Plugin ID for validating the plugin.json entry (It must be static for accessing it before loading the plugin.)
         * public static abstract string PluginID { get; }
