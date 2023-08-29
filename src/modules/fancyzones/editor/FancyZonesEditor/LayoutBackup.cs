f5879ce02f6b4072fd8462e48bce276b3373291d
seraphima <zykovas91@gmail.com>
2023-08-29 13:35:51 +0200

    init canvas zones

--- src/modules/fancyzones/editor/FancyZonesEditor/LayoutBackup.cs
+++ src/modules/fancyzones/editor/FancyZonesEditor/LayoutBackup.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation
+﻿// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -45,6 +45,7 @@ namespace FancyZonesEditor
                 else if (_backup is CanvasLayoutModel canvas)
                 {
                     canvas.RestoreTo((CanvasLayoutModel)layoutToRestore);
+                    canvas.InitTemplateZones();
                 }
             }
 
