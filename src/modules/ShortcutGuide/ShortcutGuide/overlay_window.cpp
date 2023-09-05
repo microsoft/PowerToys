83bb573223a2fb7e17c609ec82c175e434ba629d
Laszlo Nemeth <57342539+donlaci@users.noreply.github.com>
2023-09-05 15:25:24 +0200

    [Accessibility]Turn animations off based on Windows settings (#28128)
    * FindMyMouse: switch animations on/off based on global windows settings
    
    * Fancy Zones: enable/disable animations based on global Windows settings
    
    * Shortcut Guide: enabling/disabling animations based on global Windows settings
    
    * spell checker
    
    * Making MouseUtils settings dependent on global windows settings, AnimationEffects. If the global settings is disabled the MouseUtils animation settings is disabled too, description shows the reason.
    
    * Adding error detection, log on SystemParametersInfo call.
    
    * Adding infobar instead of changing description of the settingsbar.
    
    * spell checker
    
    * moving native constant into NativeMethods class

--- src/modules/ShortcutGuide/ShortcutGuide/overlay_window.cpp
+++ src/modules/ShortcutGuide/ShortcutGuide/overlay_window.cpp
@@ -5,6 +5,7 @@
 #include "start_visible.h"
 #include <common/utils/resources.h>
 #include <common/utils/window.h>
+#include <common/utils/MsWindowsSettings.h>
 
 #include "shortcut_guide.h"
 #include "trace.h"
@@ -269,11 +270,12 @@ D2D1_RECT_F D2DOverlaySVG::get_snap_right() const
 
 D2DOverlayWindow::D2DOverlayWindow() :
     total_screen({}),
-    background_animation(0.3),
-    global_windows_shortcuts_animation(0.3),
-    taskbar_icon_shortcuts_animation(0.3),
     D2DWindow()
 {
+    BOOL isEnabledAnimations = GetAnimationsEnabled();
+    background_animation = isEnabledAnimations? 0.3f : 0.f;
+    global_windows_shortcuts_animation = isEnabledAnimations ? 0.3f : 0.f;
+    taskbar_icon_shortcuts_animation = isEnabledAnimations ? 0.3f : 0.f;
     tasklist_thread = std::thread([&] {
         while (running)
         {
