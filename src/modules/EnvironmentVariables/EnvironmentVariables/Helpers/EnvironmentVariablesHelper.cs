d2dcdd262d0e8abb7507bb37b216ef95353a7fd7
Stefan Markovic <stefan@janeasystems.com>
2023-09-06 10:18:24 +0200

    EditDialog

---
+++ src/modules/EnvironmentVariables/EnvironmentVariables/Helpers/EnvironmentVariablesHelper.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections;
+using System.Security;
+using EnvironmentVariables.Models;
+
+namespace EnvironmentVariables.Helpers
+{
+    internal sealed class EnvironmentVariablesHelper
+    {
+        internal static void GetVariables(EnvironmentVariableTarget target, VariablesSet set)
+        {
+            var variables = Environment.GetEnvironmentVariables(target);
+
+            foreach (DictionaryEntry variable in variables)
+            {
+                string key = variable.Key as string;
+                string value = variable.Value as string;
+
+                if (string.IsNullOrEmpty(key))
+                {
+                    continue;
+                }
+
+                Variable entry = new Variable(key, value, set.Type);
+                set.Variables.Add(entry);
+            }
+        }
+
+        internal static bool SetVariable(Variable variable)
+        {
+            EnvironmentVariableTarget target = variable.ParentType switch
+            {
+                VariablesSetType.Profile => EnvironmentVariableTarget.User,
+                VariablesSetType.User => EnvironmentVariableTarget.User,
+                VariablesSetType.System => EnvironmentVariableTarget.Machine,
+                _ => throw new NotImplementedException(),
+            };
+
+            try
+            {
+                Environment.SetEnvironmentVariable(variable.Name, variable.Values, target);
+            }
+            catch (SecurityException)
+            {
+                // Permission denied (e.g. trying to change System variable while running non-elevated)
+                // Show specific error message
+                return false;
+            }
+            catch (Exception)
+            {
+                // Something else went wrong
+                return false;
+            }
+
+            return true;
+        }
+
+        internal static bool UnsetVariable(Variable variable)
+        {
+            EnvironmentVariableTarget target = variable.ParentType switch
+            {
+                VariablesSetType.Profile => EnvironmentVariableTarget.User,
+                VariablesSetType.User => EnvironmentVariableTarget.User,
+                VariablesSetType.System => EnvironmentVariableTarget.Machine,
+                _ => throw new NotImplementedException(),
+            };
+
+            try
+            {
+                Environment.SetEnvironmentVariable(variable.Name, string.Empty, target);
+            }
+            catch (SecurityException)
+            {
+                // Permission denied (e.g. trying to change System variable while running non-elevated)
+                // Show specific error message
+                return false;
+            }
+            catch (Exception)
+            {
+                // Something else went wrong
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
