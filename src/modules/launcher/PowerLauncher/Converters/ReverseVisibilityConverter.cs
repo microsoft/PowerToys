b87f61e849168fa5edda277ef1f4d451702f918a
Niels Laute <niels.laute@live.nl>
2023-09-12 16:38:34 +0200

    Adding keywords

---
+++ src/modules/launcher/PowerLauncher/Converters/ReverseVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PowerLauncher.Converters
+{
+    public class ReverseVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Visibility resultsVisibility = (Visibility)value;
+
+            if (resultsVisibility == Visibility.Hidden || resultsVisibility == Visibility.Collapsed)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Hidden;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Visibility.Hidden;
+        }
+    }
+}
