8bba02907f9031bd66d84e46469644e91c39812e
Stefan Markovic <stefan@janeasystems.com>
2023-09-22 23:43:27 +0200

    Fix comment

--- src/modules/EnvironmentVariables/EnvironmentVariablesModuleInterface/trace.h
+++ src/modules/EnvironmentVariables/EnvironmentVariablesModuleInterface/trace.h
@@ -6,7 +6,7 @@ public:
     static void RegisterProvider() noexcept;
     static void UnregisterProvider() noexcept;
 
-    // Log if the user has HostsFileEditor enabled or disabled
+    // Log if the user has EnvironmentVariables enabled or disabled
     static void EnableEnvironmentVariables(const bool enabled) noexcept;
 
     // Log that the user tried to activate the editor
