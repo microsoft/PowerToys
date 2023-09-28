b1da798adfb7ba8f76df78de4078fec76d002d3e
garv5014 <garv5014@gmail.com>
2023-09-28 17:12:39 -0600

    here are the tests for the fancy zones

---
+++ UnitTestFancyZonesEditor/UnitTest1.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace UnitTestFancyZonesEditor
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
