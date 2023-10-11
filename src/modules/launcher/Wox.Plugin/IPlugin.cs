49459fb762898f9460e4d44a99a653343566ebf7
Heiko <61519853+htcfreek@users.noreply.github.com>
2023-10-11 15:32:41 +0200

    review feedback 1
    Co-authored-by: Jaime Bernardo <jaime@janeasystems.com>

--- src/modules/launcher/Wox.Plugin/IPlugin.cs
+++ src/modules/launcher/Wox.Plugin/IPlugin.cs
@@ -19,7 +19,7 @@ namespace Wox.Plugin
         // Localized description
         string Description { get; }
 
-        /* The two property lines are commented because they brake the unit tests. (The Moq package used in the unit tests doesn't support the .Net 7 feature 'static abstract' properties yet.) - https://github.com/Moq/Moq/issues/1398
+        /* The two property lines are commented because they break the unit tests. (The Moq package used in the unit tests doesn't support the .Net 7 feature 'static abstract' properties yet.) - https://github.com/Moq/Moq/issues/1398
         *
         * // Plugin ID for validating the plugin.json entry (It must be static for accessing it before loading the plugin.)
         * public static abstract string PluginID { get; }
