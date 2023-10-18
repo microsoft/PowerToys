cd99a2e8488e3376fb012fcd996d2933307a580c
Garrett Vernon <70212190+garv5014@users.noreply.github.com>
2023-10-18 01:07:48 -0600

    [FZ Editor]Add tests to editor #197 + feedback (#29051)
    * here are the tests for the fancy zones
    
    * Wrote tests for GridLayoutModel
    
    * Move FancyZonesEditor tests to right place, tests for default layout model
    
    * fixed SettingTheVerticalLayoutShouldBeTheDefault test
    
    * removed coverlet in the test project
    
    * Fixes for comments on pr
    
    * squashed and updated for comments
    
    * Added the test to the pipeline
    
    ---------
    
    Co-authored-by: Drew Gordon <andrewbengordon@gmail.com>
    Co-authored-by: Caleb Wightman <agentcboy@gmail.com>

---
+++ src/modules/fancyzones/UnitTests-FancyZonesEditor/DefaultLayoutsModelTests.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using FancyZonesEditor.Models;
+
+namespace UnitTestsFancyZonesEditor;
+
+[TestClass]
+public class DefaultLayoutsModelTests
+{
+    [TestMethod]
+    public void LayoutCapacityShouldBeNumberOfMonitorConfigurations()
+    {
+        var defaultLayoutsModel = new DefaultLayoutsModel();
+        var expectedOptionCount = Enum.GetValues(typeof(MonitorConfigurationType)).Length;
+
+        var actualCapacity = defaultLayoutsModel.Layouts.Capacity;
+
+        Assert.AreEqual(expectedOptionCount, actualCapacity);
+    }
+
+    [TestMethod]
+    public void OverridingLayoutClearsOldDefault()
+    {
+        var defaultLayoutsModel = new DefaultLayoutsModel();
+        GridLayoutModel firstLayout = new GridLayoutModel();
+        CanvasLayoutModel secondLayout = new CanvasLayoutModel("steve");
+
+        defaultLayoutsModel.Set(firstLayout, MonitorConfigurationType.Horizontal);
+        Assert.AreEqual(defaultLayoutsModel.Layouts[(int)MonitorConfigurationType.Horizontal], firstLayout);
+
+        defaultLayoutsModel.Set(secondLayout, MonitorConfigurationType.Horizontal);
+        Assert.AreNotEqual(defaultLayoutsModel.Layouts[(int)MonitorConfigurationType.Horizontal], firstLayout);
+        Assert.AreEqual(defaultLayoutsModel.Layouts[(int)MonitorConfigurationType.Horizontal], secondLayout);
+    }
+
+    [TestMethod]
+    public void SettingTheVerticalLayoutShouldBeTheDefault()
+    {
+        var defaultLayoutsModel = new DefaultLayoutsModel();
+        GridLayoutModel firstLayout = new GridLayoutModel();
+        defaultLayoutsModel.Set(firstLayout, MonitorConfigurationType.Horizontal);
+        defaultLayoutsModel.Set(firstLayout, MonitorConfigurationType.Vertical);
+
+        Assert.AreEqual(defaultLayoutsModel.Layouts[(int)MonitorConfigurationType.Vertical], firstLayout);
+    }
+
+    [TestMethod]
+    public void RestoringLayoutShouldSetLayouts()
+    {
+        var defaultLayoutsModel = new DefaultLayoutsModel();
+        GridLayoutModel firstLayout = new GridLayoutModel();
+        CanvasLayoutModel secondLayout = new CanvasLayoutModel("steve");
+        var restoredLayouts = new List<LayoutModel> { firstLayout, secondLayout };
+        defaultLayoutsModel.Restore(restoredLayouts);
+
+        CollectionAssert.AreEqual(defaultLayoutsModel.Layouts, restoredLayouts);
+    }
+}
