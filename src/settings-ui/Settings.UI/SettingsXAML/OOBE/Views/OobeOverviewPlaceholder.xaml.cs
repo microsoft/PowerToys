1f936df3ebce73b726ed11c9fa38fd477b4b0fbd
Laszlo Nemeth <57342539+donlaci@users.noreply.github.com>
2023-10-20 14:23:25 +0200

    [Settings]Adding a Dashboard Panel (#29023)
    * Dashboard: modifying page content + adding SW version button.
    
    * Visual tweaks and minor viewmodel changes
    
    * Updated spacing
    
    * Adding Settings icon
    
    * Settiing the Dashboard page as the default one. Adding functionality to switch to settings pages from the Dashboard page. Localizing texts.
    
    * fixing csproj file
    
    * Reimplementing Active modules handling, showing only the active modules (and not having invisible inactive modules).
    
    * Removing unneccessary binding
    
    * Fix text wrapping
    
    * Adding Registry previewer launch, adding activation mode for FindMyMouse and QuickAccent, modify File Locksmith description.
    
    * Spell checker fix typo
    
    * Adding GPO-blocked state, modifying buttons: adding description, icon.
    
    * Modifying dashboard button layout
    
    * Use SettingsCard instead of button
    
    * Restructuring the dashboard panel
    
    * Removing togglebuttons from the left panel. Showing only active modules. Adding key remappings (to KBM)
    
    * Removing settings buttons, removing descriptions, icons from buttons. Add update of remapped keys, shortcuts.
    
    * Refactoring dashboard
    
    * Making list always visible and fixing scrolling behavior
    
    * Adding background gradient to cards
    
    * Removing keyboard manager's key mappings, minor changes in texts, fixing enabled state when GPO-enabled.
    
    * Use ListView instead of ItemsRepeater
    
    * Updates
    
    * removing right panel with all modules. Extending "left" panel with toggleswitches, showing all modules.
    
    * Separate lists
    
    * Adding Flyout with key remappings for KBM module, adding IsLocked property, icons
    
    * Visual tweaks
    
    * Tweaks
    
    * Fixing lock icon margin
    
    * Minor fixes.
    
    * Removing unused resources
    
    * Make Dashboard default when coming from the OOBE General
    
    * Removed the Previous, Next Layout buttons from FancyZones. Added activation information
    
    ---------
    
    Co-authored-by: Niels Laute <niels.laute@live.nl>

--- src/settings-ui/Settings.UI/SettingsXAML/OOBE/Views/OobeOverviewPlaceholder.xaml.cs
+++ src/settings-ui/Settings.UI/SettingsXAML/OOBE/Views/OobeOverviewPlaceholder.xaml.cs
@@ -54,7 +54,7 @@ namespace Microsoft.PowerToys.Settings.UI.OOBE.Views
         {
             if (OobeShellPage.OpenMainWindowCallback != null)
             {
-                OobeShellPage.OpenMainWindowCallback(typeof(GeneralPage));
+                OobeShellPage.OpenMainWindowCallback(typeof(DashboardPage));
             }
 
             ViewModel.LogOpeningSettingsEvent();
