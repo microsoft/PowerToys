c8d5d3c6d14c481ea9eb4587951b232d763c712b
seraphima <zykovas91@gmail.com>
2023-10-24 14:18:45 +0200

    added test project

---
+++ src/modules/fancyzones/UnitTests-FancyZones-WinAppDriver/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests_FancyZones_WinAppDriver
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+    }
+}
