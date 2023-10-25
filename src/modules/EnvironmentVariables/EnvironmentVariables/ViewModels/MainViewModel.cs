04a7adf6a4ac96874b0b61400883126ac24d39dc
Stefan Markovic <57057282+stefansjfw@users.noreply.github.com>
2023-10-25 20:03:15 +0200

    [EnvVar]Fix how expanded variables are processed (#29418)
    * Read variables from registry (without expanding), not by using Environment API
    Fix seting variables to registry
    
    * Expand in Applied variables list
    
    * Remove using
    
    * Add comments

--- src/modules/EnvironmentVariables/EnvironmentVariables/ViewModels/MainViewModel.cs
+++ src/modules/EnvironmentVariables/EnvironmentVariables/ViewModels/MainViewModel.cs
@@ -132,10 +132,13 @@ namespace EnvironmentVariables.ViewModels
             var variables = new List<Variable>();
             if (AppliedProfile != null)
             {
-                variables = variables.Concat(AppliedProfile.Variables.OrderBy(x => x.Name)).ToList();
+                variables = variables.Concat(AppliedProfile.Variables.Select(x => new Variable(x.Name, Environment.ExpandEnvironmentVariables(x.Values), VariablesSetType.Profile)).OrderBy(x => x.Name)).ToList();
             }
 
-            variables = variables.Concat(UserDefaultSet.Variables.OrderBy(x => x.Name)).Concat(SystemDefaultSet.Variables.OrderBy(x => x.Name)).ToList();
+            // Variables are expanded to be shown in the applied variables section, so the user sees their actual values.
+            variables = variables.Concat(UserDefaultSet.Variables.Select(x => new Variable(x.Name, Environment.ExpandEnvironmentVariables(x.Values), VariablesSetType.User)).OrderBy(x => x.Name))
+                                 .Concat(SystemDefaultSet.Variables.Select(x => new Variable(x.Name, Environment.ExpandEnvironmentVariables(x.Values), VariablesSetType.System)).OrderBy(x => x.Name))
+                                 .ToList();
 
             // Handle PATH variable - add USER value to the end of the SYSTEM value
             var profilePath = variables.Where(x => x.Name.Equals("PATH", StringComparison.OrdinalIgnoreCase) && x.ParentType == VariablesSetType.Profile).FirstOrDefault();
