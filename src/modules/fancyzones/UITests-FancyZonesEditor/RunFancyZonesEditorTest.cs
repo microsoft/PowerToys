d0ebc2d09fea28a6ee91cd2e6628e5a7cf766987
seraphima <zykovas91@gmail.com>
2023-10-26 14:36:10 +0200

    editor test project

---
+++ src/modules/fancyzones/UITests-FancyZonesEditor/RunFancyZonesEditorTest.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.FancyZonesEditor.UnitTests.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UITests_FancyZonesEditor
+{
+    [TestClass]
+    public class RunFancyZonesEditorTest
+    {
+        private static FancyZonesEditorSession? _session;
+        private static TestContext? _context;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _context = testContext;
+            _session = new FancyZonesEditorSession(testContext);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            _session?.Close(_context!);
+        }
+
+        [TestMethod]
+        public void RunFancyZonesEditor()
+        {
+            Assert.IsNotNull(_session?.Session);
+        }
+    }
+}
---
+++ src/modules/fancyzones/UITests-FancyZonesEditor/Utils/FancyZonesEditorSession.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace Microsoft.FancyZonesEditor.UnitTests.Utils
+{
+    public class FancyZonesEditorSession
+    {
+        protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string FancyZonesEditorPath = @"\..\..\..\PowerToys.FancyZonesEditor.exe";
+
+        public WindowsDriver<WindowsElement>? Session { get; }
+
+        public WindowsElement? MainEditorWindow { get; }
+
+        public FancyZonesEditorSession(TestContext testContext)
+        {
+            try
+            {
+                // Launch FancyZonesEditor
+                string? path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                path += FancyZonesEditorPath;
+
+                AppiumOptions opts = new AppiumOptions();
+                opts.AddAdditionalCapability("app", path);
+                Session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), opts);
+            }
+            catch (Exception ex)
+            {
+                testContext.WriteLine(ex.Message);
+            }
+
+            Assert.IsNotNull(Session);
+
+            // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
+            Session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+
+            // Find main editor window
+            MainEditorWindow = Session.FindElementByAccessibilityId("MainWindow1");
+            Assert.IsNotNull(MainEditorWindow);
+        }
+
+        public void Close(TestContext testContext)
+        {
+            // Close the session
+            if (Session != null)
+            {
+                try
+                {
+                    // FZEditor applciation can be closed by explicitly closing main editor window
+                    MainEditorWindow?.SendKeys(Keys.Alt + Keys.F4);
+                }
+                catch (Exception ex)
+                {
+                    testContext.WriteLine(ex.Message);
+                }
+
+                Session.Quit();
+                Session.Dispose();
+            }
+        }
+    }
+}
