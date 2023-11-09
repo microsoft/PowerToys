912d7ec060beffa8d9c21ef91928826e0a912541
gokcekantarci <115616017+gokcekantarci@users.noreply.github.com>
2023-11-09 17:18:00 +0300

    [PTRun][Program]Fix crashes getting images for .lnk files (#29237)
    * [PTRun] Refactor GetHBitmap to handle external DLL failures in a separate task.
    
    * Revert "[PTRun] Refactor GetHBitmap to handle external DLL failures in a separate task."
    
    This reverts commit 159c5744b8ea19c24d330d9ff9504d239eb0acfd.
    
    * [PTRun] GetBitmapSource function has been added for file extensions that cause crashes.
    
    * [PTRun] Spell Check update.
    
    * [PTRun] Renamed function and variable name.
    
    * [PTRun] ShellLinkHelper moved to Wox.Infrastructure for common use.

--- src/modules/launcher/Wox.Infrastructure/IShellLinkHelper.cs
+++ src/modules/launcher/Wox.Infrastructure/IShellLinkHelper.cs
@@ -2,7 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace Microsoft.Plugin.Program.Programs
+namespace Wox.Infrastructure
 {
     public interface IShellLinkHelper
     {
