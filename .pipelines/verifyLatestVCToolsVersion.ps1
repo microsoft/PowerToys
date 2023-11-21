74178f6060d69baf90b875d8adf6d378cebff4ad
Jeremy Sinclair <4016293+snickler@users.noreply.github.com>
2023-11-21 16:02:17 -0500

    [CI] Use latest flag for vswhere to set proper VCToolsVersion

--- .pipelines/verifyLatestVCToolsVersion.ps1
+++ .pipelines/verifyLatestVCToolsVersion.ps1
@@ -1,10 +1,5 @@
-Write-Output "Current VCToolsVersion: $env:VCToolsVersion"
-
-$LatestVCToolsVersion = (([xml](& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -include packages -format xml)).instances.instance.packages.package | ? { $_.id -eq "Microsoft.VisualCpp.Redist.14.Latest" -and $_.chip -eq "x64" }).version;
+$LatestVCToolsVersion = (([xml](& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -include packages -format xml)).instances.instance.packages.package | ? { $_.id -eq "Microsoft.VisualCpp.Redist.14.Latest" -and $_.chip -eq "x64" }).version;
 
 Write-Output "Latest VCToolsVersion: $LatestVCToolsVersion"
-
-if($env:VCToolsVersion -lt $LatestVCToolsVersion) {
-	Write-Output "Updating VCToolsVersion environment variable for job"
-	Write-Output "##vso[task.setvariable variable=VCToolsVersion]$LatestVCToolsVersion"
-}
+Write-Output "Updating VCToolsVersion environment variable for job"
+Write-Output "##vso[task.setvariable variable=VCToolsVersion]$LatestVCToolsVersion"
