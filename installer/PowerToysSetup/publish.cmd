dfb4991bf537e1a556b6f07719f8486194fcf50d
Jeremy Sinclair <4016293+snickler@users.noreply.github.com>
2023-11-21 23:14:12 -0500

    [CI] Set VCToolsVersion and unset ClearDevCommandPromptEnvVars property

--- installer/PowerToysSetup/publish.cmd
+++ installer/PowerToysSetup/publish.cmd
@@ -4,6 +4,8 @@ IF NOT DEFINED PTRoot (SET PTRoot=..\..)
 
 SET PlatformArg=%1
 IF NOT DEFINED PlatformArg (SET PlatformArg=x64)
+SET VCToolsVersion=!VCToolsVersion!
+SET ClearDevCommandPromptEnvVars=false
 
 rem In case of Release we should not use Debug CRT in VCRT forwarders
 msbuild !PTRoot!\src\modules\previewpane\MonacoPreviewHandler\MonacoPreviewHandler.csproj -t:Publish -p:Configuration="Release" -p:Platform="!PlatformArg!" -p:AppxBundle=Never -p:PowerToysRoot=!PTRoot! -p:VCRTForwarders-IncludeDebugCRT=false -p:PublishProfile=InstallationPublishProfile.pubxml
