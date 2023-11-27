cf80cf1a6509a93d4677ba01afab191942357b36
Stefan Markovic <stefan@janeasystems.com>
2023-11-27 14:08:51 +0100

    [SvgThumbnailProvider] Single app instance

--- src/modules/previewpane/SvgThumbnailProvider/Program.cs
+++ src/modules/previewpane/SvgThumbnailProvider/Program.cs
@@ -10,6 +10,8 @@ namespace Microsoft.PowerToys.ThumbnailHandler.Svg
     {
         private static SvgThumbnailProvider _thumbnailProvider;
 
+        private static Mutex mutex = new Mutex(true, "PowerToys_SvgThumbnailProvider_477AA096-FFFA-42CC-A9D0-C0FF6EC09496");
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -21,15 +23,22 @@ namespace Microsoft.PowerToys.ThumbnailHandler.Svg
             {
                 if (args.Length == 2)
                 {
-                    string filePath = args[0];
-                    uint cx = Convert.ToUInt32(args[1], 10);
-
-                    _thumbnailProvider = new SvgThumbnailProvider(filePath);
-                    Bitmap thumbnail = _thumbnailProvider.GetThumbnail(cx);
-                    if (thumbnail != null )
+                    if (mutex.WaitOne(TimeSpan.Zero, true))
                     {
-                        filePath = filePath.Replace(".svg", ".bmp");
-                        thumbnail.Save(filePath, System.Drawing.Imaging.ImageFormat.Bmp);
+                        // do the app code
+                        string filePath = args[0];
+                        uint cx = Convert.ToUInt32(args[1], 10);
+
+                        _thumbnailProvider = new SvgThumbnailProvider(filePath);
+                        Bitmap thumbnail = _thumbnailProvider.GetThumbnail(cx);
+                        if (thumbnail != null)
+                        {
+                            filePath = filePath.Replace(".svg", ".bmp");
+                            thumbnail.Save(filePath, System.Drawing.Imaging.ImageFormat.Bmp);
+                        }
+
+                        mutex.ReleaseMutex();
+                        mutex.Dispose();
                     }
                 }
                 else
