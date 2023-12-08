9e03386eb3069a7018d067ad1d7f762d08f228ae
Davide Giacometti <davide.giacometti@outlook.it>
2023-12-08 14:57:42 +0100

    [Run] XamlStyler formatting (#30011)
    * XamlStyler formatting
    
    * remove unused theme files
    
    * fix formatting

--- .pipelines/applyXamlStyling.ps1
+++ .pipelines/applyXamlStyling.ps1
@@ -107,7 +107,7 @@ if (-not $Passive)
 else 
 {
     Write-Output "Checking all files (passively)"
-    $files = Get-ChildItem -Path "$PSScriptRoot\..\src\*.xaml" -Recurse | Select-Object -ExpandProperty FullName | Where-Object { $_ -notmatch "(\\obj\\)|(\\bin\\)|(\\x64\\)|(\\Generated Files\\PowerRenameXAML\\)|(\\launcher\\PowerLauncher\\)|(\\launcher\\Wox.Plugin\\)|(\\colorPicker\\ColorPickerUI\\)" }
+    $files = Get-ChildItem -Path "$PSScriptRoot\..\src\*.xaml" -Recurse | Select-Object -ExpandProperty FullName | Where-Object { $_ -notmatch "(\\obj\\)|(\\bin\\)|(\\x64\\)|(\\Generated Files\\PowerRenameXAML\\)|(\\colorPicker\\ColorPickerUI\\)" }
 
     if ($files.count -gt 0)
     {
