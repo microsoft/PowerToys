5ed7d81984c2b25f0c95d66364c0675b23cf1eb3
Aaron Junker <aaron.junker@outlook.com>
2023-12-24 00:28:34 +0100

    [WIP]Add first draft menu

---
+++ src/modules/previewpane/FileActionsMenu/FileActionsMenu.Ui/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.Versioning;
+using System.Windows;
+using Wpf.Ui.Markup;
+
+namespace FileActionsMenu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ src/modules/previewpane/FileActionsMenu/FileActionsMenu.Ui/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Windows;
+using System.Windows.Controls;
+using Wpf.Ui.Controls;
+
+namespace FileActionsMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            if (Wpf.Ui.Appearance.Theme.GetSystemTheme() == Wpf.Ui.Appearance.SystemThemeType.Light)
+            {
+                Wpf.Ui.Appearance.Theme.Apply(Wpf.Ui.Appearance.ThemeType.Light);
+            }
+            else
+            {
+                Wpf.Ui.Appearance.Theme.Apply(Wpf.Ui.Appearance.ThemeType.Dark);
+            }
+
+            Wpf.Ui.Appearance.Watcher.Watch(this, Wpf.Ui.Appearance.BackgroundType.None, true);
+            InitializeComponent();
+            ContextMenu cm = (ContextMenu)FindResource("Menu");
+            cm.IsOpen = true;
+
+            cm.Closed += (sender, args) => Close();
+        }
+
+        private void GenerateHash(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void GenerateHashes(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
