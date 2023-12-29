074c83dee93c3cbf259fc46a2f58e83efc96685c
htcfreek <61519853+htcfreek@users.noreply.github.com>
2023-12-29 15:35:38 +0100

    add new generators

---
+++ src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.ValueGenerator/Generators/Uri/DataEscapeRequest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Text;
+using Wox.Plugin.Logger;
+
+namespace Community.PowerToys.Run.Plugin.ValueGenerator.Uri
+{
+    public class DataEscapeRequest : IComputeRequest
+    {
+        public byte[] Result { get; set; }
+
+        public string Description => "Data string escaped";
+
+        public bool IsSuccessful { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        private string DataToEscape { get; set; }
+
+        public DataEscapeRequest(string dataToEscape)
+        {
+            DataToEscape = dataToEscape ?? throw new ArgumentNullException(nameof(dataToEscape));
+        }
+
+        public bool Compute()
+        {
+            IsSuccessful = true;
+            try
+            {
+                Result = Encoding.UTF8.GetBytes(System.Uri.EscapeDataString(DataToEscape));
+            }
+            catch (Exception e)
+            {
+                Log.Exception(e.Message, e, GetType());
+                ErrorMessage = e.Message;
+                IsSuccessful = false;
+            }
+
+            return IsSuccessful;
+        }
+
+        public string ResultToString()
+        {
+            return Encoding.UTF8.GetString(Result);
+        }
+    }
+}
---
+++ src/modules/launcher/Plugins/Community.PowerToys.Run.Plugin.ValueGenerator/Generators/Uri/UrlEncodeRequest.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Security.Policy;
+using System.Text;
+using System.Web;
+using Wox.Plugin.Logger;
+
+namespace Community.PowerToys.Run.Plugin.ValueGenerator.Uri
+{
+    public class UrlEncodeRequest : IComputeRequest
+    {
+        public byte[] Result { get; set; }
+
+        public string Description => "Encoded URL";
+
+        public bool IsSuccessful { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        private string DataToEncode { get; set; }
+
+        public UrlEncodeRequest(string dataToEncode)
+        {
+            DataToEncode = dataToEncode ?? throw new ArgumentNullException(nameof(dataToEncode));
+        }
+
+        public bool Compute()
+        {
+            IsSuccessful = true;
+            try
+            {
+                Result = Encoding.UTF8.GetBytes(HttpUtility.UrlEncode(DataToEncode));
+            }
+            catch (Exception e)
+            {
+                Log.Exception(e.Message, e, GetType());
+                ErrorMessage = e.Message;
+                IsSuccessful = false;
+            }
+
+            return IsSuccessful;
+        }
+
+        public string ResultToString()
+        {
+            return Encoding.UTF8.GetString(Result);
+        }
+    }
+}
