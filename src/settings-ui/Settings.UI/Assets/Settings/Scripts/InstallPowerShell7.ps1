b8c5d1568492a2ec498a05e4ab511036564d00e2
Stefan Markovic <stefan@janeasystems.com>
2024-01-05 00:36:56 +0100

    Use Invoke-WebRequest to get the deps and then install them from local path

--- src/settings-ui/Settings.UI/Assets/Settings/Scripts/InstallPowerShell7.ps1
+++ src/settings-ui/Settings.UI/Assets/Settings/Scripts/InstallPowerShell7.ps1
@@ -6,6 +6,8 @@ if ((Get-AppxPackage microsoft.DesktopAppInstaller).Version -ge [System.Version]
 else
 {
   Write-Host "WinGet not detected. Will try to install."
+  # To speed up Invoke-WebRequest. Older versions are very slow when printing the progress.
+  $ProgressPreference = 'SilentlyContinue'
   $cpuArchitecture="x64"
   $detectedArchitecture=""
   if ($env:PROCESSOR_ARCHITEW6432 -eq $null) {
@@ -26,7 +28,9 @@ else
   else
   {
     Write-Host "Microsoft.VCLibs.140.00 not detected. Will try to install."
-    Add-AppxPackage -Path "https://aka.ms/Microsoft.VCLibs.$cpuArchitecture.14.00.Desktop.appx"
+    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.$cpuArchitecture.14.00.Desktop.appx -OutFile "$Env:TMP\Microsoft.VCLibs.14.00.appx"
+    Add-AppxPackage -Path "$Env:TMP\Microsoft.VCLibs.14.00.appx"
+    Remove-Item -Path "$Env:TMP\Microsoft.VCLibs.14.00.appx" -Force
   }
   if((Get-AppxPackage Microsoft.VCLibs.140.00.UWPDesktop).Version -ge [System.Version]"14.0.30704")
   {
@@ -35,7 +39,9 @@ else
   else
   {
     Write-Host "Microsoft.VCLibs.140.00.UWPDesktop not detected. Will try to install."
-    Add-AppxPackage -Path "https://aka.ms/Microsoft.VCLibs.$cpuArchitecture.14.00.Desktop.appx"
+    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.$cpuArchitecture.14.00.Desktop.appx -OutFile "$Env:TMP\Microsoft.VCLibs.14.00.Desktop.appx"
+    Add-AppxPackage -Path "$Env:TMP\Microsoft.VCLibs.14.00.Desktop.appx"
+    Remove-Item -Path "$Env:TMP\Microsoft.VCLibs.14.00.Desktop.appx" -Force
   }
   if (Get-AppxPackage Microsoft.UI.Xaml.2.7)
   {
@@ -50,9 +56,16 @@ else
     Expand-Archive "$Env:TMP\microsoft.ui.xaml.2.7.3.zip" -DestinationPath "$Env:TEMP\microsoft.ui.xaml.2.7.3"
     Write-Host "Installing $Env:TMP\microsoft.ui.xaml.2.7.3\tools\AppX\$cpuArchitecture\Release\Microsoft.UI.Xaml.2.7.appx"
     Add-AppxPackage "$Env:TMP\microsoft.ui.xaml.2.7.3\tools\AppX\$cpuArchitecture\Release\Microsoft.UI.Xaml.2.7.appx"
+    Remove-Item -Path "$Env:TEMP\microsoft.ui.xaml.2.7.3" -Recurse -Force
+    Remove-Item -Path "$Env:TMP\microsoft.ui.xaml.2.7.3.zip" -Force
   }
   Write-Host "Getting winget to the latest stable"
-  Add-AppxPackage -Path "https://aka.ms/getwinget"
+  Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile "$Env:TMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+  Add-AppxPackage -Path "$Env:TMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+  Remove-Item -Path "$Env:TMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force
+
+  #winget is not visible right away, so reload the PATH variable.
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 }
 
 winget install Microsoft.PowerShell --source winget
