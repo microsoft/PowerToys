bdae257cf3d6eedd0f38cb5940c38ebedb483de3
Jaime Bernardo <jaime@janeasystems.com>
2024-01-04 00:09:11 +0000

    [CmdNotFound]Improve installation workflow

--- src/settings-ui/Settings.UI/ViewModels/CmdNotFoundViewModel.cs
+++ src/settings-ui/Settings.UI/ViewModels/CmdNotFoundViewModel.cs
@@ -16,7 +16,11 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
 {
     public class CmdNotFoundViewModel : Observable
     {
-        public ButtonClickCommand CheckPowershellVersionEventHandler => new ButtonClickCommand(CheckPowershellVersion);
+        public ButtonClickCommand CheckRequirementsEventHandler => new ButtonClickCommand(CheckCommandNotFoundRequirements);
+
+        public ButtonClickCommand InstallPowerShell7EventHandler => new ButtonClickCommand(InstallPowerShell7);
+
+        public ButtonClickCommand InstallWinGetClientModuleEventHandler => new ButtonClickCommand(InstallWinGetClientModule);
 
         public ButtonClickCommand InstallModuleEventHandler => new ButtonClickCommand(InstallModule);
 
@@ -49,6 +53,8 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 // Get the enabled state from GPO.
                 _enabledStateIsGPOConfigured = true;
             }
+
+            CheckCommandNotFoundRequirements();
         }
 
         private string _commandOutputLog;
@@ -66,19 +72,64 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
             }
         }
 
+        private bool _isPowerShell7Detected;
+
+        public bool IsPowerShell7Detected
+        {
+            get => _isPowerShell7Detected;
+            set
+            {
+                if (_isPowerShell7Detected != value)
+                {
+                    _isPowerShell7Detected = value;
+                    OnPropertyChanged(nameof(IsPowerShell7Detected));
+                }
+            }
+        }
+
+        private bool _isWinGetClientModuleDetected;
+
+        public bool IsWinGetClientModuleDetected
+        {
+            get => _isWinGetClientModuleDetected;
+            set
+            {
+                if (_isWinGetClientModuleDetected != value)
+                {
+                    _isWinGetClientModuleDetected = value;
+                    OnPropertyChanged(nameof(IsWinGetClientModuleDetected));
+                }
+            }
+        }
+
+        private bool _isCommandNotFoundModuleInstalled;
+
+        public bool IsCommandNotFoundModuleInstalled
+        {
+            get => _isCommandNotFoundModuleInstalled;
+            set
+            {
+                if (_isCommandNotFoundModuleInstalled != value)
+                {
+                    _isCommandNotFoundModuleInstalled = value;
+                    OnPropertyChanged(nameof(IsCommandNotFoundModuleInstalled));
+                }
+            }
+        }
+
         public bool IsEnabledGpoConfigured
         {
             get => _enabledStateIsGPOConfigured;
         }
 
-        public void RunPowerShellScript(string powershellArguments)
+        public string RunPowerShellScript(string powershellExecutable, string powershellArguments)
         {
             string outputLog = string.Empty;
             try
             {
                 var startInfo = new ProcessStartInfo()
                 {
-                    FileName = "pwsh.exe",
+                    FileName = powershellExecutable,
                     Arguments = powershellArguments,
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
@@ -96,28 +147,98 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
             }
 
             CommandOutputLog = outputLog;
+            return outputLog;
         }
 
-        public void CheckPowershellVersion()
+        public void CheckCommandNotFoundRequirements()
         {
-            var arguments = $"-NoProfile -NonInteractive -Command $PSVersionTable";
-            RunPowerShellScript(arguments);
+            var ps1File = AssemblyDirectory + "\\Assets\\Settings\\Scripts\\CheckCmdNotFoundRequirements.ps1";
+            var arguments = $"-NoProfile -ExecutionPolicy Unrestricted -File \"{ps1File}\"";
+            var result = RunPowerShellScript("pwsh.exe", arguments);
+
+            if (result.Contains("PowerShell 7.4 or greater detected."))
+            {
+                IsPowerShell7Detected = true;
+            }
+            else if (result.Contains("PowerShell 7.4 or greater not detected."))
+            {
+                IsPowerShell7Detected = false;
+            }
+            else if (result.Contains("pwsh.exe"))
+            {
+                // Likely an error saying there was an error starting pwsh.exe, so we can assume Powershell 7 was not detected.
+                CommandOutputLog += "PowerShell 7.4 or greater not detected. Installation instructions can be found on https://learn.microsoft.com/powershell/scripting/install/installing-powershell-on-windows \r\n";
+                IsPowerShell7Detected = false;
+            }
+
+            if (result.Contains("WinGet Client module detected."))
+            {
+                IsWinGetClientModuleDetected = true;
+            }
+            else if (result.Contains("WinGet Client module not detected."))
+            {
+                IsWinGetClientModuleDetected = false;
+            }
+
+            if (result.Contains("Command Not Found module is registered in the profile file."))
+            {
+                IsCommandNotFoundModuleInstalled = true;
+            }
+            else if (result.Contains("Command Not Found module is not registered in the profile file."))
+            {
+                IsCommandNotFoundModuleInstalled = false;
+            }
+        }
+
+        public void InstallPowerShell7()
+        {
+            var arguments = $"-NoProfile -Command \"winget install --id Microsoft.Powershell --source winget\"";
+            var result = RunPowerShellScript("powershell.exe", arguments);
+            if (result.Contains("Successfully installed"))
+            {
+                IsPowerShell7Detected = true;
+            }
+        }
+
+        public void InstallWinGetClientModule()
+        {
+            var ps1File = AssemblyDirectory + "\\Assets\\Settings\\Scripts\\InstallWinGetClientModule.ps1";
+            var arguments = $"-NoProfile -ExecutionPolicy Unrestricted -File \"{ps1File}\"";
+            var result = RunPowerShellScript("pwsh.exe", arguments);
+            if (result.Contains("WinGet Client module detected."))
+            {
+                IsWinGetClientModuleDetected = true;
+            }
+            else if (result.Contains("WinGet Client module not detected."))
+            {
+                IsWinGetClientModuleDetected = false;
+            }
         }
 
         public void InstallModule()
         {
             var ps1File = AssemblyDirectory + "\\Assets\\Settings\\Scripts\\EnableModule.ps1";
             var arguments = $"-NoProfile -ExecutionPolicy Unrestricted -File \"{ps1File}\" -scriptPath \"{AssemblyDirectory}\\..\"";
-            RunPowerShellScript(arguments);
-            PowerToysTelemetry.Log.WriteEvent(new CmdNotFoundInstallEvent());
+            var result = RunPowerShellScript("pwsh.exe", arguments);
+
+            if (result.Contains("Module is already registered in the profile file.") || result.Contains("Module was successfully registered in the profile file."))
+            {
+                IsCommandNotFoundModuleInstalled = true;
+                PowerToysTelemetry.Log.WriteEvent(new CmdNotFoundInstallEvent());
+            }
         }
 
         public void UninstallModule()
         {
             var ps1File = AssemblyDirectory + "\\Assets\\Settings\\Scripts\\DisableModule.ps1";
             var arguments = $"-NoProfile -ExecutionPolicy Unrestricted -File \"{ps1File}\"";
-            RunPowerShellScript(arguments);
-            PowerToysTelemetry.Log.WriteEvent(new CmdNotFoundUninstallEvent());
+            var result = RunPowerShellScript("pwsh.exe", arguments);
+
+            if (result.Contains("Removed the Command Not Found reference from the profile file.") || result.Contains("No instance of Command Not Found was found in the profile file."))
+            {
+                IsCommandNotFoundModuleInstalled = false;
+                PowerToysTelemetry.Log.WriteEvent(new CmdNotFoundUninstallEvent());
+            }
         }
      }
 }
