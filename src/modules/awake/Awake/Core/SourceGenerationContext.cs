5fa5a73b5215f42b4aff6c4b9393ad1d619acd47
Jeremy Sinclair <4016293+snickler@users.noreply.github.com>
2024-02-13 17:13:18 -0500

    .NET 9 P1 changes

---
+++ src/modules/awake/Awake/Core/SourceGenerationContext.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Text.Json.Serialization;
+using Awake.Core.Models;
+
+using Microsoft.PowerToys.Settings.UI.Library;
+
+namespace Awake.Core
+{
+    [JsonSourceGenerationOptions(WriteIndented = true)]
+    [JsonSerializable(typeof(AwakeSettings))]
+    [JsonSerializable(typeof(SystemPowerCapabilities))]
+    internal sealed partial class SourceGenerationContext : JsonSerializerContext
+    {
+    }
+}
