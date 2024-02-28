5b95ffadfca09160226020f7a351ebb4f84927a8
Stefan Markovic <stefan@janeasystems.com>
2024-02-28 11:30:17 +0100

    Revert "Add --disable-features=RendererAppContainer WebView2Environment option (#30412)"
    This reverts commit 415843f6f4941b3ab3dade189385fae89f83a39e.

--- src/modules/previewpane/SvgThumbnailProvider/SvgThumbnailProvider.cs
+++ src/modules/previewpane/SvgThumbnailProvider/SvgThumbnailProvider.cs
@@ -143,7 +143,7 @@ namespace Microsoft.PowerToys.ThumbnailHandler.Svg
                 thumbnailDone.Set();
             };
 
-            var webView2Options = new CoreWebView2EnvironmentOptions("--block-new-web-contents --disable-features=RendererAppContainer");
+            var webView2Options = new CoreWebView2EnvironmentOptions("--block-new-web-contents");
             ConfiguredTaskAwaitable<CoreWebView2Environment>.ConfiguredTaskAwaiter
                webView2EnvironmentAwaiter = CoreWebView2Environment
                    .CreateAsync(userDataFolder: _webView2UserDataFolder, options: webView2Options)
