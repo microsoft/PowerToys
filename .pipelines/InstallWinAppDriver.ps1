f1980b8fcbb121472b4d68e868b4a6d478e0876b
seraphima <zykovas91@gmail.com>
2024-03-11 18:16:55 +0100

    installation argument

--- .pipelines/InstallWinAppDriver.ps1
+++ .pipelines/InstallWinAppDriver.ps1
@@ -2,7 +2,7 @@ $ProgressPreference = 'SilentlyContinue'
 
 $WinAppDriverDownloadUrl = "https://github.com/microsoft/WinAppDriver/releases/download/v1.2.1/WindowsApplicationDriver_1.2.1.msi"
 
-# Download WiX binaries and verify their hash sums
+# Download WinAppDriver and verify their hash sums
 Invoke-WebRequest -Uri $WinAppDriverDownloadUrl -OutFile "$($ENV:Temp)\WindowsApplicationDriver_1.2.1.msi"
 $Hash = (Get-FileHash -Algorithm SHA256 "$($ENV:Temp)\WindowsApplicationDriver_1.2.1.msi").Hash
 if ($Hash -ne 'a76a8f4e44b29bad331acf6b6c248fcc65324f502f28826ad2acd5f3c80857fe')
@@ -12,4 +12,4 @@ if ($Hash -ne 'a76a8f4e44b29bad331acf6b6c248fcc65324f502f28826ad2acd5f3c80857fe'
 }
 
 # Install WinAppDriver
-Start-Process msiexec.exe -Wait -ArgumentList "/I $($ENV:Temp)\WindowsApplicationDriver_1.2.1.msi /quiet"
+Start-Process msiexec.exe -Wait -ArgumentList "/I $($ENV:Temp)\WindowsApplicationDriver_1.2.1.msi /quiet /passive"
