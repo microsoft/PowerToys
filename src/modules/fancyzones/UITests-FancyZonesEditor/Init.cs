48d957db5c253baf9c69fbf056bfb7f22c46cee7
seraphima <zykovas91@gmail.com>
2024-03-11 15:27:30 +0100

    start WinAppDriver from the default installation path

--- src/modules/fancyzones/UITests-FancyZonesEditor/Init.cs
+++ src/modules/fancyzones/UITests-FancyZonesEditor/Init.cs
@@ -17,17 +17,9 @@ namespace Microsoft.FancyZonesEditor.UITests
         [AssemblyInitialize]
         public static void SetupAll(TestContext context)
         {
-            string? sourceDirPath = Environment.GetEnvironmentVariable("SrcPath"); // get source dir in CI
-            if (sourceDirPath == null)
-            {
-                sourceDirPath = Path.GetFullPath($"{Environment.CurrentDirectory}" + @".\..\..\..\..\..\"); // local
-            }
-
-            context.WriteLine($"Source dir: {sourceDirPath}");
-            string winAppDriver = Path.Combine(sourceDirPath, @".\deps\WinAppDriver", "WinAppDriver.exe");
-
-            context.WriteLine($"Attempting to launch WinAppDriver at: {winAppDriver}");
-            appDriver = Process.Start(winAppDriver);
+            string winAppDriverPath = "C:\\Program Files (x86)\\Windows Application Driver\\WinAppDriver.exe";
+            context.WriteLine($"Attempting to launch WinAppDriver at: {winAppDriverPath}");
+            appDriver = Process.Start(winAppDriverPath);
         }
 
         [AssemblyCleanup]
