fa8d31a36809351bff24d42a59a9e12a5fc8d3c8
damantioworks <75074566+damantioworks@users.noreply.github.com>
2024-03-12 16:42:32 +0100

    [QuickAccent] Added Schwa (ə) in Italian

--- src/modules/poweraccent/PowerAccent.Core/Languages.cs
+++ src/modules/poweraccent/PowerAccent.Core/Languages.cs
@@ -619,7 +619,7 @@ namespace PowerAccent.Core
             return letter switch
             {
                 LetterKey.VK_A => new[] { "à" },
-                LetterKey.VK_E => new[] { "è", "é", "€" },
+                LetterKey.VK_E => new[] { "è", "é", "ə", "€" },
                 LetterKey.VK_I => new[] { "ì", "í" },
                 LetterKey.VK_O => new[] { "ò", "ó" },
                 LetterKey.VK_U => new[] { "ù", "ú" },
