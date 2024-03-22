f6e7635a4e143def0a6bb2ada4d562c0de318059
Seraphima Zykova <zykovas91@gmail.com>
2024-03-22 13:10:10 +0100

    [FancyZones]UI testing that works in CI (#29453)
    * added test project
    
    * run fz test
    
    * rename proj
    
    * editor test project
    
    * check if FZ is running
    
    * rename
    
    * added assert messages
    
    * spelling
    
    * dev docs
    
    * spelling
    
    * update to latest stable
    
    * exclude ui tests deps
    
    * update packages list in notice.md
    
    * added sample tests
    
    * added file for tests run
    
    * removed unrecognized
    
    * removed run
    
    * fix test configuration
    
    * rename job
    
    * change dependance
    
    * run test template
    
    * removed condition
    
    * tabulation fix
    
    * removed arg
    
    * removed dependance
    
    * removed log
    
    * removed parameters
    
    * test
    
    * test
    
    * added parameters
    
    * pool
    
    * pool
    
    * vs test
    
    * dependance
    
    * download artifact
    
    * publish artifact
    
    * artifact publish conditions
    
    * artifact name, default download path
    
    * set folders
    
    * prepare dotnet and vstest platform
    
    * copy all
    
    * target dotnet8
    
    * test build agents
    
    * set vs test version
    
    * spellcheck
    
    * set test platform version
    
    * package feed selector
    
    * hardcoded vstest location
    
    * are other tests running?
    
    * location
    
    * vstest.console
    
    * upd command
    
    * script path
    
    * search vstest.console
    
    * vs path
    
    * tools dir
    
    * check files
    
    * try full path
    
    * try vstest task
    
    * try full path in vstest task
    
    * change path, remove unnecessary
    
    * test with full vsconsole path
    
    * winappdriver task
    
    * changed args and condition
    
    * default address
    
    * added start operation type
    
    * task name
    
    * remove resolution
    
    * Update run-ui-tests-ci.yml
    
    * Update run-ui-tests-ci.yml
    
    * Update run-ui-tests-ci.yml
    
    * Update run-ui-tests-ci.yml
    
    * AgentResolution should be a string
    
    * Update run-ui-tests-ci.yml
    
    testing against what WinUI gallery has for agent
    
    * Update run-ui-tests-ci.yml
    
    * Update run-ui-tests-ci.yml
    
    * added WinAppDriver.exe
    
    * spellcheck
    
    * remove task
    
    * checkout
    
    * path
    
    * src dir variable
    
    * added init to the second project
    
    * set longer timeout
    
    * try waiting
    
    * rerun
    
    * log session info
    
    * exclude WinAppDriver files from spell-check
    
    * split io class: editor params
    
    * remove unnecessary
    
    * move data to the common project
    
    * io test helper
    
    * write retry
    
    * Moved constants
    
    * file utils
    
    * prepare editor files before launch
    
    * remove unused file
    
    * spellcheck
    
    * create directory
    
    * fixed cleaning up
    
    * remove WinAppDriver from deps
    
    * start WinAppDriver from the default installation path
    
    * installation script
    
    * Revert "spellcheck"
    
    This reverts commit 4bdc3957305daa20a5901aae83de0622ec41a93d.
    
    * Revert "exclude WinAppDriver files from spell-check"
    
    This reverts commit 21ee6db3f5afb4cae9ea51d9468c07caa8dbc0f4.
    
    * install
    
    * installation argument
    
    * spellcheck
    
    * change winappdriver path in fz tests
    
    * delete iohelper
    
    * update docs
    
    * deleted obsolete winappdriver tests
    
    * net version
    
    * try without vstest location
    
    * spellcheck
    
    * Revert "try without vstest location"
    
    This reverts commit 7cd39f3ae6735f53f7deea8d91312a8046309459.
    
    * moved json tag constants to the common project

---
+++ src/modules/fancyzones/FancyZonesEditorCommon/Data/LayoutTemplates.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using static FancyZonesEditorCommon.Data.LayoutTemplates;
+
+namespace FancyZonesEditorCommon.Data
+{
+    public class LayoutTemplates : EditorData<TemplateLayoutsListWrapper>
+    {
+        public string File
+        {
+            get
+            {
+                return GetDataFolder() + "\\Microsoft\\PowerToys\\FancyZones\\layout-templates.json";
+            }
+        }
+
+        public struct TemplateLayoutWrapper
+        {
+            public string Type { get; set; }
+
+            public bool ShowSpacing { get; set; }
+
+            public int Spacing { get; set; }
+
+            public int ZoneCount { get; set; }
+
+            public int SensitivityRadius { get; set; }
+        }
+
+        public struct TemplateLayoutsListWrapper
+        {
+            public List<TemplateLayoutWrapper> LayoutTemplates { get; set; }
+        }
+    }
+}
---
+++ src/modules/fancyzones/UITests-FancyZonesEditor/TestConstants.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using static FancyZonesEditorCommon.Data.Constants;
+
+namespace Microsoft.FancyZonesEditor.UITests
+{
+    public static class TestConstants
+    {
+        public static readonly Dictionary<TemplateLayout, string> TemplateLayoutNames = new Dictionary<TemplateLayout, string>()
+        {
+            { TemplateLayout.Empty, "No layout" },
+            { TemplateLayout.Focus, "Focus" },
+            { TemplateLayout.Rows, "Rows" },
+            { TemplateLayout.Columns, "Columns" },
+            { TemplateLayout.Grid, "Grid" },
+            { TemplateLayout.PriorityGrid, "PriorityGrid" },
+        };
+    }
+}
