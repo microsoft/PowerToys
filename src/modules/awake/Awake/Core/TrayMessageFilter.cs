6a8975ceadb161a8f310441d61863ff84c149ed0
Den Delimarsky <sign@den.dev>
2024-04-10 14:17:25 -0700

    More general cleanup

--- src/modules/awake/Awake/Core/TrayMessageFilter.cs
+++ src/modules/awake/Awake/Core/TrayMessageFilter.cs
@@ -22,29 +22,29 @@ namespace Awake.Core
 
         public bool PreFilterMessage(ref Message m)
         {
-            var trayCommandsSize = Enum.GetNames(typeof(TrayCommands)).Length;
+            int trayCommandsSize = Enum.GetNames(typeof(TrayCommands)).Length;
 
-            switch (m.Msg)
+            if (m.Msg == (int)Native.Constants.WM_COMMAND)
             {
-                case (int)Native.Constants.WM_COMMAND:
-                    var targetCommandIndex = m.WParam.ToInt64() & 0xFFFF;
-                    switch (targetCommandIndex)
-                    {
-                        case (long)TrayCommands.TC_EXIT:
-                            Manager.CompleteExit(0, _exitSignal, true);
-                            break;
-                        case (long)TrayCommands.TC_DISPLAY_SETTING:
-                            Manager.SetDisplay();
-                            break;
-                        case (long)TrayCommands.TC_MODE_INDEFINITE:
-                            Manager.SetIndefiniteKeepAwake();
-                            break;
-                        case (long)TrayCommands.TC_MODE_PASSIVE:
-                            Manager.SetPassiveKeepAwake();
-                            break;
-                        case var _ when targetCommandIndex >= trayCommandsSize:
-                            // Format for the timer block:
-                            // TrayCommands.TC_TIME + ZERO_BASED_INDEX_IN_SETTINGS
+                long targetCommandIndex = m.WParam.ToInt64() & 0xFFFF;
+
+                switch (targetCommandIndex)
+                {
+                    case (long)TrayCommands.TC_EXIT:
+                        Manager.CompleteExit(0, _exitSignal, true);
+                        break;
+                    case (long)TrayCommands.TC_DISPLAY_SETTING:
+                        Manager.SetDisplay();
+                        break;
+                    case (long)TrayCommands.TC_MODE_INDEFINITE:
+                        Manager.SetIndefiniteKeepAwake();
+                        break;
+                    case (long)TrayCommands.TC_MODE_PASSIVE:
+                        Manager.SetPassiveKeepAwake();
+                        break;
+                    default:
+                        if (targetCommandIndex >= trayCommandsSize)
+                        {
                             AwakeSettings settings = Manager.ModuleSettings!.GetSettings<AwakeSettings>(Constants.AppName);
                             if (settings.Properties.CustomTrayTimes.Count == 0)
                             {
@@ -52,12 +52,12 @@ namespace Awake.Core
                             }
 
                             int index = (int)targetCommandIndex - (int)TrayCommands.TC_TIME;
-                            var targetTime = (uint)settings.Properties.CustomTrayTimes.ElementAt(index).Value;
+                            uint targetTime = (uint)settings.Properties.CustomTrayTimes.ElementAt(index).Value;
                             Manager.SetTimedKeepAwake(targetTime);
-                            break;
-                    }
+                        }
 
-                    break;
+                        break;
+                }
             }
 
             return false;
