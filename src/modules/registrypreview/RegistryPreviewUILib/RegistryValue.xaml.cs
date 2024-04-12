4e028510b70b11a823770822ce00f69b3123cdbd
Stefan Markovic <stefan@janeasystems.com>
2024-04-12 10:41:26 +0200

    Fix RegistryPreview package creation

--- src/modules/registrypreview/RegistryPreviewUILib/RegistryValue.xaml.cs
+++ src/modules/registrypreview/RegistryPreviewUILib/RegistryValue.xaml.cs
@@ -12,10 +12,10 @@ namespace RegistryPreviewUILib
     public class RegistryValue
     {
         // Static members
-        private static Uri uriStringValue = new Uri("ms-appx:///Assets/RegistryPreview/string32.png");
-        private static Uri uriBinaryValue = new Uri("ms-appx:///Assets/RegistryPreview/data32.png");
-        private static Uri uriDeleteValue = new Uri("ms-appx:///Assets/RegistryPreview/deleted-value32.png");
-        private static Uri uriErrorValue = new Uri("ms-appx:///Assets/RegistryPreview/error32.png");
+        private static Uri uriStringValue = new Uri("ms-appx:///Assets/RegistryPreviewUILib/string32.png");
+        private static Uri uriBinaryValue = new Uri("ms-appx:///Assets/RegistryPreviewUILib/data32.png");
+        private static Uri uriDeleteValue = new Uri("ms-appx:///Assets/RegistryPreviewUILib/deleted-value32.png");
+        private static Uri uriErrorValue = new Uri("ms-appx:///Assets/RegistryPreviewUILib/error32.png");
 
         public string Name { get; set; }
 
