8fd97bead29213d3e1bfc9f0b0e5204762006d59
Stefan Markovic <stefan@janeasystems.com>
2024-04-16 21:01:58 +0200

    Rename to EnvironmentVariablesUILib and convert to lib

---
+++ src/modules/EnvironmentVariables/EnvironmentVariablesUILib/EnvironmentVariablesUILibXAML/Converters/EnvironmentStateToTitleConverter.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using EnvironmentVariablesUILib.Helpers;
+using EnvironmentVariablesUILib.Models;
+using Microsoft.UI.Xaml.Data;
+
+namespace EnvironmentVariablesUILib.Converters;
+
+public class EnvironmentStateToTitleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        var resourceLoader = ResourceLoaderInstance.ResourceLoader;
+        var type = (EnvironmentState)value;
+        return type switch
+        {
+            EnvironmentState.ProfileNotApplicable => resourceLoader.GetString("ProfileNotApplicableTitle"),
+            _ => resourceLoader.GetString("StateNotUpToDateTitle"),
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
