9854c33523dfc26d5e87a4708ed7747f95391cd7
Michael Clayton <mike@kingslandconsulting.co.uk>
2024-05-07 23:46:57 +0100

    [Mouse Jump] - added new drawing / layout / style classes (#25482)

---
+++ src/modules/MouseUtils/MouseJumpUI/Common/Models/Drawing/RectangleInfo.cs
@@ -0,0 +1,218 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Drawing;
+using System.Text.Json.Serialization;
+using MouseJumpUI.Common.Models.Styles;
+using BorderStyle = MouseJumpUI.Common.Models.Styles.BorderStyle;
+
+namespace MouseJumpUI.Common.Models.Drawing;
+
+/// <summary>
+/// Immutable version of a System.Drawing.Rectangle object with some extra utility methods.
+/// </summary>
+public sealed class RectangleInfo
+{
+    public static readonly RectangleInfo Empty = new(0, 0, 0, 0);
+
+    public RectangleInfo(decimal x, decimal y, decimal width, decimal height)
+    {
+        this.X = x;
+        this.Y = y;
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public RectangleInfo(Rectangle rectangle)
+        : this(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height)
+    {
+    }
+
+    public RectangleInfo(Point location, SizeInfo size)
+        : this(location.X, location.Y, size.Width, size.Height)
+    {
+    }
+
+    public RectangleInfo(SizeInfo size)
+        : this(0, 0, size.Width, size.Height)
+    {
+    }
+
+    public decimal X
+    {
+        get;
+    }
+
+    public decimal Y
+    {
+        get;
+    }
+
+    public decimal Width
+    {
+        get;
+    }
+
+    public decimal Height
+    {
+        get;
+    }
+
+    [JsonIgnore]
+    public decimal Left => this.X;
+
+    [JsonIgnore]
+    public decimal Top => this.Y;
+
+    [JsonIgnore]
+    public decimal Right => this.X + this.Width;
+
+    [JsonIgnore]
+    public decimal Bottom => this.Y + this.Height;
+
+    [JsonIgnore]
+    public decimal Area => this.Width * this.Height;
+
+    [JsonIgnore]
+    public PointInfo Location => new(this.X, this.Y);
+
+    [JsonIgnore]
+    public PointInfo Midpoint => new(
+        x: this.X + (this.Width / 2),
+        y: this.Y + (this.Height / 2));
+
+    [JsonIgnore]
+    public SizeInfo Size => new(this.Width, this.Height);
+
+    public RectangleInfo Center(PointInfo point) => new(
+        x: point.X - (this.Width / 2),
+        y: point.Y - (this.Height / 2),
+        width: this.Width,
+        height: this.Height);
+
+    /// <summary>
+    /// Moves this RectangleInfo inside the specified RectangleInfo.
+    /// </summary>
+    public RectangleInfo Clamp(RectangleInfo outer)
+    {
+        if ((this.Width > outer.Width) || (this.Height > outer.Height))
+        {
+            throw new ArgumentException($"Value cannot be larger than {nameof(outer)}.");
+        }
+
+        return new(
+            x: Math.Clamp(this.X, outer.X, outer.Right - this.Width),
+            y: Math.Clamp(this.Y, outer.Y, outer.Bottom - this.Height),
+            width: this.Width,
+            height: this.Height);
+    }
+
+    /// <remarks>
+    /// Adapted from https://github.com/dotnet/runtime
+    /// See https://github.com/dotnet/runtime/blob/dfd618dc648ba9b11dd0f8034f78113d69f223cd/src/libraries/System.Drawing.Primitives/src/System/Drawing/Rectangle.cs
+    /// </remarks>
+    public bool Contains(decimal x, decimal y) =>
+        this.X <= x && x < this.X + this.Width && this.Y <= y && y < this.Y + this.Height;
+
+    /// <remarks>
+    /// Adapted from https://github.com/dotnet/runtime
+    /// See https://github.com/dotnet/runtime/blob/dfd618dc648ba9b11dd0f8034f78113d69f223cd/src/libraries/System.Drawing.Primitives/src/System/Drawing/Rectangle.cs
+    /// </remarks>
+    public bool Contains(PointInfo pt) =>
+        this.Contains(pt.X, pt.Y);
+
+    /// <remarks>
+    /// Adapted from https://github.com/dotnet/runtime
+    /// See https://github.com/dotnet/runtime/blob/dfd618dc648ba9b11dd0f8034f78113d69f223cd/src/libraries/System.Drawing.Primitives/src/System/Drawing/Rectangle.cs
+    /// </remarks>
+    public bool Contains(RectangleInfo rect) =>
+        (this.X <= rect.X) && (rect.X + rect.Width <= this.X + this.Width) &&
+        (this.Y <= rect.Y) && (rect.Y + rect.Height <= this.Y + this.Height);
+
+    public RectangleInfo Enlarge(BorderStyle border) =>
+        new(
+            this.X - border.Left,
+            this.Y - border.Top,
+            this.Width + border.Horizontal,
+            this.Height + border.Vertical);
+
+    public RectangleInfo Enlarge(MarginStyle margin) =>
+        new(
+            this.X - margin.Left,
+            this.Y - margin.Top,
+            this.Width + margin.Horizontal,
+            this.Height + margin.Vertical);
+
+    public RectangleInfo Enlarge(PaddingStyle padding) =>
+        new(
+            this.X - padding.Left,
+            this.Y - padding.Top,
+            this.Width + padding.Horizontal,
+            this.Height + padding.Vertical);
+
+    public RectangleInfo Offset(SizeInfo amount) => this.Offset(amount.Width, amount.Height);
+
+    public RectangleInfo Offset(decimal dx, decimal dy) => new(this.X + dx, this.Y + dy, this.Width, this.Height);
+
+    public RectangleInfo Scale(decimal scalingFactor) => new(
+        this.X * scalingFactor,
+        this.Y * scalingFactor,
+        this.Width * scalingFactor,
+        this.Height * scalingFactor);
+
+    public RectangleInfo Shrink(BorderStyle border) =>
+        new(
+            this.X + border.Left,
+            this.Y + border.Top,
+            this.Width - border.Horizontal,
+            this.Height - border.Vertical);
+
+    public RectangleInfo Shrink(MarginStyle margin) =>
+        new(
+            this.X + margin.Left,
+            this.Y + margin.Top,
+            this.Width - margin.Horizontal,
+            this.Height - margin.Vertical);
+
+    public RectangleInfo Shrink(PaddingStyle padding) =>
+        new(
+            this.X + padding.Left,
+            this.Y + padding.Top,
+            this.Width - padding.Horizontal,
+            this.Height - padding.Vertical);
+
+    public RectangleInfo Truncate() =>
+        new(
+            (int)this.X,
+            (int)this.Y,
+            (int)this.Width,
+            (int)this.Height);
+
+    /// <remarks>
+    /// Adapted from https://github.com/dotnet/runtime
+    /// See https://github.com/dotnet/runtime/blob/dfd618dc648ba9b11dd0f8034f78113d69f223cd/src/libraries/System.Drawing.Primitives/src/System/Drawing/Rectangle.cs
+    /// </remarks>
+    public RectangleInfo Union(RectangleInfo rect)
+    {
+        var x1 = Math.Min(this.X, rect.X);
+        var x2 = Math.Max(this.X + this.Width, rect.X + rect.Width);
+        var y1 = Math.Min(this.Y, rect.Y);
+        var y2 = Math.Max(this.Y + this.Height, rect.Y + rect.Height);
+
+        return new RectangleInfo(x1, y1, x2 - x1, y2 - y1);
+    }
+
+    public Rectangle ToRectangle() => new((int)this.X, (int)this.Y, (int)this.Width, (int)this.Height);
+
+    public override string ToString()
+    {
+        return "{" +
+            $"{nameof(this.Left)}={this.Left}," +
+            $"{nameof(this.Top)}={this.Top}," +
+            $"{nameof(this.Width)}={this.Width}," +
+            $"{nameof(this.Height)}={this.Height}" +
+            "}";
+    }
+}
---
+++ src/modules/MouseUtils/MouseJumpUI/Common/Models/Drawing/SizeInfo.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Drawing;
+using MouseJumpUI.Common.Models.Styles;
+using BorderStyle = MouseJumpUI.Common.Models.Styles.BorderStyle;
+
+namespace MouseJumpUI.Common.Models.Drawing;
+
+/// <summary>
+/// Immutable version of a System.Drawing.Size object with some extra utility methods.
+/// </summary>
+public sealed class SizeInfo
+{
+    public SizeInfo(decimal width, decimal height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public SizeInfo(Size size)
+        : this(size.Width, size.Height)
+    {
+    }
+
+    public decimal Width
+    {
+        get;
+    }
+
+    public decimal Height
+    {
+        get;
+    }
+
+    public SizeInfo Enlarge(BorderStyle border) =>
+        new(
+            this.Width + border.Horizontal,
+            this.Height + border.Vertical);
+
+    public SizeInfo Enlarge(PaddingStyle padding) =>
+        new(
+            this.Width + padding.Horizontal,
+            this.Height + padding.Vertical);
+
+    public SizeInfo Intersect(SizeInfo size) =>
+        new(
+            Math.Min(this.Width, size.Width),
+            Math.Min(this.Height, size.Height));
+
+    public SizeInfo Negate() =>
+        new(-this.Width, -this.Height);
+
+    public SizeInfo Shrink(BorderStyle border) =>
+        new(this.Width - border.Horizontal, this.Height - border.Vertical);
+
+    public SizeInfo Shrink(MarginStyle margin) =>
+        new(this.Width - margin.Horizontal, this.Height - margin.Vertical);
+
+    public SizeInfo Shrink(PaddingStyle padding) =>
+        new(this.Width - padding.Horizontal, this.Height - padding.Vertical);
+
+    public RectangleInfo PlaceAt(decimal x, decimal y) => new(x, y, this.Width, this.Height);
+
+    public SizeInfo ScaleToFit(SizeInfo bounds)
+    {
+        var widthRatio = bounds.Width / this.Width;
+        var heightRatio = bounds.Height / this.Height;
+        return widthRatio.CompareTo(heightRatio) switch
+        {
+            < 0 => new(bounds.Width, this.Height * widthRatio),
+            0 => bounds,
+            > 0 => new(this.Width * heightRatio, bounds.Height),
+        };
+    }
+
+    public SizeInfo Floor()
+    {
+        return new SizeInfo(
+            Math.Floor(this.Width),
+            Math.Floor(this.Height));
+    }
+
+    /// <summary>
+    /// Get the scaling ratio to scale obj by so that it fits inside the specified bounds
+    /// without distorting the aspect ratio.
+    /// </summary>
+    public decimal ScaleToFitRatio(SizeInfo bounds)
+    {
+        if (bounds.Width == 0 || bounds.Height == 0)
+        {
+            throw new ArgumentException($"{nameof(bounds.Width)} or {nameof(bounds.Height)} cannot be zero", nameof(bounds));
+        }
+
+        var widthRatio = bounds.Width / this.Width;
+        var heightRatio = bounds.Height / this.Height;
+        var scalingRatio = Math.Min(widthRatio, heightRatio);
+
+        return scalingRatio;
+    }
+
+    public Size ToSize() => new((int)this.Width, (int)this.Height);
+
+    public Point ToPoint() => new((int)this.Width, (int)this.Height);
+
+    public override string ToString()
+    {
+        return "{" +
+            $"{nameof(this.Width)}={this.Width}," +
+            $"{nameof(this.Height)}={this.Height}" +
+            "}";
+    }
+}
