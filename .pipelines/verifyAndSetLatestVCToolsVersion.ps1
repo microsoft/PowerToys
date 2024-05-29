4e274776e7554304f5b05a3ee73fdaf7f8abc448
Jaime Bernardo <jaime@janeasystems.com>
2024-05-29 22:35:33 +0100

    Print all versions

--- .pipelines/verifyAndSetLatestVCToolsVersion.ps1
+++ .pipelines/verifyAndSetLatestVCToolsVersion.ps1
@@ -1,3 +1,6 @@
+$LatestVersions = (([xml](& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -include packages -format xml)).instances.instance.packages.package);
+Write-Output $LatestVersions
+
 $LatestVCToolsVersion = (([xml](& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -include packages -format xml)).instances.instance.packages.package | ? { $_.id -eq "Microsoft.VisualCpp.CRT.Source" }).version;
 
 Write-Output "Latest VCToolsVersion: $LatestVCToolsVersion"
