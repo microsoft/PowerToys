c33a84c3280223b98dd357a18502705eea8a12a8
Christian Gaarden Gaardmark <cgaarden@hotmail.com>
2024-06-07 23:28:02 -0700

    Initial Installer Changes
    Not fully tested

--- installer/PowerToysSetupCustomActions/CustomAction.cpp
+++ installer/PowerToysSetupCustomActions/CustomAction.cpp
@@ -1002,7 +1002,7 @@ UINT __stdcall UnRegisterContextMenuPackagesCA(MSIHANDLE hInstall)
     try
     {
         // Packages to unregister
-        const std::vector<std::wstring> packagesToRemoveDisplayName{ { L"PowerRenameContextMenu" }, { L"ImageResizerContextMenu" }, { L"FileLocksmithContextMenu" } };
+        const std::vector<std::wstring> packagesToRemoveDisplayName{ { L"PowerRenameContextMenu" }, { L"ImageResizerContextMenu" }, { L"FileLocksmithContextMenu" }, { L"NewPlusContextMenu" } };
 
         PackageManager packageManager;
 
--- src/modules/New/NewShellExtensionContextMenu/constants.h
+++ src/modules/New/NewShellExtensionContextMenu/constants.h
@@ -16,7 +16,7 @@ namespace newplus::constants::non_localizable
 
     constexpr WCHAR settings_json_key_template_location[] = L"TemplateLocation";
 
-    constexpr WCHAR context_menu_package_name[] = L"NewPlus.ShellExtension";
+    constexpr WCHAR context_menu_package_name[] = L"NewPlusContextMenu";
 
     constexpr WCHAR msix_package_name[] = L"NewPlusPackage.msix";
     
@@ -26,9 +26,9 @@ namespace newplus::constants::non_localizable
 
     constexpr WCHAR new_icon_dark_resource_relative_path[] = L"\\Assets\\New\\New_dark.ico";
 
-    constexpr WCHAR open_templates_icon_light_resource_relative_path[] = L"\\Assets\\New\\Open templates_light.ico";
+    constexpr WCHAR open_templates_icon_light_resource_relative_path[] = L"\\Assets\\New\\Open_templates_light.ico";
 
-    constexpr WCHAR open_templates_icon_dark_resource_relative_path[] = L"\\Assets\\New\\Open templates_dark.ico";
+    constexpr WCHAR open_templates_icon_dark_resource_relative_path[] = L"\\Assets\\New\\Open_templates_dark.ico";
 
     constexpr WCHAR desktop_ini_filename[] = L"desktop.ini";
 }
