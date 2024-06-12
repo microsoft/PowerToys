79919c979125950d6eb9ea27d2c36fb194a3a3ec
Ace Kirkpatrick <acekirkpatrick@gmail.com>
2024-06-12 14:37:09 -0500

    Removed incorrect summary

--- src/settings-ui/Settings.UI/SettingsXAML/Views/PowerPreviewPage.xaml.cs
+++ src/settings-ui/Settings.UI/SettingsXAML/Views/PowerPreviewPage.xaml.cs
@@ -9,9 +9,6 @@ using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.PowerToys.Settings.UI.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class PowerPreviewPage : Page, IRefreshablePage
     {
         public PowerPreviewViewModel ViewModel { get; set; }
