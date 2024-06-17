0e74b2ee6b5ba1998f1c00517ce2ccbf7db3bc31
seraphima <zykovas91@gmail.com>
2024-06-17 19:57:09 +0200

    version

---
+++ src/modules/Projects/ProjectsLauncher/resource.h
@@ -0,0 +1,13 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ProjectLauncherResource.rc
+
+//////////////////////////////
+// Non-localizable
+
+#define FILE_DESCRIPTION "PowerToys Projects Launcher"
+#define INTERNAL_NAME "PowerToys.ProjectsLauncher"
+#define ORIGINAL_FILENAME "PowerToys.ProjectsLauncher.exe"
+
+// Non-localizable
+//////////////////////////////
---
+++ src/modules/Projects/ProjectsSnapshotTool/resource.h
@@ -0,0 +1,13 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ProjectsSnapshotTool.rc
+
+//////////////////////////////
+// Non-localizable
+
+#define FILE_DESCRIPTION "PowerToys Projects Snapthot Tool"
+#define INTERNAL_NAME "PowerToys.ProjectsSnapshotTool"
+#define ORIGINAL_FILENAME "PowerToys.ProjectsSnapshotTool.exe"
+
+// Non-localizable
+//////////////////////////////
