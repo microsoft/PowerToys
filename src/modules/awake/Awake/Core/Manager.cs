4f1f08aa6e6e8cba8b5c1cc2dbe7c0ccbfbd7bb4
Den Delimarsky <sign@den.dev>
2024-07-16 18:27:01 -0700

    Remove unused code

--- src/modules/awake/Awake/Core/Manager.cs
+++ src/modules/awake/Awake/Core/Manager.cs
@@ -5,8 +5,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
