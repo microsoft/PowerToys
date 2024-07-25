6686c5b5f5ada7d2bc14deedc36fc65fe97236ec
seraphima <zykovas91@gmail.com>
2024-07-25 15:17:02 +0200

    launcher error messages

--- src/modules/Projects/ProjectsLauncher/main.cpp
+++ src/modules/Projects/ProjectsLauncher/main.cpp
@@ -4,11 +4,14 @@
 
 #include <AppLauncher.h>
 
+#include <Generated Files/resource.h>
+
 #include <common/utils/elevation.h>
 #include <common/utils/gpo.h>
 #include <common/utils/logger_helper.h>
 #include <common/utils/process_path.h>
 #include <common/utils/UnhandledExceptionHandler.h>
+#include <common/utils/resources.h>
 
 const std::wstring moduleName = L"Projects\\ProjectsLauncher";
 const std::wstring internalPath = L"";
@@ -65,27 +68,33 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, LPSTR cmdline, int cm
             }
             else
             {
-                // TODO: show error message
                 Logger::critical("Incorrect Projects file");
+                std::wstring formattedMessage = fmt::format(GET_RESOURCE_STRING(IDS_INCORRECT_FILE_ERROR), L"projects.json");
+                MessageBox(NULL, formattedMessage.c_str(), GET_RESOURCE_STRING(IDS_PROJECTS).c_str(), MB_ICONERROR | MB_OK);
                 return 1;
             }
         }
         else
         {
-            // TODO: show error message
             Logger::critical("Incorrect Projects file");
+            std::wstring formattedMessage = fmt::format(GET_RESOURCE_STRING(IDS_INCORRECT_FILE_ERROR), L"projects.json");
+            MessageBox(NULL, formattedMessage.c_str(), GET_RESOURCE_STRING(IDS_PROJECTS).c_str(), MB_ICONERROR | MB_OK);
             return 1;
         }
     }
     catch (std::exception ex)
     {
-        Logger::error("Exception on reading projects: {}", ex.what());
+        Logger::critical("Exception on reading projects: {}", ex.what());
+        std::wstring formattedMessage = fmt::format(GET_RESOURCE_STRING(IDS_FILE_READING_ERROR), L"projects.json");
+        MessageBox(NULL, formattedMessage.c_str(), GET_RESOURCE_STRING(IDS_PROJECTS).c_str(), MB_ICONERROR | MB_OK);
         return 1;
     }
 
     if (projects.empty())
     {
         Logger::warn("Projects file is empty");
+        std::wstring formattedMessage = fmt::format(GET_RESOURCE_STRING(IDS_EMPTY_FILE), L"projects.json");
+        MessageBox(NULL, formattedMessage.c_str(), GET_RESOURCE_STRING(IDS_PROJECTS).c_str(), MB_ICONERROR | MB_OK);
         return 1;
     }
 
@@ -106,7 +115,9 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, LPSTR cmdline, int cm
 
     if (projectToLaunch.id.empty())
     {
-        Logger::info(L"Project {} not found", id);
+        Logger::critical(L"Project {} not found", id);
+        std::wstring formattedMessage = fmt::format(GET_RESOURCE_STRING(IDS_PROJECT_NOT_FOUND), id);
+        MessageBox(NULL, formattedMessage.c_str(), GET_RESOURCE_STRING(IDS_PROJECTS).c_str(), MB_ICONERROR | MB_OK);
         return 1;
     }
 
