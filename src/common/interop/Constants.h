4d7a58e8158fdff8eb2816adffaf02eed7eeba15
Jaime Bernardo <jaime@janeasystems.com>
2024-08-05 16:42:05 +0100

    Port other constant functions

--- src/common/interop/Constants.h
+++ src/common/interop/Constants.h
@@ -8,6 +8,36 @@ namespace winrt::interop::implementation
 
         static hstring AppDataPath();
         static hstring PowerLauncherSharedEvent();
+        static hstring PowerLauncherCentralizedHookSharedEvent();
+        static hstring RunSendSettingsTelemetryEvent();
+        static hstring RunExitEvent();
+        static hstring FZEExitEvent();
+        static hstring FZEToggleEvent();
+        static hstring ColorPickerSendSettingsTelemetryEvent();
+        static hstring ShowColorPickerSharedEvent();
+        static hstring ShowAdvancedPasteSharedEvent();
+        static hstring AdvancedPasteMarkdownEvent();
+        static hstring AdvancedPasteJsonEvent();
+        static hstring ShowPowerOCRSharedEvent();
+        static hstring MouseJumpShowPreviewEvent();
+        static hstring AwakeExitEvent();
+        static hstring ShowPeekEvent();
+        static hstring PowerAccentExitEvent();
+        static hstring ShortcutGuideTriggerEvent();
+        static hstring RegistryPreviewTriggerEvent();
+        static hstring MeasureToolTriggerEvent();
+        static hstring GcodePreviewResizeEvent();
+        static hstring QoiPreviewResizeEvent();
+        static hstring DevFilesPreviewResizeEvent();
+        static hstring MarkdownPreviewResizeEvent();
+        static hstring PdfPreviewResizeEvent();
+        static hstring SvgPreviewResizeEvent();
+        static hstring ShowHostsSharedEvent();
+        static hstring ShowHostsAdminSharedEvent();
+        static hstring CropAndLockThumbnailEvent();
+        static hstring CropAndLockReparentEvent();
+        static hstring ShowEnvironmentVariablesSharedEvent();
+        static hstring ShowEnvironmentVariablesAdminSharedEvent();
     };
 }
 
