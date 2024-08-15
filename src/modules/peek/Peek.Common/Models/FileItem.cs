b2c06b805c31275bef3daa482568e2fb06e1577c
Ani <115020168+drawbyperpetual@users.noreply.github.com>
2024-08-15 16:26:37 +0200

    [Peek] Fix leak in unmanaged memory

--- src/modules/peek/Peek.Common/Models/FileItem.cs
+++ src/modules/peek/Peek.Common/Models/FileItem.cs
@@ -10,47 +10,31 @@ using Windows.Storage;
 
 #nullable enable
 
-namespace Peek.Common.Models
+namespace Peek.Common.Models;
+
+public class FileItem(string path, string name) : IFileSystemItem
 {
-    public class FileItem : IFileSystemItem
+    public string Name { get; init; } = name;
+
+    public string ParsingName => string.Empty;
+
+    public string Path { get; init; } = path;
+
+    public string Extension => System.IO.Path.GetExtension(Path).ToLower(CultureInfo.InvariantCulture);
+
+    public async Task<IStorageItem?> GetStorageItemAsync() => await GetStorageFileAsync();
+
+    public async Task<StorageFile?> GetStorageFileAsync()
     {
-        private StorageFile? storageFile;
-
-        public FileItem(string path, string name)
+        try
         {
-            Path = path;
-            Name = name;
+            // Don't cache these objects; they are cheap to create but seem to have a large (unmanaged) memory footprint.
+            return await StorageFile.GetFileFromPathAsync(Path);
         }
-
-        public string Name { get; init; }
-
-        public string ParsingName => string.Empty;
-
-        public string Path { get; init; }
-
-        public string Extension => System.IO.Path.GetExtension(Path).ToLower(CultureInfo.InvariantCulture);
-
-        public async Task<IStorageItem?> GetStorageItemAsync()
+        catch (Exception ex)
         {
-            return await GetStorageFileAsync();
-        }
-
-        public async Task<StorageFile?> GetStorageFileAsync()
-        {
-            if (storageFile == null)
-            {
-                try
-                {
-                    storageFile = await StorageFile.GetFileFromPathAsync(Path);
-                }
-                catch (Exception ex)
-                {
-                    Logger.LogError("Error getting file from path. " + ex.Message);
-                    storageFile = null;
-                }
-            }
-
-            return storageFile;
+            Logger.LogError("Error getting file from path. " + ex.Message);
+            return null;
         }
     }
 }
