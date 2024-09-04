14c8f7913014f949fb84a9f5ef100972bdf1fb2c
Clint Rutkas <clint@rutkas.com>
2024-09-04 15:29:14 -0700

    simplifying namespaces

--- src/modules/cmdpal/src/WindowsCommandPalette/Builtins/Bookmark/AddBookmarkForm.cs
+++ src/modules/cmdpal/src/WindowsCommandPalette/Builtins/Bookmark/AddBookmarkForm.cs
@@ -2,7 +2,6 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Text.Json;
 using System.Text.Json.Nodes;
 using Microsoft.Windows.CommandPalette.Extensions.Helpers;
 using Windows.Foundation;
--- src/modules/cmdpal/src/WindowsCommandPalette/Builtins/Bookmark/BookmarkData.cs
+++ src/modules/cmdpal/src/WindowsCommandPalette/Builtins/Bookmark/BookmarkData.cs
@@ -2,22 +2,13 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
-using Microsoft.Windows.CommandPalette.Extensions;
-using Microsoft.Windows.CommandPalette.Extensions.Helpers;
-using Windows.Foundation;
-using Windows.System;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
 namespace Run.Bookmarks;
 
 public class BookmarkData
 {
-    public string Name = string.Empty;
-    public string Bookmark = string.Empty;
-    public string Type = string.Empty;
+    public string Name { get; set; } = string.Empty;
+
+    public string Bookmark { get; set; } = string.Empty;
+
+    public string Type { get; set; } = string.Empty;
 }
