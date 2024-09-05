28cd7967d0f6394a15116666affe4fe94c9c0987
Clint Rutkas <clint@rutkas.com>
2024-09-05 16:02:38 -0700

    defaults / spacing

--- src/modules/cmdpal/extensionsdk/Microsoft.CmdPal.Extensions.Helpers/Form.cs
+++ src/modules/cmdpal/extensionsdk/Microsoft.CmdPal.Extensions.Helpers/Form.cs
@@ -6,12 +6,17 @@ namespace Microsoft.CmdPal.Extensions.Helpers;
 
 public class Form: IForm
 {
-    public string Data { get; set; }
-    public string State { get; set; }
-    public string Template { get; set; }
+    public string Data { get; set; } = string.Empty;
+
+    public string State { get; set; } = string.Empty;
+
+    public string Template { get; set; } = string.Empty;
 
     public virtual string DataJson() => Data;
+
     public virtual string StateJson() => State;
+
     public virtual string TemplateJson() => Template;
+
     public virtual ICommandResult SubmitForm(string payload) => throw new NotImplementedException();
 }
