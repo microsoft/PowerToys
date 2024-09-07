4f14db877274238f0719eff96ff9d3011041337d
Jordi Adoumie <jordiadoumie@microsoft.com>
2024-09-07 00:36:47 -0700

    adding additional samples

---
+++ src/modules/cmdpal/extensions/SamplePagesExtension/SamplePagesCommandsProvider.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.CmdPal.Extensions;
+using Microsoft.CmdPal.Extensions.Helpers;
+using SSHKeychainExtension;
+
+namespace SamplePagesExtension;
+
+public class SamplePagesCommandsProvider : ICommandProvider
+{
+    public string DisplayName => $"Sample Pages Commands";
+
+    public IconDataType Icon => new(string.Empty);
+
+    private readonly IListItem[] _commands = [
+       new ListItem(new SampleMarkdownPage())
+       {
+           Title = "Markdown Page Sample Command",
+           Subtitle = "SamplePages Extension",
+       },
+       new ListItem(new SampleListPage())
+       {
+           Title = "List Page Sample Command",
+           Subtitle = "SamplePages Extension",
+       },
+    ];
+
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+    public void Dispose() => throw new NotImplementedException();
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+
+    public IListItem[] TopLevelCommands()
+    {
+        return _commands;
+    }
+}
