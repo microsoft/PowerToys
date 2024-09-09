d56ebb01342c8c5400ef41eda5fa2aaa73fcd9d7
Jaime Bernardo <jaime@janeasystems.com>
2024-09-09 16:10:33 +0100

    Add Settings page with Enable button

---
+++ src/settings-ui/Settings.UI/SettingsXAML/Views/ZoomItPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.PowerToys.Settings.UI.Helpers;
+using Microsoft.PowerToys.Settings.UI.Library;
+using Microsoft.PowerToys.Settings.UI.ViewModels;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.PowerToys.Settings.UI.Views
+{
+    public sealed partial class ZoomItPage : Page, IRefreshablePage
+    {
+        private ZoomItViewModel ViewModel { get; set; }
+
+        public ZoomItPage()
+        {
+            var settingsUtils = new SettingsUtils();
+            ViewModel = new ZoomItViewModel(settingsUtils, SettingsRepository<GeneralSettings>.GetInstance(settingsUtils), ShellPage.SendDefaultIPCMessage);
+            DataContext = ViewModel;
+            InitializeComponent();
+        }
+
+        public void RefreshEnabledState()
+        {
+            ViewModel.RefreshEnabledState();
+        }
+    }
+}
