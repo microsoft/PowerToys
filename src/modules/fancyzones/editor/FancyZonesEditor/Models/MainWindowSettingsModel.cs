a77ef77dcb8c6a30cc7e28f141fa713570ead7ea
Jeremy Sinclair <4016293+snickler@users.noreply.github.com>
2024-09-12 17:13:07 -0400

    [Analyzers][FancyZones] Apply fix for SA1516

--- src/modules/fancyzones/editor/FancyZonesEditor/Models/MainWindowSettingsModel.cs
+++ src/modules/fancyzones/editor/FancyZonesEditor/Models/MainWindowSettingsModel.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+
 using FancyZonesEditor.Models;
 
 namespace FancyZonesEditor
