2f64260fefccd6fcf50e168880cc2cd5e5b86a6e
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-09-17 19:22:14 -0400

    Put back events

--- src/modules/cmdpal/Microsoft.CmdPal.UI/Controls/SearchBar.xaml.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Controls/SearchBar.xaml.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 
 namespace Microsoft.CmdPal.UI.Controls;
 
@@ -13,5 +14,20 @@ public sealed partial class SearchBar : UserControl
     public SearchBar()
     {
         this.InitializeComponent();
+    }
+
+    private void BackButton_Tapped(object sender, TappedRoutedEventArgs e)
+    {
+        // TODO
+    }
+
+    private void FilterBox_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        // TODO
+    }
+
+    private void FilterBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        // TODO
     }
 }
