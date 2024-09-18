95c880ca2187843176c115f31f373bd7e8fc4a88
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-09-18 17:40:52 -0400

    Add ViewModels for ActionBar

---
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ViewModels/ActionBarViewModel.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Microsoft.CmdPal.UI.ViewModels;
+
+public partial class ActionBarViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _actionName = string.Empty;
+
+    [ObservableProperty]
+    private bool _moreCommandsAvailable = false;
+
+    [ObservableProperty]
+    private ObservableCollection<ActionBarContextItemViewModel> _contextActions = [];
+
+    public ActionBarViewModel()
+    {
+        // Just for fun
+        ActionName = "My Action";
+        MoreCommandsAvailable = true;
+        ContextActions.Add(new ActionBarContextItemViewModel("Action1", true));
+        ContextActions.Add(new ActionBarContextItemViewModel("Action2", true));
+    }
+}
--- src/modules/cmdpal/Microsoft.CmdPal.UI/Controls/ActionBar.xaml.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Controls/ActionBar.xaml.cs
@@ -2,23 +2,15 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.ObjectModel;
-using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.CmdPal.UI.ViewModels;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 
 namespace Microsoft.CmdPal.UI.Controls;
 
-[ObservableObject]
 public sealed partial class ActionBar : UserControl
 {
-    [ObservableProperty]
-    private string _actionName = string.Empty;
-
-    [ObservableProperty]
-    private bool _moreCommandsAvailable = false;
-
-    private ObservableCollection<string> _contextActions = [];
+    public ActionBarViewModel ViewModel { get; set; } = new();
 
     public ActionBar()
     {
@@ -33,12 +25,5 @@ public sealed partial class ActionBar : UserControl
     private void ActionListViewItem_Tapped(object sender, TappedRoutedEventArgs e)
     {
         // TODO
-    }
-
-    private void ActionBar_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-    {
-        // TODO Just in here for testing
-        ActionName = "My Action";
-        MoreCommandsAvailable = true;
     }
 }
