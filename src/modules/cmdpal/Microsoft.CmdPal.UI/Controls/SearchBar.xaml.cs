d6636b9450b7ddbd844dcbac916edf87f49e23ce
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-09-18 11:24:38 -0400

    Add SearchBar

---
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Controls/SearchBar.xaml.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.CmdPal.UI.Controls;
+
+public sealed partial class SearchBar : UserControl
+{
+    public bool Nested { get; set; }
+
+    public SearchBar()
+    {
+        this.InitializeComponent();
+    }
+}
