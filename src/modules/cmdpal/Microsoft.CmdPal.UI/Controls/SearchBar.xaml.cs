53b584049cf6acc81977ced668a0a24a25ae5629
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-09-19 15:35:35 -0700

    Basic keyboard handling in SearchBox demonstrating decoupled control messaging

--- src/modules/cmdpal/Microsoft.CmdPal.UI/Controls/SearchBar.xaml.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Controls/SearchBar.xaml.cs
@@ -2,8 +2,13 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.CmdPal.UI.ViewModels.Messages;
+using Microsoft.UI.Input;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
+using CoreVirtualKeyStates = Windows.UI.Core.CoreVirtualKeyStates;
+using VirtualKey = Windows.System.VirtualKey;
 
 namespace Microsoft.CmdPal.UI.Controls;
 
@@ -18,12 +23,46 @@ public sealed partial class SearchBar : UserControl
 
     private void BackButton_Tapped(object sender, TappedRoutedEventArgs e)
     {
-        // TODO
+        WeakReferenceMessenger.Default.Send<NavigateBackMessage>();
     }
 
     private void FilterBox_KeyDown(object sender, KeyRoutedEventArgs e)
     {
-        // TODO
+        if (e.Handled)
+        {
+            return;
+        }
+
+        var ctrlPressed = InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.Control).HasFlag(CoreVirtualKeyStates.Down);
+        if (e.Key == VirtualKey.Down)
+        {
+            WeakReferenceMessenger.Default.Send<NavigateNextCommand>();
+
+            e.Handled = true;
+        }
+        else if (e.Key == VirtualKey.Up)
+        {
+            WeakReferenceMessenger.Default.Send<NavigatePreviousCommand>();
+
+            e.Handled = true;
+        }
+        else if (e.Key == VirtualKey.Enter)
+        {
+            // TODO: ExecuteCommandMessage?
+            e.Handled = true;
+        } // ctrl+k
+        else if (ctrlPressed && e.Key == VirtualKey.K)
+        {
+            // TODO: ShowActionsMessage?
+            // Move code below to ActionBar
+            /*FlyoutShowOptions options = new FlyoutShowOptions
+            {
+                ShowMode = FlyoutShowMode.Standard,
+            };
+            MoreCommandsButton.Flyout.ShowAt(MoreCommandsButton, options);
+            ActionsDropdown.SelectedIndex = 0;
+            ActionsDropdown.Focus(FocusState.Programmatic);*/
+        }
     }
 
     private void FilterBox_TextChanged(object sender, TextChangedEventArgs e)
--- src/modules/cmdpal/Microsoft.CmdPal.UI/ExtViews/ListPage.xaml.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/ExtViews/ListPage.xaml.cs
@@ -2,7 +2,9 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.CmdPal.UI.ViewModels;
+using Microsoft.CmdPal.UI.ViewModels.Messages;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
@@ -12,13 +14,17 @@ namespace Microsoft.CmdPal.UI;
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class ListPage : Page
+public sealed partial class ListPage : Page,
+    IRecipient<NavigateNextCommand>,
+    IRecipient<NavigatePreviousCommand>
 {
     public ListViewModel? ViewModel { get; set;  }
 
     public ListPage()
     {
         this.InitializeComponent();
+
+        WeakReferenceMessenger.Default.RegisterAll(this);
     }
 
     protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -36,6 +42,28 @@ public sealed partial class ListPage : Page
         if (e.ClickedItem is ListItemViewModel item)
         {
             ViewModel?.InvokeItemCommand.Execute(item);
+        }
+    }
+
+    public void Receive(NavigateNextCommand message)
+    {
+        // Note: We may want to just have the notion of a 'SelectedCommand' in our VM
+        // And then have these commands manipulate that state being bound to the UI instead
+        // We may want to see how other non-list UIs need to behave to make this decision
+        // At least it's decoupled from the SearchBox now :)
+        if (ItemsList.SelectedIndex < ItemsList.Items.Count - 1)
+        {
+            ItemsList.SelectedIndex++;
+            ItemsList.ScrollIntoView(ItemsList.SelectedItem);
+        }
+    }
+
+    public void Receive(NavigatePreviousCommand message)
+    {
+        if (ItemsList.SelectedIndex > 0)
+        {
+            ItemsList.SelectedIndex--;
+            ItemsList.ScrollIntoView(ItemsList.SelectedItem);
         }
     }
 }
