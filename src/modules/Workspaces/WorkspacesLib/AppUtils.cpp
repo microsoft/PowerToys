110b852efe7f5c24d812e517e57314127180f7e2
donlaci <laszlo@janeasystems.com>
2024-09-30 13:34:33 +0200

    [Workspaces] fix snapshot tool: update capturing windows settings app

--- src/modules/Workspaces/WorkspacesLib/AppUtils.cpp
+++ src/modules/Workspaces/WorkspacesLib/AppUtils.cpp
@@ -26,6 +26,8 @@ namespace Utils
 
             constexpr const wchar_t* FileExplorerName = L"File Explorer";
             constexpr const wchar_t* FileExplorerPath = L"C:\\WINDOWS\\EXPLORER.EXE";
+            constexpr const wchar_t* SystemSettingsName = L"System Settings";
+            constexpr const wchar_t* SystemSettingsPath = L"SYSTEMSETTINGS.EXE";
             constexpr const wchar_t* PowerToys = L"PowerToys.exe";
             constexpr const wchar_t* PowerToysSettingsUpper = L"POWERTOYS.SETTINGS.EXE";
             constexpr const wchar_t* PowerToysSettings = L"PowerToys.Settings.exe";
@@ -257,6 +259,18 @@ namespace Utils
 
                     if (appPathUpper.contains(installPathUpper))
                     {
+                        // check if the found app is the System Settings. If yes, update the install path to the exe path
+                        if (appPathUpper.ends_with(NonLocalizable::SystemSettingsPath))
+                        {
+                            return AppData{
+                                .name = NonLocalizable::SystemSettingsName,
+                                .installPath = appPath,
+                                .packageFullName = appData.packageFullName,
+                                .appUserModelId = appData.appUserModelId,
+                                .canLaunchElevated = appData.canLaunchElevated
+                            };
+                        }
+
                         return appData;
                     }
 
