f60b294dcb8df1a27246aee8e5c7ac0cb1a86eaa
Gokce Kantarci <gokce@janeasystems.com>
2024-10-14 14:49:06 +0300

    [Runner] Clicking update notification opens PT Settings.

--- src/runner/UpdateUtils.cpp
+++ src/runner/UpdateUtils.cpp
@@ -54,7 +54,8 @@ void ShowNewVersionAvailable(const new_version_download_info& info)
                                                L"powertoys://update_now/" },
                                   link_button{ GET_RESOURCE_STRING(IDS_GITHUB_NEW_VERSION_MORE_INFO),
                                                L"powertoys://open_settings/" } },
-                                std::move(toast_params));
+                                std::move(toast_params),
+                                L"powertoys://open_settings/");
 }
 
 void ShowOpenSettingsForUpdate()
@@ -71,7 +72,8 @@ void ShowOpenSettingsForUpdate()
                                 GET_RESOURCE_STRING(IDS_TOAST_TITLE),
                                 {},
                                 std::move(actions),
-                                std::move(toast_params));
+                                std::move(toast_params),
+                                L"powertoys://open_settings/");
 }
 
 SHELLEXECUTEINFOW LaunchPowerToysUpdate(const wchar_t* cmdline)
