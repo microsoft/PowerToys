c3cc1bffef04a2f368033f193fb6c2e3ed0b76a9
mikeclayton <mike@kingslandconsulting.co.uk>
2024-10-22 22:17:21 +0100

    [MouseJump] delinting to trigger a build (#27511)

--- src/modules/MouseUtils/MouseJump.Common/Helpers/ConfigHelper.cs
+++ src/modules/MouseUtils/MouseJump.Common/Helpers/ConfigHelper.cs
@@ -2,10 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Drawing;
 using System.Globalization;
-using System.Linq;
 
 namespace MouseJump.Common.Helpers;
 
