6b49ad15cf71f87e25c66944726888c03182a370
PesBandi <127593627+PesBandi@users.noreply.github.com>
2024-10-30 09:19:22 +0100

    Ignore text on the same line as a timestamp

--- src/Monaco/customLanguages/srt.js
+++ src/Monaco/customLanguages/srt.js
@@ -4,36 +4,45 @@ export function srtDefinition() {
             root: [
                 [/\s*\d+/, 'number', '@block']
             ],
-
+            
             block: [
-                [/^\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}/, 'tag', '@subtitle'],
+                [/^\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}/, {
+                    cases: {
+                        '@eos': {token: 'tag', next: '@subtitle'},
+                        '@default': {token: 'tag', next: '@ignore'}
+                    }
+                }],
                 [/^$/, 'string', '@pop']
             ],
-
+            
+            ignore: [
+                [/.+$/, '', '@subtitle']
+            ],
+            
             tags: [
                 [/^$/, 'string', '@popall'],
                 [/<b>/, 'string.bold', '@bold'],
                 [/<i>/, 'string.emphasis', '@italic'],
                 [/<u>/, 'string.underline', '@underline']
             ],
-
+            
             subtitle: [
                 {include: '@tags'},
                 [/./, 'string']
             ],
-
+            
             bold: [
                 [/<\/b>/, 'string.bold', '@pop'],
                 {include: '@tags'},
                 [/./, 'string.bold'],
             ],
-
+            
             italic: [
                 [/<\/i>/, 'string.emphasis', '@pop'],
                 {include: '@tags'},
                 [/./, 'string.emphasis'],
             ],
-
+            
             underline: [
                 [/<\/u>/, 'string.underline', '@pop'],
                 {include: '@tags'},
