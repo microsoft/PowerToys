6558260c5385e1ac9cf6ca0a29bac68f30c28ef1
Aaron Junker <Aaron.Junker@outlook.com>
2024-11-02 15:10:33 +0100

    Push

---
+++ src/modules/ShortcutGuideV2/ShortcutGuide.Ui/Models/ShortcutPageParameters.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace ShortcutGuide.Models
+{
+    internal struct ShortcutPageParameters
+    {
+        public static SeachFilterObservable SearchFilter = new();
+    }
+
+    internal sealed class SeachFilterObservable
+    {
+        public event EventHandler<string>? FilterChanged;
+
+        public void OnFilterChanged(string filter)
+        {
+            FilterChanged?.Invoke(this, filter);
+        }
+    }
+}
--- src/modules/ShortcutGuideV2/ShortcutGuide.Ui/ShortcutGuideXAML/ShortcutView.xaml.cs
+++ src/modules/ShortcutGuideV2/ShortcutGuide.Ui/ShortcutGuideXAML/ShortcutView.xaml.cs
@@ -2,9 +2,13 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Globalization;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
 using ShortcutGuide.Models;
+using Windows.ApplicationModel.ConversationalAgent;
+using Windows.Storage;
 
 namespace ShortcutGuide
 {
@@ -22,7 +26,18 @@ namespace ShortcutGuide
             int i = 0;
             foreach (var category in shortcutList.Shortcuts)
             {
-                CategorySelector.Items.Add(new SelectorBarItem() { Text = category.SectionName, Name = i.ToString(CultureInfo.InvariantCulture) });
+                switch (category.SectionName)
+                {
+                    case string name when name.StartsWith("<TASKBAR1-9>", System.StringComparison.Ordinal):
+                        // Todo: Implement GetTaskbarIconPositions
+                        break;
+                    case string name when name.StartsWith('<'):
+                        break;
+                    default:
+                        CategorySelector.Items.Add(new SelectorBarItem() { Text = category.SectionName, Name = i.ToString(CultureInfo.InvariantCulture) });
+                        break;
+                }
+
                 i++;
             }
 
@@ -32,6 +47,30 @@ namespace ShortcutGuide
             foreach (var shortcut in shortcutList.Shortcuts[0].Properties)
             {
                 ShortcutListElement.Items.Add((ShortcutTemplateDataObject)shortcut);
+            }
+
+            ShortcutPageParameters.SearchFilter.FilterChanged += SearchFilter_FilterChanged;
+        }
+
+        private void SearchFilter_FilterChanged(object? sender, string e)
+        {
+            FilterBy(e);
+        }
+
+        public void FilterBy(string filter)
+        {
+            ShortcutListElement.Items.Clear();
+            foreach (var shortcut in shortcutList.Shortcuts[int.Parse(CategorySelector.SelectedItem.Name, CultureInfo.InvariantCulture)].Properties)
+            {
+                if (shortcut.Name.Contains(filter, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    ShortcutListElement.Items.Add((ShortcutTemplateDataObject)shortcut);
+                }
+            }
+
+            if (ShortcutListElement.Items.Count == 0)
+            {
+                ShortcutListElement.Items.Add(new ShortcutTemplateDataObject("No search results found", string.Empty, new StackPanel() { Visibility = Microsoft.UI.Xaml.Visibility.Collapsed }));
             }
         }
 
