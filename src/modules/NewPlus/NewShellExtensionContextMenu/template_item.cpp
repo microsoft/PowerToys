381980d50221b7a0fc0b945586dd4cd13290aa32
Christian Gaarden Gaardmark <cgaarden@hotmail.com>
2024-11-07 20:49:37 -0800

    less delta

--- src/modules/NewPlus/NewShellExtensionContextMenu/template_item.cpp
+++ src/modules/NewPlus/NewShellExtensionContextMenu/template_item.cpp
@@ -122,13 +122,13 @@ void template_item::refresh_target(const std::filesystem::path target_final_full
     SHChangeNotify(SHCNE_CREATE, SHCNF_PATH | SHCNF_FLUSH, target_final_fullpath.wstring().c_str(), NULL);
 }
 
-void template_item::enter_rename_mode(const ComPtr<IUnknown> site_of_folder, const std::filesystem::path target_fullpath) const
+void template_item::enter_rename_mode(const ComPtr<IUnknown> site, const std::filesystem::path target_fullpath) const
 {
-    std::thread thread_for_renaming_workaround(rename_on_other_thread_workaround, site_of_folder, target_fullpath);
+    std::thread thread_for_renaming_workaround(rename_on_other_thread_workaround, site, target_fullpath);
     thread_for_renaming_workaround.detach();
 }
 
-void template_item::rename_on_other_thread_workaround(const ComPtr<IUnknown> site_of_folder, const std::filesystem::path target_fullpath)
+void template_item::rename_on_other_thread_workaround(const ComPtr<IUnknown> site, const std::filesystem::path target_fullpath)
 {
     // Have been unable to have Windows Explorer Shell enter rename mode from the main thread
     // Sleep for a bit to only enter rename mode when icon has been drawn. Not strictly needed.
@@ -137,27 +137,28 @@ void template_item::rename_on_other_thread_workaround(const ComPtr<IUnknown> sit
 
     const std::wstring filename = target_fullpath.filename();
 
-    int count = 0;
     ComPtr<IServiceProvider> service_provider;
-    ComPtr<IFolderView> target_folder_view;
-    site_of_folder->QueryInterface(IID_PPV_ARGS(&service_provider));
-    service_provider->QueryService(__uuidof(IFolderView), IID_PPV_ARGS(&target_folder_view));
-    target_folder_view->ItemCount(SVGIO_ALLVIEW, &count);
+    site->QueryInterface(IID_PPV_ARGS(&service_provider));
+    ComPtr<IFolderView> folder_view;
+    service_provider->QueryService(__uuidof(IFolderView), IID_PPV_ARGS(&folder_view));
+
+    int count = 0;
+    folder_view->ItemCount(SVGIO_ALLVIEW, &count);
 
     for (int i = 0; i < count; ++i)
     {
         std::wstring path_of_item(MAX_PATH, 0);
-        LPITEMIDLIST shell_item_id_pointer;
+        LPITEMIDLIST pidl;
 
-        target_folder_view->Item(i, &shell_item_id_pointer);
-        SHGetPathFromIDList(shell_item_id_pointer, &path_of_item[0]);
-        CoTaskMemFree(shell_item_id_pointer);
+        folder_view->Item(i, &pidl);
+        SHGetPathFromIDList(pidl, &path_of_item[0]);
+        CoTaskMemFree(pidl);
 
         std::wstring current_filename = std::filesystem::path(path_of_item.c_str()).filename();
 
         if (utilities::wstring_same_when_comparing_ignore_case(filename, current_filename))
         {
-            target_folder_view->SelectItem(i, SVSI_EDIT | SVSI_SELECT | SVSI_DESELECTOTHERS | SVSI_ENSUREVISIBLE | SVSI_FOCUSED);
+            folder_view->SelectItem(i, SVSI_EDIT | SVSI_SELECT | SVSI_DESELECTOTHERS | SVSI_ENSUREVISIBLE | SVSI_FOCUSED);
             break;
         }
     }
