ceafbe8bff4e8968502bb7778992b19141d7005f
Jaime Bernardo <jaime@janeasystems.com>
2024-11-19 14:01:55 +0000

    Fix installer build

--- installer/PowerToysSetupCustomActions/CustomAction.cpp
+++ installer/PowerToysSetupCustomActions/CustomAction.cpp
@@ -1272,7 +1272,7 @@ UINT __stdcall TerminateProcessesCA(MSIHANDLE hInstall)
     }
     processes.resize(bytes / sizeof(processes[0]));
 
-    std::array<std::wstring_view, 37> processesToTerminate = {
+    std::array<std::wstring_view, 38> processesToTerminate = {
         L"PowerToys.PowerLauncher.exe",
         L"PowerToys.Settings.exe",
         L"PowerToys.AdvancedPaste.exe",
