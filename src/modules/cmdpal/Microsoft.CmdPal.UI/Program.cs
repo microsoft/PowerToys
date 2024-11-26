8b3325bb03a35703794e2b7347774ab3a8850259
Mike Griese <migrie@microsoft.com>
2024-11-26 11:11:33 -0600

    making it single instanced wasn't the source of the rooriginates

---
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Program.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using Microsoft.Windows.AppLifecycle;
+
+namespace Microsoft.CmdPal.UI;
+
+// cribbed heavily from
+//
+// https://github.com/microsoft/WindowsAppSDK-Samples/tree/main/Samples/AppLifecycle/Instancing/cs2/cs-winui-packaged/CsWinUiDesktopInstancing
+internal sealed class Program
+{
+    private static App? app;
+
+    // LOAD BEARING
+    //
+    // Main cannot be async. If it is, then the clipboard won't work, and neither will narrator.
+    [STAThread]
+    private static int Main(string[] args)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        var isRedirect = DecideRedirection();
+        if (!isRedirect)
+        {
+            Microsoft.UI.Xaml.Application.Start((p) =>
+            {
+                var context = new Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                app = new App();
+            });
+        }
+
+        return 0;
+    }
+
+    private static bool DecideRedirection()
+    {
+        var isRedirect = false;
+        var args = AppInstance.GetCurrent().GetActivatedEventArgs();
+        var keyInstance = AppInstance.FindOrRegisterForKey("randomKey");
+
+        if (keyInstance.IsCurrent)
+        {
+            keyInstance.Activated += OnActivated;
+        }
+        else
+        {
+            isRedirect = true;
+            _ = keyInstance.RedirectActivationToAsync(args);
+        }
+
+        return isRedirect;
+    }
+
+    private static void OnActivated(object? sender, AppActivationArguments args)
+    {
+        var kind = args.Kind;
+        _ = kind;
+
+        // If we already have a form, display the message now.
+        // Otherwise, add it to the collection for displaying later.
+        if (App.Current is App thisApp)
+        {
+            if (thisApp.AppWindow is not null and
+                MainWindow mainWindow)
+            {
+                mainWindow.Summon();
+            }
+        }
+    }
+}
