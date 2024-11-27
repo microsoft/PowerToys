4a17428951543e0b7e33833762161f24869dc8f7
Jeremy Sinclair <4016293+snickler@users.noreply.github.com>
2024-11-27 15:50:21 -0600

    [Analyzers][ImageResizer] Suppress IDE0073 to retain original copyright

--- src/modules/imageresizer/ui/Views/AutoDoubleConverter.cs
+++ src/modules/imageresizer/ui/Views/AutoDoubleConverter.cs
@@ -1,6 +1,8 @@
-﻿// Copyright (c) Brice Lambson
+﻿#pragma warning disable IDE0073
+// Copyright (c) Brice Lambson
 // The Brice Lambson licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.  Code forked from Brice Lambson's https://github.com/bricelam/ImageResizer/
+#pragma warning restore IDE0073
 
 using System;
 using System.Globalization;
