05e54e64c8eded87404f13cfdf83b6e5ffbeead0
Leilei Zhang <leilzh@microsoft.com>
2024-12-10 08:38:15 +0800

    add fuzz

---
+++ src/modules/AdvancedPaste/AdvancedPaste.FuzzTests/FuzzTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace AdvancedPaste.FuzzTests
+{
+    public class FuzzTests
+    {
+        public static void ValidatePhoneNumber(string someString)
+        {
+            if (someString.Length < 10)
+            {
+                return;
+            }
+        }
+
+        public static void FuzzPhoneNumber(ReadOnlySpan<byte> input)
+        {
+            ValidatePhoneNumber(input.ToString());
+        }
+    }
+}
