8e13bd799a79fd78ba55002f588b83813e70a4ca
Leilei Zhang <leilzh@microsoft.com>
2024-12-13 16:46:31 +0800

    add fuzz

---
+++ src/modules/AdvancedPaste/AdvancedPaste.FuzzTests/FuzzTests.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using AdvancedPaste.Helpers;
+using Windows.ApplicationModel.DataTransfer;
+
+// OneFuzz currently does not support .NET 9 code testing, so this is a temporary solution.
+// Create a .NET 8 project and use a file link to include the code for testing first.
+namespace AdvancedPaste.FuzzTests
+{
+    public class FuzzTests
+    {
+        public static void FuzzToJsonFromXmlOrCsv(ReadOnlySpan<byte> input)
+        {
+            var dataPackage = new DataPackage();
+            dataPackage.SetText(input.ToString());
+            _ = Task.Run(async () => await JsonHelper.ToJsonFromXmlOrCsvAsync(dataPackage.GetView())).Result;
+        }
+    }
+}
