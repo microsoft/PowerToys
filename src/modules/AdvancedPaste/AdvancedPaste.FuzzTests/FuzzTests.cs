8869578187a45d71d6e872d338fc2ffdacb2db72
Leilei Zhang <leilzh@microsoft.com>
2024-12-16 14:54:05 +0800

    add readme and update the test code

--- src/modules/AdvancedPaste/AdvancedPaste.FuzzTests/FuzzTests.cs
+++ src/modules/AdvancedPaste/AdvancedPaste.FuzzTests/FuzzTests.cs
@@ -13,9 +13,20 @@ namespace AdvancedPaste.FuzzTests
     {
         public static void FuzzToJsonFromXmlOrCsv(ReadOnlySpan<byte> input)
         {
-            var dataPackage = new DataPackage();
-            dataPackage.SetText(input.ToString());
-            _ = Task.Run(async () => await JsonHelper.ToJsonFromXmlOrCsvAsync(dataPackage.GetView())).Result;
+            try
+            {
+                var dataPackage = new DataPackage();
+                dataPackage.SetText(input.ToString());
+                _ = Task.Run(async () => await JsonHelper.ToJsonFromXmlOrCsvAsync(dataPackage.GetView())).Result;
+            }
+            catch (Exception ex) when (ex is ArgumentException)
+            {
+                // This is an example. It's important to filter out any *expected* exceptions from our code here.
+                // However, catching all exceptions is considered an anti-pattern because it may suppress legitimate
+                // issues, such as a NullReferenceException thrown by our code. In this case, we still re-throw
+                // the exception, as the ToJsonFromXmlOrCsvAsync method is not expected to throw any exceptions.
+                throw;
+            }
         }
     }
 }
