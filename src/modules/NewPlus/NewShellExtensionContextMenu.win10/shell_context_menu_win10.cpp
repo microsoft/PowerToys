08deccf549b6c1dd0d6ae737bd83155c5a929167
Jaime Bernardo <jaime@janeasystems.com>
2024-12-20 16:06:43 +0000

    Also only Query for context menu

--- src/modules/NewPlus/NewShellExtensionContextMenu.win10/shell_context_menu_win10.cpp
+++ src/modules/NewPlus/NewShellExtensionContextMenu.win10/shell_context_menu_win10.cpp
@@ -38,9 +38,9 @@ IFACEMETHODIMP shell_context_menu_win10::QueryContextMenu(HMENU menu_handle, UIN
         return E_FAIL;
     }
 
-    if (menu_flags & CMF_DEFAULTONLY)
+    if (menu_flags & (CMF_DEFAULTONLY | CMF_VERBSONLY | CMF_OPTIMIZEFORINVOKE))
     {
-        return MAKE_HRESULT(SEVERITY_SUCCESS, FACILITY_NULL, 0);
+        return E_UNEXPECTED;
     }
 
     try
