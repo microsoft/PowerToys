d395fea9f674f6fb44c1c21682c40861a841d2ba
htcfreek <61519853+htcfreek@users.noreply.github.com>
2024-12-28 14:41:30 +0100

    Move separator into custom control

---
+++ src/settings-ui/Settings.UI/SettingsXAML/Controls/MetadataDotSeparator.xaml.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.PowerToys.Settings.UI.Controls
+{
+    public sealed partial class MetadataDotSeparator : UserControl
+    {
+        public MetadataDotSeparator()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
