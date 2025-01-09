microsoft/PowerToys

commit 3a1f88cfdf60b377aaa7738c43d6c6db72432896

Centralize Loading code for Shell/Pages (#244)
## Summary of the Pull Request

This PR is ready to review with a bunch of code clean-up around shell and page initialization/navigation, progress, errors, and the back button.

There's still some future items to think about, but that's beyond the scope of this work right now. See more notes in comments below on changes:

> Alright, I cleaned up all the Page Initialization and Navigation code for Shell, it's all centralized there now (which makes sense) and removes 3x duplicated code we had in the ExtPage templates. They're just basic XAML controls now which don't really need to worry about how they get loaded, and just get their VMs.
>
> I also tidied up the massive if statement we had in ShellPage.xaml.cs for each page type so we have a singular logical flow that just branches in two statements with an inline switch for the VM constructor and the page type.
> 
> In the future, we may want to cache some page VMs there, so if you navigate back/forth we don't reload everything...
>
> This also let us remove a message which was effectively just internal communication. Should be a bit cleaner now, certainly easier to add new page types now too.

These are some of the last remnants of things from the original MVVM setup when we were still testing patterns and then just copied.

This uses `IsInitialized` for tracking the progress bar and bubbles up errors better without blocking the app (and provides better context for errors).

This also fixes the issue with the Back Button stealing focus instead of the search box (by moving it to the Shell page to leave the SearchBox control as just responsible for the searchbox/keyboard input).

-------

Future Open questions/TODO:
- [ ] How do we better track the parent extension/owner of pages/commands/etc... for error reporting?
- [x] Need to clean-up backend logic of VMs for the loading state for the new Shell page centralization

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/PageViewModel.cs
@@ -64,11 +64,21 @@ public partial class PageViewModel : ExtensionObjectViewModel, IPageContext
         }
         catch (Exception ex)
         {
-            ShowException(ex);
+            ShowException(ex, _pageModel?.Unsafe?.Name);
             return Task.FromResult(false);
         }
 
-        IsInitialized = true;
+        // Notify we're done back on the UI Thread.
+        Task.Factory.StartNew(
+            () =>
+            {
+                IsInitialized = true;
+
+                // TODO: Do we want an event/signal here that the Page Views can listen to? (i.e. ListPage setting the selected index to 0, however, in async world the user may have already started navigating around page...)
+            },
+            CancellationToken.None,
+            TaskCreationOptions.None,
+            Scheduler);
         return Task.FromResult(true);
     }
 
@@ -101,9 +111,9 @@ public partial class PageViewModel : ExtensionObjectViewModel, IPageContext
             var propName = args.PropertyName;
             FetchProperty(propName);
         }
-        catch (Exception e)
+        catch (Exception ex)
         {
-            ShowException(e);
+            ShowException(ex, _pageModel?.Unsafe?.Name);
         }
     }
 
@@ -143,22 +153,27 @@ public partial class PageViewModel : ExtensionObjectViewModel, IPageContext
         UpdateProperty(propertyName);
     }
 
-    public void ShowException(Exception ex)
+    public void ShowException(Exception ex, string? extensionHint = null)
     {
+        // Set the extensionHint to the Page Title (if we have one, and one not provided).
+        extensionHint ??= _pageModel?.Unsafe?.Title;
+
         Task.Factory.StartNew(
             () =>
         {
-            ErrorMessage += $"{ex.Message}\n{ex.Source}\n{ex.StackTrace}\n\nThis is due to a bug in the extension's code.";
+            ErrorMessage += $"A bug occurred in {$"the \"{extensionHint}\"" ?? "an unknown's"} extension's code:\n{ex.Message}\n{ex.Source}\n{ex.StackTrace}\n\n";
         },
             CancellationToken.None,
             TaskCreationOptions.None,
             Scheduler);
     }
+
+    public override string ToString() => $"{Title} ViewModel";
 }
 
 public interface IPageContext
 {
-    public void ShowException(Exception ex);
+    public void ShowException(Exception ex, string? extensionHint = null);
 
     public TaskScheduler Scheduler { get; }
 }

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ShellViewModel.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using CommunityToolkit.Common;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
@@ -11,8 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.CmdPal.UI.ViewModels;
 
-public partial class ShellViewModel(IServiceProvider _serviceProvider) : ObservableObject,
-    IRecipient<NavigateToPageMessage>
+public partial class ShellViewModel(IServiceProvider _serviceProvider, TaskScheduler _scheduler) : ObservableObject
 {
     [ObservableProperty]
     public partial bool IsLoaded { get; set; } = false;
@@ -29,8 +29,6 @@ public partial class ShellViewModel(IServiceProvider _serviceProvider) : Observa
     [RelayCommand]
     public async Task<bool> LoadAsync()
     {
-        WeakReferenceMessenger.Default.Register<NavigateToPageMessage>(this);
-
         var tlcManager = _serviceProvider.GetService<TopLevelCommandManager>();
         await tlcManager!.LoadBuiltinsAsync();
         IsLoaded = true;
@@ -53,5 +51,66 @@ public partial class ShellViewModel(IServiceProvider _serviceProvider) : Observa
         return true;
     }
 
-    public void Receive(NavigateToPageMessage message) => CurrentPage = message.Page;
+    public void LoadPageViewModel(PageViewModel viewModel)
+    {
+        // Note: We removed the general loading state, extensions sometimes use their `IsLoading`, but it's inconsistently implemented it seems.
+        // IsInitialized is our main indicator of the general overall state of loading props/items from a page we use for the progress bar
+        // This triggers that load generally with the InitializeCommand asynchronously when we navigate to a page.
+        // We could re-track the page loading status, if we need it more granularly below again, but between the initialized and error message, we can infer some status.
+        // We could also maybe move this thread offloading we do for loading into PageViewModel and better communicate between the two... a few different options.
+
+        ////LoadedState = ViewModelLoadedState.Loading;
+        if (!viewModel.IsInitialized
+            && viewModel.InitializeCommand != null)
+        {
+            _ = Task.Run(async () =>
+            {
+                // You know, this creates the situation where we wait for
+                // both loading page properties, AND the items, before we
+                // display anything.
+                //
+                // We almost need to do an async await on initialize, then
+                // just a fire-and-forget on FetchItems.
+                // RE: We do set the CurrentPage in ShellPage.xaml.cs as well, so, we kind of are doing two different things here.
+                // Definitely some more clean-up to do, but at least its centralized to one spot now.
+                viewModel.InitializeCommand.Execute(null);
+
+                await viewModel.InitializeCommand.ExecutionTask!;
+
+                if (viewModel.InitializeCommand.ExecutionTask.Status != TaskStatus.RanToCompletion)
+                {
+                    // TODO: Handle failure case
+                    System.Diagnostics.Debug.WriteLine(viewModel.InitializeCommand.ExecutionTask.Exception);
+
+                    // TODO GH #239 switch back when using the new MD text block
+                    // _ = _queue.EnqueueAsync(() =>
+                    /*_queue.TryEnqueue(new(() =>
+                    {
+                        LoadedState = ViewModelLoadedState.Error;
+                    }));*/
+                }
+                else
+                {
+                    // TODO GH #239 switch back when using the new MD text block
+                    // _ = _queue.EnqueueAsync(() =>
+                    _ = Task.Factory.StartNew(
+                        () =>
+                        {
+                            var result = (bool)viewModel.InitializeCommand.ExecutionTask.GetResultOrDefault()!;
+
+                            CurrentPage = result ? viewModel : null;
+                            ////LoadedState = result ? ViewModelLoadedState.Loaded : ViewModelLoadedState.Error;
+                        },
+                        CancellationToken.None,
+                        TaskCreationOptions.None,
+                        _scheduler);
+                }
+            });
+        }
+        else
+        {
+            CurrentPage = viewModel;
+            ////LoadedState = ViewModelLoadedState.Loaded;
+        }
+    }
 }

src/modules/cmdpal/Microsoft.CmdPal.UI/ShellPage.xaml
@@ -3,12 +3,14 @@
     x:Class="Microsoft.CmdPal.UI.ShellPage"
     xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
     xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    xmlns:animations="using:CommunityToolkit.WinUI.Animations"
     xmlns:converters="using:CommunityToolkit.WinUI.Converters"
     xmlns:cpcontrols="using:Microsoft.CmdPal.UI.Controls"
     xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
     xmlns:help="using:Microsoft.CmdPal.UI.Helpers"
     xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
     xmlns:toolkit="using:CommunityToolkit.WinUI.UI.Controls"
+    xmlns:ui="using:CommunityToolkit.WinUI"
     Background="Transparent"
     mc:Ignorable="d">
 
@@ -19,6 +21,10 @@
                 EmptyValue="Collapsed"
                 NotEmptyValue="Visible" />
             <converters:BoolNegationConverter x:Key="BoolNegationConverter" />
+            <converters:BoolToVisibilityConverter
+                x:Key="BoolToInvertedVisibilityConverter"
+                FalseValue="Visible"
+                TrueValue="Collapsed" />
         </ResourceDictionary>
     </Page.Resources>
 
@@ -32,12 +38,100 @@
             <Grid.RowDefinitions>
                 <RowDefinition Height="Auto" />
                 <RowDefinition Height="*" />
+                <RowDefinition Height="Auto" />
             </Grid.RowDefinitions>
 
-            <cpcontrols:SearchBar
-                x:Name="SearchBox"
-                Grid.Row="0"
-                CurrentPageViewModel="{x:Bind ViewModel.CurrentPage, Mode=OneWay}" />
+            <!--  Back button and search box  -->
+            <Grid
+                Padding="0,12,0,12"
+                HorizontalAlignment="Stretch"
+                VerticalAlignment="Stretch">
+                <Grid.ColumnDefinitions>
+                    <ColumnDefinition Width="44" />
+                    <ColumnDefinition Width="Auto" />
+                    <ColumnDefinition Width="*" />
+                </Grid.ColumnDefinitions>
+
+                <!--  Back button  -->
+                <FontIcon
+                    Margin="14,0,0,0"
+                    HorizontalAlignment="Center"
+                    ui:VisualExtensions.NormalizedCenterPoint="0.5,0.5"
+                    AutomationProperties.AccessibilityView="Raw"
+                    Foreground="{ThemeResource TextFillColorSecondaryBrush}"
+                    Glyph="&#xE721;"
+                    Visibility="{x:Bind ViewModel.CurrentPage.IsNested, Mode=OneWay, Converter={StaticResource BoolToInvertedVisibilityConverter}}">
+                    <animations:Implicit.ShowAnimations>
+                        <animations:OpacityAnimation
+                            From="0"
+                            To="1.0"
+                            Duration="0:0:0.340" />
+                        <animations:ScaleAnimation
+                            From="0.5"
+                            To="1"
+                            Duration="0:0:0.350" />
+                    </animations:Implicit.ShowAnimations>
+                    <animations:Implicit.HideAnimations>
+                        <animations:OpacityAnimation
+                            From="1.0"
+                            To="0"
+                            Duration="0:0:0.340" />
+                        <animations:ScaleAnimation
+                            From="1"
+                            To="0.5"
+                            Duration="0:0:0.350" />
+                    </animations:Implicit.HideAnimations>
+                </FontIcon>
+
+                <Button
+                    x:Name="BackButton"
+                    Margin="12,0,0,0"
+                    Padding="8"
+                    HorizontalAlignment="Center"
+                    VerticalAlignment="Center"
+                    ui:VisualExtensions.NormalizedCenterPoint="0.5,0.5"
+                    Content="{ui:FontIcon Glyph=&#xE72B;,
+                                          FontSize=14}"
+                    FontSize="16"
+                    Foreground="{ThemeResource TextFillColorSecondaryBrush}"
+                    Style="{StaticResource SubtleButtonStyle}"
+                    Tapped="BackButton_Tapped"
+                    ToolTipService.ToolTip="Back"
+                    Visibility="{x:Bind ViewModel.CurrentPage.IsNested, Mode=OneWay}">
+                    <animations:Implicit.ShowAnimations>
+                        <animations:OpacityAnimation
+                            From="0"
+                            To="1.0"
+                            Duration="0:0:0.340" />
+                        <animations:ScaleAnimation
+                            From="0.5"
+                            To="1"
+                            Duration="0:0:0.350" />
+                    </animations:Implicit.ShowAnimations>
+                    <animations:Implicit.HideAnimations>
+                        <animations:OpacityAnimation
+                            From="1.0"
+                            To="0"
+                            Duration="0:0:0.340" />
+                        <animations:ScaleAnimation
+                            From="1"
+                            To="0.5"
+                            Duration="0:0:0.350" />
+                    </animations:Implicit.HideAnimations>
+                </Button>
+
+                <!--  Search box  -->
+                <cpcontrols:SearchBar
+                    x:Name="SearchBox"
+                    Grid.Column="2"
+                    CurrentPageViewModel="{x:Bind ViewModel.CurrentPage, Mode=OneWay}" />
+            </Grid>
+
+            <ProgressBar
+                Grid.ColumnSpan="2"
+                VerticalAlignment="Bottom"
+                IsIndeterminate="True"
+                Visibility="{x:Bind ViewModel.CurrentPage.IsInitialized, Converter={StaticResource BoolToInvertedVisibilityConverter}, Mode=OneWay}" />
 
             <Grid
                 x:Name="ContentGrid"
@@ -50,7 +144,10 @@
                     <ColumnDefinition x:Name="DetailsColumn" Width="Auto" />
                 </Grid.ColumnDefinitions>
 
-                <Frame Name="RootFrame" IsNavigationStackEnabled="True" />
+                <Frame
+                    Name="RootFrame"
+                    IsNavigationStackEnabled="True"
+                    Navigated="RootFrame_Navigated" />
 
                 <Grid
                     x:Name="DetailsContent"
@@ -71,6 +168,7 @@
                         x:Name="HeroImageBorder"
                         Width="64"
                         Height="64"
+                        x:Load="{x:Bind IsLoaded, Mode=OneWay}"
                         SourceKey="{x:Bind ViewModel.Details.HeroImage, Mode=OneWay}"
                         SourceRequested="{x:Bind help:IconCacheProvider.SourceRequested}"
                         Visibility="{x:Bind ViewModel.Details.HasHeroImage, Mode=OneWay}" />
@@ -96,7 +194,25 @@
                 </Grid>
                 <!--  /DetailsContent  -->
             </Grid>
+            <ScrollView
+                Grid.Row="2"
+                Grid.ColumnSpan="2"
+                MaxHeight="120"
+                Background="{ThemeResource SystemControlErrorBackgroundColor}"
+                BorderBrush="{ThemeResource SystemControlErrorTextForegroundBrush}"
+                BorderThickness="0,1,0,1"
+                CornerRadius="0"
+                Visibility="{x:Bind ViewModel.CurrentPage.ErrorMessage, Converter={StaticResource StringNotEmptyToVisibilityConverter}, Mode=OneWay}">
+                <TextBlock Margin="16,8,16,16" IsTextSelectionEnabled="True">
+                    <Run
+                        FontWeight="SemiBold"
+                        Foreground="{ThemeResource SystemErrorTextColor}"
+                        Text="Error(s) on page:" />
+                    <LineBreak /><Run Text="{x:Bind ViewModel.CurrentPage.ErrorMessage, Mode=OneWay}" />
+                </TextBlock>
+            </ScrollView>
         </Grid>
+
         <cpcontrols:ActionBar Grid.Row="1" CurrentPageViewModel="{x:Bind ViewModel.CurrentPage, Mode=OneWay}" />
 
         <VisualStateManager.VisualStateGroups>

Directory.Packages.props
@@ -8,7 +8,8 @@
     <PackageVersion Include="AdaptiveCards.Templating" Version="2.0.2" />
     <PackageVersion Include="Appium.WebDriver" Version="4.4.5" />
     <PackageVersion Include="Azure.AI.OpenAI" Version="1.0.0-beta.17" />
-    <PackageVersion Include="CommunityToolkit.Mvvm" Version="8.4.0-preview2" />
+    <PackageVersion Include="CommunityToolkit.Common" Version="8.4.0" />
+    <PackageVersion Include="CommunityToolkit.Mvvm" Version="8.4.0" />
     <PackageVersion Include="CommunityToolkit.WinUI.Animations" Version="8.2.241112-preview1" />
     <PackageVersion Include="CommunityToolkit.WinUI.Collections" Version="8.2.241112-preview1" />
     <PackageVersion Include="CommunityToolkit.WinUI.Controls.Primitives" Version="8.2.241112-preview1" />

src/modules/cmdpal/Exts/SamplePagesExtension/EvilSampleListPage.cs
@@ -13,6 +13,7 @@ internal sealed partial class EvilSampleListPage : ListPage
     {
         Icon = new(string.Empty);
         Name = "Open";
+        Title = "Evil Sample Page";
     }
 
     public override IListItem[] GetItems()

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/CommandItemViewModel.cs
@@ -117,7 +117,7 @@ public partial class CommandItemViewModel : ExtensionObjectViewModel
         }
         catch (Exception ex)
         {
-            PageContext.ShowException(ex);
+            PageContext.ShowException(ex, _commandItemModel?.Unsafe?.Title);
         }
     }
 

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ListViewModel.cs
@@ -66,7 +66,7 @@ public partial class ListViewModel : PageViewModel
             }
             catch (Exception ex)
             {
-                ShowException(ex);
+                ShowException(ex, _model?.Unsafe?.Name);
             }
         }
         else
@@ -108,7 +108,9 @@ public partial class ListViewModel : PageViewModel
         }
         catch (Exception ex)
         {
-            ShowException(ex);
+            // TODO: Move this within the for loop, so we can catch issues with individual items
+            // Create a special ListItemViewModel for errors and use an ItemTemplateSelector in the ListPage to display error items differently.
+            ShowException(ex, _model?.Unsafe?.Name);
             throw;
         }
 

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/MarkdownPageViewModel.cs
@@ -52,7 +52,7 @@ public partial class MarkdownPageViewModel : PageViewModel
         }
         catch (Exception ex)
         {
-            ShowException(ex);
+            ShowException(ex, _model?.Unsafe?.Name);
             throw;
         }
     }

commit 4b9b15f493a1f2139315218592b0779d236c48d9

Fix `Loading` on pages (#290)
This subtly regressed in #244, so subtly that I didn't really notice it till I fixed it.

In that PR, we replaced the `visibility` binding of the `ProgressBar` with a binding to `IsInitialized`. For most pages, this actually just works fine - they're only initialized once they return some results. And most pages are only loading until they first return something.  

But the trick is with pages that do some `Loading` _after_ they're initialized. For them, the progress bar never shows up, because `Initialized` is already true.

I actually only stumbled upon this because of a totally different bug (which this also fixes). 

In rare cases we'd load a little out of order, and the ShellPage would try to determine if it should hide the `ErrorMessage` or not. About 25% of the time, it would evaluate `{x:Bind ViewModel.CurrentPage.ErrorMessage, Converter={StaticResource StringNotEmptyToVisibilityConverter}, Mode=OneWay}` _before_ the `CurrentPage` got set, so it would show it initially. That looked real janky.

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/LoadingPageViewModel.cs
@@ -1,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CmdPal.Extensions;
+
+namespace Microsoft.CmdPal.UI.ViewModels;
+
+public partial class LoadingPageViewModel : PageViewModel
+{
+    public LoadingPageViewModel(IPage? model, TaskScheduler scheduler)
+        : base(model, scheduler)
+    {
+        ModelIsLoading = true;
+        IsInitialized = false;
+    }
+}

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/PageViewModel.cs
@@ -15,7 +15,10 @@ public partial class PageViewModel : ExtensionObjectViewModel, IPageContext
 
     private readonly ExtensionObject<IPage> _pageModel;
 
+    public bool IsLoading => ModelIsLoading || (!IsInitialized);
+
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsLoading))]
     public partial bool IsInitialized { get; protected set; }
 
     [ObservableProperty]
@@ -38,7 +41,9 @@ public partial class PageViewModel : ExtensionObjectViewModel, IPageContext
 
     public string Title { get => string.IsNullOrEmpty(field) ? Name : field; protected set; } = string.Empty;
 
-    public bool IsLoading { get; protected set; } = true;
+    // This property maps to `IPage.IsLoading`, but we want to expose our own
+    // `IsLoading` property as a combo of this value and `IsInitialized`
+    public bool ModelIsLoading { get; protected set; } = true;
 
     public IconDataType Icon { get; protected set; } = new(string.Empty);
 
@@ -91,13 +96,14 @@ public partial class PageViewModel : ExtensionObjectViewModel, IPageContext
         }
 
         Name = page.Name;
-        IsLoading = page.IsLoading;
+        ModelIsLoading = page.IsLoading;
         Title = page.Title;
         Icon = page.Icon;
 
         // Let the UI know about our initial properties too.
         UpdateProperty(nameof(Name));
         UpdateProperty(nameof(Title));
+        UpdateProperty(nameof(ModelIsLoading));
         UpdateProperty(nameof(IsLoading));
         UpdateProperty(nameof(Icon));
 
@@ -143,7 +149,8 @@ public partial class PageViewModel : ExtensionObjectViewModel, IPageContext
                 this.Title = model.Title ?? string.Empty;
                 break;
             case nameof(IsLoading):
-                this.IsLoading = model.IsLoading;
+                this.ModelIsLoading = model.IsLoading;
+                UpdateProperty(nameof(ModelIsLoading));
                 break;
             case nameof(Icon):
                 this.Icon = model.Icon;

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/SettingsViewModel.cs
@@ -33,7 +33,7 @@ public partial class SettingsViewModel : PageViewModel
 
         Icon = new("\uE713");
         IsInitialized = true;
-        IsLoading = false;
+        ModelIsLoading = false;
         Title = "Settings";
 
         var activeProviders = GetCommandProviders();

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ShellViewModel.cs
@@ -24,7 +24,7 @@ public partial class ShellViewModel(IServiceProvider _serviceProvider, TaskSched
     public partial bool IsDetailsVisible { get; set; }
 
     [ObservableProperty]
-    public partial PageViewModel? CurrentPage { get; set; }
+    public partial PageViewModel CurrentPage { get; set; } = new LoadingPageViewModel(null, _scheduler);
 
     [RelayCommand]
     public async Task<bool> LoadAsync()
@@ -98,7 +98,7 @@ public partial class ShellViewModel(IServiceProvider _serviceProvider, TaskSched
                         {
                             var result = (bool)viewModel.InitializeCommand.ExecutionTask.GetResultOrDefault()!;
 
-                            CurrentPage = result ? viewModel : null;
+                            CurrentPage = viewModel; // result ? viewModel : null;
                             ////LoadedState = result ? ViewModelLoadedState.Loaded : ViewModelLoadedState.Error;
                         },
                         CancellationToken.None,

src/modules/cmdpal/Microsoft.CmdPal.UI/ShellPage.xaml
@@ -131,7 +131,7 @@
                 Grid.ColumnSpan="2"
                 VerticalAlignment="Bottom"
                 IsIndeterminate="True"
-                Visibility="{x:Bind ViewModel.CurrentPage.IsInitialized, Converter={StaticResource BoolToInvertedVisibilityConverter}, Mode=OneWay}" />
+                Visibility="{x:Bind ViewModel.CurrentPage.IsLoading, Mode=OneWay}" />
 
             <Grid
                 x:Name="ContentGrid"