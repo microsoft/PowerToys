664218af82f70c7e738cd1e590960eeb1b0b6a24
Seraphima Zykova <zykovas91@gmail.com>
2025-01-13 23:49:57 +0300

    [Indexer] Results ordering (#298)
    Changed results ordering to achieve the same relevance as in the current PT Run Indexer.
    
    Also adds fuzzy match support

--- src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/Indexer/Utils/QueryStringBuilder.cs
+++ src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/Indexer/Utils/QueryStringBuilder.cs
@@ -1,9 +1,12 @@
-﻿// Copyright (c) Microsoft Corporation
+// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Globalization;
+using System.Linq;
 using System.Text;
+using ManagedCommon;
 
 namespace Microsoft.CmdPal.Ext.Indexer.Indexer.Utils;
 
@@ -13,7 +16,7 @@ internal sealed class QueryStringBuilder
     private const string Properties = "System.ItemUrl, System.ItemNameDisplay, path, System.Search.EntryID, System.Kind, System.KindText, System.Search.GatherTime, System.Search.QueryPropertyHits";
     private const string FromIndex = "FROM SystemIndex WHERE";
     private const string ScopeFileConditions = "SCOPE='file:'";
-    private const string OrderConditions = "ORDER BY System.Search.Rank, System.DateModified, System.ItemNameDisplay DESC";
+    private const string OrderConditions = "ORDER BY System.DateModified DESC";
     private const string SelectQueryWithScope = Select + " " + Properties + " " + FromIndex + " (" + ScopeFileConditions + ")";
     private const string SelectQueryWithScopeAndOrderConditions = SelectQueryWithScope + " " + OrderConditions;
 
@@ -24,11 +27,33 @@ internal sealed class QueryStringBuilder
         var queryStr = new StringBuilder(SelectQueryWithScope);
 
         // Filter by item name display only
-        if (!string.IsNullOrEmpty(searchText))
+        if (!string.IsNullOrWhiteSpace(searchText))
         {
-            queryStr.Append(" AND (CONTAINS(System.ItemNameDisplay, '\"")
-                .Append(searchText)
-                .Append("*\"'))");
+            var appended = false;
+
+            try
+            {
+                var parts = searchText
+                    .Split([' '], StringSplitOptions.RemoveEmptyEntries)
+                    .Select(p => $"\"{p}*\"")
+                    .ToArray();
+
+                if (parts.Length > 0)
+                {
+                    var joinedParts = string.Join(" AND ", parts);
+                    queryStr.Append(" AND (CONTAINS(System.ItemNameDisplay, '").Append(joinedParts).Append("'))");
+                    appended = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Failed to split query string", ex);
+            }
+
+            if (!appended)
+            {
+                queryStr.Append(" AND (CONTAINS(System.ItemNameDisplay, '\"").Append(searchText).Append("*\"'))");
+            }
         }
 
         // Always add reuse where to the query
