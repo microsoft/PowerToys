342d24ccc86547ffe0b1de646008c4cb765e1d1e
Zhaopeng Wang (from Dev Box) <zhaopengwang@microsoft.com>
2025-01-22 15:41:52 +0800

    add powertoys setting UI name to fix WinAppDriver Luanch error

--- src/modules/fancyzones/UITests-FancyZones/RunFancyZonesTest.cs
+++ src/modules/fancyzones/UITests-FancyZones/RunFancyZonesTest.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.FancyZones.UnitTests.Utils;
+using Microsoft.UITests.API;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UITests_FancyZones
@@ -10,24 +11,43 @@ namespace UITests_FancyZones
     [TestClass]
     public class RunFancyZonesTest
     {
-        private static FancyZonesSession? _session;
+        private const string PowerToysPath = @"\..\..\..\WinUI3Apps\PowerToys.Settings.exe";
+        private static UITestAPI? _uITestAPI;
+
+        private static TestContext? _context;
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext testContext)
         {
-            _session = new FancyZonesSession(testContext);
         }
 
         [ClassCleanup]
         public static void ClassCleanup()
         {
-            _session?.Close();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _uITestAPI = new UITestAPI();
+            _uITestAPI.Init(PowerToysPath);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (_uITestAPI != null && _context != null)
+            {
+                _uITestAPI.Close(_context);
+            }
+
+            _context = null;
         }
 
         [TestMethod]
         public void RunFancyZones()
         {
-            Assert.IsNotNull(_session?.FancyZonesProcess);
+            Assert.IsNotNull(_uITestAPI);
         }
     }
 }
