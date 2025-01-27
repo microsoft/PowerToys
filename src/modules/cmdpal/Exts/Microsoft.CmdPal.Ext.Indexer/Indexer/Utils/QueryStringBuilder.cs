62e99fbc70e883004f161431ad14f666ab8c1142
Seraphima Zykova <zykovas91@gmail.com>
2025-01-27 22:52:27 +0300

    [Indexer] Support wildcards like PT Run (#336)
    Changed query generation to support wildcards like PT Run

--- src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/Indexer/Utils/QueryStringBuilder.cs
+++ src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/Indexer/Utils/QueryStringBuilder.cs
@@ -2,66 +2,38 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using ManagedCommon;
+using Microsoft.CmdPal.Ext.Indexer.Indexer.SystemSearch;
 
 namespace Microsoft.CmdPal.Ext.Indexer.Indexer.Utils;
 
 internal sealed class QueryStringBuilder
 {
-    private const string Select = "SELECT";
-    private const string Properties = "System.ItemUrl, System.ItemNameDisplay, path, System.Search.EntryID, System.Kind, System.KindText, System.Search.GatherTime, System.Search.QueryPropertyHits";
-    private const string FromIndex = "FROM SystemIndex WHERE";
+    private const string Properties = "System.ItemUrl, System.ItemNameDisplay, path, System.Search.EntryID, System.Kind, System.KindText";
+    private const string SystemIndex = "SystemIndex";
     private const string ScopeFileConditions = "SCOPE='file:'";
-    private const string OrderConditions = "ORDER BY System.DateModified DESC";
-    private const string SelectQueryWithScope = Select + " " + Properties + " " + FromIndex + " (" + ScopeFileConditions + ")";
-    private const string SelectQueryWithScopeAndOrderConditions = SelectQueryWithScope + " " + OrderConditions;
+    private const string OrderConditions = "System.DateModified DESC";
+    private const string SelectQueryWithScope = "SELECT " + Properties + " FROM " + SystemIndex + " WHERE (" + ScopeFileConditions + ")";
+    private const string SelectQueryWithScopeAndOrderConditions = SelectQueryWithScope + " ORDER BY " + OrderConditions;
+
+    private static ISearchQueryHelper queryHelper;
 
     public static string GeneratePrimingQuery() => SelectQueryWithScopeAndOrderConditions;
 
     public static string GenerateQuery(string searchText, uint whereId)
     {
-        var queryStr = new StringBuilder(SelectQueryWithScope);
-
-        // Filter by item name display only
-        if (!string.IsNullOrWhiteSpace(searchText))
+        if (queryHelper == null)
         {
-            var appended = false;
+            var searchManager = new CSearchManager();
+            ISearchCatalogManager catalogManager = searchManager.GetCatalog(SystemIndex);
+            queryHelper = catalogManager.GetQueryHelper();
 
-            try
-            {
-                var parts = searchText
-                    .Split([' '], StringSplitOptions.RemoveEmptyEntries)
-                    .Select(p => $"\"{p}*\"")
-                    .ToArray();
-
-                if (parts.Length > 0)
-                {
-                    var joinedParts = string.Join(" AND ", parts);
-                    queryStr.Append(" AND (CONTAINS(System.ItemNameDisplay, '").Append(joinedParts).Append("'))");
-                    appended = true;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError("Failed to split query string", ex);
-            }
-
-            if (!appended)
-            {
-                queryStr.Append(" AND (CONTAINS(System.ItemNameDisplay, '\"").Append(searchText).Append("*\"'))");
-            }
+            queryHelper.QuerySelectColumns = Properties;
+            queryHelper.QueryContentProperties = "System.FileName";
+            queryHelper.QuerySorting = OrderConditions;
         }
 
-        // Always add reuse where to the query
-        queryStr.Append(" AND ReuseWhere(")
-            .Append(whereId.ToString(CultureInfo.InvariantCulture))
-            .Append(") ")
-            .Append(OrderConditions);
-
-        return queryStr.ToString();
+        queryHelper.QueryWhereRestrictions = "AND " + ScopeFileConditions + "AND ReuseWhere(" + whereId.ToString(CultureInfo.InvariantCulture) + ")";
+        return queryHelper.GenerateSQLFromUserQuery(searchText);
     }
 }
