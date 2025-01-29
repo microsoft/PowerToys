1321eabc0aa4ff827c63bfa9e2d6a173bf5e62cd
Jaime Bernardo <jaime@janeasystems.com>
2025-01-29 11:32:36 +0000

    Revert "[ci]Remove vc tools version workaround (#37098)"
    This reverts commit 2c069ce708fcec23f3012c6434003908bead332c.

---
+++ .pipelines/verifyAndSetLatestVCToolsVersion.ps1
@@ -0,0 +1,5 @@
+$LatestVCToolsVersion = (([xml](& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest $env:VCWhereExtraVersionTarget -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -include packages -format xml)).instances.instance.packages.package | ? { $_.id -eq "Microsoft.VisualCpp.CRT.Source" }).version;
+
+Write-Output "Latest VCToolsVersion: $LatestVCToolsVersion"
+Write-Output "Updating VCToolsVersion environment variable for job"
+Write-Output "##vso[task.setvariable variable=VCToolsVersion]$LatestVCToolsVersion"
