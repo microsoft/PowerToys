cc644b19982d09fcd2122fe7590c77496c4973b9
LNKLEO <LinkeyLeo@outlook.com>
2025-02-12 08:22:08 +0800

    [Build]Compatibility with VS17.3 and later, for C++23 <expected>(#37321)
    [Build] compatibility with VS17.3 and later, for C++23 <expected> has been introduced in VS17.3.6, and std::expected has conflict/inconsistent with the makeshift (expected-lite)

--- src/common/updating/updating.h
+++ src/common/updating/updating.h
@@ -6,7 +6,14 @@
 #include <filesystem>
 #include <variant>
 #include <winrt/Windows.Foundation.h>
+//#if __MSVC_VERSION__ >= 1933 // MSVC begin to support std::unexpected in 19.33
+#if __has_include(<expected> ) // use the same way with excepted-lite to detect std::unexcepted, as using it as backup
+#include <expected>
+#define USE_STD_EXPECTED 1
+#else
 #include <expected.hpp>
+#define USE_STD_EXPECTED 0
+#endif
 
 #include <common/version/helper.h>
 
@@ -27,7 +34,11 @@ namespace updating
 
     std::future<std::optional<std::filesystem::path>> download_new_version(const new_version_download_info& new_version);
     std::filesystem::path get_pending_updates_path();
+#if USE_STD_EXPECTED
+    std::future<std::expected<github_version_info, std::wstring>> get_github_version_info_async(const bool prerelease = false);
+#else
     std::future<nonstd::expected<github_version_info, std::wstring>> get_github_version_info_async(const bool prerelease = false);
+#endif
     void cleanup_updates();
 
     // non-localized
