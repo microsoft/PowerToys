68afc6623f2681cb283a3849f3c7fde28bce9a1d
Hao Liu <liuhao3418@gmail.com>
2025-02-18 17:10:15 +0800

    [KeyboardManager WinUI3] Create WinUI3 project and wrapper for Keyboard Manager Editor (#37427)
    * Set up KBM WinUI3 Editor UI project
    
    * Test invoking the KBM library via wrapper for WinUI3 C# UI
    
    * Set up Editor Library Wrapper and enable logging
    
    * fix spelling
    
    * update spacing and remove unused file
    
    * fix formatting
    
    * update sln
    
    * update wrapper project config
    
    * import common props
    
    * update UI reference
    
    * gate the new editor with the experimentation toggle in settings

---
+++ src/modules/keyboardmanager/KeyboardManagerEditorLibraryWrapper/KeyboardManagerEditorLibraryWrapper.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <keyboardmanager/common/Helpers.h>
+#include <keyboardmanager/common/Input.h>
+#include <keyboardmanager/common/MappingConfiguration.h>
+
+extern "C" __declspec(dllexport) bool CheckIfRemappingsAreValid();
