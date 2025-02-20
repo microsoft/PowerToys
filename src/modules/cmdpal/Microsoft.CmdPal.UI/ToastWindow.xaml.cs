microsoft/PowerToys

commit a4719daf7128154cecdaca4fd32b8ad464496ffe

Add rudimentary support for Toast messages (#442)
Well, I started by wanting to just close #385. Then I thought, "well, the copy command really needs that toast, doesn't it"

one thing lead to another, and now

![clipboard-history-toast](https://github.com/user-attachments/assets/4d84225c-17bd-4b42-9587-6411888db922)

Closes #385
Closes # what I never filed `ShowToast`, from https://github.com/zadjii-msft/PowerToys/pull/395

src/modules/cmdpal/Microsoft.CmdPal.UI/ToastWindow.xaml
@@ -1,0 +1,25 @@
+﻿<?xml version="1.0" encoding="utf-8" ?>
+<Window
+    x:Class="Microsoft.CmdPal.UI.ToastWindow"
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+    xmlns:local="using:Microsoft.CmdPal.UI"
+    xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+    xmlns:ui="using:CommunityToolkit.WinUI"
+    Title="Command Palette Toast"
+    Activated="OnActivated"
+    mc:Ignorable="d">
+    <!--<Window.SystemBackdrop>
+        <MicaBackdrop />
+    </Window.SystemBackdrop>-->
+    <StackPanel x:Name="ToastGrid">
+        <TextBlock
+            x:Name="ToastText"
+            Padding="8,8,28,20"
+            FontSize="16"
+            Foreground="{ThemeResource SystemControlForegroundBaseHighBrush}"
+            Text="{x:Bind ViewModel.ToastMessage, Mode=OneWay}"
+            TextAlignment="Left" />
+    </StackPanel>
+</Window>

src/modules/cmdpal/Microsoft.CmdPal.UI/ToastWindow.xaml.cs
@@ -1,0 +1,160 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.WinUI;
+using Microsoft.CmdPal.UI.ViewModels;
+using Microsoft.UI.Composition;
+using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Windows.Graphics;
+using Windows.UI;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+using WinRT;
+
+namespace Microsoft.CmdPal.UI;
+
+public sealed partial class ToastWindow : Window
+{
+    private readonly HWND _hwnd;
+
+    public ToastViewModel ViewModel { get; } = new();
+
+    private readonly DispatcherQueueTimer _debounceTimer = DispatcherQueue.GetForCurrentThread().CreateTimer();
+
+    private DesktopAcrylicController? _acrylicController;
+    private SystemBackdropConfiguration? _configurationSource;
+
+    public ToastWindow()
+    {
+        this.InitializeComponent();
+        AppWindow.Hide();
+        AppWindow.IsShownInSwitchers = false;
+        ExtendsContentIntoTitleBar = true;
+        AppWindow.SetPresenter(AppWindowPresenterKind.CompactOverlay);
+        AppWindow.SetIcon("ms-appx:///Assets/Icons/StoreLogo.png");
+        AppWindow.Title = "Command Palette Settings";
+        AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Collapsed;
+
+        _hwnd = new HWND(WinRT.Interop.WindowNative.GetWindowHandle(this).ToInt32());
+        PInvoke.EnableWindow(_hwnd, false);
+
+        SetAcrylic();
+        ToastText.ActualThemeChanged += (s, e) => UpdateAcrylic();
+    }
+
+    private void PositionCentered()
+    {
+        var intSize = new SizeInt32
+        {
+            Width = Convert.ToInt32(ToastText.ActualWidth),
+            Height = Convert.ToInt32(ToastText.ActualHeight),
+        };
+        AppWindow.Resize(intSize);
+
+        var displayArea = DisplayArea.GetFromWindowId(AppWindow.Id, DisplayAreaFallback.Nearest);
+        if (displayArea is not null)
+        {
+            var centeredPosition = AppWindow.Position;
+            centeredPosition.X = (displayArea.WorkArea.Width - AppWindow.Size.Width) / 2;
+
+            var monitorHeight = displayArea.WorkArea.Height;
+            var windowHeight = AppWindow.Size.Height;
+            centeredPosition.Y = (int)(monitorHeight - (windowHeight * 2));
+            AppWindow.Move(centeredPosition);
+        }
+    }
+
+    public void ShowToast(string message)
+    {
+        ViewModel.ToastMessage = message;
+        DispatcherQueue.TryEnqueue(
+            DispatcherQueuePriority.Low,
+            () =>
+        {
+            PositionCentered();
+
+            // SW_SHOWNA prevents us from getting activated (and stealing FG)
+            PInvoke.ShowWindow(_hwnd, SHOW_WINDOW_CMD.SW_SHOWNA);
+
+            _debounceTimer.Debounce(
+                () =>
+                {
+                    AppWindow.Hide();
+                },
+                interval: TimeSpan.FromMilliseconds(2500),
+                immediate: false);
+        });
+    }
+
+    // We want to use DesktopAcrylicKind.Thin and custom colors as this is the default material
+    // other Shell surfaces are using, this cannot be set in XAML however.
+    private void SetAcrylic()
+    {
+        if (DesktopAcrylicController.IsSupported())
+        {
+            // Hooking up the policy object.
+            _configurationSource = new SystemBackdropConfiguration
+            {
+                // Initial configuration state.
+                IsInputActive = true,
+            };
+            UpdateAcrylic();
+        }
+    }
+
+    private void UpdateAcrylic()
+    {
+        _acrylicController = GetAcrylicConfig(Content);
+
+        // Enable the system backdrop.
+        // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+        _acrylicController.AddSystemBackdropTarget(this.As<ICompositionSupportsSystemBackdrop>());
+        _acrylicController.SetSystemBackdropConfiguration(_configurationSource);
+    }
+
+    private static DesktopAcrylicController GetAcrylicConfig(UIElement content)
+    {
+        var feContent = content as FrameworkElement;
+
+        return feContent?.ActualTheme == ElementTheme.Light
+            ? new DesktopAcrylicController()
+            {
+                Kind = DesktopAcrylicKind.Thin,
+                TintColor = Color.FromArgb(255, 243, 243, 243),
+                LuminosityOpacity = 0.90f,
+                TintOpacity = 0.0f,
+                FallbackColor = Color.FromArgb(255, 238, 238, 238),
+            }
+            : new DesktopAcrylicController()
+            {
+                Kind = DesktopAcrylicKind.Thin,
+                TintColor = Color.FromArgb(255, 32, 32, 32),
+                LuminosityOpacity = 0.96f,
+                TintOpacity = 0.5f,
+                FallbackColor = Color.FromArgb(255, 28, 28, 28),
+            };
+    }
+
+    private void DisposeAcrylic()
+    {
+        if (_acrylicController != null)
+        {
+            _acrylicController.Dispose();
+            _acrylicController = null!;
+            _configurationSource = null!;
+        }
+    }
+
+    internal void OnActivated(object sender, WindowActivatedEventArgs args)
+    {
+        if (_configurationSource != null)
+        {
+            _configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+        }
+    }
+}

src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Calc/CalculatorCommandProvider.cs
@@ -13,8 +13,8 @@ namespace Microsoft.CmdPal.Ext.Calc;
 
 public partial class CalculatorCommandProvider : CommandProvider
 {
-    // private readonly CalculatorTopLevelListItem calculatorCommand = new();
     private readonly ListItem _listItem = new(new CalculatorListPage()) { Subtitle = "Press = to type an equation" };
+    private readonly FallbackCalculatorItem _fallback = new();
 
     public CalculatorCommandProvider()
     {
@@ -24,6 +24,8 @@ public partial class CalculatorCommandProvider : CommandProvider
     }
 
     public override ICommandItem[] TopLevelCommands() => [_listItem];
+
+    public override IFallbackCommandItem[] FallbackCommands() => [_fallback];
 }
 
 // todo
@@ -87,15 +89,14 @@ public sealed partial class CalculatorListPage : DynamicListPage
         }
         else
         {
-            firstItem.TextToSuggest = ParseQuery(newSearch, out var result) ? result : string.Empty;
+            _copyContextCommand.Text = ParseQuery(newSearch, out var result) ? result : string.Empty;
             firstItem.Title = result;
             firstItem.Subtitle = newSearch;
-            _copyContextCommand.Text = result;
             firstItem.MoreCommands = [_copyContextMenuItem];
         }
     }
 
-    private bool ParseQuery(string equation, out string result)
+    internal static bool ParseQuery(string equation, out string result)
     {
         try
         {
@@ -127,5 +128,43 @@ public sealed partial class SaveCommand : InvokableCommand
     {
         SaveRequested?.Invoke(this, this);
         return CommandResult.KeepOpen();
+    }
+}
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "This is sample code")]
+internal sealed partial class FallbackCalculatorItem : FallbackCommandItem
+{
+    private readonly CopyTextCommand _copyCommand = new(string.Empty);
+
+    public FallbackCalculatorItem()
+        : base(new NoOpCommand())
+    {
+        Command = _copyCommand;
+        _copyCommand.Name = string.Empty;
+        Title = string.Empty;
+        Subtitle = "Type an equation...";
+        Icon = new IconInfo("\ue8ef"); // Calculator
+    }
+
+    public override void UpdateQuery(string query)
+    {
+        if (CalculatorListPage.ParseQuery(query, out var result))
+        {
+            _copyCommand.Text = result;
+            _copyCommand.Name = string.IsNullOrWhiteSpace(query) ? string.Empty : "Copy";
+            Title = result;
+
+            // we have to make the subtitle the equation,
+            // so that we will still string match the original query
+            // Otherwise, something like 1+2 will have a title of "3" and not match
+            Subtitle = query;
+        }
+        else
+        {
+            _copyCommand.Text = string.Empty;
+            _copyCommand.Name = string.Empty;
+            Title = string.Empty;
+            Subtitle = string.Empty;
+        }
     }
 }

src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.ClipboardHistory/Commands/PasteCommand.cs
@@ -62,7 +62,6 @@ internal sealed partial class PasteCommand : InvokableCommand
         HideWindow();
         ClipboardHelper.SendPasteKeyCombination();
         Clipboard.DeleteItemFromHistory(_clipboardItem.Item);
-        CommandResult.ShowToast("Pasting.");
-        return CommandResult.Dismiss();
+        return CommandResult.ShowToast("Pasting");
     }
 }

src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.ClipboardHistory/Pages/ClipboardHistoryListPage.cs
@@ -5,20 +5,13 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CmdPal.Ext.ClipboardHistory.Models;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
-using Microsoft.UI.Dispatching;
-using Microsoft.UI.Xaml;
 using Microsoft.Win32;
 using Windows.ApplicationModel.DataTransfer;
-using Windows.ApplicationModel.Store;
-using Windows.Storage.Streams;
 
 namespace Microsoft.CmdPal.Ext.ClipboardHistory.Pages;
 
@@ -29,19 +22,17 @@ internal sealed partial class ClipboardHistoryListPage : ListPage
 
     public ClipboardHistoryListPage()
     {
-        clipboardHistory = new ObservableCollection<ClipboardItem>();
+        clipboardHistory = [];
         _defaultIconPath = string.Empty;
         Icon = new("\uF0E3"); // ClipboardList icon
         Name = "Clipboard History";
+        Id = "com.microsoft.cmdpal.clipboardHistory";
         ShowDetails = true;
 
         Clipboard.HistoryChanged += TrackClipboardHistoryChanged_EventHandler;
     }
 
-    private void TrackClipboardHistoryChanged_EventHandler(object sender, ClipboardHistoryChangedEventArgs e)
-    {
-        RaiseItemsChanged(0);
-    }
+    private void TrackClipboardHistoryChanged_EventHandler(object sender, ClipboardHistoryChangedEventArgs e) => RaiseItemsChanged(0);
 
     private bool IsClipboardHistoryEnabled()
     {
@@ -82,7 +73,7 @@ internal sealed partial class ClipboardHistoryListPage : ListPage
     {
         try
         {
-            List<ClipboardItem> items = new();
+            List<ClipboardItem> items = [];
 
             if (!Clipboard.IsHistoryEnabled())
             {
@@ -114,7 +105,7 @@ internal sealed partial class ClipboardHistoryListPage : ListPage
             {
                 if (item.Item.Content.Contains(StandardDataFormats.Bitmap))
                 {
-                    RandomAccessStreamReference imageReceived = await item.Item.Content.GetBitmapAsync();
+                    var imageReceived = await item.Item.Content.GetBitmapAsync();
 
                     if (imageReceived != null)
                     {
@@ -129,8 +120,8 @@ internal sealed partial class ClipboardHistoryListPage : ListPage
         {
             // TODO GH #108 We need to figure out some logging
             // Logger.LogError("Loading clipboard history failed", ex);
-            Debug.WriteLine("Loading clipboard history failed");
-            Debug.WriteLine(ex.ToString());
+            ExtensionHost.ShowStatus(new StatusMessage() { Message = "Loading clipboard history failed", State = MessageState.Error });
+            ExtensionHost.LogMessage(ex.ToString());
         }
     }
 
@@ -139,7 +130,7 @@ internal sealed partial class ClipboardHistoryListPage : ListPage
         // https://github.com/microsoft/windows-rs/issues/317
         // Clipboard API needs to be called in STA or it
         // hangs.
-        Thread thread = new Thread(() =>
+        var thread = new Thread(() =>
         {
             var t = LoadClipboardHistoryAsync();
             t.ConfigureAwait(false);
@@ -153,18 +144,18 @@ internal sealed partial class ClipboardHistoryListPage : ListPage
     private ListItem[] GetClipboardHistoryListItems()
     {
         LoadClipboardHistoryInSTA();
-        ListItem[] listItems = new ListItem[clipboardHistory.Count];
+        List<ListItem> listItems = [];
         for (var i = 0; i < clipboardHistory.Count; i++)
         {
             var item = clipboardHistory[i];
-            listItems[i] = item.ToListItem();
+            if (item != null)
+            {
+                listItems.Add(item.ToListItem());
+            }
         }
 
-        return listItems;
+        return listItems.ToArray();
     }
 
-    public override IListItem[] GetItems()
-    {
-        return GetClipboardHistoryListItems();
-    }
+    public override IListItem[] GetItems() => GetClipboardHistoryListItems();
 }

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/PageViewModel.cs
@@ -30,13 +30,17 @@ public partial class PageViewModel : ExtensionObjectViewModel, IPageContext
 
     // This is set from the SearchBar
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(ShowSuggestion))]
     public partial string Filter { get; set; } = string.Empty;
 
     [ObservableProperty]
     public virtual partial string PlaceholderText { get; private set; } = "Type here to search...";
 
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(ShowSuggestion))]
     public virtual partial string TextToSuggest { get; protected set; } = string.Empty;
+
+    public bool ShowSuggestion => !string.IsNullOrEmpty(TextToSuggest) && TextToSuggest != Filter;
 
     [ObservableProperty]
     public partial CommandPaletteHost ExtensionHost { get; private set; }

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ToastViewModel.cs
@@ -1,0 +1,15 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.CmdPal.UI.ViewModels.Messages;
+
+namespace Microsoft.CmdPal.UI.ViewModels;
+
+public partial class ToastViewModel : ObservableObject
+{
+    [ObservableProperty]
+    public partial string ToastMessage { get; set; } = string.Empty;
+}

src/modules/cmdpal/Microsoft.CmdPal.UI/Controls/SearchBar.xaml
@@ -44,7 +44,7 @@
             BorderBrush="{ThemeResource TextFillColorSecondaryBrush}"
             BorderThickness="1"
             CornerRadius="4"
-            Visibility="{x:Bind CurrentPageViewModel.TextToSuggest, Converter={StaticResource StringNotEmptyToVisibilityConverter}, Mode=OneWay}">
+            Visibility="{x:Bind CurrentPageViewModel.ShowSuggestion, Mode=OneWay}">
             <FontIcon
                 HorizontalAlignment="Left"
                 VerticalAlignment="Center"
@@ -59,6 +59,6 @@
             VerticalAlignment="Stretch"
             Foreground="{ThemeResource TextControlPlaceholderForeground}"
             Text="{x:Bind CurrentPageViewModel.TextToSuggest, Mode=OneWay}"
-            Visibility="{x:Bind CurrentPageViewModel.TextToSuggest, Converter={StaticResource StringNotEmptyToVisibilityConverter}, Mode=OneWay}" />
+            Visibility="{x:Bind CurrentPageViewModel.ShowSuggestion, Mode=OneWay}" />
     </StackPanel>
 </UserControl>

commit 86911adb5e11f4dafc0f2bd5889b215bf87669a3

fix a 'hang' on quit (#444)
Hey guess what, you need to close all the windows.

regressed in #442

src/modules/cmdpal/Microsoft.CmdPal.UI/ToastWindow.xaml
@@ -9,6 +9,7 @@
     xmlns:ui="using:CommunityToolkit.WinUI"
     Title="Command Palette Toast"
     Activated="OnActivated"
+    Closed="OnClosed"
     mc:Ignorable="d">
     <!--<Window.SystemBackdrop>
         <MicaBackdrop />

src/modules/cmdpal/Microsoft.CmdPal.UI/ToastWindow.xaml.cs
@@ -2,8 +2,10 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using CommunityToolkit.Mvvm.Messaging;
 using CommunityToolkit.WinUI;
 using Microsoft.CmdPal.UI.ViewModels;
+using Microsoft.CmdPal.UI.ViewModels.Messages;
 using Microsoft.UI.Composition;
 using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Dispatching;
@@ -18,7 +20,8 @@ using WinRT;
 
 namespace Microsoft.CmdPal.UI;
 
-public sealed partial class ToastWindow : Window
+public sealed partial class ToastWindow : Window,
+    IRecipient<QuitMessage>
 {
     private readonly HWND _hwnd;
 
@@ -42,6 +45,8 @@ public sealed partial class ToastWindow : Window
 
         _hwnd = new HWND(WinRT.Interop.WindowNative.GetWindowHandle(this).ToInt32());
         PInvoke.EnableWindow(_hwnd, false);
+
+        WeakReferenceMessenger.Default.Register<QuitMessage>(this);
 
         SetAcrylic();
         ToastText.ActualThemeChanged += (s, e) => UpdateAcrylic();
@@ -90,6 +95,13 @@ public sealed partial class ToastWindow : Window
                 immediate: false);
         });
     }
+
+    public void Receive(QuitMessage message) =>
+        Close();
+
+    ////// Literally everything below here is for acrylic //////
+
+    internal void OnClosed(object sender, WindowEventArgs args) => DisposeAcrylic();
 
     // We want to use DesktopAcrylicKind.Thin and custom colors as this is the default material
     // other Shell surfaces are using, this cannot be set in XAML however.