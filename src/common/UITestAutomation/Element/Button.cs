8c21f794af19c14dd85fe913a45fbfcb36dbf8ed
Jerry Xu <n.xu@outlook.com>
2025-02-24 15:24:33 +0800

    Improve UITest Automation

--- src/common/UITestAutomation/Element/Button.cs
+++ src/common/UITestAutomation/Element/Button.cs
@@ -2,13 +2,6 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Appium.Windows;
-using OpenQA.Selenium.Interactions;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.Events;
-
 namespace Microsoft.PowerToys.UITest
 {
     /// <summary>
