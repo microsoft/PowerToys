a5a354a70fe2743a5f30f0ca6b60a6afa058dc58
Dave Rayment <dave.rayment@gmail.com>
2025-02-25 08:23:30 +0000

    [ImageResizer] Fix issues with blank Width and Height controls (#37373)
    * Allow custom preset's dimensions to be blank in the UI while still persisted as 0.
    
    * XAML formatting - reorder namespaces.
    
    * Add "(auto)" text to zero-value Width/Height in Settings. Ensure Width and Height fields in flyout are formatted to empty when their value is 0.

--- src/settings-ui/Settings.UI/Converters/ImageResizerDoubleToAutoConverter.cs
+++ src/settings-ui/Settings.UI/Converters/ImageResizerDoubleToAutoConverter.cs
@@ -1,25 +1,23 @@
-﻿#pragma warning disable IDE0073
-// Copyright (c) Brice Lambson
-// The Brice Lambson licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.  Code forked from Brice Lambson's https://github.com/bricelam/ImageResizer/
-#pragma warning restore IDE0073
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 using System;
 using System.Globalization;
-using System.Windows.Data;
+using Microsoft.UI.Xaml.Data;
 
-using ImageResizer.Properties;
-
-namespace ImageResizer.Views;
+namespace Microsoft.PowerToys.Settings.UI.Converters;
 
 /// <summary>
 /// Converts between double and string for text-based controls bound to Width or Height fields.
 /// Optionally returns localized "Auto" text when the underlying value is 0, letting the UI show,
 /// for example "(auto) x 1024 pixels".
 /// </summary>
-[ValueConversion(typeof(double), typeof(string))]
-internal class AutoDoubleConverter : IValueConverter
+public sealed partial class ImageResizerDoubleToAutoConverter : IValueConverter
 {
+    private static readonly string AutoText =
+        Helpers.ResourceLoaderInstance.ResourceLoader.GetString("ImageResizer_AutoText");
+
     /// <summary>
     /// Converts a double to a string, optionally showing "Auto" for 0 values. NaN values are
     /// converted to empty strings.
@@ -29,17 +27,16 @@ internal class AutoDoubleConverter : IValueConverter
     /// <param name="targetType">The conversion target type. <see cref="string"/> here.</param>
     /// <param name="parameter">Set to "Auto" to return the localized "Auto" string if the
     /// value is 0.</param>
-    /// <param name="culture">The <see cref="CultureInfo"/> to use for the number formatting.
-    /// </param>
+    /// <param name="language">Ignored.</param>
     /// <returns>The string representation of the passed-in value.</returns>
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+    public object Convert(object value, Type targetType, object parameter, string language) =>
         value switch
         {
             double d => d switch
             {
                 double.NaN => "0",
-                0 => (string)parameter == "Auto" ? Resources.Input_Auto : "0",
-                _ => d.ToString(culture),
+                0 => (string)parameter == "Auto" ? AutoText : "0",
+                _ => d.ToString(CultureInfo.CurrentCulture),
             },
 
             _ => "0",
@@ -52,13 +49,13 @@ internal class AutoDoubleConverter : IValueConverter
     /// <param name="value">The string value to convert.</param>
     /// <param name="targetType">The conversion target type. <see cref="double"/> here.</param>
     /// <param name="parameter">Converter parameter. Unused.</param>
-    /// <param name="culture">The <see cref="CultureInfo"/> to use for the text parsing.</param>
+    /// <param name="language">Ignored.</param>
     /// <returns>The corresponding double value.</returns>
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+    public object ConvertBack(object value, Type targetType, object parameter, string language) =>
         value switch
         {
-            null or "" => 0,
-            string text when double.TryParse(text, NumberStyles.Any, culture, out double result) => result,
-            _ => 0,
+            null or "" => 0.0,
+            string text when double.TryParse(text, NumberStyles.Any, CultureInfo.CurrentCulture, out double result) => result,
+            _ => 0.0,
         };
 }
