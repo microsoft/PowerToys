d891dfe91c02902fa041c9a55691a98eedad96c3
mantaionut <ionut@janeasystems.com>
2025-03-03 16:50:39 +0200

    Added system menu option on right click on toolbar. Fixed hide then show removing Mica effect

--- src/modules/colorPicker/ColorPickerUI/ColorEditorWindow.xaml.cs
+++ src/modules/colorPicker/ColorPickerUI/ColorEditorWindow.xaml.cs
@@ -3,12 +3,18 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Drawing.Printing;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
+using System.Windows.Shell;
 using ColorPicker.Helpers;
+using ControlzEx.Theming;
 using ManagedCommon;
+using Microsoft.Diagnostics.Tracing.Parsers.ClrPrivate;
+using Microsoft.Win32;
+using Windows.Graphics;
 
 namespace ColorPicker
 {
@@ -35,7 +41,16 @@ namespace ColorPicker
 
         protected override void OnSourceInitialized(EventArgs e)
         {
-            base.OnSourceInitialized(e);
+            WindowChrome.SetWindowChrome(
+                this,
+                new WindowChrome
+                {
+                CaptionHeight = 0,
+                CornerRadius = default,
+                GlassFrameThickness = new Thickness(-1),
+                ResizeBorderThickness = ResizeMode == ResizeMode.NoResize ? default : new Thickness(4),
+                UseAeroCaptionButtons = false,
+                });
             if (OSVersionHelper.IsWindows11())
             {
                 // ResizeMode="NoResize" removes rounded corners. So force them to rounded.
@@ -48,6 +63,30 @@ namespace ColorPicker
             {
                 // On Windows10 ResizeMode="NoResize" removes the border so we add a new one.
                 MainBorder.BorderThickness = new System.Windows.Thickness(0.5);
+            }
+
+            // Hide then Show with WindowStyle="None" will remove the Mica effect. So manually remove the titlebar.
+            RemoveWindowTitlebarContents();
+
+            base.OnSourceInitialized(e);
+        }
+
+        public void RemoveWindowTitlebarContents()
+        {
+            IntPtr handle = new WindowInteropHelper(GetWindow(this)).EnsureHandle();
+            if (handle == IntPtr.Zero)
+            {
+                return;
+            }
+
+            int windowStyleLong = GetWindowLong(handle, GWLSTYLE);
+            windowStyleLong &= ~(int)WindowStyles.WS_SYSMENU;
+
+            IntPtr result = SetWindowLong(handle, GWLSTYLE, windowStyleLong);
+            if (result.ToInt64() == 0)
+            {
+                int error = Marshal.GetLastWin32Error();
+                Logger.LogError($"SetWindowLong error {error}");
             }
         }
 
@@ -63,6 +102,38 @@ namespace ColorPicker
                 this.DragMove();
             }
         }
+
+        private void TitleBar_MouseRightClick(object sender, MouseButtonEventArgs e)
+        {
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+
+            // Get the mouse position relative to the screen
+            Point mousePosition = e.GetPosition(this);
+
+            Point screenPoint = PointToScreen(mousePosition);
+
+            // Display the system menu at the current mouse position
+            IntPtr hMenu = GetSystemMenu(hwnd, false);
+            if (hMenu != IntPtr.Zero)
+            {
+                int command = TrackPopupMenu(
+                    hMenu,
+                    TPMLEFTALIGN | TPMRETURNCMD,
+                    (int)screenPoint.X,
+                    (int)screenPoint.Y,
+                    0,
+                    hwnd,
+                    IntPtr.Zero);
+                if (command > 0)
+                {
+                    SendMessage(hwnd, WMSYSCOMMAND, new IntPtr(command), IntPtr.Zero);
+                }
+            }
+        }
+
+        private const int WMSYSCOMMAND = 0x0112;
+        private const int TPMLEFTALIGN = 0x0000;
+        private const int TPMRETURNCMD = 0x0100;
 
         // The enum flag for DwmSetWindowAttribute's second parameter, which tells the function what attribute to set.
         public enum DWMWINDOWATTRIBUTE
@@ -85,5 +156,28 @@ namespace ColorPicker
             DWMWINDOWATTRIBUTE attribute,
             ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute,
             uint cbAttribute);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32.dll")]
+        private static extern int TrackPopupMenu(IntPtr hMenu, int uFlags, int x, int y, int nReserved, IntPtr hWnd, IntPtr prcRect);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        public const int GWLSTYLE = -16;
+
+        [Flags]
+        public enum WindowStyles : uint
+        {
+            WS_SYSMENU = 0x00080000, // System menu (close/maximize/minimize button area)
+        }
     }
 }
