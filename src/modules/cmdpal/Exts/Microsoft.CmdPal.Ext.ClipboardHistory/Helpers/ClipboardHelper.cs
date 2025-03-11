microsoft/PowerToys

commit 013881783ae5d18c41f8110baf9dbdb961cfc580

fix handling command invokes ON THE UI THREAD (#518)
As bad as the title sounds. 

Fix is as easy as you'd think.

Closes #512.

src/modules/cmdpal/Microsoft.CmdPal.UI/Pages/ShellPage.xaml.cs
@@ -42,6 +42,9 @@ public sealed partial class ShellPage : Microsoft.UI.Xaml.Controls.Page,
 
     private readonly ToastWindow _toast = new();
 
+    private readonly Lock _invokeLock = new();
+    private Task? _handleInvokeTask;
+
     public ShellViewModel ViewModel { get; private set; } = App.Current.Services.GetService<ShellViewModel>()!;
 
     public event PropertyChangedEventHandler? PropertyChanged;
@@ -177,22 +180,51 @@ public sealed partial class ShellPage : Microsoft.UI.Xaml.Controls.Page,
             }
             else if (command is IInvokableCommand invokable)
             {
-                var result = invokable.Invoke(message.Context);
-                DispatcherQueue.TryEnqueue(() =>
-                {
-                    HandleCommandResultOnUiThread(result);
-                });
+                HandleInvokeCommand(message, invokable);
             }
         }
         catch (Exception ex)
         {
-            if (command is IPageContext page)
+            // TODO: It would be better to do this as a page exception, rather
+            // than a silent log message.
+            CommandPaletteHost.Instance.Log(ex.Message);
+        }
+    }
+
+    private void HandleInvokeCommand(PerformCommandMessage message, IInvokableCommand invokable)
+    {
+        lock (_invokeLock)
+        {
+            if (_handleInvokeTask != null)
             {
-                page.ShowException(ex);
+                // do nothing - a command is already doing a thing
             }
             else
             {
-                // TODO: Logging
+                _handleInvokeTask = Task.Run(() =>
+                {
+                    try
+                    {
+                        var result = invokable.Invoke(message.Context);
+                        DispatcherQueue.TryEnqueue(() =>
+                        {
+                            try
+                            {
+                                HandleCommandResultOnUiThread(result);
+                            }
+                            finally
+                            {
+                                _handleInvokeTask = null;
+                            }
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        // TODO: It would be better to do this as a page exception, rather
+                        // than a silent log message.
+                        CommandPaletteHost.Instance.Log(ex.Message);
+                    }
+                });
             }
         }
     }

src/modules/cmdpal/Exts/SamplePagesExtension/EvilSamplesPage.cs
@@ -61,6 +61,21 @@ public partial class EvilSamplesPage : ListPage
                HeroImage = null,
                Metadata = [new DetailsElement() { Key = "Oops all nulls", Data = new DetailsTags() { Tags = null } }],
            },
+        },
+        new ListItem(new AnonymousCommand(action: () =>
+        {
+            ToastStatusMessage toast = new("I should appear immediately");
+            toast.Show();
+            Thread.Sleep(5000);
+        }) { Result = CommandResult.KeepOpen() })
+        {
+           Title = "I take just forever to return something",
+           Subtitle = "The toast should appear immediately.",
+           MoreCommands = null,
+           Details = new Details()
+           {
+               Body = "This is a test for GH#512. If it doesn't appear immediately, it's likely InvokeCommand is happening on the UI thread.",
+           },
         }
     ];
 

src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/CommandPaletteHost.cs
@@ -183,4 +183,9 @@ public sealed partial class CommandPaletteHost : IExtensionHost
         this.ProcessLogMessage(new LogMessage(message));
 #endif
     }
+
+    public void Log(string message)
+    {
+        this.ProcessLogMessage(new LogMessage(message));
+    }
 }

commit bb40313443dc2ad7745017f4b93aa6b0e70c661c

Uhg, I broke everything (#528)
This is stuff that regressed in #518, for the large part. 

This is also about the time I discovered #525, #526 and #527

src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.ClipboardHistory/Helpers/ClipboardHelper.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.CmdPal.Ext.ClipboardHistory.Helpers;
 using Microsoft.CmdPal.Ext.ClipboardHistory.Models;
@@ -50,15 +51,21 @@ internal static class ClipboardHelper
 
     internal static void SetClipboardTextContent(string text)
     {
-        ExtensionHost.LogMessage(new LogMessage() { Message = "Copied text to clipboard" });
-
         if (!string.IsNullOrEmpty(text))
         {
             DataPackage output = new();
             output.SetText(text);
-            Clipboard.SetContentWithOptions(output, null);
-
-            Flush();
+            try
+            {
+                // Clipboard.SetContentWithOptions(output, null);
+                Clipboard.SetContent(output);
+                Flush();
+                ExtensionHost.LogMessage(new LogMessage() { Message = "Copied text to clipboard" });
+            }
+            catch (COMException ex)
+            {
+                ExtensionHost.LogMessage($"Error: {ex.HResult}\n{ex.Source}\n{ex.StackTrace}");
+            }
         }
     }
 
@@ -144,7 +151,7 @@ internal static class ClipboardHelper
     {
         var ignoreKeyEventFlag = (UIntPtr)0x5555;
 
-        NativeMethods.INPUT inputShift = new NativeMethods.INPUT
+        var inputShift = new NativeMethods.INPUT
         {
             type = NativeMethods.INPUTTYPE.INPUT_KEYBOARD,
             data = new NativeMethods.InputUnion
@@ -160,7 +167,7 @@ internal static class ClipboardHelper
             },
         };
 
-        NativeMethods.INPUT[] inputs = new NativeMethods.INPUT[] { inputShift };
+        var inputs = new NativeMethods.INPUT[] { inputShift };
         _ = NativeMethods.SendInput(1, inputs, NativeMethods.INPUT.Size);
     }
 

src/modules/cmdpal/Microsoft.CmdPal.UI/App.xaml.cs
@@ -6,7 +6,6 @@ using Microsoft.CmdPal.Common.Services;
 using Microsoft.CmdPal.Ext.Apps;
 using Microsoft.CmdPal.Ext.Bookmarks;
 using Microsoft.CmdPal.Ext.Calc;
-using Microsoft.CmdPal.Ext.ClipboardHistory;
 using Microsoft.CmdPal.Ext.Indexer;
 using Microsoft.CmdPal.Ext.Registry;
 using Microsoft.CmdPal.Ext.Shell;
@@ -88,7 +87,9 @@ public partial class App : Application
         services.AddSingleton<ICommandProvider, CalculatorCommandProvider>();
         services.AddSingleton<ICommandProvider, IndexerCommandsProvider>();
         services.AddSingleton<ICommandProvider, BookmarksCommandProvider>();
-        services.AddSingleton<ICommandProvider, ClipboardHistoryCommandsProvider>();
+
+        // TODO GH #527 re-enable the clipboard commands
+        // services.AddSingleton<ICommandProvider, ClipboardHistoryCommandsProvider>();
         services.AddSingleton<ICommandProvider, WindowWalkerCommandsProvider>();
         services.AddSingleton<ICommandProvider, WebSearchCommandsProvider>();
         services.AddSingleton<ICommandProvider>(winget);

src/modules/cmdpal/Microsoft.CmdPal.UI/Pages/ShellPage.xaml.cs
@@ -127,6 +127,7 @@ public sealed partial class ShellPage : Microsoft.UI.Xaml.Controls.Page,
 
             if (command is IPage page)
             {
+                // TODO GH #526 This needs more better locking too
                 _ = _queue.TryEnqueue(() =>
                 {
                     // Also hide our details pane about here, if we had one
@@ -193,6 +194,7 @@ public sealed partial class ShellPage : Microsoft.UI.Xaml.Controls.Page,
 
     private void HandleInvokeCommand(PerformCommandMessage message, IInvokableCommand invokable)
     {
+        // TODO GH #525 This needs more better locking.
         lock (_invokeLock)
         {
             if (_handleInvokeTask != null)
@@ -220,6 +222,8 @@ public sealed partial class ShellPage : Microsoft.UI.Xaml.Controls.Page,
                     }
                     catch (Exception ex)
                     {
+                        _handleInvokeTask = null;
+
                         // TODO: It would be better to do this as a page exception, rather
                         // than a silent log message.
                         CommandPaletteHost.Instance.Log(ex.Message);

src/modules/cmdpal/exts/Microsoft.CmdPal.Ext.Indexer/FallbackOpenFileItem.cs
@@ -41,6 +41,7 @@ internal sealed partial class FallbackOpenFileItem : FallbackCommandItem
         {
             Title = string.Empty;
             Subtitle = string.Empty;
+            Command = new NoOpCommand();
         }
     }
 }