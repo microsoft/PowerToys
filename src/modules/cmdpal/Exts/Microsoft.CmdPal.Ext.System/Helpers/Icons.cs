e6acb4deb62e0746231af6a49c6842438e1f672d
moooyo <42196638+moooyo@users.noreply.github.com>
2025-03-19 02:26:33 +0800

    Migrate system commands extension (#452)
    ## Summary of the Pull Request
    
    
    Ports the PT System commands to CmdPal. 
    
    Tooltips have been replaced with Details on those list items. 
    
    CommandResult.Confirm was used for most of the MessageBox usages (the only remaining one is after the recycle bin is emptied)
    
    https://github.com/user-attachments/assets/5b849f56-8346-4a6d-9ecb-13757aed738a
    
    
    ## PR Checklist
    
    - [x] **Closes:** #384 
    
    ---------
    
    Co-authored-by: Yu Leng (from Dev Box) <yuleng@microsoft.com>

---
+++ src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.System/Helpers/Icons.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace Microsoft.CmdPal.Ext.System.Helpers;
+
+public static partial class Icons
+{
+    public static IconInfo FirmwareSettingsIcon { get; } = IconHelpers.FromRelativePaths("Microsoft.CmdPal.Ext.System\\Assets\\logoff.light.png", "Microsoft.CmdPal.Ext.System\\Assets\\logoff.dark.png");
+
+    public static IconInfo LockIcon { get; } = new IconInfo("\uE72E");
+
+    public static IconInfo LogoffIcon { get; } = IconHelpers.FromRelativePaths("Microsoft.CmdPal.Ext.System\\Assets\\logoff.light.png", "Microsoft.CmdPal.Ext.System\\Assets\\logoff.dark.png");
+
+    public static IconInfo NetworkAdapterIcon { get; } = new IconInfo("\uEDA3");
+
+    public static IconInfo RecycleBinIcon { get; } = new IconInfo("\uE74D");
+
+    public static IconInfo RestartIcon { get; } = new IconInfo("\uE777");
+
+    public static IconInfo ShutdownIcon { get; } = new IconInfo("\uE7E8");
+
+    public static IconInfo SleepIcon { get; } = IconHelpers.FromRelativePaths("Microsoft.CmdPal.Ext.System\\Assets\\sleep.light.png", "Microsoft.CmdPal.Ext.System\\Assets\\sleep.dark.png");
+}
