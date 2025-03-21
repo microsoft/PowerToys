38c19740c0fab1da7e2221df7933c9ef4e114144
Mike Griese <migrie@microsoft.com>
2025-03-21 14:24:25 -0500

    hope it works

--- src/modules/cmdpal/ExtensionTemplate/TemplateCmdPalExtension/TemplateCmdPalExtension/Program.cs
+++ src/modules/cmdpal/ExtensionTemplate/TemplateCmdPalExtension/TemplateCmdPalExtension/Program.cs
@@ -5,6 +5,8 @@
 using System;
 using System.Threading;
 using Microsoft.CommandPalette.Extensions;
+using Shmuelie.WinRTServer;
+using Shmuelie.WinRTServer.CsWinRT;
 
 namespace TemplateCmdPalExtension;
 
@@ -15,15 +17,16 @@ public class Program
     {
         if (args.Length > 0 && args[0] == "-RegisterProcessAsComServer")
         {
-            using ExtensionServer server = new();
-            var extensionDisposedEvent = new ManualResetEvent(false);
-            var extensionInstance = new TemplateCmdPalExtension(extensionDisposedEvent);
-
+            await using global::Shmuelie.WinRTServer.ComServer server = new();
+            ManualResetEvent extensionDisposedEvent = new(false);
+            
             // We are instantiating an extension instance once above, and returning it every time the callback in RegisterExtension below is called.
             // This makes sure that only one instance of SampleExtension is alive, which is returned every time the host asks for the IExtension object.
             // If you want to instantiate a new instance each time the host asks, create the new instance inside the delegate.
-            server.RegisterExtension(() => extensionInstance);
-
+            TemplateCmdPalExtension extensionInstance = new(extensionDisposedEvent);
+            server.RegisterClass<DocsSample, IExtension>(() => extensionInstance);
+            server.Start();
+            
             // This will make the main thread wait until the event is signalled by the extension class.
             // Since we have single instance of the extension object, we exit as soon as it is disposed.
             extensionDisposedEvent.WaitOne();
