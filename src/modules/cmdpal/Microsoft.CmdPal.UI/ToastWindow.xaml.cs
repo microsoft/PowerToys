16aef58c14b49ea9d6ed04670896b0ea362fd701
vanzue <13770338596@163.com>
2025-03-28 16:32:22 +0800

    minor fix

--- src/modules/cmdpal/Microsoft.CmdPal.UI/ToastWindow.xaml.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/ToastWindow.xaml.cs
@@ -2,7 +2,6 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
 using CommunityToolkit.Mvvm.Messaging;
 using CommunityToolkit.WinUI;
 using ManagedCommon;
