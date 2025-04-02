6f2843b7a467c696122f333728b2eb9b5275139d
Leilei Zhang <leilzh@microsoft.com>
2025-04-02 19:05:58 +0800

    [ImageResizer]Fix: Deleting an Image Resizer preset deletes the wrong preset

--- src/settings-ui/Settings.UI/ViewModels/ImageResizerViewModel.cs
+++ src/settings-ui/Settings.UI/ViewModels/ImageResizerViewModel.cs
@@ -67,6 +67,7 @@ public partial class ImageResizerViewModel : Observable
         try
         {
             Settings = _settingsUtils.GetSettings<ImageResizerSettings>(ModuleName);
+            ImageResizerHelper.RecoverInvalidIds(Settings.Properties.ImageresizerSizes.Value);
         }
         catch (Exception e)
         {
