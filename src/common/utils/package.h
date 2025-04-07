d90b738c8502493a1172a0b98f229899fe7af123
vanzue <13770338596@163.com>
2025-04-07 16:03:16 +0800

    Com resource handle and do try catch for package metadata

--- src/common/utils/package.h
+++ src/common/utils/package.h
@@ -58,68 +58,108 @@ namespace package
         UINT16 Revision;
     };
 
+    class ComInitializer
+    {
+    public:
+        explicit ComInitializer(DWORD coInitFlags = COINIT_MULTITHREADED) :
+            _initialized(false)
+        {
+            const HRESULT hr = CoInitializeEx(nullptr, coInitFlags);
+            _initialized = SUCCEEDED(hr);
+        }
+
+        ~ComInitializer()
+        {
+            if (_initialized)
+            {
+                CoUninitialize();
+            }
+        }
+
+        bool Succeeded() const { return _initialized; }
+
+    private:
+        bool _initialized;
+    };
+
     inline bool GetPackageNameAndVersionFromAppx(
         const std::wstring& appxPath,
         std::wstring& outName,
         PACKAGE_VERSION& outVersion)
     {
-        ComPtr<IAppxFactory> factory;
-        ComPtr<IStream> stream;
-        ComPtr<IAppxPackageReader> reader;
-        ComPtr<IAppxManifestReader> manifest;
-        ComPtr<IAppxManifestPackageId> packageId;
+        try
+        {
+            ComInitializer comInit;
+            if (!comInit.Succeeded())
+            {
+                Logger::error(L"COM initialization failed.");
+                return false;
+            }
 
-        HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
-        if (FAILED(hr))
+            ComPtr<IAppxFactory> factory;
+            ComPtr<IStream> stream;
+            ComPtr<IAppxPackageReader> reader;
+            ComPtr<IAppxManifestReader> manifest;
+            ComPtr<IAppxManifestPackageId> packageId;
+
+            HRESULT hr = CoCreateInstance(__uuidof(AppxFactory), nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&factory));
+            if (FAILED(hr))
+                return false;
+
+            hr = SHCreateStreamOnFileEx(appxPath.c_str(), STGM_READ | STGM_SHARE_DENY_WRITE, FILE_ATTRIBUTE_NORMAL, FALSE, nullptr, &stream);
+            if (FAILED(hr))
+                return false;
+
+            hr = factory->CreatePackageReader(stream.Get(), &reader);
+            if (FAILED(hr))
+                return false;
+
+            hr = reader->GetManifest(&manifest);
+            if (FAILED(hr))
+                return false;
+
+            hr = manifest->GetPackageId(&packageId);
+            if (FAILED(hr))
+                return false;
+
+            LPWSTR name = nullptr;
+            hr = packageId->GetName(&name);
+            if (FAILED(hr))
+                return false;
+
+            UINT64 version = 0;
+            hr = packageId->GetVersion(&version);
+            if (FAILED(hr))
+                return false;
+
+            outName = std::wstring(name);
+            CoTaskMemFree(name);
+
+            outVersion.Major = static_cast<UINT16>((version >> 48) & 0xFFFF);
+            outVersion.Minor = static_cast<UINT16>((version >> 32) & 0xFFFF);
+            outVersion.Build = static_cast<UINT16>((version >> 16) & 0xFFFF);
+            outVersion.Revision = static_cast<UINT16>(version & 0xFFFF);
+
+            Logger::info(L"Package name: {}, version: {}.{}.{}.{}, appxPath: {}",
+                         outName,
+                         outVersion.Major,
+                         outVersion.Minor,
+                         outVersion.Build,
+                         outVersion.Revision,
+                         appxPath);
+
+            return true;
+        }
+        catch (const std::exception& ex)
+        {
+            Logger::error(L"Standard exception: {}", winrt::to_hstring(ex.what()));
             return false;
-
-        hr = CoCreateInstance(__uuidof(AppxFactory), nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&factory));
-        if (FAILED(hr))
+        }
+        catch (...)
+        {
+            Logger::error(L"Unknown or non-standard exception occurred.");
             return false;
-
-        hr = SHCreateStreamOnFileEx(appxPath.c_str(), STGM_READ | STGM_SHARE_DENY_WRITE, FILE_ATTRIBUTE_NORMAL, FALSE, nullptr, &stream);
-        if (FAILED(hr))
-            return false;
-
-        hr = factory->CreatePackageReader(stream.Get(), &reader);
-        if (FAILED(hr))
-            return false;
-
-        hr = reader->GetManifest(&manifest);
-        if (FAILED(hr))
-            return false;
-
-        hr = manifest->GetPackageId(&packageId);
-        if (FAILED(hr))
-            return false;
-
-        LPWSTR name = nullptr;
-        hr = packageId->GetName(&name);
-        if (FAILED(hr))
-            return false;
-
-        UINT64 version = 0;
-        hr = packageId->GetVersion(&version);
-        if (FAILED(hr))
-            return false;
-
-        outName = std::wstring(name);
-        CoTaskMemFree(name);
-
-        outVersion.Major = static_cast<UINT16>((version >> 48) & 0xFFFF);
-        outVersion.Minor = static_cast<UINT16>((version >> 32) & 0xFFFF);
-        outVersion.Build = static_cast<UINT16>((version >> 16) & 0xFFFF);
-        outVersion.Revision = static_cast<UINT16>(version & 0xFFFF);
-
-        Logger::info(L"Package name: {}, version: {}.{}.{}.{}, appxPath: {}",
-                     outName,
-                     outVersion.Major,
-                     outVersion.Minor,
-                     outVersion.Build,
-                     outVersion.Revision,
-                     appxPath);
-        CoUninitialize();
-        return true;
+        }
     }
 
     inline std::optional<Package> GetRegisteredPackage(std::wstring packageDisplayName, bool checkVersion)
