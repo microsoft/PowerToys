b74f2b78414a7647d65afb287e59b5aa826e6430
vanzue <13770338596@163.com>
2025-04-09 13:33:11 +0800

    remove unnecessary string

--- src/modules/Workspaces/WorkspacesLib/SteamHelper.h
+++ src/modules/Workspaces/WorkspacesLib/SteamHelper.h
@@ -6,8 +6,6 @@ namespace Utils
 {
     namespace NonLocalizable
     {
-        // When steam is running, window process's exe is this.
-        const std::wstring SteamWindowExePath = L"steamwebhelper.exe";
         const std::wstring AcfFileNameTemplate = L"appmanifest_<gameid>.acfs";
     }
 
