f334fb924b08367f54b6cd929bb5f7812caa278a
Antonín Procházka <prochazka@tocna.info>
2025-04-11 10:02:19 +0200

    Peek file using command line and named pipe. (#26422)

---
+++ src/modules/peek/Peek.CLI/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+
+if (args.Length < 1)
+{
+    Console.WriteLine("Usage: PeekPipeClient.exe <path>");
+    return;
+}
+
+using var pipe = new NamedPipeClientStream(".", "PeekPipe", PipeDirection.Out);
+pipe.Connect();
+
+using var writer = new StreamWriter(pipe);
+
+writer.WriteLine(args[0]);
+writer.WriteLine(@"C:\src\wgis\WGIS\cetin\lk\package.json");
+writer.Flush();
+writer.Close();
+pipe.Close();
