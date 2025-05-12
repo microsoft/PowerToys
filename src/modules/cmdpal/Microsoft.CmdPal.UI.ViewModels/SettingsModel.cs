ee5f7dfdfc610a3f6c1e152cbd709ad58568ad2e
Shawn Yuan <shuaiyuan@microsoft.com>
2025-05-12 15:37:42 +0800

    code clean
    Signed-off-by: Shawn Yuan <shuaiyuan@microsoft.com>

--- src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/SettingsModel.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/SettingsModel.cs
@@ -40,8 +40,6 @@ public partial class SettingsModel : ObservableObject
 
     public bool ShowSystemTrayIcon { get; set; } = true;
 
-    public bool OpenWindowInLastPos { get; set; }
-
     public bool IgnoreShortcutWhenFullscreen { get; set; } = true;
 
     public Dictionary<string, ProviderSettings> ProviderSettings { get; set; } = [];
