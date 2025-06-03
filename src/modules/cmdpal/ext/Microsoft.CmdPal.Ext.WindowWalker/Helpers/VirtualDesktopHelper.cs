4312d0868bcec717a84622da1c2fe3ea85f63749
Yu Leng <42196638+moooyo@users.noreply.github.com>
2025-06-03 14:24:13 +0800

    [cmdpal][AOT] make some built-in extensions able to be published with native AOT enabled (#39802)
    <!-- Enter a brief description/summary of your PR here. What does it
    fix/what does it change/how was it tested (even manually, if necessary)?
    -->
    ## Summary of the Pull Request
    Base on my test, some built-in extensions can be published with native
    AOT enabled with a little changes. (see this branch:
    https://github.com/microsoft/PowerToys/pull/39605).
    
    1. SystemCommands: no change need. Removed some unused code.
    2. WinGet: disable marshalling. and do a little changes in
    CreateInstance.
    3. WindowWalker: use source generation to call CoCreateInstance.
    4. WindowsTerminal: use source generation to call
    IApplicationActivationManager interface.
    
    <!-- Please review the items on the PR checklist before submitting-->
    ## PR Checklist
    
    - [x] **Closes:** #39869
    - [x] **Communication:** I've discussed this with core contributors
    already. If work hasn't been agreed, this work might be rejected
    - [x] **Tests:** Added/updated and all pass
    - [x] **Localization:** All end user facing strings can be localized
    - [ ] **Dev docs:** Added/updated
    - [ ] **New binaries:** Added on the required places
    - [ ] [JSON for
    signing](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ESRPSigning_core.json)
    for new binaries
    - [ ] [WXS for
    installer](https://github.com/microsoft/PowerToys/blob/main/installer/PowerToysSetup/Product.wxs)
    for new binaries and localization folder
    - [ ] [YML for CI
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ci/templates/build-powertoys-steps.yml)
    for new test projects
    - [ ] [YML for signed
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/release.yml)
    - [ ] **Documentation updated:** If checked, please file a pull request
    on [our docs
    repo](https://github.com/MicrosoftDocs/windows-uwp/tree/docs/hub/powertoys)
    and link it here: #xxx
    
    <!-- Provide a more detailed description of the PR, other things fixed
    or any additional comments/features here -->
    ## Detailed Description of the Pull Request / Additional comments
    
    <!-- Describe how you validated the behavior. Add automated tests
    wherever possible, but list manual validation steps taken as well -->
    ## Validation Steps Performed
    
    ---------
    
    Co-authored-by: Yu Leng <yuleng@microsoft.com>

--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.WindowWalker/Helpers/VirtualDesktopHelper.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.WindowWalker/Helpers/VirtualDesktopHelper.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 
 using Microsoft.CmdPal.Ext.WindowWalker.Properties;
@@ -48,6 +49,10 @@ public class VirtualDesktopHelper
     /// </summary>
     private readonly List<Guid> _availableDesktops = [];
 
+#pragma warning disable SA1306 // Field names should begin with lower-case letter
+    private readonly uint CLSCTXINPROCALL = 0x17;
+#pragma warning restore SA1306 // Field names should begin with lower-case letter
+
     /// <summary>
     /// Id of the current visible Desktop.
     /// </summary>
@@ -55,15 +60,27 @@ public class VirtualDesktopHelper
 
     private static readonly CompositeFormat VirtualDesktopHelperDesktop = System.Text.CompositeFormat.Parse(Properties.Resources.VirtualDesktopHelper_Desktop);
 
+    private Guid iVirtualDesktopManagerCLSID = new("aa509086-5ca9-4c25-8f95-589d3c07b48a");
+
+    private Guid iVirtualDesktopManagerIID = new("a5cd92ff-29be-454c-8d04-d82879fb3f1b");
+
     /// <summary>
     /// Initializes a new instance of the <see cref="VirtualDesktopHelper"/> class.
     /// </summary>
     /// <param name="desktopListUpdate">Setting to configure if the list of available desktops should update automatically or only when calling <see cref="UpdateDesktopList"/>. Per default this is set to manual update (false) to have less registry queries.</param>
     public VirtualDesktopHelper(bool desktopListUpdate = false)
     {
+        var cw = new StrategyBasedComWrappers();
+
         try
         {
-            _virtualDesktopManager = (IVirtualDesktopManager)new CVirtualDesktopManager();
+            var hr = NativeMethods.CoCreateInstance(ref this.iVirtualDesktopManagerCLSID, nint.Zero, CLSCTXINPROCALL, ref iVirtualDesktopManagerIID, out var virtualDesktopManagerPtr);
+            if (hr != 0)
+            {
+                throw new ArgumentException($"Failed to create IVirtualDesktopManager instance. HR: 0x{hr:X}");
+            }
+
+            _virtualDesktopManager = (IVirtualDesktopManager)cw.GetOrCreateObjectForComInstance(virtualDesktopManagerPtr, CreateObjectFlags.None);
         }
         catch (COMException ex)
         {
@@ -409,7 +426,7 @@ public class VirtualDesktopHelper
     /// <param name="hWindow">Handle of the top level window.</param>
     /// <param name="desktopId">Guid of the target desktop.</param>
     /// <returns><see langword="True"/> on success and <see langword="false"/> on failure.</returns>
-    public bool MoveWindowToDesktop(IntPtr hWindow, in Guid desktopId)
+    public bool MoveWindowToDesktop(IntPtr hWindow, ref Guid desktopId)
     {
         if (_virtualDesktopManager == null)
         {
@@ -417,7 +434,7 @@ public class VirtualDesktopHelper
             return false;
         }
 
-        var hr = _virtualDesktopManager.MoveWindowToDesktop(hWindow, desktopId);
+        var hr = _virtualDesktopManager.MoveWindowToDesktop(hWindow, ref desktopId);
         if (hr != (int)HRESULT.S_OK)
         {
             ExtensionHost.LogMessage(new LogMessage() { Message = "VirtualDesktopHelper.MoveWindowToDesktop() failed: An exception was thrown when moving the window ({hWindow}) to another desktop ({desktopId})." });
@@ -455,7 +472,7 @@ public class VirtualDesktopHelper
         }
 
         Guid newDesktop = _availableDesktops[windowDesktopNumber - 1];
-        return MoveWindowToDesktop(hWindow, newDesktop);
+        return MoveWindowToDesktop(hWindow, ref newDesktop);
     }
 
     /// <summary>
@@ -486,7 +503,7 @@ public class VirtualDesktopHelper
         }
 
         Guid newDesktop = _availableDesktops[windowDesktopNumber + 1];
-        return MoveWindowToDesktop(hWindow, newDesktop);
+        return MoveWindowToDesktop(hWindow, ref newDesktop);
     }
 
     /// <summary>
