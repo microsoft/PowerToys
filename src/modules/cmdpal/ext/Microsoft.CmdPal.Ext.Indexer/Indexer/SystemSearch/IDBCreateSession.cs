0403b47cbd9e787f340719014667162fd44624a1
Yu Leng <42196638+moooyo@users.noreply.github.com>
2025-06-04 17:15:55 +0800

    [CmdPal][AOT] Make fileSearch ext become AOT compatible (#39732)
    <!-- Enter a brief description/summary of your PR here. What does it
    fix/what does it change/how was it tested (even manually, if necessary)?
    -->
    ## Summary of the Pull Request
    * Use source generation to replace some csWin32 function call.
    * Clean up some AOT compatible issues.
    
    <!-- Please review the items on the PR checklist before submitting-->
    ## PR Checklist
    
    - [x] **Closes:** #39889
    - [x] **Communication:** I've discussed this with core contributors
    already. If work hasn't been agreed, this work might be rejected
    - [x] **Tests:** Added/updated and all pass
    - [x] **Localization:** All end user facing strings can be localized
    - [ ] **Dev docs:** Added/updated
    - [ ] **New binaries:** Added on the required places
    - [ ] [JSON for
    signing](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ESRPSigning_core.json)
    for new binaries
    - [ ] [WXS for
    installer](https://github.com/microsoft/PowerToys/blob/main/installer/PowerToysSetup/Product.wxs)
    for new binaries and localization folder
    - [ ] [YML for CI
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ci/templates/build-powertoys-steps.yml)
    for new test projects
    - [ ] [YML for signed
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/release.yml)
    - [ ] **Documentation updated:** If checked, please file a pull request
    on [our docs
    repo](https://github.com/MicrosoftDocs/windows-uwp/tree/docs/hub/powertoys)
    and link it here: #xxx
    
    <!-- Provide a more detailed description of the PR, other things fixed
    or any additional comments/features here -->
    ## Detailed Description of the Pull Request / Additional comments
    
    <!-- Describe how you validated the behavior. Add automated tests
    wherever possible, but list manual validation steps taken as well -->
    ## Validation Steps Performed
    
    If you want to try it out, please see
    https://github.com/microsoft/PowerToys/pull/39605
    
    ---------
    
    Co-authored-by: Yu Leng <yuleng@microsoft.com>

---
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Indexer/Indexer/SystemSearch/IDBCreateSession.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.CmdPal.Ext.Indexer.Indexer.SystemSearch;
+
+[Guid("0C733A5D-2A1C-11CE-ADE5-00AA0044773D")]
+[GeneratedComInterface(StringMarshalling = StringMarshalling.Utf16)]
+public partial interface IDBCreateSession
+{
+    void CreateSession(IntPtr pUnkOuter, ref Guid riid, [MarshalAs(UnmanagedType.Interface)] out object ppDBSession);
+}
