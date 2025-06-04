9f83fcee0d10957416cd6bbb586a10815a09df48
leileizhang <leilzh@microsoft.com>
2025-06-04 19:30:11 +0800

    [AOT][CmdPal] Enable NativeAOT Compatibility for CmdPal Apps Extension (#39678)
    <!-- Enter a brief description/summary of your PR here. What does it
    fix/what does it change/how was it tested (even manually, if necessary)?
    -->
    ## Summary of the Pull Request
    
    This PR introduces necessary changes to make the CmdPal.Apps extension
    compatible with NativeAOT publishing. The main updates include:
    
    1. Project configuration updates: Added NativeAOT-related properties to
    the .csproj.
    
    2. Native interop adjustments:
    
    > - Introduced NativeMethods.json and used
    [CsWin32](https://github.com/microsoft/cswin32) to generate P/Invoke
    bindings.
    > - Replaced some DllImport declarations with source-generated
    [LibraryImport] for improved AOT support.

---
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Apps/Utils/ComFreeHelper.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Com;
+
+namespace Microsoft.CmdPal.Ext.Apps.Utils;
+
+public static class ComFreeHelper
+{
+    internal static unsafe string GetStringAndFree(HRESULT hr, PWSTR ptr)
+    {
+        hr.ThrowOnFailure();
+        try
+        {
+            return ptr.ToString();
+        }
+        finally
+        {
+            PInvoke.CoTaskMemFree(ptr);
+        }
+    }
+
+    public static unsafe void ComObjectRelease<T>(T* comPtr)
+        where T : unmanaged
+    {
+        if (comPtr != null)
+        {
+            ((IUnknown*)comPtr)->Release();
+        }
+    }
+}
