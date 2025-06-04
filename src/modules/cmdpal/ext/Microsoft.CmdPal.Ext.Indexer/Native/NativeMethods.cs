d5175040ba48bc8a66ff26dc953f1b3e7f82ce8d
Yu Leng <yuleng@microsoft.com>
2025-06-04 10:48:36 +0800

    Use csWin32 to generate SHOW_WINDOW_CMD define

--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Indexer/Native/NativeMethods.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Indexer/Native/NativeMethods.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
+using Windows.Win32.UI.Shell;
 
 namespace Microsoft.CmdPal.Ext.Indexer.Native;
 
@@ -42,33 +43,5 @@ public sealed partial class NativeMethods
         public uint dwHotKey;
         public IntPtr hIconOrMonitor;
         public IntPtr hProcess;
-    }
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1069:Enums values should not be duplicated", Justification = "No we don't need to fix it")]
-    public enum SHOW_WINDOW_CMD : int
-    {
-        SW_FORCEMINIMIZE = 0x0000000B,
-        SW_HIDE = 0x00000000,
-        SW_MAXIMIZE = 0x00000003,
-        SW_MINIMIZE = 0x00000006,
-        SW_RESTORE = 0x00000009,
-        SW_SHOW = 0x00000005,
-        SW_SHOWDEFAULT = 0x0000000A,
-        SW_SHOWMAXIMIZED = 0x00000003,
-        SW_SHOWMINIMIZED = 0x00000002,
-        SW_SHOWMINNOACTIVE = 0x00000007,
-        SW_SHOWNA = 0x00000008,
-        SW_SHOWNOACTIVATE = 0x00000004,
-        SW_SHOWNORMAL = 0x00000001,
-        SW_NORMAL = 0x00000001,
-        SW_MAX = 0x0000000B,
-        SW_PARENTCLOSING = 0x00000001,
-        SW_OTHERZOOM = 0x00000002,
-        SW_PARENTOPENING = 0x00000003,
-        SW_OTHERUNZOOM = 0x00000004,
-        SW_SCROLLCHILDREN = 0x00000001,
-        SW_INVALIDATE = 0x00000002,
-        SW_ERASE = 0x00000004,
-        SW_SMOOTHSCROLL = 0x00000010,
     }
 }
