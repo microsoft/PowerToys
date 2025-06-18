770fe20f2cc46e201bd3b8a5ab192f45f1f903aa
Yu Leng <yuleng@microsoft.com>
2025-06-18 17:33:06 +0800

    Improve comHelper

--- src/common/ManagedCsWin32/ComHelper.cs
+++ src/common/ManagedCsWin32/ComHelper.cs
@@ -3,23 +3,17 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ManagedCsWin32;
 
 public static class ComHelper
 {
+    private static StrategyBasedComWrappers cw = new StrategyBasedComWrappers();
+
     public static T CreateComInstance<T>(ref Guid clsID, ref Guid iID, CLSCTX rclsCtx)
     {
-        var cw = new StrategyBasedComWrappers();
-
         var hr = Ole32.CoCreateInstance(ref clsID, IntPtr.Zero, rclsCtx, ref iID, out IntPtr comPtr);
         if (hr != 0)
         {
@@ -31,14 +25,19 @@ public static class ComHelper
             throw new ArgumentException($"Failed to create {typeof(T).Name} instance. CoCreateInstance return null ptr.");
         }
 
-        var comObject = cw.GetOrCreateObjectForComInstance(comPtr, CreateObjectFlags.None);
-
-        Marshal.Release(comPtr);
-        if (comObject == null)
+        try
         {
-            throw new ArgumentException($"Failed to create {typeof(T).Name} instance. Cast error.");
-        }
+            var comObject = cw.GetOrCreateObjectForComInstance(comPtr, CreateObjectFlags.None);
+            if (comObject == null)
+            {
+                throw new ArgumentException($"Failed to create {typeof(T).Name} instance. Cast error.");
+            }
 
-        return (T)comObject;
+            return (T)comObject;
+        }
+        finally
+        {
+            Marshal.Release(comPtr);
+        }
     }
 }
