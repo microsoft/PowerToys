4737ec987ef9b6ecc391a9582b8a5a78c364bf56
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2025-06-18 01:34:26 -0700

    Added basic support for Windows App Actions. (#39927)
    ## Summary of the Pull Request
    Adds basic support for finding, listing, and executing Windows App
    Actions on files found by the Microsoft.CmdPal.Ext.Indexer extension.
    
    ## PR Checklist
    
    - [X] **Closes:** #39926
    - [X] **Communication:** I've discussed this with core contributors
    already. If work hasn't been agreed, this work might be rejected
    - [ ] **Tests:** Added/updated and all pass
    - [X] **Localization:** All end user facing strings can be localized
    - [ ] **Dev docs:** Added/updated
    - [ ] **New binaries:** Added on the required places
    - [ ] [JSON for
    signing](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ESRPSigning_core.json)
    for new binaries
    - [ ] [WXS for
    installer](https://github.com/microsoft/PowerToys/blob/main/installer/PowerToysSetup/Product.wxs)
    for new binaries and localization folder
    - [ ] [YML for CI
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ci/templates/build-powertoys-steps.yml)
    for new test projects
    - [ ] [YML for signed
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/release.yml)
    - [ ] **Documentation updated:** If checked, please file a pull request
    on [our docs
    repo](https://github.com/MicrosoftDocs/windows-uwp/tree/docs/hub/powertoys)
    and link it here: #xxx
    
    ## Detailed Description of the Pull Request / Additional comments
    We also update cswin32 to stable version.
    
    <!-- Describe how you validated the behavior. Add automated tests
    wherever possible, but list manual validation steps taken as well -->
    ## Validation Steps Performed
    Validated that it doesn't show on older versions of Windows (<26100
    insiders) and that it does work on newer version that have the App
    Actions runtime.
    
    ---------
    
    Co-authored-by: Gordon Lam (SH) <yeelam@microsoft.com>
    Co-authored-by: Mike Griese <migrie@microsoft.com>
    Co-authored-by: Leilei Zhang <leilzh@microsoft.com>

---
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Indexer/Actions/ActionRuntimeFactory.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Microsoft.CmdPal.Ext.Indexer.Native;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Com;
+using WinRT;
+using WinRT.Interop;
+
+namespace Microsoft.CmdPal.Ext.Indexer.Data;
+
+internal static class ActionRuntimeFactory
+{
+    private const string ActionRuntimeClsidStr = "C36FEF7E-35F3-4192-9F2C-AF1FD425FB85";
+
+    // typeof(Windows.AI.Actions.IActionRuntime).GUID
+    private static readonly Guid IActionRuntimeIID = Guid.Parse("206EFA2C-C909-508A-B4B0-9482BE96DB9C");
+
+    public static unsafe global::Windows.AI.Actions.ActionRuntime CreateActionRuntime()
+    {
+        IntPtr abiPtr = default;
+        try
+        {
+            Guid classId = Guid.Parse(ActionRuntimeClsidStr);
+            Guid iid = IActionRuntimeIID;
+
+            var hresult = NativeMethods.CoCreateInstance(ref Unsafe.AsRef(in classId), IntPtr.Zero, NativeHelpers.CLSCTXLOCALSERVER, ref iid, out abiPtr);
+            Marshal.ThrowExceptionForHR((int)hresult);
+
+            return MarshalInterface<global::Windows.AI.Actions.ActionRuntime>.FromAbi(abiPtr);
+        }
+        finally
+        {
+            MarshalInspectable<object>.DisposeAbi(abiPtr);
+        }
+    }
+}
