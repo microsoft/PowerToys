250be2ddf07063b3d0dd19e80b09b8f2bfd77946
Yu Leng <42196638+moooyo@users.noreply.github.com>
2025-06-20 14:10:17 +0800

    [AOT] Fix issues when we want to log some telemetry in AOT version (#40148)
    <!-- Enter a brief description/summary of your PR here. What does it
    fix/what does it change/how was it tested (even manually, if necessary)?
    -->
    ## Summary of the Pull Request
    Telemetry.write will serialize all of the whole object graph. So we need
    to ensure it will not be trimmed.
    
    This is a safe PR because it doesn't apply any logic change. If you
    disable the AOT feature, this PR won't take any effect.
    
    <!-- Please review the items on the PR checklist before submitting-->
    ## PR Checklist
    
    - [x] **Closes:** #40149
    - [x] **Communication:** I've discussed this with core contributors
    already. If the work hasn't been agreed, this work might be rejected
    - [x] **Tests:** Added/updated and all pass
    - [ ] **Localization:** All end-user-facing strings can be localized
    - [ ] **Dev docs:** Added/updated
    - [ ] **New binaries:** Added on the required places
    - [ ] [JSON for
    signing](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ESRPSigning_core.json)
    for new binaries
    - [ ] [WXS for
    installer](https://github.com/microsoft/PowerToys/blob/main/installer/PowerToysSetup/Product.wxs)
    for new binaries and localization folder
    - [ ] [YML for CI
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ci/templates/build-powertoys-steps.yml)
    for new test projects
    - [ ] [YML for signed
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/release.yml)
    - [ ] **Documentation updated:** If checked, please file a pull request
    on [our docs
    repo](https://github.com/MicrosoftDocs/windows-uwp/tree/docs/hub/powertoys)
    and link it here: #xxx
    
    <!-- Provide a more detailed description of the PR, other things fixed,
    or any additional comments/features here -->
    ## Detailed Description of the Pull Request / Additional comments
    
    <!-- Describe how you validated the behavior. Add automated tests
    wherever possible, but list manual validation steps taken as well -->
    ## Validation Steps Performed
    
    ---------
    
    Co-authored-by: Yu Leng <yuleng@microsoft.com>

--- src/modules/cmdpal/Microsoft.CmdPal.UI/Events/CmdPalInvokeResult.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Events/CmdPalInvokeResult.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Tracing;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.PowerToys.Telemetry;
@@ -10,6 +11,7 @@ using Microsoft.PowerToys.Telemetry.Events;
 namespace Microsoft.CmdPal.UI.Events;
 
 [EventData]
+[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)]
 public class CmdPalInvokeResult : EventBase, IEvent
 {
     public string ResultKind { get; set; }
