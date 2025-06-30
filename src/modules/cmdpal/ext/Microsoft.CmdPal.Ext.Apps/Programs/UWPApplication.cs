microsoft/PowerToys

commit 79958975b4d3d708ec69e467ece39b216f0ea833

[AOT][CmdPal] Enable NativeAOT Compatibility for CmdPal Apps Extension (#39678)
<!-- Enter a brief description/summary of your PR here. What does it
fix/what does it change/how was it tested (even manually, if necessary)?
-->
## Summary of the Pull Request

This PR introduces necessary changes to make the CmdPal.Apps extension
compatible with NativeAOT publishing. The main updates include:

1. Project configuration updates: Added NativeAOT-related properties to
the .csproj.

2. Native interop adjustments:

> - Introduced NativeMethods.json and used
[CsWin32](https://github.com/microsoft/cswin32) to generate P/Invoke
bindings.
> - Replaced some DllImport declarations with source-generated
[LibraryImport] for improved AOT support.

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Apps/Programs/UWPApplication.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Xml;
 using ManagedCommon;
@@ -13,7 +14,9 @@ using Microsoft.CmdPal.Ext.Apps.Commands;
 using Microsoft.CmdPal.Ext.Apps.Properties;
 using Microsoft.CmdPal.Ext.Apps.Utils;
 using Microsoft.CommandPalette.Extensions.Toolkit;
-using static Microsoft.CmdPal.Ext.Apps.Utils.Native;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.Storage.Packaging.Appx;
 using PackageVersion = Microsoft.CmdPal.Ext.Apps.Programs.UWP.PackageVersion;
 using Theme = Microsoft.CmdPal.Ext.Apps.Utils.Theme;
 
@@ -97,27 +100,27 @@ public class UWPApplication : IProgram
         return commands;
     }
 
-    public UWPApplication(IAppxManifestApplication manifestApp, UWP package)
+    internal unsafe UWPApplication(IAppxManifestApplication* manifestApp, UWP package)
     {
         ArgumentNullException.ThrowIfNull(manifestApp);
 
-        var hr = manifestApp.GetAppUserModelId(out var tmpUserModelId);
-        UserModelId = AppxPackageHelper.CheckHRAndReturnOrThrow(hr, tmpUserModelId);
+        var hr = manifestApp->GetAppUserModelId(out var tmpUserModelIdPtr);
+        UserModelId = ComFreeHelper.GetStringAndFree(hr, tmpUserModelIdPtr);
 
-        hr = manifestApp.GetAppUserModelId(out var tmpUniqueIdentifier);
-        UniqueIdentifier = AppxPackageHelper.CheckHRAndReturnOrThrow(hr, tmpUniqueIdentifier);
+        manifestApp->GetAppUserModelId(out var tmpUniqueIdentifierPtr);
+        UniqueIdentifier = ComFreeHelper.GetStringAndFree(hr, tmpUniqueIdentifierPtr);
 
-        hr = manifestApp.GetStringValue("DisplayName", out var tmpDisplayName);
-        DisplayName = AppxPackageHelper.CheckHRAndReturnOrThrow(hr, tmpDisplayName);
+        manifestApp->GetStringValue("DisplayName", out var tmpDisplayNamePtr);
+        DisplayName = ComFreeHelper.GetStringAndFree(hr, tmpDisplayNamePtr);
 
-        hr = manifestApp.GetStringValue("Description", out var tmpDescription);
-        Description = AppxPackageHelper.CheckHRAndReturnOrThrow(hr, tmpDescription);
+        manifestApp->GetStringValue("Description", out var tmpDescriptionPtr);
+        Description = ComFreeHelper.GetStringAndFree(hr, tmpDescriptionPtr);
 
-        hr = manifestApp.GetStringValue("BackgroundColor", out var tmpBackgroundColor);
-        BackgroundColor = AppxPackageHelper.CheckHRAndReturnOrThrow(hr, tmpBackgroundColor);
+        manifestApp->GetStringValue("BackgroundColor", out var tmpBackgroundColorPtr);
+        BackgroundColor = ComFreeHelper.GetStringAndFree(hr, tmpBackgroundColorPtr);
 
-        hr = manifestApp.GetStringValue("EntryPoint", out var tmpEntryPoint);
-        EntryPoint = AppxPackageHelper.CheckHRAndReturnOrThrow(hr, tmpEntryPoint);
+        manifestApp->GetStringValue("EntryPoint", out var tmpEntryPointPtr);
+        EntryPoint = ComFreeHelper.GetStringAndFree(hr, tmpEntryPointPtr);
 
         Package = package ?? throw new ArgumentNullException(nameof(package));
 
@@ -166,7 +169,7 @@ public class UWPApplication : IProgram
         return false;
     }
 
-    internal string ResourceFromPri(string packageFullName, string resourceReference)
+    internal unsafe string ResourceFromPri(string packageFullName, string resourceReference)
     {
         const string prefix = "ms-resource:";
 
@@ -200,30 +203,8 @@ public class UWPApplication : IProgram
                 parsedFallback = prefix + "///" + key;
             }
 
-            var outBuffer = new StringBuilder(128);
-            var source = $"@{{{packageFullName}? {parsed}}}";
-            var capacity = (uint)outBuffer.Capacity;
-            var hResult = SHLoadIndirectString(source, outBuffer, capacity, IntPtr.Zero);
-            if (hResult != HRESULT.S_OK)
+            if (string.IsNullOrEmpty(parsedFallback))
             {
-                if (!string.IsNullOrEmpty(parsedFallback))
-                {
-                    var sourceFallback = $"@{{{packageFullName}? {parsedFallback}}}";
-                    hResult = SHLoadIndirectString(sourceFallback, outBuffer, capacity, IntPtr.Zero);
-                    if (hResult == HRESULT.S_OK)
-                    {
-                        var loaded = outBuffer.ToString();
-                        if (!string.IsNullOrEmpty(loaded))
-                        {
-                            return loaded;
-                        }
-                        else
-                        {
-                            return string.Empty;
-                        }
-                    }
-                }
-
                 // https://github.com/Wox-launcher/Wox/issues/964
                 // known hresult 2147942522:
                 // 'Microsoft Corporation' violates pattern constraint of '\bms-resource:.{1,256}'.
@@ -232,17 +213,40 @@ public class UWPApplication : IProgram
                 // Microsoft.BingFoodAndDrink_3.0.4.336_x64__8wekyb3d8bbwe: ms-resource:AppDescription
                 return string.Empty;
             }
-            else
+
+            var capacity = 1024U;
+            PWSTR outBuffer = new PWSTR((char*)(void*)Marshal.AllocHGlobal((int)capacity * sizeof(char)));
+            var source = $"@{{{packageFullName}? {parsed}}}";
+            void* reserved = null;
+
+            try
             {
+                PInvoke.SHLoadIndirectString(source, outBuffer, capacity, ref reserved).ThrowOnFailure();
+
                 var loaded = outBuffer.ToString();
-                if (!string.IsNullOrEmpty(loaded))
+                return string.IsNullOrEmpty(loaded) ? string.Empty : loaded;
+            }
+            catch (Exception)
+            {
+                try
                 {
-                    return loaded;
+                    var sourceFallback = $"@{{{packageFullName}?{parsedFallback}}}";
+                    PInvoke.SHLoadIndirectString(sourceFallback, outBuffer, capacity, ref reserved).ThrowOnFailure();
+                    var loaded = outBuffer.ToString();
+                    return string.IsNullOrEmpty(loaded) ? string.Empty : loaded;
                 }
-                else
+                catch (Exception)
                 {
+                    // ProgramLogger.Exception($"Unable to load resource {resourceReference} from {packageFullName}", new InvalidOperationException(), GetType(), packageFullName);
                     return string.Empty;
                 }
+                finally
+                {
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal((IntPtr)outBuffer.Value);
             }
         }
         else
@@ -258,13 +262,12 @@ public class UWPApplication : IProgram
             { PackageVersion.Windows8, "SmallLogo" },
         };
 
-    internal string LogoUriFromManifest(IAppxManifestApplication app)
+    internal unsafe string LogoUriFromManifest(IAppxManifestApplication* app)
     {
         if (_logoKeyFromVersion.TryGetValue(Package.Version, out var key))
         {
-            var hr = app.GetStringValue(key, out var logoUriFromApp);
-            _ = AppxPackageHelper.CheckHRAndReturnOrThrow(hr, logoUriFromApp);
-            return logoUriFromApp;
+            var hr = app->GetStringValue(key, out var logoUriFromAppPtr);
+            return ComFreeHelper.GetStringAndFree(hr, logoUriFromAppPtr);
         }
         else
         {
@@ -349,7 +352,7 @@ public class UWPApplication : IProgram
             var prefix = path.Substring(0, end);
             var paths = new List<string> { };
             const int appIconSize = 36;
-            var targetSizes = new List<int> { 16, 24, 30, 36, 44, 60, 72, 96, 128, 180, 256 }.AsParallel();
+            var targetSizes = new List<int> { 16, 24, 30, 36, 44, 60, 72, 96, 128, 180, 256 };
             var pathFactorPairs = new Dictionary<string, int>();
 
             foreach (var factor in targetSizes)

src/Common.Dotnet.AotCompatibility.props
@@ -7,6 +7,6 @@
     <CsWinRTAotWarningLevel>2</CsWinRTAotWarningLevel>
 
     <!-- Suppress DynamicallyAccessedMemberTypes.PublicParameterlessConstructor in fallback code path of Windows SDK projection -->
-    <WarningsNotAsErrors>IL2081;$(WarningsNotAsErrors)</WarningsNotAsErrors>
+    <WarningsNotAsErrors>IL2081;CsWinRT1028;$(WarningsNotAsErrors)</WarningsNotAsErrors>
   </PropertyGroup>
 </Project>

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Apps/AllAppsSettings.cs
@@ -71,7 +71,7 @@ public class AllAppsSettings : JsonSettingsManager
 
     internal static string SettingsJsonPath()
     {
-        string directory = Utilities.BaseSettingsPath("Microsoft.CmdPal");
+        var directory = Utilities.BaseSettingsPath("Microsoft.CmdPal");
         Directory.CreateDirectory(directory);
 
         // now, the state is just next to the exe

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Apps/AppCommand.cs
@@ -8,7 +8,12 @@ using System.Threading.Tasks;
 using ManagedCommon;
 using Microsoft.CmdPal.Ext.Apps.Programs;
 using Microsoft.CmdPal.Ext.Apps.Properties;
+using Microsoft.CmdPal.Ext.Apps.Utils;
 using Microsoft.CommandPalette.Extensions.Toolkit;
+using Windows.Services.Maps;
+using Windows.Win32;
+using Windows.Win32.System.Com;
+using Windows.Win32.UI.Shell;
 using WyHash;
 
 namespace Microsoft.CmdPal.Ext.Apps;
@@ -27,26 +32,31 @@ internal sealed partial class AppCommand : InvokableCommand
 
     internal static async Task StartApp(string aumid)
     {
-        var appManager = new ApplicationActivationManager();
-        const ActivateOptions noFlags = ActivateOptions.None;
         await Task.Run(() =>
         {
-            try
+            unsafe
             {
-                appManager.ActivateApplication(aumid, /*queryArguments*/ string.Empty, noFlags, out var unusedPid);
-            }
-            catch (System.Exception ex)
-            {
-                Logger.LogError(ex.Message);
+                IApplicationActivationManager* appManager = null;
+                try
+                {
+                    PInvoke.CoCreateInstance(typeof(ApplicationActivationManager).GUID, null, CLSCTX.CLSCTX_INPROC_SERVER, out appManager).ThrowOnFailure();
+                    using var handle = new SafeComHandle((IntPtr)appManager);
+                    appManager->ActivateApplication(
+                        aumid,
+                        string.Empty,
+                        ACTIVATEOPTIONS.AO_NONE,
+                        out var unusedPid);
+                }
+                catch (System.Exception ex)
+                {
+                    Logger.LogError(ex.Message);
+                }
             }
         }).ConfigureAwait(false);
     }
 
     internal static async Task StartExe(string path)
     {
-        var appManager = new ApplicationActivationManager();
-
-        // const ActivateOptions noFlags = ActivateOptions.None;
         await Task.Run(() =>
         {
             try

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Apps/Microsoft.CmdPal.Ext.Apps.csproj
@@ -1,11 +1,13 @@
 ﻿<Project Sdk="Microsoft.NET.Sdk">
   <Import Project="..\..\..\..\Common.Dotnet.CsWinRT.props" />
+  <Import Project="..\..\..\..\Common.Dotnet.AotCompatibility.props" />
   <PropertyGroup>
     <RootNamespace>Microsoft.CmdPal.Ext.Apps</RootNamespace>
     <Nullable>enable</Nullable>
     <OutputPath>$(SolutionDir)$(Platform)\$(Configuration)\WinUI3Apps\CmdPal</OutputPath>
     <AppendTargetFrameworkToOutputPath>false</AppendTargetFrameworkToOutputPath>
     <AppendRuntimeIdentifierToOutputPath>false</AppendRuntimeIdentifierToOutputPath>
+	<DisableRuntimeMarshalling>true</DisableRuntimeMarshalling>
   </PropertyGroup>
 
   <ItemGroup>
@@ -48,5 +50,10 @@
       <Generator>ResXFileCodeGenerator</Generator>
       <LastGenOutput>Resources.Designer.cs</LastGenOutput>
     </EmbeddedResource>
+  </ItemGroup>
+
+  <ItemGroup>
+    <AdditionalFiles Include="NativeMethods.txt" />
+    <AdditionalFiles Include="NativeMethods.json" />
   </ItemGroup>
 </Project>

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Apps/Programs/AppxFactory.cs
@@ -1,14 +1,0 @@
-// Copyright (c) Microsoft Corporation
-// The Microsoft Corporation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using System.Runtime.InteropServices;
-
-namespace Microsoft.CmdPal.Ext.Apps.Programs;
-
-// Reference : https://stackoverflow.com/questions/32122679/getting-icon-of-modern-windows-app-from-a-desktop-application
-[Guid("5842a140-ff9f-4166-8f5c-62f5b7b0c781")]
-[ComImport]
-public class AppxFactory
-{
-}

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Apps/Programs/AppxPackageHelper.cs
@@ -2,42 +2,75 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
+using ManagedCommon;
+using Microsoft.CmdPal.Ext.Apps.Utils;
+using Microsoft.UI.Xaml.Controls;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.Storage.Packaging.Appx;
 using Windows.Win32.System.Com;
-using static Microsoft.CmdPal.Ext.Apps.Utils.Native;
 
 namespace Microsoft.CmdPal.Ext.Apps.Programs;
 
 public static class AppxPackageHelper
 {
-    private static readonly IAppxFactory AppxFactory = (IAppxFactory)new AppxFactory();
-
-    // This function returns a list of attributes of applications
-    internal static IEnumerable<IAppxManifestApplication> GetAppsFromManifest(IStream stream)
+    internal static unsafe List<IntPtr> GetAppsFromManifest(IStream* stream)
     {
-        var reader = AppxFactory.CreateManifestReader(stream);
-        var manifestApps = reader.GetApplications();
+        PInvoke.CoCreateInstance(typeof(AppxFactory).GUID, null, CLSCTX.CLSCTX_INPROC_SERVER, out IAppxFactory* appxFactory).ThrowOnFailure();
+        using var handle = new SafeComHandle((IntPtr)appxFactory);
 
-        while (manifestApps.GetHasCurrent())
+        IAppxManifestReader* reader = null;
+        IAppxManifestApplicationsEnumerator* manifestApps = null;
+        var result = new List<IntPtr>();
+
+        appxFactory->CreateManifestReader(stream, &reader);
+        using var readerHandle = new SafeComHandle((IntPtr)reader);
+        reader->GetApplications(&manifestApps);
+        using var manifestAppsHandle = new SafeComHandle((IntPtr)manifestApps);
+
+        while (true)
         {
-            var manifestApp = manifestApps.GetCurrent();
-            var hr = manifestApp.GetStringValue("AppListEntry", out var appListEntry);
-            _ = CheckHRAndReturnOrThrow(hr, appListEntry);
-            if (appListEntry != "none")
+            manifestApps->GetHasCurrent(out var hasCurrent);
+            if (hasCurrent == false)
             {
-                yield return manifestApp;
+                break;
             }
 
-            manifestApps.MoveNext();
-        }
-    }
+            IAppxManifestApplication* manifestApp = null;
 
-    internal static T CheckHRAndReturnOrThrow<T>(HRESULT hr, T result)
-    {
-        if (hr != HRESULT.S_OK)
-        {
-            Marshal.ThrowExceptionForHR((int)hr);
+            try
+            {
+                manifestApps->GetCurrent(&manifestApp).ThrowOnFailure();
+
+                var hr = manifestApp->GetStringValue("AppListEntry", out var appListEntryPtr);
+                var appListEntry = ComFreeHelper.GetStringAndFree(hr, appListEntryPtr);
+
+                if (appListEntry != "none")
+                {
+                    result.Add((IntPtr)manifestApp);
+                }
+                else if (manifestApp != null)
+                {
+                    manifestApp->Release();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (manifestApp != null)
+                {
+                    manifestApp->Release();
+                }
+
+                Logger.LogError($"Failed to get current application from manifest: {ex.Message}");
+            }
+
+            manifestApps->MoveNext(out var hasNext);
+            if (hasNext == false)
+            {
+                break;
+            }
         }
 
         return result;

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Apps/Programs/IApplicationActivationManager.cs
@@ -1,47 +1,0 @@
-// Copyright (c) Microsoft Corporation
-// The Microsoft Corporation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-namespace Microsoft.CmdPal.Ext.Apps.Programs;
-
-// Reference : https://github.com/MicrosoftEdge/edge-launcher/blob/108e63df0b4cb5cd9d5e45aa7a264690851ec51d/MIcrosoftEdgeLauncherCsharp/Program.cs
-[Flags]
-public enum ActivateOptions
-{
-    None = 0x00000000,
-    DesignMode = 0x00000001,
-    NoErrorUI = 0x00000002,
-    NoSplashScreen = 0x00000004,
-}
-
-// ApplicationActivationManager
-[ComImport]
-[Guid("2e941141-7f97-4756-ba1d-9decde894a3d")]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-public interface IApplicationActivationManager
-{
-    IntPtr ActivateApplication([In] string appUserModelId, [In] string arguments, [In] ActivateOptions options, [Out] out uint processId);
-
-    IntPtr ActivateForFile([In] string appUserModelId, [In] IntPtr /*IShellItemArray* */ itemArray, [In] string verb, [Out] out uint processId);
-
-    IntPtr ActivateForProtocol([In] string appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out uint processId);
-}
-
-// Application Activation Manager Class
-[ComImport]
-[Guid("45BA127D-10A8-46EA-8AB7-56EA9078943C")]
-public class ApplicationActivationManager : IApplicationActivationManager
-{
-    [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)/*, PreserveSig*/]
-    public extern IntPtr ActivateApplication([In] string appUserModelId, [In] string arguments, [In] ActivateOptions options, [Out] out uint processId);
-
-    [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-    public extern IntPtr ActivateForFile([In] string appUserModelId, [In] IntPtr /*IShellItemArray* */ itemArray, [In] string verb, [Out] out uint processId);
-
-    [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
-    public extern IntPtr ActivateForProtocol([In] string appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out uint processId);
-}

commit fd287a5ace282bcde0b878cd28295a25cc139c2b

CmdPal: hotfix loading packaged apps
Regressed in #39678

This block of code just moved up too far. It needs to be after the first
attempt to load the resource.

closes: did we even file it yet

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Apps/Programs/UWPApplication.cs
@@ -6,8 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Xml;
 using ManagedCommon;
 using Microsoft.CmdPal.Ext.Apps.Commands;
@@ -15,7 +13,6 @@ using Microsoft.CmdPal.Ext.Apps.Properties;
 using Microsoft.CmdPal.Ext.Apps.Utils;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 using Windows.Win32;
-using Windows.Win32.Foundation;
 using Windows.Win32.Storage.Packaging.Appx;
 using PackageVersion = Microsoft.CmdPal.Ext.Apps.Programs.UWP.PackageVersion;
 using Theme = Microsoft.CmdPal.Ext.Apps.Utils.Theme;
@@ -74,7 +71,7 @@ public class UWPApplication : IProgram
 
     public List<CommandContextItem> GetCommands()
     {
-        List<CommandContextItem> commands = new List<CommandContextItem>();
+        List<CommandContextItem> commands = [];
 
         if (CanRunElevated)
         {
@@ -226,6 +223,16 @@ public class UWPApplication : IProgram
                 parsedFallback = prefix + "///" + key;
             }
 
+            Span<char> outBuffer = stackalloc char[1024];
+            var source = $"@{{{packageFullName}? {parsed}}}";
+
+            var loaded = TryLoadIndirectString(source, outBuffer, resourceReference);
+
+            if (!string.IsNullOrEmpty(loaded))
+            {
+                return loaded;
+            }
+
             if (string.IsNullOrEmpty(parsedFallback))
             {
                 // https://github.com/Wox-launcher/Wox/issues/964
@@ -235,16 +242,6 @@ public class UWPApplication : IProgram
                 // Microsoft.MicrosoftOfficeHub_17.7608.23501.0_x64__8wekyb3d8bbwe: ms-resource://Microsoft.MicrosoftOfficeHub/officehubintl/AppManifest_GetOffice_Description
                 // Microsoft.BingFoodAndDrink_3.0.4.336_x64__8wekyb3d8bbwe: ms-resource:AppDescription
                 return string.Empty;
-            }
-
-            Span<char> outBuffer = stackalloc char[1024];
-            var source = $"@{{{packageFullName}? {parsed}}}";
-
-            var loaded = TryLoadIndirectString(source, outBuffer, resourceReference);
-
-            if (!string.IsNullOrEmpty(loaded))
-            {
-                return loaded;
             }
 
             var sourceFallback = $"@{{{packageFullName}?{parsedFallback}}}";