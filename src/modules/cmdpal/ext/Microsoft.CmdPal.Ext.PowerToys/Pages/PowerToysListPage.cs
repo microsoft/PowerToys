045523d04f78f2e570ff5dc9c8166abb8e730a20
vanzue <vanzue@outlook.com>
2025-07-01 09:52:22 +0800

    init project

---
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.PowerToys/Pages/PowerToysListPage.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace Microsoft.CmdPal.Ext.PowerToys.Pages;
+
+internal partial class PowerToysListPage : DynamicListPage
+{
+    public override void UpdateSearchText(string oldSearch, string newSearch)
+    {
+        return;
+    }
+
+    public override IListItem[] GetItems() => [];
+}
---
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.PowerToys/PowerToysCommandProvider.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CmdPal.Ext.PowerToys.Properties;
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace Microsoft.CmdPal.Ext.PowerToys;
+
+public partial class PowerToysCommandsProvider : CommandProvider
+{
+    public PowerToysCommandsProvider()
+    {
+        Id = "Microsoft.PowerToys";
+        DisplayName = Resources.PowerToysProvider_DisplayName;
+        Icon = IconHelpers.FromRelativePath("Assets\\PowerToys.png");
+    }
+
+    public override ICommandItem[] TopLevelCommands()
+    {
+        return [
+            new CommandItem(new RegistryListPage())
+            {
+                Title = "Registry",
+                Subtitle = "Navigate the Windows registry",
+            }
+        ];
+    }
+}
