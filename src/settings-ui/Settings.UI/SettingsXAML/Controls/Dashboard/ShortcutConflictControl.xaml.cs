b0a4d2dc2c4b2c680b2d05a513b49d4b44cf9277
Shuai Yuan <shuai.yuan.zju@gmail.com>
2025-07-03 11:50:03 +0800

    Added IPC for hotkey conflict button control.
    Signed-off-by: Shuai Yuan <shuai.yuan.zju@gmail.com>

--- src/settings-ui/Settings.UI/SettingsXAML/Controls/Dashboard/ShortcutConflictControl.xaml.cs
+++ src/settings-ui/Settings.UI/SettingsXAML/Controls/Dashboard/ShortcutConflictControl.xaml.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.PowerToys.Settings.UI.Library.HotkeyConflicts;
+using Microsoft.PowerToys.Settings.UI.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -21,20 +23,89 @@ namespace Microsoft.PowerToys.Settings.UI.Controls
 {
     public sealed partial class ShortcutConflictControl : UserControl
     {
+        private AllHotkeyConflictsData _currentConflicts;
+
         public ShortcutConflictControl()
         {
             InitializeComponent();
+            RegisterForConflictUpdates();
             GetShortcutConflicts();
+        }
+
+        private void RegisterForConflictUpdates()
+        {
+            // Subscribe to conflict updates from the global manager
+            if (GlobalHotkeyConflictManager.Instance != null)
+            {
+                GlobalHotkeyConflictManager.Instance.ConflictsUpdated += OnConflictsUpdated;
+            }
         }
 
         private void GetShortcutConflicts()
         {
-            // TO DO: Implement the logic to retrieve and display shortcut conflicts. Make sure to Collapse this control if not conflicts are found.
+            // Request all hotkey conflicts via IPC
+            GlobalHotkeyConflictManager.Instance?.RequestAllConflicts();
+        }
+
+        private void OnConflictsUpdated(object sender, AllHotkeyConflictsEventArgs e)
+        {
+            _currentConflicts = e.Conflicts;
+
+            // Update UI on the main thread
+            this.DispatcherQueue.TryEnqueue(() =>
+            {
+                UpdateConflictDisplay();
+            });
+        }
+
+        private void UpdateConflictDisplay()
+        {
+            if (_currentConflicts == null)
+            {
+                this.Visibility = Visibility.Collapsed;
+                return;
+            }
+
+            // Check if there are any conflicts
+            bool hasConflicts = _currentConflicts.InAppConflicts.Count != 0 || _currentConflicts.SystemConflicts.Count != 0;
+
+            if (!hasConflicts)
+            {
+                this.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                this.Visibility = Visibility.Visible;
+
+                // TODO: Update UI elements to show conflict count or details
+                // For example, update button text with conflict count
+                // ShortcutConflictBtn.Content = $"View {GetTotalConflictCount()} conflicts";
+            }
+        }
+
+        private int GetTotalConflictCount()
+        {
+            if (_currentConflicts == null)
+            {
+                return 0;
+            }
+
+            return _currentConflicts.InAppConflicts.Count + _currentConflicts.SystemConflicts.Count;
         }
 
         private void ShortcutConflictBtn_Click(object sender, RoutedEventArgs e)
         {
-            // TO DO: Handle the button click event to show the shortcut conflicts window.
+            // TODO: Handle the button click event to show the shortcut conflicts window.
+            // You can now use _currentConflicts to display detailed conflict information
+        }
+
+        // Clean up event subscription when control is unloaded
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (GlobalHotkeyConflictManager.Instance != null)
+            {
+                GlobalHotkeyConflictManager.Instance.ConflictsUpdated -= OnConflictsUpdated;
+            }
         }
     }
 }
