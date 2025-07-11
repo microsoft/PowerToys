380bc23b97511988f285613a553dd4c5ec6b5b29
vanzue <vanzue@outlook.com>
2025-07-11 08:51:20 +0800

    dev

--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.PowerToys/Pages/AwakePage.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.PowerToys/Pages/AwakePage.cs
@@ -2,13 +2,27 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using Microsoft.CmdPal.Ext.PowerToys.Helper;
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
 
 namespace Microsoft.CmdPal.Ext.PowerToys.Pages;
-internal class AwakePage
+
+internal sealed partial class AwakePage : DynamicListPage
 {
+    public AwakePage()
+    {
+        Icon = IconHelpers.FromRelativePath("Assets\\Awake.png");
+        Name = Title = "Awake";
+        Id = "com.microsoft.cmdpal.powertoys.awake";
+    }
+
+    public override void UpdateSearchText(string oldSearch, string newSearch)
+    {
+        RaiseItemsChanged(0);
+        return;
+    }
+
+    public override IListItem[] GetItems() => WorkspaceItemsHelper.FilteredItems(SearchText);
 }
--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.PowerToys/Pages/WorkspacesListPage.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.PowerToys/Pages/WorkspacesListPage.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.CmdPal.Ext.PowerToys.Helper;
-using Microsoft.CmdPal.Ext.PowerToys.Properties;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 
