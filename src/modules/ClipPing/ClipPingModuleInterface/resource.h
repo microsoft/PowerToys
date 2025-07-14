12bbe4611947388ea684a9add451cb929aa74ca8
Kevin Gosse <krix33@gmail.com>
2025-07-14 12:31:12 +0200

    It compiles

---
+++ src/modules/ClipPing/ClipPingModuleInterface/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MouseWithoutBorders.rc
+//
+#define IDS_CLIPPING_NAME 101
+
+#define FILE_DESCRIPTION "PowerToys ClipPing Module"
+#define INTERNAL_NAME "PowerToys.ClipPingModuleInterface"
+#define ORIGINAL_FILENAME "PowerToys.ClipPingModuleInterface.dll"
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE 102
+#define _APS_NEXT_COMMAND_VALUE 40001
+#define _APS_NEXT_CONTROL_VALUE 1001
+#define _APS_NEXT_SYMED_VALUE 101
+#endif
+#endif
