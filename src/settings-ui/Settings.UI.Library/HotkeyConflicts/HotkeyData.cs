9aa9e5a842a0eaef5239be38b9eb9b1be60a0b7b
Shuai Yuan <shuai.yuan.zju@gmail.com>
2025-07-15 11:26:26 +0800

    update ui and localization.
    Signed-off-by: Shuai Yuan <shuai.yuan.zju@gmail.com>

--- src/settings-ui/Settings.UI.Library/HotkeyConflicts/HotkeyData.cs
+++ src/settings-ui/Settings.UI.Library/HotkeyConflicts/HotkeyData.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.PowerToys.Settings.UI.Library.Utilities;
 
 namespace Microsoft.PowerToys.Settings.UI.Library.HotkeyConflicts
 {
@@ -21,6 +22,51 @@ namespace Microsoft.PowerToys.Settings.UI.Library.HotkeyConflicts
         public bool Alt { get; set; }
 
         public int Key { get; set; }
+
+        public List<object> GetKeysList()
+        {
+            List<object> shortcutList = new List<object>();
+
+            if (Win)
+            {
+                shortcutList.Add(92); // The Windows key or button.
+            }
+
+            if (Ctrl)
+            {
+                shortcutList.Add("Ctrl");
+            }
+
+            if (Alt)
+            {
+                shortcutList.Add("Alt");
+            }
+
+            if (Shift)
+            {
+                shortcutList.Add(16); // The Shift key or button.
+            }
+
+            if (Key > 0)
+            {
+                switch (Key)
+                {
+                    // https://learn.microsoft.com/uwp/api/windows.system.virtualkey?view=winrt-20348
+                    case 38: // The Up Arrow key or button.
+                    case 40: // The Down Arrow key or button.
+                    case 37: // The Left Arrow key or button.
+                    case 39: // The Right Arrow key or button.
+                        shortcutList.Add(Key);
+                        break;
+                    default:
+                        var localKey = Helper.GetKeyName((uint)Key);
+                        shortcutList.Add(localKey);
+                        break;
+                }
+            }
+
+            return shortcutList;
+        }
 
         public override string ToString()
         {
@@ -50,7 +96,7 @@ namespace Microsoft.PowerToys.Settings.UI.Library.HotkeyConflicts
             {
                 // For virtual key codes, we can display the key name
                 // This follows the same pattern as HotkeySettings
-                var keyName = GetKeyName((uint)Key);
+                var keyName = Helper.GetKeyName((uint)Key);
                 output.Append(keyName);
             }
             else if (output.Length >= 2)
