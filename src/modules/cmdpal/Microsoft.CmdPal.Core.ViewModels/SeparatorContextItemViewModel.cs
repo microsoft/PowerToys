e6e0f6d541efc0e31df9337f798a3a70e00f8a31
Mike Griese <migrie@microsoft.com>
2025-07-22 15:12:32 -0500

    Revert "MAIN: fix this"
    This reverts commit b79cf60d94de02fd5cce97aafd5f327f7c5ce01d.

--- src/modules/cmdpal/Microsoft.CmdPal.Core.ViewModels/SeparatorContextItemViewModel.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.Core.ViewModels/SeparatorContextItemViewModel.cs
@@ -2,12 +2,11 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.CmdPal.Core.ViewModels;
 using Microsoft.CommandPalette.Extensions;
 
 namespace Microsoft.CmdPal.Core.ViewModels;
 
-// DANGER: Make sure that this is derived from a WinRT class. If not, it'll fail with AOT at runtime, in any bindings.
-public partial class SeparatorContextItemViewModel() : ObservableObject, IContextItemViewModel, ISeparatorContextItem
+public partial class SeparatorContextItemViewModel() : IContextItemViewModel, ISeparatorContextItem
 {
 }
