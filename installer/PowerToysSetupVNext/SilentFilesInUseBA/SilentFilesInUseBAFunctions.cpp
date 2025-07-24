7bdfdd5270a9072bb05d7c128d1638e5d7c91a04
Kai Tao (from Dev Box) <kaitao@microsoft.com>
2025-07-24 11:47:54 +0800

    fix

--- installer/PowerToysSetupVNext/SilentFilesInUseBA/SilentFilesInUseBAFunctions.cpp
+++ installer/PowerToysSetupVNext/SilentFilesInUseBA/SilentFilesInUseBAFunctions.cpp
@@ -16,11 +16,11 @@ public: // IBootstrapperApplication
     {
         HRESULT hr = S_OK;
 
-        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "Running detect begin BA function. fCached=%d, registrationType=%d, cPackages=%u, fCancel=%d", fCached, registrationType, cPackages, *pfCancel);
+        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** CUSTOM BA FUNCTION SYSTEM ACTIVE *** Running detect begin BA function. fCached=%d, registrationType=%d, cPackages=%u, fCancel=%d", fCached, registrationType, cPackages, *pfCancel);
 
         //-------------------------------------------------------------------------------------------------
         // YOUR CODE GOES HERE
-        BalExitOnFailure(hr, "Change this message to represent real error handling.");
+        // BalExitOnFailure(hr, "Change this message to represent real error handling.");
         //-------------------------------------------------------------------------------------------------
 
     LExit:
@@ -35,14 +35,26 @@ public: // IBAFunctions
     {
         HRESULT hr = S_OK;
 
-        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "Running plan begin BA function. cPackages=%u, fCancel=%d", cPackages, *pfCancel);
+        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** CUSTOM BA FUNCTION SYSTEM ACTIVE *** Running plan begin BA function. cPackages=%u, fCancel=%d", cPackages, *pfCancel);
 
         //-------------------------------------------------------------------------------------------------
         // YOUR CODE GOES HERE
-        BalExitOnFailure(hr, "Change this message to represent real error handling.");
+        // BalExitOnFailure(hr, "Change this message to represent real error handling.");
         //-------------------------------------------------------------------------------------------------
 
     LExit:
+        return hr;
+    }
+
+    virtual STDMETHODIMP OnExecuteBegin(
+        __in DWORD cExecutingPackages,
+        __inout BOOL* pfCancel
+        )
+    {
+        HRESULT hr = S_OK;
+
+        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** CUSTOM BA FUNCTION SYSTEM ACTIVE *** Running execute begin BA function. cExecutingPackages=%u, fCancel=%d", cExecutingPackages, *pfCancel);
+
         return hr;
     }
 
@@ -56,10 +68,30 @@ public: // IBAFunctions
     {
         HRESULT hr = S_OK;
 
-        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "Running OnExecuteFilesInUse BA function. packageId=%ls, cFiles=%u, recommendation=%d", wzPackageId, cFiles, nRecommendation);
+        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** CUSTOM BA FUNCTION CALLED *** Running OnExecuteFilesInUse BA function. packageId=%ls, cFiles=%u, recommendation=%d", wzPackageId, cFiles, nRecommendation);
+        
+        // Log each file that's in use
+        for (DWORD i = 0; i < cFiles; i++)
+        {
+            BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** FILE IN USE [%u]: %ls", i, rgwzFiles[i]);
+        }
 
         // Always return IDIGNORE to silently ignore files in use
         *pResult = IDIGNORE;
+        
+        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** BA FUNCTION RETURNING IDIGNORE - SILENTLY CONTINUING ***");
+
+        return hr;
+    }
+
+    virtual STDMETHODIMP OnExecuteComplete(
+        __in HRESULT hrStatus,
+        __inout BOOL* pfCancel
+        )
+    {
+        HRESULT hr = S_OK;
+
+        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** CUSTOM BA FUNCTION SYSTEM ACTIVE *** Running execute complete BA function. hrStatus=0x%x, fCancel=%d", hrStatus, *pfCancel);
 
         return hr;
     }
@@ -72,6 +104,7 @@ public:
         __in HMODULE hModule
         ) : CBalBaseBAFunctions(hModule)
     {
+        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** BA FUNCTION CONSTRUCTOR *** CSilentFilesInUseBAFunctions created");
     }
 
     //
@@ -79,6 +112,7 @@ public:
     //
     ~CSilentFilesInUseBAFunctions()
     {
+        BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** BA FUNCTION DESTRUCTOR *** CSilentFilesInUseBAFunctions destroyed");
     }
 };
 
@@ -92,17 +126,31 @@ HRESULT WINAPI CreateBAFunctions(
     HRESULT hr = S_OK;
     CSilentFilesInUseBAFunctions* pBAFunctions = NULL;
 
+    // First thing - log that we're being called
+    BalInitialize(pArgs->pEngine);
+    BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** CREATEBAFUNCTIONS CALLED *** BA Function DLL is being loaded!");
+
     pBAFunctions = new CSilentFilesInUseBAFunctions(hModule);
     ExitOnNull(pBAFunctions, hr, E_OUTOFMEMORY, "Failed to create new CSilentFilesInUseBAFunctions object.");
 
+    BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** CREATEBAFUNCTIONS *** Created CSilentFilesInUseBAFunctions object");
+
     hr = pBAFunctions->OnCreate(pArgs->pEngine, pArgs->pCommand);
     ExitOnFailure(hr, "Failed to call OnCreate CPrereqBaf.");
+
+    BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** CREATEBAFUNCTIONS *** OnCreate completed successfully");
 
     pResults->pfnBAFunctionsProc = BalBaseBAFunctionsProc;
     pResults->pvBAFunctionsProcContext = pBAFunctions;
     pBAFunctions = NULL;
 
+    BalLog(BOOTSTRAPPER_LOG_LEVEL_STANDARD, "*** CREATEBAFUNCTIONS SUCCESS *** BA Function system initialized");
+
 LExit:
+    if (FAILED(hr))
+    {
+        BalLog(BOOTSTRAPPER_LOG_LEVEL_ERROR, "*** CREATEBAFUNCTIONS FAILED *** hr=0x%x", hr);
+    }
     ReleaseObject(pBAFunctions);
 
     return hr;
