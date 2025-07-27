201a27d2bba828db679c3d7b9ab20bbd18745ffb
Aaron Junker <Aaron.Junker@outlook.com>
2025-07-27 20:09:54 +0200

    Add taskbar launch shortcuts and make powertoys shortcuts empty by default

--- src/modules/ShortcutGuide/ShortcutGuide.Ui/ShortcutGuideXAML/ShortcutView.xaml.cs
+++ src/modules/ShortcutGuide/ShortcutGuide.Ui/ShortcutGuideXAML/ShortcutView.xaml.cs
@@ -225,6 +225,17 @@ namespace ShortcutGuide
                 ErrorMessage.Visibility = Visibility.Visible;
                 ErrorMessage.Text = ResourceLoaderInstance.ResourceLoader.GetString("NoShortcutsInOverview");
             }
+
+            if (_showTaskbarShortcuts)
+            {
+                TaskbarLaunchShortcutsListElement.Visibility = Visibility.Visible;
+                TaskbarLaunchShortcutsListElement.Items.Clear();
+                TaskbarLaunchShortcutsTitle.Visibility = Visibility.Visible;
+                foreach (var item in _shortcutList.Shortcuts.First(x => x.SectionName.StartsWith("<TASKBAR1-9>", StringComparison.InvariantCulture)).Properties)
+                {
+                    TaskbarLaunchShortcutsListElement.Items.Add((ShortcutTemplateDataObject)item);
+                }
+            }
         }
 
         private string _searchFilter = string.Empty;
@@ -288,10 +299,12 @@ namespace ShortcutGuide
             ShortcutListElement.Items.Clear();
             RecommendedListElement.Items.Clear();
             PinnedListElement.Items.Clear();
+            TaskbarLaunchShortcutsListElement.Items.Clear();
             ErrorMessage.Visibility = Visibility.Collapsed;
             RecommendedListElement.Visibility = Visibility.Collapsed;
             PinnedListElement.Visibility = Visibility.Collapsed;
             OverviewStackPanel.Visibility = Visibility.Collapsed;
+            TaskbarLaunchShortcutsListElement.Visibility = Visibility.Collapsed;
             ShortcutListElement.Visibility = Visibility.Visible;
 
             try
