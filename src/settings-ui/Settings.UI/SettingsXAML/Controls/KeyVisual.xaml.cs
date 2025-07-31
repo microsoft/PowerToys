47401f29a227bf4a53d2cd6be3b98fbce1a07bdd
Niels Laute <niels.laute@live.nl>
2025-07-31 12:01:49 +0200

    Cleaning up and loccing KeyVisual

--- src/settings-ui/Settings.UI/SettingsXAML/Controls/KeyVisual.xaml.cs
+++ src/settings-ui/Settings.UI/SettingsXAML/Controls/KeyVisual.xaml.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using Microsoft.PowerToys.Settings.UI.Helpers;
 using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Automation;
@@ -16,13 +17,15 @@ using Windows.UI;
 namespace Microsoft.PowerToys.Settings.UI.Controls
 {
     [TemplatePart(Name = KeyPresenter, Type = typeof(ContentPresenter))]
-    [TemplateVisualState(Name = "Normal", GroupName = "CommonStates")]
-    [TemplateVisualState(Name = "Disabled", GroupName = "CommonStates")]
-    [TemplateVisualState(Name = "Error", GroupName = "CommonStates")]
+    [TemplateVisualState(Name = NormalState, GroupName = "CommonStates")]
+    [TemplateVisualState(Name = DisabledState, GroupName = "CommonStates")]
+    [TemplateVisualState(Name = InvalidState, GroupName = "CommonStates")]
     public sealed partial class KeyVisual : Control
     {
         private const string KeyPresenter = "KeyPresenter";
-        private KeyVisual _keyVisual;
+        private const string NormalState = "Normal";
+        private const string DisabledState = "Disabled";
+        private const string InvalidState = "Invalid";
         private ContentPresenter _keyPresenter;
 
         public object Content
@@ -57,8 +60,7 @@ namespace Microsoft.PowerToys.Settings.UI.Controls
         protected override void OnApplyTemplate()
         {
             IsEnabledChanged -= KeyVisual_IsEnabledChanged;
-            _keyVisual = (KeyVisual)this;
-            _keyPresenter = (ContentPresenter)_keyVisual.GetTemplateChild(KeyPresenter);
+            _keyPresenter = (ContentPresenter)this.GetTemplateChild(KeyPresenter);
             Update();
             SetVisualStates();
             IsEnabledChanged += KeyVisual_IsEnabledChanged;
@@ -86,81 +88,81 @@ namespace Microsoft.PowerToys.Settings.UI.Controls
 
         private void SetVisualStates()
         {
-            if (_keyVisual != null)
+            if (this != null)
             {
-                if (_keyVisual.IsInvalid)
+                if (IsInvalid)
                 {
-                    VisualStateManager.GoToState(_keyVisual, "Invalid", true);
+                    VisualStateManager.GoToState(this, InvalidState, true);
                 }
-                else if (!_keyVisual.IsEnabled)
+                else if (!IsEnabled)
                 {
-                    VisualStateManager.GoToState(_keyVisual, "Disabled", true);
+                    VisualStateManager.GoToState(this, DisabledState, true);
                 }
                 else
                 {
-                    VisualStateManager.GoToState(_keyVisual, "Normal", true);
+                    VisualStateManager.GoToState(this, NormalState, true);
                 }
             }
         }
 
         private void Update()
         {
-            if (_keyVisual != null && _keyVisual.Content != null)
+            if (this != null && this.Content != null)
             {
                 FrameworkElement contentElement = null;
                 string accessibleName = string.Empty;
 
-                if (_keyVisual.Content.GetType() == typeof(string))
+                if (Content.GetType() == typeof(string))
                 {
                     // For plain strings, set directly
-                    _keyVisual._keyPresenter.Content = _keyVisual.Content;
-                    _keyVisual._keyPresenter.Margin = new Thickness(0, -1, 0, 0);
-                    AutomationProperties.SetName(_keyVisual._keyPresenter, _keyVisual.Content.ToString());
+                    _keyPresenter.Content = Content;
+                    _keyPresenter.Margin = new Thickness(0, -1, 0, 0);
+                    AutomationProperties.SetName(_keyPresenter, Content.ToString());
                     return;
                 }
 
-                switch ((int)_keyVisual.Content)
+                switch ((int)Content)
                 {
                     case 13: // Enter key
-                        accessibleName = "Enter key";
+                        accessibleName = ResourceLoaderInstance.ResourceLoader.GetString("KeyEnter");
                         contentElement = CreateGlyphTextBlock("\uE751", accessibleName);
                         break;
 
                     case 8: // Back key
-                        accessibleName = "Back key";
+                        accessibleName = ResourceLoaderInstance.ResourceLoader.GetString("KeyBack");
                         contentElement = CreateGlyphTextBlock("\uE750", accessibleName);
                         break;
 
                     case 16: // Right Shift
                     case 160: // Left Shift
                     case 161: // Shift key
-                        accessibleName = "Shift key";
+                        accessibleName = ResourceLoaderInstance.ResourceLoader.GetString("KeyShift");
                         contentElement = CreateGlyphTextBlock("\uE752", accessibleName);
                         break;
 
                     case 38: // Up arrow
-                        accessibleName = "Up arrow key";
+                        accessibleName = ResourceLoaderInstance.ResourceLoader.GetString("KeyUpArrow");
                         contentElement = CreateGlyphTextBlock("\uE0E4", accessibleName);
                         break;
 
                     case 40: // Down arrow
-                        accessibleName = "Down arrow key";
+                        accessibleName = ResourceLoaderInstance.ResourceLoader.GetString("KeyDownArrow");
                         contentElement = CreateGlyphTextBlock("\uE0E5", accessibleName);
                         break;
 
                     case 37: // Left arrow
-                        accessibleName = "Left arrow key";
+                        accessibleName = ResourceLoaderInstance.ResourceLoader.GetString("KeyLeftArrow");
                         contentElement = CreateGlyphTextBlock("\uE0E2", accessibleName);
                         break;
 
                     case 39: // Right arrow
-                        accessibleName = "Right arrow key";
+                        accessibleName = ResourceLoaderInstance.ResourceLoader.GetString("KeyRightArrow");
                         contentElement = CreateGlyphTextBlock("\uE0E3", accessibleName);
                         break;
 
                     case 91: // Left Windows key
                     case 92: // Right Windows key
-                        accessibleName = "Windows key";
+                        accessibleName = ResourceLoaderInstance.ResourceLoader.GetString("KeyWindows");
 
                         Geometry geometry = GetGeometryFromAppResources("WindowsLogoData");
 
@@ -180,8 +182,8 @@ namespace Microsoft.PowerToys.Settings.UI.Controls
                             Child = winIcon,
                             HorizontalAlignment = HorizontalAlignment.Center,
                             VerticalAlignment = VerticalAlignment.Center,
-                            Height = _keyVisual.FontSize * 0.8,
-                            Width = _keyVisual.FontSize * 0.8,
+                            Height = FontSize * 0.8,
+                            Width = FontSize * 0.8,
                         };
 
                         AutomationProperties.SetName(winIconContainer, accessibleName);
@@ -189,17 +191,17 @@ namespace Microsoft.PowerToys.Settings.UI.Controls
                         break;
 
                     default:
-                        accessibleName = ((VirtualKey)_keyVisual.Content).ToString();
+                        accessibleName = ((VirtualKey)Content).ToString();
                         contentElement = new TextBlock
                         {
                             Text = accessibleName,
-                            FontSize = _keyVisual.FontSize * 0.8,
+                            FontSize = FontSize * 0.8,
                         };
                         AutomationProperties.SetName(contentElement, accessibleName);
                         break;
                 }
 
-                _keyVisual._keyPresenter.Content = contentElement;
+                _keyPresenter.Content = contentElement;
                 AccessibleName = accessibleName;
             }
         }
@@ -210,7 +212,7 @@ namespace Microsoft.PowerToys.Settings.UI.Controls
             {
                 Text = glyph,
                 FontFamily = new FontFamily("Segoe Fluent Icons, Segoe MDL2 Assets"),
-                FontSize = _keyVisual.FontSize * 0.8,
+                FontSize = FontSize * 0.8,
             };
             AutomationProperties.SetName(tb, accessibleName);
             return tb;
