72be09554e719a269bd7d803e0e9566e60345f22
Aaron Junker <Aaron.Junker@outlook.com>
2025-08-03 16:30:59 +0200

    Factor out TaskbarIndicator

--- src/modules/ShortcutGuide/ShortcutGuide.Ui/ShortcutGuideXAML/ShortcutView.xaml.cs
+++ src/modules/ShortcutGuide/ShortcutGuide.Ui/ShortcutGuideXAML/ShortcutView.xaml.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.IO;
