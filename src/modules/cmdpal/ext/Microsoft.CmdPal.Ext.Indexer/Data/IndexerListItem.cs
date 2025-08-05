microsoft/PowerToys

commit 3a0487f74aa344a10c09abdb577880ced1d7bf70

cmdpal: Add "file" context items to the run items too (#40768)
After #39955, the "exe" items from the shell commands only ever have the
"Run{as admin, as other user}" commands. This adds the rest of the
"file" commands - copy path, open in explorer, etc.

This shuffles around some commands into the toolkit and common commands
project to make this easier.

<img width="814" height="505" alt="image"
src="https://github.com/user-attachments/assets/36ae2c75-d4d6-4762-98ec-796986f39c20"
/>

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Indexer/Data/IndexerListItem.cs
@@ -3,10 +3,11 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
-using Microsoft.CmdPal.Ext.Indexer.Commands;
+using System.IO;
+using System.Linq;
+using Microsoft.CmdPal.Common.Commands;
 using Microsoft.CmdPal.Ext.Indexer.Pages;
 using Microsoft.CmdPal.Ext.Indexer.Properties;
-using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 using Windows.Foundation.Metadata;
 
@@ -28,51 +29,79 @@ internal sealed partial class IndexerListItem : ListItem
     public IndexerListItem(
         IndexerItem indexerItem,
         IncludeBrowseCommand browseByDefault = IncludeBrowseCommand.Include)
-        : base(new OpenFileCommand(indexerItem))
+        : base()
     {
         FilePath = indexerItem.FullPath;
 
         Title = indexerItem.FileName;
         Subtitle = indexerItem.FullPath;
-        List<CommandContextItem> context = [];
-        if (indexerItem.IsDirectory())
+
+        var commands = FileCommands(indexerItem.FullPath, browseByDefault);
+        if (commands.Any())
         {
-            var directoryPage = new DirectoryPage(indexerItem.FullPath);
+            Command = commands.First().Command;
+            MoreCommands = commands.Skip(1).ToArray();
+        }
+    }
+
+    public static IEnumerable<CommandContextItem> FileCommands(string fullPath)
+    {
+        return FileCommands(fullPath, IncludeBrowseCommand.Include);
+    }
+
+    internal static IEnumerable<CommandContextItem> FileCommands(
+        string fullPath,
+        IncludeBrowseCommand browseByDefault = IncludeBrowseCommand.Include)
+    {
+        List<CommandContextItem> commands = [];
+        if (!Path.Exists(fullPath))
+        {
+            return commands;
+        }
+
+        // detect whether it is a directory or file
+        var attr = File.GetAttributes(fullPath);
+        var isDir = (attr & FileAttributes.Directory) == FileAttributes.Directory;
+
+        var openCommand = new OpenFileCommand(fullPath) { Name = Resources.Indexer_Command_OpenFile };
+        if (isDir)
+        {
+            var directoryPage = new DirectoryPage(fullPath);
             if (browseByDefault == IncludeBrowseCommand.AsDefault)
             {
-                // Swap the open file command into the context menu
-                context.Add(new CommandContextItem(Command));
-                Command = directoryPage;
+                // AsDefault: browse dir first, then open in explorer
+                commands.Add(new CommandContextItem(directoryPage));
+                commands.Add(new CommandContextItem(openCommand));
             }
             else if (browseByDefault == IncludeBrowseCommand.Include)
             {
-                context.Add(new CommandContextItem(directoryPage));
+                // AsDefault: open in explorer first, then browse
+                commands.Add(new CommandContextItem(openCommand));
+                commands.Add(new CommandContextItem(directoryPage));
+            }
+            else if (browseByDefault == IncludeBrowseCommand.Exclude)
+            {
+                // AsDefault: Just open in explorer
+                commands.Add(new CommandContextItem(openCommand));
             }
         }
 
-        IContextItem[] moreCommands = [
-            ..context,
-            new CommandContextItem(new OpenWithCommand(indexerItem))];
+        commands.Add(new CommandContextItem(new OpenWithCommand(fullPath)));
+        commands.Add(new CommandContextItem(new ShowFileInFolderCommand(fullPath) { Name = Resources.Indexer_Command_ShowInFolder }));
+        commands.Add(new CommandContextItem(new CopyPathCommand(fullPath) { Name = Resources.Indexer_Command_CopyPath }));
+        commands.Add(new CommandContextItem(new OpenInConsoleCommand(fullPath)));
+        commands.Add(new CommandContextItem(new OpenPropertiesCommand(fullPath)));
 
         if (IsActionsFeatureEnabled && ApiInformation.IsApiContractPresent("Windows.AI.Actions.ActionsContract", 4))
         {
-            var actionsListContextItem = new ActionsListContextItem(indexerItem.FullPath);
+            var actionsListContextItem = new ActionsListContextItem(fullPath);
             if (actionsListContextItem.AnyActions())
             {
-                moreCommands = [
-                    .. moreCommands,
-                    actionsListContextItem
-                ];
+                commands.Add(actionsListContextItem);
             }
         }
 
-        MoreCommands = [
-            .. moreCommands,
-            new CommandContextItem(new ShowFileInFolderCommand(indexerItem.FullPath) { Name = Resources.Indexer_Command_ShowInFolder }),
-            new CommandContextItem(new CopyPathCommand(indexerItem)),
-            new CommandContextItem(new OpenInConsoleCommand(indexerItem)),
-            new CommandContextItem(new OpenPropertiesCommand(indexerItem)),
-        ];
+        return commands;
     }
 }
 

src/modules/cmdpal/Microsoft.CmdPal.Common/Commands/ExecuteActionCommand.cs
@@ -9,11 +9,11 @@ using Windows.AI.Actions.Hosting;
 
 namespace Microsoft.CmdPal.Ext.Indexer.Commands;
 
-internal sealed partial class ExecuteActionCommand : InvokableCommand
+public sealed partial class ExecuteActionCommand : InvokableCommand
 {
     private readonly ActionInstance actionInstance;
 
-    internal ExecuteActionCommand(ActionInstance actionInstance)
+    public ExecuteActionCommand(ActionInstance actionInstance)
     {
         this.actionInstance = actionInstance;
         this.Name = actionInstance.DisplayInfo.Description;

src/modules/cmdpal/Microsoft.CmdPal.Common/Commands/OpenInConsoleCommand.cs
@@ -6,28 +6,29 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using ManagedCommon;
-using Microsoft.CmdPal.Ext.Indexer.Data;
-using Microsoft.CmdPal.Ext.Indexer.Properties;
+using Microsoft.CmdPal.Common.Properties;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 
-namespace Microsoft.CmdPal.Ext.Indexer.Commands;
+namespace Microsoft.CmdPal.Common.Commands;
 
-internal sealed partial class OpenInConsoleCommand : InvokableCommand
+public partial class OpenInConsoleCommand : InvokableCommand
 {
-    private readonly IndexerItem _item;
+    internal static IconInfo OpenInConsoleIcon { get; } = new("\uE756");
 
-    internal OpenInConsoleCommand(IndexerItem item)
+    private readonly string _path;
+
+    public OpenInConsoleCommand(string fullPath)
     {
-        this._item = item;
+        this._path = fullPath;
         this.Name = Resources.Indexer_Command_OpenPathInConsole;
-        this.Icon = new IconInfo("\uE756");
+        this.Icon = OpenInConsoleIcon;
     }
 
     public override CommandResult Invoke()
     {
         using (var process = new Process())
         {
-            process.StartInfo.WorkingDirectory = Path.GetDirectoryName(_item.FullPath);
+            process.StartInfo.WorkingDirectory = Path.GetDirectoryName(_path);
             process.StartInfo.FileName = "cmd.exe";
 
             try
@@ -36,10 +37,10 @@ internal sealed partial class OpenInConsoleCommand : InvokableCommand
             }
             catch (Win32Exception ex)
             {
-                Logger.LogError($"Unable to open {_item.FullPath}", ex);
+                Logger.LogError($"Unable to open '{_path}'", ex);
             }
         }
 
-        return CommandResult.GoHome();
+        return CommandResult.Dismiss();
     }
 }

src/modules/cmdpal/Microsoft.CmdPal.Common/Commands/OpenPropertiesCommand.cs
@@ -6,17 +6,17 @@ using System;
 using System.Runtime.InteropServices;
 using ManagedCommon;
 using ManagedCsWin32;
-using Microsoft.CmdPal.Ext.Indexer.Data;
-using Microsoft.CmdPal.Ext.Indexer.Indexer.Utils;
-using Microsoft.CmdPal.Ext.Indexer.Properties;
+using Microsoft.CmdPal.Common.Properties;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 using Windows.Win32.UI.WindowsAndMessaging;
 
-namespace Microsoft.CmdPal.Ext.Indexer.Commands;
+namespace Microsoft.CmdPal.Common.Commands;
 
-internal sealed partial class OpenPropertiesCommand : InvokableCommand
+public partial class OpenPropertiesCommand : InvokableCommand
 {
-    private readonly IndexerItem _item;
+    internal static IconInfo OpenPropertiesIcon { get; } = new("\uE90F");
+
+    private readonly string _path;
 
     private static unsafe bool ShowFileProperties(string filename)
     {
@@ -31,7 +31,7 @@ internal sealed partial class OpenPropertiesCommand : InvokableCommand
                 LpVerb = propertiesPtr,
                 LpFile = filenamePtr,
                 Show = (int)SHOW_WINDOW_CMD.SW_SHOW,
-                FMask = NativeHelpers.SEEMASKINVOKEIDLIST,
+                FMask = global::Windows.Win32.PInvoke.SEE_MASK_INVOKEIDLIST,
             };
 
             return Shell32.ShellExecuteEx(ref info);
@@ -43,24 +43,24 @@ internal sealed partial class OpenPropertiesCommand : InvokableCommand
         }
     }
 
-    internal OpenPropertiesCommand(IndexerItem item)
+    public OpenPropertiesCommand(string fullPath)
     {
-        this._item = item;
+        this._path = fullPath;
         this.Name = Resources.Indexer_Command_OpenProperties;
-        this.Icon = new IconInfo("\uE90F");
+        this.Icon = OpenPropertiesIcon;
     }
 
     public override CommandResult Invoke()
     {
         try
         {
-            ShowFileProperties(_item.FullPath);
+            ShowFileProperties(_path);
         }
         catch (Exception ex)
         {
             Logger.LogError("Error showing file properties: ", ex);
         }
 
-        return CommandResult.GoHome();
+        return CommandResult.Dismiss();
     }
 }

src/modules/cmdpal/Microsoft.CmdPal.Common/Commands/OpenWithCommand.cs
@@ -4,17 +4,17 @@
 
 using System.Runtime.InteropServices;
 using ManagedCsWin32;
-using Microsoft.CmdPal.Ext.Indexer.Data;
-using Microsoft.CmdPal.Ext.Indexer.Indexer.Utils;
-using Microsoft.CmdPal.Ext.Indexer.Properties;
+using Microsoft.CmdPal.Common.Properties;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 using Windows.Win32.UI.WindowsAndMessaging;
 
-namespace Microsoft.CmdPal.Ext.Indexer.Commands;
+namespace Microsoft.CmdPal.Common.Commands;
 
-internal sealed partial class OpenWithCommand : InvokableCommand
+public partial class OpenWithCommand : InvokableCommand
 {
-    private readonly IndexerItem _item;
+    internal static IconInfo OpenWithIcon { get; } = new("\uE7AC");
+
+    private readonly string _path;
 
     private static unsafe bool OpenWith(string filename)
     {
@@ -29,7 +29,7 @@ internal sealed partial class OpenWithCommand : InvokableCommand
                 LpVerb = verbPtr,
                 LpFile = filenamePtr,
                 Show = (int)SHOW_WINDOW_CMD.SW_SHOWNORMAL,
-                FMask = NativeHelpers.SEEMASKINVOKEIDLIST,
+                FMask = global::Windows.Win32.PInvoke.SEE_MASK_INVOKEIDLIST,
             };
 
             return Shell32.ShellExecuteEx(ref info);
@@ -41,16 +41,16 @@ internal sealed partial class OpenWithCommand : InvokableCommand
         }
     }
 
-    internal OpenWithCommand(IndexerItem item)
+    public OpenWithCommand(string fullPath)
     {
-        this._item = item;
+        this._path = fullPath;
         this.Name = Resources.Indexer_Command_OpenWith;
-        this.Icon = new IconInfo("\uE7AC");
+        this.Icon = OpenWithIcon;
     }
 
     public override CommandResult Invoke()
     {
-        OpenWith(_item.FullPath);
+        OpenWith(_path);
 
         return CommandResult.GoHome();
     }

src/modules/cmdpal/Microsoft.CmdPal.Common/Microsoft.CmdPal.Common.csproj
@@ -28,7 +28,24 @@
   </ItemGroup>
 
   <ItemGroup>
+    <ProjectReference Include="..\..\..\common\ManagedCommon\ManagedCommon.csproj" />
+    <ProjectReference Include="..\..\..\common\ManagedCsWin32\ManagedCsWin32.csproj" />
     <ProjectReference Include="..\extensionsdk\Microsoft.CommandPalette.Extensions.Toolkit\Microsoft.CommandPalette.Extensions.Toolkit.csproj" />
+  </ItemGroup>
+
+  <ItemGroup>
+    <Compile Update="Properties\Resources.Designer.cs">
+      <DesignTime>True</DesignTime>
+      <AutoGen>True</AutoGen>
+      <DependentUpon>Resources.resx</DependentUpon>
+    </Compile>
+  </ItemGroup>
+
+  <ItemGroup>
+    <EmbeddedResource Update="Properties\Resources.resx">
+      <Generator>ResXFileCodeGenerator</Generator>
+      <LastGenOutput>Resources.Designer.cs</LastGenOutput>
+    </EmbeddedResource>
   </ItemGroup>
 
 </Project>

src/modules/cmdpal/Microsoft.CmdPal.Common/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.CommandPalette.Extensions.Toolkit.Properties {
+namespace Microsoft.CmdPal.Common.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Microsoft.CommandPalette.Extensions.Toolkit.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.CommandPalette.Extensions.Toolkit.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.CmdPal.Common.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -61,74 +61,38 @@ namespace Microsoft.CommandPalette.Extensions.Toolkit.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Invoke.
+        ///   Looks up a localized string similar to Open path in console.
         /// </summary>
-        internal static string AnonymousCommand_Invoke {
+        internal static string Indexer_Command_OpenPathInConsole {
             get {
-                return ResourceManager.GetString("AnonymousCommand_Invoke", resourceCulture);
+                return ResourceManager.GetString("Indexer_Command_OpenPathInConsole", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Copy path.
+        ///   Looks up a localized string similar to Properties.
         /// </summary>
-        internal static string CopyPathTextCommand_Name {
+        internal static string Indexer_Command_OpenProperties {
             get {
-                return ResourceManager.GetString("CopyPathTextCommand_Name", resourceCulture);
+                return ResourceManager.GetString("Indexer_Command_OpenProperties", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Copied path to clipboard.
+        ///   Looks up a localized string similar to Open with.
         /// </summary>
-        internal static string CopyPathTextCommand_Result {
+        internal static string Indexer_Command_OpenWith {
             get {
-                return ResourceManager.GetString("CopyPathTextCommand_Result", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Copied to clipboard.
-        /// </summary>
-        internal static string CopyTextCommand_CopiedToClipboard {
-            get {
-                return ResourceManager.GetString("CopyTextCommand_CopiedToClipboard", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Copy.
-        /// </summary>
-        internal static string CopyTextCommand_Copy {
-            get {
-                return ResourceManager.GetString("CopyTextCommand_Copy", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Open.
-        /// </summary>
-        internal static string OpenUrlCommand_Open {
-            get {
-                return ResourceManager.GetString("OpenUrlCommand_Open", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Settings.
-        /// </summary>
-        internal static string Settings {
-            get {
-                return ResourceManager.GetString("Settings", resourceCulture);
+                return ResourceManager.GetString("Indexer_Command_OpenWith", resourceCulture);
             }
         }
         
         /// <summary>
         ///   Looks up a localized string similar to Show in folder.
         /// </summary>
-        internal static string ShowFileInFolderCommand_ShowInFolder {
+        internal static string Indexer_Command_ShowInFolder {
             get {
-                return ResourceManager.GetString("ShowFileInFolderCommand_ShowInFolder", resourceCulture);
+                return ResourceManager.GetString("Indexer_Command_ShowInFolder", resourceCulture);
             }
         }
     }

src/modules/cmdpal/Microsoft.CmdPal.Common/Properties/Resources.resx
@@ -117,28 +117,16 @@
   <resheader name="writer">
     <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
   </resheader>
-  <data name="AnonymousCommand_Invoke" xml:space="preserve">
-    <value>Invoke</value>
+  <data name="Indexer_Command_OpenPathInConsole" xml:space="preserve">
+    <value>Open path in console</value>
   </data>
-  <data name="CopyTextCommand_Copy" xml:space="preserve">
-    <value>Copy</value>
+  <data name="Indexer_Command_OpenProperties" xml:space="preserve">
+    <value>Properties</value>
   </data>
-  <data name="CopyTextCommand_CopiedToClipboard" xml:space="preserve">
-    <value>Copied to clipboard</value>
+  <data name="Indexer_Command_OpenWith" xml:space="preserve">
+    <value>Open with</value>
   </data>
-  <data name="CopyPathTextCommand_Name" xml:space="preserve">
-    <value>Copy path</value>
-  </data>
-  <data name="CopyPathTextCommand_Result" xml:space="preserve">
-    <value>Copied path to clipboard</value>
-  </data>
-  <data name="OpenUrlCommand_Open" xml:space="preserve">
-    <value>Open</value>
-  </data>
-  <data name="Settings" xml:space="preserve">
-    <value>Settings</value>
-  </data>
-  <data name="ShowFileInFolderCommand_ShowInFolder" xml:space="preserve">
+  <data name="Indexer_Command_ShowInFolder" xml:space="preserve">
     <value>Show in folder</value>
   </data>
 </root>

commit 5fe03bb9c701560f5c52e73ab08af337e0b1cfcc

CmdPal: fix files not having an open command
Yea, it's that dumb.

Regressed in #40768

src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Indexer/Data/IndexerListItem.cs
@@ -85,6 +85,10 @@ internal sealed partial class IndexerListItem : ListItem
                 commands.Add(new CommandContextItem(openCommand));
             }
         }
+        else
+        {
+            commands.Add(new CommandContextItem(openCommand));
+        }
 
         commands.Add(new CommandContextItem(new OpenWithCommand(fullPath)));
         commands.Add(new CommandContextItem(new ShowFileInFolderCommand(fullPath) { Name = Resources.Indexer_Command_ShowInFolder }));