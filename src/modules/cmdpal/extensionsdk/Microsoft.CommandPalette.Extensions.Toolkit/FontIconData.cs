dd1a60c9f670d66627b29f853dc7f0894abec024
Mike Griese <migrie@microsoft.com>
2025-08-08 06:48:03 -0500

    really VS really

--- src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/FontIconData.cs
+++ src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/FontIconData.cs
@@ -3,16 +3,15 @@
 // See the LICENSE file in the project root for more information.
 
 using Windows.Foundation.Collections;
-using Windows.Storage.Streams;
 
 namespace Microsoft.CommandPalette.Extensions.Toolkit;
 
 /// <summary>
 /// Represents an icon that is a font glyph.
 /// This is used for icons that are defined by a specific font face,
-/// such as Wingdings. 
-/// 
-/// Note that Command Palette will default to using the Segoe Fluent Icons, 
+/// such as Wingdings.
+///
+/// Note that Command Palette will default to using the Segoe Fluent Icons,
 /// Segoe MDL2 Assets font for glyphs in the Segoe UI Symbol range, or Segoe
 /// UI for any other glyphs. This class is only needed if you want a non-Segoe
 /// font icon.
