dcc25e81f1a3feb2fe833ef1b8ba2e6ea0994dd7
Yu Leng <yuleng@microsoft.com>
2025-08-20 14:57:03 +0800

    Fix lint issues

--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Bookmark/IBookmarkDataSource.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Bookmark/IBookmarkDataSource.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
-
 namespace Microsoft.CmdPal.Ext.Bookmarks;
 
 public interface IBookmarkDataSource
