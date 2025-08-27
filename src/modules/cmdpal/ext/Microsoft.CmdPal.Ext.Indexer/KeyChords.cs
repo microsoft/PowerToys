5d15eee0401c5a4a0e22bb741e17372f8862e57f
Jiří Polášek <me@jiripolasek.com>
2025-08-27 13:43:10 +0200

    Add keyboard shortcuts to IndexerListItem commands to mirror ones in All Apps
    Updated the `IndexerListItem` class to include new command context items with `RequestedShortcut` properties for showing files, copying paths, and opening in console. Removed previous command context items to streamline user interaction.
    
    Introduced a new `KeyChords` class in `KeyChords.cs` to define and manage key chords for these actions, enhancing code organization and maintainability.

---
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Indexer/KeyChords.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+using Windows.System;
+
+namespace Microsoft.CmdPal.Ext.Indexer;
+
+internal static class KeyChords
+{
+    internal static KeyChord OpenFileLocation { get; } = KeyChordHelpers.FromModifiers(ctrl: true, shift: true, vkey: (int)VirtualKey.E);
+
+    internal static KeyChord CopyFilePath { get; } = KeyChordHelpers.FromModifiers(ctrl: true, shift: true, vkey: (int)VirtualKey.C);
+
+    internal static KeyChord OpenInConsole { get; } = KeyChordHelpers.FromModifiers(ctrl: true, shift: true, vkey: (int)VirtualKey.R);
+}
