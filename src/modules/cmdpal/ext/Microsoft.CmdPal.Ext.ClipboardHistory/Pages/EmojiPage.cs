4a924571de2faa0ca556192ba95c09123aa0b6cc
Mike Griese <migrie@microsoft.com>
2025-08-29 14:06:53 -0500

    init once

--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.ClipboardHistory/Pages/EmojiPage.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.ClipboardHistory/Pages/EmojiPage.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
@@ -10,6 +11,8 @@ namespace Microsoft.CmdPal.Ext.ClipboardHistory.Pages;
 
 public sealed partial class EmojiPage : ListPage
 {
+    private readonly Dictionary<string, List<ListItem>> _emojiListItems;
+
     public EmojiPage()
     {
         Icon = new IconInfo("\uE899");
@@ -17,11 +20,18 @@ public sealed partial class EmojiPage : ListPage
         Id = "com.microsoft.cmdpal.emoji";
         ShowDetails = false;
         GridProperties = new SmallGridLayout();
+
+        _emojiListItems = new Dictionary<string, List<ListItem>>();
+        foreach (var group in EmojiDict.Data)
+        {
+            var listItems = group.Value.Select(s => new EmojiListItem(s.Emoji) { Title = s.Name }).Cast<ListItem>().ToList();
+            _emojiListItems.Add(group.Key, listItems);
+        }
     }
 
     public override IListItem[] GetItems()
     {
-        var items = EmojiDict.Data["SmileysAndEmotion"].Select(s => new EmojiListItem(s.Emoji) { Title = s.Name });
+        var items = _emojiListItems["SmileysAndEmotion"];
         return items.ToArray();
     }
 }
