5a91530db70e023704e1a8568d64ad19c0b1431f
Mike Griese <migrie@microsoft.com>
2025-09-12 13:06:08 -0500

    code that compiles

---
+++ src/modules/cmdpal/ext/SamplePagesExtension/Pages/ParameterSamples.cs
@@ -0,0 +1,178 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace SamplePagesExtension.Pages;
+
+#pragma warning disable SA1402 // File may only contain a single type
+#pragma warning disable SA1649 // File name should match first type name
+#nullable enable
+
+public sealed partial class CreateNoteParametersPage : ParametersPage
+{
+    private readonly SelectFolderPage _selectFolderPage = new();
+
+    private readonly StringParameterRun _titleParameter;
+    private readonly CommandParameterRun _folderParameter;
+
+    private readonly List<IParameterRun> _parameters;
+
+    private readonly CreateNoteCommand _command;
+    private readonly ListItem _item;
+
+    public override IParameterRun[] Parameters => _parameters.ToArray();
+
+    public override IListItem Command => _item;
+
+    public CreateNoteParametersPage()
+    {
+        _titleParameter = new StringParameterRun()
+        {
+            PlaceholderText = "Note title",
+        };
+
+        _folderParameter = new CommandParameterRun()
+        {
+            PlaceholderText = "Select folder",
+            Command = _selectFolderPage,
+        };
+
+        _command = new() { TitleParameter = _titleParameter, FolderParameter = _folderParameter };
+        _item = new(_command);
+
+        _parameters = new List<IParameterRun>
+        {
+            new LabelRun("Create a note"),
+            _titleParameter,
+            new LabelRun("in"),
+            _folderParameter,
+        };
+
+        _selectFolderPage.FolderSelected += (s, folder) =>
+            {
+                _folderParameter.Value = folder;
+                _folderParameter.Icon = folder.Icon;
+                _folderParameter.DisplayText = folder.Name;
+            };
+    }
+}
+
+internal sealed class Folder
+{
+    public string? Name { get; set; }
+
+    public IconInfo? Icon { get; set; }
+}
+
+internal sealed partial class CreateNoteCommand : InvokableCommand
+{
+    internal required IStringParameterRun TitleParameter { get; init; } // set by the parameters page
+
+    internal required CommandParameterRun FolderParameter { get; init; } // set by the parameters page
+
+    public override IconInfo Icon => new("NoteAdd");
+
+    public override ICommandResult Invoke()
+    {
+        var title = TitleParameter.Text;
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            var t = new ToastStatusMessage(new StatusMessage() { Message = "Title is required", State = MessageState.Error });
+            t.Show();
+            return CommandResult.KeepOpen();
+        }
+
+        var folder = FolderParameter.Value;
+        if (folder is not Folder)
+        {
+            // This is okay, we'll create the note in the default folder
+        }
+
+        // Create the note in the specified folder
+        NoteService.CreateNoteInFolder(title, folder); // whatever your backend is
+
+        return CommandResult.Dismiss();
+    }
+}
+
+public sealed partial class SelectFolderPage : ListPage
+{
+    internal event EventHandler<Folder>? FolderSelected;
+
+    public SelectFolderPage()
+    {
+    }
+
+    private sealed partial class SelectFolderCommand : InvokableCommand
+    {
+        internal event EventHandler<Folder>? FolderSelected;
+
+        private readonly Folder _folder;
+
+        public override IconInfo Icon => _folder?.Icon ?? new(string.Empty);
+
+        public string Title => _folder?.Name ?? string.Empty;
+
+        public SelectFolderCommand(Folder folder)
+        {
+            _folder = folder;
+        }
+
+        public override ICommandResult Invoke()
+        {
+            FolderSelected?.Invoke(this, _folder);
+            return CommandResult.KeepOpen();
+        }
+    }
+
+    public override IListItem[] GetItems()
+    {
+        var listItems = new List<ListItem>();
+
+        // Populate the list with folders
+        var folders = FolderService.GetFolders(); // whatever your backend is
+        foreach (var value in folders)
+        {
+            var command = new SelectFolderCommand(value);
+            command.FolderSelected += (s, v) => { this.FolderSelected?.Invoke(this, v); };
+            var listItem = new ListItem(command);
+            listItems.Add(listItem);
+        }
+
+        return listItems.ToArray();
+    }
+}
+
+internal sealed class NoteService
+{
+    internal static void CreateNoteInFolder(string title, object? folder)
+    {
+        // Your backend logic to create a note
+        var toast = new ToastStatusMessage(new StatusMessage() { Message = $"Created note '{title}'" });
+        toast.Show();
+    }
+}
+
+internal sealed class FolderService
+{
+    internal static IEnumerable<Folder> GetFolders()
+    {
+        // Your backend logic to get folders
+        return new List<Folder>
+        {
+            new() { Name = "Personal", Icon = new IconInfo("\uEc25") },
+            new() { Name = "Work", Icon = new IconInfo("\uE821") },
+            new() { Name = "Ideas", Icon = new IconInfo("\uEA80") },
+        };
+    }
+}
+
+
+#pragma warning restore SA1649 // File name should match first type name
+#pragma warning restore SA1402 // File may only contain a single type
+#nullable disable
---
+++ src/modules/cmdpal/ext/SamplePagesExtension/Parameters.cs
@@ -0,0 +1,310 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using Windows.Foundation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Microsoft.CommandPalette.Extensions.Toolkit;
+
+#pragma warning disable SA1402 // File may only contain a single type
+#pragma warning disable SA1649 // File name should match first type name
+#nullable enable
+public interface IRequiresHostHwnd
+{
+    void SetHostHwnd(nint hostHwnd);
+}
+
+public partial class LabelRun : BaseObservable, ILabelRun
+{
+    private string? _text = string.Empty;
+
+    public virtual string? Text
+    {
+        get => _text;
+        set
+        {
+            _text = value;
+            OnPropertyChanged(nameof(Text));
+        }
+    }
+
+    public LabelRun(string text)
+    {
+        _text = text;
+    }
+
+    public LabelRun()
+    {
+    }
+}
+
+public partial class ParameterValueRun : BaseObservable, IParameterValueRun
+{
+    private string _placeholderText = string.Empty;
+
+    public virtual string PlaceholderText
+    {
+        get => _placeholderText;
+        set
+        {
+            _placeholderText = value;
+            OnPropertyChanged(nameof(PlaceholderText));
+        }
+    }
+
+    private bool _needsValue = true;
+
+    public virtual bool NeedsValue
+    {
+        get => _needsValue;
+        set
+        {
+            _needsValue = value;
+            OnPropertyChanged(nameof(NeedsValue));
+        }
+    }
+}
+
+public partial class StringParameterRun : ParameterValueRun, IStringParameterRun
+{
+    private string _text = string.Empty;
+
+    public virtual string Text
+    {
+        get => _text;
+        set
+        {
+            _text = value;
+            OnPropertyChanged(nameof(Text));
+        }
+    }
+
+    public override bool NeedsValue => string.IsNullOrEmpty(Text);
+
+    public StringParameterRun()
+    {
+    }
+
+    public StringParameterRun(string placeholderText)
+    {
+        PlaceholderText = placeholderText;
+    }
+}
+
+public partial class CommandParameterRun : ParameterValueRun
+{
+    private string? _displayText;
+
+    public virtual string? DisplayText
+    {
+        get => _displayText;
+        set
+        {
+            _displayText = value;
+            OnPropertyChanged(nameof(DisplayText));
+        }
+    }
+
+    private ICommand? _command;
+
+    public virtual ICommand? Command
+    {
+        get => _command;
+        set
+        {
+            _command = value;
+            OnPropertyChanged(nameof(Command));
+        }
+    }
+
+    private IIconInfo? _icon;
+
+    public virtual IIconInfo? Icon
+    {
+        get => _icon;
+        set
+        {
+            _icon = value;
+            OnPropertyChanged(nameof(Icon));
+        }
+    }
+
+    public override bool NeedsValue => Value == null;
+
+    public virtual ICommand? GetSelectValueCommand(ulong hostHwnd)
+    {
+        if (Command is IRequiresHostHwnd requiresHwnd)
+        {
+            requiresHwnd.SetHostHwnd((nint)hostHwnd);
+        }
+
+        return Command;
+    }
+
+    // Toolkit helper: a value for the parameter
+    private object? _value;
+
+    public virtual object? Value
+    {
+        get => _value;
+        set
+        {
+            _value = value;
+            OnPropertyChanged(nameof(Value));
+            OnPropertyChanged(nameof(NeedsValue));
+        }
+    }
+}
+
+internal sealed partial class FilePickerParameterRun : CommandParameterRun
+{
+    public StorageFile? File { get; private set; }
+
+    public FilePickerParameterRun()
+    {
+        var command = new FilePickerCommand();
+        command.FileSelected += (s, file) =>
+        {
+            File = file;
+            if (file != null)
+            {
+                Value = file;
+                DisplayText = file.Name;
+
+                // Icon = new IconInfo("File");
+            }
+            else
+            {
+                Value = null;
+                DisplayText = null;
+
+                // Icon = new IconInfo("File");
+            }
+        };
+        PlaceholderText = "Select a file";
+        Icon = new IconInfo("File");
+        Command = command;
+    }
+
+    private sealed partial class FilePickerCommand : InvokableCommand, IRequiresHostHwnd
+    {
+        public override IconInfo Icon => new("File");
+
+        public override string Name => "Pick a file";
+
+        public event EventHandler<StorageFile?>? FileSelected;
+
+        private nint _hostHwnd;
+
+        public void SetHostHwnd(nint hostHwnd)
+        {
+            _hostHwnd = hostHwnd;
+        }
+
+        public override ICommandResult Invoke()
+        {
+            PickFileAsync();
+            return CommandResult.KeepOpen();
+        }
+
+        private async void PickFileAsync()
+        {
+            var picker = new FileOpenPicker();
+
+            // You need to initialize the picker with a window handle in WinUI 3 desktop apps
+            // See https://learn.microsoft.com/en-us/windows/apps/design/controls/file-open-picker
+            WinRT.Interop.InitializeWithWindow.Initialize(picker, (nint)_hostHwnd);
+
+            var file = await picker.PickSingleFileAsync();
+            FileSelected?.Invoke(this, file);
+        }
+    }
+}
+
+public partial class SelectParameterCommand<T> : InvokableCommand
+{
+    public event TypedEventHandler<object, T>? ValueSelected;
+
+    private T _value;
+
+    public T Value
+    {
+        get => _value; protected set { _value = value; }
+    }
+
+    public SelectParameterCommand(T value)
+    {
+        _value = value;
+    }
+
+    public override ICommandResult Invoke()
+    {
+        ValueSelected?.Invoke(this, _value);
+        return CommandResult.KeepOpen();
+    }
+}
+
+public partial class StaticParameterList<T> : ListPage
+{
+    public event TypedEventHandler<object, T>? ValueSelected;
+
+    private readonly IEnumerable<T> _values;
+    private readonly List<IListItem> _items = new();
+    private bool _isInitialized;
+    private Func<T, ListItem, ListItem> _customizeListItemsCallback;
+
+    // ctor takes an IEnumerable<T> values, and a function to customize the ListItem's depending on the value
+    public StaticParameterList(IEnumerable<T> values, Func<T, ListItem> customizeListItem)
+    {
+        _values = values;
+        _customizeListItemsCallback = (value, listItem) =>
+        {
+            customizeListItem(value);
+            return listItem;
+        };
+    }
+
+    public StaticParameterList(IEnumerable<T> values, Func<T, ListItem, ListItem> customizeListItem)
+    {
+        _values = values;
+        _customizeListItemsCallback = customizeListItem;
+    }
+
+    public override IListItem[] GetItems()
+    {
+        if (!_isInitialized)
+        {
+            Initialize(_values, _customizeListItemsCallback);
+            _isInitialized = true;
+        }
+
+        return _items.ToArray();
+    }
+
+    private void Initialize(IEnumerable<T> values, Func<T, ListItem, ListItem> customizeListItem)
+    {
+        foreach (var value in values)
+        {
+            var command = new SelectParameterCommand<T>(value);
+            command.ValueSelected += (s, v) => ValueSelected?.Invoke(this, v);
+            var listItem = new ListItem(command);
+            var item = customizeListItem(value, listItem);
+            _items.Add(item);
+        }
+    }
+}
+
+public abstract partial class ParametersPage : Page, IParametersPage
+{
+    public abstract IListItem Command { get; }
+
+    public abstract IParameterRun[] Parameters { get; }
+}
+
+#pragma warning restore SA1649 // File name should match first type name
+#pragma warning restore SA1402 // File may only contain a single type
+#nullable disable
