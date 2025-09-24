ae23fb86b253d6b4d46adb6d10e085332b70c300
Mike Griese <migrie@microsoft.com>
2025-09-24 05:48:38 -0500

    now, for real

--- src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/ManagedCsWin32/Shell32.cs
+++ src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/ManagedCsWin32/Shell32.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 
 namespace ManagedCsWin32;
 
-public static partial class Shell32
+internal static partial class Shell32
 {
     [LibraryImport("SHELL32.dll", EntryPoint = "ShellExecuteExW", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
