c3398b0a017342709cc32be536aa619114bab4e5
Mike Griese <migrie@microsoft.com>
2025-09-26 18:39:00 -0500

    Allow Run to handle commandlines with spaces (#42016)
    This better handles cases where commandlines might have embedded spaces.
    For something like
    
    ```
    C:\Program Files\PowerShell\7\pwsh.exe -c write-host dawg
    ```
    
    we'll now see that `C:\Program` isn't a file, and we'll try to look at
    `C:\Program Files\PowerShell\7\pwsh.exe` instead.
    
    This code is pilfered from
    https://github.com/microsoft/terminal/pull/12348 which fixed
    https://github.com/microsoft/terminal/issues/12345. Terminal has great
    code for normalizing a string into an executable and args, so why not
    just use it here.
    
    related to #41646
    related to #41705 (but much more narrowly scoped)
    
    ----
    
    I added some tests too.
    
    drive-by fix: as I was adding tests, I added a helper for "make a change
    to a page, and await the page's ItemsChanged". This removes a bunch of
    `await 1s` calls, and brings the shell page tests from like, 7s to 500ms

---
+++ src/modules/cmdpal/Tests/Microsoft.CmdPal.Ext.Shell.UnitTests/NormalizeCommandLineTests.cs
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CmdPal.Ext.Shell.Helpers;
+using Microsoft.CmdPal.Ext.UnitTestBase;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.CmdPal.Ext.Shell.UnitTests;
+
+[TestClass]
+public class NormalizeCommandLineTests : CommandPaletteUnitTestBase
+{
+    private void NormalizeTestCore(string input, string expectedExe, string expectedArgs = "")
+    {
+        ShellListPageHelpers.NormalizeCommandLineAndArgs(input, out var exe, out var args);
+
+        Assert.AreEqual(expectedExe, exe, ignoreCase: true, culture: System.Globalization.CultureInfo.InvariantCulture);
+        Assert.AreEqual(expectedArgs, args);
+    }
+
+    [TestMethod]
+    [DataRow("ping bing.com", "c:\\Windows\\system32\\ping.exe", "bing.com")]
+    [DataRow("curl bing.com", "c:\\Windows\\system32\\curl.exe", "bing.com")]
+    [DataRow("ipconfig /all", "c:\\Windows\\system32\\ipconfig.exe", "/all")]
+    public void NormalizeCommandLineSimple(string input, string expectedExe, string expectedArgs = "")
+    {
+        NormalizeTestCore(input, expectedExe, expectedArgs);
+    }
+
+    [TestMethod]
+    [DataRow("\"C:\\Program Files\\Windows Defender\\MsMpEng.exe\"", "C:\\Program Files\\Windows Defender\\MsMpEng.exe")]
+    [DataRow("C:\\Program Files\\Windows Defender\\MsMpEng.exe", "C:\\Program Files\\Windows Defender\\MsMpEng.exe")]
+    public void NormalizeCommandLineSpacesInExecutablePath(string input, string expectedExe, string expectedArgs = "")
+    {
+        NormalizeTestCore(input, expectedExe, expectedArgs);
+    }
+
+    [TestMethod]
+    [DataRow("%SystemRoot%\\system32\\cmd.exe", "C:\\Windows\\System32\\cmd.exe")]
+    public void NormalizeWithEnvVar(string input, string expectedExe, string expectedArgs = "")
+    {
+        NormalizeTestCore(input, expectedExe, expectedArgs);
+    }
+
+    [TestMethod]
+    [DataRow("cmd --run --test", "C:\\Windows\\System32\\cmd.exe", "--run --test")]
+    [DataRow("cmd    --run   --test  ", "C:\\Windows\\System32\\cmd.exe", "--run --test")]
+    [DataRow("cmd \"--run --test\" --pass", "C:\\Windows\\System32\\cmd.exe", "--run --test --pass")]
+    public void NormalizeArgsWithSpaces(string input, string expectedExe, string expectedArgs = "")
+    {
+        NormalizeTestCore(input, expectedExe, expectedArgs);
+    }
+
+    [TestMethod]
+    [DataRow("ThereIsNoWayYouHaveAnExecutableNamedThisOnThePipeline", "ThereIsNoWayYouHaveAnExecutableNamedThisOnThePipeline", "")]
+    [DataRow("C:\\ThisPathDoesNotExist\\NoExecutable.exe", "C:\\ThisPathDoesNotExist\\NoExecutable.exe", "")]
+    public void NormalizeNonExistentExecutable(string input, string expectedExe, string expectedArgs = "")
+    {
+        NormalizeTestCore(input, expectedExe, expectedArgs);
+    }
+
+    [TestMethod]
+    [DataRow("C:\\Windows", "c:\\Windows", "")]
+    [DataRow("C:\\Windows foo /bar", "c:\\Windows", "foo /bar")]
+    public void NormalizeDirectoryAsExecutable(string input, string expectedExe, string expectedArgs = "")
+    {
+        NormalizeTestCore(input, expectedExe, expectedArgs);
+    }
+}
