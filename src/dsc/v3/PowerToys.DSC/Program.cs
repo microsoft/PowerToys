f1367bfa17a47f2a56a064885a1ec3f07165fbe5
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2025-09-28 00:12:51 -0700

    Initial DSC v3 support for PowerToys (#41132)
    <!-- Enter a brief description/summary of your PR here. What does it
    fix/what does it change/how was it tested (even manually, if necessary)?
    -->
    ## Summary of the Pull Request
    
    Tasks checklist
    - [X] Implement DSC infra in PowerToys
    - [X] Implement Settings DSC resource
      - [X] Implement Get, Set, Test, Export, Schema
      - [X] Generate manifest (DSC resource JSON)
    - [X] Added Unit Tests
    - [x] Add `NJsonSchema` v11.4.0 to the stream
    - [x] Package the manifest files so dsc.exe can discover them
    - [x] Add `PowerToys.DSC.exe` to the PATH (maybe?)
    - [x] Add `InstallLocation` in the registry key so `winget configue
    export` can export the PowerToys DSC resources
    <!-- Please review the items on the PR checklist before submitting-->
    ## PR Checklist
    
    - [X] Closes: #37276
    - [X] **Communication:** I've discussed this with core contributors
    already. If the work hasn't been agreed, this work might be rejected
    - [X] **Tests:** Added/updated and all pass
    - [X] **Localization:** All end-user-facing strings can be localized
    - [x] **Dev docs:** Added/updated
    - [x] **New binaries:** Added on the required places
    - [x] [JSON for
    signing](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ESRPSigning_core.json)
    for new binaries
    - [x] [WXS for
    installer](https://github.com/microsoft/PowerToys/blob/main/installer/PowerToysSetup/Product.wxs)
    for new binaries and localization folder
    - [x] [YML for CI
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ci/templates/build-powertoys-steps.yml)
    for new test projects
    - [x] [YML for signed
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/release.yml)
    - [x] **Documentation updated:** If checked, please file a pull request
    on [our docs
    repo](https://github.com/MicrosoftDocs/windows-uwp/tree/docs/hub/powertoys)
    and link it here: #xxx
    
    <!-- Provide a more detailed description of the PR, other things fixed,
    or any additional comments/features here -->
    ## Detailed Description of the Pull Request / Additional comments
    
    <!-- Describe how you validated the behavior. Add automated tests
    wherever possible, but list manual validation steps taken as well -->
    ## Validation Steps Performed
    
    ---------
    
    Co-authored-by: vanzue <vanzue@outlook.com>
    Co-authored-by: Kai Tao (from Dev Box) <kaitao@microsoft.com>
    Co-authored-by: Leilei Zhang <leilzh@microsoft.com>

---
+++ src/dsc/v3/PowerToys.DSC/Program.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+using PowerToys.DSC.Commands;
+
+namespace PowerToys.DSC;
+
+/// <summary>
+/// Main entry point for the PowerToys Desired State Configuration CLI application.
+/// </summary>
+public class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand(Properties.Resources.PowerToysDSC);
+        rootCommand.AddCommand(new GetCommand());
+        rootCommand.AddCommand(new SetCommand());
+        rootCommand.AddCommand(new ExportCommand());
+        rootCommand.AddCommand(new TestCommand());
+        rootCommand.AddCommand(new SchemaCommand());
+        rootCommand.AddCommand(new ManifestCommand());
+        rootCommand.AddCommand(new ModulesCommand());
+        return await rootCommand.InvokeAsync(args);
+    }
+}
