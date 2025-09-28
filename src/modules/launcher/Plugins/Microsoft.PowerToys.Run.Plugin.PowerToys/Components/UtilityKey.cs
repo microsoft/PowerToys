da250b2264662e0eb2cf3cbac84c6e4e6a03c03e
Niels Laute <niels.laute@live.nl>
2025-09-28 11:45:35 +0200

    Exclude from run plugin

--- src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.PowerToys/Components/UtilityKey.cs
+++ src/modules/launcher/Plugins/Microsoft.PowerToys.Run.Plugin.PowerToys/Components/UtilityKey.cs
@@ -14,8 +14,7 @@ namespace Microsoft.PowerToys.Run.Plugin.PowerToys.Components
         ShortcutGuide = 5,
         RegistryPreview = 6,
         CropAndLock = 7,
-        LightSwitch = 8,
-        EnvironmentVariables = 9,
-        Workspaces = 10,
+        EnvironmentVariables = 8,
+        Workspaces = 9,
     }
 }
