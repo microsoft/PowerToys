1e7c60ec23d607c28732c0fcedc51bc2b1ee7e7c
Mike Griese <migrie@microsoft.com>
2025-09-29 16:47:35 -0500

    lets move you

--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Shell/Properties/ResourceLoaderInstance.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.Shell/Properties/ResourceLoaderInstance.cs
@@ -2,7 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace Microsoft.CmdPal.Ext.Shell.Helpers;
+namespace Microsoft.CmdPal.Ext.Shell;
 
 internal static class ResourceLoaderInstance
 {
