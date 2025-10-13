77815c5472e3fe827977a81cb1af2295201ea6f2
vanzue <vanzue@outlook.com>
2025-10-13 13:50:38 +0800

    make download work

--- src/common/LanguageModelProvider/FoundryLocal/FoundryDownloadBody.cs
+++ src/common/LanguageModelProvider/FoundryLocal/FoundryDownloadBody.cs
@@ -2,6 +2,18 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Text.Json.Serialization;
+
 namespace LanguageModelProvider.FoundryLocal;
 
-internal sealed record FoundryDownloadBody(FoundryModelDownload Model, bool IgnorePipeReport);
+internal sealed class FoundryDownloadBody
+{
+    [JsonPropertyName("Model")]
+    public required FoundryModelDownload Model { get; init; }
+
+    [JsonPropertyName("token")]
+    public required string Token { get; init; }
+
+    [JsonPropertyName("IgnorePipeReport")]
+    public required bool IgnorePipeReport { get; init; }
+}
