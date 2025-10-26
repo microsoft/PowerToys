3a435db8bf529a2779a3b334615ea101c443fccb
mirmimirr <miranda.mzheng@gmail.com>
2025-10-26 11:29:59 -0400

    clean up viewmodel

--- src/settings-ui/Settings.UI/ViewModels/KeystrokeOverlayViewModel.cs
+++ src/settings-ui/Settings.UI/ViewModels/KeystrokeOverlayViewModel.cs
@@ -5,50 +5,105 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
+using System.Windows.Input;
+using global::PowerToys.GPOWrapper;
 using Microsoft.PowerToys.Settings.UI.Helpers;
 using Microsoft.PowerToys.Settings.UI.Library;
-using Microsoft.PowerToys.Settings.UI.Library.Interfaces;
 using Microsoft.PowerToys.Settings.UI.Library.Helpers;
-using Microsoft.UI;
+using Microsoft.PowerToys.Settings.UI.Library.Interfaces;
 using Windows.UI;
 
 namespace Microsoft.PowerToys.Settings.UI.ViewModels
 {
-    public class KeystrokeOverlayViewModel : PageViewModelBase
+    public partial class KeystrokeOverlayViewModel : PageViewModelBase
     {
         protected override string ModuleName => KeystrokeOverlaySettings.ModuleName;
 
-        private KeystrokeOverlaySettings _settings;
-        private GeneralSettings _generalSettings;
-        private readonly Func<string, int> _sendConfigMSG;
+        private GeneralSettings GeneralSettingsConfig { get; set; }
+
+        private readonly KeystrokeOverlaySettings _settings;
         private readonly ISettingsUtils _settingsUtils;
 
-        public KeystrokeOverlayViewModel(ISettingsUtils settingsUtils, ISettingsRepository<GeneralSettings> generalSettingsRepository, ISettingsRepository<KeystrokeOverlaySettings> moduleSettingsRepository, Func<string, int> ipcMSGCallBackFunc)
+        private Func<string, int> SendConfigMSG { get; }
+
+        private GpoRuleConfigured _enabledGpoRuleConfiguration;
+        private bool _enabledStateIsGPOConfigured;
+        private bool _isEnabled;
+
+        public KeystrokeOverlayViewModel(
+            ISettingsUtils settingsUtils,
+            ISettingsRepository<GeneralSettings> generalSettingsRepository,
+            ISettingsRepository<KeystrokeOverlaySettings> moduleSettingsRepository,
+            Func<string, int> ipcMSGCallBackFunc)
         {
             _settingsUtils = settingsUtils ?? throw new ArgumentNullException(nameof(settingsUtils));
             ArgumentNullException.ThrowIfNull(generalSettingsRepository);
             ArgumentNullException.ThrowIfNull(moduleSettingsRepository);
 
-            _generalSettings = generalSettingsRepository.SettingsConfig;
+            GeneralSettingsConfig = generalSettingsRepository.SettingsConfig;
             _settings = moduleSettingsRepository.SettingsConfig;
-            _sendConfigMSG = ipcMSGCallBackFunc;
+            SendConfigMSG = ipcMSGCallBackFunc;
+
+            // initialize backing fields if needed (optional)
 
             // initialize backing fields if needed (optional)
         }
 
+        private void InitializeEnabledValue()
+        {
+            _enabledGpoRuleConfiguration = GPOWrapper.GetConfiguredKeystrokeOverlayEnabledValue();
+            if (_enabledGpoRuleConfiguration == GpoRuleConfigured.Disabled || _enabledGpoRuleConfiguration == GpoRuleConfigured.Enabled)
+            {
+                // Get the enabled state from GPO.
+                _enabledStateIsGPOConfigured = true;
+                _isEnabled = _enabledGpoRuleConfiguration == GpoRuleConfigured.Enabled;
+            }
+            else
+            {
+                _isEnabled = GeneralSettingsConfig.Enabled.KeystrokeOverlay;
+            }
+        }
+
+        public override Dictionary<string, HotkeySettings[]> GetAllHotkeySettings()
+        {
+            var hotkeysDict = new Dictionary<string, HotkeySettings[]>
+            {
+                [ModuleName] = [SwitchMonitorHotkey],
+            };
+
+            return hotkeysDict;
+        }
+
         public bool IsEnabled
         {
-            get => _generalSettings.Enabled?.KeystrokeOverlay ?? false;
+            get => _isEnabled;
             set
             {
-                if (_generalSettings.Enabled.KeystrokeOverlay != value)
+                if (_enabledStateIsGPOConfigured)
                 {
-                    _generalSettings.Enabled.KeystrokeOverlay = value;
-                    _sendConfigMSG(new OutGoingGeneralSettings(_generalSettings).ToString());
+                    // If it's GPO configured, shouldn't be able to change this state.
+                    return;
+                }
+
+                if (value != _isEnabled)
+                {
+                    _isEnabled = value;
+
+                    // set status in the general settings configuration
+                    GeneralSettingsConfig.Enabled.KeystrokeOverlay = value;
+                    OutGoingGeneralSettings snd = new OutGoingGeneralSettings(GeneralSettingsConfig);
+
+                    SendConfigMSG(snd.ToString());
                     OnPropertyChanged(nameof(IsEnabled));
                 }
             }
+        }
+
+        public bool IsEnabledGpoConfigured
+        {
+            get => _enabledStateIsGPOConfigured;
         }
 
         public HotkeySettings SwitchMonitorHotkey
@@ -73,7 +128,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 {
                     _settings.Properties.IsDraggableOverlayEnabled.Value = value;
                     NotifySettingsChanged();
-                    OnPropertyChanged(nameof(IsDraggableOverlayEnabled));
                 }
             }
         }
@@ -87,7 +141,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 {
                     _settings.Properties.DisplayMode.Value = value;
                     NotifySettingsChanged();
-                    OnPropertyChanged(nameof(DisplayMode));
                 }
             }
         }
@@ -101,7 +154,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 {
                     _settings.Properties.OverlayTimeout.Value = value;
                     NotifySettingsChanged();
-                    OnPropertyChanged(nameof(OverlayTimeout));
                 }
             }
         }
@@ -115,7 +167,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 {
                     _settings.Properties.TextSize.Value = value;
                     NotifySettingsChanged();
-                    OnPropertyChanged(nameof(TextSize));
                 }
             }
         }
@@ -130,7 +181,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 {
                     _settings.Properties.TextColor.Value = value;
                     NotifySettingsChanged();
-                    OnPropertyChanged(nameof(TextColor));
                     OnPropertyChanged(nameof(TextColorColor));
                 }
             }
@@ -145,7 +195,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 {
                     _settings.Properties.BackgroundColor.Value = value;
                     NotifySettingsChanged();
-                    OnPropertyChanged(nameof(BackgroundColor));
                     OnPropertyChanged(nameof(BackgroundColorColor));
                 }
             }
@@ -160,7 +209,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 {
                     _settings.Properties.TextOpacity.Value = value;
                     NotifySettingsChanged();
-                    OnPropertyChanged(nameof(TextOpacity));
                 }
             }
         }
@@ -174,7 +222,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                 {
                     _settings.Properties.BackgroundOpacity.Value = value;
                     NotifySettingsChanged();
-                    OnPropertyChanged(nameof(BackgroundOpacity));
                 }
             }
         }
@@ -191,7 +238,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                     _settings.Properties.TextColor.Value = intVal;
                     NotifySettingsChanged();
                     OnPropertyChanged(nameof(TextColorColor));
-                    OnPropertyChanged(nameof(TextColor));
                 }
             }
         }
@@ -207,7 +253,6 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
                     _settings.Properties.BackgroundColor.Value = intVal;
                     NotifySettingsChanged();
                     OnPropertyChanged(nameof(BackgroundColorColor));
-                    OnPropertyChanged(nameof(BackgroundColor));
                 }
             }
         }
@@ -225,15 +270,14 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
             return (c.R << 16) | (c.G << 8) | c.B;
         }
 
-        private void NotifySettingsChanged()
+        private void NotifySettingsChanged([CallerMemberName] string propertyName = null)
         {
-            // send IPC to runner
-            _sendConfigMSG(
-                string.Format(
-                    CultureInfo.InvariantCulture,
-                    "{  \"powertoys\": { {0}: {1} } }",
-                    ModuleName,
-                    JsonSerializer.Serialize(_settings)));
+            OnPropertyChanged(propertyName);
+
+            // send settings to powertoys process
+            string jsonSettings = JsonSerializer.Serialize(_settings);
+            string ipcMessage = $"{{\"powertoys\":{{\"{ModuleName}\":{jsonSettings}}}}}";
+            SendConfigMSG(ipcMessage.ToString());
 
             // save locally
             _settingsUtils.SaveSettings(_settings.ToJsonString(), ModuleName);
@@ -241,6 +285,7 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
 
         public void RefreshEnabledState()
         {
+            InitializeEnabledValue();
             OnPropertyChanged(nameof(IsEnabled));
         }
     }
