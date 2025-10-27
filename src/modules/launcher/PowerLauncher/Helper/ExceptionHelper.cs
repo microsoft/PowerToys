0f9d68a57b3a88345e37818e4ef9c8f0a74f8311
Jiří Polášek <me@jiripolasek.com>
2025-10-27 11:55:39 +0100

    Allow silent handling of STATUS_MESSAGE_LOST from WPF (0xD0000701)

--- src/modules/launcher/PowerLauncher/Helper/ExceptionHelper.cs
+++ src/modules/launcher/PowerLauncher/Helper/ExceptionHelper.cs
@@ -7,11 +7,15 @@ using System.Runtime.InteropServices;
 
 namespace PowerLauncher.Helper
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "Win32 naming conventions")]
     internal static class ExceptionHelper
     {
-#pragma warning disable SA1310 // Field names should not contain underscore
+        private const string PresentationFrameworkExceptionSource = "PresentationFramework";
+
         private const int DWM_E_COMPOSITIONDISABLED = unchecked((int)0x80263001);
-#pragma warning restore SA1310 // Field names should not contain underscore
+
+        // HRESULT for NT STATUS STATUS_MESSAGE_LOST (0xC0000701 | 0x10000000 == 0xD0000701)
+        private const int STATUS_MESSAGE_LOST_HR = unchecked((int)0xD0000701);
 
         /// <summary>
         /// Returns true if the exception is a recoverable DWM composition exception.
@@ -24,6 +28,11 @@ namespace PowerLauncher.Helper
             }
 
             if (comException.HResult is DWM_E_COMPOSITIONDISABLED)
+            {
+                return true;
+            }
+
+            if (comException.HResult is STATUS_MESSAGE_LOST_HR && comException.Source == PresentationFrameworkExceptionSource)
             {
                 return true;
             }
