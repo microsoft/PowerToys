408f6973f6f7fcdf9e8c691c2f4b83f907e922b8
Ayaan31 <ahmada5@rpi.edu>
2025-10-27 22:38:44 -0400

    Added the required libraries

--- src/modules/ScreencastMode/ScreencastModeModuleInterface/pch.h
+++ src/modules/ScreencastMode/ScreencastModeModuleInterface/pch.h
@@ -1,5 +1,14 @@
 #pragma once
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <common/common.h>
-#include <ProjectTelemetry.h>
+
+#include <common/SettingsAPI/settings_helpers.h>
+#include <common/utils/gpo.h>
+#include <common/utils/winapi_error.h>
+#include <shlwapi.h>
+#include <shellapi.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.Globalization.h>
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
--- src/modules/ScreencastMode/ScreencastModeModuleInterface/trace.h
+++ src/modules/ScreencastMode/ScreencastModeModuleInterface/trace.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <windows.h>
+#include <TraceLoggingActivity.h>
+#include <common/telemetry/ProjectTelemetry.h>
+
+TRACELOGGING_DECLARE_PROVIDER(g_hProvider);
+
 class Trace
 {
 public:
