b3709f352644f6663b14f6705a07c4e8835a0c0a
Leilei Zhang (from Dev Box) <leilzh@microsoft.com>
2025-11-04 09:59:54 +0800

    fix build error

--- src/modules/peek/Peek.UI/Models/SelectedItemByPath.cs
+++ src/modules/peek/Peek.UI/Models/SelectedItemByPath.cs
@@ -2,6 +2,8 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 namespace Peek.UI.Models
 {
     public class SelectedItemByPath : SelectedItem
