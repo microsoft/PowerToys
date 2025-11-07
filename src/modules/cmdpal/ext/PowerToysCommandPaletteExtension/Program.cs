8c1658df7e6b67ee02908ada60109798e137e98e
kaitao-ms <kaitao1105@gmail.com>
2025-11-07 21:07:15 +0800

    Draft for pt extension

---
+++ src/modules/cmdpal/ext/PowerToysCommandPaletteExtension/Program.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading;
+using Microsoft.CommandPalette.Extensions;
+
+namespace PowerToysCommandPaletteExtension;
+
+public static class Program
+{
+    [MTAThread]
+    public static int Main(string[] args)
+    {
+        if (args.Length > 0 && args[0].Equals("-RegisterProcessAsComServer", StringComparison.OrdinalIgnoreCase))
+        {
+            using ExtensionServer server = new();
+            using ManualResetEvent extensionDisposed = new(false);
+            var extensionInstance = new PowerToysExtension(extensionDisposed);
+
+            server.RegisterExtension(() => extensionInstance);
+
+            extensionDisposed.WaitOne();
+            return 0;
+        }
+
+        Console.WriteLine("PowerToysCommandPaletteExtension launched without COM registration arguments. Exiting.");
+        return 0;
+    }
+}
