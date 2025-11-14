d517730fd0e1aca64dcafb93b71a9dc2fc0164eb
Shawn Yuan (from Dev Box) <shuaiyuan@microsoft.com>
2025-11-14 15:08:08 +0800

    remove unused file
    Signed-off-by: Shawn Yuan (from Dev Box) <shuaiyuan@microsoft.com>

--- src/settings-ui/Settings.UI.Library/AIProviderConfigurationSnapshot.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation
-// The Microsoft Corporation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-namespace Microsoft.PowerToys.Settings.UI.Library
-{
-    // Intentionally left empty after removing legacy provider snapshots.
-}
