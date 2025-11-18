56cc5354888dd89530124396234d452cbae0f99b
Zoha Ahmed <122557699+Zoha-ahmed@users.noreply.github.com>
2025-11-18 10:51:29 -0500

    adding incomplete keystroke overlay

---
+++ src/modules/ScreencastMode/ScreencastModeUI/ScreencastModeUI/KeystrokeDisplay.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI;                    
+using Microsoft.UI.Xaml.Media;         
+
+namespace ScreencastModeUI
+{
+    internal class KeystrokeDisplay
+    {
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int DisplayTimeMs { get; set; } = 5000;
+        public SolidColorBrush BackgroundBrush { get; } = new SolidColorBrush(
+            Windows.UI.Color.FromArgb(230, 50, 50, 50)); 
+        public SolidColorBrush ForegroundBrush { get; } = new SolidColorBrush(
+            Colors.White);
+        public int FontSize { get; set; } = 32;
+    }
+}
