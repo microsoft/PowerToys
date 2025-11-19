db7c0312f6f007584cc0ce42320e2dbb1e1c277a
Zoha Ahmed <122557699+Zoha-ahmed@users.noreply.github.com>
2025-11-18 19:13:13 -0500

    fixing keystroke display

--- src/modules/ScreencastMode/ScreencastModeUI/ScreencastModeUI/KeystrokeDisplay.cs
+++ src/modules/ScreencastMode/ScreencastModeUI/ScreencastModeUI/KeystrokeDisplay.cs
@@ -2,11 +2,8 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.UI.Xaml.Media;
 using Windows.UI;
 
@@ -18,9 +15,9 @@ namespace ScreencastModeUI
         public DateTime Timestamp { get; set; }
         public int DisplayTimeMs { get; set; } = 5000;
         public SolidColorBrush BackgroundBrush { get; } = new SolidColorBrush(
-            Color.FromArgb(230, 50, 50, 50)); 
+            Color.FromArgb(230, 50, 50, 50));
         public SolidColorBrush ForegroundBrush { get; } = new SolidColorBrush(
-            Colors.White); 
+            Colors.White);
         public int FontSize { get; set; } = 32;
     }
 }
