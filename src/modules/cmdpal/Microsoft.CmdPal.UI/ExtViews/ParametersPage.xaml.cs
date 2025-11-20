e4b59c821a42ec0eb7a232e606208925c861b843
Mike Griese <migrie@microsoft.com>
2025-11-20 09:53:23 -0600

    Turns out we changed the type of parameter passed

--- src/modules/cmdpal/Microsoft.CmdPal.UI/ExtViews/ParametersPage.xaml.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/ExtViews/ParametersPage.xaml.cs
@@ -2,7 +2,9 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics;
 using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.CmdPal.Core.Common;
 using Microsoft.CmdPal.Core.ViewModels;
 using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
@@ -26,7 +28,7 @@ public sealed partial class ParametersPage : Page
 
     // Using a DependencyProperty as the backing store for ViewModel.  This enables animation, styling, binding, etc...
     public static readonly DependencyProperty ViewModelProperty =
-        DependencyProperty.Register(nameof(ViewModel), typeof(ParametersPageViewModel), typeof(ParametersPage), new PropertyMetadata(null));
+        DependencyProperty.Register(nameof(ViewModel), typeof(ParametersPageViewModel), typeof(ParametersPage), new PropertyMetadata(null, OnViewModelChanged));
 
     public ParametersPage()
     {
@@ -43,10 +45,17 @@ public sealed partial class ParametersPage : Page
 
     protected override void OnNavigatedTo(NavigationEventArgs e)
     {
-        if (e.Parameter is ParametersPageViewModel vm)
+        if (e.Parameter is not AsyncNavigationRequest navigationRequest)
         {
-            ViewModel = vm;
+            throw new InvalidOperationException($"Invalid navigation parameter: {nameof(e.Parameter)} must be {nameof(AsyncNavigationRequest)}");
         }
+
+        if (navigationRequest.TargetViewModel is not ParametersPageViewModel ppvm)
+        {
+            throw new InvalidOperationException($"Invalid navigation target: AsyncNavigationRequest.{nameof(AsyncNavigationRequest.TargetViewModel)} must be {nameof(ParametersPageViewModel)}");
+        }
+
+        ViewModel = ppvm;
 
         // if (!WeakReferenceMessenger.Default.IsRegistered<ActivateSelectedListItemMessage>(this))
         // {
@@ -82,4 +91,36 @@ public sealed partial class ParametersPage : Page
     // {
     //     ViewModel?.InvokeSecondaryCommandCommand?.Execute(ViewModel);
     // }
+    private static void OnViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is ParametersPage @this)
+        {
+            if (e.OldValue is ParametersPageViewModel old)
+            {
+                old.PropertyChanged -= @this.ViewModel_PropertyChanged;
+
+                // old.ItemsUpdated -= @this.Page_ItemsUpdated;
+            }
+
+            if (e.NewValue is ParametersPageViewModel page)
+            {
+                page.PropertyChanged += @this.ViewModel_PropertyChanged;
+
+                // page.ItemsUpdated += @this.Page_ItemsUpdated;
+            }
+            else if (e.NewValue is null)
+            {
+                CoreLogger.LogDebug("cleared view model");
+            }
+        }
+    }
+
+    private void ViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        var prop = e.PropertyName;
+        if (prop == nameof(ViewModel.ShowCommand))
+        {
+            Debug.WriteLine($"ViewModel.ShowCommand {ViewModel?.ShowCommand}");
+        }
+    }
 }
