84f702946f011de533c2ae1c9476a57b60eedcfa
Jiří Polášek <me@jiripolasek.com>
2025-11-28 02:17:33 +0100

    Add custom search engine option to Web Search extension
    - Introduces a new text box in Web Search extension settings for specifying a custom search engine URI
    - If the text box is non-empty, the provided URI is used for queries
    - If left empty, the extension defaults to previous behavior, sending queries in the format "? <query>"

--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.WebSearch/Commands/SearchWebCommand.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.WebSearch/Commands/SearchWebCommand.cs
@@ -28,17 +28,44 @@ internal sealed partial class SearchWebCommand : InvokableCommand
 
     public override CommandResult Invoke()
     {
-        if (!_browserInfoService.Open($"? {Arguments}"))
+        var uri = BuildUri();
+
+        if (!_browserInfoService.Open(uri))
         {
             // TODO GH# 138 --> actually display feedback from the extension somewhere.
             return CommandResult.KeepOpen();
         }
 
+        // remember only the query, not the full URI
         if (_settingsManager.HistoryItemCount != 0)
         {
             _settingsManager.AddHistoryItem(new HistoryItem(Arguments, DateTime.Now));
         }
 
         return CommandResult.Dismiss();
+    }
+
+    private string BuildUri()
+    {
+        if (string.IsNullOrWhiteSpace(_settingsManager.CustomSearchUri))
+        {
+            return $"? " + Arguments;
+        }
+
+        // if the custom search URI contains query placeholder, replace it with the actual query
+        // otherwise append the query to the end of the URI
+        // support {query}, %query% or %s as placeholder
+        var placeholderVariants = new[] { "{query}", "%query%", "%s" };
+        foreach (var placeholder in placeholderVariants)
+        {
+            if (_settingsManager.CustomSearchUri.Contains(placeholder, StringComparison.OrdinalIgnoreCase))
+            {
+                return _settingsManager.CustomSearchUri.Replace(placeholder, Uri.EscapeDataString(Arguments), StringComparison.OrdinalIgnoreCase);
+            }
+        }
+
+        // is this too smart?
+        var separator = _settingsManager.CustomSearchUri.Contains('?') ? '&' : '?';
+        return $"{_settingsManager.CustomSearchUri}{separator}q={Uri.EscapeDataString(Arguments)}";
     }
 }
