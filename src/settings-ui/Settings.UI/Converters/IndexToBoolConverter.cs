93cd0f600f6ad9135eba3398a792a3c0cf6038aa
Thanh Nguyen <thanhnguyentuan2007@gmail.com>
2025-12-06 10:57:30 +0700

    feat: Replace Quick Accent toolbar position dropdown with 3x3 visual grid
    Closes #44111
    
    Replaced the ComboBox dropdown for toolbar position selection with a
    visual 3x3 grid of tiles, making it easier to select positions intuitively.
    
    Changes:
    - Added IndexToBoolConverter for int-to-bool binding conversion
    - Created PositionRadioButtonStyle with accent color highlighting
    - Replaced ComboBox with 3x3 Grid of styled RadioButtons
    - Each tile shows position with tooltip for accessibility
    
    The grid layout visually represents the 9 toolbar positions:
    +----+----+----+
    | TL | TC | TR |
    +----+----+----+
    | L  | C  | R  |
    +----+----+----+
    | BL | BC | BR |
    +----+----+----+

---
+++ src/settings-ui/Settings.UI/Converters/IndexToBoolConverter.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.UI.Xaml.Data;
+
+namespace Microsoft.PowerToys.Settings.UI.Converters
+{
+    /// <summary>
+    /// Converts an integer index to a boolean value for use with RadioButton groups.
+    /// The ConverterParameter specifies which index value should return true.
+    /// </summary>
+    public partial class IndexToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null || parameter == null)
+            {
+                return false;
+            }
+
+            if (value is int intValue && int.TryParse(parameter.ToString(), out int paramIndex))
+            {
+                return intValue == paramIndex;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool isChecked && isChecked && parameter != null)
+            {
+                if (int.TryParse(parameter.ToString(), out int paramIndex))
+                {
+                    return paramIndex;
+                }
+            }
+
+            // Return -1 to indicate no change (RadioButton unchecked)
+            return -1;
+        }
+    }
+}
