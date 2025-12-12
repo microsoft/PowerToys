c077cc46f18e95cb14652bd88ba1729afee7674a
Jaylyn Barbee <51131738+Jaylyn-Barbee@users.noreply.github.com>
2025-12-12 10:21:47 -0500

    Adding telemetry events for Light Switch

---
+++ LightSwitchModeUpdated.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+using Microsoft.PowerToys.Telemetry;
+using Microsoft.PowerToys.Telemetry.Events;
+
+/// <summary>
+/// Tracks which mode users are using to schedule theme changes
+/// Purpose: Identify which modes users are using the most.
+/// </summary>
+[EventData]
+[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)]
+public class LightSwitchModeUpdated : EventBase, IEvent
+{
+  /// <summary>
+  /// Gets or sets the mode the user is using.
+  /// </summary>
+  public string Mode { get; set; }
+
+  public LightSwitchModeUpdated(bool mode)
+  {
+    EventName = "LightSwitch_ModeUpdated";
+    Mode = mode;
+  }
+
+  public PartA_PrivTags PartA_PrivTags => PartA_PrivTags.ProductAndServiceUsage;
+}
---
+++ LightSwitchScheduleToggled.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+using Microsoft.PowerToys.Telemetry;
+using Microsoft.PowerToys.Telemetry.Events;
+
+/// <summary>
+/// Tracks if users are using the schedule to control their themeing.
+/// Purpose: Identify how users are using Light Switch to control their theme.
+/// </summary>
+[EventData]
+[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)]
+public class LightSwitchScheduleToggled : EventBase, IEvent
+{
+  /// <summary>
+  /// Gets or sets whether the schedule is on or off.
+  /// </summary>
+  public bool OnOrOff { get; set; }
+
+  public LightSwitchScheduleToggled(bool onOrOff)
+  {
+    EventName = "LightSwitch_ScheduleToggled";
+    OnOrOff = onOrOff;
+  }
+
+  public PartA_PrivTags PartA_PrivTags => PartA_PrivTags.ProductAndServiceUsage;
+}
