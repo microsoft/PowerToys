a79f50f33f0e6e309bc25bb50881ce843e50f69a
vanzue <vanzue@outlook.com>
2025-12-12 16:47:42 +0800

    fix

--- .pipelines/versionAndSignCheck.ps1
+++ .pipelines/versionAndSignCheck.ps1
@@ -7,7 +7,7 @@ Param(
 )
 
 $DirPath = $targetDir; #this file is in pipeline, we need root.
-$items = Get-ChildItem -Path $DirPath -File -Include *.exe, *.dll, *.ttf, *.msix, PTCustomActions -Recurse -Force -ErrorAction SilentlyContinue
+$items = Get-ChildItem -Path $DirPath -File -Include *.exe, *.dll, *.ttf, PTCustomActions -Recurse -Force -ErrorAction SilentlyContinue
 $versionExceptions = @(
     "AdaptiveCards.Templating.dll",
     "Microsoft.Windows.ApplicationModel.DynamicDependency.Projection.dll",
