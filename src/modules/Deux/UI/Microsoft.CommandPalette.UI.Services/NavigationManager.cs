036a23aee7f695796705e0db4499011eb10bffc0
Michael Jolley <mike@baldbeardedbuilder.com>
2025-12-15 16:30:51 -0600

    NavMgr

--- src/modules/Deux/UI/Microsoft.CommandPalette.UI.Services/NavigationManager.cs
+++ src/modules/Deux/UI/Microsoft.CommandPalette.UI.Services/NavigationManager.cs
@@ -2,8 +2,16 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Extensions.Logging;
+
 namespace Microsoft.CommandPalette.UI.Services;
 
 internal class NavigationManager
 {
+    private readonly ILogger logger;
+
+    public NavigationManager(ILogger logger)
+    {
+        this.logger = logger;
+    }
 }
