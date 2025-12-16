671993525d4f0c356cf83d7d8edd35e933702b38
Jeremy Sinclair <4016293+snickler@users.noreply.github.com>
2025-12-15 22:44:25 -0500

    [Workspaces] Update OnThreadExecutor usage

--- src/modules/Workspaces/WorkspacesLauncher/LauncherUIHelper.cpp
+++ src/modules/Workspaces/WorkspacesLauncher/LauncherUIHelper.cpp
@@ -35,7 +35,7 @@ LauncherUIHelper::~LauncherUIHelper()
         {
             Logger::error(L"Unable to find UI process: {}", get_last_error_or_default(GetLastError()));
         }
-    } }).wait();
+    } }).get();
 }
 
 void LauncherUIHelper::LaunchUI()
