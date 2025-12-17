de0a2617d235af554747b3e629fe9d369aa54761
Kai Tao (from Dev Box) <kaitao@microsoft.com>
2025-12-17 13:19:46 +0800

    Use global precompile header naming practice

--- installer/PowerToysSetupVNext/SilentFilesInUseBA/SilentFilesInUseBAFunctions.cpp
+++ installer/PowerToysSetupVNext/SilentFilesInUseBA/SilentFilesInUseBAFunctions.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
 
-#include "precomp.h"
+#include "pch.h"
 #include "BalBaseBAFunctions.h"
 #include "BalBaseBAFunctionsProc.h"
 
