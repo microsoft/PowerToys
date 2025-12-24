3749f3e87d07b5757dd0052d09ccccd0ee016a42
Shawn Yuan (from Dev Box) <shuaiyuan@microsoft.com>
2025-12-24 14:14:05 +0800

    update

--- src/modules/AdvancedPaste/AdvancedPaste/Services/CustomActions/SemanticKernelPasteProvider.cs
+++ src/modules/AdvancedPaste/AdvancedPaste/Services/CustomActions/SemanticKernelPasteProvider.cs
@@ -102,11 +102,7 @@ namespace AdvancedPaste.Services.CustomActions
             var imageService = kernel.GetRequiredService<ITextToImageService>();
             var width = _config.ImageWidth > 0 ? _config.ImageWidth : 1024;
             var height = _config.ImageHeight > 0 ? _config.ImageHeight : 1024;
-            var settings = new OpenAITextToImageExecutionSettings
-            {
-                Size = (width, height),
-                ResponseFormat = "b64_json",
-            };
+            var settings = new OpenAITextToImageExecutionSettings();
 
             var generatedImages = await imageService.GetImageContentsAsync(new TextContent(userMessageContent), settings, cancellationToken: cancellationToken);
 
