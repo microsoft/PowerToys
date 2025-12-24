20dcb6fb47f63ae38eeebf8ee44746bc42b12e22
Shawn Yuan (from Dev Box) <shuaiyuan@microsoft.com>
2025-12-24 11:17:17 +0800

    add localization support

---
+++ src/modules/AdvancedPaste/AdvancedPaste/AdvancedPasteXAML/Converters/PasteAIUsageToStringConverter.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using AdvancedPaste.Helpers;
+using Microsoft.PowerToys.Settings.UI.Library;
+using Microsoft.UI.Xaml.Data;
+
+namespace AdvancedPaste.Converters;
+
+public sealed partial class PasteAIUsageToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        var usage = value switch
+        {
+            string s => PasteAIUsageExtensions.FromConfigString(s),
+            PasteAIUsage u => u,
+            _ => PasteAIUsage.ChatCompletion,
+        };
+
+        return ResourceLoaderInstance.ResourceLoader.GetString($"PasteAIUsage_{usage}");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
