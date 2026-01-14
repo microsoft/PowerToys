7c870d48ea1eda88e2c5db34bd8dd74768110079
Noraa Junker <noraa.junker@outlook.com>
2026-01-14 22:22:29 +0100

    Fix version.h

--- src/common/notifications/dont_show_again.cpp
+++ src/common/notifications/dont_show_again.cpp
@@ -3,32 +3,6 @@
 
 namespace notifications
 {
-    bool disable_toast(const wchar_t* registry_path)
-    {
-        HKEY key{};
-        if (RegCreateKeyExW(HKEY_CURRENT_USER,
-                            registry_path,
-                            0,
-                            nullptr,
-                            REG_OPTION_NON_VOLATILE,
-                            KEY_ALL_ACCESS,
-                            nullptr,
-                            &key,
-                            nullptr) != ERROR_SUCCESS)
-        {
-            return false;
-        }
-        const auto now = timeutil::now();
-        const size_t buf_size = sizeof(now);
-        if (RegSetValueExW(key, nullptr, 0, REG_QWORD, reinterpret_cast<const BYTE*>(&now), buf_size) != ERROR_SUCCESS)
-        {
-            RegCloseKey(key);
-            return false;
-        }
-        RegCloseKey(key);
-        return true;
-    }
-
     bool is_toast_disabled(const wchar_t* registry_path, const int64_t disable_interval_in_days)
     {
         HKEY key{};
