e4c11018a6f0847e08e32801a1683ada3576cdef
Shawn Yuan (from Dev Box) <shuaiyuan@microsoft.com>
2026-01-28 10:43:11 +0800

    update ci pipeline

--- .pipelines/verifyAndSetLatestVCToolsVersion.ps1
+++ .pipelines/verifyAndSetLatestVCToolsVersion.ps1
@@ -1,9 +1,22 @@
-$VSInstances = ([xml](& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -include packages -format xml))
+# Construct vswhere arguments based on environment variable
+$vsWhereArgs = @('-latest', '-requires', 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64', '-include', 'packages', '-format', 'xml')
+if ($env:VCWhereExtraVersionTarget) {
+    # Add version target if specified (e.g., '-version [18.0,19.0)' for VS2026)
+    $vsWhereArgs += $env:VCWhereExtraVersionTarget.Split(' ')
+}
+
+$VSInstances = ([xml](& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' @vsWhereArgs))
 $VSPackages = $VSInstances.instances.instance.packages.package
 $LatestVCPackage = ($VSPackages | ? { $_.id -eq "Microsoft.VisualCpp.Tools.Core" })
 $LatestVCToolsVersion = $LatestVCPackage.version;
 
-$VSRoot = (& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property 'resolvedInstallationPath')
+# Construct vswhere arguments for resolvedInstallationPath
+$vsWhereRootArgs = @('-latest', '-requires', 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64', '-property', 'resolvedInstallationPath')
+if ($env:VCWhereExtraVersionTarget) {
+    $vsWhereRootArgs += $env:VCWhereExtraVersionTarget.Split(' ')
+}
+
+$VSRoot = (& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' @vsWhereRootArgs)
 $VCToolsRoot = Join-Path $VSRoot "VC\Tools\MSVC"
 
 # We have observed a few instances where the VC tools package version actually
