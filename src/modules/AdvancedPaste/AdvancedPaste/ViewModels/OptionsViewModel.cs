de66af3bd4391ec51db571708a45a0e154f72228
Gordon Lam (SH) <yeelam@microsoft.com>
2026-01-30 16:06:58 -0800

    feat(paste): add setting to toggle clipboard history button visibility

--- src/modules/AdvancedPaste/AdvancedPaste/ViewModels/OptionsViewModel.cs
+++ src/modules/AdvancedPaste/AdvancedPaste/ViewModels/OptionsViewModel.cs
@@ -232,7 +232,7 @@ namespace AdvancedPaste.ViewModels
 
         public bool ShowClipboardPreview => _userSettings.EnableClipboardPreview;
 
-        public bool ShowClipboardHistoryButton => ClipboardHistoryEnabled;
+        public bool ShowClipboardHistoryButton => ClipboardHistoryEnabled && _userSettings.ShowClipboardHistory;
 
         public bool HasIndeterminateTransformProgress => double.IsNaN(TransformProgress);
 
@@ -320,6 +320,7 @@ namespace AdvancedPaste.ViewModels
             OnPropertyChanged(nameof(AIProviders));
             OnPropertyChanged(nameof(AllowedAIProviders));
             OnPropertyChanged(nameof(ShowClipboardPreview));
+            OnPropertyChanged(nameof(ShowClipboardHistoryButton));
 
             NotifyActiveProviderChanged();
 
