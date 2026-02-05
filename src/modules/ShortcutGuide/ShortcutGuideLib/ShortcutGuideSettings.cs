aaa3559ca2638b82c706a9ec4aa363370dfcf423
Gordon Lam (SH) <yeelam@microsoft.com>
2026-02-04 20:37:23 -0800

    fix: address issue #31205 - shortcut guide multi-monitor

---
+++ src/modules/ShortcutGuide/ShortcutGuideLib/ShortcutGuideSettings.cs
@@ -0,0 +1,9 @@
+// ShortcutGuideSettings.cs - Fix for Issue #31205
+namespace ShortcutGuide
+{
+    public class ShortcutGuideSettings
+    {
+        public int OverlayOpacity { get; set; } = 90;
+        public bool ShowOnAllMonitors { get; set; } = false;
+    }
+}
