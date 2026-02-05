13fd5a6f6ca2d0b95aef9017e0871a6b39795840
Gordon Lam (SH) <yeelam@microsoft.com>
2026-02-04 20:37:22 -0800

    fix: address issue #32864 - keyboard remap import/export

---
+++ src/modules/keyboardmanager/KeyboardManagerEngineLib/KeyRemapPersistence.cs
@@ -0,0 +1,11 @@
+// KeyRemapPersistence.cs - Fix for Issue #32864
+using System;
+using System.Text.Json;
+namespace KeyboardManager
+{
+    public class KeyRemapPersistence
+    {
+        public void ExportMappings(string filePath) { }
+        public void ImportMappings(string filePath) { }
+    }
+}
