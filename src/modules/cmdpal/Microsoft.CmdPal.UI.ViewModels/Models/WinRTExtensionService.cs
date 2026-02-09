82671661a8e944b773a53db2d31a3fb82cdbbe97
Michael Jolley <mjolley@microsoft.com>
2026-02-09 13:21:41 -0600

    WinRTExtensionService

--- src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/Models/WinRTExtensionService.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/Models/WinRTExtensionService.cs
@@ -2,8 +2,45 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.CmdPal.UI.ViewModels.Services;
+using Windows.Foundation;
+
 namespace Microsoft.CmdPal.UI.ViewModels.Models;
 
-public partial class WinRTExtensionService
+public partial class WinRTExtensionService : IExtensionService, IDisposable
 {
+    public event TypedEventHandler<CommandProviderWrapper, IEnumerable<CommandProviderWrapper>>? OnCommandProviderAdded;
+
+    public event TypedEventHandler<CommandProviderWrapper, IEnumerable<CommandProviderWrapper>>? OnCommandProviderRemoved;
+
+    public event TypedEventHandler<CommandProviderWrapper, IEnumerable<TopLevelViewModel>>? OnCommandsAdded;
+
+    public event TypedEventHandler<CommandProviderWrapper, IEnumerable<TopLevelViewModel>>? OnCommandsRemoved;
+
+    public Task DisableProviderAsync(string providerId)
+    {
+
+    }
+
+    public Task EnableProviderAsync(string providerId)
+    {
+
+    }
+
+    public Task<IEnumerable<CommandProviderWrapper>> GetCommandProviderWrappersAsync(WeakReference<IPageContext> weakPageContext, bool includeDisabledExtensions = false)
+    {
+    }
+
+    public Task<IEnumerable<TopLevelViewModel>> GetTopLevelCommandsAsync()
+    {
+    }
+
+    public Task SignalStopExtensionsAsync()
+    {
+    }
+
+    public void Dispose()
+    {
+
+    }
 }
