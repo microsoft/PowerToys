5eb36176601519133e786d02a78b1e2707e7b45a
Jiří Polášek <me@jiripolasek.com>
2026-02-11 18:25:23 +0100

    CmdPal: Localize additional UI strings in Registry and Services commands (#45508)
    ## Summary of the Pull Request
    
    This PR
    - Makes the "Browse the Windows registry" text for the Registry
    extension command localizable
    - Makes the "Manage Windows Services" text for the Windows services
    command localizable
    - Makes the Windows services filters ("all", "running", "paused",
    "stopped") localizable
    - Makes the Windows service primary command strings ("Start", "Stop",
    "Resume") localizable
    - Adds comment to "Open Windows Terminal profiles" resource
    
    <!-- Please review the items on the PR checklist before submitting-->
    ## PR Checklist
    
    - [ ] Closes: #xxx
    <!-- - [ ] Closes: #yyy (add separate lines for additional resolved
    issues) -->
    - [ ] **Communication:** I've discussed this with core contributors
    already. If the work hasn't been agreed, this work might be rejected
    - [ ] **Tests:** Added/updated and all pass
    - [ ] **Localization:** All end-user-facing strings can be localized
    - [ ] **Dev docs:** Added/updated
    - [ ] **New binaries:** Added on the required places
    - [ ] [JSON for
    signing](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ESRPSigning_core.json)
    for new binaries
    - [ ] [WXS for
    installer](https://github.com/microsoft/PowerToys/blob/main/installer/PowerToysSetup/Product.wxs)
    for new binaries and localization folder
    - [ ] [YML for CI
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/ci/templates/build-powertoys-steps.yml)
    for new test projects
    - [ ] [YML for signed
    pipeline](https://github.com/microsoft/PowerToys/blob/main/.pipelines/release.yml)
    - [ ] **Documentation updated:** If checked, please file a pull request
    on [our docs
    repo](https://github.com/MicrosoftDocs/windows-uwp/tree/docs/hub/powertoys)
    and link it here: #xxx
    
    <!-- Provide a more detailed description of the PR, other things fixed,
    or any additional comments/features here -->
    ## Detailed Description of the Pull Request / Additional comments
    
    <!-- Describe how you validated the behavior. Add automated tests
    wherever possible, but list manual validation steps taken as well -->
    ## Validation Steps Performed

--- src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.WindowsTerminal/Properties/Resources.Designer.cs
+++ src/modules/cmdpal/ext/Microsoft.CmdPal.Ext.WindowsTerminal/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.CmdPal.Ext.WindowsTerminal.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
