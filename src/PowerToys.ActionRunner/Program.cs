25847a36893d7fe02c860cadeff5c99cf509de08
Noraa Junker <noraa.junker@outlook.com>
2026-02-16 22:24:23 +0100

    Fix project file of ActionRunner

--- src/PowerToys.ActionRunner/Program.cs
+++ src/PowerToys.ActionRunner/Program.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
