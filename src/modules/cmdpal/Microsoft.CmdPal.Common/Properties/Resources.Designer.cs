c23fa4ef1d8089c037117dc01e9cbac15c6fd441
Jiří Polášek <me@jiripolasek.com>
2026-02-23 00:23:37 +0100

    Make resources in Microsoft.CmdPal.Common internal and reorder structure

--- src/modules/cmdpal/Microsoft.CmdPal.Common/Properties/Resources.Designer.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.Common/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace Microsoft.CmdPal.Common.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    internal class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Microsoft.CmdPal.Common.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
+        internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.CmdPal.Common.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace Microsoft.CmdPal.Common.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
+        internal static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -67,7 +67,7 @@ namespace Microsoft.CmdPal.Common.Properties {
         ///
         ///(While you’re at it, give the details below a quick skim — just to make sure there’s nothing personal you’d prefer not to share. It’s rare, but sometimes little surprises sneak in.).
         /// </summary>
-        public static string ErrorReport_Global_Preamble {
+        internal static string ErrorReport_Global_Preamble {
             get {
                 return ResourceManager.GetString("ErrorReport_Global_Preamble", resourceCulture);
             }
