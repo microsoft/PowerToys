424a61db6b9ed6d80ea6d7b5c0df4139c7404b98
Zach Teutsch <88554871+zateutsch@users.noreply.github.com>
2026-03-16 19:53:32 -0400

    undo changes

--- src/modules/keyboardmanager/KeyboardManagerEditorUI/KeyboardManagerEditorXAML/MainWindow.xaml.cs
+++ src/modules/keyboardmanager/KeyboardManagerEditorUI/KeyboardManagerEditorXAML/MainWindow.xaml.cs
@@ -2,10 +2,25 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using KeyboardManagerEditorUI.Helpers;
-using ManagedCommon;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using WinRT.Interop;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using WinUIEx;
 
 namespace KeyboardManagerEditorUI
@@ -16,20 +31,20 @@ namespace KeyboardManagerEditorUI
         {
             this.InitializeComponent();
             SetTitleBar();
+            this.Activated += MainWindow_Activated;
+            this.Closed += MainWindow_Closed;
         }
 
         private void SetTitleBar()
         {
-            WindowHelpers.ForceTopBorder1PixelInsetOnWindows10(WindowNative.GetWindowHandle(this));
             ExtendsContentIntoTitleBar = true;
+            this.SetIcon(@"Assets\KeyboardManagerEditor\Keyboard.ico");
             this.SetTitleBar(titleBar);
-            this.SetIcon("Assets\\KeyboardManagerEditor\\icon.ico");
-            Title = ResourceLoaderInstance.ResourceLoader.GetString("WindowTitle");
+            Title = "Keyboard Manager";
         }
 
         private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
         {
-            this.SetIcon("Assets\\KeyboardManagerEditor\\icon.ico");
             if (args.WindowActivationState == WindowActivationState.Deactivated)
             {
                 // Release the keyboard hook when the window is deactivated
