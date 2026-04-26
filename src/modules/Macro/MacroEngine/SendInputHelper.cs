dadd049fc86efe11d484162743b019a054e7abe6
Kavin Charles <kavincharles@gmail.com>
2026-04-26 15:46:10 +0530

    feat(macro): add SendInputHelper with ISendInputHelper interface

---
+++ src/modules/Macro/MacroEngine/SendInputHelper.cs
@@ -0,0 +1,81 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.UI.Input.KeyboardAndMouse;
+
+namespace PowerToys.MacroEngine;
+
+public interface ISendInputHelper
+{
+    void PressKeyCombo(string combo);
+    void TypeText(string text);
+}
+
+internal sealed class SendInputHelper : ISendInputHelper
+{
+    public void PressKeyCombo(string combo)
+    {
+        var (modifierVks, mainVk) = KeyParser.ParseKeyCombo(combo);
+        var inputs = new List<INPUT>();
+
+        foreach (var mod in modifierVks)
+        {
+            inputs.Add(KeyDown(mod));
+        }
+
+        inputs.Add(KeyDown(mainVk));
+        inputs.Add(KeyUp(mainVk));
+
+        foreach (var mod in Enumerable.Reverse(modifierVks))
+        {
+            inputs.Add(KeyUp(mod));
+        }
+
+        Send(inputs);
+    }
+
+    public void TypeText(string text)
+    {
+        var inputs = new List<INPUT>(text.Length * 2);
+        foreach (char c in text)
+        {
+            inputs.Add(UnicodeKeyDown(c));
+            inputs.Add(UnicodeKeyUp(c));
+        }
+
+        Send(inputs);
+    }
+
+    private static unsafe void Send(IList<INPUT> inputs)
+    {
+        var arr = inputs.ToArray();
+        PInvoke.SendInput(arr, Marshal.SizeOf<INPUT>());
+    }
+
+    private static INPUT KeyDown(ushort vk) => new()
+    {
+        type = INPUT_TYPE.INPUT_KEYBOARD,
+        Anonymous = new() { ki = new KEYBDINPUT { wVk = (VIRTUAL_KEY)vk } },
+    };
+
+    private static INPUT KeyUp(ushort vk) => new()
+    {
+        type = INPUT_TYPE.INPUT_KEYBOARD,
+        Anonymous = new() { ki = new KEYBDINPUT { wVk = (VIRTUAL_KEY)vk, dwFlags = KEYBD_EVENT_FLAGS.KEYEVENTF_KEYUP } },
+    };
+
+    private static INPUT UnicodeKeyDown(char c) => new()
+    {
+        type = INPUT_TYPE.INPUT_KEYBOARD,
+        Anonymous = new() { ki = new KEYBDINPUT { wScan = c, dwFlags = KEYBD_EVENT_FLAGS.KEYEVENTF_UNICODE } },
+    };
+
+    private static INPUT UnicodeKeyUp(char c) => new()
+    {
+        type = INPUT_TYPE.INPUT_KEYBOARD,
+        Anonymous = new() { ki = new KEYBDINPUT { wScan = c, dwFlags = KEYBD_EVENT_FLAGS.KEYEVENTF_UNICODE | KEYBD_EVENT_FLAGS.KEYEVENTF_KEYUP } },
+    };
+}
