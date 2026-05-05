6298cd0f282db4846e0bc7082d251cd430248003
raihua <raihua.dev@gmail.com>
2026-05-06 08:38:35 +0930

    Add POLICY_CONFIGURE_ENABLED_AWAKE_INDEFINITELY to gpo.h

--- src/common/utils/gpo.h
+++ src/common/utils/gpo.h
@@ -28,6 +28,7 @@ namespace powertoys_gpo
     const std::wstring POLICY_CONFIGURE_ENABLED_GLOBAL_ALL_UTILITIES = L"ConfigureGlobalUtilityEnabledState";
     const std::wstring POLICY_CONFIGURE_ENABLED_ALWAYS_ON_TOP = L"ConfigureEnabledUtilityAlwaysOnTop";
     const std::wstring POLICY_CONFIGURE_ENABLED_AWAKE = L"ConfigureEnabledUtilityAwake";
+    const std::wstring POLICY_CONFIGURE_ENABLED_AWAKE_INDEFINITELY = L"ConfigureEnabledUtilityAwakeIndefinitely";
     const std::wstring POLICY_CONFIGURE_ENABLED_CMD_NOT_FOUND = L"ConfigureEnabledUtilityCmdNotFound";
     const std::wstring POLICY_CONFIGURE_ENABLED_COLOR_PICKER = L"ConfigureEnabledUtilityColorPicker";
     const std::wstring POLICY_CONFIGURE_ENABLED_CROP_AND_LOCK = L"ConfigureEnabledUtilityCropAndLock";
