86203eff114c72c4717e530192a5d0140482b4d2
a37543406-boop <a37543406@gmail.com>
2026-06-28 17:49:44 +0330

    Update ClipboardHelper.cs

--- src/modules/colorPicker/ColorPickerUI/Helpers/ClipboardHelper.cs
+++ src/modules/colorPicker/ColorPickerUI/Helpers/ClipboardHelper.cs
@@ -5,8 +5,6 @@
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
-using System.Collections.Generic;
-using System.Linq;
 using ManagedCommon;
 using static ColorPicker.NativeMethods;
 
@@ -14,20 +12,19 @@ namespace ColorPicker.Helpers
 {
     public static class ClipboardHelper
     {
+        /// <summary>
+        /// Defined error code for "clipboard can't open"
+        /// </summary>
         private const uint ErrorCodeClipboardCantOpen = 0x800401D0;
-        private static readonly List<string> _colorHistory = new List<string>();
 
         public static void CopyToClipboard(string colorRepresentationToCopy)
         {
             if (!string.IsNullOrEmpty(colorRepresentationToCopy))
             {
-                // ذخیره در لیست تاریخچه (بدون نیاز به فایل جداگانه)
-                if (!_colorHistory.Contains(colorRepresentationToCopy))
-                {
-                    _colorHistory.Insert(0, colorRepresentationToCopy);
-                    if (_colorHistory.Count > 10) _colorHistory.RemoveAt(10);
-                }
+                // ثبت رنگ در تاریخچه برنامه از طریق AppStateHandler
+                AppStateHandler.AddToHistory(colorRepresentationToCopy);
 
+                // nasty hack - sometimes clipboard can be in use and it will raise and exception
                 for (int i = 0; i < 10; i++)
                 {
                     try
@@ -56,7 +53,5 @@ namespace ColorPicker.Helpers
                 }
             }
         }
-
-        public static List<string> GetHistory() => _colorHistory.ToList();
     }
 }
