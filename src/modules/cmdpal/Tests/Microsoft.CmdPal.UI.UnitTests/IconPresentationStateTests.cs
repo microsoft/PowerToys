6d732d8a5d72d48f56df68034c6f4696777ef224
Jiří Polášek <me@jiripolasek.com>
2026-08-12 06:33:39 +0200

    Add placement-aware fallbacks for recycled icons

---
+++ src/modules/cmdpal/Tests/Microsoft.CmdPal.UI.UnitTests/IconPresentationStateTests.cs
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CmdPal.UI.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.CmdPal.UI.UnitTests;
+
+[TestClass]
+public class IconPresentationStateTests
+{
+    [TestMethod]
+    public void SourceChangeImmediatelyReplacesResolvedSourceWithPlacementFallback()
+    {
+        var state = new IconPresentationState<string>
+        {
+            PlacementFallback = "placement",
+        };
+        state.SetResolvedSource("preceding item", expectsImageSource: true);
+
+        Assert.IsTrue(state.ResolvedSourceExpectsImage);
+
+        state.BeginSourceChange();
+
+        Assert.AreEqual("placement", state.SelectSource(preferFallbackForResolvedSource: false));
+        Assert.IsFalse(state.HasResolvedSource);
+        Assert.IsFalse(state.ResolvedSourceExpectsImage);
+    }
+
+    [TestMethod]
+    public void PlacementFallbackTakesPrecedenceOverRequestFallback()
+    {
+        var state = new IconPresentationState<string>
+        {
+            PlacementFallback = "placement",
+        };
+        state.SetRequestFallback("request");
+
+        Assert.AreEqual("placement", state.SelectSource(preferFallbackForResolvedSource: false));
+    }
+
+    [TestMethod]
+    public void ResolvedSourceReplacesFallbackUnlessPlacementPrefersFallback()
+    {
+        var state = new IconPresentationState<string>
+        {
+            PlacementFallback = "fallback",
+        };
+        state.SetResolvedSource("resolved", expectsImageSource: false);
+
+        Assert.AreEqual("resolved", state.SelectSource(preferFallbackForResolvedSource: false));
+        Assert.AreEqual("fallback", state.SelectSource(preferFallbackForResolvedSource: true));
+    }
+
+    [TestMethod]
+    public void NewSourceDoesNotRetainPreviousRequestFallback()
+    {
+        var state = new IconPresentationState<string>();
+        state.SetRequestFallback("preceding request");
+
+        state.BeginSourceChange();
+
+        Assert.IsNull(state.RequestFallback);
+        Assert.IsNull(state.SelectSource(preferFallbackForResolvedSource: false));
+    }
+}
