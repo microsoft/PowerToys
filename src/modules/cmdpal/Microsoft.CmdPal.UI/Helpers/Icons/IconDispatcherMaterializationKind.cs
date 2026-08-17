61ab546487b6de67bdc4d90c2cdd591ba19ba3f1
Jiří Polášek <me@jiripolasek.com>
2026-08-17 19:30:35 +0200

    Add CmdPal icon loading observer foundation

---
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Helpers/Icons/IconDispatcherMaterializationKind.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.CmdPal.UI.Helpers;
+
+internal enum IconDispatcherMaterializationKind
+{
+    Unknown,
+    Empty,
+    BitmapUri,
+    SvgUri,
+    Glyph,
+    Binary,
+    SvgData,
+    BitmapStream,
+}
