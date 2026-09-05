f5ddf58fedf4e5bd28a30dbecb389a7f4b2bef67
Jiří Polášek <me@jiripolasek.com>
2026-09-05 12:46:26 +0200

    Refresh cached shelf context menus before reuse

--- src/modules/cmdpal/Microsoft.CmdPal.UI/Pages/ShellPage.xaml.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Pages/ShellPage.xaml.cs
@@ -1560,6 +1560,8 @@ public sealed partial class ShellPage : Microsoft.UI.Xaml.Controls.Page,
         {
             if (_quickAccessShelfContextViewModel is not null)
             {
+                // dirty patch here
+                QuickAccessShelfContextControl.ViewModel.UpdateContextItems();
                 return Task.FromResult<ListItemViewModel?>(_quickAccessShelfContextViewModel);
             }
 
