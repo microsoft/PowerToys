c7ff8e6b997a53aa8d03bbe5d7c8d866e27df71e
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2026-09-17 19:27:15 -0400

    Multiple structural fixes

--- src/modules/RobocopyUI/RobocopyUI.UI/Views/HomePage.xaml.cs
+++ src/modules/RobocopyUI/RobocopyUI.UI/Views/HomePage.xaml.cs
@@ -9,7 +9,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Windows.Storage.Pickers;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
