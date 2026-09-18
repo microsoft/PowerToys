2f55b75a4a1f6ed261b0103f0b5e74361f39feba
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2026-09-18 16:36:41 -0400

    Create command execution expander panel.

--- src/modules/RobocopyUI/RobocopyUI.UI/Views/HomePage.xaml.cs
+++ src/modules/RobocopyUI/RobocopyUI.UI/Views/HomePage.xaml.cs
@@ -433,8 +433,11 @@ public sealed partial class HomePage : Page
     private void RunRobocopy(string arguments)
     {
         OutputTextBox.Text = string.Empty;
-        OutputSelectorBarItem.IsEnabled = true;
-        OutputSelectorBarItem.IsSelected = true;
+        // OutputSelectorBarItem.IsEnabled = true; OutputSelectorBarItem.IsSelected = true;
+        CommandOutputExpander.IsExpanded = true;
+        CommandOutputExpander.Header = "Running...";
+
+
         var startInfo = new ProcessStartInfo
         {
             FileName = "robocopy.exe",
