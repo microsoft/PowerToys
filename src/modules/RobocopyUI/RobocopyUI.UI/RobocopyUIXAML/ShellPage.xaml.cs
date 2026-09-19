a862eaaabc6b735e227200b4acbbf43bb618ed58
Noraa Junker <Noraa.Junker@outlook.com>
2026-09-19 17:56:30 +0200

    Add icon and settings images, as well as a modern titlebar

--- src/modules/RobocopyUI/RobocopyUI.UI/RobocopyUIXAML/ShellPage.xaml.cs
+++ src/modules/RobocopyUI/RobocopyUI.UI/RobocopyUIXAML/ShellPage.xaml.cs
@@ -2,6 +2,9 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using ManagedCommon;
+using Microsoft.PowerToys.Common.UI.Controls.Window;
 using Microsoft.UI.Xaml;
 using RobocopyUI.Helpers;
 
@@ -16,6 +19,18 @@ namespace RobocopyUI
             this.Title = ResourceLoaderInstance.ResourceLoader.GetString("ShellPageWindow/Title");
 
             ContentFrame.Navigate(typeof(HomePage));
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Extend the canvas to include the title bar so the app can support theming
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(titleBar);
+            TitleBarHelper.SetPreferredTheme(this);
+
+            AppWindow.SetIcon("Assets\\RobocopyUI\\RobocopyUI.ico");
+            titleBar.Title = "Robocopy UI";
+            AppWindow.Title = "Robocopy UI";
         }
     }
 }
