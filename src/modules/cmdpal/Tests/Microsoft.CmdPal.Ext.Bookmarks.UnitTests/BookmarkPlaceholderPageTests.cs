8e921ce7ece45a5862172c193982ae07b1371073
Dhruv Shah <dhruv072002@gmail.com>
2026-09-24 11:56:33 -0700

    Fix bookmark placeholder test build

--- src/modules/cmdpal/Tests/Microsoft.CmdPal.Ext.Bookmarks.UnitTests/BookmarkPlaceholderPageTests.cs
+++ src/modules/cmdpal/Tests/Microsoft.CmdPal.Ext.Bookmarks.UnitTests/BookmarkPlaceholderPageTests.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
